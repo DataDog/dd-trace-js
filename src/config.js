0a4a4dc489771ba705b286619431849f6505c3bf
Roch Devost <roch.devost@gmail.com>
2018-02-23 08:47:30 -0500

    add usage documentation (#20)

--- src/config.js
+++ src/config.js
@@ -8,16 +8,16 @@ class Config {
   constructor (options) {
     options = options || {}
 
-    const enabled = coalesce(options.enabled, platform.env('DATADOG_TRACE_ENABLED'), true)
-    const debug = coalesce(options.debug, platform.env('DATADOG_TRACE_DEBUG'), false)
+    const enabled = coalesce(options.enabled, platform.env('DD_TRACE_ENABLED'), true)
+    const debug = coalesce(options.debug, platform.env('DD_TRACE_DEBUG'), false)
     const protocol = 'http'
-    const hostname = coalesce(options.hostname, platform.env('DATADOG_TRACE_AGENT_HOSTNAME'), 'localhost')
-    const port = coalesce(options.port, platform.env('DATADOG_TRACE_AGENT_PORT'), 8126)
+    const hostname = coalesce(options.hostname, platform.env('DD_TRACE_AGENT_HOSTNAME'), 'localhost')
+    const port = coalesce(options.port, platform.env('DD_TRACE_AGENT_PORT'), 8126)
 
     this.enabled = String(enabled) === 'true'
     this.debug = String(debug) === 'true'
-    this.service = coalesce(options.service, platform.env('DATADOG_SERVICE_NAME'))
-    this.env = coalesce(options.env, platform.env('DATADOG_ENV'))
+    this.service = coalesce(options.service, platform.env('DD_SERVICE_NAME'))
+    this.env = coalesce(options.env, platform.env('DD_ENV'))
     this.url = new URL(`${protocol}://${hostname}:${port}`)
     this.tags = coalesce(options.tags, {})
     this.flushInterval = 2000
