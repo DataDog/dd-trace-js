db11a52b929abff24815c193aa94ab032be06761
rochdev <roch.devost@datadoghq.com>
2018-05-07 16:30:50 -0400

    update express plugin to support nested router paths

--- src/plugins/express.js
+++ src/plugins/express.js
@@ -5,14 +5,11 @@ const Tags = opentracing.Tags
 const FORMAT_HTTP_HEADERS = opentracing.FORMAT_HTTP_HEADERS
 const shimmer = require('shimmer')
 const METHODS = require('methods').concat('use', 'route', 'param', 'all')
+const pathToRegExp = require('path-to-regexp')
 
 const OPERATION_NAME = 'express.request'
 
-function patch (express, tracer, config) {
-  METHODS.forEach((method) => {
-    shimmer.wrap(express.application, method, wrapper)
-  })
-
+function createWrapMethod (tracer, config) {
   function middleware (req, res, next) {
     const url = `${req.protocol}://${req.get('host')}${req.originalUrl}`
     const childOf = tracer.extract(FORMAT_HTTP_HEADERS, req.headers)
@@ -30,9 +27,10 @@ function patch (express, tracer, config) {
       res.end = function () {
         res.end = originalEnd
         const returned = res.end.apply(this, arguments)
+        const paths = tracer._context.get('express.paths')
 
-        if (req.route && req.route.path) {
-          span.setTag('resource.name', req.route.path)
+        if (paths) {
+          span.setTag('resource.name', paths.join(' -> '))
         }
 
         span.setTag('service.name', config.service || tracer._service)
@@ -48,8 +46,8 @@ function patch (express, tracer, config) {
     })
   }
 
-  function wrapper (original) {
-    return function () {
+  return function wrapMethod (original) {
+    return function methodWithTrace () {
       if (!this._datadog_trace_patched && !this._router) {
         this._datadog_trace_patched = true
         this.use(middleware)
@@ -59,10 +57,82 @@ function patch (express, tracer, config) {
   }
 }
 
-function unpatch (express) {
-  METHODS.forEach((method) => {
-    shimmer.unwrap(express.application, method)
+function createWrapProcessParams (tracer, config) {
+  const context = tracer._context
+
+  return function wrapProcessParams (processParams) {
+    return function processParamsWithTrace (layer, called, req, res, done) {
+      const matchers = layer._datadog_matchers
+      let paths = context.get('express.paths') || []
+
+      if (matchers) {
+        // Try to guess which path actually matched
+        for (let i = 0; i < matchers.length; i++) {
+          if (matchers[i].test(layer.path)) {
+            paths = paths.concat(matchers[i].path)
+
+            context.run(() => {
+              context.set('express.paths', paths)
+              layer.handle = context.bind(layer.handle)
+            })
+
+            break
+          }
+        }
+      }
+
+      return processParams.apply(this, arguments)
+    }
+  }
+}
+
+function createWrapRouterMethod () {
+  return function wrapRouterMethod (original) {
+    return function methodWithTrace (fn) {
+      const offset = this.stack.length
+      const router = original.apply(this, arguments)
+      const matchers = extractMatchers(fn)
+
+      this.stack.slice(offset).forEach(layer => {
+        layer._datadog_matchers = matchers
+      })
+
+      return router
+    }
+  }
+}
+
+function extractMatchers (fn) {
+  const arg = flatten([].concat(fn))
+
+  if (typeof arg[0] === 'function') {
+    return []
+  }
+
+  return arg.map(pattern => ({
+    path: pattern.toString(),
+    test: path => pathToRegExp(pattern).test(path)
+  }))
+}
+
+function flatten (arr) {
+  return arr.reduce((acc, val) => Array.isArray(val) ? acc.concat(flatten(val)) : acc.concat(val), [])
+}
+
+function patch (express, tracer, config) {
+  METHODS.forEach(method => {
+    shimmer.wrap(express.application, method, createWrapMethod(tracer, config))
   })
+  shimmer.wrap(express.Router, 'process_params', createWrapProcessParams(tracer, config))
+  shimmer.wrap(express.Router, 'use', createWrapRouterMethod(tracer, config))
+  shimmer.wrap(express.Router, 'route', createWrapRouterMethod(tracer, config))
+}
+
+function unpatch (express) {
+  METHODS.forEach(method => shimmer.unwrap(express.application, method))
+  shimmer.unwrap(express.Router, 'process_params')
+  shimmer.unwrap(express.Router, 'use')
+  shimmer.unwrap(express.Router, 'route')
 }
 
 module.exports = {
