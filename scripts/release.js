c91e233fd5e7443ed3b66016d4ce5eb163f819ee
rochdev <roch.devost@datadoghq.com>
2018-05-16 09:56:47 -0400

    update release script to support actual release process

--- scripts/release.js
+++ scripts/release.js
@@ -5,9 +5,4 @@ const exec = require('child_process').execSync
 exec('npm whoami')
 exec('git checkout master')
 exec('git pull')
-
-const pkg = require('../package.json')
-
-exec(`git tag v${pkg.version}`)
-exec(`git push origin refs/tags/v${pkg.version}`)
 exec('npm publish')
