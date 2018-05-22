7cd7c3b2200150ba05f039a04d6ac637796a21c9
rochdev <roch.devost@datadoghq.com>
2018-05-22 15:56:33 -0400

    split the release script to publish the docs separately

--- scripts/release.js
+++ scripts/release.js
@@ -1,29 +1,14 @@
 'use strict'
 
-const fs = require('fs')
 const exec = require('./helpers/exec')
 const title = require('./helpers/title')
 
 title(`Publishing package to the npm registry`)
 
+const pkg = require('../package.json')
+
 exec('npm whoami')
 exec('git checkout master')
 exec('git pull')
 exec('npm publish')
-
-title(`Publishing API documentation to GitHub Pages`)
-
-const pkg = require('../package.json')
-
-if (fs.existsSync('yarn.lock')) {
-  exec('yarn')
-} else {
-  exec('npm install')
-}
-
-exec('rm -rf ./out')
-exec('git clone -b gh-pages --single-branch git@github.com:DataDog/dd-trace-js.git out')
-exec('npm run jsdoc')
-exec('git add -A', { cwd: './out' })
-exec(`git commit -m "v${pkg.version}"`, { cwd: './out' })
-exec('git push', { cwd: './out' })
+exec(`node scripts/publish_docs.js "v${pkg.version}"`)
