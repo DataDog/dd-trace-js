b48d40b515796afcaac2781283c4882866bfe57f
rochdev <roch.devost@datadoghq.com>
2018-06-18 15:20:03 -0400

    update release script to also create the git tag

--- scripts/release.js
+++ scripts/release.js
@@ -5,10 +5,13 @@ const title = require('./helpers/title')
 
 title(`Publishing package to the npm registry`)
 
-const pkg = require('../package.json')
-
 exec('npm whoami')
 exec('git checkout master')
 exec('git pull')
+
+const pkg = require('../package.json')
+
+exec(`git tag v${pkg.version}`)
+exec(`git push origin refs/tags/v${pkg.version}`)
 exec('npm publish')
 exec(`node scripts/publish_docs.js "v${pkg.version}"`)
