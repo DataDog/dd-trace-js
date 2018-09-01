1f990c1faecf3c41aaab27f2c5215a563510accc
Eloy Durán <eloy.de.enige@gmail.com>
2018-09-02 00:09:21 +0200

    [http] Fix repeated traces on Node < v9 when tracing `https` requests.

--- src/plugins/http.js
+++ src/plugins/http.js
@@ -7,66 +7,81 @@ const Tags = opentracing.Tags
 const FORMAT_HTTP_HEADERS = opentracing.FORMAT_HTTP_HEADERS
 
 function patch (http, tracer, config) {
-  this.wrap(http, 'request', request => makeRequestTrace(request))
-  this.wrap(http, 'get', get => makeRequestTrace(get))
+  this.wrap(http, 'request', makeRequestTrace)
+  this.wrap(http, 'get', makeGetTrace)
 
   function makeRequestTrace (request) {
     return function requestTrace (options, callback) {
-      const uri = extractUrl(options)
-      const method = (options.method || 'GET').toUpperCase()
-
-      if (uri === `${tracer._url.href}/v0.3/traces`) {
-        return request.apply(this, [options, callback])
-      }
-
-      options = typeof options === 'string' ? url.parse(uri) : Object.assign({}, options)
-      options.headers = options.headers || {}
-
-      const parentScope = tracer.scopeManager().active()
-      const parent = parentScope && parentScope.span()
-      const span = tracer.startSpan('http.request', {
-        childOf: parent,
-        tags: {
-          [Tags.SPAN_KIND]: Tags.SPAN_KIND_RPC_CLIENT,
-          'service.name': getServiceName(tracer, config, options),
-          'resource.name': method,
-          'span.type': 'web',
-          'http.method': method,
-          'http.url': uri
-        }
-      })
-
-      if (!hasAmazonSignature(options)) {
-        tracer.inject(span, FORMAT_HTTP_HEADERS, options.headers)
-      }
-
       const req = request.call(this, options, callback)
 
-      req.on('socket', () => {
-        // empty the data stream when no other listener exists to consume it
-        if (req.listenerCount('response') === 1) {
-          req.on('response', res => res.resume())
+      // The `https.request` function before Node v9 internally invokes `http.request`. When both modules have been
+      // loaded by the user (indirectly) and both modules have been patched by us, that would lead to us double-counting
+      // the request.
+      if (isTraced(req, options)) {
+        return req
+      }
+      // Here we mark the `req` object, as opposed to also marking the `options` object like we do in `get`. The problem
+      // is that if we’d mark `options` here, it would get lost while upstream `request` derives a clone from it and
+      // thus would never get to the recursive invocation of `request`.
+      markAsTraced(req)
+
+      const span = createSpan(options)
+      if (span) {
+        const headers = createHeaders(span, options)
+        for (const header in headers) {
+          req.setHeader(header, headers[header])
         }
-      })
-
-      req.on('response', res => {
-        span.setTag(Tags.HTTP_STATUS_CODE, res.statusCode)
-
-        res.on('end', () => span.finish())
-      })
-
-      req.on('error', err => {
-        span.addTags({
-          'error.type': err.name,
-          'error.msg': err.message,
-          'error.stack': err.stack
-        })
-
-        span.finish()
-      })
-
+        registerEventHandlers(req, span)
+      }
       return req
     }
+  }
+
+  // Regardless of what Node version we’re running on, `https.get` does not invoke our patched `https.request`, thus we
+  // need to patch it as well.
+  function makeGetTrace (get) {
+    return function getTrace (options, callback) {
+      options = typeof options === 'string' ? url.parse(options) : Object.assign({}, options)
+      options.headers = options.headers || {}
+
+      // Prevent from double-counting in `request`.
+      markAsTraced(options)
+
+      const span = createSpan(options)
+      if (!span) {
+        return get.call(this, options, callback)
+      }
+
+      Object.assign(options.headers, createHeaders(span, options))
+
+      const req = get.call(this, options, callback)
+      registerEventHandlers(req, span)
+      return req
+    }
+  }
+
+  function registerEventHandlers (req, span) {
+    req.on('socket', () => {
+      // empty the data stream when no other listener exists to consume it
+      if (req.listenerCount('response') === 1) {
+        req.on('response', res => res.resume())
+      }
+    })
+
+    req.on('response', res => {
+      span.setTag(Tags.HTTP_STATUS_CODE, res.statusCode)
+      res.on('end', () => span.finish())
+    })
+
+    req.on('error', err => {
+      span.addTags({
+        'error.type': err.name,
+        'error.msg': err.message,
+        'error.stack': err.stack
+      })
+
+      span.finish()
+    })
   }
 
   function extractUrl (options) {
@@ -80,6 +95,55 @@ function patch (http, tracer, config) {
       pathname: options.path || options.pathname || '/'
     })
   }
+
+  function createSpan (options) {
+    const uri = extractUrl(options)
+    if (uri === `${tracer._url.href}/v0.3/traces`) {
+      return null
+    }
+    const method = (options.method || 'GET').toUpperCase()
+    const parentScope = tracer.scopeManager().active()
+    const parent = parentScope && parentScope.span()
+    const span = tracer.startSpan('http.request', {
+      childOf: parent,
+      tags: {
+        [Tags.SPAN_KIND]: Tags.SPAN_KIND_RPC_CLIENT,
+        'service.name': getServiceName(options),
+        'resource.name': method,
+        'span.type': 'web',
+        'http.method': method,
+        'http.url': uri
+      }
+    })
+    return span
+  }
+
+  function createHeaders (span, options) {
+    if (!hasAmazonSignature(options)) {
+      const headers = {}
+      tracer.inject(span, FORMAT_HTTP_HEADERS, headers)
+      return headers
+    }
+    return null
+  }
+
+  function getServiceName (options) {
+    if (config.splitByDomain) {
+      return getHost(options)
+    } else if (config.service) {
+      return config.service
+    }
+
+    return `${tracer._service}-http-client`
+  }
+}
+
+function markAsTraced (object) {
+  Object.defineProperty(object, '_datadog_traced', { value: true })
+}
+
+function isTraced (req, options) {
+  return req._datadog_traced || options._datadog_traced
 }
 
 function getHost (options) {
@@ -91,16 +155,6 @@ function getHost (options) {
   const port = options.port
 
   return [hostname, port].filter(val => val).join(':')
-}
-
-function getServiceName (tracer, config, options) {
-  if (config.splitByDomain) {
-    return getHost(options)
-  } else if (config.service) {
-    return config.service
-  }
-
-  return `${tracer._service}-http-client`
 }
 
 function hasAmazonSignature (options) {
