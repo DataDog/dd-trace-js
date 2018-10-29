68e45d0c4ee04efc6bf0ce88ae63c14b01faa133
rochdev <roch.devost@datadoghq.com>
2018-10-29 15:48:41 -0400

    add support for bluebird

---
+++ src/plugins/bluebird.js
@@ -0,0 +1,36 @@
+'use strict'
+
+function createWrapThen (tracer, config) {
+  return function wrapThen (then) {
+    return function thenWithTrace (didFulfill, didReject) {
+      arguments[0] = wrapCallback(tracer, didFulfill)
+      arguments[1] = wrapCallback(tracer, didReject)
+
+      return then.apply(this, arguments)
+    }
+  }
+}
+
+function wrapCallback (tracer, callback) {
+  if (typeof callback !== 'function') return callback
+
+  const scope = tracer.scopeManager().active()
+
+  return function () {
+    tracer.scopeManager().activate(scope.span())
+    return callback.apply(this, arguments)
+  }
+}
+
+module.exports = [
+  {
+    name: 'bluebird',
+    versions: ['2.0.2 - 3'], // 2.0.0 and 2.0.1 were removed from npm
+    patch (Promise, tracer, config) {
+      this.wrap(Promise.prototype, '_then', createWrapThen(tracer, config))
+    },
+    unpatch (Promise) {
+      this.unwrap(Promise.prototype, '_then')
+    }
+  }
+]
