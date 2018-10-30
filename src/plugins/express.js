5ef4f6b670f49e567565c88cc0b34c0b33a9825e
Roch Devost <roch.devost@gmail.com>
2018-10-30 16:34:51 -0400

    add support for the router module (#342)

--- src/plugins/express.js
+++ src/plugins/express.js
@@ -1,8 +1,8 @@
 'use strict'
 
 const METHODS = require('methods').concat('use', 'route', 'param', 'all')
-const pathToRegExp = require('path-to-regexp')
 const web = require('./util/web')
+const routerPlugin = require('./router')
 
 function createWrapMethod (tracer, config) {
   config = web.normalizeConfig(config)
@@ -26,128 +26,14 @@ function createWrapMethod (tracer, config) {
   }
 }
 
-function createWrapHandle (tracer, config) {
-  return function wrapHandle (handle) {
-    return function handleWithTracer (req) {
-      web.patch(req)
-
-      return handle.apply(this, arguments)
-    }
-  }
-}
-
-function createWrapProcessParams (tracer, config) {
-  return function wrapProcessParams (processParams) {
-    return function processParamsWithTrace (layer, called, req, res, done) {
-      const matchers = layer._datadog_matchers
-
-      req = done ? req : called
-
-      if (matchers) {
-        // Try to guess which path actually matched
-        for (let i = 0; i < matchers.length; i++) {
-          if (matchers[i].test(layer.path)) {
-            web.enterRoute(req, matchers[i].path)
-
-            break
-          }
-        }
-      }
-
-      return processParams.apply(this, arguments)
-    }
-  }
-}
-
-function createWrapRouterMethod (tracer) {
-  return function wrapRouterMethod (original) {
-    return function methodWithTrace (fn) {
-      const offset = this.stack.length
-      const router = original.apply(this, arguments)
-      const matchers = extractMatchers(fn)
-
-      this.stack.slice(offset).forEach(layer => {
-        const handle = layer.handle
-
-        if (handle.length === 4) {
-          layer.handle = (error, req, res, next) => {
-            return handle.call(layer, error, req, res, wrapNext(tracer, layer, req, next))
-          }
-        } else {
-          layer.handle = (req, res, next) => {
-            return handle.call(layer, req, res, wrapNext(tracer, layer, req, next))
-          }
-        }
-
-        layer._datadog_matchers = matchers
-      })
-
-      return router
-    }
-  }
-}
-
-function wrapNext (tracer, layer, req, next) {
-  if (!web.active(req)) {
-    return next
-  }
-
-  const originalNext = next
-
-  web.reactivate(req)
-
-  return function (error) {
-    if (!error && layer.path && !isFastStar(layer)) {
-      web.exitRoute(req)
-    }
-
-    process.nextTick(() => {
-      originalNext.apply(null, arguments)
-    })
-  }
-}
-
-function extractMatchers (fn) {
-  const arg = flatten([].concat(fn))
-
-  if (typeof arg[0] === 'function') {
-    return []
-  }
-
-  return arg.map(pattern => ({
-    path: pattern instanceof RegExp ? `(${pattern})` : pattern,
-    test: path => pathToRegExp(pattern).test(path)
-  }))
-}
-
-function isFastStar (layer) {
-  if (layer.regexp.fast_star !== undefined) {
-    return layer.regexp.fast_star
-  }
-
-  return layer._datadog_matchers.some(matcher => matcher.path === '*')
-}
-
-function flatten (arr) {
-  return arr.reduce((acc, val) => Array.isArray(val) ? acc.concat(flatten(val)) : acc.concat(val), [])
-}
-
 function patch (express, tracer, config) {
-  METHODS.forEach(method => {
-    this.wrap(express.application, method, createWrapMethod(tracer, config))
-  })
-  this.wrap(express.Router, 'handle', createWrapHandle(tracer, config))
-  this.wrap(express.Router, 'process_params', createWrapProcessParams(tracer, config))
-  this.wrap(express.Router, 'use', createWrapRouterMethod(tracer, config))
-  this.wrap(express.Router, 'route', createWrapRouterMethod(tracer, config))
+  this.wrap(express.application, METHODS, createWrapMethod(tracer, config))
+  routerPlugin.patch.call(this, { prototype: express.Router }, tracer, config)
 }
 
 function unpatch (express) {
-  METHODS.forEach(method => this.unwrap(express.application, method))
-  this.unwrap(express.Router, 'handle')
-  this.unwrap(express.Router, 'process_params')
-  this.unwrap(express.Router, 'use')
-  this.unwrap(express.Router, 'route')
+  this.unwrap(express.application, METHODS)
+  routerPlugin.unpatch.call(this, { prototype: express.Router })
 }
 
 module.exports = {
