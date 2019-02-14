f69cf6861f83f4d8e0827a1b73ecdf1f91b14fd8
Ryan Gordon <ryan.gordon@getcruise.com>
2019-02-14 12:42:51 -0800

    Fixing lint

--- src/platform/node/uint64be.js
+++ src/platform/node/uint64be.js
@@ -1,7 +1,9 @@
+'use strict'
+
 const Uint64BE = require('int64-buffer').Uint64BE
 
-Uint64BE.prototype.toJSON = function toJSON() {
-	return this.toString()
+Uint64BE.prototype.toJSON = function toJSON () {
+  return this.toString()
 }
 
 module.exports = Uint64BE
