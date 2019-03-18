14b365de68120d2338a4d8536d6f328b87347029
rochdev <roch.devost@datadoghq.com>
2019-03-18 13:26:41 -0400

    fix download location

--- scripts/post_install.js
+++ scripts/post_install.js
@@ -38,19 +38,13 @@ function download (tag) {
   console.log('Downloading prebuilt binaries for native addons.')
 
   const promise = new Promise((resolve, reject) => {
-    const folder = path.join(__dirname, '..', 'prebuilds')
     const url = `https://github.com/DataDog/dd-trace-js/releases/download/${tag}/addons-${name}.zip`
-
     const req = https.get(url, res => {
       if (res.statusCode !== 200) {
         return reject(new Error('Server replied with not OK status code.'))
       }
 
-      if (!fs.existsSync(folder)) {
-        fs.mkdirSync(folder)
-      }
-
-      const file = fs.createWriteStream(path.join(__dirname, '..', 'prebuilds', `addons-${name}.zip`))
+      const file = fs.createWriteStream(path.join(__dirname, '..', `addons-${name}.zip`))
       const stream = res.pipe(file)
 
       stream.on('error', reject)
@@ -69,6 +63,12 @@ function download (tag) {
 
 function extract () {
   console.log('Extracting prebuilt binaries.')
+
+  const folder = path.join(__dirname, '..', 'prebuilds')
+
+  if (!fs.existsSync(folder)) {
+    fs.mkdirSync(folder)
+  }
 
   const promise = tar.extract({
     file: `addons-${name}.tgz`,
