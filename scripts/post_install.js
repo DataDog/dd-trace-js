3a06b5c71e34808926e7680750dd4adb4d118254
rochdev <roch.devost@datadoghq.com>
2019-03-18 11:48:22 -0400

    use tar instead of archiver + code cleanup

--- scripts/post_install.js
+++ scripts/post_install.js
@@ -9,51 +9,97 @@ const path = require('path')
 const os = require('os')
 const pkg = require('../package.json')
 
-if (process.env.DD_NATIVE_METRICS !== 'false') {
-  download(err => {
-    if (err) {
-      console.log()
-      console.log([
-        'Download of prebuilt binaries failed.',
-        'Attempting to compile new binaries.'
-      ].join(' '))
-      console.log()
-    }
+const name = `${os.platform()}-${os.arch()}`
 
-    try {
-      execSync('npm run install:rebuild --scripts-prepend-node-path', { stdio: [0, 1, 2] })
-    } catch (e) {
-      console.log()
-      console.log([
-        'Compilation of native modules failed.',
-        'Falling back to JavaScript only.',
-        'Some functionalities may not be available.'
-      ].join(' '))
-      console.log()
-    }
-  })
+if (process.env.DD_NATIVE_METRICS !== 'false') {
+  getReleaseTag()
+    .then(download)
+    .then(extract)
+    .then(cleanup)
+    .catch(rebuild)
 }
 
-function download (cb) {
-  const folder = path.join(__dirname, '..', 'prebuilds')
-  const name = `${os.platform()}-${os.arch()}`
-  const version = ~pkg.version.indexOf('beta') ? 'latest' : `v${pkg.version}`
-  const url = `https://github.com/DataDog/dd-trace-js/releases/download/${version}/addons-${name}.zip`
+function rebuild () {
+  console.log('Trying to compile new binaries.')
 
-  const req = https.get(url, res => {
-    if (res.statusCode !== 200) {
-      return cb(new Error('Server replied with not OK status code.'))
-    }
+  try {
+    execSync('npm run install:rebuild --silent --scripts-prepend-node-path', { stdio: [0, 1, 2] })
+  } catch (e) {
+    console.log([
+      'Compilation of native modules failed.',
+      'Falling back to JavaScript only.',
+      'Some functionalities may not be available.'
+    ].join(' '))
+  }
+}
 
-    if (!fs.existsSync(folder)) {
-      fs.mkdirSync(folder)
-    }
+function download (tag) {
+  console.log('Downloading prebuilt binaries for native addons.')
 
-    const file = fs.createWriteStream(path.join(__dirname, '..', 'prebuilds', `addons-${name}.zip`))
-    const stream = res.pipe(file)
+  const promise = new Promise((resolve, reject) => {
+    const folder = path.join(__dirname, '..', 'prebuilds')
+    const url = `https://github.com/DataDog/dd-trace-js/releases/download/${tag}/addons-${name}.zip`
 
-    stream.on('finish', () => cb())
+    const req = https.get(url, res => {
+      if (res.statusCode !== 200) {
+        return reject(new Error('Server replied with not OK status code.'))
+      }
+
+      if (!fs.existsSync(folder)) {
+        fs.mkdirSync(folder)
+      }
+
+      const file = fs.createWriteStream(path.join(__dirname, '..', 'prebuilds', `addons-${name}.zip`))
+      const stream = res.pipe(file)
+
+      stream.on('error', reject)
+      stream.on('finish', () => resolve())
+    })
+
+    req.on('error', reject)
   })
 
-  req.on('error', cb)
+  promise.catch(() => {
+    console.log('Download of prebuilt binaries failed.')
+  })
+
+  return promise
+}
+
+function extract () {
+  // TODO
+}
+
+function cleanup () {
+  // TODO
+}
+
+function getReleaseTag () {
+  if (/^\d+\.\d+\.\d+$/.test(pkg.version)) { // official release
+    return Promise.resolve(`v${pkg.version}`)
+  }
+
+  return new Promise((resolve, reject) => {
+    const req = https.get(`https://github.com/DataDog/dd-trace-js/releases/latest`, res => {
+      let data = ''
+
+      if (res.statusCode !== 200) {
+        return reject(new Error('Server replied with not OK status code.'))
+      }
+
+      res.on('data', chunk => {
+        data += chunk
+      })
+
+      res.on('finish', () => {
+        try {
+          resolve(JSON.parse(data).tag_name)
+        } catch (e) {
+          reject(e)
+        }
+      })
+    })
+
+    req.on('error', reject)
+  })
 }
--- scripts/prebuild.js
+++ scripts/prebuild.js
@@ -2,33 +2,25 @@
 
 const prebuildify = require('prebuildify')
 const abi = require('node-abi')
-const fs = require('fs')
 const path = require('path')
 const os = require('os')
-const archiver = require('archiver')
+const tar = require('tar')
 
 const name = `${process.env.ARCH || os.platform()}-${os.arch()}`
-
-const opts = {
-  targets: abi.supportedTargets.filter(target => target.runtime === 'node'),
-  strip: false
-}
 
 const cb = err => {
   if (err) throw err
 
-  const output = fs.createWriteStream(path.join(__dirname, '..', 'prebuilds', `addons-${name}.zip`))
-  const archive = archiver('zip', {
-    zlib: { level: 9 }
-  })
-
-  archive.on('error', function (err) {
-    throw err
-  })
-
-  archive.pipe(output)
-  archive.directory(path.join(__dirname, '..', 'prebuilds', name), name)
-  archive.finalize()
+  tar.create({
+    gzip: true,
+    sync: true,
+    portable: true,
+    file: `addons-${name}.tgz`,
+    cwd: path.join(__dirname, '..', 'prebuilds')
+  }, [name])
 }
 
-prebuildify(opts, cb)
+prebuildify({
+  targets: abi.supportedTargets.filter(target => target.runtime === 'node'),
+  strip: false
+}, cb)
