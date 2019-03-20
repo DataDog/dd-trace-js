ee347a026c8e060d616fcaf7558a8d0c8bc29dac
rochdev <roch.devost@datadoghq.com>
2019-03-20 09:40:47 -0400

    update to buffer by payload size instead of by trace count

--- src/opentracing/tracer.js
+++ src/opentracing/tracer.js
@@ -30,7 +30,7 @@ class DatadogTracer extends Tracer {
     this._logInjection = config.logInjection
     this._analytics = config.analytics
     this._prioritySampler = new PrioritySampler(config.env)
-    this._writer = new Writer(this._prioritySampler, config.url, config.bufferSize)
+    this._writer = new Writer(this._prioritySampler, config.url)
     this._recorder = new Recorder(this._writer, config.flushInterval)
     this._recorder.init()
     this._sampler = new Sampler(config.sampleRate)
