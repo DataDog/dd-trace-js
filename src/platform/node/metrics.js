0a94bcff887b2034406f0c0bc5a9fc2e1e4638b1
rochdev <roch.devost@datadoghq.com>
2019-03-25 12:44:21 -0400

    add log to check why the tests are failing

--- src/platform/node/metrics.js
+++ src/platform/node/metrics.js
@@ -25,6 +25,7 @@ module.exports = function () {
         nativeMetrics = require('node-gyp-build')(path.join(__dirname, '..', '..', '..'))
         nativeMetrics.start()
       } catch (e) {
+        console.log(e) // eslint-disable-line no-console
         log.error('Unable to load native metrics module. Some metrics will not be available.')
       }
 
