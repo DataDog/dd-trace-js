878d8d08da402337937688dd85485ecd885da3ff
rochdev <roch.devost@datadoghq.com>
2019-03-27 14:26:54 -0400

    try to fix windows build

--- src/native/metrics/utils.cpp
+++ src/native/metrics/utils.cpp
@@ -3,9 +3,11 @@
 namespace datadog {
   std::string to_string(v8::Isolate *isolate, v8::Local<v8::Value> handle) {
     #if NODE_MAJOR_VERSION >= 8
-    return *v8::String::Utf8Value(isolate, handle);
+    std::string string(*v8::String::Utf8Value(isolate, handle));
     #else
-    return *v8::String::Utf8Value(handle);
+    std::string string(*v8::String::Utf8Value(handle));
     #endif
+
+    return string;
   }
 }
