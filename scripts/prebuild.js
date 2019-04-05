dce5e81bedd3fddddaf0d29a708a2a3dbc788173
rochdev <roch.devost@datadoghq.com>
2019-04-05 15:46:05 -0400

    update prebuilt binaries release to include apache notices

--- scripts/prebuild.js
+++ scripts/prebuild.js
@@ -6,6 +6,8 @@ const path = require('path')
 const os = require('os')
 const tar = require('tar')
 const semver = require('semver')
+const fs = require('fs')
+const rm = require('rimraf')
 
 const name = `${os.platform()}-${process.env.ARCH || os.arch()}`
 const targets = abi.allTargets
@@ -15,13 +17,20 @@ const targets = abi.allTargets
 const cb = err => {
   if (err) throw err
 
+  fs.copyFileSync(
+    path.join(__dirname, '..', 'src', 'native', 'tdigest', 'NOTICES'),
+    path.join(__dirname, '..', 'prebuilds', 'NOTICES')
+  )
+
   tar.create({
     gzip: true,
     sync: true,
     portable: true,
     file: `addons-${name}.tgz`,
-    cwd: path.join(__dirname, '..', 'prebuilds')
-  }, [name])
+    cwd: path.join(__dirname, '..')
+  }, ['prebuilds'])
+
+  rm.sync(path.join(__dirname, '..', 'prebuilds'))
 }
 
 prebuildify({
