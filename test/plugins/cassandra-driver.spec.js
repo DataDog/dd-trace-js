a8a29a2313e30f3f95f3dbd4b04371744a23f2d6
rochdev <roch.devost@datadoghq.com>
2019-04-08 12:41:13 -0400

    fix client.batch() promise support in cassandra-driver plugin

--- test/plugins/cassandra-driver.spec.js
+++ test/plugins/cassandra-driver.spec.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const semver = require('semver')
 const agent = require('./agent')
 const plugin = require('../../src/plugins/cassandra-driver')
 
@@ -95,7 +96,11 @@ describe('Plugin', () => {
             .then(done)
             .catch(done)
 
-          client.batch(queries, { prepare: true })
+          try {
+            client.batch(queries, { prepare: true })
+          } catch (e) {
+            // older versions require a callback
+          }
         })
 
         it('should handle errors', done => {
@@ -197,6 +202,79 @@ describe('Plugin', () => {
           client.execute('SELECT now() FROM local;', err => err && done(err))
         })
       })
+
+      // Promise support added in 3.2.0
+      if (semver.intersects(version, '>=3.2.0')) {
+        describe('with the promise API', () => {
+          let client
+
+          before(() => {
+            return agent.load(plugin, 'cassandra-driver')
+          })
+
+          after(() => {
+            return agent.close()
+          })
+
+          beforeEach(done => {
+            cassandra = require(`../../versions/cassandra-driver@${version}`).get()
+
+            client = new cassandra.Client({
+              contactPoints: ['127.0.0.1'],
+              localDataCenter: 'datacenter1',
+              keyspace: 'system'
+            })
+
+            client.keyspace
+
+            client.connect(done)
+          })
+
+          afterEach(done => {
+            client.shutdown(done)
+          })
+
+          it('should do automatic instrumentation', done => {
+            const query = 'SELECT now() FROM local;'
+
+            agent
+              .use(traces => {
+                expect(traces[0][0]).to.have.property('service', 'test-cassandra')
+                expect(traces[0][0]).to.have.property('resource', query)
+                expect(traces[0][0]).to.have.property('type', 'cassandra')
+                expect(traces[0][0].meta).to.have.property('db.type', 'cassandra')
+                expect(traces[0][0].meta).to.have.property('span.kind', 'client')
+                expect(traces[0][0].meta).to.have.property('out.host', '127.0.0.1')
+                expect(traces[0][0].meta).to.have.property('out.port', '9042')
+                expect(traces[0][0].meta).to.have.property('cassandra.query', query)
+                expect(traces[0][0].meta).to.have.property('cassandra.keyspace', 'system')
+              })
+              .then(done)
+              .catch(done)
+
+            client.execute(query)
+              .catch(done)
+          })
+
+          it('should support batch queries', done => {
+            const id = '1234'
+            const queries = [
+              { query: 'INSERT INTO test.test (id) VALUES (?)', params: [id] },
+              `UPDATE test.test SET test='test' WHERE id='${id}';`
+            ]
+
+            agent
+              .use(traces => {
+                expect(traces[0][0]).to.have.property('resource', `${queries[0].query}; ${queries[1]}`)
+              })
+              .then(done)
+              .catch(done)
+
+            client.batch(queries, { prepare: true })
+              .catch(done)
+          })
+        })
+      }
     })
   })
 })
