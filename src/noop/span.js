ea11cd87f9ff3b28f1466030249e977f1d076041
rochdev <roch.devost@datadoghq.com>
2019-04-17 12:49:36 -0400

    fix getting the tracer from a noop span

--- src/noop/span.js
+++ src/noop/span.js
@@ -3,9 +3,16 @@
 const Span = require('opentracing').Span
 const context = require('./span_context')
 
+let tracer
+
 class NoopSpan extends Span {
   _context () {
     return context
+  }
+
+  _tracer () {
+    // lazy load to avoid circular dependency
+    return (tracer || (tracer = require('../..')))._tracer
   }
 }
 
