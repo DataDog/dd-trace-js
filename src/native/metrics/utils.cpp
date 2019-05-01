6b26a65268a1b726b5716ee6cabff5ae162409be
Roch Devost <roch.devost@datadoghq.com>
2019-05-01 13:27:41 -0400

    add support for node 12 (#554)

--- src/native/metrics/utils.cpp
+++ src/native/metrics/utils.cpp
@@ -6,4 +6,12 @@ namespace datadog {
   std::string to_string(v8::Local<v8::Value> handle) {
     return *Nan::Utf8String(handle);
   }
+
+  v8::Local<v8::String> from_string(std::string str) {
+    return Nan::New(str).ToLocalChecked();
+  }
+
+  v8::Local<v8::Value> value(v8::Local<v8::Object> obj, std::string key) {
+    return Nan::Get(obj, from_string(key)).ToLocalChecked();
+  }
 }
