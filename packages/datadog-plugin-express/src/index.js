4ede45084845b18ac3624446baaecd6aca6925d5
Peter Simmons <cabbiepete@gmail.com>
2019-06-27 16:11:26 +1200

    Patch the express.set method as well
    Ensure that things like below are still workable.
    
    ```
    const qs = require('qs')
    app.set('query parser', (str) => {
      return qs.parse(str, {depth: 11});
    });
    ```
    
    Otherwise the customised query parser is never invoked if tracing is enabled.

--- packages/datadog-plugin-express/src/index.js
+++ packages/datadog-plugin-express/src/index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const METHODS = require('methods').concat('use', 'route', 'param', 'all')
+const METHODS = require('methods').concat('use', 'route', 'param', 'all', 'set')
 const web = require('../../dd-trace/src/plugins/util/web')
 const routerPlugin = require('../../datadog-plugin-router/src')
 
