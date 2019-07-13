09cfb205de706bb46da154e87b29340c608d6c65
rishabh <7551150+rishabh@users.noreply.github.com>
2019-07-13 15:40:47 -0400

    Remove env var

--- scripts/tdd.js
+++ scripts/tdd.js
@@ -15,8 +15,6 @@ const command = `yarn services && NO_DEPRECATION=* mocha --watch --inspect-brk=9
 
 if (fs.existsSync(path.join(base, `datadog-plugin-${target}`))) {
   execSync(`PLUGINS=${target} ${command} ${globs} 'packages/datadog-plugin-${target}/test/**/*.spec.js'`, options)
-} else if (target) {
-  execSync(`${command} ${globs} '${target}'`, options)
 } else {
-  execSync(`${command} ${globs} 'packages/dd-trace/test/**/*.spec.js'`, options)
+  execSync(`${command} ${globs} '${target}'`, options)
 }
