0db461298de05c70dbfea4c703bc020549f29942
rishabh <7551150+rishabh@users.noreply.github.com>
2019-08-14 08:41:08 -0400

    Fix max len line in tap harness

--- packages/dd-trace/test/plugins/harness/tap.js
+++ packages/dd-trace/test/plugins/harness/tap.js
@@ -5,10 +5,10 @@ const execSync = require('../../../../../scripts/helpers/exec')
 
 function executeTap (args, options) {
   const tracerSetupPath = path.join(__dirname, '..', 'tracer-setup.js')
-
+  const npmRunEnv = 'npm run env --silent --'
   // Inject our tracer before we run the external tests
   try {
-    return execSync(`npm run env --silent -- tap --node-arg="--require" --node-arg="${tracerSetupPath}" ${args}`, options)
+    return execSync(`${npmRunEnv} tap --node-arg="--require" --node-arg="${tracerSetupPath}" ${args}`, options)
   } catch (err) {} // eslint-disable-line no-empty
 }
 
