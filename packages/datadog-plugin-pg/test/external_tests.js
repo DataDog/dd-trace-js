fc00e4532f2f4ae200da0264940d9b968c95eff1
rishabh <7551150+rishabh@users.noreply.github.com>
2019-08-14 08:35:39 -0400

    pg boilerplate code, tests are broken unfortunately
    They also require psql and I believe the way the database is set up isn't entirely correct since a few connection-pool integration tests fail

--- packages/datadog-plugin-pg/test/external_tests.js
+++ packages/datadog-plugin-pg/test/external_tests.js
@@ -7,11 +7,22 @@ const defaultConfig = {
   repo: 'https://github.com/brianc/node-postgres',
   testType: 'custom',
   testFn: function (tracerSetupPath, options) {
-    const connectionString = 'postgres://'
+    const connectionString = 'pg://postgres:postgres@127.0.0.1:5432/postgres?application_name=test'
     const nodeCmd = `xargs -n 1 -I file node -r '${tracerSetupPath}' file ${connectionString}`
+
     try {
-      execSync(`find test/unit -name "*-tests.js" | ${nodeCmd}`, options)
+      // Create test tables
+      execSync(`npm run env -- node -r ${tracerSetupPath} script/create-test-tables.js ${connectionString}`, options)
+
+      // Run tests
+      execSync(`find test/ -name "*-tests.js" -not -path "test/native/*" | ${nodeCmd}`, options)
     } catch (error) {} // eslint-disable-line no-empty
+  },
+  testEnv: {
+    'PGUSER': 'postgres',
+    'PGPASSWORD': 'postgres',
+    'PGDATABASE': 'postgres',
+    'PGAPPNAME': 'test'
   }
 }
 
