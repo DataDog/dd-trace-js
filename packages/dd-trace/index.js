2045c21c8e54053e192825c6b25d8433dfd05d3e
DarcyRaynerDD <darcy.rayner@datadoghq.com>
2019-08-15 17:52:54 -0400

    Revert formatting change

--- packages/dd-trace/index.js
+++ packages/dd-trace/index.js
@@ -6,7 +6,7 @@ const node = require('./src/platform/node')
 platform.use(node)
 
 const TracerProxy = require('./src/proxy')
-module.exports = new TracerProxy()
 
+module.exports = new TracerProxy()
 module.exports.default = module.exports
 module.exports.tracer = module.exports
