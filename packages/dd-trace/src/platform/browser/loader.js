b9d6360aa4b30f85ae142dbbac13b2552e89c057
rochdev <roch.devost@datadoghq.com>
2019-08-21 15:00:01 -0400

    move unloading logic to browser loader

--- packages/dd-trace/src/platform/browser/loader.js
+++ packages/dd-trace/src/platform/browser/loader.js
@@ -1,7 +1,16 @@
 'use strict'
 
 class Loader {
-  reload () {}
+  constructor (instrumenter) {
+    this._instrumenter = instrumenter
+  }
+
+  reload (plugins) {
+    plugins.forEach((meta, plugin) => {
+      this._instrumenter.unload(plugin)
+      this._instrumenter.load(plugin, meta)
+    })
+  }
 
   getModules (instrumentation) {
     return [window[instrumentation.name]]
