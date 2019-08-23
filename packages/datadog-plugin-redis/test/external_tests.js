036e5a4146c9b774baf0f496d1bc342d25ae385b
rishabh <7551150+rishabh@users.noreply.github.com>
2019-08-23 13:44:58 -0400

    Attempt to fix mongodb-core and debug redis in CI

--- packages/datadog-plugin-redis/test/external_tests.js
+++ packages/datadog-plugin-redis/test/external_tests.js
@@ -1,11 +1,17 @@
 'use strict'
 
+const execSync = require('child_process').execSync
+
 const testConfigs = [
   {
     integration: 'redis',
     repo: 'https://github.com/NodeRedis/node_redis',
     framework: 'mocha',
-    args: './test/*.js ./test/commands/*.js --exit --timeout 8000'
+    args: './test/*.js ./test/commands/*.js --exit --timeout 8000',
+    setup (tracerSetupPath, options) {
+      execSync('docker ps', options)
+      execSync('npm install', options)
+    }
   }
 ]
 
