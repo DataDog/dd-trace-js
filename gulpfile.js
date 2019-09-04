f057eeb148f8922c5c15b828a9e84212354ab706
rochdev <roch.devost@datadoghq.com>
2019-09-04 15:45:38 -0400

    add browser example with a simple bundler

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp')
+const bundle = require('./scripts/bundle')
+
+const defaultTask = []
+
+function defineTask (entry, target) {
+  gulp.task('bundle', bundle.bind(this, {
+    entry: entry,
+    target: target
+  }))
+
+  defaultTask.push('bundle')
+}
+
+defineTask('./browser', './dist')
+
+gulp.task('default', gulp.parallel(defaultTask))
