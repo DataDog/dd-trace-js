802b7ac807245d918c7a881bf31211bc6d21ef1d
rochdev <roch.devost@datadoghq.com>
2019-09-26 20:21:34 -0700

    fix immediate flushes caused by interval config being ignored

--- packages/dd-trace/src/exporters/agent/index.js
+++ packages/dd-trace/src/exporters/agent/index.js
@@ -4,11 +4,11 @@ const Writer = require('./writer')
 const Scheduler = require('./scheduler')
 
 class AgentExporter {
-  constructor ({ url, interval }) {
+  constructor ({ url, flushInterval }) {
     this._writer = new Writer(url)
 
-    if (interval > 0) {
-      this._scheduler = new Scheduler(() => this._writer.flush(), interval)
+    if (flushInterval > 0) {
+      this._scheduler = new Scheduler(() => this._writer.flush(), flushInterval)
     }
     this._scheduler && this._scheduler.start()
   }
