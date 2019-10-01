a97ff9552024cac3218d2465e65b259c1709eb68
rochdev <roch.devost@datadoghq.com>
2019-10-01 15:45:42 +0200

    revert to 5 benchmark samples

--- benchmark/benchmark.js
+++ benchmark/benchmark.js
@@ -6,7 +6,7 @@ const Benchmark = require('benchmark')
 const nock = require('nock')
 
 Benchmark.options.maxTime = 0
-Benchmark.options.minSamples = 1
+Benchmark.options.minSamples = 5
 
 nock.disableNetConnect()
 
