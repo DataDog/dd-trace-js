defb9a7ab0b9e52b3097f8e8744bed4763fde1fd
rochdev <roch.devost@datadoghq.com>
2019-10-24 15:08:47 -0400

    fix service name when tracer is imported by a dependency

--- packages/dd-trace/src/platform/node/service.js
+++ packages/dd-trace/src/platform/node/service.js
@@ -13,9 +13,19 @@ function service () {
   const callerPath = parentModule()
   const parentPath = parentModule(callerPath)
   const cwd = path.dirname(parentPath || callerPath)
-  const pkg = readPkgUp.sync({ cwd }).pkg || {}
+  const pkg = findPkg(cwd)
 
   return pkg.name
+}
+
+function findPkg (cwd) {
+  let up = readPkgUp.sync({ cwd })
+
+  while (up && /\/node_modules\//.test(up.path)) {
+    up = readPkgUp.sync({ cwd: path.resolve(path.dirname(up.path), '..') })
+  }
+
+  return up && up.pkg ? up.pkg : {}
 }
 
 module.exports = service
