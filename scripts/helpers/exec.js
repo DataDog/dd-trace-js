756a36ccb88e1a80fc42f5fbd2b8c3efff245570
Roch Devost <roch.devost@datadoghq.com>
2019-11-08 21:56:48 -0500

    update release scripts with new workflow (#740)

--- scripts/helpers/exec.js
+++ scripts/helpers/exec.js
@@ -6,8 +6,17 @@ const color = require('./color')
 function exec (command, options) {
   options = Object.assign({ stdio: [0, 1, 2] }, options)
 
-  execSync(`echo "${color.GRAY}$ ${command}${color.NONE}"`, options)
-  execSync(command, options)
+  execSync(`echo "${color.GRAY}$ ${command}${color.NONE}"`, { stdio: [0, 1, 2] })
+
+  return execSync(command, options)
 }
+
+function pipe (command, options) {
+  return exec(command, Object.assign({ stdio: 'pipe' }, options))
+    .toString()
+    .replace(/\n$/, '')
+}
+
+exec.pipe = pipe
 
 module.exports = exec
