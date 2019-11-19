90d563d77d90111c57c70ba8a903e7f12d06ad1c
Roch Devost <roch.devost@datadoghq.com>
2019-11-19 09:23:33 -0500

    add rum context as tags to browser traces (#751)

--- packages/dd-trace/src/platform/browser/tags.js
+++ packages/dd-trace/src/platform/browser/tags.js
@@ -3,4 +3,9 @@
 const bowser = require('bowser/bundled')
 const navigator = bowser.parse(window.navigator.userAgent)
 
-module.exports = () => ({ navigator })
+module.exports = () => {
+  const rum = window.DD_RUM
+  const context = rum && rum.getInternalContext && rum.getInternalContext()
+
+  return { navigator, ...context }
+}
