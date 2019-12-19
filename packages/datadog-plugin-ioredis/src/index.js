54672971bdf996aa9476c09fb8b79dfd906823e2
rochdev <roch.devost@datadoghq.com>
2019-12-19 11:43:46 -0500

    add safety nets for type errors in ioredis plugin

--- packages/datadog-plugin-ioredis/src/index.js
+++ packages/datadog-plugin-ioredis/src/index.js
@@ -5,10 +5,13 @@ const tx = require('../../dd-trace/src/plugins/util/redis')
 function createWrapSendCommand (tracer, config) {
   return function wrapSendCommand (sendCommand) {
     return function sendCommandWithTrace (command, stream) {
-      const db = this.options.db
+      if (!command || !command.promise) return sendCommand.apply(this, arguments)
+
+      const options = this.options || {}
+      const db = options.db
       const span = tx.instrument(tracer, config, db, command.name, command.args)
 
-      tx.setHost(span, this.options.host, this.options.port)
+      tx.setHost(span, options.host, options.port)
       tx.wrap(span, command.promise)
 
       return tracer.scope().bind(sendCommand, span).apply(this, arguments)
