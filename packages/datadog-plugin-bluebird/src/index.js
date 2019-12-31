98320d7fd15d9d506292c4c1c903441c68cb15d8
ericmustin <mustin.eric@gmail.com>
2019-12-31 11:57:12 +0100

    handle unwrapping copies _then by maintaining reference to copies on original promise library

--- packages/datadog-plugin-bluebird/src/index.js
+++ packages/datadog-plugin-bluebird/src/index.js
@@ -2,13 +2,37 @@
 
 const tx = require('../../dd-trace/src/plugins/util/promise')
 
-function createGetNewLibraryCopyWrap (tracer, config, shim) {
+const DD_LIB_COPIES = '_datadog_library_copies'
+
+function createGetNewLibraryCopyWrap (tracer, config, originalLib, shim) {
   return function wrapGetNewLibraryCopy (getNewLibraryCopy) {
     return function getNewLibraryCopyWithTrace () {
-      const result = getNewLibraryCopy.apply(this, arguments)
-      shim.wrap(result.prototype, '_then', tx.createWrapThen(tracer, config))
-      return result
+      const libraryCopy = getNewLibraryCopy.apply(this, arguments)
+      shim.wrap(libraryCopy.prototype, '_then', tx.createWrapThen(tracer, config))
+      addToLibraryCopies(originalLib.prototype, libraryCopy)
+      return libraryCopy
     }
+  }
+}
+
+function addToLibraryCopies (originalLibPrototype, libraryCopy) {
+  let libraryCopies = originalLibPrototype[DD_LIB_COPIES]
+
+  if (!libraryCopies) {
+    libraryCopies = new Set()
+    originalLibPrototype[DD_LIB_COPIES] = libraryCopies
+  }
+
+  libraryCopies.add(libraryCopy)
+}
+
+function unwrapLibraryCopies (originalLibPrototype, shim) {
+  const libraryCopies = originalLibPrototype[DD_LIB_COPIES]
+
+  if (libraryCopies) {
+    libraryCopies.forEach(libraryCopy => shim.unwrap(libraryCopy.prototype, '_then'))
+    libraryCopies.clear()
+    delete originalLibPrototype[DD_LIB_COPIES]
   }
 }
 
@@ -17,10 +41,11 @@ module.exports = [
     name: 'bluebird',
     versions: ['^2.11.0', '^3.4.1'],
     patch (Promise, tracer, config) {
-      this.wrap(Promise, 'getNewLibraryCopy', createGetNewLibraryCopyWrap(tracer, config, this))
+      this.wrap(Promise, 'getNewLibraryCopy', createGetNewLibraryCopyWrap(tracer, config, Promise, this))
     },
     unpatch (Promise) {
       this.unwrap(Promise, 'getNewLibraryCopy')
+      unwrapLibraryCopies(Promise.prototype, this)
     }
   },
   {
