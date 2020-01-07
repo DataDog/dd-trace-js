2d6860de42e68a4c46be59b822760d65c5aaf39f
ericmustin <mustin.eric@gmail.com>
2020-01-07 01:39:07 +0100

    add version filter for bluebird version specific instrumentation

--- packages/datadog-plugin-bluebird/test/index.spec.js
+++ packages/datadog-plugin-bluebird/test/index.spec.js
@@ -1,19 +1,13 @@
 'use strict'
 
 const assertPromise = require('../../dd-trace/test/plugins/promise')
+const semver = require('semver')
 
 assertPromise('bluebird')
 
 assertPromise('bluebird', bluebird => {
-  // TODO: remove if statement when running tests only for versions ^2.11.0 and ^3.4.1
-  // https://github.com/petkaantonov/bluebird/releases/tag/v2.11.0
-  // https://github.com/petkaantonov/bluebird/releases/tag/v3.4.1
-  if (!bluebird.getNewLibraryCopy) {
-    return bluebird
-  }
-
   return bluebird.getNewLibraryCopy()
-})
+}, version => { semver.intersects(version, '^2.11.0 || ^3.4.1') })
 
 describe('bluebird.getNewLibraryCopy', () => {
   let bluebird
--- packages/dd-trace/test/plugins/promise.js
+++ packages/dd-trace/test/plugins/promise.js
@@ -1,6 +1,6 @@
 'use strict'
 
-module.exports = (name, factory) => {
+module.exports = (name, factory, versionFilter) => {
   const agent = require('./agent')
   const plugin = require(`../../../datadog-plugin-${name}/src`)
 
@@ -12,81 +12,32 @@ module.exports = (name, factory) => {
 
     describe(name, () => {
       withVersions(plugin, name, version => {
-        beforeEach(() => {
-          tracer = require('../..')
-        })
-
-        afterEach(() => {
-          return agent.close()
-        })
-
-        describe('without configuration', () => {
+        if (!versionFilter || versionFilter(version)) {
           beforeEach(() => {
-            return agent.load(plugin, name)
+            tracer = require('../..')
           })
 
-          beforeEach(() => {
-            const moduleExports = require(`../../../../versions/${name}@${version}`).get()
-
-            Promise = factory ? factory(moduleExports) : moduleExports
+          afterEach(() => {
+            return agent.close()
           })
 
-          it('should run the then() callbacks in the context where then() was called', () => {
-            if (process.env.DD_CONTEXT_PROPAGATION === 'false') return
-
-            const span = {}
-
-            let promise = new Promise((resolve, reject) => {
-              setImmediate(() => {
-                tracer.scope().activate({}, () => {
-                  resolve()
-                })
-              })
+          describe('without configuration', () => {
+            beforeEach(() => {
+              return agent.load(plugin, name)
             })
 
-            return tracer.scope().activate(span, () => {
-              for (let i = 0; i < promise.then.length; i++) {
-                const args = new Array(i + 1)
+            beforeEach(() => {
+              const moduleExports = require(`../../../../versions/${name}@${version}`).get()
 
-                args[i] = () => {
-                  expect(tracer.scope().active()).to.equal(span)
-                }
-
-                promise = promise.then.apply(promise, args)
-              }
-
-              return promise
-            })
-          })
-
-          it('should run the catch() callback in the context where catch() was called', () => {
-            if (process.env.DD_CONTEXT_PROPAGATION === 'false') return
-
-            const span = {}
-            const promise = new Promise((resolve, reject) => {
-              setImmediate(() => {
-                tracer.scope().activate({}, () => {
-                  reject(new Error())
-                })
-              })
+              Promise = factory ? factory(moduleExports) : moduleExports
             })
 
-            return tracer.scope().activate(span, () => {
-              return promise
-                .catch(err => {
-                  throw err
-                })
-                .catch(() => {
-                  expect(tracer.scope().active()).to.equal(span)
-                })
-            })
-          })
+            it('should run the then() callbacks in the context where then() was called', () => {
+              if (process.env.DD_CONTEXT_PROPAGATION === 'false') return
 
-          it('should allow to run without a scope if not available when calling then()', () => {
-            if (process.env.DD_CONTEXT_PROPAGATION === 'false') return
+              const span = {}
 
-            tracer.scope().activate(null, () => {
-              const promise = new Promise((resolve, reject) => {
+              let promise = new Promise((resolve, reject) => {
                 setImmediate(() => {
                   tracer.scope().activate({}, () => {
                     resolve()
@@ -94,13 +45,64 @@ module.exports = (name, factory) => {
                 })
               })
 
-              return promise
-                .then(() => {
-                  expect(tracer.scope().active()).to.be.null
+              return tracer.scope().activate(span, () => {
+                for (let i = 0; i < promise.then.length; i++) {
+                  const args = new Array(i + 1)
+
+                  args[i] = () => {
+                    expect(tracer.scope().active()).to.equal(span)
+                  }
+
+                  promise = promise.then.apply(promise, args)
+                }
+
+                return promise
+              })
+            })
+
+            it('should run the catch() callback in the context where catch() was called', () => {
+              if (process.env.DD_CONTEXT_PROPAGATION === 'false') return
+
+              const span = {}
+              const promise = new Promise((resolve, reject) => {
+                setImmediate(() => {
+                  tracer.scope().activate({}, () => {
+                    reject(new Error())
+                  })
                 })
+              })
+
+              return tracer.scope().activate(span, () => {
+                return promise
+                  .catch(err => {
+                    throw err
+                  })
+                  .catch(() => {
+                    expect(tracer.scope().active()).to.equal(span)
+                  })
+              })
+            })
+
+            it('should allow to run without a scope if not available when calling then()', () => {
+              if (process.env.DD_CONTEXT_PROPAGATION === 'false') return
+
+              tracer.scope().activate(null, () => {
+                const promise = new Promise((resolve, reject) => {
+                  setImmediate(() => {
+                    tracer.scope().activate({}, () => {
+                      resolve()
+                    })
+                  })
+                })
+
+                return promise
+                  .then(() => {
+                    expect(tracer.scope().active()).to.be.null
+                  })
+              })
             })
           })
-        })
+        }
       })
     })
   })
