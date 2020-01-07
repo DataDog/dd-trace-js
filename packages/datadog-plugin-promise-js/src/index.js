361c6d086c1f6c8e130bc5682aa121de938e45ac
ericmustin <mustin.eric@gmail.com>
2020-01-07 19:05:21 +0100

    only instrument promise-js on versions it isnt used native promises, add promise-js version range filtering in specs

--- packages/datadog-plugin-promise-js/src/index.js
+++ packages/datadog-plugin-promise-js/src/index.js
@@ -5,6 +5,20 @@ const tx = require('../../dd-trace/src/plugins/util/promise')
 module.exports = [
   {
     name: 'promise-js',
+    versions: ['>=0.0.7'],
+    patch (Promise, tracer, config) {
+      if (Promise !== global.Promise) {
+        this.wrap(Promise.prototype, 'then', tx.createWrapThen(tracer, config))
+      }
+    },
+    unpatch (Promise) {
+      if (Promise !== global.Promise) {
+        this.unwrap(Promise.prototype, 'then')
+      }
+    }
+  },
+  {
+    name: 'promise-js',
     versions: ['0.0.3 - 0.0.6'],
     patch (Promise, tracer, config) {
       this.wrap(Promise.prototype, 'then', tx.createWrapThen(tracer, config))
