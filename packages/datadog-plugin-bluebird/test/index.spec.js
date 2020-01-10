afdf87827fabb53546d18325e3ed3db04c4e91a5
ericmustin <mustin.eric@gmail.com>
2020-01-10 15:24:27 +0100

    es6 syntax improvement

--- packages/datadog-plugin-bluebird/test/index.spec.js
+++ packages/datadog-plugin-bluebird/test/index.spec.js
@@ -4,4 +4,4 @@ const assertPromise = require('../../dd-trace/test/plugins/promise')
 
 assertPromise('bluebird')
 
-assertPromise('bluebird', bluebird => { return bluebird.getNewLibraryCopy() }, '^2.11.0 || ^3.4.1')
+assertPromise('bluebird', bluebird => bluebird.getNewLibraryCopy(), '^2.11.0 || ^3.4.1')
