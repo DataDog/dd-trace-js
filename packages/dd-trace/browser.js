64d067ef3b8490e72086059d1679e4be1596073e
rochdev <roch.devost@datadoghq.com>
2020-01-15 16:25:24 -0500

    add missing type array polyfill

--- packages/dd-trace/browser.js
+++ packages/dd-trace/browser.js
@@ -3,6 +3,7 @@
 require('core-js/stable/object/assign') // TODO: remove dependency
 require('core-js/stable/set') // TODO: remove dependency
 require('core-js/stable/map') // TODO: remove dependency
+require('core-js/stable/typed-array') // TODO: remove dependency
 
 const platform = require('./src/platform')
 const browser = require('./src/platform/browser')
