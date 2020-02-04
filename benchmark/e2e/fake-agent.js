7e818ab78ae32c0d41bc9a64f25fbb5a91cf8d5e
Bryan English <bryan.english@datadoghq.com>
2020-02-04 16:13:10 -0500

    more profiling tooling

--- benchmark/e2e/fake-agent.js
+++ benchmark/e2e/fake-agent.js
@@ -9,19 +9,19 @@ const output = fs.createWriteStream(`./spans.nsjson`)
 const agent = http.createServer((req, res) => {
   concatStream(req, body => {
     if (body.length === 0) return res.status(200).send()
-    body = msgpack.decode(body, { codec })
-    body.forEach(trace => {
-      trace.forEach(span => {
-        output.write(JSON.stringify(span) + '\n')
-      })
-    })
+    //    body = msgpack.decode(body, { codec })
+    //    body.forEach(trace => {
+    //      trace.forEach(span => {
+    //        output.write(JSON.stringify(span) + '\n')
+    //      })
+    //    })
     res.statusCode = 200
     res.end(JSON.stringify({ rate_by_service: { 'service:,env:': 1 } }))
   })
 })
 
 agent.listen(8126, () => {
-  process.send({ ready: true })
+  if (process.send) { process.send({ ready: true }) }
 })
 agent.on('close', () => output.close())
 
