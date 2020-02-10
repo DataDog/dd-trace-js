abeada830be469b8027a692287524f3aae91498b
Bryan English <bryan.english@datadoghq.com>
2020-02-10 15:44:10 -0500

    performance: microoptimizations in hot paths
    On these hot paths, we should be using `for of` loops since they're
    quite a bit faster. The gain isn't all that much, but every little bit
    helps.

--- packages/dd-trace/src/plugins/util/web.js
+++ packages/dd-trace/src/plugins/util/web.js
@@ -218,9 +218,10 @@ function startSpan (tracer, config, req, res, name) {
 }
 
 function configureDatadogObject (tracer, span, req, res) {
-  req._datadog.tracer = tracer
-  req._datadog.span = span
-  req._datadog.res = res
+  const ddObj = req._datadog
+  ddObj.tracer = tracer
+  ddObj.span = span
+  ddObj.res = res
 }
 
 function finish (req, res) {
@@ -256,7 +257,7 @@ function wrapEnd (req) {
   res.writeHead = wrapWriteHead(req)
 
   let _end = req._datadog.end = res.end = function () {
-    req._datadog.beforeEnd.forEach(beforeEnd => beforeEnd())
+    for (const beforeEnd of req._datadog.beforeEnd) beforeEnd()
 
     finishMiddleware(req, res)
 
