a0587025d67cacc66d04e258a9d22e5b51cd7a97
Bryan English <bryan.english@datadoghq.com>
2020-02-12 13:35:02 -0500

    performance: use hand-rolled msgpack encoder
    Most of the new code is from 3aa4d9b4bb43c90a49f234a57f87d6352af4792e
    and parents, with a few optimizations made here and there.

---
+++ packages/dd-trace/src/encode/index.js
@@ -0,0 +1,172 @@
+'use strict'
+
+const Uint64BE = require('int64-buffer').Uint64BE
+const util = require('./util')
+const tokens = require('./tokens')
+const cachedString = require('./cache')(1024)
+
+const fields = getFields()
+
+const {
+  headerBuffer,
+  traceIdOffset,
+  spanIdOffset,
+  startOffset,
+  durationOffset,
+  errorOffset
+} = (() => {
+  const buffer = Buffer.alloc(1024)
+  let offset = 0
+
+  offset += copy(buffer, offset, fields.trace_id)
+  offset += copy(buffer, offset, tokens.uint64)
+  const traceIdOffset = offset
+  new Uint64BE(buffer, offset, 0) // eslint-disable-line no-new
+  offset += 8
+
+  offset += copy(buffer, offset, fields.span_id)
+  offset += copy(buffer, offset, tokens.uint64)
+  const spanIdOffset = offset
+  new Uint64BE(buffer, offset, 0) // eslint-disable-line no-new
+  offset += 8
+
+  offset += copy(buffer, offset, fields.start)
+  offset += copy(buffer, offset, tokens.uint64)
+  const startOffset = offset
+  new Uint64BE(buffer, offset, 0) // eslint-disable-line no-new
+  offset += 8
+
+  offset += copy(buffer, offset, fields.duration)
+  offset += copy(buffer, offset, tokens.uint64)
+  const durationOffset = offset
+  new Uint64BE(buffer, offset, 0) // eslint-disable-line no-new
+  offset += 8
+
+  offset += copy(buffer, offset, fields.error)
+  const errorOffset = offset
+  offset += copy(buffer, offset, tokens.int[0])
+
+  return {
+    headerBuffer: buffer.slice(0, offset),
+    traceIdOffset,
+    spanIdOffset,
+    startOffset,
+    durationOffset,
+    errorOffset
+  }
+})()
+
+function encode (buffer, offset, trace) {
+  offset = writeArrayPrefix(buffer, offset, trace)
+
+  for (const span of trace) {
+    let fieldCount = 9
+
+    span.parent_id && fieldCount++
+    span.type && fieldCount++
+
+    offset += copy(buffer, offset, tokens.map[fieldCount])
+
+    offset += copyHeader(buffer, offset, span)
+
+    if (span.parent_id) {
+      offset += copy(buffer, offset, fields.parent_id)
+      offset += copy(buffer, offset, tokens.uint64)
+      offset += copy(buffer, offset, span.parent_id.toBuffer())
+    }
+
+    offset += copy(buffer, offset, fields.name)
+    offset += write(buffer, offset, span.name)
+
+    offset += copy(buffer, offset, fields.resource)
+    offset += write(buffer, offset, span.resource)
+
+    offset += copy(buffer, offset, fields.service)
+    offset += write(buffer, offset, span.service)
+
+    if (span.type) {
+      offset += copy(buffer, offset, fields.type)
+      offset += write(buffer, offset, span.type)
+    }
+
+    offset += copy(buffer, offset, fields.meta)
+    offset = writeMap(buffer, offset, span.meta)
+  }
+
+  buffer.write('', offset) // throw if offset is out of bounds
+
+  return offset
+}
+
+function copyHeader (buffer, offset, span) {
+  copy(headerBuffer, traceIdOffset, span.trace_id.toBuffer())
+  copy(headerBuffer, spanIdOffset, span.span_id.toBuffer())
+  new Uint64BE(headerBuffer, startOffset, span.start) // eslint-disable-line no-new
+  new Uint64BE(headerBuffer, durationOffset, span.duration) // eslint-disable-line no-new
+  copy(headerBuffer, errorOffset, tokens.int[span.error])
+  return copy(buffer, offset, headerBuffer)
+}
+
+function write (buffer, offset, str) {
+  return copy(buffer, offset, cachedString(str))
+}
+
+function copy (buffer, offset, source, sourceStart, sourceEnd) {
+  const length = source.length
+
+  sourceStart = sourceStart || 0
+  sourceEnd = sourceEnd || length
+
+  if (sourceStart !== 0 || sourceEnd !== length) {
+    source = source.slice(sourceEnd, sourceEnd)
+  }
+  buffer.set(source, offset)
+
+  return source.length
+}
+
+function writeMap (buffer, offset, map) {
+  const keys = Object.keys(map)
+
+  offset += copy(buffer, offset, tokens.map[keys.length])
+
+  for (let i = 0, l = keys.length; i < l; i++) {
+    offset += write(buffer, offset, keys[i])
+    offset += write(buffer, offset, map[keys[i]])
+  }
+
+  return offset
+}
+
+function writePrefix (buffer, offset, length, tokens, startByte) {
+  if (length <= 0xffff) {
+    return copy(buffer, offset, tokens[length])
+  }
+
+  return util.writeUInt8(buffer, startByte + 1, offset) + util.writeUInt32(buffer, length, offset + 1)
+}
+
+function writeArrayPrefix (buffer, offset, array) {
+  return offset + writePrefix(buffer, offset, array.length, tokens.array, 0xdc)
+}
+
+function getFields () {
+  return [
+    'trace_id',
+    'span_id',
+    'parent_id',
+    'service',
+    'resource',
+    'name',
+    'type',
+    'error',
+    'meta',
+    'start',
+    'duration'
+  ].reduce((prev, next) => {
+    prev[next] = Buffer.concat([tokens.str[next.length], Buffer.from(next)])
+    return prev
+  }, {})
+}
+
+module.exports = encode
--- packages/dd-trace/test/encode.spec.js
+++ packages/dd-trace/test/encode.spec.js
@@ -3,6 +3,7 @@
 const msgpack = require('msgpack-lite')
 const codec = msgpack.createCodec({ int64: true })
 const id = require('../src/id')
+const { Int64BE, Uint64BE } = require('int64-buffer') // TODO: remove dependency
 
 describe('encode', () => {
   let encode
@@ -16,9 +17,16 @@ describe('encode', () => {
       trace_id: id('1234abcd1234abcd'),
       span_id: id('1234abcd1234abcd'),
       parent_id: id('1234abcd1234abcd'),
+      name: 'test',
+      resource: 'test-r',
+      service: 'test-s',
+      type: 'foo',
+      error: 0,
+      meta: {
+        bar: 'baz'
+      },
       start: 123,
-      duration: 456,
-      name: 'test'
+      duration: 456
     }]
 
     const buffer = encode(data)
@@ -29,7 +37,9 @@ describe('encode', () => {
     expect(decoded[0].trace_id.toString(16)).to.equal(data[0].trace_id.toString())
     expect(decoded[0].span_id.toString(16)).to.equal(data[0].span_id.toString())
     expect(decoded[0].parent_id.toString(16)).to.equal(data[0].parent_id.toString())
+    expect(decoded[0].start).to.be.instanceof(Int64BE)
     expect(decoded[0].start.toString()).to.equal(data[0].start.toString())
+    expect(decoded[0].duration).to.be.instanceof(Int64BE)
     expect(decoded[0].duration.toString()).to.equal(data[0].duration.toString())
     expect(decoded[0].name).to.equal(data[0].name)
   })
