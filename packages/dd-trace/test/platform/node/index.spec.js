53c2a5ec26c5d89f4369f4f46d2873cb193b2e65
Bryan English <bryan.english@datadoghq.com>
2020-02-20 14:43:30 -0500

    PR feedback

--- packages/dd-trace/test/platform/node/index.spec.js
+++ packages/dd-trace/test/platform/node/index.spec.js
@@ -337,8 +337,8 @@ describe('Platform', () => {
       describe('prefix', () => {
         it('should support fixarray', () => {
           const length = 0xf
-          const array = Buffer.allocUnsafe(5)
-          const prefixed = msgpack.prefix(array, length)
+          const buffer = Buffer.allocUnsafe(5)
+          const prefixed = msgpack.prefix(buffer, length)
 
           expect(prefixed.length).to.equal(1)
           expect(prefixed[0]).to.deep.equal(Buffer.from([0x9f]))
@@ -346,8 +346,8 @@ describe('Platform', () => {
 
         it('should should support array 16', () => {
           const length = 0xf + 1
-          const array = Buffer.allocUnsafe(5)
-          const prefixed = msgpack.prefix(array, length)
+          const buffer = Buffer.allocUnsafe(5)
+          const prefixed = msgpack.prefix(buffer, length)
 
           expect(prefixed.length).to.equal(1)
           expect(prefixed[0]).to.deep.equal(Buffer.from([0xdc, 0x00, 0x10]))
@@ -355,8 +355,8 @@ describe('Platform', () => {
 
         it('should should support array 32', () => {
           const length = 0xffff + 1
-          const array = Buffer.allocUnsafe(5)
-          const prefixed = msgpack.prefix(array, length)
+          const buffer = Buffer.allocUnsafe(5)
+          const prefixed = msgpack.prefix(buffer, length)
 
           expect(prefixed.length).to.equal(1)
           expect(prefixed[0]).to.deep.equal(Buffer.from([0xdd, 0x00, 0x01, 0x00, 0x00]))
