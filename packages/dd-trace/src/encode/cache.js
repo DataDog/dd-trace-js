ea3c2e1ecfd11612dc9199ecf9a0ad576ec9fdd3
Bryan English <bryan.english@datadoghq.com>
2020-05-12 14:40:20 -0400

    fast string cache path for some well-used strings
    These are commonly found in `metrics` and `meta`, so we'll pre-compute
    them to avoid the slightly expensive cache lookup.

--- packages/dd-trace/src/encode/cache.js
+++ packages/dd-trace/src/encode/cache.js
@@ -4,10 +4,36 @@ const LRUCache = require('mnemonist/lru-cache')
 const tokens = require('./tokens')
 const util = require('./util')
 
+const fastCache = {};
+`
+service
+version
+language
+component
+span.kind
+error.type
+error.msg
+error.stack
+_sample_rate
+_sampling_priority_v1
+javascript
+
+`.trim().split('\n').forEach(str => {
+    const buffer = Buffer.from(str, 'utf-8')
+
+    fastCache[str] = Buffer.concat([
+      prefix(buffer.length),
+      buffer
+    ])
+  })
+
 function cache (max) {
   const cache = new LRUCache(max)
 
   return value => {
+    if (value in fastCache) {
+      return fastCache[value]
+    }
     let item = cache.get(value)
 
     if (!item) {
