3ff6ed91973e21714c6d7ec91c3224a0bdebe089
Stephen Belanger <admin@stephenbelanger.com>
2020-05-14 10:50:11 -0700

    Benchmark AsyncLocalStorage
    This adds a new benchmark for AsyncLocalStorage and updates async_hooks
    benchmarks to use AsyncResource similarily.

--- benchmark/index.js
+++ benchmark/index.js
@@ -5,5 +5,6 @@ const exec = cmd => execSync(cmd, { stdio: [0, 1, 2] })
 
 exec('node benchmark/core')
 exec('node benchmark/scope/async_hooks')
+exec('node benchmark/scope/async_local_storage')
 exec('node benchmark/platform/node')
 exec('node benchmark/dd-trace')
