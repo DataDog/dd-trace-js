3e18cd2b06ca9b6ac9f04406ff97537af8f51cf3
Roch Devost <roch.devost@datadoghq.com>
2020-05-15 13:41:00 -0400

    fix wrong minimum node version for AsyncLocalStorage failing tests (#952)

--- packages/dd-trace/test/scope/async_local_storage.spec.js
+++ packages/dd-trace/test/scope/async_local_storage.spec.js
@@ -1,17 +1,18 @@
 'use strict'
 
 const {
-  AsyncLocalStorage,
   AsyncResource,
   executionAsyncId
 } = require('async_hooks')
 const Scope = require('../../src/scope/async_local_storage')
 const Span = require('opentracing').Span
+const semver = require('semver')
 const testScope = require('./test')
 
 wrapIt()
 
-if (AsyncLocalStorage) {
+// https://github.com/nodejs/node/commit/52d8afc07e005343390ebc8c6d9e1eec77acd16e#diff-0bb01a51b135a5f68d93540808bac801
+if (semver.gte(process.version, '13.14.0')) {
   describe('Scope (AsyncLocalStorage)', test)
 } else {
   describe.skip('Scope (AsyncLocalStorage)', test)
