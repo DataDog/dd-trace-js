c88da21681bd250dee780cbf089017223a347833
DarcyRaynerDD <darcy.rayner@datadoghq.com>
2020-06-01 11:27:44 -0400

    Fix log exporter tests

--- packages/dd-trace/src/exporters/log/index.js
+++ packages/dd-trace/src/exporters/log/index.js
@@ -4,25 +4,26 @@ const log = require('../../log')
 
 const TRACE_PREFIX = '{"traces":[['
 const TRACE_SUFFIX = ']]}\n'
+const TRACE_FORMAT_OVERHEAD = TRACE_PREFIX.length + TRACE_SUFFIX.length
 const MAX_SIZE = 64 * 1024 // 64kb
 
 class LogExporter {
   export (spans) {
     log.debug(() => `Adding trace to queue: ${JSON.stringify(spans)}`)
 
-    let size = TRACE_PREFIX.length + TRACE_SUFFIX.length
+    let size = TRACE_FORMAT_OVERHEAD
     let queue = []
 
     for (const span of spans) {
       const spanStr = JSON.stringify(span)
-      if (spanStr.length > MAX_SIZE) {
+      if (spanStr.length + TRACE_FORMAT_OVERHEAD > MAX_SIZE) {
         log.debug('Span too large to send to logs, dropping')
         continue
       }
-      if (spanStr.length + size + 1 > MAX_SIZE) {
+      if (spanStr.length + size > MAX_SIZE) {
         this._printSpans(queue)
         queue = []
-        size = TRACE_PREFIX.length + TRACE_SUFFIX.length
+        size = TRACE_FORMAT_OVERHEAD
       }
       size += spanStr.length + 1 // includes length of ',' character
       queue.push(spanStr)
