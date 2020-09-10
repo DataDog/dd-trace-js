0e9e82b35298a2a4823568f171bf8eca6cf16bcd
Bryan English <bryan.english@datadoghq.com>
2020-09-10 15:47:48 -0400

    Allow benchmarking arbitrary e2e apps (#1084)
    * allow running e2e benchmark in an abritrary app directory
    
    * add an express app with many routes

---
+++ benchmark/e2e/express-helloworld-manyroutes/app.js
@@ -0,0 +1,10 @@
+
+const crypto = require('crypto')
+const app = require('express')()
+
+for (let i = 0; i < 100; i++) {
+  app.get(`/${crypto.randomBytes(8).toString('hex')}`, (req, res) => res.end(''))
+}
+
+app.get('/hello', (req, res) => res.end('hello world'))
+app.listen(3000)
