6ba43ddc0df9a7f37b46a96715858ef77589953c
Eduardo <eduardo.laranjo@kaluza.com>
2020-11-13 10:51:55 +0000

    Add KafkaJS consumer wrap

--- packages/datadog-plugin-kafkajs/src/index.js
+++ packages/datadog-plugin-kafkajs/src/index.js
@@ -1,98 +1,34 @@
-'use strict'
-const {
-  consumer: {
-    createConsumerStartBatchProcessTags,
-    addCommonConsumerTags,
-    createConsumerEndBatchProcessTags
-  }
-} = require('./utils')
+"use strict";
 
-function createWrapProducer (tracer, config) {
-  return function wrapProducer (createProducer) {
-    return function producerWithTrace () {
-      const serviceName = config.service || `${tracer._service}-kafka`
-      const createdProducer = createProducer.apply(this, arguments)
+function createWrapConsumer(tracer, config) {
+  return function wrapProcessEachMessage(Consumer) {
+    return function processEachMessageWithTrace() {
+      const consumer = Consumer.apply(this, arguments);
+      const run = consumer.run;
 
-      createdProducer.send = (...args) => {
-        const { topic, messages } = args[0]
+      consumer.run = async function ({eachMessage, ...args}) {
+        // return the promise
+        return run({
+          eachMessage: tracer.wrap("kafka", {}, eachMessage),
+          args
+        });
+      };
 
-        const tags = {
-          'service.name': serviceName,
-          'resource.name': `produce to ${topic}`,
-          'span.kind': 'producer',
-          'span.type': 'queue',
-          component: 'kafka',
-          'kafka.topic': topic,
-          'kafka.batch.size': messages.length
-        }
-
-        return tracer.trace('kafka.producer.send', { tags }, () => createdProducer.send(...args))
-      }
-
-      return createdProducer
-    }
-  }
-}
-
-function createWrapConsumer (tracer, config) {
-  return function wrapConsumer (createConsumer) {
-    return function consumerWithTrace () {
-      const serviceName = config.service || `${tracer._service}-kafka`
-      const createdConsumer = createConsumer.apply(this, arguments)
-
-      const { START_BATCH_PROCESS, END_BATCH_PROCESS } = createdConsumer.events
-
-      createdConsumer.on(START_BATCH_PROCESS, ({ type, payload }) => {
-        const childOf = tracer.scope().active()
-
-        tracer.trace(type, {
-          tags: addCommonConsumerTags(
-            serviceName,
-            'Consumer start batch process',
-            createConsumerStartBatchProcessTags(payload)
-          ),
-          childOf
-        })
-      })
-
-      createdConsumer.on(END_BATCH_PROCESS, ({ type, payload }) => {
-        const childOf = tracer.scope().active()
-
-        tracer.trace(type, {
-          tags: addCommonConsumerTags(
-            serviceName,
-            'Consumer end batch process',
-            createConsumerEndBatchProcessTags(payload)
-          ),
-          childOf
-        })
-      })
-
-      return createdConsumer
-    }
-  }
+      return consumer;
+    };
+  };
 }
 
 module.exports = [
   {
-    name: 'kafkajs',
-    versions: ['>=1.2'],
-    patch ({ Kafka }, tracer, config) {
+    name: "kafkajs",
+    versions: [">=1.2"],
+    patch({ Kafka }, tracer, config) {
       this.wrap(
         Kafka.prototype,
-        'producer',
-        createWrapProducer(tracer, config)
-      )
-      this.wrap(
-        Kafka.prototype,
-        'consumer',
+        "consumer",
         createWrapConsumer(tracer, config)
-      )
-      this.wrap(Kafka.prototype, 'admin', createWrapProducer(tracer, config))
+      );
     },
-    unpatch ({ Kafka }) {
-      this.unwrap(Kafka.prototype, 'producer')
-      this.unwrap(Kafka.prototype, 'consumer')
-    }
-  }
-]
+  },
+];
