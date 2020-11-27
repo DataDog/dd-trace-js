951c32a708b1e3e0da49cf7641ea1299c31d483e
Pedro Filipe <pedro.filipe@kaluza.com>
2020-11-27 15:15:00 +0000

    Fix producer.send wrap

--- packages/datadog-plugin-kafkajs/src/index.js
+++ packages/datadog-plugin-kafkajs/src/index.js
@@ -1,74 +1,71 @@
-"use strict";
+'use strict'
 
-function createWrapProducer(tracer, config) {
-  return function wrapProducer(createProducer) {
-    return function producerWithTrace() {
-      const serviceName = config.service || `${tracer._service}-kafka`;
-      const createdProducer = createProducer.apply(this, arguments);
+function createWrapProducer (tracer, config) {
+  return function wrapProducer (createProducer) {
+    return function producerWithTrace () {
+      const serviceName = config.service || `${tracer._service}-kafka`
+      const createdProducer = createProducer.apply(this, arguments)
 
-      const send = createdProducer.send;
+      const send = createdProducer.send
 
-      createdProducer.send = (...args) => {
-        const { topic, messages } = args[0];
+      const tags = {
+        'service.name': serviceName,
+        // 'resource.name': `produce to ${topic}`,
+        'span.kind': 'producer',
+        'span.type': 'queue',
+        component: 'kafka'
+        // 'kafka.topic': topic,
+        // 'kafka.batch.size': messages.length
+      }
 
-        const tags = {
-          "service.name": serviceName,
-          "resource.name": `produce to ${topic}`,
-          "span.kind": "producer",
-          "span.type": "queue",
-          component: "kafka",
-          "kafka.topic": topic,
-          "kafka.batch.size": messages.length,
-        };
 
-        return tracer.wrap("kafka.producer.send", { tags }, () =>
-          send.apply(this, ...args)
-        );
-      };
+      createdProducer.send = tracer.wrap('kafka.producer.send', { tags }, function (...args) {
+        return send.apply(this, args)
+      })
 
-      return createdProducer;
-    };
-  };
+      return createdProducer
+    }
+  }
 }
 
-function createWrapConsumer(tracer, config) {
-  return function wrapProcessEachMessage(Consumer) {
-    return function processEachMessageWithTrace() {
-      const consumer = Consumer.apply(this, arguments);
-      const run = consumer.run;
+function createWrapConsumer (tracer, config) {
+  return function wrapProcessEachMessage (Consumer) {
+    return function processEachMessageWithTrace () {
+      const consumer = Consumer.apply(this, arguments)
+      const run = consumer.run
 
       consumer.run = async function ({ eachMessage, ...args }) {
         // return the promise
         return run({
-          eachMessage: tracer.wrap("kafkajs.consumer", {}, eachMessage),
-          args,
-        });
-      };
+          eachMessage: tracer.wrap('kafkajs.consumer', {}, eachMessage),
+          args
+        })
+      }
 
-      return consumer;
-    };
-  };
+      return consumer
+    }
+  }
 }
 
 module.exports = [
   {
-    name: "kafkajs",
-    versions: [">=1.2"],
-    patch({ Kafka }, tracer, config) {
+    name: 'kafkajs',
+    versions: ['>=1.2'],
+    patch ({ Kafka }, tracer, config) {
       this.wrap(
         Kafka.prototype,
-        "producer",
+        'producer',
         createWrapProducer(tracer, config)
-      ),
-        this.wrap(
-          Kafka.prototype,
-          "consumer",
-          createWrapConsumer(tracer, config)
-        );
+      )
+      this.wrap(
+        Kafka.prototype,
+        'consumer',
+        createWrapConsumer(tracer, config)
+      )
     },
-    unpatch({ Kafka }) {
-      this.unwrap(Kafka.prototype, "producer");
-      this.unwrap(Kafka.prototype, "consumer");
-    },
-  },
-];
+    unpatch ({ Kafka }) {
+      this.unwrap(Kafka.prototype, 'producer')
+      this.unwrap(Kafka.prototype, 'consumer')
+    }
+  }
+]
