8dd96740f8bc5712cbe85651d78180e041bc39a2
djchinia <dan.chiniara@datadoghq.com>
2020-12-09 17:35:38 -0500

    add createWrapExecute

---
+++ packages/datadog-plugin-oracledb/src/index.js
@@ -0,0 +1,29 @@
+
+function createWrapExecute (tracer, config) {
+    return function wrapExecute (execute) {
+        return function executeWithTrace (dbQuery, ...args) {
+            return tracer.trace('exec.query', {
+                tags: {
+                    'span.kind': 'client', 
+                    'sql.query': dbQuery}}, 
+                    (span) => {
+                // const service = 
+                // const connectStringObj = new URL('http://' + config.connectString);
+                // span.setTag('db', {'instance':connectStringObj.pathname.substring(1), 
+                // 'hostname':connectStringObj.hostname, 'user':config.user, port:connectStringObj.port});
+                return execute.call(this, dbQuery, ...args)
+            })
+        }
+    }
+}
+
+module.exports = { 
+    name: 'oracledb',
+    versions: ['*'],
+    patch (oracledb, tracer, config){
+        this.wrap(oracledb.Connection.prototype, 'execute', createWrapExecute(tracer, config))
+    },
+    unpatch(oracledb) {
+        this.unwrap(oracledb.Connection.prototype, 'execute')
+    }
+}
