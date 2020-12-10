5cf6178b0088b7c3a5dca6a21b7e5dc626171a85
djchinia <dan.chiniara@datadoghq.com>
2020-12-10 15:16:06 -0500

    add getServiceName

--- packages/datadog-plugin-oracledb/src/index.js
+++ packages/datadog-plugin-oracledb/src/index.js
@@ -7,12 +7,18 @@ function createWrapExecute (tracer, config) {
                 tags: {
                     'span.kind': 'client', 
                     'sql.query': dbQuery
-                }}, 
-                (span) => {
-                    const connAttrs = new URL('http://' + this._connAttrs);
-                    span.setTag('db', {'instance':connAttrs.pathname.substring(1), 
-                    'hostname':connAttrs.hostname, 'user':config.user, port:connAttrs.port
-            });
+                }
+            }, (span) => {
+                const connAttrs = this._connAttrs
+                const service = getServiceName(tracer, config, connAttrs)
+                const connectStringObj = new URL('http://' + connAttrs.connectString);
+                span.setTag('db', {
+                    'instance':connectStringObj.pathname.substring(1), 
+                    'hostname':connectStringObj.hostname, 
+                    'user':config.user, 
+                    'port':connectStringObj.port
+                });
+                span.setTag('service.name', service);
                 return execute.call(this, dbQuery, ...args)
             })
         }
@@ -26,6 +32,16 @@ function createWrapGetConnection (tracer, config) {
             conn._connAttrs = connAttrs
             return conn
         }
+    }
+}
+
+function getServiceName (tracer, config, connAttrs) {
+    if (typeof config.service === 'function') {
+        return config.service(connAttrs)
+    } else if (config.service) {
+        return config.service
+    } else {
+        return `${tracer._service}-oracledb`
     }
 }
 
