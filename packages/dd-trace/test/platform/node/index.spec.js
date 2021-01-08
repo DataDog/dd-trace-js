03752c65bffe1534b2efdf989bdcfac36eafa9ca
Roch Devost <roch.devost@datadoghq.com>
2021-01-08 11:43:16 -0500

    add container tagging support for ecs docker id (#1176)

--- packages/dd-trace/test/platform/node/index.spec.js
+++ packages/dd-trace/test/platform/node/index.spec.js
@@ -169,10 +169,84 @@ describe('Platform', () => {
       })
     })
 
+    describe('docker', () => {
+      let docker
+      let fs
+
+      beforeEach(() => {
+        fs = {
+          readFileSync: sinon.stub()
+        }
+      })
+
+      it('should return an empty ID when the cgroup cannot be read', () => {
+        docker = proxyquire('../src/platform/node/docker', { fs })
+
+        expect(docker.id()).to.be.undefined
+      })
+
+      it('should support IDs with long format', () => {
+        const cgroup = [
+          '1:name=systemd:/docker/34dc0b5e626f2c5c4c5170e34b10e7654ce36f0fcd532739f4445baabea03376'
+        ].join('\n')
+
+        fs.readFileSync.withArgs('/proc/self/cgroup').returns(Buffer.from(cgroup))
+        docker = proxyquire('../src/platform/node/docker', { fs })
+
+        expect(docker.id()).to.equal('34dc0b5e626f2c5c4c5170e34b10e7654ce36f0fcd532739f4445baabea03376')
+      })
+
+      it('should support IDs with UUID format', () => {
+        const cgroup = [
+          '1:name=systemd:/uuid/34dc0b5e-626f-2c5c-4c51-70e34b10e765'
+        ].join('\n')
+
+        fs.readFileSync.withArgs('/proc/self/cgroup').returns(Buffer.from(cgroup))
+        docker = proxyquire('../src/platform/node/docker', { fs })
+
+        expect(docker.id()).to.equal('34dc0b5e-626f-2c5c-4c51-70e34b10e765')
+      })
+
+      it('should support IDs with ECS task format', () => {
+        const cgroup = [
+          '1:name=systemd:/ecs/34dc0b5e626f2c5c4c5170e34b10e765-1234567890'
+        ].join('\n')
+
+        fs.readFileSync.withArgs('/proc/self/cgroup').returns(Buffer.from(cgroup))
+        docker = proxyquire('../src/platform/node/docker', { fs })
+
+        expect(docker.id()).to.equal('34dc0b5e626f2c5c4c5170e34b10e765-1234567890')
+      })
+
+      it('should support IDs with scope suffix', () => {
+        const cgroup = [
+          '1:name=systemd:/docker/34dc0b5e626f2c5c4c5170e34b10e7654ce36f0fcd532739f4445baabea03376.scope'
+        ].join('\n')
+
+        fs.readFileSync.withArgs('/proc/self/cgroup').returns(Buffer.from(cgroup))
+        docker = proxyquire('../src/platform/node/docker', { fs })
+
+        expect(docker.id()).to.equal('34dc0b5e626f2c5c4c5170e34b10e7654ce36f0fcd532739f4445baabea03376')
+      })
+
+      it('should support finding IDs on any line of the cgroup', () => {
+        const cgroup = [
+          '1:name=systemd:/nope',
+          '2:pids:/docker/34dc0b5e626f2c5c4c5170e34b10e7654ce36f0fcd532739f4445baabea03376',
+          '3:cpu:/invalid'
+        ].join('\n')
+
+        fs.readFileSync.withArgs('/proc/self/cgroup').returns(Buffer.from(cgroup))
+        docker = proxyquire('../src/platform/node/docker', { fs })
+
+        expect(docker.id()).to.equal('34dc0b5e626f2c5c4c5170e34b10e7654ce36f0fcd532739f4445baabea03376')
+      })
+    })
+
     describe('request', () => {
       let request
       let log
-      let getContainerInfo
+      let docker
 
       beforeEach(() => {
         nock.disableNetConnect()
@@ -180,12 +254,12 @@ describe('Platform', () => {
         log = {
           error: sinon.spy()
         }
-        getContainerInfo = {
-          sync: sinon.stub().returns({ containerId: 'abcd' })
+        docker = {
+          id: sinon.stub().returns('abcd')
         }
         platform = require('../../../src/platform/node')
         request = proxyquire('../src/platform/node/request', {
-          'container-info': getContainerInfo,
+          './docker': docker,
           '../../log': log
         }).bind(platform)
       })
