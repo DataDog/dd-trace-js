b60c624d3db8594a745cfa8920733cc7bacf489e
Juan Fernandez <juan.fernandezdealba@datadoghq.com>
2021-01-15 17:28:56 +0100

    [ci-app] use proxyquire correctly

--- packages/dd-trace/test/plugins/util/git.spec.js
+++ packages/dd-trace/test/plugins/util/git.spec.js
@@ -1,23 +1,18 @@
 const proxyquire = require('proxyquire')
 
-const { getGitMetadata, GIT_COMMIT_SHA, GIT_BRANCH, GIT_REPOSITORY_URL } = require('../../../src/plugins/util/git')
+const sanitizedExecStub = sinon.stub()
+const {
+  getGitMetadata,
+  GIT_COMMIT_SHA,
+  GIT_BRANCH,
+  GIT_REPOSITORY_URL
+} = proxyquire('../../../src/plugins/util/git', { './exec': {
+  'sanitizedExec': sanitizedExecStub
+} })
 
-describe('git', () => {
-  let childProcess
-  beforeEach(() => {
-    childProcess = proxyquire('child_process', { execSync: sinon.spy() })
-  })
+describe('gitt', () => {
   afterEach(() => {
-    childProcess.execSync.restore()
-  })
-  it('returns ci metadata if present and does not call git', () => {
-    const ciMetadata = { commitSHA: 'ciSHA', branch: 'ciBranch', repositoryUrl: 'ciRepositoryUrl' }
-    const metadata = getGitMetadata(ciMetadata)
-
-    expect(metadata).to.eql(
-      { [GIT_COMMIT_SHA]: 'ciSHA', [GIT_BRANCH]: 'ciBranch', [GIT_REPOSITORY_URL]: 'ciRepositoryUrl' }
-    )
-    expect(childProcess.execSync).not.to.have.been.called
+    sanitizedExecStub.reset()
   })
   it('calls git when some ci metadata is not present', () => {
     const ciMetadata = { commitSHA: 'ciSHA', branch: 'ciBranch' }
@@ -30,6 +25,15 @@ describe('git', () => {
       }
     )
     expect(metadata[GIT_REPOSITORY_URL]).not.to.equal('ciRepositoryUrl')
-    expect(childProcess.execSync).to.have.been.calledWith('git ls-remote --get-url', { stdio: 'pipe' })
+    expect(sanitizedExecStub).to.have.been.calledWith('git ls-remote --get-url', { stdio: 'pipe' })
+  })
+  it('returns ci metadata if present and does not call git', () => {
+    const ciMetadata = { commitSHA: 'ciSHA', branch: 'ciBranch', repositoryUrl: 'ciRepositoryUrl' }
+    const metadata = getGitMetadata(ciMetadata)
+
+    expect(metadata).to.eql(
+      { [GIT_COMMIT_SHA]: 'ciSHA', [GIT_BRANCH]: 'ciBranch', [GIT_REPOSITORY_URL]: 'ciRepositoryUrl' }
+    )
+    expect(sanitizedExecStub).not.to.have.been.called
   })
 })
