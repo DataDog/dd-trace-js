64bc8f33907d10cc66248bdaa213046bf9464b9e
rochdev <roch.devost@datadoghq.com>
2021-02-04 13:22:47 -0500

    refactor

--- packages/dd-trace/src/scope.js
+++ packages/dd-trace/src/scope.js
@@ -5,7 +5,8 @@ const scopes = require('../../../ext/scopes')
 
 const NOOP = scopes.NOOP
 
-const hasJavaScriptHooks = semver.satisfies(process.versions.node, '>=14.5 || ^12.19.0')
+// https://github.com/nodejs/node/pull/33801
+const hasJavaScriptAsyncHooks = semver.satisfies(process.versions.node, '>=14.5 || ^12.19.0')
 
 module.exports = name => {
   let Scope
@@ -14,7 +15,7 @@ module.exports = name => {
     Scope = require('./scope/base')
   } else if (name === scopes.ASYNC_LOCAL_STORAGE) {
     Scope = require('./scope/async_local_storage')
-  } else if (name === scopes.ASYNC_RESOURCE || (!name && hasJavaScriptHooks)) {
+  } else if (name === scopes.ASYNC_RESOURCE || (!name && hasJavaScriptAsyncHooks)) {
     Scope = require('./scope/async_resource')
   } else {
     Scope = require('./scope/async_hooks')
