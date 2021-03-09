bab771f3ce6c2cb37e692eea3851485ff09c16b9
Juan Fernandez <juan.fernandezdealba@datadoghq.com>
2021-03-09 13:17:38 +0100

    [ci-app] add configuration metadata

---
+++ packages/dd-trace/test/plugins/util/env.spec.js
@@ -0,0 +1,25 @@
+const os = require('os')
+const {
+  getRuntimeAndOSMetadata,
+  OS_ARCHITECTURE,
+  OS_PLATFORM,
+  OS_VERSION,
+  RUNTIME_NAME,
+  RUNTIME_VERSION
+} = require('../../../src/plugins/util/env')
+
+describe('env', () => {
+  it('reads env data', () => {
+    const envMetadata = getRuntimeAndOSMetadata()
+
+    expect(envMetadata).to.eql(
+      {
+        [RUNTIME_VERSION]: process.version,
+        [OS_ARCHITECTURE]: process.arch,
+        [OS_PLATFORM]: process.platform,
+        [RUNTIME_NAME]: 'node',
+        [OS_VERSION]: os.release()
+      }
+    )
+  })
+})
