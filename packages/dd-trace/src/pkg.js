d56ceac3651ecaa679131926fb90a223cef59d3d
Aaron Arinder <aaronarinder@gmail.com>
2021-03-30 13:06:43 -0400

    chore: dep upgrade: read-pkg-up: use maj 7 version     - using major version 8 is incompatible with node 8

--- packages/dd-trace/src/pkg.js
+++ packages/dd-trace/src/pkg.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const path = require('path')
-const { readPackageUpSync } = require('read-pkg-up')
+const readPkgUp = require('read-pkg-up')
 
 function findRoot () {
   return require.main && require.main.filename ? path.dirname(require.main.filename) : process.cwd()
@@ -9,7 +9,7 @@ function findRoot () {
 
 function findPkg () {
   const cwd = findRoot()
-  const up = readPackageUpSync({ cwd })
+  const up = readPkgUp.sync({ cwd })
 
   return up && up.pkg ? up.pkg : {}
 }
