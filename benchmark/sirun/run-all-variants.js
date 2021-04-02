89151e463b81bdb27cf64d83e2984d05d6c9f4df
Bryan English <bryan.english@datadoghq.com>
2021-04-02 16:47:06 -0400

    output every minute in sirun tests to avoid timeout

--- benchmark/sirun/run-all-variants.js
+++ benchmark/sirun/run-all-variants.js
@@ -7,12 +7,18 @@ const metaJson = require(path.join(process.cwd(), 'meta.json'))
 
 const env = Object.assign({}, process.env, { SIRUN_NO_STDIO: '1' })
 
+const interval = setInterval(() => {
+  // esline-ignore-next-line no-console
+  console.error('This test is still running one minute later...')
+}, 60000)
+const clear = () => clearInterval(interval)
+
 if (metaJson.variants) {
   const variants = metaJson.variants
   for (const variant in variants) {
     const variantEnv = Object.assign({}, env, { SIRUN_VARIANT: variant })
-    childProcess.execSync(`sirun meta.json`, { env: variantEnv, stdio: 'inherit' })
+    childProcess.exec(`sirun meta.json`, { env: variantEnv, stdio: 'inherit' }, clear)
   }
 } else {
-  childProcess.exec(`sirun meta.json`, { env, stdio: 'inherit' })
+  childProcess.exec(`sirun meta.json`, { env, stdio: 'inherit' }, clear)
 }
