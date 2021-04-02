a9e186683078bfbd179b05664f6761bdfbe3aefc
Bryan English <bryan.english@datadoghq.com>
2021-04-02 14:27:39 -0400

    updates to get-results.js (#1288)
    * support github's pagination in get-results.js
    
    * add min and max to summaries in get-results.js
    
    * use a do while loop

--- benchmark/sirun/get-results.js
+++ benchmark/sirun/get-results.js
@@ -12,8 +12,8 @@ const circleHeaders = CIRCLE_TOKEN ? {
   'circle-token': CIRCLE_TOKEN
 } : {}
 
-const statusUrl = ref =>
-  `https://api.github.com/repos/DataDog/dd-trace-js/commits/${ref}/statuses?per_page=100`
+const statusUrl = (ref, page) =>
+  `https://api.github.com/repos/DataDog/dd-trace-js/commits/${ref}/statuses?per_page=100&page=${page}`
 const artifactsUrl = num =>
   `https://circleci.com/api/v1.1/project/github/DataDog/dd-trace-js/${num}/artifacts`
 
@@ -38,7 +38,13 @@ function get (url, headers) {
 }
 
 async function getBuildNumsFromGithub (ref) {
-  const results = JSON.parse(await get(statusUrl(ref)))
+  const results = []
+  let page = 0
+  let reply
+  do {
+    reply = JSON.parse(await get(statusUrl(ref, ++page)))
+    results.push(...reply)
+  } while (reply.length === 100)
   const namesAndNums = {}
   for (const build of results.filter(s => s.context.includes('-sirun-'))) {
     const url = new URL(build.target_url)
@@ -71,7 +77,7 @@ function summary (iterations) {
   for (const [name, items] of Object.entries(stats)) {
     const m = mean(items)
     const s = stddev(m, items)
-    result[name] = { mean: m, stddev: s }
+    result[name] = { mean: m, stddev: s, min: Math.min(...items), max: Math.max(...items) }
   }
   return result
 }
