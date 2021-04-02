859393169379f423dc65cefc362595baca438dcb
Bryan English <bryan.english@datadoghq.com>
2021-04-02 16:55:08 -0400

    fix lint

--- benchmark/sirun/run-all-variants.js
+++ benchmark/sirun/run-all-variants.js
@@ -8,16 +8,22 @@ const metaJson = require(path.join(process.cwd(), 'meta.json'))
 const env = Object.assign({}, process.env, { SIRUN_NO_STDIO: '1' })
 
 const interval = setInterval(() => {
-  // esline-ignore-next-line no-console
+  // eslint-disable-next-line no-console
   console.error('This test is still running one minute later...')
 }, 60000)
 const clear = () => clearInterval(interval)
 
 if (metaJson.variants) {
   const variants = metaJson.variants
+  const len = Object.keys(variants).length
+  let count = 0
   for (const variant in variants) {
     const variantEnv = Object.assign({}, env, { SIRUN_VARIANT: variant })
-    childProcess.exec(`sirun meta.json`, { env: variantEnv, stdio: 'inherit' }, clear)
+    childProcess.exec(`sirun meta.json`, { env: variantEnv, stdio: 'inherit' }, () => {
+      if (++count === len) {
+        clear()
+      }
+    })
   }
 } else {
   childProcess.exec(`sirun meta.json`, { env, stdio: 'inherit' }, clear)
