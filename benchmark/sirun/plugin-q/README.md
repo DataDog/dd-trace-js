83bd3658d656aa0f4ff219724aa93ea25d0dbe85
Bryan English <bryan.english@datadoghq.com>
2021-04-08 11:05:08 -0400

    add sirun test for q plugin (#1296)

---
+++ benchmark/sirun/plugin-q/README.md
@@ -0,0 +1,3 @@
+This creates 50000 promises in a chain using the latest version of `q`.
+
+The variants are with the tracer and without it.
---
+++ benchmark/sirun/plugin-q/index.js
@@ -0,0 +1,18 @@
+'use strict'
+
+if (Number(process.env.USE_TRACER)) {
+  require('../../..').init()
+}
+
+const Q = require('../../../versions/q/node_modules/q')
+
+const count = process.env.COUNT ? Number(process.env.COUNT) : 50000
+
+const p = Q.defer()
+let pChain = p.promise
+
+for (let i = 0; i < count; i++) {
+  pChain = pChain.then(() => {})
+}
+
+p.resolve()
