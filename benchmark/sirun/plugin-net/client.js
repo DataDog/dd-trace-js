b2b503334a362fb2edc2c31bc482cc79f775f581
Bryan English <bryan.english@datadoghq.com>
2021-04-12 17:02:49 -0400

    fix

--- benchmark/sirun/plugin-net/client.js
+++ benchmark/sirun/plugin-net/client.js
@@ -2,17 +2,22 @@
 
 const net = require('net')
 
+const { port, reqs } = require('./common')
+
 let connectionsMade = 0
 
 function run () {
-  const client = net.connect(process.env.PORT, () => {
+  const client = net.connect(port, () => {
     client.write('hello')
     client.on('data', () => {
-      client.end()
-      if (++connectionsMade !== 10000) {
-        run()
-      }
+      client.end(() => {
+        if (++connectionsMade !== reqs) {
+          run()
+        }
+      })
     })
+  }).on('error', () => {
+    setTimeout(run, 100)
   })
 }
 run()
---
+++ benchmark/sirun/plugin-net/common.js
@@ -0,0 +1,6 @@
+'use strict'
+
+module.exports = {
+  port: 3030,
+  reqs: 1000
+}
