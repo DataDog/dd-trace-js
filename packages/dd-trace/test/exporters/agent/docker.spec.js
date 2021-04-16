796446348eacc0c7f0b768e4f87f701237fd8f04
Roch Devost <roch.devost@datadoghq.com>
2021-04-16 13:21:41 -0400

    fix container id not reporting properly in kubernetes (#1314)

--- packages/dd-trace/test/exporters/agent/docker.spec.js
+++ packages/dd-trace/test/exporters/agent/docker.spec.js
@@ -51,9 +51,22 @@ describe('docker', () => {
     expect(docker.id()).to.equal('34dc0b5e626f2c5c4c5170e34b10e765-1234567890')
   })
 
-  it('should support IDs with scope suffix', () => {
+  it('should support IDs with Kubernetes format', () => {
     const cgroup = [
-      '1:name=systemd:/docker/34dc0b5e626f2c5c4c5170e34b10e7654ce36f0fcd532739f4445baabea03376.scope'
+      '1:name=systemd:/kubepods.slice/kubepods-burstable.slice/kubepods-burstable-pod2d3da189_6407_48e3_9ab6_78188d75e609.slice/docker-7b8952daecf4c0e44bbcefe1b5c5ebc7b4839d4eefeccefe694709d3809b6199.scope' // eslint-disable-line max-len
+    ].join('\n')
+
+    fs.readFileSync.withArgs('/proc/self/cgroup').returns(Buffer.from(cgroup))
+    docker = proxyquire('../src/exporters/agent/docker', { fs })
+
+    expect(docker.id()).to.equal('7b8952daecf4c0e44bbcefe1b5c5ebc7b4839d4eefeccefe694709d3809b6199')
+  })
+
+  it('should support finding IDs on any line of the cgroup', () => {
+    const cgroup = [
+      '1:name=systemd:/nope',
+      '2:pids:/docker/34dc0b5e626f2c5c4c5170e34b10e7654ce36f0fcd532739f4445baabea03376',
+      '3:cpu:/invalid'
     ].join('\n')
 
     fs.readFileSync.withArgs('/proc/self/cgroup').returns(Buffer.from(cgroup))
@@ -62,11 +75,9 @@ describe('docker', () => {
     expect(docker.id()).to.equal('34dc0b5e626f2c5c4c5170e34b10e7654ce36f0fcd532739f4445baabea03376')
   })
 
-  it('should support finding IDs on any line of the cgroup', () => {
+  it('should support Control Group v2', () => {
     const cgroup = [
-      '1:name=systemd:/nope',
-      '2:pids:/docker/34dc0b5e626f2c5c4c5170e34b10e7654ce36f0fcd532739f4445baabea03376',
-      '3:cpu:/invalid'
+      '0::/docker/34dc0b5e626f2c5c4c5170e34b10e7654ce36f0fcd532739f4445baabea03376'
     ].join('\n')
 
     fs.readFileSync.withArgs('/proc/self/cgroup').returns(Buffer.from(cgroup))
