d1c1c1ef6e171a01500842b21f29639ea0a08dcc
Roch Devost <roch.devost@datadoghq.com>
2021-04-21 16:04:15 -0400

    remove dependency on read-pkg-up (#1323)

--- packages/dd-trace/src/pkg.js
+++ packages/dd-trace/src/pkg.js
@@ -1,17 +1,37 @@
 'use strict'
 
+const fs = require('fs')
 const path = require('path')
-const readPkgUp = require('read-pkg-up')
 
 function findRoot () {
-  return require.main && require.main.filename ? path.dirname(require.main.filename) : process.cwd()
+  return require.main && require.main.filename
+    ? path.dirname(require.main.filename)
+    : process.cwd()
 }
 
 function findPkg () {
   const cwd = findRoot()
-  const up = readPkgUp.sync({ cwd })
+  const filePath = findUp('package.json', cwd)
 
-  return up && up.pkg ? up.pkg : {}
+  try {
+    return JSON.parse(fs.readFileSync(filePath, 'utf8'))
+  } catch (e) {
+    return {}
+  }
+}
+
+function findUp (name, cwd) {
+  let directory = path.resolve(cwd)
+  const { root } = path.parse(directory)
+
+  while (true) {
+    const current = path.resolve(directory, name)
+
+    if (fs.existsSync(current)) return current
+    if (directory === root) return
+
+    directory = path.dirname(directory)
+  }
 }
 
 module.exports = findPkg()
