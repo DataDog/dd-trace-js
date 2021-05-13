8fb9fdc3dea4a56d510fa2b77a50f392654290b2
Bryan English <bryan.english@datadoghq.com>
2021-05-13 16:59:13 -0400

    add sirun benchmark for span creation

---
+++ benchmark/sirun/spans/spans.js
@@ -0,0 +1,27 @@
+const tracer = require('../../..').init({
+  scope: 'noop'
+})
+
+tracer._tracer._processor.process = function process (span) {
+  const trace = span.context()._trace
+  this._erase(trace)
+}
+
+const { FINISH } = process.env
+
+const spans = []
+
+for (let i = 0; i < 1000000; i++) {
+  const span = tracer.startSpan('some.span.name', {})
+  if (FINISH === 'now') {
+    span.finish()
+  } else {
+    spans.push(span)
+  }
+}
+
+if (FINISH !== 'now') {
+  for (let i = 0; i < 1000000; i++) {
+    spans[i].finish()
+  }
+}
