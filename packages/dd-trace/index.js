60365fb1ef29f416e6f9e2a29e7233b5d80cc4f1
rochdev <roch.devost@datadoghq.com>
2021-05-19 15:23:44 -0400

    make global variable not enumerable

--- packages/dd-trace/index.js
+++ packages/dd-trace/index.js
@@ -3,7 +3,11 @@
 if (!global._ddtrace) {
   const TracerProxy = require('./src/proxy')
 
-  global._ddtrace = new TracerProxy()
+  Object.defineProperty(global, '_ddtrace', {
+    value: new TracerProxy(),
+    enumerable: false
+  })
+
   global._ddtrace.default = global._ddtrace
   global._ddtrace.tracer = global._ddtrace
 }
