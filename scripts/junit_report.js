3845671ed2c621dc43ccd0a7c6ce84ee9cc49dd1
Juan Fernandez <juan.fernandezdealba@datadoghq.com>
2021-05-24 15:02:44 +0200

    [ci-app] simplify logic

--- scripts/junit_report.js
+++ scripts/junit_report.js
@@ -8,6 +8,10 @@ function uploadJUnitXMLReport () {
     console.log('Node version incompatible with @datadog/datadog-ci. Skipping step.')
     return
   }
+  if (process.env.CIRCLE_PR_NUMBER) {
+    console.log('Running in a fork. Skipping step.')
+    return
+  }
   // we install @datadog/datadog-ci
   execSync('yarn add --dev @datadog/datadog-ci@0.13.0', { stdio: 'inherit' })
   // we execute the upload command
