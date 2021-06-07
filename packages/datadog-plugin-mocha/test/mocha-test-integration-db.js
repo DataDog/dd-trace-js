3e35be8fac2c8a4225792eb0a00dcfeff1bf016b
Juan Fernandez <juan.fernandezdealba@datadoghq.com>
2021-06-07 11:49:20 +0200

    [ci-app] fix version

--- packages/datadog-plugin-mocha/test/mocha-test-integration-db.js
+++ packages/datadog-plugin-mocha/test/mocha-test-integration-db.js
@@ -1,5 +1,5 @@
 const { expect } = require('chai')
-const { Client } = require('../../../versions/pg').get()
+const { Client } = require('../../../versions/pg@>=4').get()
 
 describe('mocha-test-integration-db', () => {
   it('can do integration tests with db', async () => {
