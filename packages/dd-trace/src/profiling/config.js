62ee8fb56ff9292176587c1e590dc4a9238bfaef
Stephen Belanger <stephen.belanger@datadoghq.com>
2021-06-11 13:56:09 -0700

    Fix profiler config propagation (#1403)

--- packages/dd-trace/src/profiling/config.js
+++ packages/dd-trace/src/profiling/config.js
@@ -2,7 +2,9 @@
 
 const coalesce = require('koalas')
 const os = require('os')
+const { URL } = require('url')
 const { AgentExporter } = require('./exporters/agent')
+const { FileExporter } = require('./exporters/file')
 const { InspectorCpuProfiler } = require('./profilers/inspector/cpu')
 const { InspectorHeapProfiler } = require('./profilers/inspector/heap')
 const { ConsoleLogger } = require('./loggers/console')
@@ -13,7 +15,10 @@ const {
   DD_ENV,
   DD_TAGS,
   DD_SERVICE,
-  DD_VERSION
+  DD_VERSION,
+  DD_TRACE_AGENT_URL,
+  DD_AGENT_HOST,
+  DD_TRACE_AGENT_PORT
 } = process.env
 
 class Config {
@@ -29,17 +34,25 @@ class Config {
     this.service = service
     this.env = env
     this.host = host
+
     this.version = version
     this.tags = Object.assign(
       tagger.parse(DD_TAGS),
       tagger.parse(options.tags),
       tagger.parse({ env, host, service, version })
     )
-    this.logger = options.logger || new ConsoleLogger()
+    this.logger = ensureLogger(options.logger)
     this.flushInterval = flushInterval
-    this.exporters = options.exporters || [
-      new AgentExporter()
-    ]
+
+    const hostname = coalesce(options.hostname, DD_AGENT_HOST, 'localhost')
+    const port = coalesce(options.port, DD_TRACE_AGENT_PORT, 8126)
+    const url = new URL(coalesce(options.url, DD_TRACE_AGENT_URL,
+      `http://${hostname || 'localhost'}:${port || 8126}`))
+
+    this.exporters = ensureExporters(options.exporters || [
+      new AgentExporter({ url })
+    ], options)
+
     this.profilers = options.profilers || [
       new InspectorCpuProfiler(),
       new InspectorHeapProfiler()
@@ -48,3 +61,39 @@ class Config {
 }
 
 module.exports = { Config }
+
+function getExporter (name, options) {
+  switch (name) {
+    case 'agent':
+      return new AgentExporter(options)
+    case 'file':
+      return new FileExporter(options)
+  }
+}
+
+function ensureExporters (exporters, options) {
+  if (typeof exporters === 'string') {
+    exporters = exporters.split(',')
+  }
+
+  for (let i = 0; i < exporters.length; i++) {
+    const exporter = exporters[i]
+    if (typeof exporter === 'string') {
+      exporters[i] = getExporter(exporter, options)
+    }
+  }
+
+  return exporters
+}
+
+function ensureLogger (logger) {
+  if (typeof logger !== 'object' ||
+    typeof logger.debug !== 'function' ||
+    typeof logger.info !== 'function' ||
+    typeof logger.warn !== 'function' ||
+    typeof logger.error !== 'function') {
+    return new ConsoleLogger()
+  }
+
+  return logger
+}
