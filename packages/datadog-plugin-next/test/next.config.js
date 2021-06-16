7a4367bc54ef32914c03096677b557ede6a671f5
rochdev <roch.devost@datadoghq.com>
2021-06-16 17:52:47 -0400

    add support for webpack 5 and switch to webpack 4 for now

--- packages/datadog-plugin-next/test/next.config.js
+++ packages/datadog-plugin-next/test/next.config.js
@@ -5,6 +5,13 @@ module.exports = {
     ignoreDuringBuilds: true
   },
 
+  webpack5: false,
+
+  future: {
+    // legacy option
+    webpack5: false
+  },
+
   webpack: (config) => {
     const react = path.resolve(__dirname, '../../../versions/node_modules/react')
     const reactDom = path.resolve(__dirname, '../../../versions/node_modules/react-dom')
@@ -13,6 +20,12 @@ module.exports = {
       'react': `commonjs2 ${react}`,
       'react-dom': `commonjs2 ${reactDom}`
     }
+
+    // TODO: drop support for Next <10.1, enable webpack5 above, and uncomment:
+    // config.module.rules.push({
+    //   test: /\/dd-trace\/src\/native\/.+$/,
+    //   type: 'asset/source'
+    // })
 
     return config
   }
