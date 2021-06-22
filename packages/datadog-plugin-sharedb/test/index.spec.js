aa3dd6e37899feda2d6c8cf7ebc359a94b3f4ad9
Devon Winrick <devon.winrick@lever.co>
2021-06-21 17:13:45 -0700

    Tests for breaking middleware

--- packages/datadog-plugin-sharedb/test/index.spec.js
+++ packages/datadog-plugin-sharedb/test/index.spec.js
@@ -51,9 +51,55 @@ describe('Plugin', () => {
               expect(traces[0][0].meta).to.have.property('span.kind', 'client')
               expect(traces[0][0].meta).to.have.property('service', 'test')
               expect(traces[0][0].meta).to.have.property('resource.method', 'fetch')
-
-              done()
             })
+              .then(done)
+              .catch(done)
+          });
+        })
+
+        it('should be compatible with existing middleware', done => {
+          const receiveSpy = sinon.spy((request, next) => {
+            next();
+          })
+          const replySpy = sinon.spy((request, next) => {
+            next();
+          })
+          backend.use('receive', receiveSpy);
+          backend.use('reply', replySpy);
+          const doc = connection.get('some-collection', 'some-id');
+
+          doc.fetch(function(err) {
+            if (err) { throw err }
+
+            agent.use(traces => {
+              expect(receiveSpy).to.have.been.calledWithMatch(sinon.match.object, sinon.match.func);
+              expect(replySpy).to.have.been.calledWithMatch(sinon.match.object, sinon.match.func);
+              expect(traces[0][0]).to.have.property('service', 'test-sharedb')
+            })
+              .then(done)
+              .catch(done)
+          });
+        })
+
+        it('should sanitize queries', done => {
+          connection.createFetchQuery('some-collection', {
+            randomValues: {
+              property: 'query',
+              one: 1
+            }
+          }, {}, function(err) {
+            if (err) { throw err }
+
+            agent.use(traces => {
+              expect(traces[0][0]).to.have.property('service', 'test-sharedb')
+              expect(traces[0][0]).to.have.property('resource', 'query-fetch some-collection {"randomValues":{"property":"?","one":"?"}}')
+              expect(traces[0][0]).to.have.property('type', 'sharedb.request')
+              expect(traces[0][0].meta).to.have.property('span.kind', 'client')
+              expect(traces[0][0].meta).to.have.property('service', 'test')
+              expect(traces[0][0].meta).to.have.property('resource.method', 'query-fetch')
+            })
+              .then(done)
+              .catch(done)
           });
         })
       })
