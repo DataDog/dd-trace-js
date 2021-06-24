6ae1a8c449d21ee3f0085a3311d0dbca3b611f97
Devon Winrick <devon.winrick@lever.co>
2021-06-24 12:11:29 -0700

    Tests for handling malformed messages + covering receive and reply hooks

--- packages/datadog-plugin-sharedb/test/index.spec.js
+++ packages/datadog-plugin-sharedb/test/index.spec.js
@@ -105,6 +105,89 @@ describe('Plugin', () => {
               .catch(done)
           })
         })
+
+        it('should gracefully handle an invalid or unsupported message action', done => {
+          let isDone = false
+          const receiveSpy = sinon.spy((request, next) => {
+            next()
+            if (!isDone) {
+              done()
+              isDone = true
+            }
+          })
+          backend.use('receive', receiveSpy)
+          const message = {
+            data: {
+              a: 'some-unsupported-action'
+            }
+          }
+
+          backend.trigger(backend.MIDDLEWARE_ACTIONS.receive, {}, message, function noop () {})
+        })
+
+        it('should gracefully handle a message without data', done => {
+          let isDone = false
+          const receiveSpy = sinon.spy((request, next) => {
+            next()
+            if (!isDone) {
+              done()
+              isDone = true
+            }
+          })
+          backend.use('receive', receiveSpy)
+          const message = {}
+          backend.trigger(backend.MIDDLEWARE_ACTIONS.receive, {}, message, function noop () {})
+        })
+      })
+
+      describe('with configuration', () => {
+        let backend
+        let connection
+        let receiveHookSpy
+        let replyHookSpy
+
+        before(() => {
+          receiveHookSpy = sinon.spy()
+          replyHookSpy = sinon.spy()
+          return agent.load('sharedb', {
+            service: 'test-sharedb',
+            hooks: {
+              receive: receiveHookSpy,
+              reply: replyHookSpy
+            }
+          })
+        })
+
+        after(() => {
+          return agent.close()
+        })
+
+        beforeEach(() => {
+          ShareDB = require(`../../../versions/sharedb@${version}`).get()
+
+          backend = new ShareDB({ presence: true })
+          connection = backend.connect()
+        })
+
+        afterEach(() => {
+          connection.close()
+        })
+
+        it('should support receive and reply hooks', done => {
+          const doc = connection.get('some-collection', 'some-id')
+
+          doc.fetch(function (err) {
+            if (err) { throw err }
+
+            agent.use(traces => {
+              expect(traces[0][0]).to.have.property('service', 'test-sharedb')
+              expect(receiveHookSpy).to.have.been.calledWithMatch(sinon.match.object, sinon.match.object)
+              expect(replyHookSpy).to.have.been.calledWithMatch(sinon.match.object, sinon.match.object)
+            })
+              .then(done)
+              .catch(done)
+          })
+        })
       })
     })
   })
