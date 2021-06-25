6b68539c0184d6bfc29b8018f5cbad72c6e17785
Bryan English <bryan.english@datadoghq.com>
2021-06-25 16:47:16 -0400

    add startup benchmark requiring everything

--- benchmark/sirun/startup/startup-test.js
+++ benchmark/sirun/startup/startup-test.js
@@ -3,3 +3,15 @@
 if (Number(process.env.USE_TRACER)) {
   require('../../..').init()
 }
+
+if (Number(process.env.EVERYTHING)) {
+  const json = require('../../../package.json')
+  for (const pkg in json.dependencies) {
+    if (pkg !== '@types/node' && pkg !== 'nan') {
+      require(pkg)
+    }
+  }
+  for (const devPkg in json.devDependencies) {
+    require(devPkg)
+  }
+}
