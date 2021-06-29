3ba9529863700871865510a8dc707efa1d5400cf
Roch Devost <roch.devost@datadoghq.com>
2021-06-29 16:18:09 -0400

    fix incorrect 3rd-party licenses and improve license validation (#1457)

--- scripts/check_licenses.js
+++ scripts/check_licenses.js
@@ -6,13 +6,16 @@ const readline = require('readline')
 const pkg = require(path.join(__dirname, '..', '/package.json'))
 
 const filePath = path.join(__dirname, '..', '/LICENSE-3rdparty.csv')
-const deps = Object.keys(pkg.dependencies || {})
-  .concat(Object.keys(pkg.devDependencies || {}))
-  .concat(Object.keys(pkg.optionalDependencies || {}))
-  .sort()
+const deps = new Set(Object.keys(pkg.dependencies || {}))
+const devDeps = new Set(Object.keys(pkg.devDependencies || {}))
 
 let index = 0
-const licenses = []
+
+const licenses = {
+  require: new Set(),
+  dev: new Set(),
+  file: new Set()
+}
 
 const lineReader = readline.createInterface({
   input: fs.createReadStream(filePath)
@@ -24,25 +27,43 @@ lineReader.on('line', line => {
     const type = columns[0]
     const license = columns[1]
 
-    if (type === 'require' || type === 'dev') {
-      licenses.push(license)
-    }
+    licenses[type].add(license)
   }
 
   index++
 })
 
 lineReader.on('close', () => {
-  /* eslint-disable no-console */
-
-  if (JSON.stringify(deps) !== JSON.stringify(licenses.sort())) {
-    console.log('Dependencies:')
-    console.log(deps.join(','))
-    console.log()
-    console.log('Licenses:')
-    console.log(licenses.join(','))
-    console.log()
-
-    throw new Error(`Dependencies and 3rd party licenses mismatch`)
+  if (!checkLicenses(deps, 'require') || !checkLicenses(devDeps, 'dev')) {
+    process.exit(1)
   }
 })
+
+function checkLicenses (typeDeps, type) {
+  /* eslint-disable no-console */
+
+  const missing = []
+  const extraneous = []
+
+  for (const dep of typeDeps) {
+    if (!licenses[type].has(dep)) {
+      missing.push(dep)
+    }
+  }
+
+  for (const dep of licenses[type]) {
+    if (!typeDeps.has(dep)) {
+      extraneous.push(dep)
+    }
+  }
+
+  if (missing.length) {
+    console.log(`Missing 3rd-party license for ${missing.join(', ')}.`)
+  }
+
+  if (extraneous.length) {
+    console.log(`Extraneous 3rd-party license for ${extraneous.join(', ')}.`)
+  }
+
+  return missing.length === 0 && extraneous.length === 0
+}
