03aef24e73d99565023c854593ad81433acde05e
Juan Fernandez <juan.fernandezdealba@datadoghq.com>
2021-07-01 17:18:13 +0200

    [ci-app] support jasmine

--- packages/datadog-plugin-jest/src/index.js
+++ packages/datadog-plugin-jest/src/index.js
@@ -218,6 +218,112 @@ function createHandleTestEvent (tracer, testEnvironmentMetadata, instrumenter) {
   }
 }
 
+function createIt (tracer, globalConfig, globalInput, testEnvironmentMetadata) {
+  return function itWithTrace (it) {
+    return function (description, specFunction, timeout) {
+      let oldSpecFunction = specFunction
+
+      if (specFunction.length) {
+        oldSpecFunction = promisify(oldSpecFunction)
+      }
+
+      const childOf = tracer.extract('text_map', {
+        'x-datadog-trace-id': id().toString(10),
+        'x-datadog-parent-id': '0000000000000000',
+        'x-datadog-sampled': 1
+      })
+
+      const testSuite = globalInput.jasmine.testPath.replace(`${globalConfig.rootDir}/`, '')
+
+      const commonSpanTags = {
+        [TEST_TYPE]: 'test',
+        [TEST_SUITE]: testSuite,
+        [SAMPLING_RULE_DECISION]: 1,
+        [SAMPLING_PRIORITY]: AUTO_KEEP,
+        ...testEnvironmentMetadata
+      }
+      const newSpecFunction = tracer.wrap(
+        'jest.test',
+        { type: 'test',
+          childOf,
+          tags: commonSpanTags
+        },
+        async (done) => {
+          const testSpan = tracer.scope().active()
+          const { currentTestName } = globalInput.expect.getState()
+          const resource = `${testSuite}.${currentTestName}`
+          testSpan.setTag(TEST_NAME, currentTestName)
+          testSpan.setTag(RESOURCE_NAME, resource)
+          let result
+          globalInput.jasmine.testSpanByTestName[currentTestName] = testSpan
+
+          try {
+            result = await oldSpecFunction()
+            // it may have been set already if the test timed out
+            const suppressedErrors = globalInput.expect.getState().suppressedErrors
+            if (suppressedErrors && suppressedErrors.length) {
+              testSpan.setTag('error', suppressedErrors[0])
+              testSpan.setTag(TEST_STATUS, 'fail')
+            }
+            if (!testSpan._spanContext._tags[TEST_STATUS]) {
+              testSpan.setTag(TEST_STATUS, 'pass')
+            }
+          } catch (error) {
+            testSpan.setTag(TEST_STATUS, 'fail')
+            testSpan.setTag('error', error)
+            done(error)
+            throw error
+          } finally {
+            finishAllTraceSpans(testSpan)
+          }
+          done(result)
+        }
+      )
+      return it(description, newSpecFunction, timeout)
+    }
+  }
+}
+
+function createJasmineAsyncInstall (tracer, instrumenter, testEnvironmentMetadata) {
+  return function jasmineAsyncInstallWithTrace (jasmineAsyncInstall) {
+    return function (globalConfig, globalInput) {
+      globalInput.jasmine.testSpanByTestName = {}
+      instrumenter.wrap(globalInput.jasmine.Spec.prototype, 'onException', function (onException) {
+        return function (err) {
+          let activeTestSpan = tracer.scope().active()
+          if (!activeTestSpan) {
+            activeTestSpan = globalInput.jasmine.testSpanByTestName[this.getFullName()]
+          }
+          if (!activeTestSpan) {
+            return onException.apply(this, arguments)
+          }
+          const {
+            [TEST_NAME]: testName,
+            [TEST_SUITE]: testSuite,
+            [TEST_STATUS]: testStatus
+          } = activeTestSpan._spanContext._tags
+
+          const isActiveSpanFailing = this.getFullName() === testName &&
+            this.result.testPath.endsWith(testSuite)
+
+          if (isActiveSpanFailing && !testStatus) {
+            activeTestSpan.setTag(TEST_STATUS, 'fail')
+            activeTestSpan.setTag('error', err)
+            // need to manually finish, as it will not be caught in `itWithTrace`
+            activeTestSpan.finish()
+          }
+
+          return onException.apply(this, arguments)
+        }
+      })
+      instrumenter.wrap(globalInput, 'it', createIt(tracer, globalConfig, globalInput, testEnvironmentMetadata))
+      instrumenter.wrap(globalInput, 'fit', createIt(tracer, globalConfig, globalInput, testEnvironmentMetadata))
+      // missing at least xit
+      return jasmineAsyncInstall(globalConfig, globalInput)
+    }
+  }
+}
+
 module.exports = [
   {
     name: 'jest-environment-node',
@@ -255,6 +361,16 @@ module.exports = [
     unpatch: function (JsdomEnvironment) {
       this.unwrap(JsdomEnvironment.prototype, 'teardown')
       JsdomEnvironment.prototype.handleTestEvent = JsdomEnvironment.prototype.handleTestEvent._dd_original
+    }
+  },
+  {
+    name: 'jest-jasmine2',
+    versions: ['>=24.8.0'],
+    file: 'build/jasmineAsyncInstall.js',
+    patch: function (jasmineAsyncInstallExport, tracer) {
+      const testEnvironmentMetadata = getTestEnvironmentMetadata('jest')
+
+      return createJasmineAsyncInstall(tracer, this, testEnvironmentMetadata)(jasmineAsyncInstallExport.default)
     }
   }
 ]
