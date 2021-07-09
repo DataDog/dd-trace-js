4f01b510d2830bcc671eac51a0d9ddb4c1842687
Bryan English <bryan.english@datadoghq.com>
2021-07-09 15:52:02 -0400

    review feedback

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -2,17 +2,14 @@
 
 /* eslint-disable no-console */
 
-const fs = require('fs')
 const path = require('path')
-const semver = require('semver')
+const requirePackageJson = require('../packages/dd-trace/src/require-package-json.js')
 
-const nodeVersion = process.versions.node
+const nodeMajor = Number(process.versions.node.split('.')[0])
 
-const range = JSON.parse(
-  fs.readFileSync(path.join(__dirname, '..', 'package.json'), 'utf8')
-).engines.node
+const min = Number(requirePackageJson(path.join(__dirname, '..')).engines.node.match(/\d+/)[0])
 
-if (!semver.satisfies(nodeVersion, range)) {
+if (nodeMajor < min) {
   process.exitCode = 1
   console.error('\n' + `
 You're using Node.js v${process.versions.node}, which is not supported by
