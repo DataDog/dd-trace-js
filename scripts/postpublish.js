0074ae3c38abd6245aca59811d8dd18a4c6a8e2d
rochdev <roch.devost@datadoghq.com>
2021-07-21 11:40:39 -0400

    update post-publish script to not publish docs on legacy branches

--- scripts/postpublish.js
+++ scripts/postpublish.js
@@ -5,7 +5,8 @@ const exec = require('./helpers/exec')
 const pkg = require('../package.json')
 
 const betaExpr = /\d+\.\d+\.\d+-.*/
+const legacyExpr = /0\.\d+\.\d+/
 
-if (!betaExpr.test(pkg.version)) {
+if (!betaExpr.test(pkg.version) && !legacyExpr.test(pkg.version)) {
   exec(`node scripts/publish_docs.js "v${pkg.version}"`)
 }
