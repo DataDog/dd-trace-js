620b8ff5dde738471e6529000dce67beaa25d09d
Roch Devost <roch.devost@datadoghq.com>
2021-07-21 10:55:17 -0400

    remove legacy tags from postpublish script (#1518)

--- scripts/postpublish.js
+++ scripts/postpublish.js
@@ -5,13 +5,7 @@ const exec = require('./helpers/exec')
 const pkg = require('../package.json')
 
 const betaExpr = /\d+\.\d+\.\d+-.*/
-const legacyExpr = /0\.\d+\.\d+/
 
 if (!betaExpr.test(pkg.version)) {
-  if (legacyExpr.test(pkg.version)) {
-    exec(`yarn tag add dd-trace@${pkg.version} latest-node8`)
-    exec(`yarn tag add dd-trace@${pkg.version} latest-node10`)
-  }
-
   exec(`node scripts/publish_docs.js "v${pkg.version}"`)
 }
