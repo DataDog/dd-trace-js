718b325a2e18e5a570e89270448698d67b7a79fc
chris.agocs <chris.agocs@datadoghq.com>
2021-07-28 16:23:15 -0400

    Add tests for invokeWithLegacyContext = false

--- packages/datadog-plugin-aws-sdk/test/lambda.spec.js
+++ packages/datadog-plugin-aws-sdk/test/lambda.spec.js
@@ -20,7 +20,7 @@ describe('Plugin', () => {
       let lambda
       let tracer
 
-      describe('without configuration', () => {
+      describe('with the old trace context propagation', () => {
         let ZipFile
 
         before(async () => {
@@ -107,6 +107,109 @@ describe('Plugin', () => {
             const span = traces[0][0]
             const clientContextSent = Buffer.from(lambdaReq.params.ClientContext, 'base64').toString('utf-8')
             const injectedTraceData = JSON.parse(clientContextSent).custom._datadog
+            const spanContext = tracer.extract('text_map', injectedTraceData)
+
+            expect(span.resource.startsWith('invoke')).to.equal(true)
+
+            const parentId = span.span_id.toString()
+            const traceId = span.trace_id.toString()
+            expect(spanContext.toTraceId()).to.equal(traceId)
+            expect(spanContext.toSpanId()).to.equal(parentId)
+          }).then(done, done)
+
+          const lambdaReq = lambda.invoke({
+            FunctionName: 'ironmaiden',
+            Payload: '{}'
+          }, e => e && done(e))
+        })
+      })
+      describe('with the new trace context propagation', () => {
+        let ZipFile
+
+        before(async () => {
+          const lambdaFunctionCode = 'def handle(event, context):\n  return event\n'
+
+          zip.file('handler.py', lambdaFunctionCode.toString())
+          ZipFile = await zip.generateAsync({ type: 'nodebuffer' })
+        })
+
+        before(done => {
+          AWS = require(`../../../versions/aws-sdk@${version}`).get()
+
+          const lambdaEndpoint = new AWS.Endpoint('http://localhost:4566')
+          lambda = new AWS.Lambda({ endpoint: lambdaEndpoint, region: 'us-east-1' })
+
+          lambda.createFunction({
+            FunctionName: 'ironmaiden',
+            Code: { ZipFile },
+            Handler: 'handler.handle',
+            Role: 'arn:aws:iam::123456:role/test',
+            Runtime: 'python3.7'
+          }, (err, res) => {
+            if (err) return done(err)
+
+            agent.load('aws-sdk', {
+              invokeWithLegacyContext: false
+            }).then(done, done)
+          })
+          tracer = require('../../dd-trace')
+        })
+
+        after(done => {
+          lambda.deleteFunction({ FunctionName: 'ironmaiden' }, err => {
+            agent.close().then(() => done(err), done)
+          })
+        })
+
+        it('should propagate the tracing context with existing ClientContext and `custom` key', (done) => {
+          agent.use(traces => {
+            const span = traces[0][0]
+            const clientContextSent = Buffer.from(lambdaReq.params.ClientContext, 'base64').toString('utf-8')
+            const injectedTraceData = JSON.parse(clientContextSent).custom
+            const spanContext = tracer.extract('text_map', injectedTraceData)
+
+            expect(span.resource.startsWith('invoke')).to.equal(true)
+
+            const parentId = span.span_id.toString()
+            const traceId = span.trace_id.toString()
+            expect(spanContext.toTraceId()).to.equal(traceId)
+            expect(spanContext.toSpanId()).to.equal(parentId)
+          }).then(done, done)
+
+          const lambdaReq = lambda.invoke({
+            FunctionName: 'ironmaiden',
+            Payload: '{}',
+            ClientContext: createClientContext({ custom: { megadeth: 'tornado of souls' } })
+          }, e => e && done(e))
+        })
+
+        it('should propagate the tracing context with existing ClientContext and no `custom` key', (done) => {
+          agent.use(traces => {
+            const span = traces[0][0]
+            const clientContextSent = Buffer.from(lambdaReq.params.ClientContext, 'base64').toString('utf-8')
+            const injectedTraceData = JSON.parse(clientContextSent).custom
+            const spanContext = tracer.extract('text_map', injectedTraceData)
+
+            expect(span.resource.startsWith('invoke')).to.equal(true)
+
+            const parentId = span.span_id.toString()
+            const traceId = span.trace_id.toString()
+            expect(spanContext.toTraceId()).to.equal(traceId)
+            expect(spanContext.toSpanId()).to.equal(parentId)
+          }).then(done, done)
+
+          const lambdaReq = lambda.invoke({
+            FunctionName: 'ironmaiden',
+            Payload: '{}',
+            ClientContext: createClientContext({ megadeth: 'tornado of souls' })
+          }, e => e && done(e))
+        })
+
+        it('should propagate the tracing context without an existing ClientContext', (done) => {
+          agent.use(traces => {
+            const span = traces[0][0]
+            const clientContextSent = Buffer.from(lambdaReq.params.ClientContext, 'base64').toString('utf-8')
+            const injectedTraceData = JSON.parse(clientContextSent).custom
             const spanContext = tracer.extract('text_map', injectedTraceData)
 
             expect(span.resource.startsWith('invoke')).to.equal(true)
