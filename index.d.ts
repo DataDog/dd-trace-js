58052b99d6a659ab0b399832feceb1101487dcc5
chris.agocs <chris.agocs@datadoghq.com>
2021-07-28 14:11:25 -0400

    Document the new parameter

--- index.d.ts
+++ index.d.ts
@@ -735,6 +735,13 @@ declare namespace plugins {
     };
 
     /**
+     * Whether to use the legacy trace context propagation when instrumenting
+     * a direct Lambda function invocation.
+     * @default false
+     */
+    invokeWithLegacyContext?: boolean;
+
+    /**
      * Configuration for individual services to enable/disable them. Message
      * queue services can also configure the producer and consumer individually
      * by passing an object with a `producer` and `consumer` properties. The
