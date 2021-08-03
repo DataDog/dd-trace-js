5124f0d44753a8141f8e1fec584991fbd11a6a17
Roch Devost <roch.devost@datadoghq.com>
2021-08-03 15:06:58 -0400

    fix next request error when rendering directly (#1538)

--- packages/datadog-plugin-next/src/index.js
+++ packages/datadog-plugin-next/src/index.js
@@ -1,5 +1,7 @@
 'use strict'
 
+// TODO: either instrument all or none of the render functions
+
 function createWrapHandleRequest (tracer, config) {
   return function wrapHandleRequest (handleRequest) {
     return function handleRequestWithTrace (req, res, pathname, query) {
@@ -84,6 +86,8 @@ function trace (tracer, config, req, res, handler) {
 }
 
 function addPage (req, page) {
+  if (!req._datadog_next) return
+
   req._datadog_next.span.addTags({
     'resource.name': `${req.method} ${page}`.trim(),
     'next.page': page
