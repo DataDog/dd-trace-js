dbb015ec66ae4880bd72dd7801cad5b5a6234b63
simon-id <simon.id@datadoghq.com>
2021-08-03 16:30:21 +0200

    add dc_block for later use

---
+++ packages/dd-trace/src/gateway/dc_block.js
@@ -0,0 +1,61 @@
+'use strict'
+
+const dc = require('./dc')
+const shimmer = require('shimmer')
+
+function getActiveChannelPrototype () {
+  const noop = () => {}
+
+  const channel = dc.channel('imstealingurprototype')
+
+  // We have to force the channel to switch to ActiveChannel prototype
+  channel.subscribe(noop)
+
+  const activeChannelPrototype = Object.getPrototypeOf(channel)
+
+  channel.unsubscribe(noop)
+
+  return activeChannelPrototype
+}
+
+function enable () {
+  shimmer.wrap(getActiveChannelPrototype(), 'publish', (originalPublish) => {
+    return function wrappedPublish (data) {
+      let firstError
+
+      for (let i = 0; i < this._subscribers.length; i++) {
+        try {
+          const onMessage = this._subscribers[i]
+          onMessage(data, this.name)
+        } catch (err) {
+          if (!firstError && err instanceof DCBlockingError) {
+            firstError = err
+          } else {
+            process.nextTick(() => {
+              throw err
+            })
+          }
+        }
+      }
+
+      if (firstError) throw firstError
+    }
+  })
+}
+
+function disable () {
+  shimmer.unwrap(getActiveChannelPrototype(), 'publish')
+}
+
+class DCBlockingError extends Error {
+  constructor (message) {
+    super(message)
+    this.name = this.constructor.name
+  }
+}
+
+module.exports = {
+  enable,
+  disable,
+  DCBlockingError
+}
