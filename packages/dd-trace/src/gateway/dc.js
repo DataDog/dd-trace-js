c68c857ecb566eb42015ebf09ace7ef205d2ab6c
simon-id <simon.id@datadoghq.com>
2021-08-03 16:38:59 +0200

    use dc polyfill from npm instead of local

--- packages/dd-trace/src/gateway/dc.js
+++ packages/dd-trace/src/gateway/dc.js
@@ -1,16 +1,3 @@
 'use strict'
 
-// TODO: context tracking ?
-// TODO: run the blocking instrumentation
-
-let dc
-
-try {
-  dc = require('diagnostics_channel')
-} finally {
-  if (!dc || typeof dc.channel !== 'function') {
-    dc = require('./dc_polyfill')
-  }
-}
-
-module.exports = dc
+module.exports = require('diagnostics_channel')
