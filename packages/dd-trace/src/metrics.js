347fdec19828e65c87e8fffbc26d9a4fa9696587
rochdev <roch.devost@datadoghq.com>
2021-08-06 12:19:52 -0400

    fix lint

--- packages/dd-trace/src/metrics.js
+++ packages/dd-trace/src/metrics.js
@@ -3,7 +3,6 @@
 // TODO: capture every second and flush every 10 seconds
 
 const v8 = require('v8')
-const path = require('path')
 const os = require('os')
 const Client = require('./dogstatsd')
 const log = require('./log')
