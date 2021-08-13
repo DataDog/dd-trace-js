99cd5744b0bc218ded98b46537db580713a8514b
Roch Devost <roch.devost@datadoghq.com>
2021-08-13 16:03:37 -0400

    move native metrics to a separate repository (#1559)

--- scripts/should_rebuild.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-const { CI, INIT_CWD, PWD } = process.env
-
-// skip for local development, CI, or very old package managers without INIT_CWD
-if (CI === 'true' || !INIT_CWD || INIT_CWD.includes(PWD)) {
-  process.exit(1)
-}
