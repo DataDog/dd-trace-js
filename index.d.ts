fecc827d6b115f6dfdd9c7026dda0b1ad4f59786
Pedro Nascimento <pedro.nascimento@bitcapital.com.br>
2021-09-01 20:31:58 -0300

    fix typo and move propagationBlocklist to correct interface

--- index.d.ts
+++ index.d.ts
@@ -635,11 +635,6 @@ declare namespace plugins {
      * @default true
      */
     middleware?: boolean;
-
-    /**
-     * List of urls to which propagation headers should not be injected
-     */
-    propagationBlockList?: string | RegExp | ((url: string) => boolean) | (string | RegExp | ((url: string) => boolean))[];
   }
 
   /** @hidden */
@@ -669,6 +664,11 @@ declare namespace plugins {
        */
       request?: (span?: opentracing.Span, req?: ClientRequest, res?: IncomingMessage) => any;
     };
+
+    /**
+     * List of urls to which propagation headers should not be injected
+     */
+    propagationBlocklist?: string | RegExp | ((url: string) => boolean) | (string | RegExp | ((url: string) => boolean))[];
   }
 
   /** @hidden */
