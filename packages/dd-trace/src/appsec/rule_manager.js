1025ce3c917b461dfc19ae93a2053c37c6bc7666
simon-id <simon.id@datadoghq.com>
2021-09-10 10:47:10 +0200

    rule manager

---
+++ packages/dd-trace/src/appsec/rule_manager.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const callbacks = require('./callbacks')
+
+const appliedCallbacks = new Map()
+
+function applyRules (rules) {
+  // for now there is only WAF
+  const callback = new callbacks.DDWAF(rules)
+
+  appliedCallbacks.add(rules, callback)
+}
+
+function clearAllRules () {
+  for (const callback of appliedCallbacks) {
+    callback.clear()
+  }
+}
+
+module.exports = {
+  applyRules,
+  clearAllRules
+}
