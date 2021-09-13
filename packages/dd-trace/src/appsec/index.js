4369a65f519274eed0d8152480bfbc8e23f745f4
simon-id <simon.id@datadoghq.com>
2021-09-13 11:45:46 +0200

    appsec entrypoint

--- packages/dd-trace/src/appsec/index.js
+++ packages/dd-trace/src/appsec/index.js
@@ -1,8 +1,46 @@
 'use strict'
 
-function enable () {
+const fs = require('fs')
+const log = require('../log')
+const RuleManager = require('./rule_manager')
+const { INCOMING_HTTP_REQUEST_START } = require('../gateway/channels')
+const { startContext } = require('../gateway/engine/index')
+const Addresses = require('./addresses')
+
+function enable (config) {
+  try {
+    let rules = fs.readFileSync('./recommended.json')
+    rules = JSON.parse(rules)
+
+    RuleManager.applyRules(rules)
+
+    INCOMING_HTTP_REQUEST_START.subscribe(incomingHttpTranslator)
+  } catch (err) {
+    log.error(`Unable to apply AppSec rules: ${err}`)
+  }
+}
+
+function incomingHttpTranslator (data) {
+  const store = startContext()
+
+  store.set('req', data.req)
+  store.set('res', data.res)
+
+  const context = store.get('context')
+
+  context.setValue(Addresses.HTTP_INCOMING_URL, data.req.url)
+
+  const headers = data.req.headers
+  delete headers.cookie
+  context.setValue(Addresses.HTTP_INCOMING_HEADERS, headers)
+}
+
+function disable () {
+  RuleManager.clearAllRules()
+  INCOMING_HTTP_REQUEST_START.unsubscribe(incomingHttpTranslator)
 }
 
 module.exports = {
-  enable
+  enable,
+  disable
 }
