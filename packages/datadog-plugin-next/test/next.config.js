70f6c4125a615a9393f953d28033dcd73b6a7b0c
Bryan English <bryan.english@datadoghq.com>
2021-09-14 16:44:07 -0400

    fix nextjs tests via deps:true

--- packages/datadog-plugin-next/test/next.config.js
+++ packages/datadog-plugin-next/test/next.config.js
@@ -1,31 +1,18 @@
-const path = require('path')
-
 module.exports = {
   eslint: {
     ignoreDuringBuilds: true
   },
 
-  webpack5: false,
-
-  future: {
-    // legacy option
-    webpack5: false
-  },
-
   webpack: (config) => {
-    const react = path.resolve(__dirname, '../../../versions/node_modules/react')
-    const reactDom = path.resolve(__dirname, '../../../versions/node_modules/react-dom')
-
-    config.externals = {
-      'react': `commonjs2 ${react}`,
-      'react-dom': `commonjs2 ${reactDom}`
+    // TODO figure out why we need to externalize react for next@<11
+    if (Number(require('next/package.json').version.split('.')[0]) < 11) {
+      const react = require.resolve('react')
+      const reactDom = require.resolve('react-dom')
+      config.externals = {
+        'react': `commonjs2 ${react}`,
+        'react-dom': `commonjs2 ${reactDom}`
+      }
     }
-
-    // TODO: drop support for Next <10.1, enable webpack5 above, and uncomment:
-    // config.module.rules.push({
-    //   test: /\/dd-trace\/src\/native\/.+$/,
-    //   type: 'asset/source'
-    // })
 
     return config
   }
