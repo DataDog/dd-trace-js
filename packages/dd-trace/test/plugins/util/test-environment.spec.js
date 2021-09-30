aca9e8367df1d874d8ac23391d15972be89f86b8
Juan Antonio Fernández de Alba <juan.fernandezdealba@datadoghq.com>
2021-09-30 17:26:48 +0200

    Update CI Visibility Spec (#1630)

---
+++ packages/dd-trace/test/plugins/util/test-environment.spec.js
@@ -0,0 +1,38 @@
+const fs = require('fs')
+const path = require('path')
+
+const proxyquire = require('proxyquire')
+const sanitizedExecStub = sinon.stub().returns('')
+
+const { getCIMetadata } = require('../../../src/plugins/util/ci')
+const { getGitMetadata } = proxyquire('../../../src/plugins/util/git', {
+  './exec': {
+    'sanitizedExec': sanitizedExecStub
+  }
+})
+const { getTestEnvironmentMetadata } = proxyquire('../../../src/plugins/util/test', {
+  './git': {
+    'getGitMetadata': getGitMetadata
+  }
+})
+
+describe('test environment data', () => {
+  it('getCIMetadata returns an empty object if the CI is not supported', () => {
+    process.env = {}
+    expect(getCIMetadata()).to.eql({})
+  })
+
+  const ciProviders = fs.readdirSync(path.join(__dirname, 'ci-env'))
+  ciProviders.forEach(ciProvider => {
+    const assertions = require(path.join(__dirname, 'ci-env', ciProvider))
+
+    assertions.forEach(([env, expectedSpanTags], index) => {
+      it(`reads env info for spec ${index} from ${ciProvider}`, () => {
+        process.env = env
+        const tags = getTestEnvironmentMetadata()
+
+        expect(tags).to.contain(expectedSpanTags)
+      })
+    })
+  })
+})
