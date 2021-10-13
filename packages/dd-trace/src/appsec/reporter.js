66c311bc067a950f3e8b02c732f8165ea3d60ebd
Vladimir de Turckheim <vdeturckheim@users.noreply.github.com>
2021-10-13 17:09:36 +0200

    remove use of scheduler and reporting system for appsec

--- packages/dd-trace/src/appsec/reporter.js
+++ packages/dd-trace/src/appsec/reporter.js
@@ -1,32 +1,6 @@
 'use strict'
-
-const os = require('os')
-const path = require('path')
-const uuid = require('crypto-randomuuid')
-const requirePackageJson = require('../require-package-json')
 const { getContext } = require('../gateway/engine')
 const Addresses = require('./addresses')
-const Scheduler = require('../exporters/agent/scheduler')
-const request = require('../exporters/agent/request')
-const log = require('../log')
-
-const FLUSH_INTERVAL = 2e3
-const MAX_EVENT_BACKLOG = 1e6
-
-const host = {
-  context_version: '0.1.0',
-  os_type: os.type(),
-  hostname: os.hostname()
-}
-
-const tracer = {
-  context_version: '0.1.0',
-  runtime_type: 'nodejs',
-  runtime_version: process.version,
-  lib_version: requirePackageJson(path.join(__dirname, '..', '..', '..', '..')).version
-}
-
-const events = new Set()
 
 function resolveHTTPAddresses () {
   const context = getContext()
@@ -78,31 +52,6 @@ function getHeadersToSend (headers) {
   return result
 }
 
-function getTracerData () {
-  const scope = global._ddtrace._tracer.scope()
-
-  const result = {
-    serviceName: scope._config.service,
-    serviceEnv: scope._config.env,
-    serviceVersion: scope._config.version,
-    tags: Object.entries(scope._config.tags).map(([k, v]) => `${k}:${v}`) // TODO: this can be optimized
-  }
-
-  const activeSpan = scope.active()
-
-  if (activeSpan) {
-    activeSpan.setTag('manual.keep')
-    activeSpan.setTag('appsec.event', true)
-
-    const context = activeSpan.context()
-
-    result.spanId = context.toSpanId()
-    result.traceId = context.toTraceId()
-  }
-
-  return result
-}
-
 function formatAttack ({
   ruleId,
   ruleName,
@@ -127,55 +76,6 @@ function formatAttack ({
   }
 }
 
-// TODO: lock
-function flush () {
-  if (!events.size) return
-  else if (events.size >= MAX_EVENT_BACKLOG) {
-    log.warn('Dropping AppSec events because the backlog is full')
-  }
-
-  const eventsArray = Array.from(events.values())
-
-  // if they fail to send, we drop the events
-  for (let i = 0; i < eventsArray.length; ++i) {
-    events.delete(eventsArray[i])
-  }
-
-  const options = {
-    path: '/appsec/proxy/api/v2/appsecevts',
-    method: 'POST',
-    headers: {
-      'X-Api-Version': 'v0.1.0',
-      'Content-Type': 'application/json'
-    },
-    data: JSON.stringify({
-      protocol_version: 1,
-      idempotency_key: uuid(),
-      events: eventsArray
-    })
-  }
-
-  const url = global._ddtrace._tracer._exporter._writer._url
-
-  if (url.protocol === 'unix:') {
-    options.socketPath = url.pathname
-  } else {
-    options.protocol = url.protocol
-    options.hostname = url.hostname
-    options.port = url.port
-  }
-
-  request(options, (err, res, status) => {
-    if (err) {
-      log.error(err)
-    }
-  })
-}
-
-const scheduler = new Scheduler(flush, FLUSH_INTERVAL)
-scheduler.start()
-
 module.exports = {
-  scheduler,
   formatAttack
 }
