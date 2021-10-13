b4098b9b29dc6186932814c59b06bf7f47338af9
rochdev <roch.devost@datadoghq.com>
2021-10-13 15:26:04 -0400

    remove scope configuration entirely

--- packages/datadog-core/src/storage/index.js
+++ packages/datadog-core/src/storage/index.js
@@ -2,18 +2,13 @@
 
 const semver = require('semver')
 
-const { DD_TRACE_SCOPE } = process.env
-
 // https://github.com/nodejs/node/pull/33801
 const hasJavaScriptAsyncHooks = semver.satisfies(process.versions.node, '>=14.5 || ^12.19.0')
+const isAsyncLocalStorageStable = semver.satisfies(process.versions.node, '>=16.4')
 
-if (DD_TRACE_SCOPE === 'noop') {
-  module.exports = require('./noop')
-} else if (DD_TRACE_SCOPE === 'sync') {
-  module.exports = require('./sync')
-} else if (DD_TRACE_SCOPE === 'async_local_storage') { // TODO: make this the default
+if (isAsyncLocalStorageStable) {
   module.exports = require('async_hooks').AsyncLocalStorage
-} else if (DD_TRACE_SCOPE === 'async_resource' || (!DD_TRACE_SCOPE && hasJavaScriptAsyncHooks)) {
+} else if (hasJavaScriptAsyncHooks) {
   module.exports = require('./async_resource')
 } else {
   module.exports = require('./async_hooks')
--- packages/datadog-core/test/storage/test.js
+++ packages/datadog-core/test/storage/test.js
@@ -3,7 +3,7 @@
 const { expect } = require('chai')
 const { AsyncResource, executionAsyncId } = require('async_hooks')
 
-module.exports = (factory, supportsAsync = true) => {
+module.exports = factory => {
   let storage
   let store
 
@@ -41,88 +41,84 @@ module.exports = (factory, supportsAsync = true) => {
       expect(storage.getStore()).to.be.undefined
     })
 
-    if (supportsAsync) {
-      it('should persist through setTimeout', done => {
-        storage.run(store, () => {
-          setTimeout(() => {
-            expect(storage.getStore()).to.equal(store)
-            done()
-          }, 0)
-        })
+    it('should persist through setTimeout', done => {
+      storage.run(store, () => {
+        setTimeout(() => {
+          expect(storage.getStore()).to.equal(store)
+          done()
+        }, 0)
       })
+    })
 
-      it('should persist through setImmediate', done => {
-        storage.run(store, () => {
-          setImmediate(() => {
-            expect(storage.getStore()).to.equal(store)
-            done()
-          }, 0)
-        })
+    it('should persist through setImmediate', done => {
+      storage.run(store, () => {
+        setImmediate(() => {
+          expect(storage.getStore()).to.equal(store)
+          done()
+        }, 0)
       })
+    })
 
-      it('should persist through setInterval', done => {
-        storage.run(store, () => {
-          let shouldReturn = false
+    it('should persist through setInterval', done => {
+      storage.run(store, () => {
+        let shouldReturn = false
 
-          const timer = setInterval(() => {
-            expect(storage.getStore()).to.equal(store)
-
-            if (shouldReturn) {
-              clearInterval(timer)
-              return done()
-            }
-
-            shouldReturn = true
-          }, 0)
-        })
-      })
-
-      if (global.process && global.process.nextTick) {
-        it('should persist through process.nextTick', done => {
-          storage.run(store, () => {
-            process.nextTick(() => {
-              expect(storage.getStore()).to.equal(store)
-              done()
-            }, 0)
-          })
-        })
-      }
-
-      it('should persist through promises', () => {
-        const promise = Promise.resolve()
-
-        return storage.run(store, () => {
-          return promise.then(() => {
-            expect(storage.getStore()).to.equal(store)
-          })
-        })
-      })
-
-      it('should handle concurrency', done => {
-        storage.run(store, () => {
-          setImmediate(() => {
-            expect(storage.getStore()).to.equal(store)
-            done()
-          })
-        })
-
-        storage.run(store, () => {})
-      })
-
-      it('should not break propagation for nested resources', done => {
-        storage.run(store, () => {
-          const asyncResource = new AsyncResource(
-            'TEST', { triggerAsyncId: executionAsyncId(), requireManualDestroy: false }
-          )
-
-          asyncResource.runInAsyncScope(() => {})
-
+        const timer = setInterval(() => {
           expect(storage.getStore()).to.equal(store)
 
+          if (shouldReturn) {
+            clearInterval(timer)
+            return done()
+          }
+
+          shouldReturn = true
+        }, 0)
+      })
+    })
+
+    it('should persist through process.nextTick', done => {
+      storage.run(store, () => {
+        process.nextTick(() => {
+          expect(storage.getStore()).to.equal(store)
+          done()
+        }, 0)
+      })
+    })
+
+    it('should persist through promises', () => {
+      const promise = Promise.resolve()
+
+      return storage.run(store, () => {
+        return promise.then(() => {
+          expect(storage.getStore()).to.equal(store)
+        })
+      })
+    })
+
+    it('should handle concurrency', done => {
+      storage.run(store, () => {
+        setImmediate(() => {
+          expect(storage.getStore()).to.equal(store)
           done()
         })
       })
-    }
+
+      storage.run(store, () => {})
+    })
+
+    it('should not break propagation for nested resources', done => {
+      storage.run(store, () => {
+        const asyncResource = new AsyncResource(
+          'TEST', { triggerAsyncId: executionAsyncId(), requireManualDestroy: false }
+        )
+
+        asyncResource.runInAsyncScope(() => {})
+
+        expect(storage.getStore()).to.equal(store)
+
+        done()
+      })
+    })
   })
 
   describe('enterWith()', () => {
