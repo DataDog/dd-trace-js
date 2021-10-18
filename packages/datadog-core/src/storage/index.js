6cb719f6fa0d871d794073baf7bcf9ba5182e2e4
rochdev <roch.devost@datadoghq.com>
2021-10-18 09:40:31 -0400

    remove AsyncLocalStorage based storage until it's feature complete

--- packages/datadog-core/src/storage/index.js
+++ packages/datadog-core/src/storage/index.js
@@ -1,14 +1,13 @@
 'use strict'
 
+// TODO: default to AsyncLocalStorage when it supports triggerAsyncResource
+
 const semver = require('semver')
 
 // https://github.com/nodejs/node/pull/33801
 const hasJavaScriptAsyncHooks = semver.satisfies(process.versions.node, '>=14.5 || ^12.19.0')
-const isAsyncLocalStorageStable = semver.satisfies(process.versions.node, '>=16.4')
 
-if (isAsyncLocalStorageStable) {
-  module.exports = require('async_hooks').AsyncLocalStorage
-} else if (hasJavaScriptAsyncHooks) {
+if (hasJavaScriptAsyncHooks) {
   module.exports = require('./async_resource')
 } else {
   module.exports = require('./async_hooks')
