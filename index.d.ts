a82bbea0e31cb40364e4020a58019363a5b7ecc2
Deon Groenewald <deon.groenewald@lever.co>
2021-11-09 15:34:33 -0500

    http plugin client and server properties should accept a boolean

--- index.d.ts
+++ index.d.ts
@@ -972,12 +972,12 @@ declare namespace plugins {
     /**
      * Configuration for HTTP clients.
      */
-    client?: HttpClient | false,
+    client?: HttpClient | boolean,
 
     /**
      * Configuration for HTTP servers.
      */
-    server?: HttpServer | false
+    server?: HttpServer | boolean
 
     /**
      * Hooks to run before spans are finished.
@@ -1006,12 +1006,12 @@ declare namespace plugins {
     /**
      * Configuration for HTTP clients.
      */
-    client?: Http2Client | false,
+    client?: Http2Client | boolean,
 
     /**
      * Configuration for HTTP servers.
      */
-    server?: Http2Server | false
+    server?: Http2Server | boolean
   }
 
   /**
