22cdf14d7941f222ae56ea703e9e741d21fd7e3f
simon-id <simon.id@datadoghq.com>
2021-11-16 17:06:13 +0100

    add http end translator

--- packages/dd-trace/src/appsec/index.js
+++ packages/dd-trace/src/appsec/index.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 const path = require('path')
 const log = require('../log')
 const RuleManager = require('./rule_manager')
-const { INCOMING_HTTP_REQUEST_START } = require('../gateway/channels')
+const { INCOMING_HTTP_REQUEST_START, INCOMING_HTTP_REQUEST_END } = require('../gateway/channels')
 const Gateway = require('../gateway/engine/index')
 const Addresses = require('./addresses')
 const Reporter = require('./reporter')
@@ -18,7 +18,8 @@ function enable (config) {
 
     RuleManager.applyRules(rules)
 
-    INCOMING_HTTP_REQUEST_START.subscribe(incomingHttpTranslator)
+    INCOMING_HTTP_REQUEST_START.subscribe(incomingHttpStartTranslator)
+    INCOMING_HTTP_REQUEST_END.subscribe(incomingHttpEndTranslator)
 
     config.tags['_dd.appsec.enabled'] = 1
     config.tags['_dd.runtime_family'] = 'nodejs'
@@ -29,6 +30,8 @@ function enable (config) {
     Gateway.manager.addresses.add(Addresses.HTTP_INCOMING_METHOD)
     Gateway.manager.addresses.add(Addresses.HTTP_INCOMING_REMOTE_IP)
     Gateway.manager.addresses.add(Addresses.HTTP_INCOMING_REMOTE_PORT)
+    Gateway.manager.addresses.add(Addresses.HTTP_INCOMING_RESPONSE_CODE)
+    Gateway.manager.addresses.add(Addresses.HTTP_INCOMING_RESPONSE_HEADERS)
 
     Reporter.scheduler.start()
   } catch (err) {
@@ -36,7 +39,7 @@ function enable (config) {
   }
 }
 
-function incomingHttpTranslator (data) {
+function incomingHttpStartTranslator (data) {
   const store = Gateway.startContext()
 
   store.set('req', data.req)
@@ -56,6 +59,22 @@ function incomingHttpTranslator (data) {
   }, context)
 }
 
+function incomingHttpEndTranslator (data) {
+  const context = Gateway.getContext()
+
+  if (!context) return
+
+  const headers = Object.assign({}, data.res.getHeaders())
+  delete headers['set-cookie']
+
+  Gateway.propagate({
+    [Addresses.HTTP_INCOMING_RESPONSE_CODE]: data.res.statusCode,
+    [Addresses.HTTP_INCOMING_RESPONSE_HEADERS]: headers
+  }, context)
+
+  Reporter.finishAttacks(context)
+}
+
 function disable () {
   RuleManager.clearAllRules()
   if (INCOMING_HTTP_REQUEST_START.hasSubscribers) INCOMING_HTTP_REQUEST_START.unsubscribe(incomingHttpTranslator)
@@ -72,5 +91,6 @@ function disable () {
 module.exports = {
   enable,
   disable,
-  incomingHttpTranslator
+  incomingHttpStartTranslator,
+  incomingHttpEndTranslator
 }
