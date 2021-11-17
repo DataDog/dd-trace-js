539fb91db95129fa496b51a8b99bc30896a2592a
Juan Antonio Fernández de Alba <juan.fernandezdealba@datadoghq.com>
2021-11-17 08:53:48 +0100

    [ci-visibility] Update CI Visibility Spec (#1686)

--- packages/dd-trace/src/plugins/util/user-provided-git.js
+++ packages/dd-trace/src/plugins/util/user-provided-git.js
@@ -12,6 +12,8 @@ const {
   GIT_COMMIT_AUTHOR_NAME
 } = require('./tags')
 
+const { normalizeRef } = require('./ci')
+
 function removeEmptyValues (tags) {
   return Object.keys(tags).reduce((filteredTags, tag) => {
     if (!tags[tag]) {
@@ -39,11 +41,24 @@ function getUserProviderGitMetadata () {
     DD_GIT_COMMIT_AUTHOR_DATE
   } = process.env
 
+  let branch = normalizeRef(DD_GIT_BRANCH)
+  let tag = normalizeRef(DD_GIT_TAG)
+
+  if (DD_GIT_TAG) {
+    branch = undefined
+  }
+
+  // if DD_GIT_BRANCH is a tag, we associate its value to TAG instead of BRANCH
+  if ((DD_GIT_BRANCH || '').includes('origin/tags') || (DD_GIT_BRANCH || '').includes('refs/heads/tags')) {
+    branch = undefined
+    tag = normalizeRef(DD_GIT_BRANCH)
+  }
+
   return removeEmptyValues({
     [GIT_COMMIT_SHA]: DD_GIT_COMMIT_SHA,
-    [GIT_BRANCH]: DD_GIT_BRANCH,
+    [GIT_BRANCH]: branch,
     [GIT_REPOSITORY_URL]: DD_GIT_REPOSITORY_URL,
-    [GIT_TAG]: DD_GIT_TAG,
+    [GIT_TAG]: tag,
     [GIT_COMMIT_MESSAGE]: DD_GIT_COMMIT_MESSAGE,
     [GIT_COMMIT_COMMITTER_NAME]: DD_GIT_COMMIT_COMMITTER_NAME,
     [GIT_COMMIT_COMMITTER_DATE]: DD_GIT_COMMIT_COMMITTER_DATE,
