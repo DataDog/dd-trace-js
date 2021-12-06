09313b4323092765470acbf2ca9723de0737e272
rochdev <roch.devost@gmail.com>
2021-12-06 18:05:41 -0500

    lower number of iterations

--- benchmark/sirun/plugin-graphql/index.js
+++ benchmark/sirun/plugin-graphql/index.js
@@ -39,6 +39,6 @@ const source = `
 
 const variableValues = { who: 'world' }
 
-for (let i = 0; i < 25; i++) {
+for (let i = 0; i < 5; i++) {
   graphql.graphql({ schema, source, variableValues })
 }
