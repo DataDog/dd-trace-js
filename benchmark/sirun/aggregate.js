8bc9130f2faa8fd7371504df7437ec74806e52f5
rochdev <roch.devost@gmail.com>
2021-12-09 17:30:14 -0500

    fix aggregate script parsing

--- benchmark/sirun/aggregate.js
+++ benchmark/sirun/aggregate.js
@@ -4,28 +4,32 @@ const fs = require('fs')
 const path = require('path')
 const { summarizeResults } = require('./get-results')
 
+const jsons = []
 const ndjsons = fs.readdirSync(__dirname)
   .filter(n => n.endsWith('.ndjson'))
-
-fs.writeFileSync('all-sirun-output.ndjson', ndjsons)
 
 const versionResults = {}
 ndjsons.forEach(n => {
   const filename = path.join(__dirname, n.toString())
-  const contents = fs.readFileSync(filename, 'utf8').trim()
+  const lines = fs.readFileSync(filename, 'utf8').trim().split('\n')
 
-  try {
-    const results = JSON.parse(contents)
-    const nodeVersion = results.nodeVersion.split('.')[0]
-    if (!versionResults[nodeVersion]) {
-      versionResults[nodeVersion] = []
+  lines.forEach(json => {
+    try {
+      const results = JSON.parse(json)
+      const nodeVersion = results.nodeVersion.split('.')[0]
+      if (!versionResults[nodeVersion]) {
+        versionResults[nodeVersion] = []
+      }
+      versionResults[nodeVersion].push(results)
+      jsons.push(json)
+    } catch (e) {
+      console.error(`Could not parse ${filename}.`) // eslint-disable-line no-console
+      throw e
     }
-    versionResults[nodeVersion].push(results)
-  } catch (e) {
-    console.error(`Could not parse ${filename}.`) // eslint-disable-line no-console
-    throw e
-  }
+  })
 })
+
+fs.writeFileSync('all-sirun-output.ndjson', jsons.join('\n'))
 
 const buildData = {
   byVersion: true
