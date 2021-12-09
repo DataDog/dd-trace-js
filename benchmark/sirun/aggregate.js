53cd6402d4ed0560d4fad8c5fc7f2b11f1f836b2
rochdev <roch.devost@gmail.com>
2021-12-09 17:15:59 -0500

    more verbose parsing errors

--- benchmark/sirun/aggregate.js
+++ benchmark/sirun/aggregate.js
@@ -5,23 +5,26 @@ const path = require('path')
 const { summarizeResults } = require('./get-results')
 
 const ndjsons = fs.readdirSync(__dirname)
-  .map(n =>
-    n.endsWith('.ndjson')
-      ? fs.readFileSync(path.join(__dirname, n.toString()), 'utf8').trim()
-      : ''
-  )
-  .join('\n')
+  .filter(n => n.endsWith('.ndjson'))
 
 fs.writeFileSync('all-sirun-output.ndjson', ndjsons)
 
 const versionResults = {}
-ndjsons.trim().split('\n').forEach(x => {
-  const results = JSON.parse(x)
-  const nodeVersion = results.nodeVersion.split('.')[0]
-  if (!versionResults[nodeVersion]) {
-    versionResults[nodeVersion] = []
+ndjsons.forEach(n => {
+  const filename = path.join(__dirname, n.toString())
+  const contents = fs.readFileSync(filename, 'utf8').trim()
+
+  try {
+    const results = JSON.parse(contents)
+    const nodeVersion = results.nodeVersion.split('.')[0]
+    if (!versionResults[nodeVersion]) {
+      versionResults[nodeVersion] = []
+    }
+    versionResults[nodeVersion].push(results)
+  } catch (e) {
+    console.error(`Could not parse ${filename}.`) // eslint-disable-line no-console
+    throw e
   }
-  versionResults[nodeVersion].push(results)
 })
 
 const buildData = {
