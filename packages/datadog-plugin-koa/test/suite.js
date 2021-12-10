50bf20b507d86dc4e33be6d725878c96cca0b333
rochdev <roch.devost@gmail.com>
2021-12-09 19:40:26 -0500

    fix koa upstream tests hanging

--- packages/datadog-plugin-koa/test/suite.js
+++ packages/datadog-plugin-koa/test/suite.js
@@ -1,7 +1,12 @@
 'use strict'
 const suiteTest = require('../../dd-trace/test/plugins/suite')
 
-suiteTest('koa', 'koajs/koa', 'latest')
+suiteTest({
+  modName: 'koa',
+  repoUrl: 'koajs/koa',
+  commitish: 'latest',
+  parallel: false
+})
 
 // TODO enable this
 // suiteTest('@koa/router', 'koajs/router', 'latest')
