a317f17495e69f1f9eb37519a2749b17fec74216
Juan Fernandez <juan.fernandezdealba@datadoghq.com>
2021-12-13 16:35:00 +0100

    improve readibility

--- benchmark/e2e-ci/benchmark-run.js
+++ benchmark/e2e-ci/benchmark-run.js
@@ -1,28 +1,37 @@
+'use strict'
+
+/* eslint-disable no-console */
+
 const https = require('https')
+
+const API_REPOSITORY_URL = 'https://api.github.com/repos/DataDog/test-environment'
+const DISPATCH_WORKFLOW_URL = `${API_REPOSITORY_URL}/actions/workflows/dd-trace-js-tests.yml/dispatches`
+const GET_WORKFLOWS_URL = `${API_REPOSITORY_URL}/actions/runs`
+
+const getCommonHeaders = () => {
+  return {
+    'Content-Type': 'application/json',
+    'authorization': `Bearer ${process.env.GITHUB_PERSONAL_ACCESS_TOKEN}`,
+    'Accept': 'application/vnd.github.v3+json',
+    'user-agent': 'dd-trace benchmark tests'
+  }
+}
 
 const triggerWorkflow = () => {
   return new Promise((resolve, reject) => {
-    let response = ''
     const body = JSON.stringify({
       ref: 'main'
     })
-    const request = https.request('https://api.github.com/repos/DataDog/test-environment/actions/workflows/dd-trace-js-tests.yml/dispatches', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Content-Length': body.length,
-        'authorization': `Bearer ${process.env.GITHUB_PERSONAL_ACCESS_TOKEN}`,
-        'Accept': 'application/vnd.github.v3+json',
-        'user-agent': 'dd-trace benchmark tests'
-      }
-    }, (res) => {
-      res.on('data', (chunk) => {
-        response += chunk
+    const request = https.request(
+      DISPATCH_WORKFLOW_URL,
+      {
+        method: 'POST',
+        headers: getCommonHeaders()
+      }, (res) => {
+        res.on('end', () => {
+          resolve()
+        })
       })
-      res.on('end', () => {
-        resolve(JSON.parse(response))
-      })
-    })
     request.on('error', (error) => {
       reject(error)
     })
@@ -33,23 +42,18 @@ const triggerWorkflow = () => {
 
 const getWorkflowRunsInProgress = () => {
   return new Promise((resolve, reject) => {
-    let body = ''
+    let response = ''
     const request = https.request(
-      'https://api.github.com/repos/DataDog/test-environment/actions/runs?event=workflow_dispatch&status=in_progress OR waiting OR requested',
+      `${GET_WORKFLOWS_URL}?event=workflow_dispatch&status=in_progress OR waiting OR requested`,
       {
-        headers: {
-          'Content-Type': 'application/json',
-          'authorization': `Bearer ${process.env.GITHUB_PERSONAL_ACCESS_TOKEN}`,
-          'Accept': 'application/vnd.github.v3+json',
-          'user-agent': 'dd-trace benchmark tests'
-        }
+        headers: getCommonHeaders()
       },
       (res) => {
         res.on('data', (chunk) => {
-          body += chunk
+          response += chunk
         })
         res.on('end', () => {
-          resolve(JSON.parse(body))
+          resolve(JSON.parse(response))
         })
       })
     request.on('error', err => {
@@ -63,18 +67,13 @@ const getCurrentWorkflowJobs = (runId) => {
   let body = ''
   return new Promise((resolve, reject) => {
     if (!runId) {
-      reject(new Error('no job id'))
+      reject(new Error('No job run id specified'))
       return
     }
     const request = https.request(
-      `https://api.github.com/repos/DataDog/test-environment/actions/runs/${runId}/jobs`,
+      `${GET_WORKFLOWS_URL}/${runId}/jobs`,
       {
-        headers: {
-          'Content-Type': 'application/json',
-          'authorization': `Bearer ${process.env.GITHUB_PERSONAL_ACCESS_TOKEN}`,
-          'Accept': 'application/vnd.github.v3+json',
-          'user-agent': 'dd-trace benchmark tests'
-        }
+        headers: getCommonHeaders()
       },
       (res) => {
         res.on('data', (chunk) => {
@@ -100,14 +99,15 @@ const wait = (timeToWaitMs) => {
 }
 
 async function main () {
-  // trigger JS GHA
-  triggerWorkflow()
+  // Trigger JS GHA
+  await triggerWorkflow()
 
-  // give some time for GH to process the request
+  // Give some time for GH to process the request
   await wait(1000)
 
-  const res = await getWorkflowRunsInProgress()
-  const { workflow_runs: workflows } = res
+  // Get the run ID from the workflow we just triggered
+  const workflowsInProgress = await getWorkflowRunsInProgress()
+  const { workflow_runs: workflows } = workflowsInProgress
   const [{ id: runId } = {}] = workflows
 
   // Poll every 10 seconds until we have a finished status
@@ -119,7 +119,7 @@ async function main () {
         resolve()
         clearInterval(intervalId)
       } else if (status === 'completed' && conclusion !== 'success') {
-        reject(new Error('failed performance overhead test'))
+        reject(new Error('Performance overhead test failed'))
       }
     }, 10000)
   })
