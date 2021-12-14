6c56e9e7a21a4080bed59751afe285e46f37e31b
Juan Antonio Fernández de Alba <juan.fernandezdealba@datadoghq.com>
2021-12-14 10:31:57 +0100

    [ci-visibility] Improve Initialization of CI Visibility Library (#1724)

--- ci/init.js
+++ ci/init.js
@@ -1,10 +1,9 @@
-// TODO: provide a way to do this with the init function
-process.env.DD_TRACE_DISABLED_PLUGINS = 'fs'
-
 const tracer = require('../packages/dd-trace')
 
 tracer.init({
   startupLogs: false
 })
+
+tracer.use('fs', false)
 
 module.exports = tracer
