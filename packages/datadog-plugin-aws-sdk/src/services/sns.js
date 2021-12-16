11a2c868d841bf4f644d7fdf091c79d5aae7f9ba
AJ Stuyvenberg <astuyve@gmail.com>
2021-12-16 12:22:13 -0500

    feat: support trace injection for sns

--- packages/datadog-plugin-aws-sdk/src/services/sns.js
+++ packages/datadog-plugin-aws-sdk/src/services/sns.js
@@ -16,6 +16,26 @@ class Sns {
     // TODO: should arn be sanitized or quantized in some way here,
     // for example if it contains a phone number?
   }
+
+  requestInject (span, request, tracer) {
+    const operation = request.operation
+    if (operation === 'publish' || operation === 'publishBatch') {
+      if (!request.params) {
+        request.params = {}
+      }
+      if (!request.params.MessageAttributes) {
+        request.params.MessageAttributes = {}
+      } else if (Object.keys(request.params.MessageAttributes).length >= 10) { // SNS quota
+        return
+      }
+      const ddInfo = {}
+      tracer.inject(span, 'text_map', ddInfo)
+      request.params.MessageAttributes._datadog = {
+        DataType: 'String',
+        StringValue: JSON.stringify(ddInfo)
+      }
+    }
+  }
 }
 
 module.exports = Sns
