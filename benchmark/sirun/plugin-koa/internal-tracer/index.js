a6b10f08a46e999ffc57f28110723a8355106ce6
rochdev <roch.devost@gmail.com>
2021-12-27 10:27:29 -0500

    wip

--- benchmark/sirun/plugin-koa/internal-tracer/index.js
+++ benchmark/sirun/plugin-koa/internal-tracer/index.js
@@ -1,5 +1,9 @@
 'use strict'
 
+const { tracer } = require('../../../../packages/datadog-tracer')
+
+setInterval(() => tracer.flush(), 2000).unref()
+
 require('./patch')
 require('./storage')
 require('./trace')
