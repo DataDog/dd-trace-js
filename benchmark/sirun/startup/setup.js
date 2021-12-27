b70bf3ffbe4ce371010cd60fd54926a0b6952640
rochdev <roch.devost@gmail.com>
2021-12-27 10:02:11 -0500

    wip

---
+++ benchmark/sirun/startup/setup.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const webpack = require('webpack')
+
+const config = {
+  entry: [`${__dirname}/../../../packages/datadog-tracer/index.js`],
+  target: 'node',
+  mode: 'production',
+  output: {
+    path: `${__dirname}/dist`,
+    filename: 'tracer.js',
+    libraryTarget: 'umd'
+  }
+}
+
+webpack(config, (err, stats) => {
+  if (err) {
+    throw err
+  }
+
+  console.log(stats.toString()) // eslint-disable-line no-console
+})
--- benchmark/sirun/startup/startup-test.js
+++ benchmark/sirun/startup/startup-test.js
@@ -4,6 +4,14 @@ if (Number(process.env.USE_TRACER)) {
   require('../../..').init()
 }
 
+if (Number(process.env.USE_INTERNAL_TRACER)) {
+  require('../../../packages/datadog-tracer')
+}
+
+if (Number(process.env.USE_INTERNAL_TRACER_BUNDLE)) {
+  require('./dist/tracer')
+}
+
 if (Number(process.env.EVERYTHING)) {
   const json = require('../../../package.json')
   for (const pkg in json.dependencies) {
