aefbac75bef555cad44942633002469e6d6461cb
rochdev <roch.devost@gmail.com>
2022-01-10 19:45:32 -0500

    remove params tag as it causes an infinite loop

--- packages/datadog-plugin-moleculer/src/util.js
+++ packages/datadog-plugin-moleculer/src/util.js
@@ -1,16 +1,16 @@
 'use strict'
 
+// TODO: add ctx.params when nested object properties are deprecated
+
 function moleculerTags (broker, ctx, config) {
   const service = ctx.service || {}
   const action = ctx.action || {}
-  const params = config.params && ctx.params
   const meta = config.meta && ctx.meta
 
   return {
     'moleculer.context.action': action.name,
     'moleculer.context.meta': meta,
     'moleculer.context.node_id': ctx.nodeID,
-    'moleculer.context.params': params,
     'moleculer.context.request_id': ctx.requestID,
     'moleculer.context.service': service.name,
     'moleculer.namespace': broker.namespace,
