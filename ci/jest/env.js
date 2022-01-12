DataDog/dd-trace-js

commit 83bedeb761bb8697dd3a7358975593bae12c3663

[ci-visibility] Set `_dd.origin` to spans outside of a test trace when using CI Visibility (#1734)

ci/jest/env.js
@@ -1,8 +1,12 @@
 const tracer = require('../../packages/dd-trace')
+const { ORIGIN_KEY } = require('../packages/dd-trace/src/constants')
 
 tracer.init({
   startupLogs: false,
-  flushInterval: 400000
+  flushInterval: 400000,
+  tags: {
+    [ORIGIN_KEY]: 'ciapp-test'
+  }
 })
 
 tracer.use('fs', false)

ci/init.js
@@ -1,7 +1,11 @@
 const tracer = require('../packages/dd-trace')
+const { ORIGIN_KEY } = require('../packages/dd-trace/src/constants')
 
 tracer.init({
-  startupLogs: false
+  startupLogs: false,
+  tags: {
+    [ORIGIN_KEY]: 'ciapp-test'
+  }
 })
 
 tracer.use('fs', false)

commit 3ac50b0b94a520197ab5ff39898024c203b391b0

Fix typo introduced in #1734 (#1749)

ci/jest/env.js
@@ -1,5 +1,5 @@
 const tracer = require('../../packages/dd-trace')
-const { ORIGIN_KEY } = require('../packages/dd-trace/src/constants')
+const { ORIGIN_KEY } = require('../../packages/dd-trace/src/constants')
 
 tracer.init({
   startupLogs: false,