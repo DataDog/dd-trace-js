72a2d7aa4d9e5e6a0d8771feb23bb00b15d7f33a
Ayan Khan <37887955+khanayan123@users.noreply.github.com>
2022-01-13 14:28:20 -0500

    Migrate mysql to new plugin system (#1751)
    Co-authored-by: Ayan Khan <ayan.khan@datadoghq.com>

--- packages/datadog-plugin-mysql/src/index.js
+++ packages/datadog-plugin-mysql/src/index.js
@@ -1,110 +1,58 @@
 'use strict'
 
-const Tags = require('opentracing').Tags
+const Plugin = require('../../dd-trace/src/plugins/plugin')
+const { storage } = require('../../datadog-core')
 const analyticsSampler = require('../../dd-trace/src/analytics_sampler')
 
-function createWrapQuery (tracer, config) {
-  return function wrapQuery (query) {
-    return function queryWithTrace (sql, values, cb) {
-      const scope = tracer.scope()
-      const childOf = scope.active()
-      const span = tracer.startSpan('mysql.query', {
+class MySQLPlugin extends Plugin {
+  static get name () {
+    return 'mysql'
+  }
+
+  constructor (...args) {
+    super(...args)
+
+    this.addSub('apm:mysql:query:start', ([sql, conf]) => {
+      const store = storage.getStore()
+      const childOf = store ? store.span : store
+      const span = this.tracer.startSpan('mysql.query', {
         childOf,
         tags: {
-          [Tags.SPAN_KIND]: Tags.SPAN_KIND_RPC_CLIENT,
-          'service.name': config.service || `${tracer._service}-mysql`,
+          'service.name': this.config.service || `${this.tracer._service}-mysql`,
           'span.type': 'sql',
           'span.kind': 'client',
           'db.type': 'mysql',
-          'db.user': this.config.user,
-          'out.host': this.config.host,
-          'out.port': this.config.port
+          'db.user': conf.user,
+          'out.host': conf.host,
+          'out.port': conf.port,
+          'resource.name': sql
         }
       })
 
-      if (this.config.database) {
-        span.setTag('db.name', this.config.database)
+      if (conf.database) {
+        span.setTag('db.name', conf.database)
       }
 
-      analyticsSampler.sample(span, config.measured)
+      analyticsSampler.sample(span, this.config.measured)
+      this.enter(span, store)
+    })
 
-      const sequence = scope.bind(query, span).apply(this, arguments)
+    this.addSub('apm:mysql:query:end', () => {
+      this.exit()
+    })
 
-      scope.bind(sequence)
-
-      span.setTag('resource.name', sequence.sql)
-
-      if (sequence._callback) {
-        sequence._callback = wrapCallback(tracer, span, childOf, sequence._callback)
-      } else {
-        sequence.on('end', () => {
-          span.finish()
-        })
+    this.addSub('apm:mysql:query:error', err => {
+      if (err) {
+        const span = storage.getStore().span
+        span.setTag('error', err)
       }
+    })
 
-      return sequence
-    }
+    this.addSub('apm:mysql:query:async-end', () => {
+      const span = storage.getStore().span
+      span.finish()
+    })
   }
 }
 
-function createWrapGetConnection (tracer, config) {
-  return function wrapGetConnection (getConnection) {
-    return function getConnectionWithTrace (cb) {
-      const scope = tracer.scope()
-
-      arguments[0] = scope.bind(cb)
-
-      return scope.bind(getConnection).apply(this, arguments)
-    }
-  }
-}
-
-function wrapCallback (tracer, span, parent, done) {
-  return tracer.scope().bind((...args) => {
-    const err = args[0]
-    if (err) {
-      span.addTags({
-        'error.type': err.name,
-        'error.msg': err.message,
-        'error.stack': err.stack
-      })
-    }
-
-    span.finish()
-
-    done(...args)
-  }, parent)
-}
-
-function patchConnection (Connection, tracer, config) {
-  this.wrap(Connection.prototype, 'query', createWrapQuery(tracer, config))
-}
-
-function unpatchConnection (Connection) {
-  this.unwrap(Connection.prototype, 'query')
-}
-
-function patchPool (Pool, tracer, config) {
-  this.wrap(Pool.prototype, 'getConnection', createWrapGetConnection(tracer, config))
-}
-
-function unpatchPool (Pool) {
-  this.unwrap(Pool.prototype, 'getConnection')
-}
-
-module.exports = [
-  {
-    name: 'mysql',
-    file: 'lib/Connection.js',
-    versions: ['>=2'],
-    patch: patchConnection,
-    unpatch: unpatchConnection
-  },
-  {
-    name: 'mysql',
-    file: 'lib/Pool.js',
-    versions: ['>=2'],
-    patch: patchPool,
-    unpatch: unpatchPool
-  }
-]
+module.exports = MySQLPlugin
