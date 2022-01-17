aca1ed06ad4ed5b1f208afe39ab8903bfb73dbf7
Juan Fernandez <juan.fernandezdealba@datadoghq.com>
2022-01-17 12:30:03 +0100

    update tests

--- packages/datadog-plugin-cypress/test/index.spec.js
+++ packages/datadog-plugin-cypress/test/index.spec.js
@@ -62,7 +62,7 @@ describe('Plugin', () => {
               [TEST_NAME]: 'can visit a page renders a hello world',
               [TEST_STATUS]: 'pass',
               [TEST_SUITE]: 'cypress/integration/integration-test.js',
-              [TEST_TYPE]: 'test',
+              [TEST_TYPE]: 'browser',
               [ORIGIN_KEY]: CI_APP_ORIGIN,
               [TEST_IS_RUM_ACTIVE]: 'true'
             })
@@ -82,7 +82,7 @@ describe('Plugin', () => {
               [TEST_NAME]: 'can visit a page will fail',
               [TEST_STATUS]: 'fail',
               [TEST_SUITE]: 'cypress/integration/integration-test.js',
-              [TEST_TYPE]: 'test',
+              [TEST_TYPE]: 'browser',
               [ORIGIN_KEY]: CI_APP_ORIGIN,
               [ERROR_TYPE]: 'AssertionError',
               [TEST_IS_RUM_ACTIVE]: 'true'
