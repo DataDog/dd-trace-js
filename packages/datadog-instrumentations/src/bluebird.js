c0a34f6afaa61ffba2c804a668186ad79be34b52
Bryan English <bryan.english@datadoghq.com>
2022-01-19 19:43:09 -0500

    simplify bluebird library copies, removing old unneeded logic

--- packages/datadog-instrumentations/src/bluebird.js
+++ packages/datadog-instrumentations/src/bluebird.js
@@ -4,33 +4,15 @@ const { addHook } = require('./helpers/instrument')
 const { wrapThen } = require('./helpers/promise')
 const shimmer = require('../../datadog-shimmer')
 
-const DD_LIB_COPIES = '_datadog_library_copies'
-
 function createGetNewLibraryCopyWrap (originalLib) {
   return function wrapGetNewLibraryCopy (getNewLibraryCopy) {
     return function getNewLibraryCopyWithTrace () {
       const libraryCopy = getNewLibraryCopy.apply(this, arguments)
       shimmer.wrap(libraryCopy.prototype, '_then', wrapThen)
       shimmer.wrap(libraryCopy, 'getNewLibraryCopy', createGetNewLibraryCopyWrap(originalLib))
-      addToLibraryCopies(originalLib, libraryCopy)
       return libraryCopy
     }
   }
-}
-
-function addToLibraryCopies (originalLib, libraryCopy) {
-  let libraryCopies = originalLib[DD_LIB_COPIES]
-
-  if (!libraryCopies) {
-    libraryCopies = new Set()
-
-    Object.defineProperty(originalLib, DD_LIB_COPIES, {
-      writable: true,
-      configurable: true,
-      value: libraryCopies
-    })
-  }
-  libraryCopies.add(libraryCopy)
 }
 
 addHook({ name: 'bluebird', versions: ['>=2.0.2'] }, Promise => {
