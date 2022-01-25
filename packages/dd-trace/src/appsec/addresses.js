bd7996d8a820d62a7271989a95ae7d346a5467d6
simon-id <simon.id@datadoghq.com>
2022-01-25 14:13:29 +0100

    update addresses list

--- packages/dd-trace/src/appsec/addresses.js
+++ packages/dd-trace/src/appsec/addresses.js
@@ -1,11 +1,18 @@
 'use strict'
 
 module.exports = {
-  HTTP_INCOMING_URL: 'server.request.uri.raw',
+  HTTP_INCOMING_BODY: 'server.request.body',
+  HTTP_INCOMING_QUERY: 'server.request.query',
   HTTP_INCOMING_HEADERS: 'server.request.headers.no_cookies',
+  // TODO: 'server.request.trailers',
+  HTTP_INCOMING_URL: 'server.request.uri.raw',
   HTTP_INCOMING_METHOD: 'server.request.method',
-  HTTP_INCOMING_REMOTE_IP: 'server.request.client_ip',
-  HTTP_INCOMING_REMOTE_PORT: 'server.request.client_port',
+  HTTP_INCOMING_ROUTE: 'server.request.framework_endpoint',
+  HTTP_INCOMING_PARAMS: 'server.request.path_params',
+  HTTP_INCOMING_COOKIES: 'server.request.cookies',
   HTTP_INCOMING_RESPONSE_CODE: 'server.response.status',
-  HTTP_INCOMING_RESPONSE_HEADERS: 'server.response.headers.no_cookies'
+  HTTP_INCOMING_RESPONSE_HEADERS: 'server.response.headers.no_cookies',
+  // TODO: 'server.response.trailers',
+  HTTP_INCOMING_REMOTE_IP: 'server.request.client_ip',
+  HTTP_INCOMING_REMOTE_PORT: 'server.request.client_port'
 }
