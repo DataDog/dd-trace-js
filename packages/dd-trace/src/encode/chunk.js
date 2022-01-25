c1efe5e0aa9a1e697f25910c97236d145b0340df
Bradley Farias <bradley.meck@gmail.com>
2022-01-25 09:50:34 -0600

    revert: less diff

--- packages/dd-trace/src/encode/chunk.js
+++ packages/dd-trace/src/encode/chunk.js
@@ -21,7 +21,7 @@ class Chunk {
     if (length < 0x20) { // fixstr
       this.reserve(length + 1)
       this.length += 1
-      this.buffer[this.length] = length | 0xa0
+      this.buffer[offset] = length | 0xa0
     } else if (length < 0x100000000) { // str 32
       this.reserve(length + 5)
       this.length += 5
