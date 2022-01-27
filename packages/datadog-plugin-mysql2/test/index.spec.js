13dd5bb51d585ae357b1d829a4497beb6dfaefc8
Ayan Khan <ayan.khan@datadoghq.com>
2022-01-27 09:43:18 -0500

    move mysql2 instrumentation to its own file

--- packages/datadog-plugin-mysql2/test/index.spec.js
+++ packages/datadog-plugin-mysql2/test/index.spec.js
@@ -8,7 +8,7 @@ describe('Plugin', () => {
   let tracer
 
   describe('mysql2', () => {
-    withVersions('mysql', 'mysql2', version => {
+    withVersions('mysql2', 'mysql2', version => {
       beforeEach(() => {
         tracer = require('../../dd-trace')
       })
