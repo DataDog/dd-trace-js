f415e9720920ebe4775bdce51475038d964d5079
simon-id <simon.id@datadoghq.com>
2022-02-07 10:59:31 +0100

    add RUST_BACKTRACE=1 to sirun bench

--- benchmark/sirun/run-all-variants.js
+++ benchmark/sirun/run-all-variants.js
@@ -21,7 +21,7 @@ function exec (...args) {
 
 const metaJson = require(path.join(process.cwd(), 'meta.json'))
 
-const env = Object.assign({}, process.env, { DD_TRACE_STARTUP_LOGS: 'false' })
+const env = Object.assign({ RUST_BACKTRACE: 1 }, process.env, { DD_TRACE_STARTUP_LOGS: 'false' })
 
 function streamAddVersion (input) {
   input.rl = readline.createInterface({ input })
