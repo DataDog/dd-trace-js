ef59a6ff9034c1dbbf4913b0f9ff323c085e2ede
Bradley Farias <bradley.meck@gmail.com>
2022-02-11 08:56:50 -0600

    generate a little less garbage when adding tags (#1690)
    * generate a little less garbage when adding tags
    
    * fixup: bad rebase
    
    * fixup: bad rebase
    
    * fixup: remove console
    
    * revert: leave uint32 writing as is, check upstream
    
    * revert: less diff
    
    * revert: less diff

--- packages/dd-trace/src/tagger.js
+++ packages/dd-trace/src/tagger.js
@@ -5,32 +5,25 @@ const log = require('./log')
 function add (carrier, keyValuePairs) {
   if (!carrier || !keyValuePairs) return
 
-  if (typeof keyValuePairs === 'string') {
-    return add(
-      carrier,
-      keyValuePairs
-        .split(',')
-        .filter(tag => tag.indexOf(':') !== -1)
-        .reduce((prev, next) => {
-          const tag = next.split(':')
-          const key = tag[0].trim()
-          const value = tag.slice(1).join(':').trim()
-
-          prev[key] = value
-
-          return prev
-        }, {})
-    )
-  }
-
   if (Array.isArray(keyValuePairs)) {
     return keyValuePairs.forEach(tags => add(carrier, tags))
   }
 
   try {
-    Object.keys(keyValuePairs).forEach(key => {
-      carrier[key] = keyValuePairs[key]
-    })
+    if (typeof keyValuePairs === 'string') {
+      const segments = keyValuePairs.split(',')
+      for (const segment of segments) {
+        const separatorIndex = segment.indexOf(':')
+        if (separatorIndex === -1) continue
+
+        const key = segment.slice(0, separatorIndex)
+        const value = segment.slice(separatorIndex + 1)
+
+        carrier[key.trim()] = value.trim()
+      }
+    } else {
+      Object.assign(carrier, keyValuePairs)
+    }
   } catch (e) {
     log.error(e)
   }
