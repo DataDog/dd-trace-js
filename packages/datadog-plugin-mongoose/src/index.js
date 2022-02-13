850fd7c01af92cea4c7a46d5612c62e780ab8224
Bryan English <bryan.english@datadoghq.com>
2022-02-13 14:28:32 -0500

    simplify mongoose plugin

--- packages/datadog-plugin-mongoose/src/index.js
+++ packages/datadog-plugin-mongoose/src/index.js
@@ -1,29 +1,17 @@
 'use strict'
 
 const { wrapThen } = require('../../datadog-instrumentations/src/helpers/promise')
+const { AsyncResource } = require('../../datadog-instrumentations/src/helpers/instrument')
 
-function createWrapCollectionAddQueue (tracer, config) {
-  return function wrapAddQueue (addQueue) {
-    return function addQueueWithTrace (name) {
-      const scope = tracer.scope()
-
-      if (typeof name === 'function') {
-        arguments[0] = scope.bind(name)
-      } else if (typeof this[name] === 'function') {
-        arguments[0] = scope.bind((...args) => this[name](...args))
-      }
-
-      return addQueue.apply(this, arguments)
+function wrapAddQueue (addQueue) {
+  return function addQueueWithTrace (name) {
+    if (typeof name === 'function') {
+      arguments[0] = AsyncResource.bind(name)
+    } else if (typeof this[name] === 'function') {
+      arguments[0] = AsyncResource.bind((...args) => this[name](...args))
     }
-  }
-}
 
-function createWrapWrapCallback (tracer, config) {
-  return function wrapWrapCallback ($wrapCallback) {
-    return function $wrapCallbackWithTrace (cb) {
-      arguments[0] = tracer.scope().bind(cb)
-      return $wrapCallback.apply(this, arguments)
-    }
+    return addQueue.apply(this, arguments)
   }
 }
 
@@ -31,13 +19,12 @@ module.exports = [
   {
     name: 'mongoose',
     versions: ['>=4.6.4'],
-    patch (mongoose, tracer, config) {
+    patch (mongoose) {
       if (mongoose.Promise !== global.Promise) {
         this.wrap(mongoose.Promise.prototype, 'then', wrapThen)
       }
 
-      this.wrap(mongoose.Collection.prototype, 'addQueue', createWrapCollectionAddQueue(tracer, config))
-      this.wrap(mongoose.Model, '$wrapCallback', createWrapWrapCallback(tracer, config))
+      this.wrap(mongoose.Collection.prototype, 'addQueue', wrapAddQueue)
     },
     unpatch (mongoose) {
       if (mongoose.Promise !== global.Promise) {
@@ -45,7 +32,6 @@ module.exports = [
       }
 
       this.unwrap(mongoose.Collection.prototype, 'addQueue')
-      this.unwrap(mongoose.Model, '$wrapCallback')
     }
   }
 ]
