9f184c237b21517c9b2384fa21d0e597e4123dad
Ayan Khan <37887955+khanayan123@users.noreply.github.com>
2022-02-15 15:51:53 -0500

    migrate redis and ioredis to new plugin system (#1821)
    * convert redis to new plugin system, tests are not currently passing
    
    * passing without configuration redis legacy tests, save current state
    
    * add ioredis conversion
    
    * remove unnecessary comments from test file
    
    * override configure method
    
    * remove try catch from addCommand wrap
    
    * update skip comment to a jsdoc style comment

--- packages/datadog-plugin-redis/src/index.js
+++ packages/datadog-plugin-redis/src/index.js
@@ -1,104 +1,120 @@
 'use strict'
 
-// TODO: always use uppercase for command names
+const Plugin = require('../../dd-trace/src/plugins/plugin')
+const { storage } = require('../../datadog-core')
+const analyticsSampler = require('../../dd-trace/src/analytics_sampler')
+const urlFilter = require('../../dd-trace/src/plugins/util/urlfilter')
 
-const tx = require('../../dd-trace/src/plugins/util/redis')
-
-function createWrapAddCommand (tracer, config) {
-  return function wrapAddCommand (addCommand) {
-    return function addCommandWithTrace (command) {
-      const name = command[0]
-      const args = command.slice(1)
-
-      if (!config.filter(name)) return addCommand.apply(this, arguments)
-
-      const scope = tracer.scope()
-      const span = startSpan(tracer, config, this, name, args)
-
-      return tx.wrap(span, scope.bind(addCommand, span).apply(this, arguments))
-    }
+class RedisPlugin extends Plugin {
+  static get name () {
+    return 'redis'
   }
-}
 
-function createWrapInternalSendCommand (tracer, config) {
-  return function wrapInternalSendCommand (internalSendCommand) {
-    return function internalSendCommandWithTrace (options) {
-      if (!config.filter(options.command)) return internalSendCommand.apply(this, arguments)
+  constructor (...args) {
+    super(...args)
 
-      const scope = tracer.scope()
-      const span = startSpan(tracer, config, this, options.command, options.args)
+    this.addSub(`apm:${this.constructor.name}:command:start`, (
+      { db, command, args, connectionOptions, connectionName }
+    ) => {
+      if (!this.config.filter(command)) {
+        return this.skip()
+      }
+      const store = storage.getStore()
+      const childOf = store ? store.span : store
+      const span = this.tracer.startSpan('redis.command', {
+        childOf,
+        tags: {
+          'span.kind': 'client',
+          'resource.name': command,
+          'span.type': 'redis',
+          'db.type': 'redis',
+          'db.name': db || '0',
+          'redis.raw_command': formatCommand(command, args)
+        }
+      })
 
-      options.callback = scope.bind(tx.wrap(span, options.callback))
+      span.setTag('service.name', this.config.service || `${span.context()._tags['service.name']}-redis`)
 
-      return scope.bind(internalSendCommand, span).apply(this, arguments)
-    }
-  }
-}
+      analyticsSampler.sample(span, this.config.measured)
 
-function createWrapSendCommand (tracer, config) {
-  return function wrapSendCommand (sendCommand) {
-    return function sendCommandWithTrace (command, args, callback) {
-      if (!config.filter(command)) return sendCommand.apply(this, arguments)
-
-      const scope = tracer.scope()
-      const span = startSpan(tracer, config, this, command, args)
-
-      if (typeof callback === 'function') {
-        arguments[2] = scope.bind(tx.wrap(span, callback))
-      } else if (Array.isArray(args) && typeof args[args.length - 1] === 'function') {
-        args[args.length - 1] = scope.bind(tx.wrap(span, args[args.length - 1]))
-      } else {
-        arguments[2] = tx.wrap(span)
+      if (connectionOptions) {
+        span.addTags({
+          'out.host': connectionOptions.host,
+          'out.port': connectionOptions.port
+        })
       }
 
-      return scope.bind(sendCommand, span).apply(this, arguments)
-    }
+      if (this.config.splitByInstance && connectionName) {
+        const service = this.config.service
+          ? `${this.config.service}-${connectionName}`
+          : connectionName
+
+        span.setTag('service.name', service)
+      }
+
+      this.enter(span, store)
+    })
+
+    this.addSub(`apm:${this.constructor.name}:command:end`, () => {
+      this.exit()
+    })
+
+    this.addSub(`apm:${this.constructor.name}:command:error`, err => {
+      const span = storage.getStore().span
+      span.setTag('error', err)
+    })
+
+    this.addSub(`apm:${this.constructor.name}:command:async-end`, () => {
+      const span = storage.getStore().span
+      span.finish()
+    })
+  }
+
+  configure (config) {
+    super.configure(normalizeConfig(config))
   }
 }
 
-function startSpan (tracer, config, client, command, args) {
-  const db = client.selected_db
-  const connectionOptions = client.connection_options || client.connection_option || client.connectionOption || {}
-  const span = tx.instrument(tracer, config, db, command, args)
+function formatCommand (command, args) {
+  command = command.toUpperCase()
 
-  tx.setHost(span, connectionOptions.host, connectionOptions.port)
+  if (!args || command === 'AUTH') return command
 
-  return span
+  for (let i = 0, l = args.length; i < l; i++) {
+    if (typeof args[i] === 'function') continue
+
+    command = `${command} ${formatArg(args[i])}`
+
+    if (command.length > 1000) return trim(command, 1000)
+  }
+
+  return command
 }
 
-module.exports = [
-  {
-    name: '@node-redis/client',
-    versions: ['>=1'],
-    file: 'dist/lib/client/commands-queue.js',
-    patch (redis, tracer, config) {
-      config = tx.normalizeConfig(config)
-      this.wrap(redis.default.prototype, 'addCommand', createWrapAddCommand(tracer, config))
-    },
-    unpatch (redis) {
-      this.unwrap(redis.default.prototype, 'addCommand')
-    }
-  },
-  {
-    name: 'redis',
-    versions: ['>=2.6 <4'],
-    patch (redis, tracer, config) {
-      config = tx.normalizeConfig(config)
-      this.wrap(redis.RedisClient.prototype, 'internal_send_command', createWrapInternalSendCommand(tracer, config))
-    },
-    unpatch (redis) {
-      this.unwrap(redis.RedisClient.prototype, 'internal_send_command')
-    }
-  },
-  {
-    name: 'redis',
-    versions: ['>=0.12 <2.6'],
-    patch (redis, tracer, config) {
-      config = tx.normalizeConfig(config)
-      this.wrap(redis.RedisClient.prototype, 'send_command', createWrapSendCommand(tracer, config))
-    },
-    unpatch (redis) {
-      this.unwrap(redis.RedisClient.prototype, 'send_command')
-    }
+function formatArg (arg) {
+  switch (typeof arg) {
+    case 'string':
+    case 'number':
+      return trim(String(arg), 100)
+    default:
+      return '?'
   }
-]
+}
+
+function trim (str, maxlen) {
+  if (str.length > maxlen) {
+    str = str.substr(0, maxlen - 3) + '...'
+  }
+
+  return str
+}
+
+function normalizeConfig (config) {
+  const filter = urlFilter.getFilter(config)
+
+  return Object.assign({}, config, {
+    filter
+  })
+}
+
+module.exports = RedisPlugin
