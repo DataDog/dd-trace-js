1a9c89d59ebdbd73258c1d79cd88ad5ed5390327
Ayan Khan <ayan.khan@datadoghq.com>
2022-02-15 10:55:06 -0500

    plugin converted, all tests passing

--- packages/datadog-plugin-sharedb/src/index.js
+++ packages/datadog-plugin-sharedb/src/index.js
@@ -1,35 +1,55 @@
 'use strict'
 
-/**
- * @description The enum values in this map are not exposed from ShareDB, so the keys are hard-coded here.
- * The values were derived from: https://github.com/share/sharedb/blob/master/lib/client/connection.js#L196
- */
-const READABLE_ACTION_NAMES = {
-  hs: 'handshake',
-  qf: 'query-fetch',
-  qs: 'query-subscribe',
-  qu: 'query-unsubscribe',
-  bf: 'bulk-fetch',
-  bs: 'bulk-subscribe',
-  bu: 'bulk-unsubscribe',
-  f: 'fetch',
-  s: 'subscribe',
-  u: 'unsubscribe',
-  op: 'op',
-  nf: 'snapshot-fetch',
-  nt: 'snapshot-fetch-by-ts',
-  p: 'presence-broadcast',
-  pr: 'presence-request',
-  ps: 'presence-subscribe',
-  pu: 'presence-unsubscribe'
-}
+const Plugin = require('../../dd-trace/src/plugins/plugin')
+const { storage } = require('../../datadog-core')
+const analyticsSampler = require('../../dd-trace/src/analytics_sampler')
 
-function getReadableActionName (action) {
-  const actionName = READABLE_ACTION_NAMES[action]
-  if (actionName === undefined) {
-    return action
+class SharedbPlugin extends Plugin {
+  static get name () {
+    return 'sharedb'
   }
-  return actionName
+
+  constructor (...args) {
+    super(...args)
+
+    this.addSub(`apm:sharedb:_handleMessage:start`, ({ actionName, request }) => {
+      const store = storage.getStore()
+      const childOf = store ? store.span : store
+      const span = this.tracer.startSpan('sharedb.request', {
+        childOf,
+        tags: {
+          'service.name': this.config.service || this.tracer._service,
+          'span.kind': 'server',
+          'sharedb.action': actionName,
+          'resource.name': getReadableResourceName(actionName, request.c, request.q)
+        }
+      })
+
+      if (this.config.hooks && this.config.hooks.receive) {
+        this.config.hooks.receive(span, request)
+      }
+
+      analyticsSampler.sample(span, this.config.measured)
+      this.enter(span, store)
+    })
+
+    this.addSub(`apm:sharedb:_handleMessage:end`, () => {
+      this.exit()
+    })
+
+    this.addSub(`apm:sharedb:_handleMessage:error`, err => {
+      const span = storage.getStore().span
+      span.setTag('error', err)
+    })
+
+    this.addSub(`apm:sharedb:_handleMessage:async-end`, ({ request, res }) => {
+      const span = storage.getStore().span
+      if (this.config.hooks && this.config.hooks.reply) {
+        this.config.hooks.reply(span, request, res)
+      }
+      span.finish()
+    })
+  }
 }
 
 function getReadableResourceName (readableActionName, collection, query) {
@@ -60,62 +80,4 @@ function isObject (val) {
   return typeof val === 'object' && val !== null && !(val instanceof Array)
 }
 
-function wrapCallback (config, tracer, request, span, done) {
-  return tracer.scope().bind((err, res) => {
-    if (err) {
-      span.setTag('error', err)
-    }
-
-    if (config.hooks && config.hooks.reply) {
-      config.hooks.reply(span, request, res)
-    }
-
-    span.finish()
-
-    if (done) {
-      done(err, res)
-    }
-  })
-}
-
-function createAgentWrapHandle (tracer, config) {
-  return function wrapHandleMessage (origHandleMessageFn) { // called once
-    return function handleMessageWithTrace (request, callback) { // called for each trigger
-      const action = request.a
-
-      const actionName = getReadableActionName(action)
-
-      const scope = tracer.scope()
-      const childOf = scope.active()
-      const span = tracer.startSpan('sharedb.request', {
-        childOf,
-        tags: {
-          'service.name': config.service || tracer._service,
-          'span.kind': 'server',
-          'sharedb.action': actionName,
-          'resource.name': getReadableResourceName(actionName, request.c, request.q)
-        }
-      })
-
-      if (config.hooks && config.hooks.receive) {
-        config.hooks.receive(span, request)
-      }
-
-      arguments[1] = wrapCallback(config, tracer, request, span, callback)
-
-      return tracer.scope().bind(origHandleMessageFn, span).apply(this, arguments)
-    }
-  }
-}
-
-module.exports = {
-  name: 'sharedb',
-  versions: ['>=1'],
-  file: 'lib/agent.js',
-  patch (Agent, tracer, config) {
-    this.wrap(Agent.prototype, '_handleMessage', createAgentWrapHandle(tracer, config))
-  },
-  unpatch (Agent) {
-    this.unwrap(Agent.prototype, '_handleMessage')
-  }
-}
+module.exports = SharedbPlugin
