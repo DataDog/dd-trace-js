8432853f77a43becb828425ec9701212ac04432a
Ayan Khan <37887955+khanayan123@users.noreply.github.com>
2022-03-03 15:03:57 -0500

    migrate amqplib to new plugin system (#1866)
    * convert amqplib to new plugin system
    
    Co-authored-by: Ayan Khan <ayan.khan@datadoghq.com>

--- packages/datadog-plugin-amqplib/src/index.js
+++ packages/datadog-plugin-amqplib/src/index.js
@@ -1,74 +1,92 @@
 'use strict'
 
-const kebabCase = require('lodash.kebabcase')
+const Plugin = require('../../dd-trace/src/plugins/plugin')
+const { storage } = require('../../datadog-core')
 const analyticsSampler = require('../../dd-trace/src/analytics_sampler')
 const { TEXT_MAP } = require('../../../ext/formats')
 
-let methods = {}
-
-function createWrapSendImmediately (tracer, config) {
-  return function wrapSendImmediately (sendImmediately) {
-    return function sendImmediatelyWithTrace (method, fields) {
-      return sendWithTrace(sendImmediately, this, arguments, tracer, config, methods[method], fields)
-    }
+class AmqplibPlugin extends Plugin {
+  static get name () {
+    return 'amqplib'
   }
-}
 
-function createWrapSendMessage (tracer, config) {
-  return function wrapSendMessage (sendMessage) {
-    return function sendMessageWithTrace (fields) {
-      return sendWithTrace(sendMessage, this, arguments, tracer, config, 'basic.publish', fields)
-    }
-  }
-}
+  constructor (...args) {
+    super(...args)
 
-function createWrapDispatchMessage (tracer, config) {
-  return function wrapDispatchMessage (dispatchMessage) {
-    return function dispatchMessageWithTrace (fields, message) {
-      const childOf = extract(tracer, message)
-      const span = tracer.startSpan('amqp.command', { childOf })
+    this.addSub(`apm:amqplib:command:start`, ({ channel, method, fields, message }) => {
+      const store = storage.getStore()
+      let childOf
 
-      addTags(this, tracer, config, span, 'basic.deliver', fields)
+      if (method === 'basic.deliver') {
+        childOf = extract(this.tracer, message)
+      } else {
+        fields.headers = fields.headers || {}
+        childOf = store ? store.span : store
+      }
 
-      analyticsSampler.sample(span, config.measured, true)
-
-      tracer.scope().activate(span, () => {
-        try {
-          dispatchMessage.apply(this, arguments)
-        } catch (e) {
-          throw addError(span, e)
-        } finally {
-          span.finish()
+      const span = this.tracer.startSpan('amqp.command', {
+        childOf,
+        tags: {
+          'service.name': this.config.service || `${this.tracer._service}-amqp`,
+          'resource.name': getResourceName(method, fields)
         }
       })
-    }
-  }
-}
 
-function sendWithTrace (send, channel, args, tracer, config, method, fields) {
-  const childOf = tracer.scope().active()
-  const span = tracer.startSpan('amqp.command', { childOf })
+      if (channel && channel.connection && channel.connection.stream) {
+        span.addTags({
+          'out.host': channel.connection.stream._host,
+          'out.port': channel.connection.stream.remotePort
+        })
+      }
+      const fieldNames = [
+        'queue',
+        'exchange',
+        'routingKey',
+        'consumerTag',
+        'source',
+        'destination'
+      ]
 
-  fields.headers = fields.headers || {}
+      switch (method) {
+        case 'basic.publish':
+          span.setTag('span.kind', 'producer')
+          break
+        case 'basic.consume':
+        case 'basic.get':
+        case 'basic.deliver':
+          span.addTags({
+            'span.kind': 'consumer',
+            'span.type': 'worker'
+          })
+          break
+        default:
+          span.setTag('span.kind', 'client')
+      }
 
-  addTags(channel, tracer, config, span, method, fields)
-  tracer.inject(span, TEXT_MAP, fields.headers)
+      fieldNames.forEach(field => {
+        fields[field] !== undefined && span.setTag(`amqp.${field}`, fields[field])
+      })
+      if (method === 'basic.deliver') {
+        analyticsSampler.sample(span, this.config.measured, true)
+      } else {
+        this.tracer.inject(span, TEXT_MAP, fields.headers)
+        analyticsSampler.sample(span, this.config.measured)
+      }
 
-  analyticsSampler.sample(span, config.measured)
+      this.enter(span, store)
+    })
 
-  return tracer.scope().activate(span, () => {
-    try {
-      return send.apply(channel, args)
-    } catch (e) {
-      throw addError(span, e)
-    } finally {
+    this.addSub(`apm:amqplib:command:end`, () => {
+      const span = storage.getStore().span
       span.finish()
-    }
-  })
-}
+      this.exit()
+    })
 
-function isCamelCase (str) {
-  return /([A-Z][a-z0-9]+)+/.test(str)
+    this.addSub(`apm:amqplib:command:error`, err => {
+      const span = storage.getStore().span
+      span.setTag('error', err)
+    })
+  }
 }
 
 function getResourceName (method, fields = {}) {
@@ -82,93 +100,10 @@ function getResourceName (method, fields = {}) {
   ].filter(val => val).join(' ')
 }
 
-function addError (span, error) {
-  span.addTags({
-    'error.type': error.name,
-    'error.msg': error.message,
-    'error.stack': error.stack
-  })
-
-  return error
-}
-
-function addTags (channel, tracer, config, span, method, fields) {
-  const fieldNames = [
-    'queue',
-    'exchange',
-    'routingKey',
-    'consumerTag',
-    'source',
-    'destination'
-  ]
-
-  span.addTags({
-    'service.name': config.service || `${tracer._service}-amqp`,
-    'resource.name': getResourceName(method, fields)
-  })
-
-  if (channel && channel.connection && channel.connection.stream) {
-    span.addTags({
-      'out.host': channel.connection.stream._host,
-      'out.port': channel.connection.stream.remotePort
-    })
-  }
-
-  switch (method) {
-    case 'basic.publish':
-      span.setTag('span.kind', 'producer')
-      break
-    case 'basic.consume':
-    case 'basic.get':
-    case 'basic.deliver':
-      span.addTags({
-        'span.kind': 'consumer',
-        'span.type': 'worker'
-      })
-      break
-    default:
-      span.setTag('span.kind', 'client')
-  }
-
-  fieldNames.forEach(field => {
-    fields[field] !== undefined && span.setTag(`amqp.${field}`, fields[field])
-  })
-}
-
 function extract (tracer, message) {
   return message
     ? tracer.extract(TEXT_MAP, message.properties.headers)
     : null
 }
 
-module.exports = [
-  {
-    name: 'amqplib',
-    file: 'lib/defs.js',
-    versions: ['>=0.5'],
-    patch (defs, tracer, config) {
-      methods = Object.keys(defs)
-        .filter(key => Number.isInteger(defs[key]))
-        .filter(key => isCamelCase(key))
-        .reduce((acc, key) => Object.assign(acc, { [defs[key]]: kebabCase(key).replace('-', '.') }), {})
-    },
-    unpatch (defs) {
-      methods = {}
-    }
-  },
-  {
-    name: 'amqplib',
-    file: 'lib/channel.js',
-    versions: ['>=0.5'],
-    patch (channel, tracer, config) {
-      this.wrap(channel.Channel.prototype, 'sendImmediately', createWrapSendImmediately(tracer, config))
-      this.wrap(channel.Channel.prototype, 'sendMessage', createWrapSendMessage(tracer, config))
-      this.wrap(channel.BaseChannel.prototype, 'dispatchMessage', createWrapDispatchMessage(tracer, config))
-    },
-    unpatch (channel) {
-      this.unwrap(channel.Channel.prototype, 'sendImmediately')
-      this.unwrap(channel.Channel.prototype, 'sendMessage')
-      this.unwrap(channel.BaseChannel.prototype, 'dispatchMessage')
-    }
-  }
-]
+module.exports = AmqplibPlugin
