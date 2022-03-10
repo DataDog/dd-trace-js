caef09e7a48ea781ae3157257b4c2c49437cda37
rochdev <roch.devost@gmail.com>
2022-03-10 16:49:54 -0500

    fix redis tests

--- packages/datadog-plugin-redis/test/legacy.spec.js
+++ packages/datadog-plugin-redis/test/legacy.spec.js
@@ -50,7 +50,7 @@ describe('Plugin', () => {
               expect(traces[0][0].meta).to.have.property('span.kind', 'client')
               expect(traces[0][0].meta).to.have.property('out.host', '127.0.0.1')
               expect(traces[0][0].meta).to.have.property('redis.raw_command', 'GET foo')
-              expect(traces[0][0].metrics).to.have.property('out.port', 6379)
+              expect(traces[0][0].meta).to.have.property('out.port', '6379')
             })
             .then(done)
             .catch(done)
