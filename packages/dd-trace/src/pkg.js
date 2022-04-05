b83bbb04d3284f1cd84d8be9f676cb2de6b29fba
Ayan Khan <37887955+khanayan123@users.noreply.github.com>
2022-04-05 18:46:41 -0400

    stop tracer from breaking when path.parse return undefined (#1973)
    Co-authored-by: Ayan Khan <ayan.khan@datadoghq.com>

--- packages/dd-trace/src/pkg.js
+++ packages/dd-trace/src/pkg.js
@@ -11,7 +11,14 @@ function findRoot () {
 
 function findPkg () {
   const cwd = findRoot()
-  const filePath = findUp('package.json', cwd)
+  const directory = path.resolve(cwd)
+  const res = path.parse(directory)
+
+  if (!res) return {}
+
+  const { root } = res
+
+  const filePath = findUp('package.json', root, directory)
 
   try {
     return JSON.parse(fs.readFileSync(filePath, 'utf8'))
@@ -20,14 +27,12 @@ function findPkg () {
   }
 }
 
-function findUp (name, cwd) {
-  let directory = path.resolve(cwd)
-  const { root } = path.parse(directory)
-
+function findUp (name, root, directory) {
   while (true) {
     const current = path.resolve(directory, name)
 
     if (fs.existsSync(current)) return current
+
     if (directory === root) return
 
     directory = path.dirname(directory)
