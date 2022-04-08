284e85d9b1cffa53b0dd78fc483d88ad2d1cd37f
Juan Antonio Fernández de Alba <juan.fernandezdealba@datadoghq.com>
2022-04-08 16:34:09 +0200

    [ci-visibility] Rewrite Cypress Plugin (#1987)

--- packages/datadog-plugin-cypress/src/index.js
+++ packages/datadog-plugin-cypress/src/index.js
@@ -1,6 +1,11 @@
-module.exports = [
-  {
-    name: 'cypress',
-    versions: ['>=6.7.0']
+const Plugin = require('../../dd-trace/src/plugins/plugin')
+
+// Cypress plugin does not patch any library. This is just a placeholder to
+// follow the structure of the plugins
+class CypressPlugin extends Plugin {
+  static get name () {
+    return 'cypress'
   }
-]
+}
+
+module.exports = CypressPlugin
