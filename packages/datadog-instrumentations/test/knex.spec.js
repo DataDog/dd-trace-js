d990774e03cc5154997d7c308372c1c53ee78598
Bryan English <bryan.english@datadoghq.com>
2022-05-02 16:36:02 -0400

    migrate knex to new plugin system (#1844)

---
+++ packages/datadog-instrumentations/test/knex.spec.js
@@ -0,0 +1,37 @@
+'use strict'
+
+require('../src/knex')
+const { storage } = require('../../datadog-core')
+
+describe('Instrumentation', () => {
+  let knex
+  let client
+  const store = 'store'
+
+  describe('knex', () => {
+    withVersions('knex', 'knex', version => {
+      describe('without configuration', () => {
+        beforeEach(() => {
+          knex = require(`../../../versions/knex@${version}`).get()
+          client = knex({
+            client: 'pg',
+            connection: {
+              filename: ':memory:'
+            }
+          })
+        })
+        afterEach(() => client.destroy())
+
+        it('should propagate context', () =>
+          storage.run(store, () =>
+            client.raw('PRAGMA user_version')
+              .finally(() => {
+                expect(storage.getStore()).to.equal(store)
+              })
+              .catch(() => {})
+          )
+        )
+      })
+    })
+  })
+})
