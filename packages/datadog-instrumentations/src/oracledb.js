9c051c33c7791fe53e9ce7e9e7a8ec044f44a786
Bryan English <bryan.english@datadoghq.com>
2022-05-03 14:19:57 -0400

    migrate oracledb to new plugin system (#2024)

---
+++ packages/datadog-instrumentations/src/oracledb.js
@@ -0,0 +1,128 @@
+'use strict'
+
+const {
+  channel,
+  addHook,
+  AsyncResource
+} = require('./helpers/instrument')
+const shimmer = require('../../datadog-shimmer')
+
+const connectionAttributes = new WeakMap()
+const poolAttributes = new WeakMap()
+
+const executeStartChannel = channel('apm:oracledb:execute:start')
+const executeErrorChannel = channel('apm:oracledb:execute:error')
+const executeFinishChannel = channel('apm:oracledb:execute:finish')
+
+function finish (err) {
+  if (err) {
+    executeErrorChannel.publish(err)
+  }
+  executeFinishChannel.publish(undefined)
+}
+
+addHook({ name: 'oracledb', versions: ['5'] }, oracledb => {
+  shimmer.wrap(oracledb.Connection.prototype, 'execute', execute => {
+    return function wrappedExecute (dbQuery, ...args) {
+      if (!executeStartChannel.hasSubscribers) {
+        return execute.apply(this, arguments)
+      }
+
+      if (arguments.length && typeof arguments[arguments.length - 1] === 'function') {
+        const cb = arguments[arguments.length - 1]
+        const outerAr = new AsyncResource('apm:oracledb:outer-scope')
+        arguments[arguments.length - 1] = function wrappedCb (err, result) {
+          finish(err)
+          return outerAr.runInAsyncScope(() => cb.apply(this, arguments))
+        }
+      }
+
+      return new AsyncResource('apm:oracledb:inner-scope').runInAsyncScope(() => {
+        const connAttrs = connectionAttributes.get(this)
+        executeStartChannel.publish({ query: dbQuery, connAttrs })
+        try {
+          let result = execute.apply(this, arguments)
+
+          if (result && typeof result.then === 'function') {
+            result = result.then(
+              x => {
+                finish()
+                return x
+              },
+              e => {
+                finish(e)
+                throw e
+              }
+            )
+          }
+
+          return result
+        } catch (err) {
+          executeErrorChannel.publish(err)
+          throw err
+        }
+      })
+    }
+  })
+  shimmer.wrap(oracledb, 'getConnection', getConnection => {
+    return function wrappedGetConnection (connAttrs, callback) {
+      if (callback) {
+        arguments[1] = (err, connection) => {
+          if (connection) {
+            connectionAttributes.set(connection, connAttrs)
+          }
+          callback(err, connection)
+        }
+
+        getConnection.apply(this, arguments)
+      } else {
+        return getConnection.apply(this, arguments).then((connection) => {
+          connectionAttributes.set(connection, connAttrs)
+          return connection
+        })
+      }
+    }
+  })
+  shimmer.wrap(oracledb, 'createPool', createPool => {
+    return function wrappedCreatePool (poolAttrs, callback) {
+      if (callback) {
+        arguments[1] = (err, pool) => {
+          if (pool) {
+            poolAttributes.set(pool, poolAttrs)
+          }
+          callback(err, pool)
+        }
+
+        createPool.apply(this, arguments)
+      } else {
+        return createPool.apply(this, arguments).then((pool) => {
+          poolAttributes.set(pool, poolAttrs)
+          return pool
+        })
+      }
+    }
+  })
+  shimmer.wrap(oracledb.Pool.prototype, 'getConnection', getConnection => {
+    return function wrappedGetConnection () {
+      let callback
+      if (typeof arguments[arguments.length - 1] === 'function') {
+        callback = arguments[arguments.length - 1]
+      }
+      if (callback) {
+        arguments[arguments.length - 1] = (err, connection) => {
+          if (connection) {
+            connectionAttributes.set(connection, poolAttributes.get(this))
+          }
+          callback(err, connection)
+        }
+        getConnection.apply(this, arguments)
+      } else {
+        return getConnection.apply(this, arguments).then((connection) => {
+          connectionAttributes.set(connection, poolAttributes.get(this))
+          return connection
+        })
+      }
+    }
+  })
+  return oracledb
+})
