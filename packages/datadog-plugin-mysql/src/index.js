202a745345c22224e16c7c9a4c4a1e97fbe98f7f
John Hatcher <john.hatcher@teacherspayteachers.com>
2022-05-19 12:21:55 -0400

    Support passing in a function to determine service name for mysql

--- packages/datadog-plugin-mysql/src/index.js
+++ packages/datadog-plugin-mysql/src/index.js
@@ -13,12 +13,13 @@ class MySQLPlugin extends Plugin {
     super(...args)
 
     this.addSub(`apm:${this.constructor.name}:query:start`, ({ sql, conf }) => {
+      const service = getServiceName(this.tracer, this.config)
       const store = storage.getStore()
       const childOf = store ? store.span : store
       const span = this.tracer.startSpan('mysql.query', {
         childOf,
         tags: {
-          'service.name': this.config.service || `${this.tracer._service}-mysql`,
+          'service.name': service,
           'span.type': 'sql',
           'span.kind': 'client',
           'db.type': 'mysql',
@@ -52,6 +53,16 @@ class MySQLPlugin extends Plugin {
       const span = storage.getStore().span
       span.finish()
     })
+  }
+}
+
+function getServiceName (tracer, config) {
+  if (typeof config.service === 'function') {
+    return config.service()
+  } else if (config.service) {
+    return config.service
+  } else {
+    return `${tracer._service}-mysql`
   }
 }
 
