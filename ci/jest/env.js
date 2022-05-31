ca465ede1aa19b06d8b35a51a52ce6ef67e29d76
Juan Antonio Fernández de Alba <juan.fernandezdealba@datadoghq.com>
2022-05-31 17:12:22 +0200

    [ci-visibility] Do not instrument tests if agentless is setup but no API key is provided (#2072)

--- ci/jest/env.js
+++ ci/jest/env.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */
+
 const tracer = require('../../packages/dd-trace')
 const { ORIGIN_KEY } = require('../../packages/dd-trace/src/constants')
 
@@ -8,13 +10,19 @@ const options = {
   }
 }
 
-if (process.env.DD_CIVISIBILITY_AGENTLESS_ENABLED && (process.env.DATADOG_API_KEY || process.env.DD_API_KEY)) {
-  tracer.init({
-    ...options,
-    experimental: {
-      exporter: 'datadog'
-    }
-  })
+if (process.env.DD_CIVISIBILITY_AGENTLESS_ENABLED) {
+  if (process.env.DATADOG_API_KEY || process.env.DD_API_KEY) {
+    tracer.init({
+      ...options,
+      experimental: {
+        exporter: 'datadog'
+      }
+    })
+  } else {
+    console.error(`DD_CIVISIBILITY_AGENTLESS_ENABLED is set, \
+but neither DD_API_KEY nor DATADOG_API_KEY are set in your environment, \
+so dd-trace will not be initialized.`)
+  }
 } else {
   tracer.init({
     ...options,
