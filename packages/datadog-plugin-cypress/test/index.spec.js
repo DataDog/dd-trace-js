a0b41c7cdfe4f117028d1ec119bb47687419e769
Juan Fernandez <juan.fernandezdealba@datadoghq.com>
2022-06-02 17:10:48 +0200

    revert changes to CI

--- packages/datadog-plugin-cypress/test/index.spec.js
+++ packages/datadog-plugin-cypress/test/index.spec.js
@@ -27,9 +27,9 @@ describe('Plugin', function () {
   let cypressExecutable
   let appPort
   let agentListenPort
-  this.timeout(20000)
   withVersions('cypress', 'cypress', (version, moduleName) => {
     beforeEach(function () {
+      this.timeout(10000)
       return agent.load().then(() => {
         agentListenPort = agent.server.address().port
         cypressExecutable = require(`../../../versions/cypress@${version}`).get()
@@ -43,6 +43,7 @@ describe('Plugin', function () {
     afterEach(done => appServer.close(done))
 
     describe('cypress', function () {
+      this.timeout(120000)
       it('instruments tests', function (done) {
         process.env.DD_TRACE_AGENT_PORT = agentListenPort
         cypressExecutable.run({
