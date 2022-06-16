72a08eec504cbfc8f634b5e1b444ff9b91578f42
simon-id <simon.id@datadoghq.com>
2022-06-16 10:37:25 +0200

    Update server.js

--- packages/datadog-plugin-http2/src/server.js
+++ packages/datadog-plugin-http2/src/server.js
@@ -137,7 +137,6 @@ function finishStream (stream) {
 function addRequestTags (stream, headers, config) {
   const span = stream._datadog.span
   let url = `${headers[HTTP2_HEADER_SCHEME]}://${headers[HTTP2_HEADER_AUTHORITY]}${headers[HTTP2_HEADER_PATH]}`
-  
   url = web.obfuscateQs(config.qsObfuscator, url)
 
   span.addTags({
