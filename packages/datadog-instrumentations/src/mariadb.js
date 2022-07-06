c669bde59ef80d8616b75837b1fe85ee47f34263
Jamie Hodge <jamiehodge@me.com>
2022-07-06 16:51:30 +0200

    address comments

--- packages/datadog-instrumentations/src/mariadb.js
+++ packages/datadog-instrumentations/src/mariadb.js
@@ -1,82 +1,85 @@
-'use strict'
+"use strict";
 
-const {
-  channel,
-  addHook,
-  AsyncResource
-} = require('./helpers/instrument')
+const { channel, addHook, AsyncResource } = require("./helpers/instrument");
 
-const shimmer = require('../../datadog-shimmer')
+const shimmer = require("../../datadog-shimmer");
 
-const startCh = channel('apm:mariadb:query:start');
-const finishCh = channel('apm:mariadb:query:finish');
-const errorCh = channel('apm:mariadb:query:error');
+const startCh = channel("apm:mariadb:query:start");
+const finishCh = channel("apm:mariadb:query:finish");
+const errorCh = channel("apm:mariadb:query:error");
+
+function wrapQueryCmd(queryCmd) {
+  return function (_conn, sql, _values) {
+    if (!startCh.hasSubscribers) {
+      return queryCmd.apply(this, arguments);
+    }
+
+    const asyncResource = new AsyncResource("bound-anonymous-fn");
+
+    return asyncResource.runInAsyncScope(() => {
+      startCh.publish({ sql });
+
+      try {
+        const promise = queryCmd.apply(this, arguments);
+
+        promise.then(
+          asyncResource.bind(() => finishCh.publish()),
+          asyncResource.bind((e) => {
+            errorCh.publish(e);
+            finishCh.publish();
+          })
+        );
+
+        return promise;
+      } catch (e) {
+        errorCh.publish(e);
+        finishCh.publish();
+        throw e;
+      }
+    });
+  };
+}
+
+function wrapQueryStream(queryStream) {
+  return function (sql, _values) {
+    if (!startCh.hasSubscribers) {
+      return queryStream.apply(this, arguments);
+    }
+
+    const asyncResource = new AsyncResource("bound-anonymous-fn");
+
+    return asyncResource.runInAsyncScope(() => {
+      startCh.publish({ sql });
+
+      try {
+        const stream = queryStream.apply(this, arguments);
+
+        stream
+          .once("end", () => finishCh.publish())
+          .once("error", (e) => {
+            errorCh.publish(e);
+            finishCh.publish();
+          });
+
+        return stream;
+      } catch (e) {
+        errorCh.publish(e);
+        finishCh.publish();
+        throw e;
+      }
+    });
+  };
+}
 
 addHook(
   {
-    name: 'mariadb',
-    file: 'lib/connection-promise.js',
-    versions: ['>=3'],
+    name: "mariadb",
+    file: "lib/connection-promise.js",
+    versions: [">=3"],
   },
   (ConnectionPromise) => {
-    shimmer.wrap(ConnectionPromise, '_QUERY_CMD', (queryCmd) => {
-      return function (_conn, sql, _values) {
-        if (!startCh.hasSubscribers) {
-          return queryCmd.apply(this, arguments);
-        }
-
-        const asyncResource = new AsyncResource('bound-anonymous-fn');
-
-        return asyncResource.runInAsyncScope(() => {
-          startCh.publish({ sql });
-
-          try {
-            const promise = queryCmd.apply(this, arguments);
-
-            promise.then(
-              asyncResource.bind(() => finishCh.publish()),
-              asyncResource.bind((e) => {
-                errorCh.publish(e);
-                finishCh.publish();
-              })
-            );
-
-            return promise;
-          } catch (e) {
-            errorCh.publish(e);
-            finishCh.publish();
-            throw e;
-          }
-        });
-      };
-    });
-
-    shimmer.wrap(ConnectionPromise.prototype, 'queryStream', (queryStream) => {
-      return function (sql, _values) {
-        if (!startCh.hasSubscribers) {
-          return queryStream.apply(this, arguments);
-        }
-
-        startCh.publish({ sql });
-
-        try {
-          const stream = queryStream.apply(this, arguments);
-
-          stream
-            .once('end', () => finishCh.publish())
-            .once('error', (e) => {
-              errorCh.publish(e);
-              finishCh.publish();
-            });
-
-          return stream;
-        } catch (e) {
-          errorCh.publish(e);
-          finishCh.publish();
-          throw e;
-        }
-      };
-    });
+    shimmer.wrap(ConnectionPromise, "_QUERY_CMD", wrapQueryCmd);
+    shimmer.wrap(ConnectionPromise.prototype, "queryStream", wrapQueryStream);
 
     return ConnectionPromise;
   }
