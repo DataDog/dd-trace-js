e73f4de2815818aa3460acd79a955e022e83d4ec
sabrenner <sam.brenner@datadoghq.com>
2022-07-27 10:22:06 -0400

    remove problematic versions from testing

--- packages/datadog-instrumentations/src/couchbase.js
+++ packages/datadog-instrumentations/src/couchbase.js
@@ -232,7 +232,7 @@ addHook({ name: 'couchbase', file: 'lib/cluster.js', versions: ['>=3.0.0 <3.2.0'
 
 // semver >=3.2.0
 
-addHook({ name: 'couchbase', file: 'dist/collection.js', versions: ['>=3.2.0'] }, collection => {
+addHook({ name: 'couchbase', file: 'dist/collection.js', versions: ['>=3.2.0', '4.0.0', '4.1.x'] }, collection => {
   const Collection = collection.Collection
 
   wrapAllNames(['upsert', 'insert', 'replace'], name => {
@@ -242,7 +242,10 @@ addHook({ name: 'couchbase', file: 'dist/collection.js', versions: ['>=3.2.0'] }
   return collection
 })
 
-addHook({ name: 'couchbase', file: 'dist/cluster.js', versions: ['3.2.0 - 3.2.1', '>=3.2.2'] }, cluster => {
+addHook({
+  name: 'couchbase',
+  file: 'dist/cluster.js',
+  versions: ['3.2.0 - 3.2.1', '3.2.2 - 3.2.4', '4.0.0', '4.1.x'] }, cluster => {
   const Cluster = cluster.Cluster
 
   shimmer.wrap(Cluster.prototype, 'query', wrapV3Query)
