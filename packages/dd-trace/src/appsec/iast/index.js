bd09fcb1dcd0c06040bab5b34ff34a19210063a3
Ugaitz Urien <ugaitz.urien@datadoghq.com>
2022-07-27 14:53:10 +0200

    Improve IAST context storage

--- packages/dd-trace/src/appsec/iast/index.js
+++ packages/dd-trace/src/appsec/iast/index.js
@@ -1,25 +1,41 @@
-const { incomingHttpRequestEnd } = require('../gateway/channels')
+const { incomingHttpRequestStart, incomingHttpRequestEnd } = require('../gateway/channels')
 const { sendVulnerabilities } = require('./vulnerability-reporter')
 const web = require('../../plugins/util/web')
 const IAST_CONTEXT_KEY = Symbol('_dd.iast.context')
+const { storage } = require('../../../../datadog-core')
 
 function enable () {
   incomingHttpRequestEnd.subscribe(onIncomingHttpRequestEnd)
+  incomingHttpRequestStart.subscribe(onIncomingHttpRequestStart)
 }
 
 function disable () {
   if (incomingHttpRequestEnd.hasSubscribers) incomingHttpRequestEnd.unsubscribe(onIncomingHttpRequestEnd)
+  if (incomingHttpRequestStart.hasSubscribers) incomingHttpRequestStart.unsubscribe(onIncomingHttpRequestStart)
 }
 
-function onIncomingHttpRequestEnd (data) {
+function onIncomingHttpRequestStart (data) {
+  // TODO Add OCE check
   if (data && data.req) {
-    const topContext = web.getContext(data.req)
-    const iastContext = topContext && topContext[IAST_CONTEXT_KEY]
-    if (iastContext) {
-      const rootSpan = topContext.span
-      sendVulnerabilities(iastContext, rootSpan)
+    const store = storage.getStore()
+    if (store) {
+      const topContext = web.getContext(data.req)
+      if (topContext) {
+        const rootSpan = topContext.span
+        store[IAST_CONTEXT_KEY] = { rootSpan, req: data.req }
+      }
     }
   }
 }
 
-module.exports = { enable, disable, onIncomingHttpRequestEnd, IAST_CONTEXT_KEY }
+function onIncomingHttpRequestEnd (data) {
+  if (data && data.req) {
+    const store = storage.getStore()
+    const iastContext = store && store[IAST_CONTEXT_KEY]
+    if (iastContext && iastContext.rootSpan) {
+      sendVulnerabilities(iastContext, iastContext.rootSpan)
+    }
+  }
+}
+
+module.exports = { enable, disable, onIncomingHttpRequestEnd, onIncomingHttpRequestStart, IAST_CONTEXT_KEY }
--- packages/dd-trace/test/appsec/iast/index.spec.js
+++ packages/dd-trace/test/appsec/iast/index.spec.js
@@ -5,6 +5,7 @@ describe('IAST Index', () => {
   let web
   let vulnerabilityReporter
   let IAST
+  let datadogCore
   beforeEach(() => {
     web = {
       getContext: sinon.stub()
@@ -12,9 +13,15 @@ describe('IAST Index', () => {
     vulnerabilityReporter = {
       sendVulnerabilities: sinon.stub()
     }
+    datadogCore = {
+      storage: {
+        getStore: sinon.stub()
+      }
+    }
     IAST = proxyquire('../../../src/appsec/iast', {
       '../../plugins/util/web': web,
-      './vulnerability-reporter': vulnerabilityReporter
+      './vulnerability-reporter': vulnerabilityReporter,
+      '../../../../datadog-core': datadogCore
     })
     sinon.stub(incomingHttpRequestEnd, 'subscribe')
   })
@@ -39,6 +46,40 @@ describe('IAST Index', () => {
       IAST.disable()
       expect(incomingHttpRequestEnd.unsubscribe)
         .to.have.been.calledOnceWithExactly(IAST.onIncomingHttpRequestEnd)
+    })
+  })
+
+  describe('onIncomingHttpRequestStart', () => {
+    it('should not fail with unexpected data', () => {
+      IAST.onIncomingHttpRequestStart()
+      IAST.onIncomingHttpRequestStart(null)
+      IAST.onIncomingHttpRequestStart({})
+    })
+
+    it('should not fail with unexpected context', () => {
+      datadogCore.storage.getStore.returns({})
+      web.getContext.returns(null)
+      IAST.onIncomingHttpRequestStart({ req: {} })
+      expect(web.getContext).to.be.calledOnce
+    })
+
+    it('should not fail with unexpected store', () => {
+      web.getContext.returns({})
+      datadogCore.storage.getStore.returns(null)
+      IAST.onIncomingHttpRequestStart({ req: {} })
+      expect(datadogCore.storage.getStore).to.be.calledOnce
+    })
+
+    it('Adds IAST context to store', () => {
+      const store = {}
+      const topContext = { span: {} }
+      const data = { req: {} }
+      datadogCore.storage.getStore.returns(store)
+      web.getContext.returns(topContext)
+      IAST.onIncomingHttpRequestStart(data)
+      expect(store[IAST.IAST_CONTEXT_KEY]).not.null
+      expect(store[IAST.IAST_CONTEXT_KEY].req).equals(data.req)
+      expect(store[IAST.IAST_CONTEXT_KEY].rootSpan).equals(topContext.span)
     })
   })
 
@@ -69,10 +110,10 @@ describe('IAST Index', () => {
 
     it('should call send vulnerabilities with context with span and iast context', () => {
       const span = { key: 'val' }
-      const iastContext = { vulnerabilities: [] }
-      const context = { span }
-      context[IAST.IAST_CONTEXT_KEY] = iastContext
-      web.getContext.returns(context)
+      const iastContext = { vulnerabilities: [], rootSpan: span }
+      const store = { span }
+      store[IAST.IAST_CONTEXT_KEY] = iastContext
+      datadogCore.storage.getStore.returns(store)
       IAST.onIncomingHttpRequestEnd({ req: {} })
       expect(vulnerabilityReporter.sendVulnerabilities).to.be.calledOnceWith(iastContext, span)
     })
