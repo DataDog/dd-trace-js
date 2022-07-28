602f626974362595923d9ddc192efe2949da19c3
sabrenner <sam.brenner@datadoghq.com>
2022-07-28 15:12:00 -0400

    back to ignoring 3.2.2-3.2.4 and 4.1.x

--- packages/datadog-plugin-couchbase/test/index.spec.js
+++ packages/datadog-plugin-couchbase/test/index.spec.js
@@ -148,10 +148,14 @@ describe('Plugin', () => {
       })
     })
 
+    const ignoreVersions = version =>
+      semver.intersects(version, '3.2.2 - 3.2.4') ||
+      semver.intersects(version, '^4.1.0')
+
     withVersions('couchbase', 'couchbase', '>=3.0.0', version => {
       let collection
 
-      if (!semver.intersects(version, '^4.1.0')) {
+      if (!ignoreVersions(version)) {
         describe('without configuration', () => {
           beforeEach(done => {
             agent.load('couchbase').then(() => {
