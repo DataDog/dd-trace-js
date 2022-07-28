0d119be05948a96fc2fbbafc13b731c01cce0d8c
sabrenner <sam.brenner@datadoghq.com>
2022-07-28 11:40:15 -0400

    allowing 3.2.4 to run to test a version >=3.2.2

--- packages/datadog-instrumentations/src/couchbase.js
+++ packages/datadog-instrumentations/src/couchbase.js
@@ -245,7 +245,8 @@ addHook({ name: 'couchbase', file: 'dist/collection.js', versions: ['>=3.2.0', '
 addHook({
   name: 'couchbase',
   file: 'dist/cluster.js',
-  versions: ['3.2.0 - 3.2.1', '3.2.2 - 3.2.4', '4.0.0', '4.1.x'] }, cluster => {
+  // unsafe versions in testing: 3.2.2 - 3.2.3, 4.1.x
+  versions: ['3.2.0 - 3.2.1', '3.2.2 - 3.2.3', '3.2.4 - 4.0.0', '4.1.x'] }, cluster => {
   const Cluster = cluster.Cluster
 
   shimmer.wrap(Cluster.prototype, 'query', wrapV3Query)
