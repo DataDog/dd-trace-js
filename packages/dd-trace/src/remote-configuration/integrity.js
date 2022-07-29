8d59ead24e07766570f1a4ec65b89fc2a58573f0
Vladimir de Turckheim <vlad2t@hotmail.com>
2022-07-29 15:46:33 +0200

    add integrity checks for remote configuration

---
+++ packages/dd-trace/src/remote-configuration/integrity.js
@@ -0,0 +1,66 @@
+'use strict'
+
+const crypto = require('crypto')
+const { canonicalize } = require('json-canonicalize')
+
+function findSig (signatures, rcTargetsKeyId) {
+  for (const entry of signatures) {
+    if (entry.keyid === rcTargetsKeyId) {
+      if (!entry.sig) break
+      return entry.sig
+    }
+  }
+  throw new Error(`missing signature for key ${rcTargetsKeyId}`)
+}
+
+function localAtob (str) {
+  return Buffer.from(str, 'base64').toString('ascii')
+}
+
+function toDER (hex) {
+  return Buffer.concat([
+    Buffer.from('302a300506032b6570032100', 'hex'), // Static value containing OID for algorithm
+    Buffer.from(hex, 'hex')
+  ])
+}
+
+function checkIntegrity (rcTargetsKey, rcTargetsKeyId, clientGetConfigsResponse) {
+  const targets = clientGetConfigsResponse.targets
+  if (!targets) throw new Error('no field \'targets\' in clientGetConfigsResponse object')
+
+  const targetFiles = clientGetConfigsResponse.target_files
+  if (!targetFiles) throw new Error('no field \'target_files\' in clientGetConfigsResponse object')
+
+  const { signed, signatures } = JSON.parse(localAtob(targets))
+  if (!signed) throw new Error('no field \'signed\' in targets object')
+  if (!signatures) throw new Error('no field \'signatures\' in targets object')
+
+  const sig = findSig(signatures, rcTargetsKeyId)
+  const rawSigned = Buffer.from(canonicalize(signed))
+
+  const key = crypto.createPublicKey({
+    format: 'der',
+    type: 'spki',
+    key: toDER(rcTargetsKey)
+  })
+
+  // The algorithm is defined based on the oid
+  const valid = crypto.verify(null, rawSigned, key, Buffer.from(sig, 'hex'))
+  if (!valid) throw new Error(`invalid signature for key ${rcTargetsKeyId}`)
+
+  for (let i = 0; i < targetFiles.length; ++i) {
+    const configMeta = signed.targets[targetFiles[i].path]
+    if (!configMeta) throw new Error(`target ${targetFiles[i].path} not found in targets`)
+    const raw = localAtob(targetFiles[i].raw)
+    const hash = crypto.createHash('sha256')
+    const rawHash = hash.update(Buffer.from(raw)).digest('hex');
+    if (raw.length !== configMeta.length || rawHash !== configMeta.hashes.sha256){
+      throw new Error(`target ${targetFiles[i].path} expected sha256 was ${configMeta.hashes.sha256}, ${rawHash} found`)
+    }
+  }
+  return true
+}
+
+module.exports = {
+  checkIntegrity
+}
---
+++ packages/dd-trace/test/remote-configuration/integrity.spec.js
@@ -0,0 +1,68 @@
+'use strict'
+require('../setup/node');
+
+const {checkIntegrity} = require('../../src/remote-configuration/integrity')
+const validOneFile = require('./samples/validOneFile.json')
+const tufTargetsMissingTargetFile = require('./samples/tufTargetsMissingTargetFile.json')
+const tufTargetsInvalidSignature = require('./samples/tufTargetsInvalidSignature.json')
+const tufTargetsSignedWithInvalidKey = require('./samples/tufTargetsSignedWithInvalidKey.json')
+const tufTargetsInvalidTargetFileHash = require('./samples/tufTargetsInvalidTargetFileHash.json')
+const tufTargetsInvalidTargetFileLength = require('./samples/tufTargetsInvalidTargetFileLength.json')
+
+const TEST_KEY_ID = 'ed7672c9a24abda78872ee32ee71c7cb1d5235e8db4ecbf1ca28b9c50eb75d9e'
+const TEST_PUBLIC_KEY = '7d3102e39abe71044d207550bda239c71380d013ec5a115f79f51622630054e6'
+
+describe('Remote configuration Index', () => {
+
+  it('should accept a valid payload', () => {
+    const res = checkIntegrity(TEST_PUBLIC_KEY, TEST_KEY_ID, validOneFile)
+    expect(res)
+  })
+
+  it('should fail on tufTargetsMissingTargetFile', () => {
+    try {
+      checkIntegrity(TEST_PUBLIC_KEY, TEST_KEY_ID, tufTargetsMissingTargetFile)
+    } catch (e) {
+      expect(e.message).to
+        .equal('target datadog/2/ASM_DD/ASM_DD-base/config not found in targets')
+    }
+  })
+
+  it('should fail on tufTargetsInvalidSignature', () => {
+    try {
+      checkIntegrity(TEST_PUBLIC_KEY, TEST_KEY_ID, tufTargetsInvalidSignature)
+    } catch (e) {
+      expect(e.message).to
+        .equal('invalid signature for key ed7672c9a24abda78872ee32ee71c7cb1d5235e8db4ecbf1ca28b9c50eb75d9e')
+    }
+  })
+
+  it('should fail on tufTargetsSignedWithInvalidKey', () => {
+    try {
+      checkIntegrity(TEST_PUBLIC_KEY, TEST_KEY_ID, tufTargetsSignedWithInvalidKey)
+    } catch (e) {
+      expect(e.message).to
+        .equal('missing signature for key ed7672c9a24abda78872ee32ee71c7cb1d5235e8db4ecbf1ca28b9c50eb75d9e')
+    }
+  })
+
+  it('should fail on tufTargetsInvalidTargetFileHash', () => {
+    try {
+      checkIntegrity(TEST_PUBLIC_KEY, TEST_KEY_ID, tufTargetsInvalidTargetFileHash)
+    } catch (e) {
+      expect(e.message).to
+        .equal('target datadog/2/ASM_DD/ASM_DD-base/config expected sha256 was 66616b6568617368, ' +
+          '4608754de40c82df65d6b8d5d16b2d722721c87bc6fb37fb24cec58ae2d300b7 found')
+    }
+  })
+
+  it('should fail on tufTargetsInvalidTargetFileLength', () => {
+    try {
+      checkIntegrity(TEST_PUBLIC_KEY, TEST_KEY_ID, tufTargetsInvalidTargetFileLength)
+    } catch (e) {
+      expect(e.message).to
+        .equal('target datadog/2/ASM_DD/ASM_DD-base/config expected sha256 was 66616b6568617368, ' +
+          '4608754de40c82df65d6b8d5d16b2d722721c87bc6fb37fb24cec58ae2d300b7 found')
+    }
+  })
+})
