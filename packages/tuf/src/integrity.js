8e19aa31c3aec3f5f79b6a7b0f8a36843c642908
Vladimir de Turckheim <vlad2t@hotmail.com>
2022-08-04 15:14:25 +0200

    alternative way to get rawSigned

--- packages/tuf/src/integrity.js
+++ packages/tuf/src/integrity.js
@@ -1,7 +1,6 @@
 'use strict'
 
 const crypto = require('crypto')
-// const { serialize } = require('./cjson')
 const { extractSigned } = require('./extraction')
 
 function findSig (signatures, rcTargetsKeyId) {
