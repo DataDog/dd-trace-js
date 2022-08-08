1675386aeaed32d2b4e18832e56cc742c6b13418
sabrenner <sam.brenner@datadoghq.com>
2022-08-08 09:44:48 -0400

    wip

--- packages/datadog-plugin-couchbase/test/index.spec.js
+++ packages/datadog-plugin-couchbase/test/index.spec.js
@@ -152,6 +152,7 @@ describe('Plugin', () => {
       })
     } else {
       const ignoreVersions = version =>
+        semver.intersects(version, '3.2.0') ||
         semver.intersects(version, '^4.1.0')
 
       withVersions('couchbase', 'couchbase', '>=3.0.0', version => {
