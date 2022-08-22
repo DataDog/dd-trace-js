a39c4a81223b9bc9d18ace326ef96ac10753e76c
Vladimir de Turckheim <vlad2t@hotmail.com>
2022-08-22 14:53:30 +0200

    add real life test from agent

--- packages/tuf/test/integrity.spec.js
+++ packages/tuf/test/integrity.spec.js
@@ -1,5 +1,4 @@
 'use strict'
-
 const { expect } = require('chai')
 const { checkIntegrity } = require('../src/integrity')
 const validOneFile = require('./samples/validOneFile.json')
@@ -8,6 +7,7 @@ const tufTargetsInvalidSignature = require('./samples/tufTargetsInvalidSignature
 const tufTargetsSignedWithInvalidKey = require('./samples/targetsSignedWithInvalidKey.json')
 const tufTargetsInvalidTargetFileHash = require('./samples/tufTargetsInvalidTargetFileHash.json')
 const tufTargetsInvalidTargetFileLength = require('./samples/tufTargetsInvalidTargetFileLength.json')
+const payloadFromAgent = require('./samples/paylaodfromAgent.json')
 
 const TEST_KEY_ID = 'ed7672c9a24abda78872ee32ee71c7cb1d5235e8db4ecbf1ca28b9c50eb75d9e'
 const TEST_PUBLIC_KEY = '7d3102e39abe71044d207550bda239c71380d013ec5a115f79f51622630054e6'
@@ -65,6 +65,12 @@ describe('TUF', () => {
           .equal('target datadog/2/ASM_DD/ASM_DD-base/config expected sha256 was 66616b6568617368, ' +
             '4608754de40c82df65d6b8d5d16b2d722721c87bc6fb37fb24cec58ae2d300b7 found')
       }
+    })
+
+    it('should work on a real life example (from the agent\'s code)', () => {
+      const key = 'e3f1f98c9da02a93bb547f448b472d727e14b22455235796fe49863856252508'
+      const keyId = '5c4ece41241a1bb513f6e3e5df74ab7d5183dfffbd71bfd43127920d880569fd'
+      expect(checkIntegrity(key, keyId, payloadFromAgent)).to.equal(true)
     })
   })
 })
