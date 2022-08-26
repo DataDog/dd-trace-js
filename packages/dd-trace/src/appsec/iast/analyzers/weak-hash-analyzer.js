1e2887e48b93011568712feee156706961205cbf
Ugaitz Urien <ugaitz.urien@datadoghq.com>
2022-08-26 12:36:29 +0200

    Fix PR comments

--- packages/dd-trace/src/appsec/iast/analyzers/weak-hash-analyzer.js
+++ packages/dd-trace/src/appsec/iast/analyzers/weak-hash-analyzer.js
@@ -1,11 +1,11 @@
 'use strict'
 const Analyzer = require('./vulnerability-analyzer')
 
-const INSECURE_HASH_ALGORITHMS = [
+const INSECURE_HASH_ALGORITHMS = new Set([
   'md4', 'md4WithRSAEncryption', 'RSA-MD4',
   'RSA-MD5', 'md5', 'md5-sha1', 'ssl3-md5', 'md5WithRSAEncryption',
   'RSA-SHA1', 'RSA-SHA1-2', 'sha1', 'md5-sha1', 'sha1WithRSAEncryption', 'ssl3-sha1'
-].map(algorithm => algorithm.toLowerCase())
+].map(algorithm => algorithm.toLowerCase()))
 
 class WeakHashAnalyzer extends Analyzer {
   constructor () {
@@ -14,8 +14,8 @@ class WeakHashAnalyzer extends Analyzer {
   }
 
   _isVulnerable (algorithm) {
-    if (algorithm && typeof algorithm === 'string') {
-      return INSECURE_HASH_ALGORITHMS.indexOf(algorithm.toLowerCase()) !== -1
+    if (typeof algorithm === 'string') {
+      return INSECURE_HASH_ALGORITHMS.has(algorithm.toLowerCase())
     }
     return false
   }
