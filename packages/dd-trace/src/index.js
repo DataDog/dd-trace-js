761408a08b9fefd36927fdad30b720f88bbc531c
rochdev <roch.devost@gmail.com>
2022-08-26 11:10:54 -0400

    code cleanup

--- packages/dd-trace/src/index.js
+++ packages/dd-trace/src/index.js
@@ -2,12 +2,8 @@
 
 const { isFalse } = require('./util')
 
-let inJestWorker = true
-try {
-  jest // eslint-disable-line no-undef
-} catch (e) {
-  inJestWorker = false // global `jest` is only present in Jest workers
-}
+// Global `jest` is only present in Jest workers.
+const inJestWorker = typeof jest !== 'undefined'
 
 module.exports = isFalse(process.env.DD_TRACE_ENABLED) || inJestWorker
   ? require('./noop/proxy')
