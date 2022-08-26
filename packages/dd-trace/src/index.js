06a40a0dea2fc7fdf93a50de4cb0b74f3a5ae821
rochdev <roch.devost@gmail.com>
2022-08-26 11:02:16 -0400

    check for actual jest variable

--- packages/dd-trace/src/index.js
+++ packages/dd-trace/src/index.js
@@ -2,6 +2,13 @@
 
 const { isFalse } = require('./util')
 
-module.exports = isFalse(process.env.DD_TRACE_ENABLED) || global.jest
+let inJestWorker = true
+try {
+  jest // eslint-disable-line no-undef
+} catch (e) {
+  inJestWorker = false // global `jest` is only present in Jest workers
+}
+
+module.exports = isFalse(process.env.DD_TRACE_ENABLED) || inJestWorker
   ? require('./noop/proxy')
   : require('./proxy')
