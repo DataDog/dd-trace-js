b6a1f60f2026954502a6d970d8ac169244c05936
Ugaitz Urien <ugaitz.urien@datadoghq.com>
2022-09-01 11:47:46 +0200

    Detect transitive dependencies (#2258)
    Co-authored-by: CarlesDD <carles.capell@datadoghq.com>

--- packages/dd-trace/src/iitm.js
+++ packages/dd-trace/src/iitm.js
@@ -2,8 +2,19 @@
 
 const semver = require('semver')
 const logger = require('./log')
+const { addHook } = require('import-in-the-middle')
+const dc = require('diagnostics_channel')
 
 if (semver.satisfies(process.versions.node, '^12.20.0 || >=14.13.1')) {
+  const moduleLoadStartChannel = dc.channel('dd-trace:moduleLoadStart')
+  addHook((name, namespace) => {
+    if (moduleLoadStartChannel.hasSubscribers) {
+      moduleLoadStartChannel.publish({
+        filename: name,
+        module: namespace
+      })
+    }
+  })
   module.exports = require('import-in-the-middle')
 } else {
   logger.warn('ESM is not fully supported by this version of Node.js, ' +
