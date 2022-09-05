b658339454372c46df198bf75beab304d0441805
CarlesDD <carles.capell@datadoghq.com>
2022-09-05 12:29:10 +0200

    Sort multiple analyzer export alphabetically

--- packages/dd-trace/src/appsec/iast/analyzers/analyzers.js
+++ packages/dd-trace/src/appsec/iast/analyzers/analyzers.js
@@ -1,4 +1,4 @@
 module.exports = {
-  'WEAK_HASH_ANALYZER': require('./weak-hash-analyzer'),
-  'WEAK_CIPHER_ANALYZER': require('./weak-cipher-analyzer')
+  'WEAK_CIPHER_ANALYZER': require('./weak-cipher-analyzer'),
+  'WEAK_HASH_ANALYZER': require('./weak-hash-analyzer')
 }
