518ec510871c3034c1b2c23913f77a606d85f741
Juan Antonio Fernández de Alba <juan.fernandezdealba@datadoghq.com>
2022-09-09 10:06:57 +0200

    [ci-visibility] Test suite visibility: support `jest` (#2122)

--- packages/datadog-plugin-jest/src/index.js
+++ packages/datadog-plugin-jest/src/index.js
@@ -9,10 +9,15 @@ const {
   getTestEnvironmentMetadata,
   getTestParentSpan,
   getTestCommonTags,
+  getTestSessionCommonTags,
+  getTestSuiteCommonTags,
   TEST_PARAMETERS,
   getCodeOwnersFileEntries,
   getCodeOwnersForFilename,
-  TEST_CODE_OWNERS
+  TEST_CODE_OWNERS,
+  TEST_SESSION_ID,
+  TEST_SUITE_ID,
+  TEST_COMMAND
 } = require('../../dd-trace/src/plugins/util/test')
 
 // https://github.com/facebook/jest/blob/d6ad15b0f88a05816c2fe034dd6900d28315d570/packages/jest-worker/src/types.ts#L38
@@ -58,6 +63,87 @@ class JestPlugin extends Plugin {
     this.testEnvironmentMetadata = getTestEnvironmentMetadata('jest', this.config)
     this.codeOwnersEntries = getCodeOwnersFileEntries()
 
+    this.addSub('ci:jest:session:start', (command) => {
+      if (!this.config.isAgentlessEnabled) {
+        return
+      }
+      const store = storage.getStore()
+      const childOf = getTestParentSpan(this.tracer)
+      const testSessionSpanMetadata = getTestSessionCommonTags(command, this.tracer._version)
+
+      const testSessionSpan = this.tracer.startSpan('jest.test_session', {
+        childOf,
+        tags: {
+          ...this.testEnvironmentMetadata,
+          ...testSessionSpanMetadata
+        }
+      })
+      this.enter(testSessionSpan, store)
+    })
+
+    this.addSub('ci:jest:session:finish', (status) => {
+      if (!this.config.isAgentlessEnabled) {
+        return
+      }
+      const testSessionSpan = storage.getStore().span
+      testSessionSpan.setTag(TEST_STATUS, status)
+      testSessionSpan.finish()
+      finishAllTraceSpans(testSessionSpan)
+      this.tracer._exporter._writer.flush()
+    })
+
+    // Test suites can be run in a different process from jest's main one.
+    // This subscriber changes the configuration objects from jest to inject the trace id
+    // of the test session to the processes that run the test suites.
+    this.addSub('ci:jest:session:configuration', configs => {
+      if (!this.config.isAgentlessEnabled) {
+        return
+      }
+      const testSessionSpan = storage.getStore().span
+      configs.forEach(config => {
+        config._ddTestSessionId = testSessionSpan.context()._traceId.toString(10)
+        config._ddTestCommand = testSessionSpan.context()._tags[TEST_COMMAND]
+      })
+    })
+
+    this.addSub('ci:jest:test-suite:start', ({ testSuite, testEnvironmentOptions }) => {
+      if (!this.config.isAgentlessEnabled) {
+        return
+      }
+
+      const { _ddTestSessionId: testSessionId, _ddTestCommand: testCommand } = testEnvironmentOptions
+
+      const store = storage.getStore()
+
+      const testSessionSpanContext = this.tracer.extract('text_map', {
+        'x-datadog-trace-id': testSessionId,
+        'x-datadog-parent-id': '0000000000000000'
+      })
+
+      const testSuiteMetadata = getTestSuiteCommonTags(testCommand, this.tracer._version, testSuite)
+
+      const testSuiteSpan = this.tracer.startSpan('jest.test_suite', {
+        childOf: testSessionSpanContext,
+        tags: {
+          ...this.testEnvironmentMetadata,
+          ...testSuiteMetadata
+        }
+      })
+      this.enter(testSuiteSpan, store)
+    })
+
+    this.addSub('ci:jest:test-suite:finish', ({ status, errorMessage }) => {
+      if (!this.config.isAgentlessEnabled) {
+        return
+      }
+      const testSuiteSpan = storage.getStore().span
+      testSuiteSpan.setTag(TEST_STATUS, status)
+      if (errorMessage) {
+        testSuiteSpan.setTag('error', new Error(errorMessage))
+      }
+      testSuiteSpan.finish()
+    })
+
     this.addSub('ci:jest:test:code-coverage', (coverageFiles) => {
       if (!this.config.isAgentlessEnabled || !this.config.isIntelligentTestRunnerEnabled) {
         return
@@ -96,7 +182,20 @@ class JestPlugin extends Plugin {
   }
 
   startTestSpan (test) {
-    const { childOf, ...testSpanMetadata } = getTestSpanMetadata(this.tracer, test)
+    const suiteTags = {}
+    const store = storage.getStore()
+    const testSuiteSpan = store ? store.span : undefined
+    if (testSuiteSpan) {
+      const testSuiteId = testSuiteSpan.context()._spanId.toString(10)
+      suiteTags[TEST_SUITE_ID] = testSuiteId
+      suiteTags[TEST_SESSION_ID] = testSuiteSpan.context()._traceId.toString(10)
+      suiteTags[TEST_COMMAND] = testSuiteSpan.context()._tags[TEST_COMMAND]
+    }
+
+    const {
+      childOf,
+      ...testSpanMetadata
+    } = getTestSpanMetadata(this.tracer, test)
 
     const codeOwners = getCodeOwnersForFilename(test.suite, this.codeOwnersEntries)
 
@@ -109,7 +208,8 @@ class JestPlugin extends Plugin {
         childOf,
         tags: {
           ...this.testEnvironmentMetadata,
-          ...testSpanMetadata
+          ...testSpanMetadata,
+          ...suiteTags
         }
       })
 
--- packages/datadog-plugin-jest/test/jasmine2.spec.js
+++ packages/datadog-plugin-jest/test/jasmine2.spec.js
@@ -31,13 +31,12 @@ describe('Plugin', () => {
     testPathIgnorePatterns: ['/node_modules/'],
     coverageReporters: [],
     reporters: [],
-    testRunner: 'jest-jasmine2',
     silent: true,
     cache: false,
     maxWorkers: '50%'
   }
 
-  withVersions('jest', ['jest-jasmine2'], (version, moduleName) => {
+  withVersions('jest', ['jest-jasmine2'], (version) => {
     afterEach(() => {
       const jestTestFile = fs.readdirSync(__dirname).filter(name => name.startsWith('jest-'))
       jestTestFile.forEach((testFile) => {
@@ -52,6 +51,9 @@ describe('Plugin', () => {
         .reply(200, 'OK')
 
       return agent.load(['jest', 'http'], { service: 'test' }).then(() => {
+        jestCommonOptions.testRunner =
+          require(`../../../versions/jest@${version}`).getPath('jest-jasmine2')
+
         jestExecutable = require(`../../../versions/jest@${version}`).get()
       })
     })
