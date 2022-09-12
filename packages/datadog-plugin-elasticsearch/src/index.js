e3efbb03bac16af8b4ecc95d87a6473f5b4579a9
rochdev <roch.devost@gmail.com>
2022-09-12 17:57:48 -0400

    convert elasticsearch

--- packages/datadog-plugin-elasticsearch/src/index.js
+++ packages/datadog-plugin-elasticsearch/src/index.js
@@ -1,49 +1,33 @@
 'use strict'
 
-const Plugin = require('../../dd-trace/src/plugins/plugin')
-const { storage } = require('../../datadog-core')
-const analyticsSampler = require('../../dd-trace/src/analytics_sampler')
+const DatabasePlugin = require('../../dd-trace/src/plugins/database')
 
-class ElasticsearchPlugin extends Plugin {
-  static get name () {
-    return 'elasticsearch'
+class ElasticsearchPlugin extends DatabasePlugin {
+  static name = 'elasticsearch'
+  static prefix = 'apm:elasticsearch:query'
+
+  start ({ params }) {
+    const body = getBody(params.body || params.bulkBody)
+
+    this.startSpan('elasticsearch.query', {
+      service: this.config.service || `${this.tracer._service}-elasticsearch`,
+      resource: `${params.method} ${quantizePath(params.path)}`,
+      type: 'elasticsearch',
+      kind: 'client',
+      meta: {
+        'db.type': 'elasticsearch',
+        'elasticsearch.url': params.path,
+        'elasticsearch.method': params.method,
+        'elasticsearch.body': body,
+        'elasticsearch.params': JSON.stringify(params.querystring || params.query)
+      }
+    })
   }
 
-  constructor (...args) {
-    super(...args)
-
-    this.addSub('apm:elasticsearch:query:start', ({ params }) => {
-      const store = storage.getStore()
-      const childOf = store ? store.span : store
-      const body = getBody(params.body || params.bulkBody)
-      const span = this.tracer.startSpan('elasticsearch.query', {
-        childOf,
-        tags: {
-          'db.type': 'elasticsearch',
-          'span.kind': 'client',
-          'service.name': this.config.service || `${this.tracer._service}-elasticsearch`,
-          'resource.name': `${params.method} ${quantizePath(params.path)}`,
-          'span.type': 'elasticsearch',
-          'elasticsearch.url': params.path,
-          'elasticsearch.method': params.method,
-          'elasticsearch.body': body,
-          'elasticsearch.params': JSON.stringify(params.querystring || params.query)
-        }
-      })
-      analyticsSampler.sample(span, this.config.measured)
-      this.enter(span, store)
-    })
-
-    this.addSub('apm:elasticsearch:query:error', err => {
-      const span = storage.getStore().span
-      span.setTag('error', err)
-    })
-
-    this.addSub('apm:elasticsearch:query:finish', ({ params }) => {
-      const span = storage.getStore().span
-      this.config.hooks.query(span, params)
-      span.finish()
-    })
+  finish ({ params }) {
+    const span = this.activeSpan()
+    this.config.hooks.query(span, params)
+    super.finish({ params })
   }
 
   configure (config) {
