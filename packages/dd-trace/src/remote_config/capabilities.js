e675b2c4fefe2053669b1451ec8ce39539beb164
simon-id <simon.diaz@datadoghq.com>
2022-09-23 11:11:25 +0200

    correct capabilities values

--- packages/dd-trace/src/remote_config/capabilities.js
+++ packages/dd-trace/src/remote_config/capabilities.js
@@ -1,7 +1,7 @@
 'use strict'
 
 module.exports = {
-  ASM_ACTIVATION: 1n << 2n,
-  ASM_IP_BLOCKING: 1n << 3n,
-  ASM_DD_RULES: 1n << 4n,
+  ASM_ACTIVATION: 1n << 1n,
+  ASM_IP_BLOCKING: 1n << 2n,
+  ASM_DD_RULES: 1n << 3n,
 }
