5252ec53ddf27f380298602fe73d1706e32206b2
rochdev <roch.devost@gmail.com>
2022-10-05 13:16:09 -0400

    convert dns

---
+++ packages/datadog-plugin-dns/src/resolve.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const ClientPlugin = require('../../dd-trace/src/plugins/client')
+
+class DNSResolvePlugin extends ClientPlugin {
+  static get name () { return 'dns' }
+  static get operation () { return 'resolve' }
+
+  start ([hostname, maybeType]) {
+    const rrtype = typeof maybeType === 'string' ? maybeType : 'A'
+
+    this.startSpan('dns.resolve', {
+      service: this.config.service,
+      resource: `${rrtype} ${hostname}`,
+      kind: 'client',
+      meta: {
+        'dns.hostname': hostname,
+        'dns.rrtype': rrtype
+      }
+    })
+  }
+}
+
+module.exports = DNSResolvePlugin
