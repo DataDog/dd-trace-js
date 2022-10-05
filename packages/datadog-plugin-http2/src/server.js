450bef4e1573ceebbec404ef0002b6d2ff847979
Stephen Belanger <stephen.belanger@datadoghq.com>
2022-10-05 15:27:11 +0100

    Add support for http2 servers in compatibility mode (#2415)
    * Add support for http2 servers in compatibility mode
    
    * fix typo
    
    Co-authored-by: Roch Devost <roch.devost@datadoghq.com>

--- packages/datadog-plugin-http2/src/server.js
+++ packages/datadog-plugin-http2/src/server.js
@@ -3,10 +3,51 @@
 // Plugin temporarily disabled. See https://github.com/DataDog/dd-trace-js/issues/312
 
 const Plugin = require('../../dd-trace/src/plugins/plugin')
+const { storage } = require('../../datadog-core')
+const web = require('../../dd-trace/src/plugins/util/web')
+const { incomingHttpRequestStart } = require('../../dd-trace/src/appsec/gateway/channels')
 
 class Http2ServerPlugin extends Plugin {
   static get name () {
     return 'http2'
+  }
+
+  constructor (...args) {
+    super(...args)
+
+    this.addSub('apm:http2:server:request:start', ({ req, res }) => {
+      const store = storage.getStore()
+      const span = web.startSpan(this.tracer, this.config, req, res, 'web.request')
+
+      this.enter(span, { ...store, req })
+
+      const context = web.getContext(req)
+
+      if (!context.instrumented) {
+        context.res.writeHead = web.wrapWriteHead(context)
+        context.instrumented = true
+      }
+
+      if (incomingHttpRequestStart.hasSubscribers) {
+        incomingHttpRequestStart.publish({ req, res })
+      }
+    })
+
+    this.addSub('apm:http2:server:request:error', (error) => {
+      web.addError(error)
+    })
+
+    this.addSub('apm:http2:server:request:finish', ({ req }) => {
+      const context = web.getContext(req)
+
+      if (!context || !context.res) return // Not created by a http.Server instance.
+
+      web.finishAll(context)
+    })
+  }
+
+  configure (config) {
+    return super.configure(web.normalizeConfig(config))
   }
 }
 
