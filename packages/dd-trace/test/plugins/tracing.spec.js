6a908bc2e35c4818f71a135bb939ce78fccb70fc
rochdev <roch.devost@gmail.com>
2022-10-11 16:01:53 -0400

    fix lint errors

--- packages/dd-trace/test/plugins/tracing.spec.js
+++ packages/dd-trace/test/plugins/tracing.spec.js
@@ -1,22 +1,22 @@
-const TracingPlugin = require("../../src/plugins/tracing")
+const TracingPlugin = require('../../src/plugins/tracing')
 
-describe("TracingPlugin", () => {
-  describe("startSpan method", () => {
-    it("passes given childOf relationship to the tracer", () => {
+describe('TracingPlugin', () => {
+  describe('startSpan method', () => {
+    it('passes given childOf relationship to the tracer', () => {
       const startSpanSpy = sinon.spy()
       const plugin = new TracingPlugin({
         _tracer: {
-          startSpan: startSpanSpy,
-        },
+          startSpan: startSpanSpy
+        }
       })
       plugin.configure({})
 
-      plugin.startSpan("Test span", { childOf: "some parent span" })
+      plugin.startSpan('Test span', { childOf: 'some parent span' })
 
       expect(startSpanSpy).to.have.been.calledWith(
-        "Test span",
+        'Test span',
         sinon.match({
-          childOf: "some parent span",
+          childOf: 'some parent span'
         })
       )
     })
