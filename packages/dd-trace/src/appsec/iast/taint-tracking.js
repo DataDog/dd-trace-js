799281ce1043ce970c38c2f2e627d0b63c433cbd
Igor Unanua <igor.unanua@datadoghq.com>
2022-10-19 09:59:05 +0200

    Protect Rewriter constructor

--- packages/dd-trace/src/appsec/iast/taint-tracking.js
+++ packages/dd-trace/src/appsec/iast/taint-tracking.js
@@ -5,17 +5,32 @@ const IAST_TRANSACTION_ID = Symbol('_dd.iast.transactionId')
 const TaintedUtils = require('@datadog/native-iast-taint-tracking')
 const { Rewriter } = require('@datadog/native-iast-rewriter')
 
-const rewriter = new Rewriter();
+let rewriter;
+const getRewriter = function() {
+  if (!rewriter){
+    try {
+      rewriter = new Rewriter()
+    }catch(e){
+      // log exception?
+    }
+  }
+  return rewriter
+}
 
 const enableRewriter = function() {
-    shimmer.wrap(module.__proto__, '_compile', (compileMethod) => function(content, filename){
+  let rewriter = getRewriter();
+  if (rewriter){
+    shimmer.wrap(module.__proto__, '_compile', compileMethod => function(content, filename){
+        // TODO: filter node_modules & set prepareStackTrace
         try{
-            content = rewriter.rewrite(content, filename);
+          content = rewriter.rewrite(content, filename);
         }catch(e){
-            // log exception
+          // log exception
         }
         return compileMethod.apply(this, [content, filename]);
-    })
+      }
+    )
+  }
 }
 
 const disableRewriter = function() {
@@ -24,56 +39,56 @@ const disableRewriter = function() {
 
 const noop = function(res){return res}
 const TaintTrackingDummy = {
-    plusOperator: noop
+  plusOperator: noop
 }
 
 const TaintTracking = {
-    plusOperator: function(res, op1, op2){
-        try {
-            if (typeof res !== 'string' ||
-                (typeof op1 !== 'string' && typeof op2 !== 'string')){ return res; }
+  plusOperator: function(res, op1, op2){
+    try {
+      if (typeof res !== 'string' ||
+        (typeof op1 !== 'string' && typeof op2 !== 'string')){ return res; }
 
-            const store = storage.getStore()
-            const iastContext = iastContextFunctions.getIastContext(store)
-            const transactionId = iastContext && iastContext[IAST_TRANSACTION_ID]
-            if (transactionId){
-                res = TaintedUtils.concat(transactionId, res, op1, op2)
-            }
-        }catch(e){
-            // log exception?
-        }
-        return res
+      const store = storage.getStore()
+      const iastContext = iastContextFunctions.getIastContext(store)
+      const transactionId = iastContext && iastContext[IAST_TRANSACTION_ID]
+      if (transactionId){
+        res = TaintedUtils.concat(transactionId, res, op1, op2)
+      }
+    }catch(e){
+      // log exception?
     }
+    return res
+  }
 };
 
 const createTransaction = function(id, iastContext){
-    const transactionId = TaintedUtils.createTransaction(id)
-    iastContext[IAST_TRANSACTION_ID] = transactionId
+  const transactionId = TaintedUtils.createTransaction(id)
+  iastContext[IAST_TRANSACTION_ID] = transactionId
 }
 const removeTransaction = function(iastContext){
     if (iastContext[IAST_TRANSACTION_ID]){
-        const transactionId = iastContext[IAST_TRANSACTION_ID]
-        TaintedUtils.removeTransaction(transactionId)
+      const transactionId = iastContext[IAST_TRANSACTION_ID]
+      TaintedUtils.removeTransaction(transactionId)
     }
 }
 
 const enableTaintTracking = function(enable){
-    let success
-    if (enable && TaintedUtils) {
-        global._ddiast = global._ddiast || TaintTracking;
-        enableRewriter()
-        success = true
-    }
-    else {
-        disableRewriter()
-        global._ddiast = TaintTrackingDummy
-        success = false
-    }
-    return success
+  let success
+  if (enable && TaintedUtils) {
+    global._ddiast = global._ddiast || TaintTracking;
+    enableRewriter()
+    success = true
+  }
+  else {
+    disableRewriter()
+    global._ddiast = TaintTrackingDummy
+    success = false
+  }
+  return success
 }
 
 module.exports = {
-    createTransaction,
-    removeTransaction,
-    enableTaintTracking
+  createTransaction,
+  removeTransaction,
+  enableTaintTracking
 }
