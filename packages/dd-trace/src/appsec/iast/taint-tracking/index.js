4eaa0ddb433f1010ebca34790ae354bfe31f551e
Igor Unanua <igor.unanua@datadoghq.com>
2022-10-20 14:51:29 +0200

    Protect addon loading failures

--- packages/dd-trace/src/appsec/iast/taint-tracking/index.js
+++ packages/dd-trace/src/appsec/iast/taint-tracking/index.js
@@ -1,12 +1,20 @@
 'use strict'
 
 const Module = require('module')
-const { Rewriter } = require('@datadog/native-iast-rewriter')
-const TaintedUtils = require('@datadog/native-iast-taint-tracking')
-const iastContextFunctions = require('../iast-context')
+
 const shimmer = require('../../../../../datadog-shimmer')
 const { storage } = require('../../../../../datadog-core')
+const iastContextFunctions = require('../iast-context')
 const log = require('../../../log')
+
+let Rewriter
+let TaintedUtils
+try {
+  Rewriter = require('@datadog/native-iast-rewriter').Rewriter
+  TaintedUtils = require('@datadog/native-iast-taint-tracking')
+} catch(e) {
+  log.error(e)
+}
 const TaintTrackingFilter = require('./taint-tracking-filter')
 
 const IAST_TRANSACTION_ID = Symbol('_dd.iast.transactionId')
