42fee958e53aea41932d42037af55e7ba0e12cc3
crysmags <crystal.magloire@datadoghq.com>
2022-10-27 16:31:41 -0400

    createSQLInjectionComment function to be passed to plugins and create sql comment

--- packages/dd-trace/src/plugins/database.js
+++ packages/dd-trace/src/plugins/database.js
@@ -4,6 +4,17 @@ const StoragePlugin = require('./storage')
 
 class DatabasePlugin extends StoragePlugin {
   static get operation () { return 'query' }
+
+  createSQLInjectionComment = () =>  {
+
+    const dddbs = encodeURIComponent(this.config.service)
+    const dde = encodeURIComponent(this.tracer._env)
+    const ddps = encodeURIComponent(this.tracer._service)
+    const ddpv = encodeURIComponent(this.tracer._version)
+  
+    return `/*dddbs='${dddbs}',dde='${dde}',ddps='${ddps}',ddpv='${ddpv}'*/ `
+  
+  }
 }
 
 module.exports = DatabasePlugin
