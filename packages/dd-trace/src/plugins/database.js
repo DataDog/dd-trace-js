48dfa77ec51bda5beea1578b3dcc7b03c06c05de
crysmags <crystal.magloire@datadoghq.com>
2022-10-27 16:25:57 -0400

    APM DBM Link for Postgres
    parent 2c24cf896bded18ad4c7629b8509c04b8d2f092b
    author crysmags <crystal.magloire@datadoghq.com> 1666902357 -0400
    committer Thomas Hunter II <tlhunter@datadog.com> 1669236233 -0800
    
    added sqlInjectionMode to the tracer config
    
    createSQLInjectionComment function to be passed to plugins and create sql comment
    
    adding sqlInjectionMode to be shared between tracer and plugin configs
    
    plugins calls createSQLInjectionComment when sqlInjectionMode is set to service
    
    passing pgQuery to the publish call so we can update the query with the sql comment
    
    testing sql comment injection
    
    plugins calls createSQLInjectionComment when sqlInjectionMode is set
    
    testing sql comment injection
    
    updating API docs to include sqlCommentInjection
    
    updating API docs to include sqlCommentInjection when set to service
    
    adding in dbmPropagationMode to the docs
    
    update function that creates tag injection comment
    
    formatting dbm propagation tests
    
    updating to include dbmPropagationMode configurations
    
    updating to include dbmPropagationMode to be shared between plugin and tracer
    
    updating function name to dbm propagation
    
    updating dbm propagation testing
    
    formatting dbm propagation tests
    
    pushing conditional logic up to db class
    
    adding logic to propagation to default back to service and log to debug
    
    added a function that will check the propagation setting and add the comment
    
    updating docs to default to disabled
    
    updating config to default to disabled
    
    reassigning pgQuery to query in params
    
    updating the debug log to have... well.. information
    
    updating pg plugin to use query instead of pgQuery
    
    updating query param
    
    testing full propagation with tracer configs
    
    updating injectDBMquery to include full propagation
    
    adding full propagation mode to docs
    
    changing test summaries to propagation from injection
    
    adding back in blank line
    
    removing debug log block for propagation
    
    removing logger
    
    adding logic to avoid additional runs of encoding the tags
    
    adding logic to avoid additional runs of encoding the tags
    
    creating buildTraceparent function instead of calling TextMapPropagator
    
    resolving local conflict
    
    adding a propagation help to clean up code
    
    removing line to ensure no merge conflict
    
    adding test for injecting dbm tag
    
    adding logic to inject dbm tag
    
    adding env tag to agent
    
    commit
    
    updating injectDbmQuery
    
    updating for cleaner code in createDBMPropagationCommentService
    
    updating database class
    
    updating datadbase class

--- packages/dd-trace/src/plugins/database.js
+++ packages/dd-trace/src/plugins/database.js
@@ -4,6 +4,62 @@ const StoragePlugin = require('./storage')
 
 class DatabasePlugin extends StoragePlugin {
   static get operation () { return 'query' }
+
+  constructor (...args) {
+    super(...args)
+    this.serviceTags = {
+      dddbs: '',
+      encodedDddbs: '',
+      dde: '',
+      encodedDde: '',
+      ddps: '',
+      encodedDdps: '',
+      ddpv: '',
+      encodedDdpv: ''
+    }
+  }
+  encodingServiceTags (serviceTag, encodeATag, spanConfig) {
+    if (serviceTag !== spanConfig) {
+      this.serviceTags[serviceTag] = spanConfig
+      this.serviceTags[encodeATag] = encodeURIComponent(spanConfig)
+    }
+  }
+
+  createDBMPropagationCommentService () {
+    this.encodingServiceTags('dddbs', 'encodedDddbs', this.config.service)
+    this.encodingServiceTags('dde', 'encodedDde', this.tracer._env)
+    this.encodingServiceTags('ddps', 'encodedDdps', this.tracer._service)
+    this.encodingServiceTags('ddpv', 'encodedDdpv', this.tracer._version)
+
+    const { encodedDddbs, encodedDde, encodedDdps, encodedDdpv } = this.serviceTags
+
+    return `dddbs='${encodedDddbs}',dde='${encodedDde}',` +
+    `ddps='${encodedDdps}',ddpv='${encodedDdpv}'`
+  }
+
+  // TODO create the buildTraceparent as a helper function function possibly in trace
+  // agent core to be used by both Db plugins and TextMapPropagator
+  buildTraceparent () {
+    const span = this.activeSpan._spanContext
+    const sampling = span._sampling.priority > 0 ? '01' : '00'
+    const traceId = span._traceId.toString(16).padStart(32, '0')
+    const spanId = span._spanId.toString(16).padStart(16, '0')
+    return `01-${traceId}-${spanId}-${sampling}`
+  }
+
+  injectDbmQuery (query) {
+    if (this.config.dbmPropagationMode === 'disabled') {
+      return query
+    }
+    const servicePropagation = this.createDBMPropagationCommentService()
+    if (this.config.dbmPropagationMode === 'service') {
+      return `/*${servicePropagation}*/ ${query}`
+    } else if (this.config.dbmPropagationMode === 'full') {
+      this.activeSpan.setTag('_dd.dbm_trace_injected', 'true')
+      const traceparent = this.buildTraceparent()
+      return `/*${servicePropagation},traceparent='${traceparent}'*/ ${query}`
+    }
+  }
 }
 
 module.exports = DatabasePlugin
