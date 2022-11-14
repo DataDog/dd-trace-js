3b269d4ad798969f4fb10c86ea85c2829c7a6293
Juan Fernandez <juan.fernandezdealba@datadoghq.com>
2022-11-14 08:25:35 +0100

    enable git upload if itr is enabled

--- packages/dd-trace/src/proxy.js
+++ packages/dd-trace/src/proxy.js
@@ -58,7 +58,7 @@ class Tracer extends NoopProxy {
         telemetry.start(config, this._pluginManager)
       }
 
-      if (config.isGitUploadEnabled) {
+      if (config.isGitUploadEnabled || config.isIntelligentTestRunnerEnabled) {
         sendGitMetadata(config.site, (err) => {
           if (err) {
             log.error(`Error uploading git metadata: ${err}`)
