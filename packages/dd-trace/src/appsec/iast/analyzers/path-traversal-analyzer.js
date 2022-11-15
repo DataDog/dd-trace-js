2b6ee66bd43beca48305be79377b5b03896f6a97
Julio Gonzalez <julio.gonzalez@datadoghq.com>
2022-11-15 12:46:35 +0100

    Fix tests.

--- packages/dd-trace/src/appsec/iast/analyzers/path-traversal-analyzer.js
+++ packages/dd-trace/src/appsec/iast/analyzers/path-traversal-analyzer.js
@@ -6,17 +6,31 @@ class PathTraversalAnalyzer extends Analyzer {
   constructor () {
     super('PATH_TRAVERSAL')
     this.addSub('datadog:fs:access', path => this.analyze(path))
+    this.evidence = ''
   }
 
-  _isVulnerable (path, ctx) {
-    if (typeof path === 'string') {
-      return isTainted(ctx, path)
-    } else {
-      return false
+  _isVulnerable (pathArray, ctx) {
+    let ret = false
+    if (typeof pathArray !== 'object' || pathArray.constructor !== Array) {
+      return ret
     }
+
+    for (const value of pathArray) {
+      if (typeof value === 'string') {
+        ret = isTainted(ctx, value)
+        if (ret) {
+          // Return the first vulnerable argument
+          this.evidence = value
+          break
+        }
+      }
+    }
+
+    return ret
   }
 
-  _getEvidence (value, ctx) {
+  _getEvidence (ctx) {
+    const value = this.evidence
     const ranges = getRanges(ctx, value)
     return { value, ranges }
   }
