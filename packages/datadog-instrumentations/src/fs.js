e38bc08a01469abcf98daa36f40556aa4a18ea8b
Julio Gonzalez <julio.gonzalez@datadoghq.com>
2022-11-15 12:45:10 +0100

    Tidy fs instrumentations.

--- packages/datadog-instrumentations/src/fs.js
+++ packages/datadog-instrumentations/src/fs.js
@@ -5,20 +5,35 @@ const {
   addHook
 } = require('./helpers/instrument')
 const shimmer = require('../../datadog-shimmer')
+
 const fsCahnnel = channel('datadog:fs:access')
-const fsMethods = ['openSync']
+const onePathMethods = ['access', 'appendFile', 'chmod', 'lchown', 'mkdir', 'mkdtemp', 'mkdtempSync', 'open',
+  'openSync', 'opendir', 'readdir', 'readFile', 'readlink', 'realpath', 'rm', 'rmdir', 'stat', 'truncate', 'unlink',
+  'utimes', 'writeFile', 'watch']
+const onePathMethodsSync = ['accessSync', 'appendFileSync', 'chmodSync', 'chownSync', 'lchownSync', 'mkdirSync',
+  'opendirSync', 'readdirSync', 'readFileSync', 'readlinkSync', 'realpathSync', 'rmSync', 'rmdirSync', 'statSync',
+  'truncateSync', 'unlinkSync', 'utimesSync', 'writeFileSync']
+const twoPathMethods = ['copyFile', 'link', 'rename', 'symlink']
+const twoPathMethodsSync = ['copyFileSync', 'linkSync', 'renameSync', 'symlinkSync']
 
 addHook({ name: 'fs' }, fs => {
-  shimmer.massWrap(fs, fsMethods, wrapFsMethod(fsCahnnel))
+  shimmer.massWrap(fs, onePathMethods.concat(onePathMethodsSync), wrapFsMethod(fsCahnnel, 1))
+  shimmer.massWrap(fs, twoPathMethods.concat(twoPathMethodsSync), wrapFsMethod(fsCahnnel, 2))
+
+  shimmer.massWrap(fs.promises, onePathMethods, wrapFsMethod(fsCahnnel, 1))
+  shimmer.massWrap(fs.promises, twoPathMethods, wrapFsMethod(fsCahnnel, 2))
   return fs
 })
 
-function wrapFsMethod (channel) {
+function wrapFsMethod (channel, numParams) {
   function wrapMethod (fsMethod) {
     return function () {
       if (channel.hasSubscribers && arguments.length) {
-        // TODO(julio): check if arg 0 is String
-        channel.publish(arguments[0])
+        const args = []
+        for (let i = 0; i < numParams; i++) {
+          args.push(arguments[i])
+        }
+        channel.publish(args)
       }
       return fsMethod.apply(this, arguments)
     }
