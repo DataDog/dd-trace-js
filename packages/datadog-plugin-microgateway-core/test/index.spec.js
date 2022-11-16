6de7252cbb3818eabece497778efddba9ea60616
wconti27 <william.conti@datadoghq.com>
2022-11-16 09:12:35 -0500

    Address Reviewer Comments

--- packages/datadog-plugin-microgateway-core/test/index.spec.js
+++ packages/datadog-plugin-microgateway-core/test/index.spec.js
@@ -77,10 +77,10 @@ describe('Plugin', () => {
             .use(traces => {
               const spans = traces[0]
 
-              expect(spans[0]).to.have.property('name', 'web.request')
+              expect(spans[0]).to.have.property('name', 'microgateway.request')
               expect(spans[0]).to.have.property('service', 'test')
               expect(spans[0]).to.have.property('type', 'web')
-              expect(spans[0]).to.have.property('resource', 'GET')
+              expect(spans[0]).to.have.property('resource', 'GET /v1')
               expect(spans[0].meta).to.have.property('span.kind', 'server')
               expect(spans[0].meta).to.have.property('http.url', `http://localhost:${gatewayPort}/v1/foo`)
               expect(spans[0].meta).to.have.property('http.method', 'GET')
--- packages/dd-trace/src/plugins/tracing.js
+++ packages/dd-trace/src/plugins/tracing.js
@@ -82,7 +82,7 @@ class TracingPlugin extends Plugin {
       }
     })
 
-    if (span && this.component) { span.setTag(COMPONENT, this.component) }
+    span.setTag(COMPONENT, this.component)
 
     analyticsSampler.sample(span, this.config.measured)
 
