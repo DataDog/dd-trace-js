b0283b8d63b919e3f13e5aaaa3dfe501413c8130
rochdev <roch.devost@gmail.com>
2022-11-22 16:37:19 -0500

    comment out entire test instead of just line calling it

--- packages/dd-trace/test/appsec/iast/overhead-controller.spec.js
+++ packages/dd-trace/test/appsec/iast/overhead-controller.spec.js
@@ -1,15 +1,15 @@
 'use strict'
 
 const overheadController = require('../../../src/appsec/iast/overhead-controller')
-const vulnerabilityReporter = require('../../../src/appsec/iast/vulnerability-reporter')
+// const vulnerabilityReporter = require('../../../src/appsec/iast/vulnerability-reporter')
 const DatadogSpanContext = require('../../../src/opentracing/span_context')
 const Config = require('../../../src/config')
 const id = require('../../../src/id')
-const iast = require('../../../src/appsec/iast')
-const { testInRequest } = require('./utils')
-const agent = require('../../plugins/agent')
-const axios = require('axios')
-const { EventEmitter } = require('events')
+// const iast = require('../../../src/appsec/iast')
+// const { testInRequest } = require('./utils')
+// const agent = require('../../plugins/agent')
+// const axios = require('axios')
+// const { EventEmitter } = require('events')
 
 describe('Overhead controller', () => {
   const oceContextKey = overheadController.OVERHEAD_CONTROLLER_CONTEXT_KEY
@@ -180,231 +180,231 @@ describe('Overhead controller', () => {
   })
   describe('full feature', () => {
     describe('multiple request at same time', () => {
-      const TEST_REQUEST_STARTED = 'test-request-started'
-      const TEST_REQUEST_FINISHED = 'test-request-finished'
+      // const TEST_REQUEST_STARTED = 'test-request-started'
+      // const TEST_REQUEST_FINISHED = 'test-request-finished'
 
-      const FIRST_REQUEST = '/first'
-      const SECOND_REQUEST = '/second'
-      const THIRD_REQUEST = '/third'
-      const FOURTH_REQUEST = '/fourth'
-      const FIFTH_REQUEST = '/fifth'
+      // const FIRST_REQUEST = '/first'
+      // const SECOND_REQUEST = '/second'
+      // const THIRD_REQUEST = '/third'
+      // const FOURTH_REQUEST = '/fourth'
+      // const FIFTH_REQUEST = '/fifth'
 
-      const testRequestEventEmitter = new EventEmitter()
-      let requestResolvers = {}
+      // const testRequestEventEmitter = new EventEmitter()
+      // let requestResolvers = {}
 
-      function app (req) {
-        return new Promise((resolve) => {
-          const crypto = require('crypto')
-          crypto.createHash('sha1')
-          requestResolvers[req.url] = () => {
-            resolve()
-            testRequestEventEmitter.emit(TEST_REQUEST_FINISHED, req.url)
-          }
-          testRequestEventEmitter.emit(TEST_REQUEST_STARTED, req.url)
-        })
-      }
+      // function app (req) {
+      //   return new Promise((resolve) => {
+      //     const crypto = require('crypto')
+      //     crypto.createHash('sha1')
+      //     requestResolvers[req.url] = () => {
+      //       resolve()
+      //       testRequestEventEmitter.emit(TEST_REQUEST_FINISHED, req.url)
+      //     }
+      //     testRequestEventEmitter.emit(TEST_REQUEST_STARTED, req.url)
+      //   })
+      // }
 
-      function tests (serverConfig) {
-        const handlers = []
+      // function tests (serverConfig) {
+      //   const handlers = []
 
-        beforeEach(() => {
-          testRequestEventEmitter
-            .removeAllListeners(TEST_REQUEST_STARTED)
-            .removeAllListeners(TEST_REQUEST_FINISHED)
-          requestResolvers = {}
-        })
+      //   beforeEach(() => {
+      //     testRequestEventEmitter
+      //       .removeAllListeners(TEST_REQUEST_STARTED)
+      //       .removeAllListeners(TEST_REQUEST_FINISHED)
+      //     requestResolvers = {}
+      //   })
 
-        afterEach(() => {
-          handlers.forEach(agent.unsubscribe)
-          handlers.splice(0)
-        })
+      //   afterEach(() => {
+      //     handlers.forEach(agent.unsubscribe)
+      //     handlers.splice(0)
+      //   })
 
-        afterEach(() => {
-          vulnerabilityReporter.clearCache()
-          iast.disable()
-        })
+      //   afterEach(() => {
+      //     vulnerabilityReporter.clearCache()
+      //     iast.disable()
+      //   })
 
-        it('should detect vulnerabilities only in one if max concurrent is 1', (done) => {
-          const config = new Config({
-            experimental: {
-              iast: {
-                enabled: true,
-                requestSampling: 100,
-                maxConcurrentRequests: 1
-              }
-            }
-          })
-          iast.enable(config)
-          let urlCounter = 0
-          const handler = function (traces) {
-            try {
-              for (let i = 0; i < traces.length; i++) {
-                for (let j = 0; j < traces[i].length; j++) {
-                  const trace = traces[i][j]
-                  if (trace.type === 'web') {
-                    const url = trace.meta['http.url']
-                    if (url.includes(FIRST_REQUEST)) {
-                      expect(trace.meta['_dd.iast.json']).not.to.be.undefined
-                      urlCounter++
-                    } else if (url.includes(SECOND_REQUEST)) {
-                      expect(trace.meta['_dd.iast.json']).to.be.undefined
-                      urlCounter++
-                    }
-                    if (urlCounter === 2) {
-                      done()
-                    }
-                  }
-                }
-              }
-            } catch (e) {
-              agent.unsubscribe(handler)
-              done(e)
-            }
-          }
-          handlers.push(handler)
-          agent.subscribe(handler)
-          testRequestEventEmitter.on(TEST_REQUEST_STARTED, (url) => {
-            if (url === FIRST_REQUEST) {
-              axios.get(`http://localhost:${serverConfig.port}${SECOND_REQUEST}`).then().catch(done)
-            } else if (url === SECOND_REQUEST) {
-              requestResolvers[FIRST_REQUEST]()
-              requestResolvers[SECOND_REQUEST]()
-            }
-          })
-          axios.get(`http://localhost:${serverConfig.port}${FIRST_REQUEST}`).then().catch(done)
-        })
+      //   it('should detect vulnerabilities only in one if max concurrent is 1', (done) => {
+      //     const config = new Config({
+      //       experimental: {
+      //         iast: {
+      //           enabled: true,
+      //           requestSampling: 100,
+      //           maxConcurrentRequests: 1
+      //         }
+      //       }
+      //     })
+      //     iast.enable(config)
+      //     let urlCounter = 0
+      //     const handler = function (traces) {
+      //       try {
+      //         for (let i = 0; i < traces.length; i++) {
+      //           for (let j = 0; j < traces[i].length; j++) {
+      //             const trace = traces[i][j]
+      //             if (trace.type === 'web') {
+      //               const url = trace.meta['http.url']
+      //               if (url.includes(FIRST_REQUEST)) {
+      //                 expect(trace.meta['_dd.iast.json']).not.to.be.undefined
+      //                 urlCounter++
+      //               } else if (url.includes(SECOND_REQUEST)) {
+      //                 expect(trace.meta['_dd.iast.json']).to.be.undefined
+      //                 urlCounter++
+      //               }
+      //               if (urlCounter === 2) {
+      //                 done()
+      //               }
+      //             }
+      //           }
+      //         }
+      //       } catch (e) {
+      //         agent.unsubscribe(handler)
+      //         done(e)
+      //       }
+      //     }
+      //     handlers.push(handler)
+      //     agent.subscribe(handler)
+      //     testRequestEventEmitter.on(TEST_REQUEST_STARTED, (url) => {
+      //       if (url === FIRST_REQUEST) {
+      //         axios.get(`http://localhost:${serverConfig.port}${SECOND_REQUEST}`).then().catch(done)
+      //       } else if (url === SECOND_REQUEST) {
+      //         requestResolvers[FIRST_REQUEST]()
+      //         requestResolvers[SECOND_REQUEST]()
+      //       }
+      //     })
+      //     axios.get(`http://localhost:${serverConfig.port}${FIRST_REQUEST}`).then().catch(done)
+      //   })
 
-        it('should detect vulnerabilities in both if max concurrent is 2', (done) => {
-          const config = new Config({
-            experimental: {
-              iast: {
-                enabled: true,
-                requestSampling: 100,
-                maxConcurrentRequests: 2
-              }
-            }
-          })
-          iast.enable(config)
-          let urlCounter = 0
-          const handler = function (traces) {
-            try {
-              for (let i = 0; i < traces.length; i++) {
-                for (let j = 0; j < traces[i].length; j++) {
-                  const trace = traces[i][j]
-                  if (trace.type === 'web') {
-                    urlCounter++
-                    expect(trace.meta['_dd.iast.json']).not.to.be.undefined
-                    if (urlCounter === 2) {
-                      done()
-                    }
-                  }
-                }
-              }
-            } catch (e) {
-              agent.unsubscribe(handler)
-              done(e)
-            }
-          }
-          handlers.push(handler)
-          agent.subscribe(handler)
-          testRequestEventEmitter.on(TEST_REQUEST_STARTED, (url) => {
-            if (url === FIRST_REQUEST) {
-              axios.get(`http://localhost:${serverConfig.port}${SECOND_REQUEST}`).then().catch(done)
-            } else if (url === SECOND_REQUEST) {
-              setImmediate(() => {
-                requestResolvers[FIRST_REQUEST]()
-                vulnerabilityReporter.clearCache()
-              })
-            }
-          })
-          testRequestEventEmitter.on(TEST_REQUEST_FINISHED, (url) => {
-            if (url === FIRST_REQUEST) {
-              setImmediate(() => {
-                requestResolvers[SECOND_REQUEST]()
-                vulnerabilityReporter.clearCache()
-              })
-            }
-          })
-          axios.get(`http://localhost:${serverConfig.port}${FIRST_REQUEST}`).then().catch(done)
-        })
+      //   it('should detect vulnerabilities in both if max concurrent is 2', (done) => {
+      //     const config = new Config({
+      //       experimental: {
+      //         iast: {
+      //           enabled: true,
+      //           requestSampling: 100,
+      //           maxConcurrentRequests: 2
+      //         }
+      //       }
+      //     })
+      //     iast.enable(config)
+      //     let urlCounter = 0
+      //     const handler = function (traces) {
+      //       try {
+      //         for (let i = 0; i < traces.length; i++) {
+      //           for (let j = 0; j < traces[i].length; j++) {
+      //             const trace = traces[i][j]
+      //             if (trace.type === 'web') {
+      //               urlCounter++
+      //               expect(trace.meta['_dd.iast.json']).not.to.be.undefined
+      //               if (urlCounter === 2) {
+      //                 done()
+      //               }
+      //             }
+      //           }
+      //         }
+      //       } catch (e) {
+      //         agent.unsubscribe(handler)
+      //         done(e)
+      //       }
+      //     }
+      //     handlers.push(handler)
+      //     agent.subscribe(handler)
+      //     testRequestEventEmitter.on(TEST_REQUEST_STARTED, (url) => {
+      //       if (url === FIRST_REQUEST) {
+      //         axios.get(`http://localhost:${serverConfig.port}${SECOND_REQUEST}`).then().catch(done)
+      //       } else if (url === SECOND_REQUEST) {
+      //         setImmediate(() => {
+      //           requestResolvers[FIRST_REQUEST]()
+      //           vulnerabilityReporter.clearCache()
+      //         })
+      //       }
+      //     })
+      //     testRequestEventEmitter.on(TEST_REQUEST_FINISHED, (url) => {
+      //       if (url === FIRST_REQUEST) {
+      //         setImmediate(() => {
+      //           requestResolvers[SECOND_REQUEST]()
+      //           vulnerabilityReporter.clearCache()
+      //         })
+      //       }
+      //     })
+      //     axios.get(`http://localhost:${serverConfig.port}${FIRST_REQUEST}`).then().catch(done)
+      //   })
 
-        it('should recovery requests budget', function (done) {
-          // 3 in parallel => 2 detects - 1 not detects
-          // on finish the first => launch 2 - should detect 1 more
-          const config = new Config({
-            experimental: {
-              iast: {
-                enabled: true,
-                requestSampling: 100,
-                maxConcurrentRequests: 2
-              }
-            }
-          })
-          iast.enable(config)
+      //   it('should recovery requests budget', function (done) {
+      //     // 3 in parallel => 2 detects - 1 not detects
+      //     // on finish the first => launch 2 - should detect 1 more
+      //     const config = new Config({
+      //       experimental: {
+      //         iast: {
+      //           enabled: true,
+      //           requestSampling: 100,
+      //           maxConcurrentRequests: 2
+      //         }
+      //       }
+      //     })
+      //     iast.enable(config)
 
-          let counter = 0
-          const handler = function (traces) {
-            try {
-              for (let i = 0; i < traces.length; i++) {
-                for (let j = 0; j < traces[i].length; j++) {
-                  const trace = traces[i][j]
-                  if (trace.type === 'web') {
-                    counter++
-                    const url = trace.meta['http.url']
-                    if (url.includes(FIRST_REQUEST)) {
-                      expect(trace.meta['_dd.iast.json']).not.to.be.undefined
-                    } else if (url.includes(SECOND_REQUEST)) {
-                      expect(trace.meta['_dd.iast.json']).not.to.be.undefined
-                    } else if (url.includes(THIRD_REQUEST)) {
-                      expect(trace.meta['_dd.iast.json']).to.be.undefined
-                    } else if (url.includes(FOURTH_REQUEST)) {
-                      expect(trace.meta['_dd.iast.json']).not.to.be.undefined
-                    } else if (url.includes(FIFTH_REQUEST)) {
-                      expect(trace.meta['_dd.iast.json']).to.be.undefined
-                    }
-                    counter === 5 && done()
-                  }
-                }
-              }
-            } catch (e) {
-              agent.unsubscribe(handler)
-              done(e)
-            }
-          }
-          handlers.push(handler)
-          agent.subscribe(handler)
+      //     let counter = 0
+      //     const handler = function (traces) {
+      //       try {
+      //         for (let i = 0; i < traces.length; i++) {
+      //           for (let j = 0; j < traces[i].length; j++) {
+      //             const trace = traces[i][j]
+      //             if (trace.type === 'web') {
+      //               counter++
+      //               const url = trace.meta['http.url']
+      //               if (url.includes(FIRST_REQUEST)) {
+      //                 expect(trace.meta['_dd.iast.json']).not.to.be.undefined
+      //               } else if (url.includes(SECOND_REQUEST)) {
+      //                 expect(trace.meta['_dd.iast.json']).not.to.be.undefined
+      //               } else if (url.includes(THIRD_REQUEST)) {
+      //                 expect(trace.meta['_dd.iast.json']).to.be.undefined
+      //               } else if (url.includes(FOURTH_REQUEST)) {
+      //                 expect(trace.meta['_dd.iast.json']).not.to.be.undefined
+      //               } else if (url.includes(FIFTH_REQUEST)) {
+      //                 expect(trace.meta['_dd.iast.json']).to.be.undefined
+      //               }
+      //               counter === 5 && done()
+      //             }
+      //           }
+      //         }
+      //       } catch (e) {
+      //         agent.unsubscribe(handler)
+      //         done(e)
+      //       }
+      //     }
+      //     handlers.push(handler)
+      //     agent.subscribe(handler)
 
-          testRequestEventEmitter.on(TEST_REQUEST_STARTED, (url) => {
-            if (url === FIRST_REQUEST) {
-              axios.get(`http://localhost:${serverConfig.port}${SECOND_REQUEST}`).then().catch(done)
-            } else if (url === SECOND_REQUEST) {
-              axios.get(`http://localhost:${serverConfig.port}${THIRD_REQUEST}`).then().catch(done)
-            } else if (url === THIRD_REQUEST) {
-              requestResolvers[FIRST_REQUEST]()
-            } else if (url === FIFTH_REQUEST) {
-              requestResolvers[SECOND_REQUEST]()
-              vulnerabilityReporter.clearCache()
-            }
-          })
-          testRequestEventEmitter.on(TEST_REQUEST_FINISHED, (url) => {
-            if (url === FIRST_REQUEST) {
-              axios.get(`http://localhost:${serverConfig.port}${FOURTH_REQUEST}`).then().catch(done)
-              axios.get(`http://localhost:${serverConfig.port}${FIFTH_REQUEST}`).then().catch(done)
-            } else if (url === SECOND_REQUEST) {
-              setImmediate(() => {
-                vulnerabilityReporter.clearCache()
-                requestResolvers[THIRD_REQUEST]()
-                requestResolvers[FOURTH_REQUEST]()
-                requestResolvers[FIFTH_REQUEST]()
-              })
-            }
-          })
+      //     testRequestEventEmitter.on(TEST_REQUEST_STARTED, (url) => {
+      //       if (url === FIRST_REQUEST) {
+      //         axios.get(`http://localhost:${serverConfig.port}${SECOND_REQUEST}`).then().catch(done)
+      //       } else if (url === SECOND_REQUEST) {
+      //         axios.get(`http://localhost:${serverConfig.port}${THIRD_REQUEST}`).then().catch(done)
+      //       } else if (url === THIRD_REQUEST) {
+      //         requestResolvers[FIRST_REQUEST]()
+      //       } else if (url === FIFTH_REQUEST) {
+      //         requestResolvers[SECOND_REQUEST]()
+      //         vulnerabilityReporter.clearCache()
+      //       }
+      //     })
+      //     testRequestEventEmitter.on(TEST_REQUEST_FINISHED, (url) => {
+      //       if (url === FIRST_REQUEST) {
+      //         axios.get(`http://localhost:${serverConfig.port}${FOURTH_REQUEST}`).then().catch(done)
+      //         axios.get(`http://localhost:${serverConfig.port}${FIFTH_REQUEST}`).then().catch(done)
+      //       } else if (url === SECOND_REQUEST) {
+      //         setImmediate(() => {
+      //           vulnerabilityReporter.clearCache()
+      //           requestResolvers[THIRD_REQUEST]()
+      //           requestResolvers[FOURTH_REQUEST]()
+      //           requestResolvers[FIFTH_REQUEST]()
+      //         })
+      //       }
+      //     })
 
-          axios.get(`http://localhost:${serverConfig.port}${FIRST_REQUEST}`).then().catch(done)
-        })
-      }
+      //     axios.get(`http://localhost:${serverConfig.port}${FIRST_REQUEST}`).then().catch(done)
+      //   })
+      // }
 
-      () => testInRequest(app, tests) // TODO: re-enable when fixed with tap
+      // testInRequest(app, tests) // TODO: re-enable when fixed with tap
     })
   })
 })
