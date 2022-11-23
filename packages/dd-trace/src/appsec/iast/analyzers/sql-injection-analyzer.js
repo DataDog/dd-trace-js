c2a539a7a817c0322ef8fd8309fd44af7cc0a99c
Ugaitz Urien <ugaitz.urien@datadoghq.com>
2022-11-23 09:15:39 +0100

    Add command injection vulnerability detection (#2540)

--- packages/dd-trace/src/appsec/iast/analyzers/sql-injection-analyzer.js
+++ packages/dd-trace/src/appsec/iast/analyzers/sql-injection-analyzer.js
@@ -1,25 +1,12 @@
 'use strict'
-const Analyzer = require('./vulnerability-analyzer')
-const { isTainted, getRanges } = require('../taint-tracking/operations')
+const InjectionAnalyzer = require('./injection-analyzer')
 
-class SqlInjectionAnalyzer extends Analyzer {
+class SqlInjectionAnalyzer extends InjectionAnalyzer {
   constructor () {
     super('SQL_INJECTION')
     this.addSub('apm:mysql:query:start', ({ sql }) => this.analyze(sql))
     this.addSub('apm:mysql2:query:start', ({ sql }) => this.analyze(sql))
     this.addSub('apm:pg:query:start', ({ statement }) => this.analyze(statement))
-  }
-
-  _isVulnerable (query, iastContext) {
-    if (query) {
-      return isTainted(iastContext, query)
-    }
-    return false
-  }
-
-  _getEvidence (value, iastContext) {
-    const ranges = getRanges(iastContext, value)
-    return { value, ranges }
   }
 }
 
