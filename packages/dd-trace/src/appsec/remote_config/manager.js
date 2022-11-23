ced3cee7a52fbb942f185398c81a485bd1256ec3
simon-id <simon.id@datadoghq.com>
2022-11-23 10:07:20 +0100

    Remote config client + AppSec remote activation (#2401)
    * update state format
    
    * fill products automatically
    
    * update path regex
    
    * add client capabilities
    
    * update scheduler
    
    * new capabilities syntax
    
    * correct capabilities values
    
    * appsec activation
    
    * move gateway clearing
    
    * make a difference between false and undefined in appsec config
    
    * truth table for local and remote config
    
    * protect against double enable
    
    * rename FEATURES to ASM_FEATURES
    
    * only activate remote config when appsec not locally configured
    
    * move scheduler.js
    
    * complete the state management
    
    * add more request options
    
    * automatic start and stop
    
    * simplify the caching mecanism
    
    * move remote config to appsec folder
    
    * update scheduler to use timeouts instead of intervals

---
+++ packages/dd-trace/src/appsec/remote_config/manager.js
@@ -0,0 +1,264 @@
+'use strict'
+
+const uuid = require('crypto-randomuuid')
+const { EventEmitter } = require('events')
+const Scheduler = require('./scheduler')
+const tracerVersion = require('../../../../../package.json').version
+const request = require('../../exporters/common/request')
+const log = require('../../log')
+
+const clientId = uuid()
+
+const POLL_INTERVAL = 5e3
+const DEFAULT_CAPABILITY = Buffer.alloc(1).toString('base64') // 0x00
+
+// There MUST NOT exist separate instances of RC clients in a tracer making separate ClientGetConfigsRequest
+// with their own separated Client.ClientState.
+class RemoteConfigManager extends EventEmitter {
+  constructor (config) {
+    super()
+
+    this.scheduler = new Scheduler((cb) => this.poll(cb), POLL_INTERVAL)
+
+    this.requestOptions = {
+      url: config.url,
+      hostname: config.hostname,
+      port: config.port,
+      method: 'POST',
+      path: '/v0.7/config'
+    }
+
+    this.state = {
+      client: {
+        state: { // updated by `parseConfig()`
+          root_version: 1,
+          targets_version: 0,
+          config_states: [],
+          has_error: false,
+          error: '',
+          backend_client_state: ''
+        },
+        id: clientId,
+        products: [], // updated by `updateProducts()`
+        is_tracer: true,
+        client_tracer: {
+          runtime_id: config.tags['runtime-id'],
+          language: 'node',
+          tracer_version: tracerVersion,
+          service: config.service,
+          env: config.env,
+          app_version: config.version
+        },
+        capabilities: DEFAULT_CAPABILITY // updated by `updateCapabilities()`
+      },
+      cached_target_files: [] // updated by `parseConfig()`
+    }
+
+    this.appliedConfigs = new Map()
+  }
+
+  updateCapabilities (mask, value) {
+    const hex = Buffer.from(this.state.client.capabilities, 'base64').toString('hex')
+
+    let num = BigInt(`0x${hex}`)
+
+    if (value) {
+      num |= mask
+    } else {
+      num &= ~mask
+    }
+
+    let str = num.toString(16)
+
+    if (str.length % 2) str = `0${str}`
+
+    this.state.client.capabilities = Buffer.from(str, 'hex').toString('base64')
+  }
+
+  on (event, listener) {
+    super.on(event, listener)
+
+    this.state.client.products = this.eventNames()
+
+    this.scheduler.start()
+
+    return this
+  }
+
+  off (event, listener) {
+    super.off(event, listener)
+
+    this.state.client.products = this.eventNames()
+
+    if (!this.state.client.products.length) {
+      this.scheduler.stop()
+    }
+
+    return this
+  }
+
+  poll (cb) {
+    request(JSON.stringify(this.state), this.requestOptions, (err, data, statusCode) => {
+      // 404 means RC is disabled, ignore it
+      if (statusCode === 404) return cb()
+
+      if (err) {
+        log.error(err)
+        return cb()
+      }
+
+      // if error was just sent, reset the state
+      if (this.state.client.state.has_error) {
+        this.state.client.state.has_error = false
+        this.state.client.state.error = ''
+      }
+
+      if (data && data !== '{}') { // '{}' means the tracer is up to date
+        try {
+          this.parseConfig(JSON.parse(data))
+        } catch (err) {
+          log.error(`Could not parse remote config response: ${err}`)
+
+          this.state.client.state.has_error = true
+          this.state.client.state.error = err.toString()
+        }
+      }
+
+      cb()
+    })
+  }
+
+  // `client_configs` is the list of config paths to have applied
+  // `targets` is the signed index with metadata for config files
+  // `target_files` is the list of config files containing the actual config data
+  parseConfig ({
+    client_configs: clientConfigs = [],
+    targets,
+    target_files: targetFiles = []
+  }) {
+    const toUnapply = []
+    const toApply = []
+    const toModify = []
+
+    for (const appliedConfig of this.appliedConfigs.values()) {
+      if (!clientConfigs.includes(appliedConfig.path)) {
+        toUnapply.push(appliedConfig)
+      }
+    }
+
+    targets = fromBase64JSON(targets)
+
+    if (targets) {
+      for (const path of clientConfigs) {
+        const meta = targets.signed.targets[path]
+        if (!meta) throw new Error(`Unable to find target for path ${path}`)
+
+        const current = this.appliedConfigs.get(path)
+
+        const newConf = {}
+
+        if (current) {
+          if (current.hashes.sha256 === meta.hashes.sha256) continue
+
+          toModify.push(newConf)
+        } else {
+          toApply.push(newConf)
+        }
+
+        const file = targetFiles.find(file => file.path === path)
+        if (!file) throw new Error(`Unable to find file for path ${path}`)
+
+        // TODO: verify signatures
+        //       verify length
+        //       verify hash
+        //       verify _type
+        // TODO: new Date(meta.signed.expires) ignore the Targets data if it has expired ?
+
+        const { product, id } = parseConfigPath(path)
+
+        Object.assign(newConf, {
+          path,
+          product,
+          id,
+          version: meta.custom.v,
+          apply_state: 1,
+          apply_error: '',
+          length: meta.length,
+          hashes: meta.hashes,
+          file: fromBase64JSON(file.raw)
+        })
+      }
+
+      this.state.client.state.targets_version = targets.signed.version
+      this.state.client.state.backend_client_state = targets.signed.custom.opaque_backend_state
+    }
+
+    if (toUnapply.length || toApply.length || toModify.length) {
+      this.dispatch(toUnapply, 'unapply')
+      this.dispatch(toApply, 'apply')
+      this.dispatch(toModify, 'modify')
+
+      this.state.client.state.config_states = []
+      this.state.cached_target_files = []
+
+      for (const conf of this.appliedConfigs.values()) {
+        this.state.client.state.config_states.push({
+          id: conf.id,
+          version: conf.version,
+          product: conf.product,
+          apply_state: conf.apply_state,
+          apply_error: conf.apply_error
+        })
+
+        this.state.cached_target_files.push({
+          path: conf.path,
+          length: conf.length,
+          hashes: Object.entries(conf.hashes).map((entry) => ({ algorithm: entry[0], hash: entry[1] }))
+        })
+      }
+    }
+  }
+
+  dispatch (list, action) {
+    for (const item of list) {
+      try {
+        // TODO: do we want to pass old and new config ?
+        this.emit(item.product, action, item.file)
+
+        item.apply_state = 2
+      } catch (err) {
+        item.apply_state = 3
+        item.apply_error = err.toString()
+      }
+
+      if (action === 'unapply') {
+        this.appliedConfigs.delete(item.path)
+      } else {
+        this.appliedConfigs.set(item.path, item)
+      }
+    }
+  }
+}
+
+function fromBase64JSON (str) {
+  if (!str) return null
+
+  return JSON.parse(Buffer.from(str, 'base64').toString())
+}
+
+const configPathRegex = /^(?:datadog\/\d+|employee)\/([^/]+)\/([^/]+)\/[^/]+$/
+
+function parseConfigPath (configPath) {
+  const match = configPathRegex.exec(configPath)
+
+  if (!match || !match[1] || !match[2]) {
+    throw new Error(`Unable to parse path ${configPath}`)
+  }
+
+  return {
+    product: match[1],
+    id: match[2]
+  }
+}
+
+module.exports = RemoteConfigManager
---
+++ packages/dd-trace/test/appsec/remote_config/manager.spec.js
@@ -0,0 +1,590 @@
+'use strict'
+
+const Capabilities = require('../../../src/appsec/remote_config/capabilities')
+
+const noop = () => {}
+
+describe('RemoteConfigManager', () => {
+  let uuid
+  let scheduler
+  let Scheduler
+  let request
+  let log
+  let RemoteConfigManager
+  let config
+  let rc
+
+  beforeEach(() => {
+    uuid = sinon.stub().returns('1234-5678')
+
+    scheduler = {
+      start: sinon.spy(),
+      stop: sinon.spy()
+    }
+
+    Scheduler = sinon.stub().returns(scheduler)
+
+    request = sinon.stub()
+
+    log = {
+      error: sinon.spy()
+    }
+
+    RemoteConfigManager = proxyquire('../src/appsec/remote_config/manager', {
+      'crypto-randomuuid': uuid,
+      './scheduler': Scheduler,
+      '../../../../../package.json': { version: '3.0.0' },
+      '../../exporters/common/request': request,
+      '../../log': log
+    })
+
+    config = {
+      url: 'http://127.0.0.1:1337',
+      hostname: '127.0.0.1',
+      port: '1337',
+      tags: {
+        'runtime-id': 'runtimeId'
+      },
+      service: 'serviceName',
+      env: 'serviceEnv',
+      version: 'appVersion'
+    }
+
+    rc = new RemoteConfigManager(config)
+  })
+
+  it('should instantiate RemoteConfigManager', () => {
+    sinon.stub(rc, 'poll')
+
+    expect(Scheduler).to.have.been.calledOnce
+    const [firstArg, secondArg] = Scheduler.firstCall.args
+    expect(firstArg).to.be.a('function')
+    expect(secondArg).to.equal(5e3)
+
+    firstArg(noop)
+    expect(rc.poll).to.have.calledOnceWithExactly(noop)
+
+    expect(rc.scheduler).to.equal(scheduler)
+
+    expect(rc.requestOptions).to.deep.equal({
+      method: 'POST',
+      url: config.url,
+      hostname: config.hostname,
+      port: config.port,
+      path: '/v0.7/config'
+    })
+
+    expect(rc.state).to.deep.equal({
+      client: {
+        state: {
+          root_version: 1,
+          targets_version: 0,
+          config_states: [],
+          has_error: false,
+          error: '',
+          backend_client_state: ''
+        },
+        id: '1234-5678',
+        products: [],
+        is_tracer: true,
+        client_tracer: {
+          runtime_id: config.tags['runtime-id'],
+          language: 'node',
+          tracer_version: '3.0.0',
+          service: config.service,
+          env: config.env,
+          app_version: config.version
+        },
+        capabilities: 'AA=='
+      },
+      cached_target_files: []
+    })
+
+    expect(rc.appliedConfigs).to.be.an.instanceOf(Map)
+  })
+
+  describe('updateCapabilities', () => {
+    it('should set multiple capabilities to true', () => {
+      rc.updateCapabilities(Capabilities.ASM_ACTIVATION, true)
+      expect(rc.state.client.capabilities).to.equal('Ag==')
+
+      rc.updateCapabilities(Capabilities.ASM_IP_BLOCKING, true)
+      expect(rc.state.client.capabilities).to.equal('Bg==')
+
+      rc.updateCapabilities(Capabilities.ASM_DD_RULES, true)
+      expect(rc.state.client.capabilities).to.equal('Dg==')
+    })
+
+    it('should set multiple capabilities to false', () => {
+      rc.state.client.capabilities = 'Dg=='
+
+      rc.updateCapabilities(Capabilities.ASM_ACTIVATION, false)
+      expect(rc.state.client.capabilities).to.equal('DA==')
+
+      rc.updateCapabilities(Capabilities.ASM_IP_BLOCKING, false)
+      expect(rc.state.client.capabilities).to.equal('CA==')
+
+      rc.updateCapabilities(Capabilities.ASM_DD_RULES, false)
+      expect(rc.state.client.capabilities).to.equal('AA==')
+    })
+
+    it('should set an arbitrary amount of capabilities', () => {
+      rc.updateCapabilities(1n << 1n, true)
+      rc.updateCapabilities(1n << 200n, true)
+      expect(rc.state.client.capabilities).to.equal('AQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAI=')
+
+      rc.updateCapabilities(1n << 200n, false)
+      expect(rc.state.client.capabilities).to.equal('Ag==')
+    })
+  })
+
+  describe('on/off', () => {
+    it('should update the product list and autostart or autostop', () => {
+      expect(rc.on('ASM_FEATURES', noop)).to.equal(rc)
+
+      expect(rc.state.client.products).to.deep.equal(['ASM_FEATURES'])
+      expect(rc.scheduler.start).to.have.been.calledOnce
+
+      rc.on('ASM_DATA', noop)
+      rc.on('ASM_DD', noop)
+
+      expect(rc.state.client.products).to.deep.equal(['ASM_FEATURES', 'ASM_DATA', 'ASM_DD'])
+      expect(rc.scheduler.start).to.have.been.calledThrice
+
+      expect(rc.off('ASM_FEATURES', noop)).to.equal(rc)
+
+      expect(rc.state.client.products).to.deep.equal(['ASM_DATA', 'ASM_DD'])
+
+      rc.off('ASM_DATA', noop)
+
+      expect(rc.scheduler.stop).to.not.have.been.called
+
+      rc.off('ASM_DD', noop)
+
+      expect(rc.scheduler.stop).to.have.been.calledOnce
+      expect(rc.state.client.products).to.be.empty
+    })
+  })
+
+  describe('poll', () => {
+    beforeEach(() => {
+      sinon.stub(rc, 'parseConfig')
+    })
+
+    it('should request and do nothing when received status 404', (cb) => {
+      request.yieldsRight(new Error('Response received 404'), '{"a":"b"}', 404)
+
+      const payload = JSON.stringify(rc.state)
+
+      rc.poll(() => {
+        expect(request).to.have.been.calledOnceWith(payload, rc.requestOptions)
+        expect(log.error).to.not.have.been.called
+        expect(rc.parseConfig).to.not.have.been.called
+        cb()
+      })
+    })
+
+    it('should request and log when received error', (cb) => {
+      const err = new Error('Response received 500')
+      request.yieldsRight(err, '{"a":"b"}', 500)
+
+      const payload = JSON.stringify(rc.state)
+
+      rc.poll(() => {
+        expect(request).to.have.been.calledOnceWith(payload, rc.requestOptions)
+        expect(log.error).to.have.been.calledOnceWithExactly(err)
+        expect(rc.parseConfig).to.not.have.been.called
+        cb()
+      })
+    })
+
+    it('should request and call parseConfig when payload is not empty', (cb) => {
+      request.yieldsRight(null, '{"a":"b"}', 200)
+
+      const payload = JSON.stringify(rc.state)
+
+      rc.poll(() => {
+        expect(request).to.have.been.calledOnceWith(payload, rc.requestOptions)
+        expect(log.error).to.not.have.been.called
+        expect(rc.parseConfig).to.have.been.calledOnceWithExactly({ a: 'b' })
+        cb()
+      })
+    })
+
+    it('should catch exceptions, update the error state, and clear the error state at next request', (cb) => {
+      request
+        .onFirstCall().yieldsRight(null, '{"a":"b"}', 200)
+        .onSecondCall().yieldsRight(null, null, 200)
+      rc.parseConfig.onFirstCall().throws(new Error('Unable to parse config'))
+
+      const payload = JSON.stringify(rc.state)
+
+      rc.poll(() => {
+        expect(request).to.have.been.calledOnceWith(payload, rc.requestOptions)
+        expect(rc.parseConfig).to.have.been.calledOnceWithExactly({ a: 'b' })
+        expect(log.error).to.have.been
+          .calledOnceWithExactly('Could not parse remote config response: Error: Unable to parse config')
+        expect(rc.state.client.state.has_error).to.be.true
+        expect(rc.state.client.state.error).to.equal('Error: Unable to parse config')
+
+        const payload2 = JSON.stringify(rc.state)
+
+        rc.poll(() => {
+          expect(request).to.have.been.calledTwice
+          expect(request.secondCall).to.have.been.calledWith(payload2, rc.requestOptions)
+          expect(rc.parseConfig).to.have.been.calledOnce
+          expect(log.error).to.have.been.calledOnce
+          expect(rc.state.client.state.has_error).to.be.false
+          expect(rc.state.client.state.error).to.be.empty
+          cb()
+        })
+      })
+    })
+
+    it('should request and do nothing when payload is empty JSON object', (cb) => {
+      request.yieldsRight(null, '{}', 200)
+
+      const payload = JSON.stringify(rc.state)
+
+      rc.poll(() => {
+        expect(request).to.have.been.calledOnceWith(payload, rc.requestOptions)
+        expect(log.error).to.not.have.been.called
+        expect(rc.parseConfig).to.not.have.been.called
+        cb()
+      })
+    })
+  })
+
+  describe('parseConfig', () => {
+    let payload
+    const func = () => rc.parseConfig(payload)
+    let previousState
+
+    beforeEach(() => {
+      sinon.stub(rc, 'dispatch').callsFake((list, action) => {
+        for (const item of list) {
+          item.apply_state = 2
+
+          if (action === 'unapply') rc.appliedConfigs.delete(item.path)
+          else rc.appliedConfigs.set(item.path, item)
+        }
+      })
+
+      previousState = JSON.parse(JSON.stringify(rc.state))
+    })
+
+    it('should do nothing if passed an empty payload', () => {
+      payload = {}
+
+      expect(func).to.not.throw()
+      expect(rc.dispatch).to.not.have.been.called
+      expect(rc.state).to.deep.equal(previousState)
+    })
+
+    it('should throw when target is not found', () => {
+      payload = {
+        client_configs: ['datadog/42/PRODUCT/confId/config'],
+        targets: toBase64({
+          signed: {
+            targets: {
+              'datadog/42/OTHERPRODUCT/confId/config': {}
+            }
+          }
+        })
+      }
+
+      expect(func).to.throw('Unable to find target for path datadog/42/PRODUCT/confId/config')
+      expect(rc.dispatch).to.not.have.been.called
+      expect(rc.state).to.deep.equal(previousState)
+    })
+
+    it('should throw when target file is not found', () => {
+      payload = {
+        client_configs: ['datadog/42/PRODUCT/confId/config'],
+        targets: toBase64({
+          signed: {
+            targets: {
+              'datadog/42/PRODUCT/confId/config': {
+                hashes: {
+                  sha256: 'haaaxx'
+                }
+              }
+            }
+          }
+        })
+      }
+
+      expect(func).to.throw('Unable to find file for path datadog/42/PRODUCT/confId/config')
+      expect(rc.dispatch).to.not.have.been.called
+      expect(rc.state).to.deep.equal(previousState)
+    })
+
+    it('should throw when config path cannot be parsed', () => {
+      payload = {
+        client_configs: ['datadog/42/confId/config'],
+        targets: toBase64({
+          signed: {
+            targets: {
+              'datadog/42/confId/config': {
+                hashes: {
+                  sha256: 'haaaxx'
+                }
+              }
+            }
+          }
+        }),
+        target_files: [{
+          path: 'datadog/42/confId/config',
+          raw: toBase64({})
+        }]
+      }
+
+      expect(func).to.throw('Unable to parse path datadog/42/confId/config')
+      expect(rc.dispatch).to.not.have.been.called
+      expect(rc.state).to.deep.equal(previousState)
+    })
+
+    it('should parse the config, call dispatch, and update the state', () => {
+      rc.appliedConfigs.set('datadog/42/UNAPPLY/confId/config', {
+        path: 'datadog/42/UNAPPLY/confId/config',
+        product: 'UNAPPLY',
+        id: 'confId',
+        version: 69,
+        apply_state: 2,
+        apply_error: '',
+        length: 147,
+        hashes: { sha256: 'anotherHash' },
+        file: { asm: { enabled: true } }
+      })
+      rc.appliedConfigs.set('datadog/42/IGNORE/confId/config', {
+        path: 'datadog/42/IGNORE/confId/config',
+        product: 'IGNORE',
+        id: 'confId',
+        version: 43,
+        apply_state: 2,
+        apply_error: '',
+        length: 420,
+        hashes: { sha256: 'sameHash' },
+        file: {}
+      })
+      rc.appliedConfigs.set('datadog/42/MODIFY/confId/config', {
+        path: 'datadog/42/MODIFY/confId/config',
+        product: 'MODIFY',
+        id: 'confId',
+        version: 11,
+        apply_state: 2,
+        apply_error: '',
+        length: 147,
+        hashes: { sha256: 'oldHash' },
+        file: { config: 'oldConf' }
+      })
+
+      payload = {
+        client_configs: [
+          'datadog/42/IGNORE/confId/config',
+          'datadog/42/MODIFY/confId/config',
+          'datadog/42/APPLY/confId/config'
+        ],
+        targets: toBase64({
+          signed: {
+            custom: {
+              opaque_backend_state: 'opaquestateinbase64'
+            },
+            targets: {
+              'datadog/42/IGNORE/confId/config': {
+                custom: {
+                  v: 43
+                },
+                hashes: {
+                  sha256: 'sameHash'
+                },
+                length: 420
+              },
+              'datadog/42/MODIFY/confId/config': {
+                custom: {
+                  v: 12
+                },
+                hashes: {
+                  sha256: 'newHash'
+                },
+                length: 147
+              },
+              'datadog/42/APPLY/confId/config': {
+                custom: {
+                  v: 1
+                },
+                hashes: {
+                  sha256: 'haaaxx'
+                },
+                length: 0
+              }
+            },
+            version: 12345
+          }
+        }),
+        target_files: [
+          {
+            path: 'datadog/42/MODIFY/confId/config',
+            raw: toBase64({ config: 'newConf' })
+          },
+          {
+            path: 'datadog/42/APPLY/confId/config',
+            raw: ''
+          }
+        ]
+      }
+
+      expect(func).to.not.throw()
+
+      expect(rc.state.client.state.targets_version).to.equal(12345)
+      expect(rc.state.client.state.backend_client_state).to.equal('opaquestateinbase64')
+
+      expect(rc.dispatch).to.have.been.calledThrice
+      expect(rc.dispatch.firstCall).to.have.been.calledWithExactly([{
+        path: 'datadog/42/UNAPPLY/confId/config',
+        product: 'UNAPPLY',
+        id: 'confId',
+        version: 69,
+        apply_state: 2,
+        apply_error: '',
+        length: 147,
+        hashes: { sha256: 'anotherHash' },
+        file: { asm: { enabled: true } }
+      }], 'unapply')
+      expect(rc.dispatch.secondCall).to.have.been.calledWithExactly([{
+        path: 'datadog/42/APPLY/confId/config',
+        product: 'APPLY',
+        id: 'confId',
+        version: 1,
+        apply_state: 2,
+        apply_error: '',
+        length: 0,
+        hashes: { sha256: 'haaaxx' },
+        file: null
+      }], 'apply')
+      expect(rc.dispatch.thirdCall).to.have.been.calledWithExactly([{
+        path: 'datadog/42/MODIFY/confId/config',
+        product: 'MODIFY',
+        id: 'confId',
+        version: 12,
+        apply_state: 2,
+        apply_error: '',
+        length: 147,
+        hashes: { sha256: 'newHash' },
+        file: { config: 'newConf' }
+      }], 'modify')
+
+      expect(rc.state.client.state.config_states).to.deep.equal([
+        {
+          id: 'confId',
+          version: 43,
+          product: 'IGNORE',
+          apply_state: 2,
+          apply_error: ''
+        },
+        {
+          id: 'confId',
+          version: 12,
+          product: 'MODIFY',
+          apply_state: 2,
+          apply_error: ''
+        },
+        {
+          id: 'confId',
+          version: 1,
+          product: 'APPLY',
+          apply_state: 2,
+          apply_error: ''
+        }
+      ])
+      expect(rc.state.cached_target_files).to.deep.equal([
+        {
+          path: 'datadog/42/IGNORE/confId/config',
+          length: 420,
+          hashes: [{ algorithm: 'sha256', hash: 'sameHash' }]
+        },
+        {
+          path: 'datadog/42/MODIFY/confId/config',
+          length: 147,
+          hashes: [{ algorithm: 'sha256', hash: 'newHash' }]
+        },
+        {
+          path: 'datadog/42/APPLY/confId/config',
+          length: 0,
+          hashes: [{ algorithm: 'sha256', hash: 'haaaxx' }]
+        }
+      ])
+    })
+  })
+
+  describe('dispatch', () => {
+    beforeEach(() => {
+      sinon.stub(rc, 'emit')
+    })
+
+    it('should call emit for each config, catch errors, and update the state', () => {
+      rc.emit.onSecondCall().throws(new Error('Unable to apply config'))
+
+      const list = [
+        {
+          path: 'datadog/42/ASM_FEATURES/confId/config',
+          product: 'ASM_FEATURES',
+          apply_state: 1,
+          apply_error: '',
+          file: { asm: { enabled: true } }
+        },
+        {
+          path: 'datadog/42/ASM_DATA/confId/config',
+          product: 'ASM_DATA',
+          apply_state: 1,
+          apply_error: '',
+          file: { data: [1, 2, 3] }
+        },
+        {
+          path: 'datadog/42/ASM_DD/confId/config',
+          product: 'ASM_DD',
+          apply_state: 1,
+          apply_error: '',
+          file: { rules: [4, 5, 6] }
+        }
+      ]
+
+      rc.dispatch(list, 'apply')
+
+      expect(rc.emit).to.have.been.calledThrice
+      expect(rc.emit.firstCall).to.have.been.calledWithExactly('ASM_FEATURES', 'apply', { asm: { enabled: true } })
+      expect(rc.emit.secondCall).to.have.been.calledWithExactly('ASM_DATA', 'apply', { data: [1, 2, 3] })
+      expect(rc.emit.thirdCall).to.have.been.calledWithExactly('ASM_DD', 'apply', { rules: [4, 5, 6] })
+
+      expect(list[0].apply_state).to.equal(2)
+      expect(list[0].apply_error).to.equal('')
+      expect(list[1].apply_state).to.equal(3)
+      expect(list[1].apply_error).to.equal('Error: Unable to apply config')
+      expect(list[2].apply_state).to.equal(2)
+      expect(list[2].apply_error).to.equal('')
+
+      expect(rc.appliedConfigs.get('datadog/42/ASM_FEATURES/confId/config')).to.equal(list[0])
+      expect(rc.appliedConfigs.get('datadog/42/ASM_DATA/confId/config')).to.equal(list[1])
+      expect(rc.appliedConfigs.get('datadog/42/ASM_DD/confId/config')).to.equal(list[2])
+    })
+
+    it('should delete config from state when action is unapply', () => {
+      rc.appliedConfigs.set('datadog/42/ASM_FEATURES/confId/config', {
+        path: 'datadog/42/ASM_FEATURES/confId/config',
+        product: 'ASM_FEATURES',
+        apply_state: 2,
+        apply_error: '',
+        file: { asm: { enabled: true } }
+      })
+
+      rc.dispatch([rc.appliedConfigs.get('datadog/42/ASM_FEATURES/confId/config')], 'unapply')
+
+      expect(rc.emit).to.have.been.calledOnceWithExactly('ASM_FEATURES', 'unapply', { asm: { enabled: true } })
+      expect(rc.appliedConfigs).to.be.empty
+    })
+  })
+})
+
+function toBase64 (data) {
+  return Buffer.from(JSON.stringify(data), 'utf8').toString('base64')
+}
