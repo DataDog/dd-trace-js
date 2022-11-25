cf13170886b83b3700bb23e3136f6a1843a4478f
Julio Gonzalez <107922352+hoolioh@users.noreply.github.com>
2022-11-25 09:10:18 +0100

    Path traversal vulnerability detection

--- packages/dd-trace/src/appsec/iast/analyzers/analyzers.js
+++ packages/dd-trace/src/appsec/iast/analyzers/analyzers.js
@@ -2,5 +2,6 @@ module.exports = {
   'WEAK_CIPHER_ANALYZER': require('./weak-cipher-analyzer'),
   'WEAK_HASH_ANALYZER': require('./weak-hash-analyzer'),
   'SQL_INJECTION_ANALYZER': require('./sql-injection-analyzer'),
-  'COMMAND_INJECTION_ANALYZER': require('./command-injection-analyzer')
+  'COMMAND_INJECTION_ANALYZER': require('./command-injection-analyzer'),
+  'PATH_TRAVERSAL_ANALYZER': require('./path-traversal-analyzer')
 }
