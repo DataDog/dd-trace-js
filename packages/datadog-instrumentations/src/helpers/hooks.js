6b9d183a8ce3cd3d6b2bc8a4fee886c9f4cfcacf
Julio Gonzalez <107922352+hoolioh@users.noreply.github.com>
2022-11-25 09:10:18 +0100

    Path traversal vulnerability detection

--- packages/datadog-instrumentations/src/helpers/hooks.js
+++ packages/datadog-instrumentations/src/helpers/hooks.js
@@ -31,6 +31,7 @@ module.exports = {
   'fastify': () => require('../fastify'),
   'find-my-way': () => require('../find-my-way'),
   'fs': () => require('../fs'),
+  'node:fs': () => require('../fs'),
   'graphql': () => require('../graphql'),
   'grpc': () => require('../grpc'),
   'hapi': () => require('../hapi'),
