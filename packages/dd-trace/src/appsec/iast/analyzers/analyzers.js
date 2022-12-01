57587b09dea0276550486c7c2fff0d62ec6d6251
Ugaitz Urien <ugaitz.urien@datadoghq.com>
2022-12-01 13:18:19 +0100

    Remove path traversal from 2.x

--- packages/dd-trace/src/appsec/iast/analyzers/analyzers.js
+++ packages/dd-trace/src/appsec/iast/analyzers/analyzers.js
@@ -2,6 +2,5 @@ module.exports = {
   'WEAK_CIPHER_ANALYZER': require('./weak-cipher-analyzer'),
   'WEAK_HASH_ANALYZER': require('./weak-hash-analyzer'),
   'SQL_INJECTION_ANALYZER': require('./sql-injection-analyzer'),
-  'COMMAND_INJECTION_ANALYZER': require('./command-injection-analyzer'),
-  'PATH_TRAVERSAL_ANALYZER': require('./path-traversal-analyzer')
+  'COMMAND_INJECTION_ANALYZER': require('./command-injection-analyzer')
 }
