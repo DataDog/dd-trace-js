830466e9e7ae2a7dfd259f2028bae197ee64ed28
rochdev <roch.devost@gmail.com>
2022-12-06 18:04:54 -0500

    skip using tap feature instead of commenting the test

--- packages/dd-trace/test/setup/core.js
+++ packages/dd-trace/test/setup/core.js
@@ -18,6 +18,12 @@ global.proxyquire = proxyquire
 global.withVersions = withVersions
 global.withExports = withExports
 
+if (typeof global.describe.skip !== 'function') {
+  global.describe.skip = function (name, fn, opts = {}) {
+    return global.describe(name, fn, { skip: true, ...opts })
+  }
+}
+
 process.env.DD_TRACE_TELEMETRY_ENABLED = 'false'
 
 function loadInst (plugin) {
