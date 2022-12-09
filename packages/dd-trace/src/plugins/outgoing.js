bccf3c458f7009d895287c4fa3ed10d525cbeea8
wconti27 <william.conti@datadoghq.com>
2022-12-09 16:21:25 -0500

    Add host resolution

--- packages/dd-trace/src/plugins/outgoing.js
+++ packages/dd-trace/src/plugins/outgoing.js
@@ -1,5 +1,7 @@
 'use strict'
 
+import { ipVersion } from 'ip-address-validator';
+
 const TracingPlugin = require('./tracing')
 
 // const transportProtocols = ['ip_tcp', 'ip_udp', 'unix', 'pipe', 'inproc', 'other']
@@ -23,10 +25,37 @@ class OutgoingPlugin extends TracingPlugin {
 
     if (!span) return
 
+    hostDetails = this.resolveHostDetails(hostname)
+
     span.addTags({
-      'out.host': hostname,
+      ...hostDetails,
       'network.destination.port': port
     })
+  }
+
+  resolveHostDetails(host) {
+
+    if (host == 'localhost') {
+      const hostIP = '127.0.0.1'
+      const hostName = host
+      return {
+        'network.destination.ip': hostIP,
+        'network.destination.name': hostName,
+      }
+    }
+    // ipVersion returns 4, 6 or undefined depending on if input string is IPV4, IPV6, or not a valid IP
+    else if (ipVersion(host) != undefined) {
+      const hostIP = host
+      return {
+        'network.destination.ip': hostIP,
+      }
+    }
+    else {
+      const hostName = host
+      return {
+        'network.destination.name': hostName,
+      }
+    }
   }
 }
 
