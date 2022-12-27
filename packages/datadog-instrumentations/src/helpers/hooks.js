95c9656c52a3d4ff43e0214e1ab0072ada850052
Igor Unanua <igor.unanua@datadoghq.com>
2022-12-27 15:27:50 +0100

    LDAP analyzer (#2618)
    * ldapjs instrumentation
    
    * Update docs and index.d.ts
    
    * LDAP plugin test
    
    * Wrap Client._send results to avoid losing the context
    
    * Use WeakMap get and set methods
    
    * handle multiple subscriptions to the same event, wrap addListener, skip analyze if previous value was vulnerable
    
    * Wrap Client._send callback always and lint

--- packages/datadog-instrumentations/src/helpers/hooks.js
+++ packages/datadog-instrumentations/src/helpers/hooks.js
@@ -48,6 +48,7 @@ module.exports = {
   'koa': () => require('../koa'),
   'koa-router': () => require('../koa'),
   'kafkajs': () => require('../kafkajs'),
+  'ldapjs': () => require('../ldapjs'),
   'limitd-client': () => require('../limitd-client'),
   'mariadb': () => require('../mariadb'),
   'memcached': () => require('../memcached'),
