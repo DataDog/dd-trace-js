84e15280cfba41f2c158beb8c6b2985d2eff3b3e
crysmags <crystal.magloire@datadoghq.com>
2022-12-30 19:03:58 -0500

    adding mysql2 propagation support

--- packages/datadog-plugin-mysql2/src/index.js
+++ packages/datadog-plugin-mysql2/src/index.js
@@ -1,9 +1,43 @@
 'use strict'
 
-const MySQLPlugin = require('../../datadog-plugin-mysql/src')
+const DatabasePlugin = require('../../dd-trace/src/plugins/database')
 
-class MySQL2Plugin extends MySQLPlugin {
+class MySQL2Plugin extends DatabasePlugin {
   static get name () { return 'mysql2' }
+  static get system () { return 'mysql' }
+
+  start ({ sqlStatement, sql, conf: dbConfig }) {
+    const service = getServiceName(this.config, dbConfig)
+
+    this.startSpan(`${this.system}.query`, {
+      service,
+      resource: sql,
+      type: 'sql',
+      kind: 'client',
+      meta: {
+        'db.type': this.system,
+        'db.user': dbConfig.user,
+        'db.name': dbConfig.database,
+        'out.host': dbConfig.host,
+        'out.port': dbConfig.port
+      }
+    })
+    if (this.config.dbmPropagationMode !== 'disabled') {
+      if (sqlStatement.statement !== undefined) {
+        sqlStatement.statement.query = this.injectDbmQuery(sqlStatement.statement.query)
+      } else if (sqlStatement.sql) {
+        sqlStatement.sql = this.injectDbmQuery(sqlStatement.sql)
+      }
+    }
+  }
+}
+
+function getServiceName (config, dbConfig) {
+  if (typeof config.service === 'function') {
+    return config.service(dbConfig)
+  }
+
+  return config.service
 }
 
 module.exports = MySQL2Plugin
