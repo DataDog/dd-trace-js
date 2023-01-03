3cb0f1bd60fba6d819bb0ec013e663ccd6b2abea
Igor Unanua <igor.unanua@datadoghq.com>
2023-01-03 12:27:40 +0100

    Add test

--- packages/dd-trace/test/appsec/iast/overhead-controller.spec.js
+++ packages/dd-trace/test/appsec/iast/overhead-controller.spec.js
@@ -186,14 +186,17 @@ describe('Overhead controller', () => {
       const THIRD_REQUEST = '/third'
       const FOURTH_REQUEST = '/fourth'
       const FIFTH_REQUEST = '/fifth'
+      const SECURE_REQUEST = '/secure'
 
       const testRequestEventEmitter = new EventEmitter()
       let requestResolvers = {}
 
       function app (req) {
         return new Promise((resolve) => {
-          const crypto = require('crypto')
-          crypto.createHash('sha1')
+          if (req.url.indexOf('secure') === -1) {
+            const crypto = require('crypto')
+            crypto.createHash('sha1')
+          }
           requestResolvers[req.url] = () => {
             resolve()
             testRequestEventEmitter.emit(TEST_REQUEST_FINISHED, req.url)
@@ -402,6 +405,51 @@ describe('Overhead controller', () => {
           })
 
           axios.get(`http://localhost:${serverConfig.port}${FIRST_REQUEST}`).then().catch(done)
+        })
+
+        it('should add _dd.iast.enabled tag even when no vulnerability is detected', (done) => {
+          const config = new Config({
+            experimental: {
+              iast: {
+                enabled: true,
+                requestSampling: 100,
+                maxConcurrentRequests: 1
+              }
+            }
+          })
+          iast.enable(config)
+
+          const handler = function (traces) {
+            try {
+              for (let i = 0; i < traces.length; i++) {
+                for (let j = 0; j < traces[i].length; j++) {
+                  const trace = traces[i][j]
+                  if (trace.type === 'web') {
+                    const url = trace.meta['http.url']
+                    if (url.includes(SECURE_REQUEST)) {
+                      expect(trace.meta['_dd.iast.json']).to.be.undefined
+                      expect(trace.meta['_dd.iast.enabled']).eq('1')
+                    }
+                    done()
+                  }
+                }
+              }
+            } catch (e) {
+              agent.unsubscribe(handler)
+              done(e)
+            }
+          }
+          handlers.push(handler)
+          agent.subscribe(handler)
+
+          testRequestEventEmitter.on(TEST_REQUEST_STARTED, (url) => {
+            if (url === SECURE_REQUEST) {
+              setImmediate(() => {
+                requestResolvers[SECURE_REQUEST]()
+              })
+            }
+          })
+          axios.get(`http://localhost:${serverConfig.port}${SECURE_REQUEST}`).then().catch(done)
         })
       }
 
