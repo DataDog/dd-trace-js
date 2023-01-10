54b6c5a66264ddc729d73915cbc817ad5cf83f45
Ugaitz Urien <ugaitz.urien@datadoghq.com>
2023-01-10 09:11:32 +0100

    Apply again release compatible commits (#2565 #2568 #2618 #2642) (#2656)
    * Fix pg and add plugin style test in iast sqli detection (#2565)
    
    * Fix pg and add plugin style test in iast sqli detection
    
    * Test that the vulnerability is not detected when it not exists
    
    * Fix comments in PR
    
    * Comment in PR
    
    * CSI trim methods rewriting (#2568)
    
    * CSI trim methods call
    
    * Unify Csi method definitions in one file and use them to configure the rewriter and to do prototype runtime validations
    
    * String trim propagation integration tests
    
    * Include commands with multibyte chars
    
    * Add string + operator test and fix TaintedUtils.concat invocation
    
    * 'should not catch original Error' test
    
    * LDAP analyzer (#2618)
    
    * ldapjs instrumentation
    
    * Update docs and index.d.ts
    
    * LDAP plugin test
    
    * Wrap Client._send results to avoid losing the context
    
    * Use WeakMap get and set methods
    
    * handle multiple subscriptions to the same event, wrap addListener, skip analyze if previous value was vulnerable
    
    * Wrap Client._send callback always and lint
    
    * Update rewriter version (#2642)
    
    Co-authored-by: Igor Unanua <igor.unanua@datadoghq.com>

---
+++ packages/dd-trace/test/appsec/iast/analyzers/sql-injection-analyzer.pg.plugin.spec.js
@@ -0,0 +1,46 @@
+const { testThatRequestHasVulnerability, testThatRequestHasNoVulnerability } = require('../utils')
+const { storage } = require('../../../../../datadog-core')
+const iastContextFunctions = require('../../../../src/appsec/iast/iast-context')
+const { newTaintedString } = require('../../../../src/appsec/iast/taint-tracking/operations')
+const vulnerabilityReporter = require('../../../../src/appsec/iast/vulnerability-reporter')
+
+describe('sql-injection-analyzer with pg', () => {
+  let pg
+  let client
+  withVersions('pg', 'pg', version => {
+    describe('pg', () => {
+      beforeEach((done) => {
+        pg = require(`../../../../../../versions/pg@${version}`).get()
+        client = new pg.Client({
+          user: 'postgres',
+          password: 'postgres',
+          database: 'postgres',
+          application_name: 'test'
+        })
+        vulnerabilityReporter.clearCache()
+        client.connect(err => done(err))
+      })
+
+      afterEach((done) => {
+        client.end(done)
+      })
+
+      describe('has vulnerability', () => {
+        testThatRequestHasVulnerability(() => {
+          const store = storage.getStore()
+          const iastCtx = iastContextFunctions.getIastContext(store)
+          let sql = 'SELECT 1'
+          sql = newTaintedString(iastCtx, sql, 'param', 'Request')
+          return client.query(sql)
+        }, 'SQL_INJECTION')
+      })
+
+      describe('has no vulnerability', () => {
+        testThatRequestHasNoVulnerability(() => {
+          const sql = 'SELECT 1'
+          return client.query(sql)
+        }, 'SQL_INJECTION')
+      })
+    })
+  })
+})
