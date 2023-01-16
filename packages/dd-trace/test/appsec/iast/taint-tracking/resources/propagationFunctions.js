04121b8baf2f677c77b15ec2c95e7b260a8da5f7
Igor Unanua <igor.unanua@datadoghq.com>
2023-01-16 11:32:00 +0100

    Add taint tracking support for concat, substring, substr, slice and replace methods (#2629)
    * CSI concat
    
    * Include substring, substr, slice and replace
    
    * Implement programatically TaintTracking methods
    
    * Use includes

--- packages/dd-trace/test/appsec/iast/taint-tracking/resources/propagationFunctions.js
+++ packages/dd-trace/test/appsec/iast/taint-tracking/resources/propagationFunctions.js
@@ -24,11 +24,56 @@ function trimEndStr (str) {
   return str.trimEnd()
 }
 
+function trimProtoStr (str) {
+  return String.prototype.trim.call(str)
+}
+
+function concatStr (str) {
+  return str.concat(' ', 'b', 'c')
+}
+
+function concatTaintedStr (str) {
+  return 'ls '.concat(' ', str, 'c')
+}
+
+function concatProtoStr (str) {
+  return String.prototype.concat.call(str, ' a ', ' b ')
+}
+
+function substringStr (str) {
+  return str.substring(1, 4)
+}
+
+function substrStr (str) {
+  return str.substr(1, 4)
+}
+
+function sliceStr (str) {
+  return str.slice(1, 4)
+}
+
+function replaceStr (str) {
+  return str.replace('ls', 'sl')
+}
+
+function replaceRegexStr (str) {
+  return str.replace(/ls/g, 'ls')
+}
+
 module.exports = {
   concatSuffix,
   insertStr,
   appendStr,
   trimStr,
   trimStartStr,
-  trimEndStr
+  trimEndStr,
+  trimProtoStr,
+  concatStr,
+  concatTaintedStr,
+  concatProtoStr,
+  substringStr,
+  substrStr,
+  sliceStr,
+  replaceStr,
+  replaceRegexStr
 }
