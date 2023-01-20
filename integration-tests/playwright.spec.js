0d21583d718b31ef37df1595f27dd7e508570059
Juan Fernandez <juan.fernandezdealba@datadoghq.com>
2023-01-20 14:59:59 +0100

    proper tests

--- integration-tests/playwright.spec.js
+++ integration-tests/playwright.spec.js
@@ -12,6 +12,7 @@ const {
 } = require('./helpers')
 const { FakeCiVisIntake } = require('./ci-visibility-intake')
 const webAppServer = require('./ci-visibility/web-app-server')
+const { TEST_STATUS } = require('../packages/dd-trace/src/plugins/util/test')
 
 describe.only('playwright', () => {
   let sandbox, cwd, receiver, childProcess, webAppPort
@@ -19,9 +20,8 @@ describe.only('playwright', () => {
     sandbox = await createSandbox(['@playwright/test'], true)
     cwd = sandbox.folder
     // install necessary browser
-    await execSync('npx playwright install', { cwd })
+    execSync('npx playwright install', { cwd })
     webAppPort = await getPort()
-    debugger
     webAppServer.listen(webAppPort)
   })
 
@@ -39,7 +39,7 @@ describe.only('playwright', () => {
     childProcess.kill()
     await receiver.stop()
   })
-  const reportMethods = ['agentless' ]//'evp proxy']
+  const reportMethods = ['agentless', 'evp proxy']
 
   reportMethods.forEach((reportMethod) => {
     context(`reporting via ${reportMethod}`, () => {
@@ -48,10 +48,46 @@ describe.only('playwright', () => {
           ? getCiVisAgentlessConfig(receiver.port) : getCiVisEvpProxyConfig(receiver.port)
         const reportUrl = reportMethod === 'agentless' ? '/api/v2/citestcycle' : '/evp_proxy/v2/api/v2/citestcycle'
 
-        receiver.gatherPayloads(({ url }) => url === reportUrl).then(payloads => {
-          debugger
+        receiver.gatherPayloads(({ url }) => url === reportUrl, 10000).then((payloads) => {
+          const events = payloads.flatMap(({ payload }) => payload.events)
+
+          const testSessionEvent = events.find(event => event.type === 'test_session_end')
+          const testSuiteEvents = events.filter(event => event.type === 'test_suite_end')
+          const testEvents = events.filter(event => event.type === 'test')
+
+          const stepEvents = events.filter(event => event.type === 'span')
+
+          assert.equal(testSessionEvent.content.resource, 'test_session.playwright test')
+
+          assert.equal(testSessionEvent.content.meta[TEST_STATUS], 'fail')
+
+          assert.includeMembers(testSuiteEvents.map(suite => suite.content.resource), [
+            'test_suite.ci-visibility/playwright-tests/todo-list-page-test.js',
+            'test_suite.ci-visibility/playwright-tests/landing-page-test.js'
+          ])
+
+          assert.includeMembers(testSuiteEvents.map(suite => suite.content.meta[TEST_STATUS]), [
+            'pass',
+            'fail'
+          ])
+
+          assert.includeMembers(testEvents.map(test => test.content.resource), [
+            'ci-visibility/playwright-tests/landing-page-test.js.should work with passing tests',
+            'ci-visibility/playwright-tests/todo-list-page-test.js.should work with failing tests'
+          ])
+
+          assert.includeMembers(testEvents.map(test => test.content.meta[TEST_STATUS]), [
+            'pass',
+            'fail'
+          ])
+
+          stepEvents.forEach(stepEvent => {
+            assert.equal(stepEvent.content.name, 'playwright.step')
+            assert.property(stepEvent.content.meta, 'playwright.step')
+          })
+
           done()
-        }, 5000)
+        }).catch(done)
 
         childProcess = exec(
           './node_modules/.bin/playwright test',
