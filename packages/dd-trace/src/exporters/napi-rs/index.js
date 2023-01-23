c8849f171e5ad7cf130f9ffb85810c5a5929665b
David Lee <david.lee@datadoghq.com>
2023-01-23 09:34:01 -0800

    add napi-rs exporter

---
+++ packages/dd-trace/src/exporters/napi-rs/index.js
@@ -0,0 +1,22 @@
+/* eslint-disable */
+'use strict'
+
+const { sendTrace } = require("./node-napi-rs.darwin-arm64.node")
+
+class NAPIRSExporter {
+  constructor () {}
+
+  export (spans) {
+    for (const span of spans) {
+      span.trace_id = parseInt(span.trace_id, 16)
+      span.parent_id = parseInt(span.parent_id, 16)
+      span.span_id = parseInt(span.span_id, 16)
+    }
+
+    const payload = JSON.stringify(spans)
+
+    sendTrace(payload, 0)
+  }
+}
+
+module.exports = NAPIRSExporter
