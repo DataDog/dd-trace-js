afdd04c9214e73ba99db8ae1c3e46e65ba401914
maxday <maxime.david@datadoghq.com>
2023-01-25 11:11:16 -0500

    add fake timer

--- packages/dd-trace/index.js
+++ packages/dd-trace/index.js
@@ -1,15 +1,5 @@
 'use strict'
 
-function wait(ms) {
-  var start = Date.now(),
-      now = start;
-  while (now - start < ms) {
-    now = Date.now();
-  }
-}
-
-wait(1000);
-
 if (!global._ddtrace) {
   const TracerProxy = require('./src')
 
