07f7585cccb93a3e2d531eb2be2c31226d162cb3
Juan Antonio Fernández de Alba <juan.fernandezdealba@datadoghq.com>
2023-01-26 16:51:21 +0100

    [ci-visibility] New Test Code Coverage Format  (#2614)

--- packages/dd-trace/src/encode/coverage-ci-visibility.js
+++ packages/dd-trace/src/encode/coverage-ci-visibility.js
@@ -1,56 +1,35 @@
 'use strict'
 const { AgentEncoder } = require('./0.4')
 const Chunk = require('./chunk')
-const log = require('../log')
 
 const FormData = require('../exporters/common/form-data')
 
-const COVERAGE_PAYLOAD_VERSION = 1
-const COVERAGE_KEYS_LENGTH = 4
-const MAXIMUM_NUM_COVERAGE_FILES = 100
+const COVERAGE_PAYLOAD_VERSION = 2
+const COVERAGE_KEYS_LENGTH = 2
 
 class CoverageCIVisibilityEncoder extends AgentEncoder {
   constructor () {
     super(...arguments)
     this._coverageBytes = new Chunk()
     this.form = new FormData()
-    this.fileIndex = 1
+    this._coveragesCount = 0
     this.reset()
   }
 
   count () {
-    return this.fileIndex - 1
+    return this._coveragesCount
   }
 
   encode (coverage) {
-    const bytes = this._coverageBytes
-    const coverageBuffer = this.encodeCodeCoverage(bytes, coverage)
-    const coverageFilename = `coverage${this.fileIndex++}`
-
-    this.form.append(
-      coverageFilename,
-      coverageBuffer,
-      {
-        filename: `${coverageFilename}.msgpack`,
-        contentType: 'application/msgpack'
-      }
-    )
-
-    if (this.fileIndex === MAXIMUM_NUM_COVERAGE_FILES) {
-      log.debug('Coverage buffer reached the limit, flushing')
-      this._writer.flush()
-    }
-
-    this.reset()
+    this._coveragesCount++
+    this.encodeCodeCoverage(this._coverageBytes, coverage)
   }
 
   encodeCodeCoverage (bytes, coverage) {
-    this._encodeMapPrefix(bytes, COVERAGE_KEYS_LENGTH)
-    this._encodeString(bytes, 'version')
-    this._encodeInteger(bytes, COVERAGE_PAYLOAD_VERSION)
-    this._encodeString(bytes, 'trace_id')
+    this._encodeMapPrefix(bytes, 3)
+    this._encodeString(bytes, 'test_session_id')
     this._encodeId(bytes, coverage.traceId)
-    this._encodeString(bytes, 'span_id')
+    this._encodeString(bytes, 'test_suite_id')
     this._encodeId(bytes, coverage.spanId)
     this._encodeString(bytes, 'files')
     this._encodeArrayPrefix(bytes, coverage.files)
@@ -59,12 +38,6 @@ class CoverageCIVisibilityEncoder extends AgentEncoder {
       this._encodeString(bytes, 'filename')
       this._encodeString(bytes, filename)
     }
-    const traceSize = bytes.length
-    const buffer = Buffer.allocUnsafe(traceSize)
-
-    bytes.buffer.copy(buffer, 0, 0, bytes.length)
-
-    return buffer
   }
 
   reset () {
@@ -72,9 +45,51 @@ class CoverageCIVisibilityEncoder extends AgentEncoder {
     if (this._coverageBytes) {
       this._coverageBytes.length = 0
     }
+    this._coveragesCount = 0
+    this._encodePayloadStart(this._coverageBytes)
+  }
+
+  _encodePayloadStart (bytes) {
+    const payload = {
+      version: COVERAGE_PAYLOAD_VERSION,
+      coverages: []
+    }
+    this._encodeMapPrefix(bytes, COVERAGE_KEYS_LENGTH)
+    this._encodeString(bytes, 'version')
+    this._encodeInteger(bytes, payload.version)
+    this._encodeString(bytes, 'coverages')
+    // Get offset of the coverages list to update the length of the array when calling `makePayload`
+    this._coveragesOffset = bytes.length
+    bytes.reserve(5)
+    bytes.length += 5
   }
 
   makePayload () {
+    const bytes = this._coverageBytes
+
+    const coveragesOffset = this._coveragesOffset
+    const coveragesCount = this._coveragesCount
+
+    // update with number of coverages
+    bytes.buffer[coveragesOffset] = 0xdd
+    bytes.buffer[coveragesOffset + 1] = coveragesCount >> 24
+    bytes.buffer[coveragesOffset + 2] = coveragesCount >> 16
+    bytes.buffer[coveragesOffset + 3] = coveragesCount >> 8
+    bytes.buffer[coveragesOffset + 4] = coveragesCount
+
+    const traceSize = bytes.length
+    const buffer = Buffer.allocUnsafe(traceSize)
+
+    bytes.buffer.copy(buffer, 0, 0, bytes.length)
+
+    this.form.append(
+      'coverage1',
+      buffer,
+      {
+        filename: `coverage1.msgpack`,
+        contentType: 'application/msgpack'
+      }
+    )
     this.form.append(
       'event',
       // The intake requires a populated dictionary here. Simply having {} is not valid.
@@ -86,7 +101,7 @@ class CoverageCIVisibilityEncoder extends AgentEncoder {
     const form = this.form
 
     this.form = new FormData()
-    this.fileIndex = 1
+    this.reset()
 
     return form
   }
