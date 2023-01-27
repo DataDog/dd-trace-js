7e4ff4b3c6aabce1de756c69e5292137a0133cb0
crysmags <crystal.magloire@datadoghq.com>
2023-01-27 14:13:07 -0500

    adding query normalization and passing to payload

--- packages/datadog-plugin-mysql2/src/index.js
+++ packages/datadog-plugin-mysql2/src/index.js
@@ -1,46 +1,9 @@
 'use strict'
 
-const DatabasePlugin = require('../../dd-trace/src/plugins/database')
+const MySQLPlugin = require('../../datadog-plugin-mysql/src')
 
-class MySQL2Plugin extends DatabasePlugin {
+class MySQL2Plugin extends MySQLPlugin {
   static get name () { return 'mysql2' }
-  static get system () { return 'mysql' }
-
-  start ({ sqlStatement, sql, conf: dbConfig }) {
-    const service = getServiceName(this.config, dbConfig)
-
-    this.startSpan(`${this.system}.query`, {
-      service,
-      resource: sql,
-      type: 'sql',
-      kind: 'client',
-      meta: {
-        'db.type': this.system,
-        'db.user': dbConfig.user,
-        'db.name': dbConfig.database,
-        'out.host': dbConfig.host,
-        'out.port': dbConfig.port
-      }
-    })
-    getPropagation(sqlStatement, service, this)
-  }
-}
-
-function getPropagation (sqlStatement, service, source) {
-  if (source.config.dbmPropagationMode !== 'disabled') {
-    if (sqlStatement.statement !== undefined) {
-      sqlStatement.statement.query = source.injectDbmQuery(sqlStatement.statement.query, service)
-    } else if (sqlStatement.sql) {
-      sqlStatement.sql = source.injectDbmQuery(sqlStatement.sql, service)
-    }
-  }
-}
-function getServiceName (config, dbConfig) {
-  if (typeof config.service === 'function') {
-    return config.service(dbConfig)
-  }
-
-  return config.service
 }
 
 module.exports = MySQL2Plugin
