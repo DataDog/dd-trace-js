8f5eff34b5b4312268a88b41528cfac9af970a31
Ugaitz Urien <ugaitz.urien@datadoghq.com>
2023-02-02 14:38:10 +0100

    Fix fs instrumentation test in node12 (#2761)

--- packages/datadog-instrumentations/test/fs.spec.js
+++ packages/datadog-instrumentations/test/fs.spec.js
@@ -1,11 +1,17 @@
 const agent = require('../../dd-trace/test/plugins/agent')
+const semver = require('semver')
 describe('fs instrumentation', () => {
-  it('require node:fs should work', () => {
-    return agent.load('node:fs', undefined, { flushInterval: 1 }).then(() => {
-      const fs = require('node:fs')
-      expect(fs).not.to.be.undefined
-    })
+  afterEach(() => {
+    return agent.close({ ritmReset: false })
   })
+  if (semver.satisfies(process.versions.node, '>12')) {
+    it('require node:fs should work', () => {
+      return agent.load('node:fs', undefined, { flushInterval: 1 }).then(() => {
+        const fs = require('node:fs')
+        expect(fs).not.to.be.undefined
+      })
+    })
+  }
   it('require fs should work', () => {
     return agent.load('fs', undefined, { flushInterval: 1 }).then(() => {
       const fs = require('fs')
