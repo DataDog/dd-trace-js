98e226c37eaae65793189390c5f55867ea938f40
Julio Gonzalez <julio.gonzalez@datadoghq.com>
2023-02-02 13:22:15 +0100

    Fix tests and split setUser ones into a new file.

---
+++ packages/dd-trace/test/appsec/sdk/set_user.spec.js
@@ -0,0 +1,131 @@
+const proxyquire = require('proxyquire')
+const agent = require('../../plugins/agent')
+const tracer = require('../../../../../index')
+const appsec = require('../../../src/appsec')
+const Config = require('../../../src/config')
+const getPort = require('get-port')
+const axios = require('axios')
+
+describe('Set user API', () => {
+  describe('Check internal callings', () => {
+    const tracer = {}
+    let sdk, mockSetTag, mockRootSpan, getRootSpan
+
+    beforeEach(() => {
+      mockSetTag = sinon.stub()
+      mockRootSpan = {
+        context: () => {
+          return { _tags: { 'usr.id': 'mockUser' } }
+        },
+        setTag: mockSetTag }
+      getRootSpan = sinon.stub().returns(mockRootSpan)
+
+      const AppsecSDK = proxyquire('../../../src/appsec/sdk', {
+        './utils': { getRootSpan }
+      })
+
+      sdk = new AppsecSDK(tracer)
+    })
+
+    it('Check setUser', () => {
+      const user = { id: 'user' }
+      sdk.setUser(user)
+      expect(mockSetTag).to.be.calledWith('usr.id', 'user')
+    })
+
+    it('Check setUser with no user', () => {
+      sdk.setUser()
+      expect(mockSetTag).not.to.have.been.called
+    })
+
+    it('Check setUser with no user id', () => {
+      const user = {}
+      sdk.setUser(user)
+      expect(mockSetTag).not.to.have.been.called
+    })
+  })
+
+  describe('Check internal callings, no rootSpan', () => {
+    const tracer = {}
+    const getRootSpan = sinon.stub().returns(undefined)
+
+    const AppsecSDK = proxyquire('../../../src/appsec/sdk', {
+      './utils': { getRootSpan }
+    })
+
+    const sdk = new AppsecSDK(tracer)
+
+    it('Check setUser no rootSpan', () => {
+      sdk._setUser = sinon.stub()
+      sdk.setUser({ id: 'user' })
+      expect(getRootSpan).to.be.calledWith(tracer)
+      expect(sdk._setUser).not.to.have.been.called
+    })
+  })
+
+  describe('Integration with the tracer', () => {
+    let http
+    let controller
+    let appListener
+    let port
+
+    function listener (req, res) {
+      if (controller) {
+        controller(req, res)
+      }
+    }
+
+    before(async () => {
+      port = await getPort()
+      await agent.load('http')
+      http = require('http')
+    })
+
+    before(done => {
+      const server = new http.Server(listener)
+      appListener = server
+        .listen(port, 'localhost', () => done())
+    })
+
+    after(() => {
+      appsec.disable()
+      appListener.close()
+      return agent.close({ ritmReset: false })
+    })
+
+    beforeEach(() => {
+      const config = new Config({
+        appsec: {
+          enabled: true
+        }
+      })
+      appsec.enable(config)
+    })
+
+    describe('setUser', () => {
+      it('should set a proper user', (done) => {
+        controller = (req, res) => {
+          const user = { id: 'testUser' }
+          tracer.appsec.setUser(user)
+          res.end()
+        }
+        agent.use(traces => {
+          expect(traces[0][0].meta).to.have.property('usr.id', 'testUser')
+        }).then(done).catch(done)
+        axios.get(`http://localhost:${port}/`)
+      })
+
+      it('Last user should prevail', (done) => {
+        controller = (req, res) => {
+          tracer.appsec.setUser({ id: 'testUser' })
+          tracer.appsec.setUser({ id: 'testUser2' })
+          res.end()
+        }
+        agent.use(traces => {
+          expect(traces[0][0].meta).to.have.property('usr.id', 'testUser2')
+        }).then(done).catch(done)
+        axios.get(`http://localhost:${port}/`)
+      })
+    })
+  })
+})
--- packages/dd-trace/test/appsec/sdk/user_blocking.spec.js
+++ packages/dd-trace/test/appsec/sdk/user_blocking.spec.js
@@ -1,59 +1,194 @@
-'use strict'
-
 const proxyquire = require('proxyquire')
 const agent = require('../../plugins/agent')
+const tracer = require('../../../../../index')
+const appsec = require('../../../src/appsec')
+const Config = require('../../../src/config')
 const getPort = require('get-port')
 const axios = require('axios')
-const tracer = require('../../../../../index')
+const RuleManager = require('../../../src/appsec/rule_manager')
+const fs = require('fs')
+const crypto = require('crypto')
 
-describe('Appsec SDK', () => {
-  describe('calls to external methods', () => {
-    let trackUserLoginSuccessEvent, trackUserLoginFailureEvent, trackCustomEvent
-    let appsecSdk
+describe('User blocking API', () => {
+  describe('Check internal callings', () => {
     const tracer = {}
+    const mockReq = { protocol: 'https' }
+    const mockRes = { headersSent: false }
+    const storage = { getStore: () => {
+      return { req: mockReq, res: mockRes }
+    } }
+    const block = sinon.stub()
+    const root = sinon.stub().returns({})
+    const loadTemplates = sinon.stub()
+    let sdk, isUserBlocked, mockSetTag, mockRootSpan, getRootSpan
 
     beforeEach(() => {
-      trackUserLoginSuccessEvent = sinon.stub()
-      trackUserLoginFailureEvent = sinon.stub()
-      trackCustomEvent = sinon.stub()
+      isUserBlocked = sinon.stub()
+      mockSetTag = sinon.stub()
+      mockRootSpan = {
+        context: () => {
+          return { _tags: { 'usr.id': 'mockUser' } }
+        },
+        setTag: mockSetTag }
+      block.reset()
+      getRootSpan = sinon.stub().returns(mockRootSpan)
 
-      const AppsecSdk = proxyquire('../../../src/appsec/sdk', {
-        './track_event': { trackUserLoginSuccessEvent, trackUserLoginFailureEvent, trackCustomEvent }
+      const AppsecSDK = proxyquire('../../../src/appsec/sdk', {
+        './user_blocking': { isUserBlocked },
+        '../blocking': { block, loadTemplates },
+        '../../plugins/util/web': { root },
+        '../../../../datadog-core': { storage },
+        './utils': { getRootSpan }
       })
 
-      appsecSdk = new AppsecSdk(tracer)
+      sdk = new AppsecSDK(tracer)
     })
 
-    it('trackUserLoginSuccessEvent should call track_event trackUserLoginSuccessEvent', () => {
-      const user = { id: 'user_id' }
-      const metadata = { key: 'value' }
-      appsecSdk.trackUserLoginSuccessEvent(user, metadata)
-
-      expect(trackUserLoginSuccessEvent).to.have.been.calledOnceWithExactly(tracer, user, metadata)
+    it('Check isUserBlocked with a valid user', () => {
+      const user = { id: 'user' }
+      sdk.isUserBlocked(user)
+      expect(isUserBlocked).to.be.calledWith(user)
     })
 
-    it('trackUserLoginFailureEvent should call track_event trackUserLoginFailureEvent', () => {
-      const userId = 'user_id'
-      const exists = false
-      const metadata = { key: 'value' }
-      appsecSdk.trackUserLoginFailureEvent(userId, exists, metadata)
-
-      expect(trackUserLoginFailureEvent).to.have.been.calledOnceWithExactly(tracer, userId, exists, metadata)
+    it('Check isUserBlocked with an empty user', () => {
+      const user = {}
+      const ret = sdk.isUserBlocked(user)
+      expect(ret).to.be.false
+      expect(isUserBlocked).not.to.have.been.called
     })
 
-    it('trackCustomEvent should call track_event trackCustomEvent', () => {
-      const metadata = { key: 'value' }
-      appsecSdk.trackCustomEvent('event_name', metadata)
+    it('Check isUserBlocked with no user', () => {
+      const ret = sdk.isUserBlocked()
+      expect(ret).to.be.false
+      expect(isUserBlocked).not.to.have.been.called
+    })
 
-      expect(trackCustomEvent).to.have.been.calledOnceWithExactly(tracer, 'event_name', metadata)
+    it('Check blockRequest', () => {
+      sdk.blockRequest({}, {})
+      expect(block).to.be.calledWith({ req: {}, res: {}, topSpan: mockRootSpan })
+    })
+
+    it('Check blockRequest no params', () => {
+      sdk.blockRequest()
+      expect(block).to.be.calledWith({ req: mockReq, res: mockRes, topSpan: mockRootSpan })
     })
   })
 
-  describe('in request', () => {
+  describe('Check internal callings, no rootSpan', () => {
+    const tracer = {}
+    const isUserBlocked = sinon.stub()
+    const block = sinon.stub()
+    const root = sinon.stub().returns({})
+    const loadTemplates = sinon.stub()
+    const mockReq = { protocol: 'https' }
+    const mockRes = { headersSent: false }
+    const storage = { getStore: () => {
+      return { req: mockReq, res: mockRes }
+    } }
+    const getRootSpan = sinon.stub().returns(undefined)
+
+    const AppsecSDK = proxyquire('../../../src/appsec/sdk', {
+      './user_blocking': { isUserBlocked },
+      '../blocking': { block, loadTemplates },
+      '../../plugins/util/web': { root },
+      '../../../../datadog-core': { storage },
+      './utils': { getRootSpan }
+    })
+
+    const sdk = new AppsecSDK(tracer)
+
+    it('Check blockRequest no rootSpan', () => {
+      const ret = sdk.blockRequest({}, {})
+      expect(ret).to.be.false
+      expect(block).not.to.have.been.called
+    })
+
+    it('Check isUserBlocked no rootSpan', () => {
+      const ret = sdk.isUserBlocked({ id: 'user' })
+      expect(getRootSpan).to.be.calledWith(tracer)
+      expect(ret).to.be.false
+      expect(isUserBlocked).not.to.have.been.called
+    })
+  })
+
+  describe('Check internal callings, no storage', () => {
+    const tracer = {}
+    const isUserBlocked = sinon.stub()
+    const block = sinon.stub()
+    const root = sinon.stub().returns({})
+    const loadTemplates = sinon.stub()
+    const storage = { getStore: () => {
+      return undefined
+    } }
+    const getRootSpan = sinon.stub().returns(undefined)
+
+    const AppsecSDK = proxyquire('../../../src/appsec/sdk', {
+      './user_blocking': { isUserBlocked },
+      '../blocking': { block, loadTemplates },
+      '../../plugins/util/web': { root },
+      '../../../../datadog-core': { storage },
+      './utils': { getRootSpan }
+    })
+
+    const sdk = new AppsecSDK(tracer)
+
+    it('Check blockRequest no storage', () => {
+      const ret = sdk.blockRequest()
+      expect(ret).to.be.false
+      expect(getRootSpan).not.to.have.been.called
+    })
+  })
+
+  describe('Integration with the tracer', () => {
     let http
     let controller
     let appListener
     let port
+    const blockedUser = 'blockedUser'
+    const blockRuleData = {
+      rules_data: [{
+        data: [
+          { value: blockedUser }
+        ],
+        id: 'blocked_users',
+        type: 'data_with_expiration'
+      }
+      ] }
+    const rules = JSON.stringify({
+      version: '2.2',
+      metadata: {
+        rules_version: '1.4.2'
+      },
+      rules: [
+        {
+          id: 'blk-001-002',
+          name: 'Block User Addresses',
+          tags: {
+            type: 'block_user',
+            category: 'security_response'
+          },
+          conditions: [
+            {
+              parameters: {
+                inputs: [
+                  {
+                    address: 'usr.id'
+                  }
+                ],
+                data: 'blocked_users'
+              },
+              operator: 'exact_match'
+            }
+          ],
+          transformers: [],
+          on_match: [
+            'block'
+          ]
+        }
+      ]
+    })
+
+    const rulesPath = `block_rule_${crypto.randomUUID()}`
 
     function listener (req, res) {
       if (controller) {
@@ -68,130 +203,114 @@ describe('Appsec SDK', () => {
     })
 
     before(done => {
+      fs.writeFileSync(rulesPath, rules)
       const server = new http.Server(listener)
       appListener = server
         .listen(port, 'localhost', () => done())
     })
 
     after(() => {
+      fs.rmSync(rulesPath)
+      appsec.disable()
       appListener.close()
       return agent.close({ ritmReset: false })
     })
 
-    describe('trackUserLoginSuccessEvent', () => {
-      it('should track valid user', (done) => {
+    beforeEach(() => {
+      const config = new Config({
+        appsec: {
+          enabled: true,
+          rules: rulesPath
+        }
+      })
+      appsec.enable(config)
+      RuleManager.updateAsmData('apply', blockRuleData, 'asm_data')
+    })
+
+    describe('setUser', () => {
+      it('should set a proper user', (done) => {
         controller = (req, res) => {
-          tracer.appsec.trackUserLoginSuccessEvent({
-            id: 'test_user_id'
-          }, { metakey: 'metaValue' })
+          const user = { id: 'testUser' }
+          tracer.appsec.setUser(user)
           res.end()
         }
         agent.use(traces => {
-          expect(traces[0][0].meta).to.have.property('appsec.events.users.login.success.track', 'true')
-          expect(traces[0][0].meta).to.have.property('usr.id', 'test_user_id')
-          expect(traces[0][0].meta).to.have.property('appsec.events.users.login.success.metakey', 'metaValue')
-          expect(traces[0][0].meta).to.have.property('manual.keep', 'true')
+          expect(traces[0][0].meta).to.have.property('usr.id', 'testUser')
         }).then(done).catch(done)
         axios.get(`http://localhost:${port}/`)
       })
 
-      it('should not track without user', (done) => {
+      it('Last user should prevail', (done) => {
         controller = (req, res) => {
-          tracer.appsec.trackUserLoginSuccessEvent(undefined, { metakey: 'metaValue' })
+          tracer.appsec.setUser({ id: 'testUser' })
+          tracer.appsec.setUser({ id: 'testUser2' })
           res.end()
         }
         agent.use(traces => {
-          expect(traces[0][0].meta).to.not.have.property('appsec.events.users.login.success.track', 'true')
-        }).then(done).catch(done)
-        axios.get(`http://localhost:${port}/`)
-      })
-
-      it('should not track without calling the sdk method', (done) => {
-        controller = (req, res) => {
-          res.end()
-        }
-        agent.use(traces => {
-          expect(traces[0][0].meta).to.not.have.property('appsec.events.users.login.success.track', 'true')
+          expect(traces[0][0].meta).to.have.property('usr.id', 'testUser2')
         }).then(done).catch(done)
         axios.get(`http://localhost:${port}/`)
       })
     })
 
-    describe('trackUserLoginFailureEvent', () => {
-      it('should track valid existing user', (done) => {
+    describe('isUserBlocked', () => {
+      it('If user is not defined it should set the user', (done) => {
         controller = (req, res) => {
-          tracer.appsec.trackUserLoginFailureEvent('test_user_id', true, { metakey: 'metaValue' })
+          const ret = tracer.appsec.isUserBlocked({ id: 'testUser3' })
+          expect(ret).to.be.false
           res.end()
         }
         agent.use(traces => {
-          expect(traces[0][0].meta).to.have.property('appsec.events.users.login.failure.track', 'true')
-          expect(traces[0][0].meta).to.have.property('appsec.events.users.login.failure.usr.id', 'test_user_id')
-          expect(traces[0][0].meta).to.have.property('appsec.events.users.login.failure.usr.exists', 'true')
-          expect(traces[0][0].meta).to.have.property('appsec.events.users.login.failure.metakey', 'metaValue')
-          expect(traces[0][0].meta).to.have.property('manual.keep', 'true')
+          expect(traces[0][0].meta).to.have.property('usr.id', 'testUser3')
         }).then(done).catch(done)
         axios.get(`http://localhost:${port}/`)
       })
 
-      it('should track valid non existing user', (done) => {
+      it('If user is already defined it should not set the user', (done) => {
         controller = (req, res) => {
-          tracer.appsec.trackUserLoginFailureEvent('test_user_id', false, { metakey: 'metaValue' })
+          tracer.setUser({ id: 'testUser' })
+          const ret = tracer.appsec.isUserBlocked({ id: 'testUser3' })
+          expect(ret).to.be.false
           res.end()
         }
         agent.use(traces => {
-          expect(traces[0][0].meta).to.have.property('appsec.events.users.login.failure.track', 'true')
-          expect(traces[0][0].meta).to.have.property('appsec.events.users.login.failure.usr.id', 'test_user_id')
-          expect(traces[0][0].meta).to.have.property('appsec.events.users.login.failure.usr.exists', 'false')
-          expect(traces[0][0].meta).to.have.property('appsec.events.users.login.failure.metakey', 'metaValue')
-          expect(traces[0][0].meta).to.have.property('manual.keep', 'true')
+          expect(traces[0][0].meta).to.have.property('usr.id', 'testUser')
         }).then(done).catch(done)
         axios.get(`http://localhost:${port}/`)
       })
 
-      it('should not track without user', (done) => {
+      it('If user is in the blocklist it should return true', (done) => {
         controller = (req, res) => {
-          tracer.appsec.trackUserLoginFailureEvent(undefined, false, { metakey: 'metaValue' })
+          const ret = tracer.appsec.isUserBlocked({ id: 'blockedUser' })
+          expect(ret).to.be.true
           res.end()
         }
         agent.use(traces => {
-          expect(traces[0][0].meta).to.not.have.property('appsec.events.users.login.failure.track', 'true')
-        }).then(done).catch(done)
-        axios.get(`http://localhost:${port}/`)
-      })
-
-      it('should not track without calling the sdk method', (done) => {
-        controller = (req, res) => {
-          res.end()
-        }
-        agent.use(traces => {
-          expect(traces[0][0].meta).to.not.have.property('appsec.events.users.login.failure.track', 'true')
+          expect(traces[0][0].meta).to.have.property('usr.id', 'blockedUser')
         }).then(done).catch(done)
         axios.get(`http://localhost:${port}/`)
       })
     })
 
-    describe('trackCustomEvent', () => {
-      it('should track valid event name', (done) => {
+    describe('blockRequest', () => {
+      it('When called it should set the proper tag', (done) => {
         controller = (req, res) => {
-          tracer.appsec.trackCustomEvent('my-custom-event', { metakey: 'metaValue' })
-          res.end()
+          tracer.appsec.blockRequest(req, res)
         }
         agent.use(traces => {
-          expect(traces[0][0].meta).to.have.property('appsec.events.my-custom-event.track', 'true')
-          expect(traces[0][0].meta).to.have.property('appsec.events.my-custom-event.metakey', 'metaValue')
-          expect(traces[0][0].meta).to.have.property('manual.keep', 'true')
+          expect(traces[0][0].meta).to.have.property('appsec.blocked', 'true')
         }).then(done).catch(done)
         axios.get(`http://localhost:${port}/`)
       })
 
-      it('should not track invalid event name', (done) => {
+      it('When called with no params it should get them from the store', (done) => {
         controller = (req, res) => {
-          tracer.appsec.trackCustomEvent(null, { metakey: 'metaValue' })
-          tracer.appsec.trackCustomEvent({ event: 'name' }, { metakey: 'metaValue' })
-          res.end()
+          if (!tracer.appsec.blockRequest()) {
+            res.end()
+          }
         }
         agent.use(traces => {
-          expect(traces[0][0].meta).to.not.have.property('manual.keep', 'true')
+          expect(traces[0][0].meta).to.have.property('appsec.blocked', 'true')
         }).then(done).catch(done)
         axios.get(`http://localhost:${port}/`)
       })
