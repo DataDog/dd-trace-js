026191d10948d273d084a304b5da29fe044f4703
Juan Fernandez <juan.fernandezdealba@datadoghq.com>
2023-02-07 16:24:47 +0100

    add tests

--- packages/dd-trace/test/ci-visibility/exporters/ci-visibility-exporter.spec.js
+++ packages/dd-trace/test/ci-visibility/exporters/ci-visibility-exporter.spec.js
@@ -52,6 +52,21 @@ describe('CI Visibility Exporter', () => {
   })
 
   describe('getItrConfiguration', () => {
+    it('should upload git metadata when getItrConfiguration is called, regardless of ITR config', (done) => {
+      const scope = nock(`http://localhost:${port}`)
+        .post('/api/v2/git/repository/search_commits')
+        .reply(200, JSON.stringify({
+          data: []
+        }))
+        .post('/api/v2/git/repository/packfile')
+        .reply(202, '')
+
+      const ciVisibilityExporter = new CiVisibilityExporter({ port, isGitUploadEnabled: true })
+      ciVisibilityExporter.getItrConfiguration({}, () => {
+        expect(scope.isDone()).not.to.be.true
+        done()
+      })
+    })
     context('if ITR is not enabled', () => {
       it('should resolve immediately if ITR is not enabled', (done) => {
         const scope = nock(`http://localhost:${port}`)
@@ -154,6 +169,14 @@ describe('CI Visibility Exporter', () => {
     })
     context('if ITR is enabled and the tracer can use CI Vis Protocol', () => {
       it('should request the API after git upload promise is resolved', (done) => {
+        nock(`http://localhost:${port}`)
+          .post('/api/v2/git/repository/search_commits')
+          .reply(200, JSON.stringify({
+            data: []
+          }))
+          .post('/api/v2/git/repository/packfile')
+          .reply(202, '')
+
         const scope = nock(`http://localhost:${port}`)
           .post('/api/v2/ci/tests/skippable')
           .reply(200, JSON.stringify({
@@ -165,7 +188,11 @@ describe('CI Visibility Exporter', () => {
             }]
           }))
 
-        const ciVisibilityExporter = new CiVisibilityExporter({ port, isIntelligentTestRunnerEnabled: true })
+        const ciVisibilityExporter = new CiVisibilityExporter({
+          port,
+          isIntelligentTestRunnerEnabled: true,
+          isGitUploadEnabled: true
+        })
 
         ciVisibilityExporter._itrConfig = { isSuitesSkippingEnabled: true }
         ciVisibilityExporter._resolveCanUseCiVisProtocol(true)
@@ -176,7 +203,7 @@ describe('CI Visibility Exporter', () => {
           expect(scope.isDone()).to.be.true
           done()
         })
-        ciVisibilityExporter._resolveGit()
+        ciVisibilityExporter.sendGitMetadata()
       })
     })
     context('if ITR is enabled and the tracer can use CI Vis Protocol but git upload fails', () => {
