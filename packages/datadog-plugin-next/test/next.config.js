d2861786f052fcf23d4f5e87a34698c4ce98be1f
David Lee <david.lee@datadoghq.com>
2023-02-09 13:30:37 -0800

    Update next.config.js

--- packages/datadog-plugin-next/test/next.config.js
+++ packages/datadog-plugin-next/test/next.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   eslint: {
     ignoreDuringBuilds: true
-  }
+  },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.module.rules.push({
+      test: /\.node$/,
+      loader: "node-loader",
+    });
+
+    // Important: return the modified config
+    return config;
+  },
 }
