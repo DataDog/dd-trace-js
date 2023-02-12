c27ad69ad3739025bd009c38a5661a9e03cc4945
Ugaitz Urien <ugaitz.urien@datadoghq.com>
2023-02-12 16:45:10 +0100

    Support cookie-parser

--- packages/datadog-instrumentations/src/helpers/hooks.js
+++ packages/datadog-instrumentations/src/helpers/hooks.js
@@ -25,6 +25,7 @@ module.exports = {
   'child_process': () => require('../child-process'),
   'node:child_process': () => require('../child-process'),
   'connect': () => require('../connect'),
+  'cookie-parser': () => require('../cookie-parser'),
   'couchbase': () => require('../couchbase'),
   'crypto': () => require('../crypto'),
   'cypress': () => require('../cypress'),
