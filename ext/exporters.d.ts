2ca7dcfee5f143c0b3b909b31604d1f831b59fe9
David Lee <david.lee@datadoghq.com>
2023-02-15 09:01:27 -0800

    add napi-rs exporter

--- ext/exporters.d.ts
+++ ext/exporters.d.ts
@@ -2,7 +2,8 @@ declare const exporters: {
   LOG: 'log',
   AGENT: 'agent',
   DATADOG: 'datadog',
-  AGENT_PROXY: 'agent_proxy'
+  AGENT_PROXY: 'agent_proxy',
+  NAPI_RS: 'napi_rs'
 }
 
 export = exporters
---
+++ packages/dd-trace/src/exporters/napi-rs/index.ts
@@ -0,0 +1,30 @@
+/* eslint-disable */
+'use strict'
+
+let sendTrace
+
+if (process.platform == 'linux' && process.arch == 'x64') {
+  sendTrace = require("./node-napi-rs.linux-x64.node").sendTrace
+} else if (process.platform == 'darwin' && process.arch == 'arm64') {
+  sendTrace = require("./node-napi-rs.darwin-arm64.node").sendTrace
+} else {
+  console.log("the NAPI_RS exporter does not support " + process.platform + "-" + process.arch);
+}
+
+class NAPIRSExporter {
+  constructor () {}
+
+  export (spans) {
+    for (const span of spans) {
+      span.trace_id = parseInt(span.trace_id, 16)
+      span.parent_id = parseInt(span.parent_id, 16)
+      span.span_id = parseInt(span.span_id, 16)
+    }
+
+    const payload = JSON.stringify(spans)
+
+    sendTrace(payload, 0);
+  }
+}
+
+module.exports = NAPIRSExporter
