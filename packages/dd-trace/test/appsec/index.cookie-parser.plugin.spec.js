0b14a8a33820480043be284b8f03b8894c8207a8
Ugaitz Urien <ugaitz.urien@datadoghq.com>
2023-02-15 14:36:16 +0100

    Add cookie block test

--- packages/dd-trace/test/appsec/index.cookie-parser.plugin.spec.js
+++ packages/dd-trace/test/appsec/index.cookie-parser.plugin.spec.js
@@ -9,21 +9,21 @@ const agent = require('../plugins/agent')
 const appsec = require('../../src/appsec')
 const Config = require('../../src/config')
 
-withVersions('body-parser', 'body-parser', version => {
-  describe('Suspicious request blocking - body-parser', () => {
+withVersions('cookie-parser', 'cookie-parser', version => {
+  describe('Suspicious request blocking - cookie-parser', () => {
     let port, server, requestBody, rulesPath
 
     before(() => {
-      return agent.load(['express', 'body-parser', 'http'], { client: false })
+      return agent.load(['express', 'cookie-parser', 'http'], { client: false })
     })
 
     before((done) => {
       const express = require('../../../../versions/express').get()
-      const bodyParser = require(`../../../../versions/body-parser@${version}`).get()
+      const cookieParser = require(`../../../../versions/cookie-parser@${version}`).get()
 
       const app = express()
-      app.use(bodyParser.json())
-      app.post('/', (req, res) => {
+      app.use(cookieParser())
+      app.get('/', (req, res) => {
         requestBody()
         res.end('DONE')
       })
@@ -44,8 +44,8 @@ withVersions('body-parser', 'body-parser', version => {
         },
         rules: [
           {
-            id: 'test-rule-id-1',
-            name: 'test-rule-name-1',
+            id: 'test-rule-id-2',
+            name: 'test-rule-name-2',
             tags: {
               type: 'security_scanner',
               category: 'attack_attempt'
@@ -55,7 +55,7 @@ withVersions('body-parser', 'body-parser', version => {
                 parameters: {
                   inputs: [
                     {
-                      address: 'server.request.body'
+                      address: 'server.request.cookies'
                     }
                   ],
                   list: [
@@ -104,7 +104,11 @@ withVersions('body-parser', 'body-parser', version => {
     })
 
     it('should not block the request without an attack', async () => {
-      const res = await axios.post(`http://localhost:${port}/`, { key: 'value' })
+      const res = await axios.get(`http://localhost:${port}/`, {
+        headers: {
+          Cookie: 'a=b'
+        }
+      })
 
       expect(requestBody).to.be.calledOnce
       expect(res.data).to.be.equal('DONE')
@@ -112,8 +116,11 @@ withVersions('body-parser', 'body-parser', version => {
 
     it('should block the request when attack is detected', async () => {
       try {
-        await axios.post(`http://localhost:${port}/`, { key: 'testattack' })
-
+        await axios.get(`http://localhost:${port}/`, {
+          headers: {
+            Cookie: 'a=testattack'
+          }
+        })
         return Promise.reject(new Error('Request should not return 200'))
       } catch (e) {
         expect(e.response.status).to.be.equals(403)
