e68d26394cddc7a9c63c4c5bff13842e1590b115
Stephen Belanger <stephen.belanger@datadoghq.com>
2023-02-21 08:58:27 -0800

    Fix span sampling (#2779)

--- packages/dd-trace/src/opentracing/span_context.js
+++ packages/dd-trace/src/opentracing/span_context.js
@@ -12,7 +12,7 @@ class DatadogSpanContext {
     this._name = props.name
     this._isFinished = props.isFinished || false
     this._tags = props.tags || {}
-    this._sampling = props.sampling || {}
+    this._sampling = Object.assign({}, props.sampling)
     this._baggageItems = props.baggageItems || {}
     this._traceparent = props.traceparent
     this._tracestate = props.tracestate
