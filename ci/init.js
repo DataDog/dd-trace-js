249e940b9c2915464ec914678d8db05875efc05a
Juan Fernandez <juan.fernandezdealba@datadoghq.com>
2023-02-24 14:48:18 +0100

    undo flush interval change

--- ci/init.js
+++ ci/init.js
@@ -3,15 +3,13 @@ const tracer = require('../packages/dd-trace')
 const { ORIGIN_KEY } = require('../packages/dd-trace/src/constants')
 const { isTrue } = require('../packages/dd-trace/src/util')
 
-const isJestWorker = () => !!process.env.JEST_WORKER_ID
-
 const options = {
   startupLogs: false,
   tags: {
     [ORIGIN_KEY]: 'ciapp-test'
   },
   isCiVisibility: true,
-  flushInterval: isJestWorker() ? 120000 : 5000
+  flushInterval: 5000
 }
 
 let shouldInit = true
