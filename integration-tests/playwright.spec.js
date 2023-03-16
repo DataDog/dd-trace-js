5c24f636b4edd53ebd56dcb10985369c35979a9b
Juan Fernandez <juan.fernandezdealba@datadoghq.com>
2023-03-16 17:11:58 +0100

    change to 60 and remove only

--- integration-tests/playwright.spec.js
+++ integration-tests/playwright.spec.js
@@ -22,11 +22,11 @@ const isOldNode = semver.satisfies(process.version, '<=12')
 const versions = ['1.18.0', isOldNode ? '1.21.0' : 'latest']
 
 versions.forEach((version) => {
-  describe.only(`playwright@${version}`, () => {
+  describe(`playwright@${version}`, () => {
     let sandbox, cwd, receiver, childProcess, webAppPort
     before(async function () {
-      // bump from 30 seconds because playwright dependencies are heavy
-      this.timeout(45000)
+      // bump from 30 to 60 seconds because playwright dependencies are heavy
+      this.timeout(60000)
       sandbox = await createSandbox([`@playwright/test@${version}`], true)
       cwd = sandbox.folder
       // install necessary browser
