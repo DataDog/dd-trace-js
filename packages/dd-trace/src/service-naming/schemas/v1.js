30be97a36baf46812e07223d538ca4c67c357056
Jordi Bertran de Balanda <jordi.bertran@datadoghq.com>
2023-03-17 14:39:41 +0100

    add attribute schema v1 for rhea

--- packages/dd-trace/src/service-naming/schemas/v1.js
+++ packages/dd-trace/src/service-naming/schemas/v1.js
@@ -1,1 +1,20 @@
-module.exports = {}
+function identityService (ddService) {
+  return ddService
+}
+
+module.exports = {
+  messaging: {
+    outbound: {
+      rhea: {
+        opName: () => 'amqp.send',
+        serviceName: identityService
+      }
+    },
+    inbound: {
+      rhea: {
+        opName: () => 'amqp.process',
+        serviceName: identityService
+      }
+    }
+  }
+}
