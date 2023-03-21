f7943629af63916dc99be2798bb078628baa2188
Attila Szegedi <attila.szegedi@datadoghq.com>
2023-03-21 13:09:32 +0100

    [PROF-7353] Jumping on the destructuring assignment bandwagon

--- packages/dd-trace/test/asserts/profile.js
+++ packages/dd-trace/test/asserts/profile.js
@@ -1,6 +1,6 @@
 'use strict'
 
-module.exports = ({ Assertion, expect }, utils) => {
+module.exports = ({ Assertion, expect }, { expectTypes }) => {
   Assertion.addProperty('valueType', function () {
     const obj = this._obj
 
@@ -15,7 +15,7 @@ module.exports = ({ Assertion, expect }, utils) => {
     expect(obj).to.be.an('object')
 
     expect(obj.timeNanos).to.be.a('bigint')
-    utils.expectTypes(expect(obj.period), ['number', 'bigint'])
+    expectTypes(expect(obj.period), ['number', 'bigint'])
     expect(obj.periodType).to.be.a.valueType
     expect(obj.sampleType).to.be.an('array').and.have.length(2)
     expect(obj.sample).to.be.an('array')
