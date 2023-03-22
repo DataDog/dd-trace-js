7c9ab12e2cd2fa2106dce55c497de8afb78cc0f1
jordan gonzález <30836115+duncanista@users.noreply.github.com>
2023-03-22 13:16:43 -0400

    [lambda] Fix potential crash when no active span exists on impending timeout (#2871)
    * fix logic in `crashFlush` for unhandled exception when no active span exists
    
    * update fixture handlers
    
    * refactor tests and add more unit tests

--- packages/dd-trace/test/lambda/fixtures/handler.js
+++ packages/dd-trace/test/lambda/fixtures/handler.js
@@ -1,20 +1,49 @@
 'use strict'
 const _tracer = require('../../../../dd-trace')
 
-exports.handler = async (...args) => {
-  const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 
-  await _tracer.trace('self.sleepy', () => sleep(50))
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'hello!'
-      },
-      null,
-      2
-    )
-  }
+const sampleResponse = {
+  statusCode: 200,
+  body: JSON.stringify(
+    {
+      message: 'hello!'
+    },
+    null,
+    2
+  )
+}
+
+const handler = async (_event, _context) => {
+  const response = sampleResponse
 
   return response
+}
+
+const timeoutHandler = async (...args) => {
+  await _tracer.trace('self.sleepy', () => {
+    return sleep(50)
+  })
+  const response = sampleResponse
+
+  return response
+}
+
+const finishSpansEarlyTimeoutHandler = async (...args) => {
+  const response = sampleResponse
+
+  // mimick closing spans early
+  const currentSpan = _tracer.scope().active()
+  currentSpan.finish()
+
+  // timeout
+  await sleep(50)
+
+  return response
+}
+
+module.exports = {
+  finishSpansEarlyTimeoutHandler,
+  handler,
+  timeoutHandler
 }
