7a6c6dcfded9b74d1e7da16a239bd3a1fb37901e
Roch Devost <roch.devost@datadoghq.com>
2023-03-23 21:53:15 -0400

    fix mariadb connection pool spans leaking across queries (#2925)

--- packages/datadog-instrumentations/src/mariadb.js
+++ packages/datadog-instrumentations/src/mariadb.js
@@ -7,17 +7,17 @@ const shimmer = require('../../datadog-shimmer')
 const startCh = channel('apm:mariadb:query:start')
 const finishCh = channel('apm:mariadb:query:finish')
 const errorCh = channel('apm:mariadb:query:error')
+const skipCh = channel('apm:mariadb:pool:skip')
+const unskipCh = channel('apm:mariadb:pool:unskip')
 
-function wrapCommandStart (start) {
+function wrapCommandStart (start, callbackResource) {
   return function () {
     if (!startCh.hasSubscribers) return start.apply(this, arguments)
-
-    const callbackResource = new AsyncResource('bound-anonymous-fn')
 
     const resolve = callbackResource.bind(this.resolve)
     const reject = callbackResource.bind(this.reject)
 
-    const asyncResource = new AsyncResource('bound-anonymous-fn')
+    const asyncResource = callbackResource.runInAsyncScope(() => new AsyncResource('bound-anonymous-fn'))
 
     shimmer.wrap(this, 'resolve', function wrapResolve () {
       return function () {
@@ -47,17 +47,136 @@ function wrapCommandStart (start) {
   }
 }
 
+function wrapCommand (Command) {
+  return class extends Command {
+    constructor (...args) {
+      super(...args)
+
+      const callbackResource = new AsyncResource('bound-anonymous-fn')
+
+      if (this.start) {
+        this.start = wrapCommandStart(this.start, callbackResource)
+      }
+    }
+  }
+}
+
+function createWrapQuery (options) {
+  return function wrapQuery (query) {
+    return function (sql) {
+      if (!startCh.hasSubscribers) return query.apply(this, arguments)
+
+      const asyncResource = new AsyncResource('bound-anonymous-fn')
+
+      return asyncResource.runInAsyncScope(() => {
+        startCh.publish({ sql, conf: options })
+
+        return query.apply(this, arguments)
+          .then(result => {
+            finishCh.publish()
+            return result
+          }, error => {
+            errorCh.publish(error)
+            finishCh.publish()
+            throw error
+          })
+      }, 'bound-anonymous-fn')
+    }
+  }
+}
+
+function createWrapQueryCallback (options) {
+  return function wrapQuery (query) {
+    return function (sql) {
+      if (!startCh.hasSubscribers) return query.apply(this, arguments)
+
+      const cb = arguments[arguments.length - 1]
+      const asyncResource = new AsyncResource('bound-anonymous-fn')
+      const callbackResource = new AsyncResource('bound-anonymous-fn')
+
+      if (typeof cb !== 'function') {
+        arguments.length = arguments.length + 1
+      }
+
+      arguments[arguments.length - 1] = asyncResource.bind(function (err) {
+        if (err) {
+          errorCh.publish(err)
+        }
+
+        finishCh.publish()
+
+        if (typeof cb === 'function') {
+          return callbackResource.runInAsyncScope(() => cb.apply(this, arguments))
+        }
+      })
+
+      return asyncResource.runInAsyncScope(() => {
+        startCh.publish({ sql, conf: options })
+
+        return query.apply(this, arguments)
+      }, 'bound-anonymous-fn')
+    }
+  }
+}
+
+function wrapConnection (Connection, promiseMethod) {
+  return function (options) {
+    Connection.apply(this, arguments)
+
+    shimmer.wrap(this, promiseMethod, createWrapQuery(options))
+    shimmer.wrap(this, '_queryCallback', createWrapQueryCallback(options))
+  }
+}
+
+function wrapPoolBase (PoolBase) {
+  return function (options, processTask, createConnectionPool, pingPromise) {
+    arguments[1] = wrapPoolMethod(processTask)
+    arguments[2] = wrapPoolMethod(createConnectionPool)
+
+    PoolBase.apply(this, arguments)
+
+    shimmer.wrap(this, 'query', createWrapQuery(options.connOptions))
+  }
+}
+
+// It's not possible to prevent connection pools from leaking across queries,
+// so instead we just skip instrumentation completely to avoid memory leaks
+// and/or orphan spans.
+function wrapPoolMethod (createConnection) {
+  return function () {
+    skipCh.publish()
+    try {
+      return createConnection.apply(this, arguments)
+    } finally {
+      unskipCh.publish()
+    }
+  }
+}
+
 const name = 'mariadb'
-const versions = ['>=2.0.3']
 
-addHook({ name, file: 'lib/cmd/query.js', versions }, (Query) => {
-  shimmer.wrap(Query.prototype, 'start', wrapCommandStart)
-
-  return Query
+addHook({ name, file: 'lib/cmd/query.js', versions: ['>=3'] }, (Query) => {
+  return wrapCommand(Query)
 })
 
-addHook({ name, file: 'lib/cmd/execute.js', versions }, (Query) => {
-  shimmer.wrap(Query.prototype, 'start', wrapCommandStart)
+addHook({ name, file: 'lib/cmd/execute.js', versions: ['>=3'] }, (Execute) => {
+  return wrapCommand(Execute)
+})
 
-  return Query
+addHook({ name, file: 'lib/pool.js', versions: ['>=3'] }, (Pool) => {
+  shimmer.wrap(Pool.prototype, '_createConnection', wrapPoolMethod)
+
+  return Pool
+})
+
+addHook({ name, file: 'lib/connection.js', versions: ['>=2.5.2 <3'] }, (Connection) => {
+  return shimmer.wrap(Connection, wrapConnection(Connection, '_queryPromise'))
+})
+
+addHook({ name, file: 'lib/connection.js', versions: ['>=2.0.4 <=2.5.1'] }, (Connection) => {
+  return shimmer.wrap(Connection, wrapConnection(Connection, 'query'))
+})
+
+addHook({ name, file: 'lib/pool-base.js', versions: ['>=2.0.4 <3'] }, (PoolBase) => {
+  return shimmer.wrap(PoolBase, wrapPoolBase(PoolBase))
 })
