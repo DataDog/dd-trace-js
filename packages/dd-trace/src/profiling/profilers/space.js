08b0c7fc6254a185afd2f2ad63743254f1f969c9
Nicolas Savoire <nicolas.savoire@datadoghq.com>
2023-03-25 13:02:04 +0100

    Export heap profile on v8 heap OOM (#2882)
    * Add OOM monitoring experimental feature.
    
    * Add integration tests for OOM monitoring
    
    * Bump pprof required version

--- packages/dd-trace/src/profiling/profilers/space.js
+++ packages/dd-trace/src/profiling/profilers/space.js
@@ -1,17 +1,37 @@
 'use strict'
 
+const { oomExportStrategies } = require('../constants')
+
+function strategiesToCallbackMode (strategies, callbackMode) {
+  const hasInterrupt = strategies.includes(oomExportStrategies.INTERRUPT_CALLBACK) ? callbackMode.Interrupt : 0
+  const hasCallback = strategies.includes(oomExportStrategies.ASYNC_CALLBACK) ? callbackMode.Async : 0
+  return hasInterrupt | hasCallback
+}
+
 class NativeSpaceProfiler {
   constructor (options = {}) {
     this.type = 'space'
     this._samplingInterval = options.samplingInterval || 512 * 1024
     this._stackDepth = options.stackDepth || 64
     this._pprof = undefined
+    this._oomMonitoring = options.oomMonitoring || {}
   }
 
-  start ({ mapper } = {}) {
+  start ({ mapper, nearOOMCallback } = {}) {
     this._mapper = mapper
     this._pprof = require('@datadog/pprof')
     this._pprof.heap.start(this._samplingInterval, this._stackDepth)
+    if (this._oomMonitoring.enabled) {
+      const strategies = this._oomMonitoring.exportStrategies
+      this._pprof.heap.monitorOutOfMemory(
+        this._oomMonitoring.heapLimitExtensionSize,
+        this._oomMonitoring.maxHeapExtensionCount,
+        strategies.includes(oomExportStrategies.LOGS),
+        strategies.includes(oomExportStrategies.PROCESS) ? this._oomMonitoring.exportCommand : [],
+        (profile) => nearOOMCallback(this.type, this._pprof.encodeSync(profile)),
+        strategiesToCallbackMode(strategies, this._pprof.heap.CallbackMode)
+      )
+    }
   }
 
   profile () {
