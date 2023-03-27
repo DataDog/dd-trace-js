9767f5f56a751f68abd521336fa283217c35600c
Jordi Bertran de Balanda <jordi.bertran@datadoghq.com>
2023-03-27 18:35:06 +0200

    add attribute schema v0 for rhea

--- packages/dd-trace/src/service-naming/schemas/v0.js
+++ packages/dd-trace/src/service-naming/schemas/v0.js
@@ -1,1 +1,16 @@
-module.exports = {}
+module.exports = {
+  messaging: {
+    outbound: {
+      rhea: {
+        opName: () => 'amqp.send',
+        serviceName: (ddService) => `${ddService}-amqp-producer`
+      }
+    },
+    inbound: {
+      rhea: {
+        opName: () => 'amqp.receive',
+        serviceName: (ddService) => ddService
+      }
+    }
+  }
+}
