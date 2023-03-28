ab41540518e28de49ad61c92c18775b2bebb93b1
Juan Antonio Fernández de Alba <juan.fernandezdealba@datadoghq.com>
2023-03-28 14:25:34 +0200

    Use os instead of node:os (#2948)

--- scripts/install_plugin_modules.js
+++ scripts/install_plugin_modules.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const fs = require('fs')
-const os = require('node:os')
+const os = require('os')
 const path = require('path')
 const crypto = require('crypto')
 const semver = require('semver')
