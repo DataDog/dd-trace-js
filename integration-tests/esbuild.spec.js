b05ab695778aed468bfd8f8904b664e6397580cd
Thomas Hunter II <tlhunter@datadog.com>
2023-03-31 11:31:03 -0700

    support bundling via esbuild (#2763)

---
+++ integration-tests/esbuild.spec.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+'use strict'
+
+const chproc = require('child_process')
+const path = require('path')
+
+const CWD = process.cwd()
+const TEST_DIR = path.join(__dirname, 'esbuild')
+
+// eslint-disable-next-line no-console
+console.log(`cd ${TEST_DIR}`)
+process.chdir(TEST_DIR)
+
+// eslint-disable-next-line no-console
+console.log('npm run build')
+chproc.execSync('npm run build')
+
+// eslint-disable-next-line no-console
+console.log('npm run built')
+try {
+  chproc.execSync('npm run built', {
+    timeout: 1000 * 30
+  })
+} catch (err) {
+  // eslint-disable-next-line no-console
+  console.error(err)
+  process.exit(1)
+} finally {
+  process.chdir(CWD)
+}
