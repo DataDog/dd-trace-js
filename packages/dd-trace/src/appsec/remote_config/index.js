65a17d2788b042a48d37d82592f9abbd2211b9ab
simon-id <simon.id@datadoghq.com>
2023-04-05 11:20:18 +0200

    batch  all waf update RC subscriptions

--- packages/dd-trace/src/appsec/remote_config/index.js
+++ packages/dd-trace/src/appsec/remote_config/index.js
@@ -2,7 +2,7 @@
 
 const RemoteConfigManager = require('./manager')
 const RemoteConfigCapabilities = require('./capabilities')
-const { updateAsmData, updateAsmDD, updateAsm } = require('../rule_manager')
+const { updateWafFromRC } = require('../rule_manager')
 
 let rc
 
@@ -32,59 +32,41 @@ function enable (config) {
   }
 }
 
-function enableAsmData (appsecConfig) {
+function enableWafUpdate (appsecConfig) {
   if (rc && appsecConfig && appsecConfig.rules === undefined) {
     rc.updateCapabilities(RemoteConfigCapabilities.ASM_IP_BLOCKING, true)
     rc.updateCapabilities(RemoteConfigCapabilities.ASM_USER_BLOCKING, true)
-    rc.on('ASM_DATA', updateAsmData)
-  }
-}
-
-function disableAsmData () {
-  if (rc) {
-    rc.updateCapabilities(RemoteConfigCapabilities.ASM_IP_BLOCKING, false)
-    rc.updateCapabilities(RemoteConfigCapabilities.ASM_USER_BLOCKING, false)
-    rc.off('ASM_DATA', updateAsmData)
-  }
-}
-
-function enableAsmDD (appsecConfig) {
-  if (rc && appsecConfig && appsecConfig.rules === undefined) {
-    rc.updateCapabilities(RemoteConfigCapabilities.ASM_DD_RULES, true)
-    rc.on('ASM_DD', updateAsmDD)
-  }
-}
-
-function disableAsmDD () {
-  if (rc) {
-    rc.updateCapabilities(RemoteConfigCapabilities.ASM_DD_RULES, false)
-    rc.off('ASM_DD', updateAsmDD)
-  }
-}
-
-function enableAsm (appsecConfig) {
-  if (rc && appsecConfig && appsecConfig.rules === undefined) {
     // TODO: we should have a different capability for rule override
     rc.updateCapabilities(RemoteConfigCapabilities.ASM_DD_RULES, true)
     rc.updateCapabilities(RemoteConfigCapabilities.ASM_EXCLUSIONS, true)
-    rc.on('ASM', updateAsm)
+
+    rc.on('ASM_DATA', noop)
+    rc.on('ASM_DD', noop)
+    rc.on('ASM', noop)
+
+    rc.on(RemoteConfigManager.kPreUpdate, updateWafFromRC)
   }
 }
 
-function disableAsm () {
+function disableWafUpdate () {
   if (rc) {
+    rc.updateCapabilities(RemoteConfigCapabilities.ASM_IP_BLOCKING, false)
+    rc.updateCapabilities(RemoteConfigCapabilities.ASM_USER_BLOCKING, false)
     rc.updateCapabilities(RemoteConfigCapabilities.ASM_DD_RULES, false)
     rc.updateCapabilities(RemoteConfigCapabilities.ASM_EXCLUSIONS, false)
-    rc.off('ASM', updateAsm)
+
+    rc.off('ASM_DATA', noop)
+    rc.off('ASM_DD', noop)
+    rc.off('ASM', noop)
+
+    rc.off(RemoteConfigManager.kPreUpdate, updateWafFromRC)
   }
 }
+
+function noop () {}
 
 module.exports = {
   enable,
-  enableAsmData,
-  disableAsmData,
-  enableAsmDD,
-  disableAsmDD,
-  enableAsm,
-  disableAsm
+  enableWafUpdate,
+  disableWafUpdate
 }
