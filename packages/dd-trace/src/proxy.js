211ccbc625c69c1f90d3f98f2f358cd0343be774
David Lee <david.lee@datadoghq.com>
2023-04-12 12:07:21 -0700

    change info log to debug

--- packages/dd-trace/src/proxy.js
+++ packages/dd-trace/src/proxy.js
@@ -83,7 +83,7 @@ function maybeStartServerlessMiniAgent () {
   if (!isGCPFunction) {
     return
   }
-  log.info('Starting Serverless Mini Agent')
+  log.debug('Starting Serverless Mini Agent')
   if (!rustBinaryPath) {
     log.error('Serverless Mini Agent did not start. Please provide a DD_MINI_AGENT_PATH environment variable.')
   }
@@ -94,10 +94,10 @@ function maybeStartServerlessMiniAgent () {
     const miniAgentProcess = spawn(rustBinaryPath)
 
     miniAgentProcess.stdout.on('data', (data) => {
-      log.info(data.toString())
+      log.debug(data.toString())
     })
     miniAgentProcess.on('close', (code) => {
-      log.info(`Mini Agent exited with code ${code}`)
+      log.debug(`Mini Agent exited with code ${code}`)
     })
     miniAgentProcess.on('error', (err) => {
       log.error(`Mini Agent errored out: ${err}`)
