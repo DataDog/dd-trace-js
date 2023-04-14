ff1a8b21a3896b9d6ffed4ef1230b4afde94cd69
rochdev <roch.devost@gmail.com>
2023-04-14 15:01:49 -0400

    update sirun benchmark variants to run in parallel

--- benchmark/sirun/run-one-variant.js
+++ benchmark/sirun/run-one-variant.js
@@ -3,8 +3,6 @@
 'use strict'
 
 const childProcess = require('child_process')
-const fs = require('fs')
-const path = require('path')
 const readline = require('readline')
 
 process.env.DD_TRACE_TELEMETRY_ENABLED = 'false'
@@ -24,9 +22,6 @@ function exec (...args) {
   })
 }
 
-require('./squash-affinity')
-
-const metaJson = require(path.join(process.cwd(), 'meta.json'))
 const env = Object.assign({}, process.env, { DD_TRACE_STARTUP_LOGS: 'false' })
 
 function streamAddVersion (input) {
@@ -50,21 +45,7 @@ function getStdio () {
 
 (async () => {
   try {
-    if (metaJson.variants) {
-      const variants = metaJson.variants
-      for (const variant in variants) {
-        const variantEnv = Object.assign({}, env, { SIRUN_VARIANT: variant })
-        await exec('sirun', ['meta-temp.json'], { env: variantEnv, stdio: getStdio() })
-      }
-    } else {
-      await exec('sirun', ['meta-temp.json'], { env, stdio: getStdio() })
-    }
-
-    try {
-      fs.unlinkSync(path.join(process.cwd(), 'meta-temp.json'))
-    } catch (e) {
-      // it's ok if we can't delete a temp file
-    }
+    await exec('sirun', ['meta-temp.json'], { env, stdio: getStdio() })
   } catch (e) {
     setImmediate(() => {
       throw e // Older Node versions don't fail on uncaught promise rejections.
--- benchmark/sirun/runall.sh
+++ benchmark/sirun/runall.sh
@@ -33,21 +33,45 @@ export ENABLE_AFFINITY=true
 echo "using Node.js ${VERSION}"
 CPU_AFFINITY=24 # reset for each node.js version
 
-for D in *; do
-  if [ -d "${D}" ]; then
-    echo "running ${D} in background, pinned to core ${CPU_AFFINITY}..."
-    cd "${D}"
-    (time node ../run-all-variants.js >> ../results.ndjson && echo "${D} finished.") &
-    cd ..
+run_all_variants () {
+  local variants="$(node ../get-variants.js)"
+
+  node ../squash-affinity.js
+
+  for V in $variants; do
+    echo "running ${1}/${V} in background, pinned to core ${CPU_AFFINITY}..."
+
+    export SIRUN_VARIANT=$V
+
+    (time node ../run-one-variant.js >> ../results.ndjson && echo "${1}/${V} finished.") &
     ((CPU_AFFINITY=CPU_AFFINITY+1))
+  done
+}
+
+for D in *; do
+  if [ "${D}" == "encoding" ]; then
+    cd "${D}"
+
+    run_all_variants $D
+
+    cd ..
   fi
 done
 
 wait # waits until all tests are complete before continuing
 
+# TODO: cleanup even when something fails
+for D in *; do
+  if [ -d "${D}" ]; then
+    unlink "${D}/meta-temp.json" 2>/dev/null
+  fi
+done
+
 node ./strip-unwanted-results.js
 
-echo "Benchmark Results:"
-cat ./results.ndjson
+if [ "$DEBUG_RESULTS" == "true" ]; then
+  echo "Benchmark Results:"
+  cat ./results.ndjson
+fi
 
 echo "all tests for ${VERSION} have now completed."
