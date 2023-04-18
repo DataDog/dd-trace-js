fc169d919174d8ba2d31555fe6eae2638d9fcd7e
David Lee <david.lee@datadoghq.com>
2023-04-18 10:29:54 -0700

    move maybeStartServerlessMiniAgent out of proxy

--- packages/dd-trace/src/proxy.js
+++ packages/dd-trace/src/proxy.js
@@ -9,6 +9,7 @@ const telemetry = require('./telemetry')
 const PluginManager = require('./plugin_manager')
 const remoteConfig = require('./appsec/remote_config')
 const AppsecSdk = require('./appsec/sdk')
+const { maybeStartServerlessMiniAgent } = require('./serverless')
 
 class Tracer extends NoopProxy {
   constructor () {
@@ -73,37 +74,6 @@ class Tracer extends NoopProxy {
   use () {
     this._pluginManager.configurePlugin(...arguments)
     return this
-  }
-}
-
-function maybeStartServerlessMiniAgent () {
-  const isGCPFunction = process.env.K_SERVICE !== undefined || process.env.FUNCTION_NAME !== undefined
-  const rustBinaryPath = process.env.DD_MINI_AGENT_PATH
-
-  if (!isGCPFunction) {
-    return
-  }
-  if (!rustBinaryPath) {
-    log.error('Serverless Mini Agent did not start. Please provide a DD_MINI_AGENT_PATH environment variable.')
-    return
-  }
-
-  try {
-    const { spawn } = require('child_process')
-
-    const miniAgentProcess = spawn(rustBinaryPath)
-
-    miniAgentProcess.stdout.on('data', (data) => {
-      log.debug(data.toString())
-    })
-    miniAgentProcess.on('close', (code) => {
-      log.debug(`Mini Agent exited with code ${code}`)
-    })
-    miniAgentProcess.on('error', (err) => {
-      log.error(`Mini Agent errored out: ${err}`)
-    })
-  } catch (err) {
-    log.error(`Error spawning mini agent process: ${err}`)
   }
 }
 
---
+++ packages/dd-trace/src/serverless.js
@@ -0,0 +1,37 @@
+'use strict'
+const log = require('./log')
+
+function maybeStartServerlessMiniAgent () {
+  const isGCPFunction = process.env.K_SERVICE !== undefined || process.env.FUNCTION_NAME !== undefined
+  const rustBinaryPath = process.env.DD_MINI_AGENT_PATH
+
+  if (!isGCPFunction) {
+    return
+  }
+  if (!rustBinaryPath) {
+    log.error('Serverless Mini Agent did not start. Please provide a DD_MINI_AGENT_PATH environment variable.')
+    return
+  }
+
+  try {
+    const { spawn } = require('child_process')
+
+    const miniAgentProcess = spawn(rustBinaryPath)
+
+    miniAgentProcess.stdout.on('data', (data) => {
+      log.debug(data.toString())
+    })
+    miniAgentProcess.on('close', (code) => {
+      log.debug(`Mini Agent exited with code ${code}`)
+    })
+    miniAgentProcess.on('error', (err) => {
+      log.error(`Mini Agent errored out: ${err}`)
+    })
+  } catch (err) {
+    log.error(`Error spawning mini agent process: ${err}`)
+  }
+}
+
+module.exports = {
+  maybeStartServerlessMiniAgent
+}
