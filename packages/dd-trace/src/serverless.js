d199ccce99c652c22c2a180668d129fe04a56611
David Lee <david.lee@datadoghq.com>
2023-04-18 14:27:48 -0700

    add check + test for invalid rust binary paths

--- packages/dd-trace/src/serverless.js
+++ packages/dd-trace/src/serverless.js
@@ -1,5 +1,6 @@
 'use strict'
 const log = require('./log')
+const fs = require('fs');
 
 function maybeStartServerlessMiniAgent () {
   const isGCPFunction = process.env.K_SERVICE !== undefined || process.env.FUNCTION_NAME !== undefined
@@ -10,6 +11,13 @@ function maybeStartServerlessMiniAgent () {
   }
   if (!rustBinaryPath) {
     log.error('Serverless Mini Agent did not start. Please provide a DD_MINI_AGENT_PATH environment variable.')
+    return
+  }
+
+  // trying to spawn with an invalid path will return a non-descriptive error, so we want to catch
+  // invalid paths and log our own error.
+  if (!fs.existsSync(rustBinaryPath)) {
+    log.error('Serverless Mini Agent did not start. DD_MINI_AGENT_PATH points to a non-existent file.')
     return
   }
 
--- packages/dd-trace/test/serverless.spec.js
+++ packages/dd-trace/test/serverless.spec.js
@@ -2,12 +2,14 @@
 
 const { maybeStartServerlessMiniAgent } = require('../src/serverless')
 const childProcess = require('child_process')
+const fs = require('fs');
+const log = require('../src/log')
 
 require('./setup/tap')
 
 describe('Serverless', () => {
   const childProcessSpawnSpy = sinon.spy(childProcess, 'spawn')
-
+  const existsSyncStub = sinon.stub(fs, "existsSync").returns(true)
   afterEach(() => {
     childProcessSpawnSpy.resetHistory()
   })
@@ -29,6 +31,25 @@ describe('Serverless', () => {
 
     expect(childProcessSpawnSpy).to.not.have.been.called
     delete process.env.K_SERVICE
+  })
+
+  it('dont spawn mini agent if mini agent path is invalid', () => {
+    process.env.K_SERVICE = 'test_function'
+    process.env.DD_MINI_AGENT_PATH = 'fake_path'
+
+    const logErrorSpy = sinon.spy(log, "error")
+
+    existsSyncStub.returns(false)
+
+    maybeStartServerlessMiniAgent()
+
+    // trying to spawn with an invalid path will return a non-descriptive error, so we want to catch
+    // invalid paths and log our own error.
+    expect(logErrorSpy).to.have.been.calledOnceWith("Serverless Mini Agent did not start. DD_MINI_AGENT_PATH points to a non-existent file.")
+
+    expect(childProcessSpawnSpy).to.not.have.been.called
+    delete process.env.K_SERVICE
+    existsSyncStub.returns(true)
   })
 
   it('spawn mini agent when FUNCTION_NAME env var is defined', () => {
