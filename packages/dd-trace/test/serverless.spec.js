219d3ad04600d414e5794fe7c846e0f9a5b3547a
David Lee <david.lee@datadoghq.com>
2023-04-24 16:27:04 -0700

    Update serverless.spec.js

--- packages/dd-trace/test/serverless.spec.js
+++ packages/dd-trace/test/serverless.spec.js
@@ -1,40 +1,62 @@
 'use strict'
 
-const { maybeStartServerlessMiniAgent } = require('../src/serverless')
-const childProcess = require('child_process')
 const fs = require('fs')
 const log = require('../src/log')
+const Proxy = require('../src/proxy')
+const serverless = require('../src/serverless')
 
 require('./setup/tap')
 
 describe('Serverless', () => {
-  const childProcessSpawnSpy = sinon.spy(childProcess, 'spawn')
+  const maybeStartServerlessMiniAgentSpy = sinon.spy(serverless, 'maybeStartServerlessMiniAgent')
   const existsSyncStub = sinon.stub(fs, 'existsSync').returns(true)
 
   let env
+  let proxy
 
   beforeEach(() => {
     env = process.env
     process.env = {}
+    proxy = new Proxy()
   })
 
   afterEach(() => {
     process.env = env
-    childProcessSpawnSpy.resetHistory()
+    maybeStartServerlessMiniAgentSpy.resetHistory()
   })
 
-  it('dont spawn mini agent if not in google cloud function', () => {
+  it('should not spawn mini agent if not in google cloud function', () => {
     // if (K_SERVICE && FUNCTION_TARGET) env vars are set, we are in a GCP function with a newer runtime
     // if (FUNCTION_NAME && GCP_PROJECT) env vars are set, we are in a GCP function with a deprecated runtime
     process.env.DD_MINI_AGENT_PATH = 'fake_path'
 
-    maybeStartServerlessMiniAgent()
+    proxy.init()
 
-    expect(childProcessSpawnSpy).to.not.have.been.called
+    expect(maybeStartServerlessMiniAgentSpy).to.not.have.been.called
     delete process.env.DD_MINI_AGENT_PATH
   })
 
-  it('dont spawn mini agent if mini agent path is invalid', () => {
+  it('should spawn mini agent when FUNCTION_NAME and GCP_PROJECT env vars are defined', () => {
+    process.env.FUNCTION_NAME = 'test_function'
+    process.env.GCP_PROJECT = 'test_project'
+    process.env.DD_MINI_AGENT_PATH = 'fake_path'
+
+    proxy.init()
+
+    expect(maybeStartServerlessMiniAgentSpy).to.have.been.calledOnce
+  })
+
+  it('should spawn mini agent when K_SERVICE and FUNCTION_TARGET env vars are defined', () => {
+    process.env.K_SERVICE = 'test_function'
+    process.env.FUNCTION_TARGET = 'function_target'
+    process.env.DD_MINI_AGENT_PATH = 'fake_path'
+
+    proxy.init()
+
+    expect(maybeStartServerlessMiniAgentSpy).to.have.been.calledOnce
+  })
+
+  it('should log error if mini agent binary path is invalid', () => {
     process.env.K_SERVICE = 'test_function'
     process.env.FUNCTION_TARGET = 'function_target'
     process.env.DD_MINI_AGENT_PATH = 'fake_path'
@@ -43,35 +65,13 @@ describe('Serverless', () => {
 
     existsSyncStub.returns(false)
 
-    maybeStartServerlessMiniAgent()
+    proxy.init()
 
     // trying to spawn with an invalid path will return a non-descriptive error, so we want to catch
     // invalid paths and log our own error.
     expect(logErrorSpy).to.have.been.calledOnceWith(
       'Serverless Mini Agent did not start. Could not find mini agent binary.'
     )
-
-    expect(childProcessSpawnSpy).to.not.have.been.called
     existsSyncStub.returns(true)
-  })
-
-  it('spawn mini agent when FUNCTION_NAME and GCP_PROJECT env vars are defined', () => {
-    process.env.FUNCTION_NAME = 'test_function'
-    process.env.GCP_PROJECT = 'test_project'
-    process.env.DD_MINI_AGENT_PATH = 'fake_path'
-
-    maybeStartServerlessMiniAgent()
-
-    expect(childProcessSpawnSpy).to.have.been.calledOnce
-  })
-
-  it('spawn mini agent when K_SERVICE and FUNCTION_TARGET env vars are defined', () => {
-    process.env.K_SERVICE = 'test_function'
-    process.env.FUNCTION_TARGET = 'function_target'
-    process.env.DD_MINI_AGENT_PATH = 'fake_path'
-
-    maybeStartServerlessMiniAgent()
-
-    expect(childProcessSpawnSpy).to.have.been.calledOnce
   })
 })
