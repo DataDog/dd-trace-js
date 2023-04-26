df51e1b1cf6d35ab47d5acf917d8d41e8ce63d38
David Lee <david.lee@datadoghq.com>
2023-04-26 15:45:37 -0700

    test

--- packages/dd-trace/src/proxy.js
+++ packages/dd-trace/src/proxy.js
@@ -26,16 +26,7 @@ class Tracer extends NoopProxy {
     try {
       const config = new Config(options) // TODO: support dynamic config
 
-      // const isDeprecatedGCPFunction = process.env.FUNCTION_NAME !== undefined && process.env.GCP_PROJECT !== undefined
-      // const isNewerGCPFunction = process.env.K_SERVICE !== undefined && process.env.FUNCTION_TARGET !== undefined
-      // const inGCPFunction = isDeprecatedGCPFunction || isNewerGCPFunction
-
-      // if (inGCPFunction) {
-      //   const serverless = require('./serverless')
-      //   serverless.maybeStartServerlessMiniAgent()
-      // }
-
-      // const { isInGCPFunction } = require('./gcp_function/serverless')
+      require('./serverless').maybeStartServerlessMiniAgent()
 
       if (config.remoteConfig.enabled && !config.isCiVisibility) {
         remoteConfig.enable(config)
---
+++ packages/dd-trace/src/serverless.js
@@ -0,0 +1,50 @@
+'use strict'
+
+function maybeStartServerlessMiniAgent () {
+  const isDeprecatedGCPFunction = process.env.FUNCTION_NAME !== undefined && process.env.GCP_PROJECT !== undefined
+  const isNewerGCPFunction = process.env.K_SERVICE !== undefined && process.env.FUNCTION_TARGET !== undefined
+  const inGCPFunction = isDeprecatedGCPFunction || isNewerGCPFunction
+
+  if (!inGCPFunction) {
+    return
+  }
+
+  let rustBinaryPath =
+    '/workspace/node_modules/datadog-sma/datadog-serverless-agent-linux-amd64/datadog-serverless-trace-mini-agent'
+
+  if (process.env.DD_MINI_AGENT_PATH !== undefined) {
+    rustBinaryPath = process.env.DD_MINI_AGENT_PATH
+  }
+
+  const fs = require('fs')
+  const log = require('./log')
+
+  // trying to spawn with an invalid path will return a non-descriptive error, so we want to catch
+  // invalid paths and log our own error.
+  if (!fs.existsSync(rustBinaryPath)) {
+    log.error('Serverless Mini Agent did not start. Could not find mini agent binary.')
+    return
+  }
+
+  try {
+    const { spawn } = require('child_process')
+
+    const miniAgentProcess = spawn(rustBinaryPath)
+
+    miniAgentProcess.stdout.on('data', (data) => {
+      log.debug(data.toString())
+    })
+    miniAgentProcess.on('close', (code) => {
+      log.error(`Mini Agent exited with code ${code}`)
+    })
+    miniAgentProcess.on('error', (err) => {
+      log.error(`Mini Agent errored out: ${err}`)
+    })
+  } catch (err) {
+    log.error(`Error spawning mini agent process: ${err}`)
+  }
+}
+
+module.exports = {
+  maybeStartServerlessMiniAgent
+}
