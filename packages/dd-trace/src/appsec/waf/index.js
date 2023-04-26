0094f91c2caa273e2f12076499f0752f885ae6e1
Ugaitz Urien <ugaitz.urien@datadoghq.com>
2023-04-26 16:37:05 +0200

    Update appsec rules from Remote Config (#2654)
    * Add rules update and refactor whole waf
    
    * move appsec channels file
    
    * Add changes to work with libddwaf update method and support more RC properties
    
    * update waf bindings to 3.1.0
    
    * simplify reporter
    
    * remove unnecesary virtual addresses
    
    * cleanup outdated comment
    
    * simplify incomingHttpStartHandler
    
    * simplify waf_manager.js
    
    * add pre update hook to RC manager
    
    * update rc manager
    
    * batch all waf update RC subscriptions
    
    * put apply_states codes in a constant file
    
    * inline function
    
    * add update to waf manager
    
    * completely rework RC waf updates
    
    * renamed a few things in tests
    
    * fix dc path
    
    * fix webpack not parsing class fields
    
    * lazy load everything
    
    * fix unapply configs not being emitted
    
    ---------
    
    Co-authored-by: simon-id <simon.id@datadoghq.com>
    Co-authored-by: Julio Gonzalez <julio.gonzalez@datadoghq.com>

---
+++ packages/dd-trace/src/appsec/waf/index.js
@@ -0,0 +1,75 @@
+'use strict'
+
+const { storage } = require('../../../../datadog-core')
+const log = require('../../log')
+
+const waf = {
+  wafManager: null,
+  init,
+  destroy,
+  update,
+  run: noop,
+  disposeContext: noop
+}
+
+function init (rules, config) {
+  destroy()
+
+  // dirty require to make startup faster for serverless
+  const WAFManager = require('./waf_manager')
+
+  waf.wafManager = new WAFManager(rules, config)
+
+  waf.run = run
+  waf.disposeContext = disposeContext
+}
+
+function destroy () {
+  if (waf.wafManager) {
+    waf.wafManager.destroy()
+    waf.wafManager = null
+  }
+
+  waf.run = noop
+  waf.disposeContext = noop
+}
+
+function update (newRules) {
+  // TODO: check race conditions between Appsec enable/disable and WAF updates, the whole RC state management in general
+  if (!waf.wafManager) throw new Error('Cannot update disabled WAF')
+
+  try {
+    waf.wafManager.ddwaf.update(newRules)
+  } catch (err) {
+    log.error('Could not apply rules from remote config')
+    throw err
+  }
+}
+
+function run (data, req) {
+  if (!req) {
+    const store = storage.getStore()
+    if (!store || !store.req) {
+      log.warn('Request object not available in waf.run')
+      return
+    }
+
+    req = store.req
+  }
+
+  const wafContext = waf.wafManager.getWAFContext(req)
+
+  return wafContext.run(data)
+}
+
+function disposeContext (req) {
+  const wafContext = waf.wafManager.getWAFContext(req)
+
+  if (wafContext && !wafContext.ddwafContext.disposed) {
+    wafContext.dispose()
+  }
+}
+
+function noop () {}
+
+module.exports = waf
