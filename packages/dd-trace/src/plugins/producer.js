4fadc98aa08dc7de9174277dd0835622b05dc7b6
Jordi Bertran de Balanda <jbertran@users.noreply.github.com>
2023-04-27 10:59:00 +0200

    Introduce service naming framework (#2941)
    * Add interaction with the `DD_TRACE_SPAN_ATTRIBUTE_SCHEMA` environment variable
    * Provide the building blocks for 2 versions of an service and operation naming schema:
        * `v0` is the current version, i.e. a representation of the existing state of the plugin. One could checkout the commit with the introduction of this version, and have unchanged tests still pass.
        * `v1` is the new naming schema, made consistent across all tracers.
    * Introduce a testing harness for easy tests of `v0` _and_ `v1` naming
    * Apply this versioned naming for all messaging integrations - `rhea`, `amqp(10|lib)`, `kafkajs`, `google-cloud-pubsub`

--- packages/dd-trace/src/plugins/producer.js
+++ packages/dd-trace/src/plugins/producer.js
@@ -1,9 +1,24 @@
 'use strict'
 
-const OutgoingPlugin = require('./outgoing')
+const OutboundPlugin = require('./outbound')
 
-class ProducerPlugin extends OutgoingPlugin {
+class ProducerPlugin extends OutboundPlugin {
   static get operation () { return 'publish' }
+  static get kind () { return 'producer' }
+  static get type () { return 'messaging' }
+
+  startSpan (options) {
+    const spanDefaults = {
+      service: this.config.service || this.serviceName(),
+      kind: this.constructor.kind
+    }
+    Object.keys(spanDefaults).forEach(
+      key => {
+        if (!options[key]) options[key] = spanDefaults[key]
+      }
+    )
+    return super.startSpan(this.operationName(), options)
+  }
 }
 
 module.exports = ProducerPlugin
