a60bea855cc1a6691bce24d7c48b7684a66b32f1
simon-id <simon.id@datadoghq.com>
2023-04-28 11:42:31 +0200

    Update body-parser.js

--- packages/datadog-instrumentations/src/body-parser.js
+++ packages/datadog-instrumentations/src/body-parser.js
@@ -3,6 +3,7 @@
 const { AbortController } = require('node-abort-controller') // AbortController is not available in node <15
 const shimmer = require('../../datadog-shimmer')
 const { channel, addHook, AsyncResource } = require('./helpers/instrument')
+
 const bodyParserReadCh = channel('datadog:body-parser:read:finish')
 
 function publishRequestBodyAndNext (req, res, next) {
