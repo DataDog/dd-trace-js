38c642189610b4191ae2f5acef0b63460d314ab9
Thomas Hunter II <tlhunter@datadog.com>
2023-05-01 15:46:12 -0700

    getting messy

--- integration-tests/esbuild.spec.js
+++ integration-tests/esbuild.spec.js
@@ -7,14 +7,26 @@ const path = require('path')
 
 const CWD = process.cwd()
 const TEST_DIR = path.join(__dirname, 'esbuild')
+const DD_DIR = path.join(__dirname, '..')
+
+console.log(`cd ${DD_DIR}`)
+process.chdir(DD_DIR)
+
+console.log('yarn link')
+chproc.execSync('yarn link')
+
+console.log(`cd ${CWD}`)
+process.chdir(CWD)
+
+console.log('yarn link dd-trace')
+chproc.execSync('yarn link dd-trace')
+
+console.log('npm run build')
+chproc.execSync('npm run build')
 
 // eslint-disable-next-line no-console
 console.log(`cd ${TEST_DIR}`)
 process.chdir(TEST_DIR)
-
-// eslint-disable-next-line no-console
-console.log('bash npm run link')
-chproc.execSync('bash npm run link')
 
 // eslint-disable-next-line no-console
 console.log('npm run build')
