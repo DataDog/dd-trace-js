b33feaca8665c1c37d99ee4ec50a9a8a8623eaf5
Igor Unanua <igor.unanua@datadoghq.com>
2023-05-03 13:17:06 +0200

    Support array payloads in telemetry send-data (#3047)

--- packages/dd-trace/src/telemetry/send-data.js
+++ packages/dd-trace/src/telemetry/send-data.js
@@ -1,13 +1,23 @@
 const request = require('../exporters/common/request')
 let seqId = 0
+
+function getPayload (payload) {
+  // Some telemetry endpoints payloads accept collections of elements such as the 'logs' endpoint.
+  // 'logs' request type payload is meant to send library logs to Datadog’s backend.
+  if (Array.isArray(payload)) {
+    return payload
+  } else {
+    const { logger, tags, serviceMapping, ...trimmedPayload } = payload
+    return trimmedPayload
+  }
+}
+
 function sendData (config, application, host, reqType, payload = {}) {
   const {
     hostname,
     port,
     url
   } = config
-
-  const { logger, tags, serviceMapping, ...trimmedPayload } = payload
 
   const options = {
     url,
@@ -27,7 +37,7 @@ function sendData (config, application, host, reqType, payload = {}) {
     tracer_time: Math.floor(Date.now() / 1000),
     runtime_id: config.tags['runtime-id'],
     seq_id: ++seqId,
-    payload: trimmedPayload,
+    payload: getPayload(payload),
     application,
     host
   })
