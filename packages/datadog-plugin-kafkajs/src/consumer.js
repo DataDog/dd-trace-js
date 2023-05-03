cb44a6dc4fe1af2b2acc2348822d4c3c15b81e87
rochdev <roch.devost@gmail.com>
2023-05-03 17:36:12 -0400

    fix kafka plugin trying to extract null headers as string

--- packages/datadog-plugin-kafkajs/src/consumer.js
+++ packages/datadog-plugin-kafkajs/src/consumer.js
@@ -30,6 +30,8 @@ function extract (tracer, bufferMap) {
   const textMap = {}
 
   for (const key of Object.keys(bufferMap)) {
+    if (bufferMap[key] === null || bufferMap[key] === undefined) continue
+
     textMap[key] = bufferMap[key].toString()
   }
 
