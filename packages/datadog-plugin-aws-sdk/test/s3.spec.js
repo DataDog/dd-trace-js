5a1c746a160348dc8852fdd5d73bedea796b986b
Andrew Rodriguez <49878080+zARODz11z@users.noreply.github.com>
2023-05-04 14:07:33 -0700

    Consistent APM Span tags for AWS SDK Requests (#2930)
    * added region, aws_service, and resourcename i.e. queuename tags to existing services. Still lacking aws_account
    
    * add unit test assertions
    
    * change to rulename to match eventbridge metric tags
    
    * edit tests, aws_service, and xname tags
    
    * lint
    
    * test
    
    * Trigger CI
    
    * space
    
    * edit the resourcename
    
    * revert files
    
    * conflicts
    
    * whitespace
    
    * lint
    
    * remove only
    
    * fix some tests
    
    * lint
    
    * test
    
    * add broken in progress s3 spec
    
    * add dynamodb & s3 spec
    
    * lint and increase timeout for all tests to very high number
    
    * empty commit
    
    * only additive changes for new tags queuename, region, aws_service
    
    * fix eventbridge
    
    * add sqs assertions
    
    * sqs spec testing
    
    * sqs changes
    
    * fix sqs
    
    * space
    
    * sns and sqs
    
    * s3 spec with post to reset since localstack has bug deleting bucket
    
    * add lambda metadata assertions
    
    * remove only
    
    * add kinesis assertions
    
    * add eventbridge and fix s3 only
    
    * dynamodb spec
    
    * add assertions to aws-sdk spec
    
    * eventbridge tests
    
    * re add removed tag
    
    * lint and remove version constraints
    
    * remove timeouts
    
    * add dynamo to externals
    
    * see if dynamo was the only problem

---
+++ packages/datadog-plugin-aws-sdk/test/s3.spec.js
@@ -0,0 +1,96 @@
+'use strict'
+
+const agent = require('../../dd-trace/test/plugins/agent')
+const { setup } = require('./spec_helpers')
+const axios = require('axios')
+
+const bucketName = 's3-bucket-name-test'
+
+/* eslint-disable no-console */
+async function resetLocalStackS3 () {
+  try {
+    await axios.post('http://localhost:4566/reset')
+    console.log('LocalStack S3 reset successful')
+  } catch (error) {
+    console.error('Error resetting LocalStack S3:', error.message)
+  }
+}
+
+describe('Plugin', () => {
+  describe('aws-sdk (s3)', function () {
+    setup()
+
+    withVersions('aws-sdk', ['aws-sdk', '@aws-sdk/smithy-client'], (version, moduleName) => {
+      let AWS
+      let s3
+      let tracer
+
+      const s3ClientName = moduleName === '@aws-sdk/smithy-client' ? '@aws-sdk/client-s3' : 'aws-sdk'
+      describe('with configuration', () => {
+        before(() => {
+          tracer = require('../../dd-trace')
+          tracer.init()
+          return agent.load('aws-sdk')
+        })
+
+        before(done => {
+          AWS = require(`../../../versions/${s3ClientName}@${version}`).get()
+
+          s3 = new AWS.S3({ endpoint: 'http://127.0.0.1:4566', s3ForcePathStyle: true, region: 'us-east-1' })
+          s3.createBucket({ Bucket: bucketName }, (err) => {
+            if (err) return done(err)
+            done()
+          })
+        })
+
+        after(done => {
+          s3.deleteBucket({ Bucket: bucketName }, () => {
+            done()
+          })
+        })
+
+        after(async () => {
+          await resetLocalStackS3()
+          return agent.close({ ritmReset: false })
+        })
+
+        it('should allow disabling a specific span kind of a service', (done) => {
+          let total = 0
+
+          agent.use(traces => {
+            const span = traces[0][0]
+            expect(span).to.include({
+              name: 'aws.request',
+              resource: `putObject ${bucketName}`
+            })
+
+            expect(span.meta).to.include({
+              'bucketname': bucketName,
+              'aws_service': 'S3',
+              'region': 'us-east-1'
+            })
+
+            total++
+          }).catch(() => {}, { timeoutMs: 100 })
+
+          s3.putObject({
+            Bucket: bucketName,
+            Key: 'test-key',
+            Body: 'test body'
+          }, (err) => {
+            if (err) return done(err)
+
+            setTimeout(() => {
+              try {
+                expect(total).to.equal(1)
+                done()
+              } catch (e) {
+                done(e)
+              }
+            }, 250)
+          })
+        })
+      })
+    })
+  })
+})
