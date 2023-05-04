2e7ca68edb286a899a7701a29d3820241ee128f7
Juan Antonio Fernández de Alba <juan.fernandezdealba@datadoghq.com>
2023-05-04 16:43:04 +0200

    [ci-visibility] Fix test.fixme logic in playwright (#3100)

--- packages/datadog-instrumentations/src/playwright.js
+++ packages/datadog-instrumentations/src/playwright.js
@@ -214,10 +214,20 @@ function runnerHook (runnerExport, playwrightVersion) {
     })
 
     const runAllTestsReturn = await runAllTests.apply(this, arguments)
+
+    Object.values(remainingTestsByFile).forEach(tests => {
+      // `tests` should normally be empty, but if it isn't,
+      // there were tests that did not go through `testBegin` or `testEnd`,
+      // because they were skipped
+      tests.forEach(test => {
+        testBeginHandler(test)
+        testEndHandler(test, 'skip')
+      })
+    })
+
     const sessionStatus = runAllTestsReturn.status || runAllTestsReturn
 
     let onDone
-
     const flushWait = new Promise(resolve => {
       onDone = resolve
     })
