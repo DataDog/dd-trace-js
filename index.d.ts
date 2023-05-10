e51d9c31d10282b867d9c2acc8442a93c8a55aef
CarlesDD <carles.capell@datadoghq.com>
2023-05-10 09:06:42 +0200

    Add default value to IAST enable redaction config flag

--- index.d.ts
+++ index.d.ts
@@ -442,6 +442,7 @@ export declare interface TracerOptions {
       deduplicationEnabled?: boolean
       /**
        * Whether to enable vulnerability redaction
+       * @default true
        */
       redactionEnabled?: boolean
     }
