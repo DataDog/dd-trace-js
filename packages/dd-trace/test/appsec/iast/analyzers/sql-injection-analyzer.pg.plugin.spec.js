3a0d70d07c492d6263a20835037d603c49957ee8
CarlesDD <carles.capell@datadoghq.com>
2023-05-17 15:41:04 +0200

    Test IAST pg with native Client

--- packages/dd-trace/test/appsec/iast/analyzers/sql-injection-analyzer.pg.plugin.spec.js
+++ packages/dd-trace/test/appsec/iast/analyzers/sql-injection-analyzer.pg.plugin.spec.js
@@ -5,6 +5,7 @@ const { storage } = require('../../../../../datadog-core')
 const iastContextFunctions = require('../../../../src/appsec/iast/iast-context')
 const { newTaintedString } = require('../../../../src/appsec/iast/taint-tracking/operations')
 const vulnerabilityReporter = require('../../../../src/appsec/iast/vulnerability-reporter')
+const semver = require("semver");
 
 const clients = {
   pg: pg => pg.Client
@@ -17,10 +18,11 @@ if (process.env.PG_TEST_NATIVE === 'true') {
 describe('sql-injection-analyzer with pg', () => {
   let pg
   let client
-  withVersions('pg', 'pg', version => {
+  const range = semver.satisfies(process.versions.node, '^12.0.0') ? '>=4.5.5' : '>=8.0.3'
+
+  withVersions('pg', 'pg', range, version => {
     Object.keys(clients).forEach(implementation => {
       describe(`when using ${implementation}.Client`, () => {
-
         prepareTestServerForIast('pg', (testThatRequestHasVulnerability, testThatRequestHasNoVulnerability) => {
           beforeEach((done) => {
             pg = require(`../../../../../../versions/pg@${version}`).get()
@@ -33,10 +35,7 @@ describe('sql-injection-analyzer with pg', () => {
               application_name: 'test'
             })
             vulnerabilityReporter.clearCache()
-            client.connect(err => {
-              console.log(err)
-              done(err)
-            })
+            client.connect(done)
           })
 
           afterEach(async () => {
