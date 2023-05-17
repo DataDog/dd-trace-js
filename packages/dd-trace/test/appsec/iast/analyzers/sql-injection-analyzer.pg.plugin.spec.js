b805ed776951a5d7cf30b8798f5371729f677a0e
CarlesDD <carles.capell@datadoghq.com>
2023-05-17 09:50:07 +0200

    Test IAST pg with native Client

--- packages/dd-trace/test/appsec/iast/analyzers/sql-injection-analyzer.pg.plugin.spec.js
+++ packages/dd-trace/test/appsec/iast/analyzers/sql-injection-analyzer.pg.plugin.spec.js
@@ -1,46 +1,58 @@
 'use strict'
 
-//const semver = require('semver')
 const { prepareTestServerForIast } = require('../utils')
 const { storage } = require('../../../../../datadog-core')
 const iastContextFunctions = require('../../../../src/appsec/iast/iast-context')
 const { newTaintedString } = require('../../../../src/appsec/iast/taint-tracking/operations')
 const vulnerabilityReporter = require('../../../../src/appsec/iast/vulnerability-reporter')
 
+const clients = {
+  pg: pg => pg.Client
+}
+
+if (process.env.PG_TEST_NATIVE === 'true') {
+  clients['pg.native'] = pg => pg.native.Client
+}
+
 describe('sql-injection-analyzer with pg', () => {
   let pg
   let client
   withVersions('pg', 'pg', version => {
-    //if (semver.satisfies(version, '<8.0.3')) return
-    prepareTestServerForIast('pg', (testThatRequestHasVulnerability, testThatRequestHasNoVulnerability) => {
-      beforeEach((done) => {
-        pg = require(`../../../../../../versions/pg@${version}`).get()
-        client = new pg.native.Client({
-          user: 'postgres',
-          password: 'postgres',
-          database: 'postgres',
-          application_name: 'test'
+    Object.keys(clients).forEach(implementation => {
+      describe(`when using ${implementation}.Client`, () => {
+
+        prepareTestServerForIast('pg', (testThatRequestHasVulnerability, testThatRequestHasNoVulnerability) => {
+          beforeEach((done) => {
+            pg = require(`../../../../../../versions/pg@${version}`).get()
+            const Client = clients[implementation](pg)
+            client = new Client({
+              user: 'postgres',
+              password: 'postgres',
+              database: 'postgres',
+              application_name: 'test'
+            })
+            vulnerabilityReporter.clearCache()
+            client.connect(err => done(err))
+          })
+
+          afterEach(async () => {
+            await client.end()
+          })
+
+          testThatRequestHasVulnerability(() => {
+            const store = storage.getStore()
+            const iastCtx = iastContextFunctions.getIastContext(store)
+            let sql = 'SELECT 1'
+            sql = newTaintedString(iastCtx, sql, 'param', 'Request')
+            return client.query(sql)
+          }, 'SQL_INJECTION')
+
+          testThatRequestHasNoVulnerability(() => {
+            const sql = 'SELECT 1'
+            return client.query(sql)
+          }, 'SQL_INJECTION')
         })
-        vulnerabilityReporter.clearCache()
-        client.connect(err => done(err))
       })
-
-      afterEach(async () => {
-        await client.end()
-      })
-
-      testThatRequestHasVulnerability(() => {
-        const store = storage.getStore()
-        const iastCtx = iastContextFunctions.getIastContext(store)
-        let sql = 'SELECT 1'
-        sql = newTaintedString(iastCtx, sql, 'param', 'Request')
-        return client.query(sql)
-      }, 'SQL_INJECTION')
-
-      testThatRequestHasNoVulnerability(() => {
-        const sql = 'SELECT 1'
-        return client.query(sql)
-      }, 'SQL_INJECTION')
     })
   })
 })
