57b83359342e167a2a8699d598c790ce1e6fab33
Ugaitz Urien <ugaitz.urien@datadoghq.com>
2023-05-17 12:35:49 +0200

    Fix test

--- packages/dd-trace/test/appsec/iast/taint-tracking/plugin.spec.js
+++ packages/dd-trace/test/appsec/iast/taint-tracking/plugin.spec.js
@@ -25,9 +25,10 @@ describe('IAST Taint tracking plugin', () => {
   afterEach(sinon.restore)
 
   it('Should subscribe to body parser and qs channel', () => {
-    expect(taintTrackingPlugin._subscriptions).to.have.lengthOf(2)
+    expect(taintTrackingPlugin._subscriptions).to.have.lengthOf(3)
     expect(taintTrackingPlugin._subscriptions[0]._channel.name).to.equals('datadog:body-parser:read:finish')
     expect(taintTrackingPlugin._subscriptions[1]._channel.name).to.equals('datadog:qs:parse:finish')
+    expect(taintTrackingPlugin._subscriptions[2]._channel.name).to.equals('apm:express:middleware:next')
   })
 
   it('Should taint full object', () => {
