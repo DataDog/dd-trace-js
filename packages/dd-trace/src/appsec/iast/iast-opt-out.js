c9ae7da2056f6113ee863f334656cbd5f2a4c8e4
Ugaitz Urien <ugaitz.urien@datadoghq.com>
2023-05-20 11:55:09 +0200

    Set optimal behaviour

--- packages/dd-trace/src/appsec/iast/iast-opt-out.js
+++ packages/dd-trace/src/appsec/iast/iast-opt-out.js
@@ -1,6 +1,6 @@
 'use strict'
 
-// const { enableOptOutAnalyzers, disableOptOutAnalyzers } = require('./analyzers')
+const { enableOptOutAnalyzers, disableOptOutAnalyzers } = require('./analyzers')
 const overheadController = require('./overhead-controller')
 const dc = require('../../../../diagnostics_channel')
 const { storage } = require('../../../../datadog-core')
@@ -13,37 +13,37 @@ const requestStart = dc.channel('dd-trace:incomingHttpRequestStart')
 const requestClose = dc.channel('dd-trace:incomingHttpRequestEnd')
 
 function enable (config, _tracer) {
-  // requestStart.subscribe(onIncomingHttpRequestStart)
+  requestStart.subscribe(onIncomingHttpRequestStart)
   overheadController.configure(config.iast)
   // overheadController.startGlobalContext()
 }
 
 let isInIastRequest = false
-//
-// function onIncomingHttpRequestStart (data) {
-//   if (!isInIastRequest && Math.random() < 0) {
-//     isInIastRequest = true
-//     if (data && data.req) {
-//       const store = storage.getStore()
-//       if (store) {
-//         const topContext = web.getContext(data.req)
-//         if (topContext) {
-//           const rootSpan = topContext.span
-//           const iastContext = iastContextFunctions.saveIastContext(store, topContext, { rootSpan, req: data.req })
-//           overheadController.initializeRequestContext(iastContext)
-//           if (rootSpan.addTags) {
-//             rootSpan.addTags({
-//               [IAST_ENABLED_TAG_KEY]: 1
-//             })
-//           }
-//           enableOptOutAnalyzers()
-//           requestClose.subscribe(onIncomingHttpRequestEnd)
-//         }
-//       }
-//     }
-//   }
-// }
-//
+
+function onIncomingHttpRequestStart (data) {
+  if (!isInIastRequest && Math.random() < 0.001) {
+    isInIastRequest = true
+    if (data && data.req) {
+      const store = storage.getStore()
+      if (store) {
+        const topContext = web.getContext(data.req)
+        if (topContext) {
+          const rootSpan = topContext.span
+          const iastContext = iastContextFunctions.saveIastContext(store, topContext, { rootSpan, req: data.req })
+          overheadController.initializeRequestContext(iastContext)
+          if (rootSpan.addTags) {
+            rootSpan.addTags({
+              [IAST_ENABLED_TAG_KEY]: 1
+            })
+          }
+          enableOptOutAnalyzers()
+          requestClose.subscribe(onIncomingHttpRequestEnd)
+        }
+      }
+    }
+  }
+}
+
 function onIncomingHttpRequestEnd (data) {
   if (data && data.req) {
     const store = storage.getStore()
