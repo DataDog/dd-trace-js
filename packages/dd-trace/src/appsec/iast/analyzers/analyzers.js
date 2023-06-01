3d991a64b8c107eb4072f5a18e0496aa6eac8011
Ugaitz Urien <ugaitz.urien@datadoghq.com>
2023-06-01 08:23:45 +0200

    Detect SSRF vulnerabilities (#3115)
    * Detect SSRF vulnerabilities
    
    * Fix test
    
    * Add space
    
    * Understand arguments in publisher instead of the subscriber
    
    * Redact sensitive information in SSRF vulnerabilities
    
    * Tiny style change
    
    * Tiny code styles
    
    * Use SSRF enum instead of literal
    
    * Try to reduce flaky test
    
    * Rename originalArgs to originalUrlAndOptions
    
    * Fix comment in PR
    
    * Do not normalize arguments twice in http/client.js

--- packages/dd-trace/src/appsec/iast/analyzers/analyzers.js
+++ packages/dd-trace/src/appsec/iast/analyzers/analyzers.js
@@ -1,10 +1,11 @@
 'use strict'
 
 module.exports = {
-  'WEAK_CIPHER_ANALYZER': require('./weak-cipher-analyzer'),
-  'WEAK_HASH_ANALYZER': require('./weak-hash-analyzer'),
-  'SQL_INJECTION_ANALYZER': require('./sql-injection-analyzer'),
-  'PATH_TRAVERSAL_ANALYZER': require('./path-traversal-analyzer'),
   'COMMAND_INJECTION_ANALYZER': require('./command-injection-analyzer'),
-  'LDAP_ANALYZER': require('./ldap-injection-analyzer')
+  'LDAP_ANALYZER': require('./ldap-injection-analyzer'),
+  'PATH_TRAVERSAL_ANALYZER': require('./path-traversal-analyzer'),
+  'SQL_INJECTION_ANALYZER': require('./sql-injection-analyzer'),
+  'SSRF': require('./ssrf-analyzer'),
+  'WEAK_CIPHER_ANALYZER': require('./weak-cipher-analyzer'),
+  'WEAK_HASH_ANALYZER': require('./weak-hash-analyzer')
 }
