f9521388b3ebdd1431db995aa3bf6a919def00b1
Ugaitz Urien <ugaitz.urien@datadoghq.com>
2023-06-01 08:46:00 +0200

    Insecure cookie vulnerability detection (#3184)
    * Initial version of insecure-cookie vulnerability
    
    * Small fix
    
    * Add tests
    
    * rename const
    
    * Fix test
    
    * Exclude express file from insecure cookie stack trace
    
    * Some code styles + tests
    
    * Small code spaces
    
    * intermediate-cookies-analyzer to set-cookies-header-intercepteor
    
    * Comments in the PR
    
    * Comments in PR
    
    * Ignore insecure cookie when cookie value is empty
    
    * Reuse excluded paths array

--- packages/dd-trace/src/appsec/iast/analyzers/index.js
+++ packages/dd-trace/src/appsec/iast/analyzers/index.js
@@ -1,14 +1,17 @@
 'use strict'
 
 const analyzers = require('./analyzers')
+const setCookiesHeaderInterceptor = require('./set-cookies-header-interceptor')
 
 function enableAllAnalyzers () {
+  setCookiesHeaderInterceptor.configure(true)
   for (const analyzer in analyzers) {
     analyzers[analyzer].configure(true)
   }
 }
 
 function disableAllAnalyzers () {
+  setCookiesHeaderInterceptor.configure(false)
   for (const analyzer in analyzers) {
     analyzers[analyzer].configure(false)
   }
