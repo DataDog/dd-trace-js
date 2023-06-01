d21bf7fb5b36af5044d51288c82d8d4840798d83
Ugaitz Urien <ugaitz.urien@datadoghq.com>
2023-06-01 08:46:00 +0200

    Insecure cookie vulnerability detection (#3184)
    * Initial version of insecure-cookie vulnerability
    
    * Small fix
    
    * Add tests
    
    * rename const
    
    * Fix test
    
    * Exclude express file from insecure cookie stack trace
    
    * Some code styles + tests
    
    * Small code spaces
    
    * intermediate-cookies-analyzer to set-cookies-header-intercepteor
    
    * Comments in the PR
    
    * Comments in PR
    
    * Ignore insecure cookie when cookie value is empty
    
    * Reuse excluded paths array

--- packages/dd-trace/src/appsec/channels.js
+++ packages/dd-trace/src/appsec/channels.js
@@ -4,8 +4,9 @@ const dc = require('../../../diagnostics_channel')
 
 // TODO: use TBD naming convention
 module.exports = {
+  bodyParser: dc.channel('datadog:body-parser:read:finish'),
   incomingHttpRequestStart: dc.channel('dd-trace:incomingHttpRequestStart'),
   incomingHttpRequestEnd: dc.channel('dd-trace:incomingHttpRequestEnd'),
-  bodyParser: dc.channel('datadog:body-parser:read:finish'),
-  queryParser: dc.channel('datadog:query:read:finish')
+  queryParser: dc.channel('datadog:query:read:finish'),
+  setCookieChannel: dc.channel('datadog:iast:set-cookie')
 }
