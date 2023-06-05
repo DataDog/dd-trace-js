a54779afa7cc95fe20d7078a4df7a66aef89087a
Thomas Hunter II <tlhunter@datadog.com>
2023-06-05 13:13:42 -0700

    adding knex to integration test

--- integration-tests/esbuild/build.js
+++ integration-tests/esbuild/build.js
@@ -9,7 +9,18 @@ esbuild.build({
   outfile: 'out.js',
   plugins: [ddPlugin],
   platform: 'node',
-  target: ['node16']
+  target: ['node16'],
+  external: [
+    // dead code paths introduced by knex
+    'pg',
+    'mysql2',
+    'better-sqlite3',
+    'sqlite3',
+    'mysql',
+    'oracledb',
+    'pg-query-stream',
+    'tedious'
+  ]
 }).catch((err) => {
   console.error(err) // eslint-disable-line no-console
   process.exit(1)
