e3f298b0074732a1764e7565c842fc3ceac604ee
Juan Antonio Fernández de Alba <juan.fernandezdealba@datadoghq.com>
2023-06-19 17:05:12 +0200

    [ci-visibility] Use correct repository URL for git metadata upload (#3253)

--- packages/dd-trace/test/ci-visibility/exporters/ci-visibility-exporter.spec.js
+++ packages/dd-trace/test/ci-visibility/exporters/ci-visibility-exporter.spec.js
@@ -59,6 +59,23 @@ describe('CI Visibility Exporter', () => {
       ciVisibilityExporter._resolveCanUseCiVisProtocol(true)
       ciVisibilityExporter.sendGitMetadata()
     })
+    it('should use the input repository URL', (done) => {
+      nock(`http://localhost:${port}`)
+        .post('/api/v2/git/repository/search_commits')
+        .reply(200, function () {
+          const { meta: { repository_url: repositoryUrl } } = JSON.parse(this.req.requestBodyBuffers.toString())
+          expect(repositoryUrl).to.equal('https://custom-git@datadog.com')
+          done()
+        })
+        .post('/api/v2/git/repository/packfile')
+        .reply(202, '')
+
+      const url = new URL(`http://localhost:${port}`)
+      const ciVisibilityExporter = new CiVisibilityExporter({ url, isGitUploadEnabled: true })
+
+      ciVisibilityExporter._resolveCanUseCiVisProtocol(true)
+      ciVisibilityExporter.sendGitMetadata('https://custom-git@datadog.com')
+    })
   })
 
   describe('getItrConfiguration', () => {
