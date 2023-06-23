2429ee7b60aa069f0f9461495ec5be49b1539563
Thomas Hunter II <tlhunter@datadog.com>
2023-06-23 11:24:02 -0700

    add instrumentation for the openai package (#3155)
    - adds support for the OpenAI package
    - includes many things that aren't typical with our other integrations
      - also sends metrics, even when runtime metrics are disabled for the tracer
      - also sends logs directly to the datadog backend, bypassing the agent
    - overall this is the most intricate of any integration we've had so far

--- packages/datadog-instrumentations/src/helpers/hooks.js
+++ packages/datadog-instrumentations/src/helpers/hooks.js
@@ -69,6 +69,7 @@ module.exports = {
   'net': () => require('../net'),
   'next': () => require('../next'),
   'oracledb': () => require('../oracledb'),
+  'openai': () => require('../openai'),
   'paperplane': () => require('../paperplane'),
   'pg': () => require('../pg'),
   'pino': () => require('../pino'),
