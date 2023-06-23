38d5699f40fedb71e580d590a8b8c8e3aeb1ba11
Jordi Bertran de Balanda <jbertran@users.noreply.github.com>
2023-06-23 14:52:43 +0200

    Apply service naming workflow to databases  (#3256)
    * add cassandra-driver versions
    * add elasticsearch/opensearch versions
    * add mongodb versions
    * add pg versions
    * add oracledb versions

--- packages/dd-trace/src/service-naming/schemas/v1/storage.js
+++ packages/dd-trace/src/service-naming/schemas/v1/storage.js
@@ -14,8 +14,23 @@ const mySQLNaming = {
   serviceName: identityService
 }
 
+function withFunction (service, config, params) {
+  if (typeof config.service === 'function') {
+    return config.service(params)
+  }
+  return configWithFallback(service, config)
+}
+
 const storage = {
   client: {
+    'cassandra-driver': {
+      opName: () => 'cassandra.query',
+      serviceName: configWithFallback
+    },
+    elasticsearch: {
+      opName: () => 'elasticsearch.query',
+      serviceName: configWithFallback
+    },
     ioredis: redisNaming,
     mariadb: {
       opName: () => 'mariadb.query',
@@ -25,8 +40,24 @@ const storage = {
       opName: () => 'memcached.command',
       serviceName: configWithFallback
     },
+    'mongodb-core': {
+      opName: () => 'mongodb.query',
+      serviceName: configWithFallback
+    },
     mysql: mySQLNaming,
     mysql2: mySQLNaming,
+    opensearch: {
+      opName: () => 'opensearch.query',
+      serviceName: configWithFallback
+    },
+    oracledb: {
+      opName: () => 'oracle.query',
+      serviceName: withFunction
+    },
+    pg: {
+      opName: () => 'postgresql.query',
+      serviceName: withFunction
+    },
     redis: redisNaming,
     tedious: {
       opName: () => 'mssql.query',
