935675f0844cc07f24bacfcf8c860ccb3b78b14b
Jordi Bertran de Balanda <jordi.bertran@datadoghq.com>
2023-06-28 15:57:29 +0200

    move short-circuit computation to `service-naming`

--- packages/dd-trace/src/service-naming/index.js
+++ packages/dd-trace/src/service-naming/index.js
@@ -3,7 +3,7 @@ const { schemaDefinitions } = require('./schemas')
 class SchemaManager {
   constructor () {
     this.schemas = schemaDefinitions
-    this.config = { spanAttributeSchema: 'v0' }
+    this.config = { spanAttributeSchema: 'v0', traceRemoveIntegrationServiceNamesEnabled: false }
   }
 
   get schema () {
@@ -14,15 +14,20 @@ class SchemaManager {
     return this.config.spanAttributeSchema
   }
 
+  get shouldUseConsistentServiceNaming () {
+    return this.config.traceRemoveIntegrationServiceNamesEnabled && this.version === 'v0'
+  }
+
   opName (type, kind, plugin, ...opNameArgs) {
     return this.schema.getOpName(type, kind, plugin, ...opNameArgs)
   }
 
   serviceName (type, kind, plugin, ...serviceNameArgs) {
-    if (this.config.traceRemoveIntegrationServiceNamesEnabled && this.version === 'v0') {
-      return this.config.service
-    }
     return this.schema.getServiceName(type, kind, plugin, this.config.service, ...serviceNameArgs)
+  }
+
+  shortCircuitServiceName (pluginConfig) {
+    return pluginConfig.service || this.config.service
   }
 
   configure (config = {}) {
