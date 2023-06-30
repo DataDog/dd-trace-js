777883a47e8783181e58528dd2b5a2fe47ad81dc
Andrea Marziali <andrea.marziali@datadoghq.com>
2023-06-30 15:29:11 +0200

    mongodb expose peer.service (#3303)
    * mongodb expose peer.service
    
    * include review comments
    
    ---------
    
    Co-authored-by: Jordi Bertran de Balanda <jbertran@users.noreply.github.com>

--- packages/datadog-plugin-mongodb-core/test/mongodb.spec.js
+++ packages/datadog-plugin-mongodb-core/test/mongodb.spec.js
@@ -76,6 +76,12 @@ describe('Plugin', () => {
         })
 
         describe('server', () => {
+          withPeerService(
+            () => tracer,
+            () => collection.insertOne({ a: 1 }, {}, () => {}),
+            'test', 'peer.service'
+          )
+
           it('should do automatic instrumentation', done => {
             agent
               .use(traces => {
