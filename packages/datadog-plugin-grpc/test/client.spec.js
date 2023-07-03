d6b71746a10862f517e7a63a965ed9269dfcba62
Andrea Marziali <andrea.marziali@datadoghq.com>
2023-07-03 19:09:04 +0200

    grpc: service representation naming (#3327)
    * grpc: service representation naming
    
    * fix tests

--- packages/datadog-plugin-grpc/test/client.spec.js
+++ packages/datadog-plugin-grpc/test/client.spec.js
@@ -5,8 +5,12 @@ const getPort = require('get-port')
 const Readable = require('stream').Readable
 const getService = require('./service')
 const loader = require('../../../versions/@grpc/proto-loader').get()
-const pkgs = ['grpc', '@grpc/grpc-js']
 const { ERROR_MESSAGE, ERROR_TYPE, ERROR_STACK } = require('../../dd-trace/src/constants')
+
+const nodeMajor = parseInt(process.versions.node.split('.')[0])
+const pkgs = nodeMajor > 14 ? ['@grpc/grpc-js'] : ['grpc', '@grpc/grpc-js']
+
+const namingSchema = require('./naming')
 
 describe('Plugin', () => {
   let grpc
@@ -102,6 +106,13 @@ describe('Plugin', () => {
                 () => tracer,
                 (done) => client.getUnary({ first: 'foobar' }, () => done()),
                 'test.TestService', 'rpc.service')
+
+              withNamingSchema(
+                (done) => client.getUnary({ first: 'foobar' }, () => done()),
+                () => namingSchema.client.opName,
+                () => namingSchema.client.serviceName,
+                'test'
+              )
 
               client.getUnary({ first: 'foobar' }, () => {})
               return agent
--- packages/dd-trace/src/service-naming/schemas/v0/web.js
+++ packages/dd-trace/src/service-naming/schemas/v0/web.js
@@ -2,12 +2,20 @@ const { identityService } = require('../util')
 
 const web = {
   client: {
+    grpc: {
+      opName: () => 'grpc.client',
+      serviceName: identityService
+    },
     moleculer: {
       opName: () => 'moleculer.call',
       serviceName: identityService
     }
   },
   server: {
+    grpc: {
+      opName: () => 'grpc.server',
+      serviceName: identityService
+    },
     moleculer: {
       opName: () => 'moleculer.action',
       serviceName: identityService
