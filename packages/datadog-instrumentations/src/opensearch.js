e80255d3cdef08a4ef85699032f591df42aecf9c
Andrea Marziali <andrea.marziali@datadoghq.com>
2023-07-03 10:18:35 +0200

    elasticsearch and opensearch: extract connection host for peer.service (#3309)
    * elasticsearch and opensearch: extract connection host for peer.service
    
    * apply review suggestions

--- packages/datadog-instrumentations/src/opensearch.js
+++ packages/datadog-instrumentations/src/opensearch.js
@@ -2,9 +2,10 @@
 
 const { addHook } = require('./helpers/instrument')
 const shimmer = require('../../datadog-shimmer')
-const { createWrapRequest } = require('./elasticsearch')
+const { createWrapRequest, createWrapGetConnection } = require('./elasticsearch')
 
 addHook({ name: '@opensearch-project/opensearch', file: 'lib/Transport.js', versions: ['>=1'] }, Transport => {
   shimmer.wrap(Transport.prototype, 'request', createWrapRequest('opensearch'))
+  shimmer.wrap(Transport.prototype, 'getConnection', createWrapGetConnection('opensearch'))
   return Transport
 })
