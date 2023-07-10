3042d199b253649085afa32c3777a8cb7686694f
CarlesDD <carles.capell@datadoghq.com>
2023-07-10 13:44:43 +0200

    Disable debug log level

--- packages/dd-trace/src/appsec/iast/taint-tracking/rewriter.js
+++ packages/dd-trace/src/appsec/iast/taint-tracking/rewriter.js
@@ -43,7 +43,7 @@ function getRewriter () {
       const chainSourceMap = isEnableSourceMapsFlagPresent()
       getRewriterOriginalPathAndLineFromSourceMap =
         getGetOriginalPathAndLineFromSourceMapFunction(chainSourceMap, iastRewriter.getOriginalPathAndLineFromSourceMap)
-      rewriter = new Rewriter({ csiMethods, chainSourceMap, logLevel: "DEBUG", logger: {"error": console.log, log: console.log, debug: console.log}})
+      rewriter = new Rewriter({ csiMethods, chainSourceMap })
     } catch (e) {
       iastLog.error('Unable to initialize TaintTracking Rewriter')
         .errorAndPublish(e)
