93ff4ac94f13844f8ad19a05b09a9923977ea84d
rochdev <roch.devost@gmail.com>
2023-07-14 12:17:33 -0400

    revert unrelated change

--- packages/dd-trace/src/config.js
+++ packages/dd-trace/src/config.js
@@ -509,7 +509,7 @@ ken|consumer_?(?:id|key|secret)|sign(?:ed|ature)?|auth(?:entication|orization)?)
       })
     }
 
-    const defaultFlushInterval = inServerlessEnvironment ? 0 : 1000
+    const defaultFlushInterval = inServerlessEnvironment ? 0 : 2000
 
     this.tracing = !isFalse(DD_TRACING_ENABLED)
     this.dbmPropagationMode = DD_DBM_PROPAGATION_MODE
