9696f250c9c7870172e0da6293aa45ebbb6cb74a
AJ Stuyvenberg <astuyve@gmail.com>
2023-07-17 13:01:28 -0400

    feat: More lint

--- packages/dd-trace/src/lambda/handler.js
+++ packages/dd-trace/src/lambda/handler.js
@@ -86,10 +86,10 @@ exports.datadog = function datadog (lambdaHandler) {
     const context = extractContext(args)
 
     checkTimeout(context)
-    if (lambdaHandler.constructor.name === "AsyncFunction") {
-      return Promise.resolve(lambdaHandler.apply(this, args)).then((res) => { clearTimeout(__lambdaTimeout); return res })
+    if (lambdaHandler.constructor.name === 'AsyncFunction') {
+      return Promise.resolve(lambdaHandler.apply(this, args))
+        .then((res) => { clearTimeout(__lambdaTimeout); return res })
     }
     return lambdaHandler.apply(this, args)
-
   }
 }
