1090ce2db8bf9dbdf85122dcee03417fb379e6b0
Thomas Hunter II <tlhunter@datadog.com>
2023-07-17 18:19:06 -0700

    openai: be defensive about response object shape (#3408)

--- packages/datadog-plugin-openai/test/index.spec.js
+++ packages/datadog-plugin-openai/test/index.spec.js
@@ -55,7 +55,12 @@ describe('Plugin', () => {
       describe('createCompletion()', () => {
         let scope
 
-        before(() => {
+        after(() => {
+          nock.removeInterceptor(scope)
+          scope.done()
+        })
+
+        it('makes a successful call', async () => {
           scope = nock('https://api.openai.com:443')
             .post('/v1/completions')
             .reply(200, {
@@ -87,14 +92,7 @@ describe('Plugin', () => {
               'x-ratelimit-reset-tokens', '3ms',
               'x-request-id', '7df89d8afe7bf24dc04e2c4dd4962d7f'
             ])
-        })
 
-        after(() => {
-          nock.removeInterceptor(scope)
-          scope.done()
-        })
-
-        it('makes a successful call', async () => {
           const checkTraces = agent
             .use(traces => {
               expect(traces[0][0]).to.have.property('name', 'openai.request')
@@ -187,6 +185,44 @@ describe('Plugin', () => {
               }
             ]
           })
+        })
+
+        it('should not throw with empty response body', async () => {
+          scope = nock('https://api.openai.com:443')
+            .post('/v1/completions')
+            .reply(200, {}, [
+              'Date', 'Mon, 15 May 2023 17:24:22 GMT',
+              'Content-Type', 'application/json',
+              'Content-Length', '349',
+              'Connection', 'close',
+              'openai-model', 'text-davinci-002',
+              'openai-organization', 'kill-9',
+              'openai-processing-ms', '442',
+              'openai-version', '2020-10-01',
+              'x-ratelimit-limit-requests', '3000',
+              'x-ratelimit-limit-tokens', '250000',
+              'x-ratelimit-remaining-requests', '2999',
+              'x-ratelimit-remaining-tokens', '249984',
+              'x-ratelimit-reset-requests', '20ms',
+              'x-ratelimit-reset-tokens', '3ms',
+              'x-request-id', '7df89d8afe7bf24dc04e2c4dd4962d7f'
+            ])
+
+          const checkTraces = agent
+            .use(traces => {
+              expect(traces[0][0]).to.have.property('name', 'openai.request')
+            })
+
+          await openai.createCompletion({
+            model: 'text-davinci-002',
+            prompt: 'Hello, ',
+            suffix: 'foo',
+            stream: true
+          })
+
+          await checkTraces
+
+          clock.tick(10 * 1000)
         })
       })
 
