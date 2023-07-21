4f712db1e32c25b4a0acfaf5b14df0189c907291
Ayan Khan <khanayan_123@hotmail.com>
2023-07-21 10:10:51 -0400

    refactor fastify integration test helper

--- packages/datadog-plugin-fastify/test/integration-test/helper.mjs
+++ packages/datadog-plugin-fastify/test/integration-test/helper.mjs
@@ -3,16 +3,12 @@ export async function createAndStartServer (app) {
     return 'hello, world\n'
   })
 
-  const startServer = async () => {
-    try {
-      await app.listen(0)
-      const address = app.server.address()
-      const port = address.port
-      process.send({ port })
-    } catch (err) {
-      process.exit(1)
-    }
+  try {
+    await app.listen(0)
+    const address = app.server.address()
+    const port = address.port
+    process.send({ port })
+  } catch (err) {
+    process.exit(1)
   }
-  startServer()
-  return startServer
 }
