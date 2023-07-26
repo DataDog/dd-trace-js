e24730ad211f43c4337e11e74a6483fd2744153c
Thomas Hunter II <tlhunter@datadog.com>
2023-07-26 09:38:21 -0700

    esbuild: fix resolving of deep modules (#3448)
    ### What does this PR do?
    - fixes the ESBuild plugin to properly load deep instrumented package modules
    - deletes DCITM, moves functionality to `packages/datadog-instrumentations/src/helpers/bundler-register.js`
    - uses a single DC channel for all modules instead of a channel per module
    
    ### Motivation
    - currently it only supports the highest level entry into a package, e.g. `require('express')`
    - more deeply nested modules, like most of them we define via `addHook()` with a files arg, were overlooked
    - in practice most things don't actually work -- save for the Express integration test

---
+++ packages/datadog-instrumentations/src/helpers/bundler-register.js
@@ -0,0 +1,54 @@
+'use strict'
+
+// eslint-disable-next-line n/no-restricted-require
+const dc = require('diagnostics_channel')
+
+const {
+  filename,
+  loadChannel,
+  matchVersion
+} = require('./register.js')
+const hooks = require('./hooks')
+const instrumentations = require('./instrumentations')
+const log = require('../../../dd-trace/src/log')
+
+const CHANNEL = 'dd-trace:bundler:load'
+
+if (!dc.subscribe) {
+  dc.subscribe = (channel, cb) => {
+    dc.channel(channel).subscribe(cb)
+  }
+}
+if (!dc.unsubscribe) {
+  dc.unsubscribe = (channel, cb) => {
+    if (dc.channel(channel).hasSubscribers) {
+      dc.channel(channel).unsubscribe(cb)
+    }
+  }
+}
+
+dc.subscribe(CHANNEL, (payload) => {
+  try {
+    hooks[payload.package]()
+  } catch (err) {
+    log.error(`esbuild-wrapped ${payload.package} missing in list of hooks`)
+    throw err
+  }
+
+  if (!instrumentations[payload.package]) {
+    log.error(`esbuild-wrapped ${payload.package} missing in list of instrumentations`)
+    return
+  }
+
+  for (const { name, file, versions, hook } of instrumentations[payload.package]) {
+    if (payload.path !== filename(name, file)) continue
+    if (!matchVersion(payload.version, versions)) continue
+
+    try {
+      loadChannel.publish({ name, version: payload.version, file })
+      payload.module = hook(payload.module, payload.version)
+    } catch (e) {
+      log.error(e)
+    }
+  }
+})
