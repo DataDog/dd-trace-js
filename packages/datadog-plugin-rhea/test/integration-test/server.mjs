45bde539bc5054c309d65f94f81405d5a182a0c5
Ayan Khan <khanayan_123@hotmail.com>
2023-07-29 17:49:18 -0400

    fix rhea integration test setup

--- packages/datadog-plugin-rhea/test/integration-test/server.mjs
+++ packages/datadog-plugin-rhea/test/integration-test/server.mjs
@@ -1,47 +1,21 @@
 import 'dd-trace/init.js'
-import rhea from 'rhea'
+import container from 'rhea'
 
-async function connectToAMQP () {
-  return new Promise((resolve, reject) => {
-    const connection = rhea.connect({
-      username: 'admin',
-      password: 'admin',
-      host: 'localhost',
-      port: 5673
-    })
-
-    connection.on('connection_open', (context) => {
-      const sender = context.connection.open_sender('amq.topic')
-      const receiver = context.connection.open_receiver('amq.topic')
-
-      context.sender = sender
-      context.receiver = receiver
-      resolve(context)
-    })
-
-    connection.on('error', (error) => {
-      reject(error)
-    })
-
-    connection.on('connection_close', () => {
-      if (!connection._closing) {
-        connection._closing = true
-        resolve()
-      }
-    })
-  })
-}
-
-async function runIntegrationTest () {
-  const context = await connectToAMQP()
+container.on('connection_open', function (context) {
+  context.connection.open_receiver('amq.topic')
+  context.connection.open_sender('amq.topic')
+})
+container.on('message', function (context) {
+  context.connection.close()
+})
+container.on('sendable', function (context) {
   context.sender.send({ body: 'Hello World!' })
+  context.sender.detach()
+})
 
-  await new Promise((resolve) => {
-    context.connection.once('connection_close', () => {
-      resolve()
-    })
-    context.connection.close()
-  })
-}
-
-runIntegrationTest()
+container.connect({
+  username: 'admin',
+  password: 'admin',
+  host: 'localhost',
+  port: 5673
+})
