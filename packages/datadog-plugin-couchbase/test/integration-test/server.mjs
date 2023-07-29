06ac921d8ac4cbd8a9328897ad3cfaca5549ec9c
Ayan Khan <khanayan_123@hotmail.com>
2023-07-29 17:54:38 -0400

    fix couchbase test

--- packages/datadog-plugin-couchbase/test/integration-test/server.mjs
+++ packages/datadog-plugin-couchbase/test/integration-test/server.mjs
@@ -1,63 +1,33 @@
 import 'dd-trace/init.js'
-import couchbase from 'couchbase'
+import {
+  Bucket,
+  Cluster,
+  Collection,
+  connect,
+  GetResult
+} from 'couchbase'
 
-async function connectToCouchbase () {
-  return new Promise((resolve, reject) => {
-    const cluster = new couchbase.Cluster('couchbase://localhost')
-    cluster.authenticate('Administrator', 'password')
-    const bucket = cluster.bucket('datadog-test')
-
-    bucket.on('connect', () => {
-      resolve({ cluster, bucket })
+async function main () {
+  const cluster = await connect(
+    'couchbase://127.0.0.1',
+    {
+      username: 'Administrator',
+      password: 'password'
     })
 
-    bucket.on('error', (error) => {
-      reject(error)
-    })
+  const bucket = cluster.bucket('default')
+  const coll = bucket.defaultCollection()
+  await coll.upsert('testdoc', { foo: 'bar' })
+
+  const res = await coll.get('testdoc')
+  console.log(res.content)
+}
+
+// Run the main function
+main()
+  .then((_) => {
+    console.log('Success!')
   })
-}
-
-async function runIntegrationTest () {
-  try {
-    const { cluster, bucket } = await connectToCouchbase()
-
-    const query = 'SELECT 1 + 1 AS solution'
-    const n1qlQuery = couchbase.N1qlQuery.fromString(query)
-
-    // Run a N1QL query
-    bucket.query(n1qlQuery, (err) => {
-      if (err) {
-        console.error('Error running N1QL query:', err.message)
-      }
-    })
-
-    // Perform a normal cluster query operation with a callback
-    cluster.query(query, (err) => {
-      if (err) {
-        console.error('Error running cluster query:', err.message)
-      }
-    })
-
-    const documentId = 'foo'
-    bucket.get(documentId, (err) => {
-      if (err) {
-        console.error('Error fetching document:', err.message)
-      }
-    })
-
-    await new Promise((resolve) => {
-      cluster.once('error', (error) => {
-        console.error('Error closing cluster:', error.message)
-      })
-      cluster.once('close', () => {
-        console.log('Cluster closed gracefully.')
-        resolve()
-      })
-      cluster.close()
-    })
-  } catch (error) {
-    console.error('Error connecting to Couchbase:', error.message)
-  }
-}
-
-runIntegrationTest()
+  .catch((err) => {
+    console.log('ERR:', err)
+  })
