3b7c1add3e684591a82aa3b702ab1ae61b96ece8
Roch Devost <roch.devost@datadoghq.com>
2023-07-31 10:21:24 -0400

    add option to truncate pg query (#3487)

--- packages/dd-trace/src/plugins/database.js
+++ packages/dd-trace/src/plugins/database.js
@@ -55,6 +55,18 @@ class DatabasePlugin extends StoragePlugin {
       return `/*${servicePropagation},traceparent='${traceparent}'*/ ${query}`
     }
   }
+
+  maybeTruncate (query) {
+    const maxLength = typeof this.config.truncate === 'number'
+      ? this.config.truncate
+      : 5000 // same as what the agent does
+
+    if (this.config.truncate && query && query.length > maxLength) {
+      query = `${query.slice(0, maxLength - 3)}...`
+    }
+
+    return query
+  }
 }
 
 module.exports = DatabasePlugin
