05283a4b8b132832d55136609a12085898326dfe
Ayan Khan <khanayan_123@hotmail.com>
2023-07-31 14:48:10 -0400

    try grpc again

--- packages/datadog-plugin-grpc/test/integration-test/client.spec.js
+++ packages/datadog-plugin-grpc/test/integration-test/client.spec.js
@@ -36,16 +36,18 @@ describe('esm', () => {
     await agent.stop()
   })
 
-  context('connect', () => {
+  context('grpc', () => {
     it('is instrumented', async () => {
-      proc = await spawnPluginIntegrationTestProc(sandbox.folder, 'integration-test/server.mjs', agent.port)
-
-      return curlAndAssertMessage(agent, proc, ({ headers, payload }) => {
+      const res = agent.assertMessageReceived(({ headers, payload }) => {
         console.log(headers, payload)
         assert.propertyVal(headers, 'host', `127.0.0.1:${agent.port}`)
         assert.isArray(payload)
         assert.strictEqual(checkSpansForServiceName(payload, 'grpc.server'), true)
-      })
+      }, undefined)
+
+      proc = await spawnPluginIntegrationTestProc(sandbox.folder, 'server.mjs', agent.port, undefined)
+
+      await res
     }).timeout(20000)
   })
 })
