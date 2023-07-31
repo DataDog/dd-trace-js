91a89dd2ab3d95202cf3822cbe4730ac145894e6
Ayan Khan <khanayan_123@hotmail.com>
2023-07-31 11:04:54 -0400

    oracledb not tracing, try something different

--- packages/datadog-plugin-oracledb/test/integration-test/server.mjs
+++ packages/datadog-plugin-oracledb/test/integration-test/server.mjs
@@ -1,7 +1,6 @@
 import 'dd-trace/init.js'
 import oracledb from 'oracledb'
 
-
 const hostname = process.env.CI ? 'oracledb' : 'localhost'
 const config = {
   user: 'test',
@@ -11,6 +10,14 @@ const config = {
 
 const dbQuery = 'select current_timestamp from dual'
 
-const connection = await oracledb.getConnection(config)
+const connection = await oracledb.getConnection({
+  ...config,
+  connectString: `
+    (DESCRIPTION=
+      (ADDRESS=(PROTOCOL=TCP)(HOST=${hostname})(PORT=1521))
+      (CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=xepdb1))
+    )
+  `
+})
 await connection.execute(dbQuery)
 await connection.close()
