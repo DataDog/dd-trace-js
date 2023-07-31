101347eceaecaa3e297c436eefc3aec38e2eb3a1
Ayan Khan <khanayan_123@hotmail.com>
2023-07-31 14:58:52 -0400

    try again

--- packages/datadog-plugin-grpc/test/integration-test/server.mjs
+++ packages/datadog-plugin-grpc/test/integration-test/server.mjs
@@ -12,42 +12,36 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 let server
+const port = await getPort()
 
-async function buildClient (service, port) {
-  service = Object.assign(
-    {
-      getBidi: () => {},
-      getServerStream: () => {},
-      getClientStream: () => {},
-      getUnary: () => {}
-    },
-    service
-  )
+function buildClient (service, callback) {
+  service = Object.assign({
+    getBidi: () => {},
+    getServerStream: () => {},
+    getClientStream: () => {},
+    getUnary: () => {}
+  }, service)
 
-  console.log(4)
-  const protoPath = resolve(__dirname, '../test.proto')
-  const definition = loader.loadSync(protoPath)
+  const loader = require('../../../versions/@grpc/proto-loader').get()
+  const definition = loader.loadSync(`${__dirname}/test.proto`)
   const TestService = grpc.loadPackageDefinition(definition).test.TestService
 
   server = new grpc.Server()
 
   return new Promise((resolve, reject) => {
-    console.log(5)
     if (server.bindAsync) {
-      console.log(6)
       server.bindAsync(`0.0.0.0:${port}`, grpc.ServerCredentials.createInsecure(), (err) => {
         if (err) return reject(err)
 
         server.addService(TestService.service, service)
         server.start()
-        console.log(7)
+
         resolve(new TestService(`localhost:${port}`, grpc.credentials.createInsecure()))
       })
     } else {
       server.bind(`0.0.0.0:${port}`, grpc.ServerCredentials.createInsecure())
       server.addService(TestService.service, service)
       server.start()
-      console.log(8)
 
       resolve(new TestService(`localhost:${port}`, grpc.credentials.createInsecure()))
     }
@@ -56,32 +50,21 @@ async function buildClient (service, port) {
 
 async function runTest () {
   try {
-    const port = await getPort()
-    console.log(1)
-    const client = await buildClient(
-      {
-        getUnary: (_, callback) => callback()
-      },
-      port
-    )
-
-    console.log(2)
+    
+    const client = await buildClient({
+      getUnary: (_, callback) => callback()
+    })
 
     client.getUnary({ first: 'foobar' }, () => {})
 
     // client.close()
-    console.log(3)
 
-    server.forceShutdown()
+    await server.forceShutdown()
 
     console.log('Client connection closed gracefully.')
   } catch (error) {
     console.error('Error occurred during the test:', error)
   }
 }
-
-process.on('uncaughtException', function (err) {
-  console.log(err)
-})
 
 runTest()
