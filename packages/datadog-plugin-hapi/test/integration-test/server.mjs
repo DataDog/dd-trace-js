3a8059133b86e4a6b0b327768def33c660a5ac9a
Ayan Khan <khanayan_123@hotmail.com>
2023-08-01 15:26:38 -0400

    fix hapi

--- packages/datadog-plugin-hapi/test/integration-test/server.mjs
+++ packages/datadog-plugin-hapi/test/integration-test/server.mjs
@@ -3,11 +3,15 @@ import Hapi from '@hapi/hapi'
 
 const init = async () => {
   const server = Hapi.server({ port: 0, host: 'localhost' })
-  server.route({ method: 'GET', path: '/', handler: (request, h) => 'hello, world\n' })
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+      return h.response('hello, world\n').code(200)
+    }
+  })
   await server.start()
   process.send({ port: server.info.port })
 }
-
-process.on('unhandledRejection', () => process.exit(1))
 
 init()
