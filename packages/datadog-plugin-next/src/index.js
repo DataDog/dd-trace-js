6822c726d815b96b52b93953b495a0b058dd5f75
Roch Devost <roch.devost@datadoghq.com>
2023-08-02 12:23:54 -0400

    fix duplicate next spans and incorrect req/res in hooks (#3494)

--- packages/datadog-plugin-next/src/index.js
+++ packages/datadog-plugin-next/src/index.js
@@ -16,7 +16,7 @@ class NextPlugin extends ServerPlugin {
     this.addSub('apm:next:page:load', message => this.pageLoad(message))
   }
 
-  start ({ req, res }) {
+  bindStart ({ req, res }) {
     const store = storage.getStore()
     const childOf = store ? store.span : store
     const span = this.tracer.startSpan(this.operationName(), {
@@ -33,9 +33,13 @@ class NextPlugin extends ServerPlugin {
 
     analyticsSampler.sample(span, this.config.measured, true)
 
-    this.enter(span, store)
-
     this._requests.set(span, req)
+
+    return { ...store, span }
+  }
+
+  error ({ span, error }) {
+    this.addError(error, span)
   }
 
   finish ({ req, res }) {
