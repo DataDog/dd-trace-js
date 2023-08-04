738271e4842878478551333cd8a3f9a153692d5b
Ayan Khan <khanayan_123@hotmail.com>
2023-08-04 15:28:36 -0400

    adds esm integration test for next

---
+++ packages/datadog-plugin-next/test/integration-test/client.spec.js
@@ -0,0 +1,46 @@
+'use strict'
+
+const {
+  FakeAgent,
+  createSandbox,
+  curlAndAssertMessage,
+  checkSpansForServiceName,
+  spawnPluginIntegrationTestProc
+} = require('../../../../integration-tests/helpers')
+const { assert } = require('chai')
+
+describe('esm', () => {
+  let agent
+  let proc
+  let sandbox
+
+  before(async function () {
+    this.timeout(20000)
+    sandbox = await createSandbox(['next', 'react', 'react-dom'], false, ['./packages/datadog-plugin-next/test/*'])
+  })
+
+  after(async () => {
+    await sandbox.remove()
+  })
+
+  beforeEach(async () => {
+    agent = await new FakeAgent().start()
+  })
+
+  afterEach(async () => {
+    proc && proc.kill()
+    await agent.stop()
+  })
+
+  context('next', () => {
+    it('is instrumented', async () => {
+      proc = await spawnPluginIntegrationTestProc(sandbox.folder, 'integration-test/server.mjs', agent.port)
+
+      return curlAndAssertMessage(agent, proc, ({ headers, payload }) => {
+        assert.propertyVal(headers, 'host', `127.0.0.1:${agent.port}`)
+        assert.isArray(payload)
+        assert.strictEqual(checkSpansForServiceName(payload, 'next.request'), true)
+      })
+    }).timeout(20000)
+  })
+})
