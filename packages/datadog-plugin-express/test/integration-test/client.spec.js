1fa80946cdd9380789ce87ab94851fa87bf008bf
Ayan Khan <khanayan_123@hotmail.com>
2023-08-10 13:10:47 -0400

    esm now should be supported for node 20, so no need for esmTestSkipper (#3506)
    * remove esmTestSkipper for esm integration tests

--- packages/datadog-plugin-express/test/integration-test/client.spec.js
+++ packages/datadog-plugin-express/test/integration-test/client.spec.js
@@ -4,12 +4,9 @@ const {
   FakeAgent,
   createSandbox,
   curlAndAssertMessage,
-  esmTestSkipper,
   spawnPluginIntegrationTestProc
 } = require('../../../../integration-tests/helpers')
 const { assert } = require('chai')
-
-const describe = esmTestSkipper()
 
 describe('esm', () => {
   let agent
