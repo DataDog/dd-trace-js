aa3e64b63d30819dff5e7cd3f3dc29d588452da3
Thomas Hunter II <tlhunter@datadog.com>
2023-08-18 17:29:21 -0700

    esbuild: early bailout when loading app local modules (#3551)
    - bail out of the esbuild bundling process early if the currently considered file is local to the application
    - we only need to bundling if we're requiring a package from the outside or if we're in a package requiring from the inside
    - also treats filenames beginning with `@` as local filenames, a common convention with Next.js (and Vue?)

---
+++ integration-tests/esbuild/build-and-test-typescript.mjs
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+import fs from 'fs'
+
+import * as esbuild from 'esbuild'
+
+import ddPlugin from '../../esbuild.js'
+
+await esbuild.build({
+  entryPoints: ['typescript-app.ts'],
+  bundle: true,
+  outfile: 'typescript-app-out.js',
+  minify: false,
+  sourcemap: false,
+  platform: 'node',
+  target: 'es2022',
+  plugins: [ddPlugin],
+  external: [
+    'graphql/language/visitor',
+    'graphql/language/printer',
+    'graphql/utilities',
+  ],
+})
+
+console.log('ok') // eslint-disable-line no-console
+fs.rmSync('typescript-app-out.js')
