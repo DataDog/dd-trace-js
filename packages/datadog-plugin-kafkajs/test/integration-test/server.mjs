9cbd63597c199f817eac8e7e22d5bdd3f91e9feb
Ayan Khan <khanayan_123@hotmail.com>
2023-08-23 14:34:21 -0400

    fix esm tracing for kafkajs and add esm integration test (#3571)

---
+++ packages/datadog-plugin-kafkajs/test/integration-test/server.mjs
@@ -0,0 +1,19 @@
+import 'dd-trace/init.js'
+import { Kafka } from 'kafkajs'
+
+const kafka = new Kafka({
+  clientId: 'my-app',
+  brokers: ['127.0.0.1:9092']
+})
+
+const sendMessage = async (topic, messages) => {
+  const producer = kafka.producer()
+  await producer.connect()
+  await producer.send({
+    topic,
+    messages
+  })
+  await producer.disconnect()
+}
+
+await sendMessage('test-topic', [{ key: 'key1', value: 'test2' }])
