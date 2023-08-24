6aa96db271aa075b3214d3d7da758382cecc8ee2
Ayan Khan <khanayan_123@hotmail.com>
2023-08-24 10:11:05 -0400

    fix esm tracing for oracledb

---
+++ packages/datadog-plugin-oracledb/test/integration-test/server.mjs
@@ -0,0 +1,40 @@
+import 'dd-trace/init.js'
+import oracledb from 'oracledb';
+
+const hostname = process.env.CI ? 'oracledb' : 'localhost';
+const config = {
+  user: 'test',
+  password: 'Oracle18',
+  connectString: `${hostname}:1521/xepdb1`
+};
+
+const dbQuery = 'select current_timestamp from dual';
+
+let connection;
+
+try {
+  connection = await oracledb.getConnection({
+    ...config,
+    connectString: `
+      (DESCRIPTION=
+        (ADDRESS=(PROTOCOL=TCP)(HOST=${hostname})(PORT=1521))
+        (CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=xepdb1))
+      )
+    `
+  });
+
+  const result = await connection.execute(dbQuery);
+  console.log('Query result:', result.rows);
+
+} catch (error) {
+  console.error('Error:', error);
+
+} finally {
+  if (connection) {
+    try {
+      await connection.close();
+    } catch (error) {
+      console.error('Error closing connection:', error);
+    }
+  }
+}
