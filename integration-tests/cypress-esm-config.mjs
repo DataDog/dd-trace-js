23e340656b3b1f2fd88b4118f223f66b59b155df
Ayan Khan <khanayan_123@hotmail.com>
2023-08-24 10:27:22 -0400

    Khanayan123/add esm tests for testing plugins (#3555)
    * add esm integration tests for cypress, cucumber, mocha, and jest

---
+++ integration-tests/cypress-esm-config.mjs
@@ -0,0 +1,21 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import cypress from 'cypress'
+
+async function runCypress () {
+  await cypress.run({
+    config: {
+      defaultCommandTimeout: 100,
+      e2e: {
+        setupNodeEvents (on, config) {
+          import('../ci/cypress/plugin.js').then(module => {
+            module.default(on, config)
+          })
+        }
+      },
+      video: false
+    },
+    quiet: true
+  })
+}
+
+runCypress()
