7ffdfb97e380148fc6e8e1fd8abb4640592d9f7d
CarlesDD <carles.capell@datadoghq.com>
2023-08-25 09:34:29 +0200

    Add startup time benchmark for iast

---
+++ benchmark/sirun/appsec-iast/insecure-bank.js
@@ -0,0 +1,14 @@
+const http = require('http')
+const app = require('/opt/insecure-bank-js/app')
+
+const { port } = require('./common')
+
+app.set('port', port)
+const server = http.createServer(app)
+
+function onListening() {
+  server.close()
+}
+
+server.listen(port)
+server.on('listening', onListening)
