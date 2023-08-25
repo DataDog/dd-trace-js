2a3cc0a6181b20c36cfff787fb14495f4e40490c
Ayan Khan <khanayan_123@hotmail.com>
2023-08-25 14:13:04 -0400

    try again

--- packages/datadog-plugin-oracledb/test/integration-test/client.spec.js
+++ packages/datadog-plugin-oracledb/test/integration-test/client.spec.js
@@ -40,7 +40,9 @@ describe('esm', () => {
         assert.strictEqual(checkSpansForServiceName(payload, 'oracle.query'), true)
       })
 
-      proc = await spawnPluginIntegrationTestProc(sandbox.folder, 'server.mjs', agent.port)
+      proc = await spawnPluginIntegrationTestProc(sandbox.folder, 'server.mjs', agent.port, undefined, {
+        LD_LIBRARY_PATH: '/usr/lib/oracle/18.5/client64/lib:'
+      })
 
       await res
     }).timeout(20000)
