d301bf7c123848d47d52daf9ac2e9e19b22a6b13
Ayan Khan <khanayan_123@hotmail.com>
2023-08-25 12:14:12 -0400

    clean up

--- packages/datadog-plugin-oracledb/test/integration-test/server.mjs
+++ packages/datadog-plugin-oracledb/test/integration-test/server.mjs
@@ -1,40 +1,20 @@
 import 'dd-trace/init.js'
-import oracledb from 'oracledb';
+import oracledb from 'oracledb'
 
-const hostname = process.env.CI ? 'oracledb' : 'localhost';
+const hostname = process.env.CI ? 'oracledb' : 'localhost'
 const config = {
   user: 'test',
   password: 'Oracle18',
   connectString: `${hostname}:1521/xepdb1`
 };
 
-const dbQuery = 'select current_timestamp from dual';
+const dbQuery = 'select current_timestamp from dual'
 
 let connection;
 
-try {
-  // connection = await oracledb.getConnection({
-  //   ...config,
-  //   connectString: `
-  //     (DESCRIPTION=
-  //       (ADDRESS=(PROTOCOL=TCP)(HOST=${hostname})(PORT=1521))
-  //       (CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=xepdb1))
-  //     )
-  //   `
-  // });
-  connection = await oracledb.getConnection(config)
-  await connection.execute(dbQuery);
-  console.log('Ran');
+connection = await oracledb.getConnection(config)
+await connection.execute(dbQuery)
 
-} catch (error) {
-  console.error('Error:', error);
-
-} finally {
-  if (connection) {
-    try {
-      await connection.close();
-    } catch (error) {
-      console.error('Error closing connection:', error);
-    }
-  }
+if (connection) {
+  await connection.close()
 }
