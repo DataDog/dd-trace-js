c59967273bad04c35d974f147b89db51b6cd3a8c
Ayan Khan <khanayan_123@hotmail.com>
2023-08-28 10:06:05 -0400

    adds esm integration tests for grpc

---
+++ packages/datadog-plugin-grpc/test/integration-test/server.mjs
@@ -0,0 +1,54 @@
+import * as grpc from '@grpc/grpc-js';
+import * as protoLoader from '@grpc/proto-loader';
+import path from 'path';
+import getPort from 'get-port';
+
+const currentDirectoryPath = path.dirname(new URL(import.meta.url).pathname);
+const parentDirectoryPath = path.resolve(currentDirectoryPath, '..');
+
+console.log(5555, parentDirectoryPath);
+
+let server;
+let port = await getPort();
+
+function buildClient (service, callback) {
+  service = Object.assign({
+    getBidi: () => {},
+    getServerStream: () => {},
+    getClientStream: () => {},
+    getUnary: () => {}
+  }, service)
+
+  // const loader = require('../../../versions/@grpc/proto-loader').get()
+  const definition = protoLoader.loadSync(`${parentDirectoryPath}/test.proto`)
+  const TestService = grpc.loadPackageDefinition(definition).test.TestService
+
+  server = new grpc.Server()
+
+  return new Promise((resolve, reject) => {
+    if (server.bindAsync) {
+      server.bindAsync(`0.0.0.0:${port}`, grpc.ServerCredentials.createInsecure(), (err) => {
+        if (err) return reject(err)
+
+        server.addService(TestService.service, service)
+        server.start()
+
+        resolve(new TestService(`localhost:${port}`, grpc.credentials.createInsecure()))
+      })
+    } else {
+      server.bind(`0.0.0.0:${port}`, grpc.ServerCredentials.createInsecure())
+      server.addService(TestService.service, service)
+      server.start()
+
+      resolve(new TestService(`localhost:${port}`, grpc.credentials.createInsecure()))
+    }
+  })
+}
+
+const client = await buildClient({
+  getUnary: (_, callback) => callback()
+})
+
+client.getUnary({ first: 'foobar' }, () => {})
+
+server.forceShutdown()
