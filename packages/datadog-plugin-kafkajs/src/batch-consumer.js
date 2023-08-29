323f2ce0ed6f66744b81fde6b866c80b18882494
Allan Paiste <allan.paiste@katanamrp.com>
2023-08-29 16:04:47 +0300

    remove trace continuation from one message; switch to using first/last offset

--- packages/datadog-plugin-kafkajs/src/batch-consumer.js
+++ packages/datadog-plugin-kafkajs/src/batch-consumer.js
@@ -1,8 +1,6 @@
 'use strict'
 const ConsumerPlugin = require('dd-trace/packages/dd-trace/src/plugins/consumer')
 
-const kafkaMessageStub = { headers: [] }
-
 class KafkajsBatchConsumerPlugin extends ConsumerPlugin {
   static get id () {
     return 'kafkajs'
@@ -12,24 +10,17 @@ class KafkajsBatchConsumerPlugin extends ConsumerPlugin {
   }
 
   start ({ topic, partition, messages, groupId }) {
-    const message = process.env.DD_EXPERIMENTAL_KAFKAJS_PLUGIN_TRACE_FIRST_BATCH_MESSAGE
-      ? messages[0]
-      : kafkaMessageStub
-
     if (this.config.dsmEnabled) {
-      this.tracer.decodeDataStreamsContext(message.headers['dd-pathway-ctx'])
       this.tracer.setCheckpoint(['direction:in', `group:${groupId}`, `topic:${topic}`, 'type:kafka'])
     }
-    const childOf = extract(this.tracer, message.headers)
     this.startSpan({
-      childOf,
       resource: topic,
       type: 'worker',
       meta: {
         component: 'kafkajs',
         'kafka.topic': topic,
         'kafka.message.offset': messages[0].offset,
-        'kafka.message.offsets': messages.map((m) => m.offset)
+        'kafka.message.offset.last': messages[messages.length - 1].offset
       },
       metrics: {
         'kafka.partition': partition,
@@ -37,20 +28,6 @@ class KafkajsBatchConsumerPlugin extends ConsumerPlugin {
       }
     })
   }
-}
-
-function extract (tracer, bufferMap) {
-  if (!bufferMap) return null
-
-  const textMap = {}
-
-  for (const key of Object.keys(bufferMap)) {
-    if (bufferMap[key] === null || bufferMap[key] === undefined) continue
-
-    textMap[key] = bufferMap[key].toString()
-  }
-
-  return tracer.extract('text_map', textMap)
 }
 
 module.exports = KafkajsBatchConsumerPlugin
