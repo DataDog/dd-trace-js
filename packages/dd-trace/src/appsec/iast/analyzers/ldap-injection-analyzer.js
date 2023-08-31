f713edce171ea5bd3f6e7f577f1c3fc16da00b8d
Carles Capell <107924659+CarlesDD@users.noreply.github.com>
2023-08-31 07:56:53 +0200

    Fix LDAPi vulnerability location (#3593)

--- packages/dd-trace/src/appsec/iast/analyzers/ldap-injection-analyzer.js
+++ packages/dd-trace/src/appsec/iast/analyzers/ldap-injection-analyzer.js
@@ -1,6 +1,9 @@
 'use strict'
 const InjectionAnalyzer = require('./injection-analyzer')
 const { LDAP_INJECTION } = require('../vulnerabilities')
+const { getNodeModulesPaths } = require('../path-line')
+
+const EXCLUDED_PATHS = getNodeModulesPaths('ldapjs-promise')
 
 class LdapInjectionAnalyzer extends InjectionAnalyzer {
   constructor () {
@@ -9,6 +12,10 @@ class LdapInjectionAnalyzer extends InjectionAnalyzer {
 
   onConfigure () {
     this.addSub('datadog:ldapjs:client:search', ({ base, filter }) => this.analyzeAll(base, filter))
+  }
+
+  _getExcludedPaths () {
+    return EXCLUDED_PATHS
   }
 }
 
