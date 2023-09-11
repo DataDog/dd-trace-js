7796d42002277ab6627b2c8cf4d3e6cd6e6c3581
Ugaitz Urien <ugaitz.urien@datadoghq.com>
2023-09-11 09:17:22 +0200

    Fix tests

--- packages/dd-trace/test/appsec/plugins/scrub_cmd_params.spec.js
+++ packages/dd-trace/test/appsec/plugins/scrub_cmd_params.spec.js
@@ -11,14 +11,22 @@ describe('scrub cmds', () => {
 
   it('Should scrub environment variables', () => {
     expect(scrubCmdParams('ENV=XXX LD_PRELOAD=YYY ls')).to.be.deep.equal(['ENV=?', 'LD_PRELOAD=YYY', 'ls'])
+    expect(scrubCmdParams('DD_TEST=info SHELL=zsh ls -l')).to.be.deep.equal(['DD_TEST=?', 'SHELL=?', 'ls', '-l'])
   })
 
   it('Should scrub secret values', () => {
     expect(scrubCmdParams('cmd --pass abc --token=def')).to.be.deep.equal(['cmd', '--pass', '?', '--token=?'])
+
+    expect(scrubCmdParams('mysqladmin -u root password very_secret'))
+      .to.be.deep.equal(['mysqladmin', '-u', 'root', 'password', '?'])
+
+    expect(scrubCmdParams('test -password very_secret -api_key 1234'))
+      .to.be.deep.equal(['test', '-password', '?', '-api_key', '?'])
   })
 
   it('Should scrub md5 commands', () => {
     expect(scrubCmdParams('md5 -s pony')).to.be.deep.equal(['md5', '?', '?'])
+
     expect(scrubCmdParams('cat passwords.txt | while read line; do; md5 -s $line; done')).to.be.deep
       .equal([
         'cat',
@@ -35,6 +43,13 @@ describe('scrub cmds', () => {
         '?',
         ';',
         'done'
+      ])
+  })
+
+  it('should scrub shell expressions', () => {
+    expect(scrubCmdParams('md5 -s secret ; mysqladmin -u root password 1234 | test api_key 4321'))
+      .to.be.deep.equal([
+        'md5', '?', '?', ';', 'mysqladmin', '-u', 'root', 'password', '?', '|', 'test', 'api_key', '?'
       ])
   })
 
