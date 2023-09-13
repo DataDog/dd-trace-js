a3e675538738c81c96f0011eae60b199966ffd85
Ayan Khan <khanayan_123@hotmail.com>
2023-09-13 09:41:47 -0400

    fixes and adds esm integration tests for oracledb (#3580)
    * adds esm integration tests for oracledb

---
+++ packages/datadog-plugin-oracledb/test/integration-test/client.spec.js
@@ -0,0 +1,50 @@
+'use strict'
+
+const {
+  FakeAgent,
+  createSandbox,
+  checkSpansForServiceName,
+  spawnPluginIntegrationTestProc
+} = require('../../../../integration-tests/helpers')
+const { assert } = require('chai')
+
+describe('esm', () => {
+  let agent
+  let proc
+  let sandbox
+
+  before(async function () {
+    this.timeout(20000)
+    sandbox = await createSandbox(['oracledb@^5.0.0'], false, [
+      `./packages/datadog-plugin-oracledb/test/integration-test/*`])
+  })
+
+  after(async () => {
+    await sandbox.remove()
+  })
+
+  beforeEach(async () => {
+    agent = await new FakeAgent().start()
+  })
+
+  afterEach(async () => {
+    proc && proc.kill()
+    await agent.stop()
+  })
+
+  context('oracledb', () => {
+    it('is instrumented', async () => {
+      const res = agent.assertMessageReceived(({ headers, payload }) => {
+        assert.propertyVal(headers, 'host', `127.0.0.1:${agent.port}`)
+        assert.isArray(payload)
+        assert.strictEqual(checkSpansForServiceName(payload, 'oracle.query'), true)
+      })
+
+      proc = await spawnPluginIntegrationTestProc(sandbox.folder, 'server.mjs', agent.port, undefined, {
+        LD_LIBRARY_PATH: '/usr/lib/oracle/18.5/client64/lib:'
+      })
+
+      await res
+    }).timeout(20000)
+  })
+})
