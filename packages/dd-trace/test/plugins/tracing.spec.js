afc84eaa7fb2402795bf7a28ae8d09b2063a6030
Jordi Bertran de Balanda <jordi.bertran@datadoghq.com>
2023-09-14 10:53:07 +0200

    Add `_dd.base_service` tag (#3557)
    * compute _dd.base_service in format
    * add _dd.base_service test for custom instrumentation
    * add generic plugin behaviour tests

--- packages/dd-trace/test/plugins/tracing.spec.js
+++ packages/dd-trace/test/plugins/tracing.spec.js
@@ -3,6 +3,9 @@
 require('../setup/tap')
 
 const TracingPlugin = require('../../src/plugins/tracing')
+const agent = require('../plugins/agent')
+const plugins = require('../../src/plugins')
+const { channel } = require('../../../diagnostics_channel')
 
 describe('TracingPlugin', () => {
   describe('startSpan method', () => {
@@ -22,6 +25,94 @@ describe('TracingPlugin', () => {
         sinon.match({
           childOf: 'some parent span'
         })
+      )
+    })
+  })
+})
+
+describe('common Plugin behaviour', () => {
+  before(() => agent.load())
+  after(() => agent.close({ ritmReset: false }))
+  class CommonPlugin extends TracingPlugin {
+    static get id () { return 'commonPlugin' }
+    static get operation () { return 'dothings' }
+
+    start () {
+      return this.startSpan('common.operation', {
+        service: this.config.service || this._tracerConfig.service
+      }, true)
+    }
+  }
+
+  class SuffixPlugin extends TracingPlugin {
+    static get id () { return 'suffixPlugin' }
+    static get operation () { return 'dothings' }
+    start () {
+      return this.startSpan('common.operation', {
+        service: this.config.service || `${this.tracer._service}-suffix`
+      }, true)
+    }
+  }
+
+  const testPlugins = { 'commonPlugin': CommonPlugin, 'suffixPlugin': SuffixPlugin }
+  const loadChannel = channel('dd-trace:instrumentation:load')
+
+  before(() => {
+    for (const [name, cls] of Object.entries(testPlugins)) {
+      plugins[name] = cls
+      loadChannel.publish({ name })
+    }
+  })
+
+  after(() => { Object.keys(testPlugins).forEach(name => delete plugins[name]) })
+
+  describe('tagBaseService', () => {
+    function makeSpan (done, pluginName, pluginConf, spanExpectations) {
+      /**
+       * Make plugin `pluginName` generate a span given `pluginConf` plugin init
+       * and verify it against spanExpectations: span -> void
+       */
+      const startCh = channel(`apm:${pluginName}:dothings:start`)
+      const finishCh = channel(`apm:${pluginName}:dothings:finish`)
+
+      agent.reload(pluginName, pluginConf)
+
+      agent.use(
+        traces => {
+          const span = traces[0][0]
+          spanExpectations(span)
+        }
+      ).then(done).catch(done)
+
+      startCh.publish({ foo: 'bar' })
+      finishCh.publish({})
+    }
+
+    it('should tag when tracer service does not match plugin', done => {
+      makeSpan(
+        done, 'commonPlugin', { service: 'not-the-right-test' },
+        span => {
+          expect(span).to.have.property('service', 'not-the-right-test')
+          expect(span.meta).to.have.property('_dd.base_service', 'test')
+        }
+      )
+    })
+    it('should tag when plugin impl does not match tracer service', done => {
+      makeSpan(
+        done, 'suffixPlugin', {},
+        span => {
+          expect(span).to.have.property('service', 'test-suffix')
+          expect(span.meta).to.have.property('_dd.base_service', 'test')
+        }
+      )
+    })
+    it('should not tag when service matches tracer service', done => {
+      makeSpan(
+        done, 'commonPlugin', {},
+        span => {
+          expect(span).to.have.property('service', 'test')
+          expect(span.meta).to.not.have.property('_dd.base_service', 'test')
+        }
       )
     })
   })
