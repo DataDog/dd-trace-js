11e2f46ee8c9fd6d46c112517b1c019fc5a38dde
Ayan Khan <khanayan_123@hotmail.com>
2023-09-21 14:18:13 -0400

    manually fix nextjs rebase error

--- packages/datadog-plugin-next/test/integration-test/client.spec.js
+++ packages/datadog-plugin-next/test/integration-test/client.spec.js
@@ -18,51 +18,11 @@ describe('esm', () => {
   let sandbox
 
   withVersions('next', 'next', DD_MAJOR >= 4 && '>=11', version => {
-<<<<<<< HEAD
-<<<<<<< HEAD
-    // skip any semver incompatible versions
-=======
->>>>>>> 8f704e9fd (address feedback)
     before(async function () {
       // next builds slower in the CI, match timeout with unit tests
       this.timeout(120 * 1000)
       sandbox = await createSandbox([`'next@${version}'`, 'react', 'react-dom'],
         false, ['./packages/datadog-plugin-next/test/integration-test/*'], 'yarn exec next build')
-<<<<<<< HEAD
-=======
-    describe('next', () => {
-      before(async function () {
-        // next builds slower in the CI, match timeout with unit tests
-        this.timeout(120 * 1000)
-        sandbox = await createSandbox([`'next@${version}'`, 'react', 'react-dom'],
-          false, ['./packages/datadog-plugin-next/test/integration-test/*'], 'yarn exec next build')
-      })
-
-      after(async () => {
-        await sandbox.remove()
-      })
-
-      beforeEach(async () => {
-        agent = await new FakeAgent().start()
-      })
-
-      afterEach(async () => {
-        proc && proc.kill()
-        await agent.stop()
-      })
-
-      it('is instrumented', async () => {
-        proc = await spawnPluginIntegrationTestProc(sandbox.folder, 'server.mjs', agent.port)
-
-        return curlAndAssertMessage(agent, proc, ({ headers, payload }) => {
-          assert.propertyVal(headers, 'host', `127.0.0.1:${agent.port}`)
-          assert.isArray(payload)
-          assert.strictEqual(checkSpansForServiceName(payload, 'next.request'), true)
-        })
-      }).timeout(120 * 1000)
->>>>>>> 8f1aa2fc4 (address feedback)
-=======
->>>>>>> 8f704e9fd (address feedback)
     })
 
     after(async () => {
@@ -79,23 +39,14 @@ describe('esm', () => {
     })
 
     it('is instrumented', async () => {
-<<<<<<< HEAD
       proc = await spawnPluginIntegrationTestProc(sandbox.folder, 'server.mjs', agent.port, undefined, {
         NODE_OPTIONS: `--loader=${hookFile} --require dd-trace/init`
       })
-=======
-      proc = await spawnPluginIntegrationTestProc(sandbox.folder, 'server.mjs', agent.port)
-
->>>>>>> 8f704e9fd (address feedback)
       return curlAndAssertMessage(agent, proc, ({ headers, payload }) => {
         assert.propertyVal(headers, 'host', `127.0.0.1:${agent.port}`)
         assert.isArray(payload)
         assert.strictEqual(checkSpansForServiceName(payload, 'next.request'), true)
-<<<<<<< HEAD
       }, undefined, undefined, true)
-=======
-      })
->>>>>>> 8f704e9fd (address feedback)
     }).timeout(120 * 1000)
   })
 })
