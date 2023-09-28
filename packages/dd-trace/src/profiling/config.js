df3d4e56789ae783011fb96aeccf6c61b43b9481
Attila Szegedi <attila.szegedi@datadoghq.com>
2023-09-28 13:07:05 +0200

    make sure code hotspots being enabled is consistent with endpoint profiling being enabled

--- packages/dd-trace/src/profiling/config.js
+++ packages/dd-trace/src/profiling/config.js
@@ -127,10 +127,25 @@ class Config {
     const profilers = options.profilers
       ? options.profilers
       : getProfilers({ DD_PROFILING_HEAP_ENABLED, DD_PROFILING_WALLTIME_ENABLED, DD_PROFILING_PROFILERS })
-    this.codeHotspotsEnabled = isTrue(coalesce(options.codeHotspotsEnabled,
-      DD_PROFILING_CODEHOTSPOTS_ENABLED,
-      DD_PROFILING_EXPERIMENTAL_CODEHOTSPOTS_ENABLED, false))
+
+    function getCodeHotspotsOptionsOr (defvalue) {
+      return coalesce(options.codeHotspotsEnabled,
+        DD_PROFILING_CODEHOTSPOTS_ENABLED,
+        DD_PROFILING_EXPERIMENTAL_CODEHOTSPOTS_ENABLED, defvalue)
+    }
+    this.codeHotspotsEnabled = isTrue(getCodeHotspotsOptionsOr(false))
     logExperimentalVarDeprecation('CODEHOTSPOTS_ENABLED')
+    if (this.endpointCollectionEnabled && !this.codeHotspotsEnabled) {
+      if (getCodeHotspotsOptionsOr(undefined) !== undefined) {
+        this.logger.warn(
+          'Endpoint collection is enabled, but Code Hotspots are disabled. ' +
+          'Enable Code Hotspots too for endpoint collection to work.')
+        this.endpointCollectionEnabled = false
+      } else {
+        this.logger.info('Code Hotspots are implicitly enabled by endpoint collection.')
+        this.codeHotspotsEnabled = true
+      }
+    }
 
     this.profilers = ensureProfilers(profilers, this)
   }
