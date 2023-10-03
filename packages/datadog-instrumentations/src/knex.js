8e3c7113c742d1788640fa258ca46133f95ca1fb
Carles Capell <107924659+CarlesDD@users.noreply.github.com>
2023-10-03 10:45:27 +0200

    Fix SQLi location when using knex [APPSEC-10688] (#3607)
    * Fix SQLi location when using knex
    
    * Remove onProgress wrapping from knex then
    
    * Rename asyncResource
    
    * Add spacing in code to improve readability
    
    * Avoid using shimmer to wrap then from knex raw result
    
    * Detect dialect for SQLi with knex
    
    * Instrument knex catch and asCallback

--- packages/datadog-instrumentations/src/knex.js
+++ packages/datadog-instrumentations/src/knex.js
@@ -1,8 +1,11 @@
 'use strict'
 
-const { addHook } = require('./helpers/instrument')
+const { addHook, channel } = require('./helpers/instrument')
 const { wrapThen } = require('./helpers/promise')
 const shimmer = require('../../datadog-shimmer')
+
+const startRawQueryCh = channel('datadog:knex:raw:start')
+const finishRawQueryCh = channel('datadog:knex:raw:finish')
 
 patch('lib/query/builder.js')
 patch('lib/raw.js')
@@ -17,4 +20,62 @@ function patch (file) {
     shimmer.wrap(Builder.prototype, 'then', wrapThen)
     return Builder
   })
+}
+
+addHook({
+  name: 'knex',
+  versions: ['>=2'],
+  file: 'lib/knex-builder/Knex.js'
+}, Knex => {
+  shimmer.wrap(Knex.Client.prototype, 'raw', raw => function () {
+    if (!startRawQueryCh.hasSubscribers) {
+      return raw.apply(this, arguments)
+    }
+
+    const sql = arguments[0]
+
+    // Skip query done by Knex to get the value used for undefined
+    if (sql === 'DEFAULT') {
+      return raw.apply(this, arguments)
+    }
+
+    function finish () {
+      finishRawQueryCh.publish()
+    }
+
+    startRawQueryCh.publish({ sql, dialect: this.dialect })
+
+    const rawResult = raw.apply(this, arguments)
+
+    shimmer.wrap(rawResult, 'then', originalThen => function () {
+      arguments[0] = wrapCallbackWithFinish(arguments[0], finish)
+      arguments[1] = wrapCallbackWithFinish(arguments[1], finish)
+
+      const originalThenResult = originalThen.apply(this, arguments)
+
+      shimmer.wrap(originalThenResult, 'catch', originalCatch => function () {
+        arguments[0] = wrapCallbackWithFinish(arguments[0], finish)
+        return originalCatch.apply(this, arguments)
+      })
+
+      return originalThenResult
+    })
+
+    shimmer.wrap(rawResult, 'asCallback', originalAsCallback => function () {
+      arguments[0] = wrapCallbackWithFinish(arguments[0], finish)
+      return originalAsCallback.apply(this, arguments)
+    })
+
+    return rawResult
+  })
+  return Knex
+})
+
+function wrapCallbackWithFinish (callback, finish) {
+  if (typeof callback !== 'function') return callback
+
+  return function () {
+    finish()
+    callback.apply(this, arguments)
+  }
 }
