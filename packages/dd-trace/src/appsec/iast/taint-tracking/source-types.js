551816c7cc7632a27d01a0e0f5ad9726f1fffab2
Carles Capell <107924659+CarlesDD@users.noreply.github.com>
2023-10-03 11:28:48 +0200

    Change source origin from HTTP_REQUEST_PATH to HTTP_REQUEST_URI (#3644)

--- packages/dd-trace/src/appsec/iast/taint-tracking/source-types.js
+++ packages/dd-trace/src/appsec/iast/taint-tracking/source-types.js
@@ -8,5 +8,6 @@ module.exports = {
   HTTP_REQUEST_HEADER_VALUE: 'http.request.header',
   HTTP_REQUEST_PARAMETER: 'http.request.parameter',
   HTTP_REQUEST_PATH: 'http.request.path',
-  HTTP_REQUEST_PATH_PARAM: 'http.request.path.parameter'
+  HTTP_REQUEST_PATH_PARAM: 'http.request.path.parameter',
+  HTTP_REQUEST_URI: 'http.request.uri'
 }
