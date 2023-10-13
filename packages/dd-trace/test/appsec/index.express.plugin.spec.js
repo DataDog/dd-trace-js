4e4b9e62f6748f414419e6ed2370781b71e51e5a
Julio Gonzalez <julio.gonzalez@datadoghq.com>
2023-10-13 15:39:22 +0200

    Move API security tests to express plugin file.

--- packages/dd-trace/test/appsec/index.express.plugin.spec.js
+++ packages/dd-trace/test/appsec/index.express.plugin.spec.js
@@ -25,6 +25,10 @@ withVersions('express', 'express', version => {
         res.end('DONE')
       })
 
+      app.post('/', (req, res) => {
+        res.end('DONE')
+      })
+
       getPort().then(newPort => {
         port = newPort
         server = app.listen(port, () => {
@@ -33,37 +37,90 @@ withVersions('express', 'express', version => {
       })
     })
 
-    beforeEach(async () => {
-      requestBody = sinon.stub()
-      appsec.enable(new Config({ appsec: { enabled: true, rules: path.join(__dirname, 'express-rules.json') } }))
-    })
-
-    afterEach(() => {
-      appsec.disable()
-    })
-
     after(() => {
       server.close()
       return agent.close({ ritmReset: false })
     })
 
-    it('should not block the request without an attack', async () => {
-      const res = await axios.get(`http://localhost:${port}/?key=value`)
+    describe('Blocking', () => {
+      beforeEach(async () => {
+        requestBody = sinon.stub()
+        appsec.enable(new Config({ appsec: { enabled: true, rules: path.join(__dirname, 'express-rules.json') } }))
+      })
 
-      expect(requestBody).to.be.calledOnce
-      expect(res.data).to.be.equal('DONE')
+      afterEach(() => {
+        appsec.disable()
+      })
+
+      it('should not block the request without an attack', async () => {
+        const res = await axios.get(`http://localhost:${port}/?key=value`)
+
+        expect(requestBody).to.be.calledOnce
+        expect(res.data).to.be.equal('DONE')
+      })
+
+      it('should block the request when attack is detected', async () => {
+        try {
+          await axios.get(`http://localhost:${port}/?key=testattack`)
+
+          return Promise.reject(new Error('Request should not return 200'))
+        } catch (e) {
+          expect(e.response.status).to.be.equals(403)
+          expect(e.response.data).to.be.deep.equal(JSON.parse(json))
+          expect(requestBody).not.to.be.called
+        }
+      })
     })
 
-    it('should block the request when attack is detected', async () => {
-      try {
-        await axios.get(`http://localhost:${port}/?key=testattack`)
+    describe('Api Security', () => {
+      let config
 
-        return Promise.reject(new Error('Request should not return 200'))
-      } catch (e) {
-        expect(e.response.status).to.be.equals(403)
-        expect(e.response.data).to.be.deep.equal(JSON.parse(json))
-        expect(requestBody).not.to.be.called
-      }
+      beforeEach(() => {
+        config = new Config({
+          appsec: {
+            enabled: true,
+            rules: path.join(__dirname, 'api_security_rules.json'),
+            apiSecurity: {
+              enabled: true,
+              requestSampling: 0
+            }
+          }
+        })
+      })
+
+      afterEach(() => {
+        appsec.disable()
+      })
+
+      it('should not get the schema', async () => {
+        appsec.enable(config)
+
+        const res = await axios.post(`http://localhost:${port}/`, { key: 'testattack' })
+
+        await agent.use((traces) => {
+          const span = traces[0][0]
+          expect(span.meta).not.to.haveOwnProperty('_dd.appsec.s.req.body')
+        })
+
+        expect(res.status).to.be.equal(200)
+        expect(res.data).to.be.equal('DONE')
+      })
+
+      it('should get the schema', async () => {
+        config.appsec.apiSecurity.requestSampling = 1
+        appsec.enable(config)
+
+        const res = await axios.post(`http://localhost:${port}/`, { key: 'testattack' })
+
+        await agent.use((traces) => {
+          const span = traces[0][0]
+          expect(span.meta).to.haveOwnProperty('_dd.appsec.s.req.body')
+          expect(span.meta['_dd.appsec.s.req.body']).to.be.equal('H4sIAAAAAAAAA4u2iAUA8YntnQMAAAA=')
+        })
+
+        expect(res.status).to.be.equal(200)
+        expect(res.data).to.be.equal('DONE')
+      })
     })
   })
 })
