042f384e9494a9823c982eea480ed5e9078c6557
Sam Brenner <sam.brenner@datadoghq.com>
2023-10-17 10:37:55 +0200

    make next.config.js dynamic

--- packages/datadog-plugin-next/test/next.config.js
+++ packages/datadog-plugin-next/test/next.config.js
@@ -1,10 +1,24 @@
-module.exports = {
+// Build up the object dynamically to reduce warnings in output
+
+const { satisfies } = require('semver')
+
+const { VERSION } = process.env
+
+const config = {
   eslint: {
     ignoreDuringBuilds: true
   },
-  // this will warn when server is run normally, but it won't have any other effect
-  output: 'standalone',
-  experimental: {
-    appDir: true // likewise here, will just warn if appDir is not experimental but not change anything
-  }
+  experimental: {}
 }
+
+// standalone can be set
+if (satisfies(VERSION, '>=12.0.0')) {
+  config.output = 'standalone'
+}
+
+// appDir needs to be enabled as experimental
+if (satisfies(VERSION, '>=13.3.0 <13.4.0')) {
+  config.experimental.appDir = true
+}
+
+module.exports = config
