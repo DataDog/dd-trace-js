e3dd5185554cc01022312d9a30929c955b38f299
Juan Antonio Fernández de Alba <juan.fernandezdealba@datadoghq.com>
2023-10-26 14:40:08 +0200

    [ci-visibility] Add test for playwright custom metrics (#3746)

--- integration-tests/ci-visibility/playwright-tests/landing-page-test.js
+++ integration-tests/ci-visibility/playwright-tests/landing-page-test.js
@@ -22,6 +22,7 @@ test.describe('playwright', () => {
   })
   test('should work with annotated tests', async ({ page }) => {
     test.info().annotations.push({ type: 'DD_TAGS[test.memory.usage]', description: 'low' })
+    test.info().annotations.push({ type: 'DD_TAGS[test.memory.allocations]', description: 16 })
     // this is malformed and should be ignored
     test.info().annotations.push({ type: 'DD_TAGS[test.invalid', description: 'high' })
     await expect(page.locator('.hello-world')).toHaveText([
