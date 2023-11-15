dc6400f139f75166ed1aac524483da735be7ebda
Igor Unanua <igor.unanua@datadoghq.com>
2023-11-15 12:45:10 +0100

    Use mongo directly

--- packages/dd-trace/test/appsec/iast/analyzers/nosql-injection-mongodb-analyzer.mquery.plugin.spec.js
+++ packages/dd-trace/test/appsec/iast/analyzers/nosql-injection-mongodb-analyzer.mquery.plugin.spec.js
@@ -3,89 +3,98 @@
 const { prepareTestServerForIastInExpress } = require('../utils')
 const axios = require('axios')
 const agent = require('../../../plugins/agent')
-const semver = require('semver')
 const os = require('os')
 const path = require('path')
+const semver = require('semver')
 const fs = require('fs')
-const { NODE_MAJOR } = require('../../../../../../version')
 
 describe('nosql injection detection in mongodb - whole feature', () => {
   withVersions('express', 'express', '>4.18.0', expressVersion => {
-    withVersions('mongoose', 'mongoose', '>=5.0.0', mongooseVersion => {
-      const specificMongooseVersion = require(`../../../../../../versions/mongoose@${mongooseVersion}`).version()
-      if (NODE_MAJOR === 14 && semver.satisfies(specificMongooseVersion, '>=8')) return
+    withVersions('mongodb', 'mongodb', mongodbVersion => {
+      const mongodb = require(`../../../../../../versions/mongodb@${mongodbVersion}`)
 
-      const vulnerableMethodFilename = 'mquery-vulnerable-method.js'
-      let mongoose, Test, tmpFilePath, dbName
+      const satisfiesNodeVersionForMongo3and4 =
+        (semver.satisfies(process.version, '<14.20.1') && semver.satisfies(mongodb.version(), '>=3.3 <5'))
+      const satisfiesNodeVersionForMongo5 =
+        (semver.satisfies(process.version, '>=14.20.1 <16.20.1') && semver.satisfies(mongodb.version(), '5'))
+      const satisfiesNodeVersionForMongo6 =
+        (semver.satisfies(process.version, '>=16.20.1') && semver.satisfies(mongodb.version(), '>=6'))
 
-      before(() => {
-        return agent.load(['mongoose'])
-      })
+      if (!satisfiesNodeVersionForMongo3and4 && !satisfiesNodeVersionForMongo5 && !satisfiesNodeVersionForMongo6) return
 
-      before(async () => {
-        const id = require('../../../../src/id')
-        dbName = id().toString()
-        mongoose = require(`../../../../../../versions/mongoose@${mongooseVersion}`).get()
+      withVersions('express-mongo-sanitize', 'mquery', mqueryVersion => {
+        let mquery
 
-        mongoose.connect(`mongodb://localhost:27017/${dbName}`, {
-          useNewUrlParser: true,
-          useUnifiedTopology: true
+        const vulnerableMethodFilename = 'mquery-vulnerable-method.js'
+        let client, testCollection, tmpFilePath, dbName
+
+        before(() => {
+          return agent.load(['mongodb'], { client: false }, { flushInterval: 1 })
         })
 
-        Test = mongoose.model('Test', { name: String })
+        before(async () => {
+          const id = require('../../../../src/id')
+          dbName = id().toString()
+          const mongo = require(`../../../../../../versions/mongodb@${mongodbVersion}`).get()
+          mquery = require(`../../../../../../versions/mquery@${mqueryVersion}`).get()
 
-        const src = path.join(__dirname, 'resources', vulnerableMethodFilename)
+          client = new mongo.MongoClient(`mongodb://localhost:27017/${dbName}`, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+          })
+          await client.connect()
 
-        tmpFilePath = path.join(os.tmpdir(), vulnerableMethodFilename)
-        try {
+          testCollection = client.db().collection('Test')
+
+          const src = path.join(__dirname, 'resources', vulnerableMethodFilename)
+
+          tmpFilePath = path.join(os.tmpdir(), vulnerableMethodFilename)
+          try {
+            fs.unlinkSync(tmpFilePath)
+          } catch (e) {
+            // ignore the error
+          }
+          fs.copyFileSync(src, tmpFilePath)
+        })
+
+        after(async () => {
           fs.unlinkSync(tmpFilePath)
-        } catch (e) {
-          // ignore the error
-        }
-        fs.copyFileSync(src, tmpFilePath)
-      })
-
-      after(() => {
-        fs.unlinkSync(tmpFilePath)
-        return mongoose.disconnect()
-      })
-
-      withVersions('mongoose', 'mquery', mqueryVersion => {
-        const mquery = require(`../../../../../../versions/mquery@${mqueryVersion}`).get()
+          await client.close()
+        })
 
         prepareTestServerForIastInExpress('Test with mquery', expressVersion,
           (testThatRequestHasVulnerability, testThatRequestHasNoVulnerability) => {
-            // testThatRequestHasVulnerability({
-            //   fn: async (req, res) => {
-            //     mquery()
-            //       .find({
-            //         name: req.query.key,
-            //         value: [1, 2,
-            //           'value',
-            //           false, req.query.key]
-            //       })
-            //       .collection(Test.collection)
-            //       .then(() => {
-            //         res.end()
-            //       }).catch((err) => {
-            //         res.end()
-            //       })
-            //   },
-            //   vulnerability: 'NOSQL_MONGODB_INJECTION',
-            //   makeRequest: (done, config) => {
-            //     axios.get(`http://localhost:${config.port}/?key=value`).catch(done)
-            //   }
-            // })
+            testThatRequestHasVulnerability({
+              fn: async (req, res) => {
+                mquery()
+                  .find({
+                    name: req.query.key,
+                    value: [1, 2,
+                      'value',
+                      false, req.query.key]
+                  })
+                  .collection(testCollection)
+                  .then(() => {
+                    res.end()
+                  }).catch((err) => {
+                    res.end()
+                  })
+              },
+              vulnerability: 'NOSQL_MONGODB_INJECTION',
+              makeRequest: (done, config) => {
+                axios.get(`http://localhost:${config.port}/?key=value`).catch(done)
+              }
+            })
 
             testThatRequestHasVulnerability({
-              testDescription: 'should have NOSQL_MONGODB_INJECTION vulnerability in correct file and line',
+              testDescription: 'should have NOSQL_MONGODB_INJECTION vulnerability in correct file and line [find]',
               fn: async (req, res) => {
                 const filter = {
                   name: {
                     child: [req.query.key]
                   }
                 }
-                require(tmpFilePath)(Test.collection, filter, () => {
+                require(tmpFilePath).vulnerableFind(testCollection, filter, () => {
                   res.end()
                 })
               },
@@ -102,8 +111,33 @@ describe('nosql injection detection in mongodb - whole feature', () => {
               }
             })
 
+            testThatRequestHasVulnerability({
+              testDescription: 'should have NOSQL_MONGODB_INJECTION vulnerability in correct file and line [findOne]',
+              fn: async (req, res) => {
+                const filter = {
+                  name: {
+                    child: [req.query.key]
+                  }
+                }
+                require(tmpFilePath).vulnerableFindOne(testCollection, filter, () => {
+                  res.end()
+                })
+              },
+              vulnerability: 'NOSQL_MONGODB_INJECTION',
+              makeRequest: (done, config) => {
+                axios.get(`http://localhost:${config.port}/?key=value`).catch(done)
+              },
+              occurrences: {
+                occurrences: 1,
+                location: {
+                  path: vulnerableMethodFilename,
+                  line: 16
+                }
+              }
+            })
+
             testThatRequestHasNoVulnerability(async (req, res) => {
-              mquery(Test.collection)
+              mquery(testCollection)
                 .find({
                   name: 'test'
                 }).then(() => {
@@ -111,6 +145,31 @@ describe('nosql injection detection in mongodb - whole feature', () => {
                 })
             }, 'NOSQL_MONGODB_INJECTION')
           })
+
+        withVersions('express-mongo-sanitize', 'express-mongo-sanitize', expressMongoSanitizeVersion => {
+          prepareTestServerForIastInExpress('Test with sanitization middleware', expressVersion, (expressApp) => {
+            const mongoSanitize =
+                require(`../../../../../../versions/express-mongo-sanitize@${expressMongoSanitizeVersion}`).get()
+            expressApp.use(mongoSanitize())
+          }, (testThatRequestHasVulnerability, testThatRequestHasNoVulnerability) => {
+            testThatRequestHasNoVulnerability({
+              fn: async (req, res) => {
+                const filter = {
+                  name: {
+                    child: [req.query.key]
+                  }
+                }
+                require(tmpFilePath).vulnerableFindOne(testCollection, filter, () => {
+                  res.end()
+                })
+              },
+              vulnerability: 'NOSQL_MONGODB_INJECTION',
+              makeRequest: (done, config) => {
+                axios.get(`http://localhost:${config.port}/?key=value`).catch(done)
+              }
+            })
+          })
+        })
       })
     })
   })
--- packages/dd-trace/test/appsec/iast/analyzers/resources/mquery-vulnerable-method.js
+++ packages/dd-trace/test/appsec/iast/analyzers/resources/mquery-vulnerable-method.js
@@ -2,9 +2,21 @@
 
 const mquery = require('mquery')
 
-module.exports = function vulnerableMethod (collection, filter, cb) {
+function vulnerableFind (collection, filter, cb) {
   return mquery()
     .find(filter)
     .collection(collection)
     .then(cb).catch(cb)
+}
+
+function vulnerableFindOne (collection, filter, cb) {
+  return mquery()
+    .findOne(filter)
+    .collection(collection)
+    .then(cb).catch(cb)
+}
+
+module.exports = {
+  vulnerableFind,
+  vulnerableFindOne
 }
