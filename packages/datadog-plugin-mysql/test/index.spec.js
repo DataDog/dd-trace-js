36de1cb80f2aa2df3bfd19bb92cd3ea57692a298
Thomas Hunter II <tlhunter@datadog.com>
2023-11-15 08:57:18 -0800

    core: enable 128-bit ids by default for v5+ (#3656)
    - makes 128 bit ID generation the default
    - but only for v5+ of the tracer

--- packages/datadog-plugin-mysql/test/index.spec.js
+++ packages/datadog-plugin-mysql/test/index.spec.js
@@ -3,6 +3,7 @@
 const agent = require('../../dd-trace/test/plugins/agent')
 const proxyquire = require('proxyquire').noPreserveCache()
 const { ERROR_MESSAGE, ERROR_TYPE, ERROR_STACK } = require('../../dd-trace/src/constants')
+const { DD_MAJOR } = require('../../../version')
 
 const { expectedSchema, rawExpectedSchema } = require('./naming')
 
@@ -454,14 +455,19 @@ describe('Plugin', () => {
         it('query text should contain traceparent', done => {
           let queryText = ''
           agent.use(traces => {
-            const traceId = traces[0][0].trace_id.toString(16).padStart(32, '0')
+            const expectedTimePrefix = DD_MAJOR >= 5
+              ? Math.floor(clock.now / 1000).toString(16).padStart(8, '0').padEnd(16, '0')
+              : '0000000000000000'
+            const traceId = expectedTimePrefix + traces[0][0].trace_id.toString(16).padStart(16, '0')
             const spanId = traces[0][0].span_id.toString(16).padStart(16, '0')
 
             expect(queryText).to.equal(
               `/*dddbs='post',dde='tester',ddps='test',ddpv='8.4.0',` +
               `traceparent='00-${traceId}-${spanId}-00'*/ SELECT 1 + 1 AS solution`)
           }).then(done, done)
+          const clock = sinon.useFakeTimers(new Date())
           connection.query('SELECT 1 + 1 AS solution', () => {
+            clock.restore()
             queryText = connection._protocol._queue[0].sql
           })
         })
@@ -531,14 +537,19 @@ describe('Plugin', () => {
         it('query text should contain traceparent', done => {
           let queryText = ''
           agent.use(traces => {
-            const traceId = traces[0][0].trace_id.toString(16).padStart(32, '0')
+            const expectedTimePrefix = DD_MAJOR >= 5
+              ? Math.floor(clock.now / 1000).toString(16).padStart(8, '0').padEnd(16, '0')
+              : '0000000000000000'
+            const traceId = expectedTimePrefix + traces[0][0].trace_id.toString(16).padStart(16, '0')
             const spanId = traces[0][0].span_id.toString(16).padStart(16, '0')
 
             expect(queryText).to.equal(
               `/*dddbs='post',dde='tester',ddps='test',ddpv='8.4.0',` +
               `traceparent='00-${traceId}-${spanId}-00'*/ SELECT 1 + 1 AS solution`)
           }).then(done, done)
+          const clock = sinon.useFakeTimers(new Date())
           pool.query('SELECT 1 + 1 AS solution', () => {
+            clock.restore()
             queryText = pool._allConnections[0]._protocol._queue[0].sql
           })
         })
