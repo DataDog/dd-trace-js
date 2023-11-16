7e1161a510d32f0c076a4258c43830ebb475b14d
Ayan Khan <khanayan_123@hotmail.com>
2023-11-16 02:55:07 -0500

    try

--- packages/datadog-instrumentations/src/aerospike.js
+++ packages/datadog-instrumentations/src/aerospike.js
@@ -74,15 +74,15 @@ if (NODE_MAJOR === 20) {
   })
 }
 
-if (NODE_MAJOR > 14 && NODE_MAJOR <= 20) {
-  addHook({ name: 'aerospike', file: 'lib/commands/command.js', versions: ['5.5.0 - 5.7.0'] }, commandFactory => {
-    return shimmer.wrap(commandFactory, wrapCreateCommand(commandFactory))
-  })
-}
+// if (NODE_MAJOR > 14 && NODE_MAJOR <= 20) {
+//   addHook({ name: 'aerospike', file: 'lib/commands/command.js', versions: ['5.5.0 - 5.7.0'] }, commandFactory => {
+//     return shimmer.wrap(commandFactory, wrapCreateCommand(commandFactory))
+//   })
+// }
 
-if (NODE_MAJOR >= 14 && NODE_MAJOR < 20) {
-  addHook({ name: 'aerospike', file: 'lib/commands/command.js', versions: ['4', '5.0.0 - 5.3.0'] },
-    commandFactory => {
-      return shimmer.wrap(commandFactory, wrapCreateCommand(commandFactory))
-    })
-}
+// if (NODE_MAJOR >= 14 && NODE_MAJOR < 20) {
+//   addHook({ name: 'aerospike', file: 'lib/commands/command.js', versions: ['3.5.0', '4', '5.0.0 - 5.3.0'] },
+//     commandFactory => {
+//       return shimmer.wrap(commandFactory, wrapCreateCommand(commandFactory))
+//     })
+// }
