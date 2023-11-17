9b593d0bdefb19efc4dbd7bbd500d3253c9abb3f
Ayan Khan <khanayan_123@hotmail.com>
2023-11-17 17:25:14 -0500

    try

--- packages/datadog-plugin-aerospike/test/index.spec.js
+++ packages/datadog-plugin-aerospike/test/index.spec.js
@@ -200,7 +200,6 @@ describe('Plugin', () => {
                 datatype: aerospike.indexDataType.STRING
               }
               client.createIndex(index, (error, job) => {
-                console.log(10000000000)
                 job.wait((waitError) => {
                   const query = client.query(ns, 'demo')
                   const queryPolicy = {
@@ -209,6 +208,7 @@ describe('Plugin', () => {
                   query.select('id', 'tags')
                   query.where(aerospike.filter.contains('tags', 'green', aerospike.indexType.LIST))
                   const stream = query.foreach(queryPolicy)
+                  console.log(10000000000)
                   stream.on('end', () => {})
                 })
               })
