3a4a4b090434732dad3e7886c74a7d6fdcaeadde
Igor Unanua <igor.unanua@datadoghq.com>
2023-11-20 17:23:35 +0100

    Load appsec rules in appsec/rule_manager.js (#3805)
    * Load appsec rules in rule_manager.js instead of config.js

--- packages/dd-trace/src/config.js
+++ packages/dd-trace/src/config.js
@@ -610,7 +610,7 @@ ken|consumer_?(?:id|key|secret)|sign(?:ed|ature)?|auth(?:entication|orization)?)
     this.tagsHeaderMaxLength = parseInt(DD_TRACE_X_DATADOG_TAGS_MAX_LENGTH)
     this.appsec = {
       enabled: DD_APPSEC_ENABLED,
-      rules: DD_APPSEC_RULES ? safeJsonParse(maybeFile(DD_APPSEC_RULES)) : require('./appsec/recommended.json'),
+      rules: DD_APPSEC_RULES,
       customRulesProvided: !!DD_APPSEC_RULES,
       rateLimit: DD_APPSEC_TRACE_RATE_LIMIT,
       wafTimeout: DD_APPSEC_WAF_TIMEOUT,
