9c95719990b1719d2681af0804d6f86cb8dcdd06
Thomas Hunter II <tlhunter@datadog.com>
2023-11-30 13:51:32 -0800

    configurable tracing header w/ aws signed request
    - fixes #3719
    - see #3796 for prior attempt

--- packages/datadog-plugin-http/test/client.spec.js
+++ packages/datadog-plugin-http/test/client.spec.js
@@ -1052,6 +1052,52 @@ describe('Plugin', () => {
         })
       })
 
+      describe('with config enableTracingWithAmazonSignature enabled', () => {
+        let config
+
+        beforeEach(() => {
+          config = {
+            enableTracingWithAmazonSignature: true
+          }
+
+          return agent.load('http', config)
+            .then(() => {
+              http = require(protocol)
+              express = require('express')
+            })
+        })
+
+        it('should inject tracing header into AWS signed request', done => {
+          const app = express()
+
+          app.get('/', (req, res) => {
+            try {
+              expect(req.get('x-datadog-trace-id')).to.be.a('string')
+              expect(req.get('x-datadog-parent-id')).to.be.a('string')
+
+              res.status(200).send()
+
+              done()
+            } catch (e) {
+              done(e)
+            }
+          })
+
+          getPort().then(port => {
+            appListener = server(app, port, () => {
+              const req = http.request({
+                port,
+                headers: {
+                  Authorization: 'AWS4-HMAC-SHA256 ...'
+                }
+              })
+
+              req.end()
+            })
+          })
+        })
+      })
+
       describe('with validateStatus configuration', () => {
         let config
 
