69ca0cd03c6ea7b730b61f5d97efe5133378a4c0
Thomas Hunter II <tlhunter@datadog.com>
2023-12-01 12:28:15 -0800

    configurable tracing header w/ aws signed request (#3836)
    - adds a new `http` service configuration option `enablePropagationWithAmazonHeaders`
    - defaults to `false` which is current behavior
    - when set to `true` it will allow injecting tracing headers for requests signed via AWS IAM headers
    - allows customer applications which communicate across services with signed requests to be traced
    - making this a configurable since there may be use-cases which the change could break

--- docs/test.ts
+++ docs/test.ts
@@ -292,6 +292,9 @@ tracer.use('http', {
 tracer.use('http', {
   client: httpClientOptions
 });
+tracer.use('http', {
+  enablePropagationWithAmazonHeaders: true
+});
 tracer.use('http2');
 tracer.use('http2', {
   server: http2ServerOptions
