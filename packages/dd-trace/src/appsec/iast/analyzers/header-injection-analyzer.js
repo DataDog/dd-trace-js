7bc7eb4f55877fb85e5dac36990d576ddc220bbe
Ugaitz Urien <ugaitz.urien@datadoghq.com>
2023-12-05 16:29:52 +0100

    Detect header injection code vulnerability (#3813)

---
+++ packages/dd-trace/src/appsec/iast/analyzers/header-injection-analyzer.js
@@ -0,0 +1,96 @@
+'use strict'
+
+const InjectionAnalyzer = require('./injection-analyzer')
+const { HEADER_INJECTION } = require('../vulnerabilities')
+const { getNodeModulesPaths } = require('../path-line')
+const { HEADER_NAME_VALUE_SEPARATOR } = require('../vulnerabilities-formatter/constants')
+const { getRanges } = require('../taint-tracking/operations')
+const {
+  HTTP_REQUEST_COOKIE_NAME,
+  HTTP_REQUEST_COOKIE_VALUE,
+  HTTP_REQUEST_HEADER_VALUE
+} = require('../taint-tracking/source-types')
+
+const EXCLUDED_PATHS = getNodeModulesPaths('express')
+const EXCLUDED_HEADER_NAMES = [
+  'location',
+  'sec-websocket-location',
+  'sec-websocket-accept',
+  'upgrade',
+  'connection'
+]
+
+class HeaderInjectionAnalyzer extends InjectionAnalyzer {
+  constructor () {
+    super(HEADER_INJECTION)
+  }
+
+  onConfigure () {
+    this.addSub('datadog:http:server:response:set-header:finish', ({ name, value }) => {
+      if (Array.isArray(value)) {
+        for (let i = 0; i < value.length; i++) {
+          const headerValue = value[i]
+
+          this.analyze({ name, value: headerValue })
+        }
+      } else {
+        this.analyze({ name, value })
+      }
+    })
+  }
+
+  _isVulnerable ({ name, value }, iastContext) {
+    const lowerCasedHeaderName = name?.trim().toLowerCase()
+
+    if (this.isExcludedHeaderName(lowerCasedHeaderName) || typeof value !== 'string') return
+
+    return super._isVulnerable(value, iastContext) &&
+      !(this.isCookieExclusion(lowerCasedHeaderName, value, iastContext) ||
+        this.isAccessControlAllowOriginExclusion(lowerCasedHeaderName, value, iastContext))
+  }
+
+  _getEvidence (headerInfo, iastContext) {
+    const prefix = headerInfo.name + HEADER_NAME_VALUE_SEPARATOR
+    const prefixLength = prefix.length
+
+    const evidence = super._getEvidence(headerInfo.value, iastContext)
+    evidence.value = prefix + evidence.value
+    evidence.ranges = evidence.ranges.map(range => {
+      return {
+        ...range,
+        start: range.start + prefixLength,
+        end: range.end + prefixLength
+      }
+    })
+
+    return evidence
+  }
+
+  isExcludedHeaderName (name) {
+    return EXCLUDED_HEADER_NAMES.includes(name)
+  }
+
+  isCookieExclusion (name, value, iastContext) {
+    if (name === 'set-cookie') {
+      return getRanges(iastContext, value)
+        .every(range => range.iinfo.type === HTTP_REQUEST_COOKIE_VALUE || range.iinfo.type === HTTP_REQUEST_COOKIE_NAME)
+    }
+
+    return false
+  }
+
+  isAccessControlAllowOriginExclusion (name, value, iastContext) {
+    if (name === 'access-control-allow-origin') {
+      return getRanges(iastContext, value)
+        .every(range => range.iinfo.type === HTTP_REQUEST_HEADER_VALUE)
+    }
+
+    return false
+  }
+
+  _getExcludedPaths () {
+    return EXCLUDED_PATHS
+  }
+}
+
+module.exports = new HeaderInjectionAnalyzer()
