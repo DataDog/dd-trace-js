68f2d61c6cf0d939589f058d04e35285f9b8f2c2
William Conti <58711692+wconti27@users.noreply.github.com>
2023-12-12 14:00:23 -0500

    chore: fix aws-sdk failing test suite (#3860)
    * fix aws-sdk plugin tests

--- packages/datadog-plugin-aws-sdk/test/kinesis.spec.js
+++ packages/datadog-plugin-aws-sdk/test/kinesis.spec.js
@@ -19,7 +19,8 @@ describe('Kinesis', () => {
       return agent.load('aws-sdk')
     })
 
-    before(done => {
+    before(function (done) {
+      this.timeout(0)
       AWS = require(`../../../versions/${kinesisClientName}@${version}`).get()
 
       const params = {
@@ -40,7 +41,7 @@ describe('Kinesis', () => {
       }, (err, res) => {
         if (err) return done(err)
 
-        helpers.waitForActiveStream(kinesis, done)
+        helpers.waitForActiveStream(this, kinesis, done)
       })
     })
 
