afcefb127876c9f32f8c96263c25db3fbc3c2f63
William Conti <william.conti@datadoghq.com>
2023-12-12 12:23:14 -0500

    fix sns tests

--- packages/datadog-plugin-aws-sdk/test/sns.spec.js
+++ packages/datadog-plugin-aws-sdk/test/sns.spec.js
@@ -8,21 +8,22 @@ const { rawExpectedSchema } = require('./sns-naming')
 const { ENTRY_PARENT_HASH } = require('../../dd-trace/src/datastreams/processor')
 const { computePathwayHash } = require('../../dd-trace/src/datastreams/pathway')
 const DataStreamsContext = require('../../dd-trace/src/data_streams_context')
+const { producer } = require('../../dd-trace/src/service-naming/schemas/v0/messaging')
 
 const expectedProducerHash = computePathwayHash(
   'test',
   'tester',
-  ['direction:out', 'topic:TestTopic', 'type:sns'],
+  ['direction:out', 'topic:TestTopicDSM', 'type:sns'],
   ENTRY_PARENT_HASH
 )
 const expectedConsumerHash = computePathwayHash(
   'test',
   'tester',
-  ['direction:in', 'topic:TestQueue', 'type:sqs'],
+  ['direction:in', 'topic:TestQueueDSM', 'type:sqs'],
   expectedProducerHash
 )
 
-describe('Sns', () => {
+describe('Sns', function() {
   setup()
 
   withVersions('aws-sdk', ['aws-sdk', '@aws-sdk/smithy-client'], (version, moduleName) => {
@@ -55,31 +56,19 @@ describe('Sns', () => {
       }).then(done, done)
     }
 
-    beforeEach(() => {
-      tracer = require('../../dd-trace')
-      tracer.use('aws-sdk')
-    })
-
-    before(() => {
-      parentId = '0'
-      spanId = '0'
-
-      return agent.load('aws-sdk')
-    })
-
-    before(done => {
+    function createResources (queueName, topicName, cb) {
       const { SNS } = require(`../../../versions/${snsClientName}@${version}`).get()
       const { SQS } = require(`../../../versions/${sqsClientName}@${version}`).get()
 
       sns = new SNS({ endpoint: 'http://127.0.0.1:4566', region: 'us-east-1' })
       sqs = new SQS({ endpoint: 'http://127.0.0.1:4566', region: 'us-east-1' })
 
-      sns.createTopic({ Name: 'TestTopic' }, (err, data) => {
+      sns.createTopic({ Name: topicName }, (err, data) => {
         if (err) return done(err)
 
         TopicArn = data.TopicArn
 
-        sqs.createQueue({ QueueName: 'TestQueue' }, (err, data) => {
+        sqs.createQueue({ QueueName: queueName }, (err, data) => {
           if (err) return done(err)
 
           QueueUrl = data.QueueUrl
@@ -101,148 +90,188 @@ describe('Sns', () => {
               WaitTimeSeconds: 1
             }
 
-            done()
+            cb()
           })
         })
       })
-    })
+    }
 
-    after(done => {
-      sns.deleteTopic({ TopicArn }, done)
-    })
+    describe('no configuration', () => {
 
-    after(done => {
-      sqs.deleteQueue({ QueueUrl }, done)
-    })
+      before(() => {
+        parentId = '0'
+        spanId = '0'
 
-    after(() => {
-      return agent.close({ ritmReset: false })
-    })
+        return agent.load('aws-sdk', { sns: { dsmEnabled: false }}, { dsmEnabled: true })
+      })
 
-    withPeerService(
-      () => tracer,
-      'aws-sdk',
-      (done) => sns.publish({
-        TopicArn,
-        Message: 'message 1'
-      }, (err) => err && done()),
-      'TestTopic', 'topicname')
+      before(done => {
+        process.env.DD_DATA_STREAMS_ENABLED = 'true'
+        tracer = require('../../dd-trace')
+        tracer.use('aws-sdk', { sns: { dsmEnabled: false }})
 
-    withNamingSchema(
-      (done) => sns.publish({
-        TopicArn,
-        Message: 'message 1'
-      }, (err) => err && done()),
-      rawExpectedSchema.producer,
-      {
-        desc: 'producer'
+        createResources('TestQueue', 'TestTopic', done)
+      })
+
+      after(done => {
+        sns.deleteTopic({ TopicArn }, done)
+      })
+
+      after(done => {
+        sqs.deleteQueue({ QueueUrl }, done)
+      })
+
+      after(() => {
+        return agent.close({ ritmReset: false })
+      })
+
+      withPeerService(
+        () => tracer,
+        'aws-sdk',
+        (done) => sns.publish({
+          TopicArn,
+          Message: 'message 1'
+        }, (err) => err && done()),
+        'TestTopic', 'topicname')
+  
+      withNamingSchema(
+        (done) => sns.publish({
+          TopicArn,
+          Message: 'message 1'
+        }, (err) => err && done()),
+        rawExpectedSchema.producer,
+        {
+          desc: 'producer'
+        }
+      )
+  
+      withNamingSchema(
+        (done) => sns.getTopicAttributes({
+          TopicArn
+        }, (err) => err && done(err)),
+        rawExpectedSchema.client,
+        {
+          desc: 'client'
+        }
+      )
+  
+      it('injects trace context to SNS publish', done => {
+        assertPropagation(done)
+  
+        sns.subscribe(subParams, (err, data) => {
+          if (err) return done(err)
+  
+          sqs.receiveMessage(receiveParams, e => e && done(e))
+          sns.publish({ TopicArn, Message: 'message 1' }, (e) => {
+            if (e) done(e)
+          })
+        })
+      })
+  
+      // There is a bug in 3.x (but not 3.0.0) that will be fixed in 3.261
+      // https://github.com/aws/aws-sdk-js-v3/issues/2861
+      if (!semver.intersects(version, '<3 || >3.0.0')) {
+        it('injects trace context to SNS publishBatch', done => {
+          assertPropagation(done)
+  
+          sns.subscribe(subParams, (err, data) => {
+            if (err) return done(err)
+  
+            sqs.receiveMessage(receiveParams, e => e && done(e))
+            sns.publishBatch({
+              TopicArn,
+              PublishBatchRequestEntries: [
+                { Id: '1', Message: 'message 1' },
+                { Id: '2', Message: 'message 2' }
+              ]
+            }, e => e && done(e))
+          })
+        })
       }
-    )
-
-    withNamingSchema(
-      (done) => sns.getTopicAttributes({
-        TopicArn
-      }, (err) => err && done(err)),
-      rawExpectedSchema.client,
-      {
-        desc: 'client'
+  
+      // TODO: Figure out why this fails only in 3.0.0
+      if (version !== '3.0.0') {
+        it('skips injecting trace context to SNS if message attributes are full', done => {
+          sns.subscribe(subParams, (err, data) => {
+            if (err) return done(err)
+  
+            sqs.receiveMessage(receiveParams, (err, data) => {
+              if (err) return done(err)
+  
+              try {
+                expect(data.Messages[0].Body).to.not.include('datadog')
+                done()
+              } catch (e) {
+                done(e)
+              }
+            })
+  
+            sns.publish({
+              TopicArn,
+              Message: 'message 1',
+              MessageAttributes: {
+                keyOne: { DataType: 'String', StringValue: 'keyOne' },
+                keyTwo: { DataType: 'String', StringValue: 'keyTwo' },
+                keyThree: { DataType: 'String', StringValue: 'keyThree' },
+                keyFour: { DataType: 'String', StringValue: 'keyFour' },
+                keyFive: { DataType: 'String', StringValue: 'keyFive' },
+                keySix: { DataType: 'String', StringValue: 'keySix' },
+                keySeven: { DataType: 'String', StringValue: 'keySeven' },
+                keyEight: { DataType: 'String', StringValue: 'keyEight' },
+                keyNine: { DataType: 'String', StringValue: 'keyNine' },
+                keyTen: { DataType: 'String', StringValue: 'keyTen' }
+              }
+            }, e => e && done(e))
+          })
+        })
       }
-    )
-
-    it('injects trace context to SNS publish', done => {
-      assertPropagation(done)
-
-      sns.subscribe(subParams, (err, data) => {
-        if (err) return done(err)
-
-        sqs.receiveMessage(receiveParams, e => e && done(e))
+  
+      it('generates tags for proper publish calls', done => {
+        agent.use(traces => {
+          const span = traces[0][0]
+  
+          expect(span.resource).to.equal(`publish ${TopicArn}`)
+          expect(span.meta).to.include({
+            'aws.sns.topic_arn': TopicArn,
+            'topicname': 'TestTopic',
+            'aws_service': 'SNS',
+            'region': 'us-east-1'
+          })
+        }).then(done, done)
+  
         sns.publish({ TopicArn, Message: 'message 1' }, e => e && done(e))
       })
     })
 
-    // There is a bug in 3.x (but not 3.0.0) that will be fixed in 3.261
-    // https://github.com/aws/aws-sdk-js-v3/issues/2861
-    if (!semver.intersects(version, '<3 || >3.0.0')) {
-      it('injects trace context to SNS publishBatch', done => {
-        assertPropagation(done)
-
-        sns.subscribe(subParams, (err, data) => {
-          if (err) return done(err)
-
-          sqs.receiveMessage(receiveParams, e => e && done(e))
-          sns.publishBatch({
-            TopicArn,
-            PublishBatchRequestEntries: [
-              { Id: '1', Message: 'message 1' },
-              { Id: '2', Message: 'message 2' }
-            ]
-          }, e => e && done(e))
-        })
-      })
-    }
-
-    // TODO: Figure out why this fails only in 3.0.0
-    if (version !== '3.0.0') {
-      it('skips injecting trace context to SNS if message attributes are full', done => {
-        sns.subscribe(subParams, (err, data) => {
-          if (err) return done(err)
-
-          sqs.receiveMessage(receiveParams, (err, data) => {
-            if (err) return done(err)
-
-            try {
-              expect(data.Messages[0].Body).to.not.include('datadog')
-              done()
-            } catch (e) {
-              done(e)
-            }
-          })
-
-          sns.publish({
-            TopicArn,
-            Message: 'message 1',
-            MessageAttributes: {
-              keyOne: { DataType: 'String', StringValue: 'keyOne' },
-              keyTwo: { DataType: 'String', StringValue: 'keyTwo' },
-              keyThree: { DataType: 'String', StringValue: 'keyThree' },
-              keyFour: { DataType: 'String', StringValue: 'keyFour' },
-              keyFive: { DataType: 'String', StringValue: 'keyFive' },
-              keySix: { DataType: 'String', StringValue: 'keySix' },
-              keySeven: { DataType: 'String', StringValue: 'keySeven' },
-              keyEight: { DataType: 'String', StringValue: 'keyEight' },
-              keyNine: { DataType: 'String', StringValue: 'keyNine' },
-              keyTen: { DataType: 'String', StringValue: 'keyTen' }
-            }
-          }, e => e && done(e))
-        })
-      })
-    }
-
-    it('generates tags for proper publish calls', done => {
-      agent.use(traces => {
-        const span = traces[0][0]
-
-        expect(span.resource).to.equal(`publish ${TopicArn}`)
-        expect(span.meta).to.include({
-          'aws.sns.topic_arn': TopicArn,
-          'topicname': 'TestTopic',
-          'aws_service': 'SNS',
-          'region': 'us-east-1'
-        })
-      }).then(done, done)
-
-      sns.publish({ TopicArn, Message: 'message 1' }, e => e && done(e))
-    })
-
     describe('Data Streams Monitoring', () => {
-      beforeEach(() => {
-        tracer._initialized = false
-        tracer.init({ dsmEnabled: true })
-        tracer.use('aws-sdk', { sns: { dsmEnabled: true }, sqs: { dsmEnabled: true } })
-        return agent.load('aws-sdk', { sns: { dsmEnabled: true }, sqs: { dsmEnabled: true } })
+      before(() => {
+        return agent.load('aws-sdk', { sns: { dsmEnabled: true }, sqs: { dsmEnabled: true } }, { dsmEnabled: true })
+      })
+
+      before(done => {
+        process.env.DD_DATA_STREAMS_ENABLED = 'true'
+        tracer = require('../../dd-trace')
+        tracer.use('aws-sdk', { sns: { dsmEnabled: true }, sqs: { dsmEnabled: true }})
+
+        createResources('TestQueueDSM', 'TestTopicDSM', done)
+      })
+
+      after(done => {
+        sns.deleteTopic({ TopicArn }, done)
+      })
+
+      after(done => {
+        sqs.deleteQueue({ QueueUrl }, done)
+      })
+
+      after(() => {
+        return agent.close({ ritmReset: false })
       })
 
       it('injects DSM trace context to SNS publish', done => {
+        let producerHashCreated = false
+        let consumerHashCreated = false
+        
         if (DataStreamsContext.setDataStreamsContext.isSinonProxy) {
           DataStreamsContext.setDataStreamsContext.restore()
         }
@@ -256,14 +285,22 @@ describe('Sns', () => {
             (err, res) => {
               if (err) return done(err)
 
-              expect(
-                setDataStreamsContextSpy.args[setDataStreamsContextSpy.args.length - 1][0].hash
-              ).to.equal(expectedConsumerHash)
+              setDataStreamsContextSpy.args.forEach(functionCall => {
+                if (functionCall[0].hash === expectedConsumerHash) {
+                  consumerHashCreated = true
+                } else if (functionCall[0].hash === expectedProducerHash) {
+                  producerHashCreated = true
+                }
+              })
+              
+
+              expect(consumerHashCreated).to.equal(true)
+              expect(producerHashCreated).to.equal(true)
               setDataStreamsContextSpy.restore()
               done()
             })
           sns.publish(
-            { TopicArn, Message: 'message 1' },
+            { TopicArn, Message: 'message DSM' },
             (err) => {
               if (err) return done(err)
             })
