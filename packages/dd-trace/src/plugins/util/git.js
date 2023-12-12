58472f850ba5c93741d0dae1274d44f529656cfe
Juan Antonio Fernández de Alba <juan.fernandezdealba@datadoghq.com>
2023-12-12 10:29:24 +0100

    [ci-visibility] Speed up git unshallow (#3839)

--- packages/dd-trace/src/plugins/util/git.js
+++ packages/dd-trace/src/plugins/util/git.js
@@ -111,7 +111,7 @@ function getLatestCommits () {
   }
 }
 
-function getCommitsToUpload (commitsToExclude, commitsToInclude) {
+function getCommitsRevList (commitsToExclude, commitsToInclude) {
   const commitsToExcludeString = commitsToExclude.map(commit => `^${commit}`)
 
   try {
@@ -236,7 +236,7 @@ module.exports = {
   getLatestCommits,
   getRepositoryUrl,
   generatePackFilesForCommits,
-  getCommitsToUpload,
+  getCommitsRevList,
   GIT_REV_LIST_MAX_BUFFER,
   isShallowRepository,
   unshallowRepository
