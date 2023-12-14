f88b0c17081fc2b0a8e29379bb5b6a277233eeef
Thomas Hunter II <tlhunter@datadog.com>
2023-12-14 09:25:18 -0800

    only skip pino error tests for node 21
    - specifically looking for exactly node 21
    - this way we will have to fix it when node 22 LTS is released

--- packages/datadog-plugin-pino/test/index.spec.js
+++ packages/datadog-plugin-pino/test/index.spec.js
@@ -3,6 +3,7 @@
 const Writable = require('stream').Writable
 const agent = require('../../dd-trace/test/plugins/agent')
 const semver = require('semver')
+const { NODE_MAJOR } = require('../../../version')
 
 describe('Plugin', () => {
   let logger
@@ -132,8 +133,10 @@ describe('Plugin', () => {
               } else { // pino <7
                 expect(record).to.have.property('msg', error.message)
                 // ** TODO ** add this back once we fix it
-                // expect(record).to.have.property('type', 'Error')
-                // expect(record).to.have.property('stack', error.stack)
+                if (NODE_MAJOR !== 21) {
+                  expect(record).to.have.property('type', 'Error')
+                  expect(record).to.have.property('stack', error.stack)
+                }
               }
             })
           })
