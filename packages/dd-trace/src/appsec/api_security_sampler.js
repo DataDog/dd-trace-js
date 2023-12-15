c267f86594dab029391017a1c84a3d9c78a59f87
Igor Unanua <igor.unanua@datadoghq.com>
2023-12-15 13:21:43 +0100

    Apply same coertion logic as in config.js

--- packages/dd-trace/src/appsec/api_security_sampler.js
+++ packages/dd-trace/src/appsec/api_security_sampler.js
@@ -19,16 +19,18 @@ function setRequestSampling (sampling) {
 }
 
 function parseRequestSampling (requestSampling) {
-  const parsed = parseFloat(requestSampling)
+  let parsed = parseFloat(requestSampling)
 
-  if (!isNaN(parsed) && parsed >= 0 && parsed <= 1) {
-    return parsed
-  } else {
+  if (isNaN(parsed)) {
     log.warn(`Incorrect API Security request sampling value: ${requestSampling}`)
 
     // NOTE: 0 or 0.1 the default value?
-    return 0
+    parsed = 0
+  } else {
+    parsed = Math.min(1, Math.max(0, parsed))
   }
+
+  return parsed
 }
 
 function sampleRequest () {
