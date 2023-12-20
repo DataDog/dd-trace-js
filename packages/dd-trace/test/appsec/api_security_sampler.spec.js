c8e88ee96e330bd397e75184fe158d2b08bc3af7
Igor Unanua <igor.unanua@datadoghq.com>
2023-12-20 12:22:25 +0100

    API security sample rate via RC (#3868)
    * New RC ASM_API_SECURITY_SAMPLE_RATE capability
    
    * Do not report ASM_API_SECURITY_SAMPLE_RATE capability if apiSecurity is not enabled
    
    * Change Activation constants case
    
    * Update ASM_API_SECURITY_SAMPLE_RATE capability when enabling RemoteConfig
    
    * Apply same coertion logic as in config.js
    
    * Clean up
    
    * Do not turn off ASM_API_SECURITY_SAMPLE_RATE when disabling waf update
    
    * Include api_security_sampler test case
    
    * Fix test comment
    
    * remove NOTE

---
+++ packages/dd-trace/test/appsec/api_security_sampler.spec.js
@@ -0,0 +1,71 @@
+'use strict'
+
+const apiSecuritySampler = require('../../src/appsec/api_security_sampler')
+
+describe('Api Security Sampler', () => {
+  let config
+
+  beforeEach(() => {
+    config = {
+      apiSecurity: {
+        enabled: true,
+        requestSampling: 1
+      }
+    }
+
+    sinon.stub(Math, 'random').returns(0.3)
+  })
+
+  afterEach(sinon.restore)
+
+  describe('sampleRequest', () => {
+    it('should sample request if enabled and sampling 1', () => {
+      apiSecuritySampler.configure(config)
+
+      expect(apiSecuritySampler.sampleRequest()).to.true
+    })
+
+    it('should not sample request if enabled and sampling 0', () => {
+      config.apiSecurity.requestSampling = 0
+      apiSecuritySampler.configure(config)
+
+      expect(apiSecuritySampler.sampleRequest()).to.false
+    })
+
+    it('should sample request if enabled and sampling greater than random', () => {
+      config.apiSecurity.requestSampling = 0.5
+
+      apiSecuritySampler.configure(config)
+
+      expect(apiSecuritySampler.sampleRequest()).to.true
+    })
+
+    it('should not sample request if enabled and sampling less than random', () => {
+      config.apiSecurity.requestSampling = 0.1
+
+      apiSecuritySampler.configure(config)
+
+      expect(apiSecuritySampler.sampleRequest()).to.false
+    })
+
+    it('should not sample request if incorrect config value', () => {
+      config.apiSecurity.requestSampling = NaN
+
+      apiSecuritySampler.configure(config)
+
+      expect(apiSecuritySampler.sampleRequest()).to.false
+    })
+
+    it('should sample request according to the config', () => {
+      config.apiSecurity.requestSampling = 1
+
+      apiSecuritySampler.configure(config)
+
+      expect(apiSecuritySampler.sampleRequest()).to.true
+
+      apiSecuritySampler.setRequestSampling(0)
+
+      expect(apiSecuritySampler.sampleRequest()).to.false
+    })
+  })
+})
