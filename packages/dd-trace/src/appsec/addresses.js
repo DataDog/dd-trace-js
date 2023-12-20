148d2dc96d58afee065a02989464c1cfca2d673b
Julio Gonzalez <107922352+hoolioh@users.noreply.github.com>
2023-12-20 10:43:32 +0100

    GraphQL Blocking (#3819)
    * Upload module skeleton.
    
    * Blocking in apollo, very very first version
    
    * Move graphql implementation to another module.
    
    * Blocking for apollo-server-core, ugly but it works, lets find a better way
    
    * Use real blocking data
    
    * Set blocking to true.
    
    * Throw before resolver execution in order to stop the operation's execution flow.
    
    * Use HttpQueryError in apollo-server-core
    
    * Blocking test in apollo-server-fastify
    
    * Refactor graphql blocking.
    
    * Remove previous implementation which only supported monitoring.
    * Add new waf address in order to check the payload of every resolver.
    * Use apm start resolver address instead of a new one.
    * Remove mock and perform an actual call to the waf.
    
    * Add non blocking graphql test
    
    * Move abortController constructor to context creation.
    
    This reduces the performance overhead due to just one instance is shared across the whole query exectution.
    
    * Add pollo-server-express block tests
    
    * Add unit tests.
    
    * Add @apollo/server tests
    
    * Update test rules for blocking by `graphql.server.resolver`
    
    * Block with graphql templates data
    
    * Add tests.
    
    * Block with graphql data in graphql endpoint
    
    * Fix tests.
    
    * Execute @apollo/server and apollo-server-express tests
    
    * Unify code in @apollo/server and apollo-server-core
    
    * update comments
    
    * Add appsec.blocked tag in blocked requests
    
    * Add test with non graphql block response
    
    * Tests for block with redirect
    
    * Prevent creation of resolve span when it is blocked before the execution of the resolve code
    
    * Refactor addResolver in order to get directives information.
    
    * Add tests to block on directives.
    
    * Add test for directives.
    
    * Undo prevent creating resolve span
    
    * Configurable graphql blocking json
    
    * Refactor graphql
    
    * Using resolver instead of resolvers.
    * Change graphql channel name to be consistent with the others.
    
    * Small changes in blocking
    
    * Move resover information resolution to plugin.
    
    * Revert "Move resover information resolution to plugin."
    
    This reverts commit 7cc8561bc567a385088959bcf65deaccdb5f0ec0.
    
    * Remove resolver information from context, pass it in a different field instead.
    
    * Throw custom exception rather than send an empty array.
    
    * Update packages/datadog-instrumentations/src/graphql.js
    
    Co-authored-by: Ugaitz Urien <ugaitz.urien@datadoghq.com>
    
    * Change a bit apollo-server-core instrumentation
    
    * Protect Header map, if in future version it is moved/removed, prevent breaks
    
    * Remove some duplicated code
    
    * Update packages/datadog-instrumentations/src/apollo-server.js
    
    Co-authored-by: Carles Capell <107924659+CarlesDD@users.noreply.github.com>
    
    * Fix comments in the PR
    
    * Fix PR comments.
    
    * Fix some comments in the PR
    
    * Move resolver information formatting to the plugin.
    
    * Fix PR comments.
    
    * Fix proper use of Promise.race.
    
    ---------
    
    Co-authored-by: Ugaitz Urien <ugaitz.urien@datadoghq.com>
    Co-authored-by: Carles Capell <107924659+CarlesDD@users.noreply.github.com>

--- packages/dd-trace/src/appsec/addresses.js
+++ packages/dd-trace/src/appsec/addresses.js
@@ -13,6 +13,7 @@ module.exports = {
   HTTP_INCOMING_RESPONSE_HEADERS: 'server.response.headers.no_cookies',
   // TODO: 'server.response.trailers',
   HTTP_INCOMING_GRAPHQL_RESOLVERS: 'graphql.server.all_resolvers',
+  HTTP_INCOMING_GRAPHQL_RESOLVER: 'graphql.server.resolver',
 
   HTTP_CLIENT_IP: 'http.client_ip',
 
