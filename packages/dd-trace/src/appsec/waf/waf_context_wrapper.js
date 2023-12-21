fbb0389de9d8aa6bbaed87370196bead325319bc
Julio Gonzalez <julio.gonzalez@datadoghq.com>
2023-12-21 11:01:40 +0100

    Add test.

--- packages/dd-trace/src/appsec/waf/waf_context_wrapper.js
+++ packages/dd-trace/src/appsec/waf/waf_context_wrapper.js
@@ -25,7 +25,7 @@ class WAFContextWrapper {
     const newAddressesToSkip = new Set(this.addressesToSkip)
 
     if (persistent && typeof persistent === 'object') {
-      // TODO: possible optimizaion: only send params that haven't already been sent with same value to this wafContext
+      // TODO: possible optimization: only send params that haven't already been sent with same value to this wafContext
       for (const key of Object.keys(persistent)) {
         // TODO: requiredAddresses is no longer used due to processor addresses are not included in the list. Check on
         // future versions when the actual addresses are included in the 'loaded' section inside diagnostics.
--- packages/dd-trace/test/appsec/waf/waf_context_wrapper.spec.js
+++ packages/dd-trace/test/appsec/waf/waf_context_wrapper.spec.js
@@ -21,4 +21,33 @@ describe('WAFContextWrapper', () => {
 
     expect(ddwafContext.run).to.have.been.calledOnceWithExactly(payload, 1000)
   })
+
+  it('Should send ephemeral addreses every time', () => {
+    const ddwafContext = {
+      run: sinon.stub()
+    }
+    const wafContextWrapper = new WAFContextWrapper(ddwafContext, 1000, '1.14.0', '1.8.0')
+
+    const payload = {
+      persistent: {
+        [addresses.HTTP_INCOMING_QUERY]: { key: 'value' }
+      },
+      ephemeral: {
+        [addresses.HTTP_INCOMING_GRAPHQL_RESOLVER]: { anotherKey: 'anotherValue' }
+      }
+    }
+
+    wafContextWrapper.run(payload)
+    wafContextWrapper.run(payload)
+
+    expect(ddwafContext.run).to.have.been.calledTwice
+    expect(ddwafContext.run.firstCall).to.have.been.calledWithExactly(payload, 1000)
+    expect(ddwafContext.run.secondCall).to.have.been.calledWithExactly({
+      ephemeral: {
+        [addresses.HTTP_INCOMING_GRAPHQL_RESOLVER]: {
+          anotherKey: 'anotherValue'
+        }
+      }
+    }, 1000)
+  })
 })
