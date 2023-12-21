e5cad775fb549ed9ea0462e4cacad0f9f5b997fc
Julio Gonzalez <107922352+hoolioh@users.noreply.github.com>
2023-12-21 11:12:57 +0100

    Pass resolver address as ephemeral type (#3897)
    * Update waf bindings to 6.0.0.
    
    * Pass graphql.server.resolver as ephemeral address type.
    
    * Add test.

--- packages/dd-trace/test/appsec/waf/waf_context_wrapper.spec.js
+++ packages/dd-trace/test/appsec/waf/waf_context_wrapper.spec.js
@@ -11,12 +11,43 @@ describe('WAFContextWrapper', () => {
     const wafContextWrapper = new WAFContextWrapper(ddwafContext, 1000, '1.14.0', '1.8.0')
 
     const payload = {
-      [addresses.HTTP_INCOMING_QUERY]: { key: 'value' }
+      persistent: {
+        [addresses.HTTP_INCOMING_QUERY]: { key: 'value' }
+      }
     }
 
     wafContextWrapper.run(payload)
     wafContextWrapper.run(payload)
 
     expect(ddwafContext.run).to.have.been.calledOnceWithExactly(payload, 1000)
+  })
+
+  it('Should send ephemeral addreses every time', () => {
+    const ddwafContext = {
+      run: sinon.stub()
+    }
+    const wafContextWrapper = new WAFContextWrapper(ddwafContext, 1000, '1.14.0', '1.8.0')
+
+    const payload = {
+      persistent: {
+        [addresses.HTTP_INCOMING_QUERY]: { key: 'value' }
+      },
+      ephemeral: {
+        [addresses.HTTP_INCOMING_GRAPHQL_RESOLVER]: { anotherKey: 'anotherValue' }
+      }
+    }
+
+    wafContextWrapper.run(payload)
+    wafContextWrapper.run(payload)
+
+    expect(ddwafContext.run).to.have.been.calledTwice
+    expect(ddwafContext.run.firstCall).to.have.been.calledWithExactly(payload, 1000)
+    expect(ddwafContext.run.secondCall).to.have.been.calledWithExactly({
+      ephemeral: {
+        [addresses.HTTP_INCOMING_GRAPHQL_RESOLVER]: {
+          anotherKey: 'anotherValue'
+        }
+      }
+    }, 1000)
   })
 })
