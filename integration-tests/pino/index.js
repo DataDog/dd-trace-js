3ce04b0e1c6685d031ea4b8158ef1db22bc80de8
Bowen Brooks <39347269+bojbrook@users.noreply.github.com>
2024-01-02 07:06:38 -0500

    Adding Pino Integration-test  (#2002)

---
+++ integration-tests/pino/index.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const options = {}
+
+if (process.env.AGENT_PORT) {
+  options.port = process.env.AGENT_PORT
+}
+
+if (process.env.lOG_INJECTION) {
+  options.logInjection = process.env.lOG_INJECTION
+}
+
+const tracer = require('dd-trace').init(options)
+
+const http = require('http')
+const logger = require('pino')()
+
+const server = http
+  .createServer((req, res) => {
+    const span = tracer.scope().active()
+    const contextTraceId = span.context().toTraceId()
+    const contextSpanId = span.context().toSpanId()
+    logger.info(
+      { custom: { trace_id: contextTraceId, span_id: contextSpanId } },
+      'Creating server'
+    )
+    res.end('hello, world\n')
+  })
+  .listen(0, () => {
+    const port = server.address().port
+    process.send({ port })
+  })
