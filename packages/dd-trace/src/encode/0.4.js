bb26c7a4d3b907d468a97a3a1a9702712ad07de1
Sam Brenner <sam.brenner@datadoghq.com>
2024-01-03 15:16:44 -0500

    check that encoding is the issue

--- packages/dd-trace/src/encode/0.4.js
+++ packages/dd-trace/src/encode/0.4.js
@@ -114,8 +114,8 @@ class AgentEncoder {
       this._encodeMap(bytes, span.meta)
       this._encodeString(bytes, 'metrics')
       this._encodeMap(bytes, span.metrics)
-      this._encodeString(bytes, 'span_links')
-      this._encodeString(bytes, span.links)
+      // this._encodeString(bytes, 'span_links')
+      // this._encodeString(bytes, span.links)
     }
   }
 
