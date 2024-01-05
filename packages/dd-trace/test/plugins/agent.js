4a5fe9442a189caba74b84bd05e1555ce52b1ce3
Sam Brenner <sam.brenner@datadoghq.com>
2024-01-05 16:21:14 -0500

    remove console log

--- packages/dd-trace/test/plugins/agent.js
+++ packages/dd-trace/test/plugins/agent.js
@@ -118,8 +118,6 @@ function handleTraceRequest (req, res, sendToTestAgent) {
           body += chunk
         })
         testAgentRes.on('end', () => {
-          // eslint-disable-next-line no-console
-          console.log('body is', body)
           res.status(400).send(body)
         })
       }
