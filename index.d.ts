6396361bbad40b2964ebd0fadacf7fba8ec3238c
Ben Asher <ben@ashbyhq.com>
2024-01-11 11:39:34 -0800

    fix comment

--- index.d.ts
+++ index.d.ts
@@ -1385,7 +1385,7 @@ declare namespace tracer {
      */
     interface ioredis extends Instrumentation {
       /**
-       * List of commands that should be instrumented.
+       * List of commands that should be instrumented. Commands must be in
        * lowercase for example 'xread'.
        *
        * @default /^.*$/
