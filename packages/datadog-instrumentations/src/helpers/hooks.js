65568c6cc5c8017b2cb55551c70fd3d1b8c605bc
Igor Unanua <igor.unanua@datadoghq.com>
2024-01-25 09:23:57 +0100

    Fix mquery vulnerability location [APPSEC-12226] (#3797)
    * Fix mquery vulnerability location
    
    * Instrument mquery
    
    * Analyze findOneAndUpdate second arg
    
    * Use TracingChannel
    
    * Remove 'tracing:' prefix

--- packages/datadog-instrumentations/src/helpers/hooks.js
+++ packages/datadog-instrumentations/src/helpers/hooks.js
@@ -73,6 +73,7 @@ module.exports = {
   'mongodb': () => require('../mongodb'),
   'mongodb-core': () => require('../mongodb-core'),
   'mongoose': () => require('../mongoose'),
+  'mquery': () => require('../mquery'),
   'mysql': () => require('../mysql'),
   'mysql2': () => require('../mysql2'),
   'net': () => require('../net'),
