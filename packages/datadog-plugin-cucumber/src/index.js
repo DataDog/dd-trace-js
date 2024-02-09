3dab48783270e465e94d4635628381fa7c916936
Juan Fernandez <juan.fernandezdealba@datadoghq.com>
2024-02-09 11:18:29 +0100

    logs to cucumber

--- packages/datadog-plugin-cucumber/src/index.js
+++ packages/datadog-plugin-cucumber/src/index.js
@@ -141,6 +141,8 @@ class CucumberPlugin extends CiPlugin {
       const store = storage.getStore()
       const testSuite = getTestSuitePath(testFileAbsolutePath, this.sourceRoot)
       const testSourceFile = getTestSuitePath(testFileAbsolutePath, this.repositoryRoot)
+      console.log('testFileAbsolutePath', testFileAbsolutePath)
+      console.log('this.repositoryRoot', this.repositoryRoot)
       const testSpan = this.startTestSpan(testName, testSuite, testSourceFile, testSourceLine)
 
       this.enter(testSpan, store)
