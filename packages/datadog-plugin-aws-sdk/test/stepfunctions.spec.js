8b5a06b3e64920e595bd381216fab48ace5dc230
chris.agocs <chris.agocs@datadoghq.com>
2024-02-15 15:12:45 -0800

    first pass + basic test

---
+++ packages/datadog-plugin-aws-sdk/test/stepfunctions.spec.js
@@ -0,0 +1,59 @@
+/* eslint-disable max-len */
+'use strict'
+
+const StepFunctions = require('../src/services/stepfunctions')
+const tracer = require('../../dd-trace')
+
+
+describe('StepFunctions', () => {
+  withVersions('aws-sdk', ['aws-sdk', '@aws-sdk/smithy-client'], (version, moduleName) => {
+    let traceId
+    let parentId
+    let spanId
+    before(() => {
+      tracer.init()
+      span = {
+        finish: sinon.spy(() => {}),
+        context: () => {
+          return {
+            _sampling: {
+              priority: 1
+            },
+            _trace: {
+              started: [],
+              origin: ''
+            },
+            _traceFlags: {
+              sampled: 1
+            },
+            'x-datadog-trace-id': traceId,
+            'x-datadog-parent-id': parentId,
+            'x-datadog-sampling-priority': '1',
+            toTraceId: () => {
+              return traceId
+            },
+            toSpanId: () => {
+              return spanId
+            }
+          }
+        },
+        addTags: sinon.stub(),
+        setTag: sinon.stub()
+      }
+      tracer._tracer.startSpan = sinon.spy(() => {
+        return span
+      })
+    })
+
+    it('generates tags for an event', () => {
+      const eventbridge = new StepFunctions(tracer)
+      const params = {
+        statemachinearn: 'arn:aws:states:us-east-1:425362996713:stateMachine:agocs-test-noop-state-machine-2'
+      }
+      expect(eventbridge.generateTags(params, 'start_execution', {})).to.deep.equal({
+        'resource.name': 'start_execution',
+        'statemachinearn': 'arn:aws:states:us-east-1:425362996713:stateMachine:agocs-test-noop-state-machine-2'
+      })
+    })
+
+})
