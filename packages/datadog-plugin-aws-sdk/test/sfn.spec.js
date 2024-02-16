1e0e415643c829631b62d26e0167ba073108610a
chris.agocs <chris.agocs@datadoghq.com>
2024-02-16 14:16:52 -0800

    Fix capitalization on statemachinearn, add a test

--- packages/datadog-plugin-aws-sdk/test/sfn.spec.js
+++ packages/datadog-plugin-aws-sdk/test/sfn.spec.js
@@ -45,13 +45,25 @@ describe('Sfn', () => {
       })
     })
 
-    it('generates tags for an event', () => {
+    it('generates tags for a start_execution', () => {
       const sfn = new Sfn(tracer)
       const params = {
         statemachinearn: 'arn:aws:states:us-east-1:425362996713:stateMachine:agocs-test-noop-state-machine-2'
       }
       expect(sfn.generateTags(params, 'start_execution', {})).to.deep.equal({
         'resource.name': 'start_execution',
+        'statemachinearn': 'arn:aws:states:us-east-1:425362996713:stateMachine:agocs-test-noop-state-machine-2'
+      })
+    })
+
+    it('generates tags for a start_execution with a name', () => {
+      const sfn = new Sfn(tracer)
+      const params = {
+        stateMachineArn: 'arn:aws:states:us-east-1:425362996713:stateMachine:agocs-test-noop-state-machine-2',
+        name: 'my-execution'
+      }
+      expect(sfn.generateTags(params, 'start_execution', {})).to.deep.equal({
+        'resource.name': 'start_execution my-execution',
         'statemachinearn': 'arn:aws:states:us-east-1:425362996713:stateMachine:agocs-test-noop-state-machine-2'
       })
     })
