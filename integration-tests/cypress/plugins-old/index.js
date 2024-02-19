a958c6af0146ec65a56c462e437984fb5aca1cb9
Juan Antonio Fernández de Alba <juan.fernandezdealba@datadoghq.com>
2024-02-19 11:36:44 +0100

    [ci-visibility] Cypress: do not lose all the trace if `testSuiteSpan` can't be created (#4072)

--- integration-tests/cypress/plugins-old/index.js
+++ integration-tests/cypress/plugins-old/index.js
@@ -1,3 +1,6 @@
 module.exports = (on, config) => {
+  if (process.env.CYPRESS_ENABLE_INCOMPATIBLE_PLUGIN) {
+    require('cypress-fail-fast/plugin')(on, config)
+  }
   require('dd-trace/ci/cypress/plugin')(on, config)
 }
