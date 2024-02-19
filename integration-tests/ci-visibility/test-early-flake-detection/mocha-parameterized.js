ca6b4bd8b30745bdc480dafb423c3d106477a862
Juan Antonio Fernández de Alba <juan.fernandezdealba@datadoghq.com>
2024-02-19 08:54:05 +0100

    [ci-visibility] Early flake detection for mocha  (#4060)

---
+++ integration-tests/ci-visibility/test-early-flake-detection/mocha-parameterized.js
@@ -0,0 +1,8 @@
+const { expect } = require('chai')
+const forEach = require('mocha-each')
+
+describe('parameterized', () => {
+  forEach(['parameter 1', 'parameter 2']).it('test %s', (value) => {
+    expect(value.startsWith('parameter')).to.be.true
+  })
+})
