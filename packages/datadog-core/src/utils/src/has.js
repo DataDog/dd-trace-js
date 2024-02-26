eafd44ea88f85315887ee5dfb1281822a5ccae83
Ida Liu <119438987+ida613@users.noreply.github.com>
2024-02-26 13:57:57 -0500

    Update packages/datadog-core/src/utils/src/has.js
    Co-authored-by: Attila Szegedi <szegedi@users.noreply.github.com>

--- packages/datadog-core/src/utils/src/has.js
+++ packages/datadog-core/src/utils/src/has.js
@@ -3,15 +3,12 @@
 module.exports = (object, path) => {
   const pathArr = path.split('.')
   let property = object
-  let i
-  for (i = 0; i < pathArr.length - 1; i++) {
-    const n = pathArr[i]
+  for (const n of pathArr) {
     if (property.hasOwnProperty(n)) {
       property = property[n]
     } else {
       return false
     }
   }
-  if (property.hasOwnProperty(pathArr[i])) return true
-  return false
+  return true
 }
