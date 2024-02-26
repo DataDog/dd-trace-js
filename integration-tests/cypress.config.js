ec00a52d6ceb8cf8413ca26ee18e3503573f525f
Juan Antonio Fernández de Alba <juan.fernandezdealba@datadoghq.com>
2024-02-26 09:09:15 +0100

    [ci-visibility] Allow using `after:run` directly  (#4090)

--- integration-tests/cypress.config.js
+++ integration-tests/cypress.config.js
@@ -6,6 +6,13 @@ module.exports = {
         require('cypress-fail-fast/plugin')(on, config)
       }
       require('dd-trace/ci/cypress/plugin')(on, config)
+      if (process.env.CYPRESS_ENABLE_AFTER_RUN_CUSTOM) {
+        on('after:run', (...args) => {
+          // do custom stuff
+          // and call after-run at the end
+          require('dd-trace/ci/cypress/after-run')(...args)
+        })
+      }
     }
   },
   video: false,
