6e714513e99aa8983022f811e9e43f06a136775d
Juan Antonio Fernández de Alba <juan.fernandezdealba@datadoghq.com>
2024-02-29 08:48:27 +0100

    [ci-visibility] Fix negative duration in playwright steps (#4113)
    Co-authored-by: Romain Komorn <136473744+romainkomorndatadog@users.noreply.github.com>

---
+++ integration-tests/ci-visibility/playwright-tests-error/before-all-timeout-test.js
@@ -0,0 +1,19 @@
+const { test, expect } = require('@playwright/test')
+
+test.beforeEach(async ({ page }) => {
+  await page.goto(process.env.PW_BASE_URL)
+})
+
+const waitFor = (ms) => new Promise(resolve => setTimeout(resolve, ms))
+
+test.describe('playwright', () => {
+  test.beforeAll(async () => {
+    // timeout error
+    await waitFor(3100)
+  })
+  test('should work with passing tests', async ({ page }) => {
+    await expect(page.locator('.hello-world')).toHaveText([
+      'Hello World'
+    ])
+  })
+})
