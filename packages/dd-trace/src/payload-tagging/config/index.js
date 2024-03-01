f3661378fa838e39e689926ba03b32fc1b00f2ad
Jordi Bertran de Balanda <jordi.bertran@datadoghq.com>
2024-03-01 15:30:15 +0100

    introduce payload tagging logic

---
+++ packages/dd-trace/src/payload-tagging/config/index.js
@@ -0,0 +1,29 @@
+const sdks = { }
+
+function getSDKRules (sdk, requestInput, responseInput) {
+  return Object.fromEntries(
+    Object.entries(sdk).map(([service, serviceRules]) => {
+      return [
+        service,
+        {
+          request: serviceRules.request.concat(requestInput || []),
+          response: serviceRules.response.concat(responseInput || []),
+          expand: serviceRules.expand || []
+        }
+      ]
+    })
+  )
+}
+
+function appendRules (requestInput, responseInput) {
+  return Object.fromEntries(
+    Object.entries(sdks).map(([name, sdk]) => {
+      return [
+        name,
+        getSDKRules(sdk, requestInput, responseInput)
+      ]
+    })
+  )
+}
+
+module.exports = { appendRules }
