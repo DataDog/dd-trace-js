49652d8fade7400f7aa318e4c09f06fefe4b9105
Juan Antonio Fernández de Alba <juan.fernandezdealba@datadoghq.com>
2024-03-05 11:00:59 +0100

    [ci-visibility] Expose cypress' after:spec handler (#4127)

--- integration-tests/cypress-esm-config.mjs
+++ integration-tests/cypress-esm-config.mjs
@@ -15,6 +15,24 @@ async function runCypress () {
           import('dd-trace/ci/cypress/plugin').then(module => {
             module.default(on, config)
           })
+          if (process.env.CYPRESS_ENABLE_AFTER_RUN_CUSTOM) {
+            on('after:run', (...args) => {
+              // do custom stuff
+              // and call after-run at the end
+              return import('dd-trace/ci/cypress/after-run').then(module => {
+                module.default(...args)
+              })
+            })
+          }
+          if (process.env.CYPRESS_ENABLE_AFTER_SPEC_CUSTOM) {
+            on('after:spec', (...args) => {
+              // do custom stuff
+              // and call after-spec at the end
+              return import('dd-trace/ci/cypress/after-spec').then(module => {
+                module.default(...args)
+              })
+            })
+          }
         },
         specPattern: process.env.SPEC_PATTERN || 'cypress/e2e/**/*.cy.js'
       },
