c85f50d8afbf15d91ad4f9de056ba355d5736dfe
Ayan Khan <khanayan_123@hotmail.com>
2024-03-07 10:29:41 -0500

    Khanayan123/instrument apollo gateway (#4102)
    * instrument apollo gateway

---
+++ packages/datadog-plugin-apollo/src/gateway/plan.js
@@ -0,0 +1,13 @@
+
+'use strict'
+
+const ApolloBasePlugin = require('../../../dd-trace/src/plugins/apollo')
+
+class ApolloGatewayPlanPlugin extends ApolloBasePlugin {
+  static get operation () { return 'plan' }
+  static get prefix () {
+    return 'tracing:apm:apollo:gateway:plan'
+  }
+}
+
+module.exports = ApolloGatewayPlanPlugin
