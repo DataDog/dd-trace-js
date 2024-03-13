aa69f72c12c8d86dcbd02acea5a01cae7357f932
Igor Unanua <igor.unanua@datadoghq.com>
2024-03-13 14:48:43 +0100

    Use hardcoded-rule-type constants

--- packages/dd-trace/src/appsec/iast/analyzers/hardcoded-secret-analyzer.js
+++ packages/dd-trace/src/appsec/iast/analyzers/hardcoded-secret-analyzer.js
@@ -2,12 +2,13 @@
 
 const { HARDCODED_SECRET } = require('../vulnerabilities')
 const HardcodedBaseAnalyzer = require('./hardcoded-base-analyzer')
+const { ValueOnly } = require('./hardcoded-rule-type')
 
 const allRules = require('./hardcoded-secret-rules')
 
 class HardcodedSecretAnalyzer extends HardcodedBaseAnalyzer {
   constructor () {
-    super(HARDCODED_SECRET, allRules, allRules.filter(rule => rule.type === 'ValueOnly'))
+    super(HARDCODED_SECRET, allRules, allRules.filter(rule => rule.type === ValueOnly))
   }
 }
 
