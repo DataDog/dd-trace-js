d9629f25018d04bbeca90178b5ccedd6666692db
Igor Unanua <igor.unanua@datadoghq.com>
2024-03-13 14:58:36 +0100

    Remove eslint comment

--- packages/dd-trace/src/appsec/iast/analyzers/hardcoded-password-rules.js
+++ packages/dd-trace/src/appsec/iast/analyzers/hardcoded-password-rules.js
@@ -6,8 +6,6 @@ const { NameAndValue } = require('./hardcoded-rule-type')
 module.exports = [
   {
     id: 'hardcoded-password',
-
-    // eslint-disable-next-line max-len
     regex: /(?:pwd|pswd|pass|secret)(?:[0-9a-z\-_\t.]{0,20})(?:[\s|']|[\s|""]){0,3}(?:=|>|:{1,3}=|\|\|:|<=|=>|:|\?=)(?:'|""|\s|=|\x60){0,5}([0-9a-z\-_.=]{10,150})(?:['"\s\x60;]|$)/i,
     type: NameAndValue
   }
