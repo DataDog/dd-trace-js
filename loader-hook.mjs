f39720d188c1b9ffb06672eed5724aa80c8db61b
Ida Liu <119438987+ida613@users.noreply.github.com>
2024-03-25 17:08:05 -0400

    added module list to pass into iitm hook

--- loader-hook.mjs
+++ loader-hook.mjs
@@ -1,1 +1,8 @@
+import { createHook } from 'import-in-the-middle/hook.js'
 export * from 'import-in-the-middle/hook.mjs'
+
+// import * as moduleList from './packages/datadog-instrumentations/src/helpers/dependency-list.js'
+
+// const { load, resolve, getFormat, getSource } = createHook(import.meta, moduleList)
+
+// export { load, resolve, getFormat, getSource }
