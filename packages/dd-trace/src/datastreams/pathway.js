7d99016a495cac4d6737e74466f6939f7c50894b
William Conti <william.conti@datadoghq.com>
2024-04-05 11:24:21 -0400

    Revert "change encoding / decoding of hashes to match other tracers"
    This reverts commit abf28ca4fc0540854c80ae52eaa98ba6e93e94c5.

--- packages/dd-trace/src/datastreams/pathway.js
+++ packages/dd-trace/src/datastreams/pathway.js
@@ -1,50 +1,39 @@
-const { toBufferLE } = require('bigint-buffer')
-const { encodeVarInt64, decodeVarInt64 } = require('./encoding')
+// encoding used here is sha256
+// other languages use FNV1
+// this inconsistency is ok because hashes do not need to be consistent across services
+const crypto = require('crypto')
+const { encodeVarint, decodeVarint } = require('./encoding')
+const LRUCache = require('lru-cache')
+
+const options = { max: 500 }
+const cache = new LRUCache(options)
 
 const CONTEXT_PROPAGATION_KEY = 'dd-pathway-ctx'
 const CONTEXT_PROPAGATION_KEY_BASE64 = 'dd-pathway-ctx-base64'
 
-const FNV_64_PRIME = BigInt('0x100000001B3')
-const FNV1_64_INIT = BigInt('0xCBF29CE484222325')
+function shaHash (checkpointString) {
+  const hash = crypto.createHash('md5').update(checkpointString).digest('hex').slice(0, 16)
+  return Buffer.from(hash, 'hex')
+}
 
-function fnv (data, hvalInit, fnvPrime, fnvSize) {
-  let hval = hvalInit
-  for (let i = 0; i < data.length; i++) {
-    hval = (hval * fnvPrime) % fnvSize
-    hval ^= BigInt(data[i])
+function computeHash (service, env, edgeTags, parentHash) {
+  const key = `${service}${env}` + edgeTags.join('') + parentHash.toString()
+  if (cache.get(key)) {
+    return cache.get(key)
   }
-  return hval
-}
-
-function fnv1Base64 (data) {
-  return fnv(data, FNV1_64_INIT, FNV_64_PRIME, 2n ** 64n)
-}
-
-function getBytes (s) {
-  return Buffer.from(s, 'utf-8')
-}
-
-function computeHash (service, env, tags, parentHash) {
-  let b = Buffer.concat([getBytes(service), getBytes(env)])
-  for (const t of tags) {
-    b = Buffer.concat([b, getBytes(t)])
-  }
-  const nodeHash = fnv1Base64(b)
-  // console.log(node_hash)
-  const nodeHashBuffer = toBufferLE(nodeHash, 8)
-
-  const parentHashBuffer = toBufferLE(BigInt(parentHash), 8)
-
-  const combinedBuffer = Buffer.concat([nodeHashBuffer, parentHashBuffer])
-  return fnv1Base64(combinedBuffer)
+  const currentHash = shaHash(`${service}${env}` + edgeTags.join(''))
+  const buf = Buffer.concat([currentHash, parentHash], 16)
+  const val = shaHash(buf.toString())
+  cache.set(key, val)
+  return val
 }
 
 function encodePathwayContext (dataStreamsContext) {
   return Buffer.concat([
-    toBufferLE(BigInt(dataStreamsContext.hash), 8),
-    encodeVarInt64(BigInt(dataStreamsContext.pathway_start_sec * 1e3)),
-    encodeVarInt64(BigInt(dataStreamsContext.current_edge_start_sec * 1e3))
-  ])
+    dataStreamsContext.hash,
+    Buffer.from(encodeVarint(Math.round(dataStreamsContext.pathwayStartNs / 1e6))),
+    Buffer.from(encodeVarint(Math.round(dataStreamsContext.edgeStartNs / 1e6)))
+  ], 20)
 }
 
 function encodePathwayContextBase64 (dataStreamsContext) {
@@ -59,11 +48,11 @@ function decodePathwayContext (pathwayContext) {
   // hash and parent hash are in LE
   const pathwayHash = pathwayContext.subarray(0, 8)
   const encodedTimestamps = pathwayContext.subarray(8)
-  const [pathwayStartMs, encodedTimeSincePrev] = decodeVarInt64(encodedTimestamps)
+  const [pathwayStartMs, encodedTimeSincePrev] = decodeVarint(encodedTimestamps)
   if (pathwayStartMs === undefined) {
     return null
   }
-  const [edgeStartMs] = decodeVarInt64(encodedTimeSincePrev)
+  const [edgeStartMs] = decodeVarint(encodedTimeSincePrev)
   if (edgeStartMs === undefined) {
     return null
   }
