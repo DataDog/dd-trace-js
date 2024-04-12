372846a4904e01c9562300596791d9bb1f91a8a4
roberto montero <roberto.montero@datadoghq.com>
2024-04-12 14:01:19 +0200

    build deb and rpm snapshots

---
+++ .gitlab/build-deb-rpm-snapshot.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+verdaccio &
+sleep 5
+npm install -g npm-cli-adduser
+npm-cli-adduser -u test -p test -e email@email.com -r http://localhost:4873
+yarn install
+content=`cat ./package.json | tr '\n' ' '`
+current_version=$(jq '.version' <<< "$content" )
+current_version=$(echo "$current_version" | tr -d '"')
+current_version+="$CI_VERSION_SUFFIX"
+npm version --no-git-tag-version $current_version
+npm publish --tag dev --registry http://localhost:4873
+echo "Finish"
+export JS_PACKAGE_VERSION=$current_version
