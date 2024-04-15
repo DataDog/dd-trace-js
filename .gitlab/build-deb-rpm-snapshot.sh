d57ec671086a97c0572dbcc1aa24e07c20a7b5e6
Igor Unanua <igor.unanua@datadoghq.com>
2024-04-15 13:07:08 +0200

    Do not start verdaccio, npm pack and run fpm with the just packed .tgz (#4240)
    * Do not start verdaccio, npm pack and run fpm with the just packed .tgz
    Co-authored-by: roberto montero <roberto.montero@datadoghq.com>

--- .gitlab/build-deb-rpm-snapshot.sh
+++ .gitlab/build-deb-rpm-snapshot.sh
@@ -4,8 +4,6 @@
 #We build the npm package setting the snapshot version and then we publish it to the local registry
 #Use local registry  to build the deb and rpm packages
 
-verdaccio &
-sleep 5
 npm install -g npm-cli-adduser
 npm-cli-adduser -u test -p test -e email@email.com -r http://localhost:4873
 yarn install
@@ -14,9 +12,9 @@ current_version=$(jq '.version' <<< "$content" )
 current_version=$(echo "$current_version" | tr -d '"')
 current_version+="$CI_VERSION_SUFFIX"
 npm version --no-git-tag-version $current_version
-npm publish --tag dev --registry http://localhost:4873
+npm pack
 export JS_PACKAGE_VERSION=$current_version
-
+cp dd-trace-$JS_PACKAGE_VERSION.tgz packaging/dd-trace-$JS_PACKAGE_VERSION.tgz
 echo "Generating Version: $JS_PACKAGE_VERSION"
 cd packaging
 echo -n $JS_PACKAGE_VERSION > auto_inject-node.version
@@ -26,11 +24,9 @@ source common_build_functions.sh
 # Extract package to a dir to make changes
 fpm --input-type npm \
   --npm-package-name-prefix "" \
-  --npm-registry http://localhost:4873 \
   --output-type dir --prefix "" \
-  --version "$JS_PACKAGE_VERSION" \
   --verbose \
-  --name dd-trace dd-trace
+  --name dd-trace ./dd-trace-$JS_PACKAGE_VERSION.tgz
 
 cp auto_inject-node.version dd-trace.dir/lib/version
 
