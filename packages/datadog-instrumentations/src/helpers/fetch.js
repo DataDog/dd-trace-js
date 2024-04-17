b56e69c2334401279ce2396a38b21a2d7c45db88
rochdev <roch.devost@gmail.com>
2024-04-17 11:43:36 -0400

    update fetch to use tracing channel

---
+++ packages/datadog-instrumentations/src/helpers/fetch.js
@@ -0,0 +1,21 @@
+'use strict'
+
+exports.createWrapFetch = function createWrapFetch (Request, ch) {
+  return function wrapFetch (fetch) {
+    if (typeof fetch !== 'function') return fetch
+
+    return function (input, init) {
+      if (!ch.start.hasSubscribers) return fetch.apply(this, arguments)
+
+      const req = new Request(input, init)
+      const headers = req.headers
+      const ctx = { req, headers }
+
+      return ch.tracePromise(() => {
+        const res = fetch.call(this, req, { headers: ctx.headers })
+        ctx.res = res
+        return res
+      }, ctx)
+    }
+  }
+}
