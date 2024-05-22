bf62d808c07caa6637b043b8f339b9be534ee9a1
Laplie Anderson <laplie.anderson@datadoghq.com>
2024-05-22 18:50:44 -0400

    OCI package building

---
+++ .gitlab/build_oci_package.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+cd ../dd-trace-js
+
+npm pack
+
+cp dd-trace-js*.tar.gz ../packaging/dd-trace-js.tar.gz
+
+mkdir -p ../packaging/sources
+
+jq --raw-output '.version' package.json > ../packaging/sources/version
+
+cd ../packaging
+
+npm install dd-trace-js.tar.gz
+
+cp -R node_modules sources/
+
+export VERSION=$(<sources/version)
+
+datadog-package create \
+  --version="$VERSION" \
+  --package="datadog-apm-library-js" \
+  --archive=true \
+  --archive-path="datadog-apm-library-$VERSION.tar" \
+  ./sources
