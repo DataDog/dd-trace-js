ead2f410433b794f14170023e7b7cfcfb51dce3c
Juan Antonio Fernández de Alba <juan.fernandezdealba@datadoghq.com>
2024-05-23 10:03:03 +0200

    [ci-visibility] Support mocha parallel mode (#4314)

--- ext/exporters.d.ts
+++ ext/exporters.d.ts
@@ -5,6 +5,7 @@ declare const exporters: {
   AGENT_PROXY: 'agent_proxy',
   JEST_WORKER: 'jest_worker',
   CUCUMBER_WORKER: 'cucumber_worker'
+  MOCHA_WORKER: 'mocha_worker'
 }
 
 export = exporters
