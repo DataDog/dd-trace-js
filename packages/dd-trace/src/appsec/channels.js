ed1ce0692dde90c93a5899efafd483ebd9a74bb0
Ugaitz Urien <ugaitz.urien@datadoghq.com>
2024-06-04 10:19:04 +0200

    Exploit prevention monitoring ssrf (#4361)
    ---------
    
    Co-authored-by: Carles Capell <107924659+CarlesDD@users.noreply.github.com>

--- packages/dd-trace/src/appsec/channels.js
+++ packages/dd-trace/src/appsec/channels.js
@@ -17,5 +17,6 @@ module.exports = {
   setCookieChannel: dc.channel('datadog:iast:set-cookie'),
   nextBodyParsed: dc.channel('apm:next:body-parsed'),
   nextQueryParsed: dc.channel('apm:next:query-parsed'),
-  responseBody: dc.channel('datadog:express:response:json:start')
+  responseBody: dc.channel('datadog:express:response:json:start'),
+  httpClientRequestStart: dc.channel('apm:http:client:request:start')
 }
