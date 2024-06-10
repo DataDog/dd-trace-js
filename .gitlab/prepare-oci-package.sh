406e18aa3b48191b721d1c16c558ed97f1808614
Laplie Anderson <laplie.anderson@datadoghq.com>
2024-06-10 19:21:11 -0400

    typo

--- .gitlab/prepare-oci-package.sh
+++ .gitlab/prepare-oci-package.sh
@@ -15,7 +15,7 @@ rm packaging/sources/*.json # package.json and package-lock.json are unneeded
 if [ -n "$CI_COMMIT_TAG" ] && [ -z "$JS_PACKAGE_VERSION" ]; then
   JS_PACKAGE_VERSION=${CI_COMMIT_TAG##v}
 elif [ -z "$CI_COMMIT_TAG" ] && [ -z "$JS_PACKAGE_VERSION" ]; then
-  JS_PACKAGE_VERSION="$(jq --raw-output '.version' package.json)${$CI_VERSION_SUFFIX}"
+  JS_PACKAGE_VERSION="$(jq --raw-output '.version' package.json)${CI_VERSION_SUFFIX}"
 fi
 
 echo -n $JS_PACKAGE_VERSION > packaging/sources/version
