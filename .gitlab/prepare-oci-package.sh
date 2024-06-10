fc91abf0787f6c4cdda62682112d59b4854655f1
Laplie Anderson <laplie.anderson@datadoghq.com>
2024-06-10 16:18:15 -0400

    First cut

--- .gitlab/prepare-oci-package.sh
+++ .gitlab/prepare-oci-package.sh
@@ -15,16 +15,9 @@ rm packaging/sources/*.json # package.json and package-lock.json are unneeded
 if [ -n "$CI_COMMIT_TAG" ] && [ -z "$JS_PACKAGE_VERSION" ]; then
   JS_PACKAGE_VERSION=${CI_COMMIT_TAG##v}
 elif [ -z "$CI_COMMIT_TAG" ] && [ -z "$JS_PACKAGE_VERSION" ]; then
-  JS_PACKAGE_VERSION="$(jq --raw-output '.version' package.json).pipeline.${CI_PIPELINE_ID}.beta.${CI_COMMIT_SHORT_SHA}"
+  JS_PACKAGE_VERSION="$(jq --raw-output '.version' package.json)${$CI_VERSION_SUFFIX}"
 fi
-echo -n $JS_PACKAGE_VERSION > packaging/auto_inject-node.version
+
 echo -n $JS_PACKAGE_VERSION > packaging/sources/version
 
 cd packaging
-
-datadog-package create \
-  --version="$JS_PACKAGE_VERSION" \
-  --package="datadog-apm-library-js" \
-  --archive=true \
-  --archive-path="datadog-apm-library-js-$JS_PACKAGE_VERSION.tar" \
-  ./sources
