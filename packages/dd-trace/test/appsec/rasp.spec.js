350296a6177b3d8009a4786f6714bee85a00e0f8
Ugaitz Urien <ugaitz.urien@datadoghq.com>
2024-06-11 13:27:19 +0200

    Exploit prevention metrics (#4370)
    ---------
    
    Co-authored-by: Carles Capell <107924659+CarlesDD@users.noreply.github.com>
    Co-authored-by: simon-id <simon.id@datadoghq.com>

--- packages/dd-trace/test/appsec/rasp.spec.js
+++ packages/dd-trace/test/appsec/rasp.spec.js
@@ -41,7 +41,7 @@ describe('RASP', () => {
       httpClientRequestStart.publish(ctx)
 
       const persistent = { [addresses.HTTP_OUTGOING_URL]: 'http://example.com' }
-      sinon.assert.calledOnceWithExactly(waf.run, { persistent }, req)
+      sinon.assert.calledOnceWithExactly(waf.run, { persistent }, req, 'ssrf')
     })
 
     it('should not analyze ssrf if rasp is disabled', () => {
