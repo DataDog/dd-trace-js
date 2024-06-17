004649e59cee917b1b020f8ab5a04c7557c61042
snek <the@snek.dev>
2024-06-17 14:50:20 -0700

    Add support for Deno

--- packages/datadog-core/src/storage/index.js
+++ packages/datadog-core/src/storage/index.js
@@ -7,7 +7,9 @@ const semver = require('semver')
 // https://github.com/nodejs/node/pull/33801
 const hasJavaScriptAsyncHooks = semver.satisfies(process.versions.node, '>=14.5')
 
-if (hasJavaScriptAsyncHooks) {
+if (typeof Deno !== 'undefined') {
+  module.exports = require('./async_local_storage')
+} else if (hasJavaScriptAsyncHooks) {
   module.exports = require('./async_resource')
 } else {
   module.exports = require('./async_hooks')
