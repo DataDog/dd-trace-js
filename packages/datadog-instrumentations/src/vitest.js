17de8593f6715f1379ddba779b90c821348cd63b
Juan Fernandez <juan.fernandezdealba@datadoghq.com>
2024-06-19 17:40:45 +0200

    support 1.6.0 too

--- packages/datadog-instrumentations/src/vitest.js
+++ packages/datadog-instrumentations/src/vitest.js
@@ -1,6 +1,5 @@
 const { addHook, channel, AsyncResource } = require('./helpers/instrument')
 const shimmer = require('../../datadog-shimmer')
-
 // Needs modifying node_modules/.pnpm/import-in-the-middle@1.7.3/node_modules/import-in-the-middle/index.js
 // the specifiers thing I don't understand
 
@@ -23,45 +22,103 @@ const isVitestTestRunner = (vitestPackage) => {
   return vitestPackage.VitestTestRunner
 }
 
-const isVitestPlugin = (vitestPackage) => {
-  return vitestPackage.B?.name === 'BaseSequencer'
+// maybe not valid???
+// const isVitestPlugin = (vitestPackage) => {
+//   return vitestPackage.B?.name === 'BaseSequencer'
+// }
+
+// from 1.6.0 at least, BaseSequencer is not exported at the same point as startVitest
+// const isVitestPlugin = (vitestPackage) => {
+//   return vitestPackage.s?.name === 'startVitest'
+// }
+
+const isCac = (vitestPackage) => {
+  return vitestPackage.c?.name === 'createCLI'
+}
+
+const isReporterPackage = (vitestPackage) => {
+  // return vitestPackage.a?.name === 'BasicReporter' // it doesn't even export, so can't use BaseReport
+  return vitestPackage.B?.name === 'BaseSequencer' // this is probably used everywhere, so maybe it has info
 }
 
 // --- do I need to specify a file? That's problematic because they contain a hash
 
+// maybe use getEnvPackageName as "start" of session?
+
 // vitestPackage.s only works for 1.1.0 - it doesn't for 1.6.0
 addHook({
   name: 'vitest',
-  versions: ['>=1.1.0 <1.6.0']
+  versions: ['>=1.1.0']
 }, (vitestPackage, frameworkVersion) => {
-  debugger
-  if (isVitestPlugin(vitestPackage)) {
-    //
-    console.log('is S still s???????', vitestPackage)
-    // TODO: will the "s" (minified version) be the same in future versions?
-    shimmer.wrap(vitestPackage, 's', startVitest => async function () {
-      let onFinish
-
-      const flushPromise = new Promise(resolve => {
-        onFinish = resolve
-      })
-
+  if (isCac(vitestPackage)) {
+    const oldCreateCli = vitestPackage.c
+    // could be start session
+    // TODO: change to shimmer
+    vitestPackage.c = function () {
       sessionAsyncResource.runInAsyncScope(() => {
         // TODO: change command to proper command
         testSessionStartCh.publish({ command: 'vitest run', frameworkVersion })
       })
-      const res = await startVitest.apply(this, arguments)
+      return oldCreateCli.apply(this, arguments)
+    }
+  }
+  if (isReporterPackage(vitestPackage)) {
+    // this hack seems to work for 1.6.0
+    shimmer.wrap(vitestPackage.B.prototype, 'sort', sort => async function () {
+      // maybe this could also be start, instead of having two different places - easier
 
-      sessionAsyncResource.runInAsyncScope(() => {
-        // TODO: get proper status
-        testSessionFinishCh.publish({ status: 'pass', onFinish })
+      // BY THIS TIME IT'S TOO LATE!!!!! -> changing process.env does not propagate to the worker threads
+      // sessionAsyncResource.runInAsyncScope(() => {
+      //   // TODO: change command to proper command
+      //   testSessionStartCh.publish({ command: 'vitest run', frameworkVersion })
+      // })
+
+      shimmer.wrap(this.ctx, 'exit', exit => async function () {
+        let onFinish
+
+        const flushPromise = new Promise(resolve => {
+          onFinish = resolve
+        })
+        sessionAsyncResource.runInAsyncScope(() => {
+          // TODO: get proper status
+          testSessionFinishCh.publish({ status: 'pass', onFinish })
+        })
+
+        await flushPromise
+
+        return exit.apply(this, arguments)
       })
 
-      await flushPromise
-
-      return res
+      return sort.apply(this, arguments)
     })
   }
+  // if (isVitestPlugin(vitestPackage)) {
+  //   // this DOES NOT WORK for 1.6.0
+  //   // TODO: will the "s" (minified version) be the same in future versions?
+  //   shimmer.wrap(vitestPackage, 's', startVitest => async function () {
+  //     let onFinish
+
+  //     const flushPromise = new Promise(resolve => {
+  //       onFinish = resolve
+  //     })
+
+  //     sessionAsyncResource.runInAsyncScope(() => {
+  //       // TODO: change command to proper command
+  //       testSessionStartCh.publish({ command: 'vitest run', frameworkVersion })
+  //     })
+  //     const res = await startVitest.apply(this, arguments)
+
+  //     console.log('SHOULD NOT LOG THIS!!!!!!!!!!')
+  //     sessionAsyncResource.runInAsyncScope(() => {
+  //       // TODO: get proper status
+  //       testSessionFinishCh.publish({ status: 'pass', onFinish })
+  //     })
+
+  //     await flushPromise
+
+  //     return res
+  //   })
+  // }
 
   if (isVitestTestRunner(vitestPackage)) {
     // test start
@@ -100,7 +157,7 @@ addHook({
   versions: ['>=0.0.0']
 }, vitestPackage => {
   if (vitestPackage.startTests) {
-    shimmer.wrap(vitestPackage, 'startTests', startTests => async function (testPath, vitestTestRunner) {
+    shimmer.wrap(vitestPackage, 'startTests', startTests => async function (testPath) {
       const asyncResource = new AsyncResource('bound-anonymous-fn')
       asyncResource.runInAsyncScope(() => {
         testSuiteStartCh.publish(testPath[0])
