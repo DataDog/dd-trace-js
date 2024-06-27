ea45adc67edf8ea02bf99248e2f21463600e0e17
jordan gonzález <30836115+duncanista@users.noreply.github.com>
2024-06-27 19:23:55 -0400

    add comment on when we can disable it

--- packages/dd-trace/src/lambda/index.js
+++ packages/dd-trace/src/lambda/index.js
@@ -2,6 +2,10 @@
 
 const { registerLambdaHook } = require('./runtime/ritm')
 
+/**
+ * It is safe to do it this way, since customers will never be expected to disable
+ * this specific instrumentation through the init config object.
+ */
 const _DD_TRACE_DISABLED_INSTRUMENTATIONS = process.env.DD_TRACE_DISABLED_INSTRUMENTATIONS || ''
 const _disabledInstrumentations = new Set(
   _DD_TRACE_DISABLED_INSTRUMENTATIONS ? _DD_TRACE_DISABLED_INSTRUMENTATIONS.split(',') : []
