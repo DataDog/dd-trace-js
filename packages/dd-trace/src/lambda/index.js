5256214c27c4a736ff6aaf5a6dba12558516eeaf
jordan gonzález <30836115+duncanista@users.noreply.github.com>
2024-06-27 15:03:51 -0400

    gate timeout spans under `lambda`

--- packages/dd-trace/src/lambda/index.js
+++ packages/dd-trace/src/lambda/index.js
@@ -2,4 +2,11 @@
 
 const { registerLambdaHook } = require('./runtime/ritm')
 
-registerLambdaHook()
+const _DD_TRACE_DISABLED_INSTRUMENTATIONS = process.env.DD_TRACE_DISABLED_INSTRUMENTATIONS || ''
+const _disabledInstrumentations = new Set(
+  _DD_TRACE_DISABLED_INSTRUMENTATIONS ? _DD_TRACE_DISABLED_INSTRUMENTATIONS.split(',') : []
+)
+
+if (!_disabledInstrumentations.has('lambda')) {
+  registerLambdaHook()
+}
