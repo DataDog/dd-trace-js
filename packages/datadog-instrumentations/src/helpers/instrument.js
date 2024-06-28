747435ee8be40fe2645066be0f6e4c0d97baf61b
Juan Antonio Fernández de Alba <juan.fernandezdealba@datadoghq.com>
2024-06-28 14:32:50 +0200

    [test-visibility] Add support for vitest (#4415)

--- packages/datadog-instrumentations/src/helpers/instrument.js
+++ packages/datadog-instrumentations/src/helpers/instrument.js
@@ -17,10 +17,11 @@ exports.channel = function (name) {
 /**
  * @param {string} args.name module name
  * @param {string[]} args.versions array of semver range strings
- * @param {string} args.file path to file within package to instrument?
+ * @param {string} args.file path to file within package to instrument
+ * @param {string} args.filePattern pattern to match files within package to instrument
  * @param Function hook
  */
-exports.addHook = function addHook ({ name, versions, file }, hook) {
+exports.addHook = function addHook ({ name, versions, file, filePattern }, hook) {
   if (typeof name === 'string') {
     name = [name]
   }
@@ -29,7 +30,7 @@ exports.addHook = function addHook ({ name, versions, file }, hook) {
     if (!instrumentations[val]) {
       instrumentations[val] = []
     }
-    instrumentations[val].push({ name: val, versions, file, hook })
+    instrumentations[val].push({ name: val, versions, file, filePattern, hook })
   }
 }
 
