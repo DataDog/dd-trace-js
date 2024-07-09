07efec4c672ac9760a3aac4ff8752cabab96c7a5
Juan Antonio Fernández de Alba <juan.fernandezdealba@datadoghq.com>
2024-07-09 11:30:04 +0200

    [test visibility] Add automatic flaky test retries to Mocha (#4453)

--- packages/datadog-instrumentations/src/mocha/utils.js
+++ packages/datadog-instrumentations/src/mocha/utils.js
@@ -11,6 +11,8 @@ const shimmer = require('../../../datadog-shimmer')
 // test channels
 const testStartCh = channel('ci:mocha:test:start')
 const testFinishCh = channel('ci:mocha:test:finish')
+// after a test has failed, we'll publish to this channel
+const testRetryCh = channel('ci:mocha:test:retry')
 const errorCh = channel('ci:mocha:test:error')
 const skipCh = channel('ci:mocha:test:skip')
 
@@ -22,6 +24,8 @@ const originalFns = new WeakMap()
 const testToStartLine = new WeakMap()
 const testFileToSuiteAr = new Map()
 const wrappedFunctions = new WeakSet()
+
+const NUM_FAILED_TEST_RETRIES = 5
 
 function isNewTest (test, knownTests) {
   const testSuite = getTestSuitePath(test.file, process.cwd())
@@ -70,6 +74,10 @@ function isMochaRetry (test) {
   return test._currentRetry !== undefined && test._currentRetry !== 0
 }
 
+function isLastRetry (test) {
+  return test._currentRetry === test._retries
+}
+
 function getTestFullName (test) {
   return `mocha.${getTestSuitePath(test.file, process.cwd())}.${removeEfdStringFromTestName(test.fullTitle())}`
 }
@@ -84,22 +92,34 @@ function getTestStatus (test) {
   return 'pass'
 }
 
-function getTestAsyncResource (test) {
+function getTestToArKey (test) {
   if (!test.fn) {
-    return testToAr.get(test)
+    return test
   }
   if (!wrappedFunctions.has(test.fn)) {
-    return testToAr.get(test.fn)
+    return test.fn
   }
   const originalFn = originalFns.get(test.fn)
-  return testToAr.get(originalFn)
+  return originalFn
 }
 
-function runnableWrapper (RunnablePackage) {
+function getTestAsyncResource (test) {
+  const key = getTestToArKey(test)
+  return testToAr.get(key)
+}
+
+function runnableWrapper (RunnablePackage, libraryConfig) {
   shimmer.wrap(RunnablePackage.prototype, 'run', run => function () {
     if (!testStartCh.hasSubscribers) {
       return run.apply(this, arguments)
     }
+    // Flaky test retries does not work in parallel mode
+    if (libraryConfig?.isFlakyTestRetriesEnabled) {
+      this.retries(NUM_FAILED_TEST_RETRIES)
+    }
+    // The reason why the wrapping logic is here is because we need to cover
+    // `afterEach` and `beforeEach` hooks as well.
+    // It can't be done in `getOnTestHandler` because it's only called for tests.
     const isBeforeEach = this.parent._beforeEach.includes(this)
     const isAfterEach = this.parent._afterEach.includes(this)
 
@@ -135,11 +155,16 @@ function runnableWrapper (RunnablePackage) {
 
 function getOnTestHandler (isMain, newTests) {
   return function (test) {
-    if (isMochaRetry(test)) {
-      return
-    }
     const testStartLine = testToStartLine.get(test)
     const asyncResource = new AsyncResource('bound-anonymous-fn')
+
+    // This may be a retry. If this is the case, `test.fn` is already wrapped,
+    // so we need to restore it.
+    if (wrappedFunctions.has(test.fn)) {
+      const originalFn = originalFns.get(test.fn)
+      test.fn = originalFn
+      wrappedFunctions.delete(test.fn)
+    }
     testToAr.set(test.fn, asyncResource)
 
     const {
@@ -186,7 +211,7 @@ function getOnTestEndHandler () {
     // if there are afterEach to be run, we don't finish the test yet
     if (asyncResource && !test.parent._afterEach.length) {
       asyncResource.runInAsyncScope(() => {
-        testFinishCh.publish(status)
+        testFinishCh.publish({ status, hasBeenRetried: isMochaRetry(test) })
       })
     }
   }
@@ -197,12 +222,17 @@ function getOnHookEndHandler () {
     const test = hook.ctx.currentTest
     if (test && hook.parent._afterEach.includes(hook)) { // only if it's an afterEach
       const isLastAfterEach = hook.parent._afterEach.indexOf(hook) === hook.parent._afterEach.length - 1
+      if (test._retries > 0 && !isLastRetry(test)) {
+        return
+      }
       if (isLastAfterEach) {
         const status = getTestStatus(test)
         const asyncResource = getTestAsyncResource(test)
-        asyncResource.runInAsyncScope(() => {
-          testFinishCh.publish(status)
-        })
+        if (asyncResource) {
+          asyncResource.runInAsyncScope(() => {
+            testFinishCh.publish({ status, hasBeenRetried: isMochaRetry(test) })
+          })
+        }
       }
     }
   }
@@ -226,7 +256,7 @@ function getOnFailHandler (isMain) {
           err.message = `${testOrHook.fullTitle()}: ${err.message}`
           errorCh.publish(err)
           // if it's a hook and it has failed, 'test end' will not be called
-          testFinishCh.publish('fail')
+          testFinishCh.publish({ status: 'fail', hasBeenRetried: isMochaRetry(test) })
         } else {
           errorCh.publish(err)
         }
@@ -247,6 +277,20 @@ function getOnFailHandler (isMain) {
         })
       }
     }
+  }
+}
+
+function getOnTestRetryHandler () {
+  return function (test) {
+    const asyncResource = getTestAsyncResource(test)
+    if (asyncResource) {
+      const isFirstAttempt = test._currentRetry === 0
+      asyncResource.runInAsyncScope(() => {
+        testRetryCh.publish(isFirstAttempt)
+      })
+    }
+    const key = getTestToArKey(test)
+    testToAr.delete(key)
   }
 }
 
@@ -299,6 +343,7 @@ module.exports = {
   testToStartLine,
   getOnTestHandler,
   getOnTestEndHandler,
+  getOnTestRetryHandler,
   getOnHookEndHandler,
   getOnFailHandler,
   getOnPendingHandler,
