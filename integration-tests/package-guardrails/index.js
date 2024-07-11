2ab1df2dc73a27d506d385658c0b262c00dd1312
Bryan English <bryan.english@datadoghq.com>
2024-07-11 10:15:11 -0400

    lint

--- integration-tests/package-guardrails/index.js
+++ integration-tests/package-guardrails/index.js
@@ -1,14 +1,16 @@
 'use strict'
 
-try {
+/* eslint-disable no-console */
+/* eslint-disable import/no-extraneous-dependencies */
 
+try {
   const P = require('bluebird')
 
   const isWrapped = P.prototype._then.toString().includes('AsyncResource')
 
-  // eslint-disable-next-line no-console
   console.log(isWrapped)
 } catch (e) {
   const fastify = require('fastify')
+
   console.log(fastify.toString().startsWith('function shim'))
 }
