d14fe446ec0d30e14fbe1d51989a0fbd9de21a1f
Ida Liu <119438987+ida613@users.noreply.github.com>
2024-07-31 14:27:34 -0400

    bug fix

--- packages/dd-trace/test/opentracing/span_context.spec.js
+++ packages/dd-trace/test/opentracing/span_context.spec.js
@@ -48,6 +48,7 @@ describe('SpanContext', () => {
       _tags: {},
       _sampling: { priority: 2 },
       _spanSampling: undefined,
+      _baggageItems: { foo: 'bar' },
       _noop: noop,
       _trace: {
         started: ['span1', 'span2'],
