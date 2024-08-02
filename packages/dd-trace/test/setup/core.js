b3120ae5a72a9782ef6728cba825aa7bbd41e1b7
Bryan English <bryan.english@datadoghq.com>
2024-08-02 15:54:20 -0400

    only run in SSI mode on release

--- packages/dd-trace/test/setup/core.js
+++ packages/dd-trace/test/setup/core.js
@@ -43,5 +43,8 @@ if (global.describe && typeof global.describe.skip !== 'function') {
 
 process.env.DD_INSTRUMENTATION_TELEMETRY_ENABLED = 'false'
 
-process.env.DD_INJECTION_ENABLED = 'true'
-process.env.DD_INJECT_FORCE = 'true'
+// If this is a release PR, set the SSI variables.
+if (/^v\d+\.x$/.test(process.env.GITHUB_BASE_REF || '')) {
+  process.env.DD_INJECTION_ENABLED = 'true'
+  process.env.DD_INJECT_FORCE = 'true'
+}
