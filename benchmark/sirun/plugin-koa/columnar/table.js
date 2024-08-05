929c03cb25da04d42319883e4feafa69f6aa5ca2
rochdev <roch.devost@gmail.com>
2024-08-05 15:00:19 -0400

    fix invalid max byte length

--- benchmark/sirun/plugin-koa/columnar/table.js
+++ benchmark/sirun/plugin-koa/columnar/table.js
@@ -9,7 +9,7 @@ class Table {
     this._strings = strings
 
     for (const [name, TypedArray] of Object.entries(columnTypes)) {
-      const buffer = new ArrayBuffer(this._capacity, { maxByteLength: 65536 })
+      const buffer = new ArrayBuffer(this._capacity, { maxByteLength: 2 ** 32 })
 
       this.columns[name] = new TypedArray(buffer)
     }
@@ -27,7 +27,7 @@ class Table {
       this._capacity = this._capacity * 2
 
       for (const column of Object.values(this.columns)) {
-        column.buffer.resize(this._capacity)
+        column.buffer.resize(this._capacity * column.buffer.BYTES_PER_ELEMENT)
       }
     }
   }
