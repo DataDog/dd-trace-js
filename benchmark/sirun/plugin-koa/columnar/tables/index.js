d2efa884094af8cc94f5a21cae8956cd55514b44
rochdev <roch.devost@gmail.com>
2024-08-05 15:00:59 -0400

    code cleanup

--- benchmark/sirun/plugin-koa/columnar/tables/index.js
+++ benchmark/sirun/plugin-koa/columnar/tables/index.js
@@ -6,24 +6,15 @@ const { SegmentStartTable } = require('./segment')
 const { SpanFinishTable } = require('./span')
 const { AddMetricTable, AddTagTable } = require('./tag')
 const { WebRequestFinishTable, WebRequestStartTable } = require('./web')
-const {
-  ADD_METRIC,
-  ADD_TAG,
-  ERROR,
-  MYSQL_QUERY_START,
-  SEGMENT_START,
-  SPAN_FINISH,
-  WEB_REQUEST_FINISH,
-  WEB_REQUEST_START
-} = require('../events')
+const events = require('../events')
 
 module.exports = {
-  [ADD_METRIC]: AddMetricTable,
-  [ADD_TAG]: AddTagTable,
-  [ERROR]: ExceptionTable,
-  [MYSQL_QUERY_START]: MysqlQueryStartTable,
-  [SEGMENT_START]: SegmentStartTable,
-  [SPAN_FINISH]: SpanFinishTable,
-  [WEB_REQUEST_FINISH]: WebRequestFinishTable,
-  [WEB_REQUEST_START]: WebRequestStartTable
+  [events.ADD_METRIC]: AddMetricTable,
+  [events.ADD_TAG]: AddTagTable,
+  [events.ERROR]: ExceptionTable,
+  [events.MYSQL_QUERY_START]: MysqlQueryStartTable,
+  [events.SEGMENT_START]: SegmentStartTable,
+  [events.SPAN_FINISH]: SpanFinishTable,
+  [events.WEB_REQUEST_FINISH]: WebRequestFinishTable,
+  [events.WEB_REQUEST_START]: WebRequestStartTable
 }
