03c5feda0e93d45aa60b21fd3735df24ece6d426
Juan Fernandez <juan.fernandezdealba@datadoghq.com>
2024-08-07 14:56:18 +0200

    vitest telemetry

--- packages/datadog-plugin-vitest/src/index.js
+++ packages/datadog-plugin-vitest/src/index.js
@@ -8,9 +8,14 @@ const {
   getTestSuiteCommonTags,
   TEST_SOURCE_FILE,
   TEST_IS_RETRY,
-  TEST_CODE_COVERAGE_LINES_PCT
+  TEST_CODE_COVERAGE_LINES_PCT,
+  TEST_CODE_OWNERS
 } = require('../../dd-trace/src/plugins/util/test')
 const { COMPONENT } = require('../../dd-trace/src/constants')
+const {
+  TELEMETRY_EVENT_CREATED,
+  TELEMETRY_EVENT_FINISHED
+} = require('../../dd-trace/src/ci-visibility/telemetry')
 
 // Milliseconds that we subtract from the error test duration
 // so that they do not overlap with the following test
@@ -65,6 +70,9 @@ class VitestPlugin extends CiPlugin {
       const span = store?.span
 
       if (span) {
+        this.telemetry.ciVisEvent(TELEMETRY_EVENT_FINISHED, 'test', {
+          hasCodeowners: !!span.context()._tags[TEST_CODE_OWNERS]
+        })
         span.setTag(TEST_STATUS, 'pass')
         span.finish(this.taskToFinishTime.get(task))
         finishAllTraceSpans(span)
@@ -76,6 +84,9 @@ class VitestPlugin extends CiPlugin {
       const span = store?.span
 
       if (span) {
+        this.telemetry.ciVisEvent(TELEMETRY_EVENT_FINISHED, 'test', {
+          hasCodeowners: !!span.context()._tags[TEST_CODE_OWNERS]
+        })
         span.setTag(TEST_STATUS, 'fail')
 
         if (error) {
@@ -92,7 +103,7 @@ class VitestPlugin extends CiPlugin {
 
     this.addSub('ci:vitest:test:skip', ({ testName, testSuiteAbsolutePath }) => {
       const testSuite = getTestSuitePath(testSuiteAbsolutePath, this.repositoryRoot)
-      this.startTestSpan(
+      const testSpan = this.startTestSpan(
         testName,
         testSuite,
         this.testSuiteSpan,
@@ -100,7 +111,11 @@ class VitestPlugin extends CiPlugin {
           [TEST_SOURCE_FILE]: testSuite,
           [TEST_STATUS]: 'skip'
         }
-      ).finish()
+      )
+      this.telemetry.ciVisEvent(TELEMETRY_EVENT_FINISHED, 'test', {
+        hasCodeowners: !!testSpan.context()._tags[TEST_CODE_OWNERS]
+      })
+      testSpan.finish()
     })
 
     this.addSub('ci:vitest:test-suite:start', ({ testSuiteAbsolutePath, frameworkVersion }) => {
@@ -125,6 +140,7 @@ class VitestPlugin extends CiPlugin {
           ...testSuiteMetadata
         }
       })
+      this.telemetry.ciVisEvent(TELEMETRY_EVENT_CREATED, 'suite')
       const store = storage.getStore()
       this.enter(testSuiteSpan, store)
       this.testSuiteSpan = testSuiteSpan
@@ -138,6 +154,7 @@ class VitestPlugin extends CiPlugin {
         span.finish()
         finishAllTraceSpans(span)
       }
+      this.telemetry.ciVisEvent(TELEMETRY_EVENT_FINISHED, 'suite')
       // TODO: too frequent flush - find for method in worker to decrease frequency
       this.tracer._exporter.flush(onFinish)
     })
@@ -163,7 +180,9 @@ class VitestPlugin extends CiPlugin {
         this.testSessionSpan.setTag(TEST_CODE_COVERAGE_LINES_PCT, testCodeCoverageLinesTotal)
       }
       this.testModuleSpan.finish()
+      this.telemetry.ciVisEvent(TELEMETRY_EVENT_FINISHED, 'module')
       this.testSessionSpan.finish()
+      this.telemetry.ciVisEvent(TELEMETRY_EVENT_FINISHED, 'session')
       finishAllTraceSpans(this.testSessionSpan)
       this.tracer._exporter.flush(onFinish)
     })
