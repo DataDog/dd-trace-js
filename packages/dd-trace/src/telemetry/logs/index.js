3caea99aa841cc517e910242cb8ebaec4dfe1fd6
Bryan English <bryan.english@datadoghq.com>
2024-08-13 17:41:38 -0400

    log error to telmetry (#4605)
    * log.error() calls should go to telemetry
    
    * remove unneccessary telemetry log calls

--- packages/dd-trace/src/telemetry/logs/index.js
+++ packages/dd-trace/src/telemetry/logs/index.js
@@ -5,6 +5,7 @@ const logCollector = require('./log-collector')
 const { sendData } = require('../send-data')
 
 const telemetryLog = dc.channel('datadog:telemetry:log')
+const errorLog = dc.channel('datadog:log:error')
 
 let enabled = false
 
@@ -33,12 +34,24 @@ function onLog (log) {
   }
 }
 
+function onErrorLog (msg) {
+  if (msg instanceof Error) {
+    onLog({
+      level: 'ERROR',
+      message: msg.message,
+      stack: msg.stack
+    })
+  }
+}
+
 function start (config) {
   if (!config.telemetry.logCollection || enabled) return
 
   enabled = true
 
   telemetryLog.subscribe(onLog)
+
+  errorLog.subscribe(onErrorLog)
 }
 
 function stop () {
@@ -47,6 +60,8 @@ function stop () {
   if (telemetryLog.hasSubscribers) {
     telemetryLog.unsubscribe(onLog)
   }
+
+  errorLog.unsubscribe(onErrorLog)
 }
 
 function send (config, application, host) {
