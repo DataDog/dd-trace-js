bbdb6798b032ade16e2c64fffebaf064da8e2fc3
rochdev <roch.devost@gmail.com>
2024-08-19 18:56:27 -0400

    add span start table

--- benchmark/sirun/plugin-koa/columnar/tables/span.js
+++ benchmark/sirun/plugin-koa/columnar/tables/span.js
@@ -2,6 +2,36 @@
 
 const { Table } = require('../table')
 
+class SpanStartTable extends Table {
+  constructor () {
+    super({
+      ticks: BigUint64Array,
+      segment_id: BigUint64Array,
+      span_id: BigUint64Array,
+      parent_id: BigUint64Array,
+      name: Uint16Array,
+      service: Uint16Array,
+      resource: Uint16Array,
+      type: Uint16Array
+    })
+  }
+
+  insert (spanContext, name, service, resource, type) {
+    this.reserve()
+
+    this.columns.ticks[this.length] = process.hrtime.bigint()
+    this.columns.segment_id[this.length] = spanContext.segment.segmentId
+    this.columns.span_id[this.length] = spanContext.spanId
+    this.columns.parent_id[this.length] = spanContext.parentId
+    this.columns.name[this.length] = name
+    this.columns.service[this.length] = service
+    this.columns.resource[this.length] = resource
+    this.columns.type[this.length] = type
+
+    this.length++
+  }
+}
+
 class SpanFinishTable extends Table {
   constructor () {
     super({
@@ -22,4 +52,4 @@ class SpanFinishTable extends Table {
   }
 }
 
-module.exports = { SpanFinishTable }
+module.exports = { SpanStartTable, SpanFinishTable }
