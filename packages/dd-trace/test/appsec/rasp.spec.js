fa35b90406ae24c56b40c0203eddfb27c38f0f89
Igor Unanua <igor.unanua@datadoghq.com>
2024-08-27 14:26:36 +0200

    some rasp lfi tests

--- packages/dd-trace/test/appsec/rasp.spec.js
+++ packages/dd-trace/test/appsec/rasp.spec.js
@@ -1,12 +1,12 @@
 'use strict'
 
 const proxyquire = require('proxyquire')
-const { httpClientRequestStart } = require('../../src/appsec/channels')
+const { httpClientRequestStart, fsOperationStart } = require('../../src/appsec/channels')
 const addresses = require('../../src/appsec/addresses')
 const { handleUncaughtExceptionMonitor } = require('../../src/appsec/rasp')
 
 describe('RASP', () => {
-  let waf, rasp, datadogCore, stackTrace, web
+  let waf, rasp, datadogCore, stackTrace, web, blocking
 
   beforeEach(() => {
     datadogCore = {
@@ -26,11 +26,16 @@ describe('RASP', () => {
       root: sinon.stub()
     }
 
+    blocking = {
+      block: sinon.stub()
+    }
+
     rasp = proxyquire('../../src/appsec/rasp', {
       '../../../datadog-core': datadogCore,
       './waf': waf,
       './stack_trace': stackTrace,
-      './../plugins/util/web': web
+      './../plugins/util/web': web,
+      './blocking': blocking
     })
 
     const config = {
@@ -166,6 +171,81 @@ describe('RASP', () => {
       httpClientRequestStart.publish(ctx)
 
       sinon.assert.notCalled(waf.run)
+    })
+  })
+
+  describe('analyzeLfi', () => {
+    const path = '/etc/passwd'
+    const ctx = { path }
+    const req = {}
+    const res = {}
+
+    it('should analyze lfi for root fs operations', () => {
+      const fs = { root: true }
+      datadogCore.storage.getStore.returns({ req, fs })
+
+      fsOperationStart.publish(ctx)
+
+      const persistent = { [addresses.FS_OPERATION_PATH]: path }
+      sinon.assert.calledOnceWithExactly(waf.run, { persistent }, req, 'lfi')
+    })
+
+    it('should NOT analyze lfi for child fs operations', () => {
+      const fs = {}
+      datadogCore.storage.getStore.returns({ req, fs })
+
+      fsOperationStart.publish(ctx)
+
+      sinon.assert.notCalled(waf.run)
+    })
+
+    it('should NOT analyze lfi for excluded operations', () => {
+      const fs = { opExcluded: true }
+      datadogCore.storage.getStore.returns({ req, fs })
+
+      fsOperationStart.publish(ctx)
+
+      sinon.assert.notCalled(waf.run)
+    })
+
+    it('should block req if there is a block_request action', () => {
+      const fs = { root: true }
+      datadogCore.storage.getStore.returns({ req, res, fs })
+
+      const blockingAction = {
+        block_request: {}
+      }
+      waf.run.returns(blockingAction)
+
+      const rootSpan = {
+        context: () => {
+          return { _name: 'express.request' }
+        }
+      }
+      web.root.returns(rootSpan)
+
+      fsOperationStart.publish(ctx)
+
+      sinon.assert.calledOnceWithExactly(blocking.block, req, res, rootSpan, null, blockingAction.block_request)
+    })
+
+    it('should not block req if there is no block_request action', () => {
+      const fs = { root: true }
+      datadogCore.storage.getStore.returns({ req, res, fs })
+
+      const blockingAction = {}
+      waf.run.returns(blockingAction)
+
+      const rootSpan = {
+        context: () => {
+          return { _name: 'express.request' }
+        }
+      }
+      web.root.returns(rootSpan)
+
+      fsOperationStart.publish(ctx)
+
+      sinon.assert.notCalled(blocking.block)
     })
   })
 
