32c6089f8183874683b56a35389cf6546ca3909f
Thomas Watson <w@tson.dk>
2024-09-04 13:06:51 +0200

    Allow Remote Config acknowledgements to be async (#4638)
    Remote Config updates coming from the Agent needs to be acknowledged. This PR
    changes the existing behavior by optionally allowing the acknowledgement to be
    async. This is achieved by:
    
    - Changing the way you register products from using the `EventEmitter` API to a
      custom API that only allows a single handler per product (event emitters can
      have more than one listener, which is problematic as we can't support more
      than one of the listeners to acknowledge the config). The new API is as
      follows:
      - `rc.setProductHandler(product, handler)` (prevously `rc.on(product,
        handler)`)
      - `rc.removeProductHandler(product)` (prevously `rc.off(product, handler)`)
    
    - The new product handler is called similar to the old event listener, with the
      following exception: A new optional 4th argument is supplied called `ack`.
      This is a callback which can be called (sync or async) either without any
      arguments (to set the state to `ACKNOWLEDGED`) or with a single error argument
      (to set the state to `ERROR`).
      - If the handler function signature takes less than 4 arguments or doesn't use
        the rest operator (`...args`), and...
        - ...the handler doesn't return a `Promise`: The state is set to
          `ACKNOWLEDGED` right away without the handler having to do anything
          (existing behavior)
        - ...the handler returns a `Promise`, we wait until the promise is resolved
          or rejected and set the state accordingly (new behavior)
      - If the handler function signature takes 4 or more arguments or use the rest
        operator (`...args`): The state is left as `UNACKNOWLEDGED` until the `ack`
        callback is called (new behavior)
      - In any case, the state is still set to `ERROR` if the handler throws
        (existing behavior)
    
    The `RemoteConfigManager` is still an `EventEmitter` however because the
    `kPreUpdate` symbol is still being emitted.

--- packages/dd-trace/test/appsec/remote_config/manager.spec.js
+++ packages/dd-trace/test/appsec/remote_config/manager.spec.js
@@ -149,30 +149,31 @@ describe('RemoteConfigManager', () => {
     })
   })
 
-  describe('on/off', () => {
+  describe('setProductHandler/removeProductHandler', () => {
     it('should update the product list and autostart or autostop', () => {
-      expect(rc.on('ASM_FEATURES', noop)).to.equal(rc)
+      expect(rc.scheduler.start).to.not.have.been.called
+
+      rc.setProductHandler('ASM_FEATURES', noop)
 
       expect(rc.state.client.products).to.deep.equal(['ASM_FEATURES'])
-      expect(rc.scheduler.start).to.have.been.calledOnce
+      expect(rc.scheduler.start).to.have.been.called
 
-      rc.on('ASM_DATA', noop)
-      rc.on('ASM_DD', noop)
+      rc.setProductHandler('ASM_DATA', noop)
+      rc.setProductHandler('ASM_DD', noop)
 
       expect(rc.state.client.products).to.deep.equal(['ASM_FEATURES', 'ASM_DATA', 'ASM_DD'])
-      expect(rc.scheduler.start).to.have.been.calledThrice
 
-      expect(rc.off('ASM_FEATURES', noop)).to.equal(rc)
+      rc.removeProductHandler('ASM_FEATURES')
 
       expect(rc.state.client.products).to.deep.equal(['ASM_DATA', 'ASM_DD'])
 
-      rc.off('ASM_DATA', noop)
+      rc.removeProductHandler('ASM_DATA')
 
       expect(rc.scheduler.stop).to.not.have.been.called
 
-      rc.off('ASM_DD', noop)
+      rc.removeProductHandler('ASM_DD')
 
-      expect(rc.scheduler.stop).to.have.been.calledOnce
+      expect(rc.scheduler.stop).to.have.been.called
       expect(rc.state.client.products).to.be.empty
     })
   })
@@ -552,63 +553,101 @@ describe('RemoteConfigManager', () => {
   })
 
   describe('dispatch', () => {
-    beforeEach(() => {
-      sinon.stub(rc, 'emit')
-    })
+    it('should call registered handler for each config, catch errors, and update the state', (done) => {
+      const syncGoodNonAckHandler = sinon.spy()
+      const syncBadNonAckHandler = sinon.spy(() => { throw new Error('sync fn') })
+      const asyncGoodHandler = sinon.spy(async () => {})
+      const asyncBadHandler = sinon.spy(async () => { throw new Error('async fn') })
+      const syncGoodAckHandler = sinon.spy((action, conf, id, ack) => { ack() })
+      const syncBadAckHandler = sinon.spy((action, conf, id, ack) => { ack(new Error('sync ack fn')) })
+      const asyncGoodAckHandler = sinon.spy((action, conf, id, ack) => { setImmediate(ack) })
+      const asyncBadAckHandler = sinon.spy((action, conf, id, ack) => {
+        setImmediate(ack.bind(null, new Error('async ack fn')))
+      })
+      const unackHandler = sinon.spy((action, conf, id, ack) => {})
 
-    it('should call emit for each config, catch errors, and update the state', () => {
-      rc.emit.onFirstCall().returns(true)
-      rc.emit.onSecondCall().throws(new Error('Unable to apply config'))
-      rc.emit.onThirdCall().returns(true)
+      rc.setProductHandler('PRODUCT_0', syncGoodNonAckHandler)
+      rc.setProductHandler('PRODUCT_1', syncBadNonAckHandler)
+      rc.setProductHandler('PRODUCT_2', asyncGoodHandler)
+      rc.setProductHandler('PRODUCT_3', asyncBadHandler)
+      rc.setProductHandler('PRODUCT_4', syncGoodAckHandler)
+      rc.setProductHandler('PRODUCT_5', syncBadAckHandler)
+      rc.setProductHandler('PRODUCT_6', asyncGoodAckHandler)
+      rc.setProductHandler('PRODUCT_7', asyncBadAckHandler)
+      rc.setProductHandler('PRODUCT_8', unackHandler)
 
-      const list = [
-        {
-          id: 'asm_features',
-          path: 'datadog/42/ASM_FEATURES/confId/config',
-          product: 'ASM_FEATURES',
+      const list = []
+      for (let i = 0; i < 9; i++) {
+        list[i] = {
+          id: `id_${i}`,
+          path: `datadog/42/PRODUCT_${i}/confId/config`,
+          product: `PRODUCT_${i}`,
           apply_state: UNACKNOWLEDGED,
           apply_error: '',
-          file: { asm: { enabled: true } }
-        },
-        {
-          id: 'asm_data',
-          path: 'datadog/42/ASM_DATA/confId/config',
-          product: 'ASM_DATA',
-          apply_state: UNACKNOWLEDGED,
-          apply_error: '',
-          file: { data: [1, 2, 3] }
-        },
-        {
-          id: 'asm_dd',
-          path: 'datadog/42/ASM_DD/confId/config',
-          product: 'ASM_DD',
-          apply_state: UNACKNOWLEDGED,
-          apply_error: '',
-          file: { rules: [4, 5, 6] }
+          file: { index: i }
         }
-      ]
+      }
 
       rc.dispatch(list, 'apply')
 
-      expect(rc.emit).to.have.been.calledThrice
-      expect(rc.emit.firstCall).to.have.been
-        .calledWithExactly('ASM_FEATURES', 'apply', { asm: { enabled: true } }, 'asm_features')
-      expect(rc.emit.secondCall).to.have.been.calledWithExactly('ASM_DATA', 'apply', { data: [1, 2, 3] }, 'asm_data')
-      expect(rc.emit.thirdCall).to.have.been.calledWithExactly('ASM_DD', 'apply', { rules: [4, 5, 6] }, 'asm_dd')
+      expect(syncGoodNonAckHandler).to.have.been.calledOnceWithExactly('apply', list[0].file, list[0].id)
+      expect(syncBadNonAckHandler).to.have.been.calledOnceWithExactly('apply', list[1].file, list[1].id)
+      expect(asyncGoodHandler).to.have.been.calledOnceWithExactly('apply', list[2].file, list[2].id)
+      expect(asyncBadHandler).to.have.been.calledOnceWithExactly('apply', list[3].file, list[3].id)
+      assertAsyncHandlerCallArguments(syncGoodAckHandler, 'apply', list[4].file, list[4].id)
+      assertAsyncHandlerCallArguments(syncBadAckHandler, 'apply', list[5].file, list[5].id)
+      assertAsyncHandlerCallArguments(asyncGoodAckHandler, 'apply', list[6].file, list[6].id)
+      assertAsyncHandlerCallArguments(asyncBadAckHandler, 'apply', list[7].file, list[7].id)
+      assertAsyncHandlerCallArguments(unackHandler, 'apply', list[8].file, list[8].id)
 
       expect(list[0].apply_state).to.equal(ACKNOWLEDGED)
       expect(list[0].apply_error).to.equal('')
       expect(list[1].apply_state).to.equal(ERROR)
-      expect(list[1].apply_error).to.equal('Error: Unable to apply config')
-      expect(list[2].apply_state).to.equal(ACKNOWLEDGED)
+      expect(list[1].apply_error).to.equal('Error: sync fn')
+      expect(list[2].apply_state).to.equal(UNACKNOWLEDGED)
       expect(list[2].apply_error).to.equal('')
+      expect(list[3].apply_state).to.equal(UNACKNOWLEDGED)
+      expect(list[3].apply_error).to.equal('')
+      expect(list[4].apply_state).to.equal(ACKNOWLEDGED)
+      expect(list[4].apply_error).to.equal('')
+      expect(list[5].apply_state).to.equal(ERROR)
+      expect(list[5].apply_error).to.equal('Error: sync ack fn')
+      expect(list[6].apply_state).to.equal(UNACKNOWLEDGED)
+      expect(list[6].apply_error).to.equal('')
+      expect(list[7].apply_state).to.equal(UNACKNOWLEDGED)
+      expect(list[7].apply_error).to.equal('')
+      expect(list[8].apply_state).to.equal(UNACKNOWLEDGED)
+      expect(list[8].apply_error).to.equal('')
 
-      expect(rc.appliedConfigs.get('datadog/42/ASM_FEATURES/confId/config')).to.equal(list[0])
-      expect(rc.appliedConfigs.get('datadog/42/ASM_DATA/confId/config')).to.equal(list[1])
-      expect(rc.appliedConfigs.get('datadog/42/ASM_DD/confId/config')).to.equal(list[2])
+      for (let i = 0; i < list.length; i++) {
+        expect(rc.appliedConfigs.get(`datadog/42/PRODUCT_${i}/confId/config`)).to.equal(list[i])
+      }
+
+      setImmediate(() => {
+        expect(list[2].apply_state).to.equal(ACKNOWLEDGED)
+        expect(list[2].apply_error).to.equal('')
+        expect(list[3].apply_state).to.equal(ERROR)
+        expect(list[3].apply_error).to.equal('Error: async fn')
+        expect(list[6].apply_state).to.equal(ACKNOWLEDGED)
+        expect(list[6].apply_error).to.equal('')
+        expect(list[7].apply_state).to.equal(ERROR)
+        expect(list[7].apply_error).to.equal('Error: async ack fn')
+        expect(list[8].apply_state).to.equal(UNACKNOWLEDGED)
+        expect(list[8].apply_error).to.equal('')
+        done()
+      })
+
+      function assertAsyncHandlerCallArguments (handler, ...expectedArgs) {
+        expect(handler).to.have.been.calledOnceWith(...expectedArgs)
+        expect(handler.args[0].length).to.equal(expectedArgs.length + 1)
+        expect(handler.args[0][handler.args[0].length - 1]).to.be.a('function')
+      }
     })
 
     it('should delete config from state when action is unapply', () => {
+      const handler = sinon.spy()
+      rc.setProductHandler('ASM_FEATURES', handler)
+
       rc.appliedConfigs.set('datadog/42/ASM_FEATURES/confId/config', {
         id: 'asm_data',
         path: 'datadog/42/ASM_FEATURES/confId/config',
@@ -620,8 +659,7 @@ describe('RemoteConfigManager', () => {
 
       rc.dispatch([rc.appliedConfigs.get('datadog/42/ASM_FEATURES/confId/config')], 'unapply')
 
-      expect(rc.emit).to.have.been
-        .calledOnceWithExactly('ASM_FEATURES', 'unapply', { asm: { enabled: true } }, 'asm_data')
+      expect(handler).to.have.been.calledOnceWithExactly('unapply', { asm: { enabled: true } }, 'asm_data')
       expect(rc.appliedConfigs).to.be.empty
     })
   })
