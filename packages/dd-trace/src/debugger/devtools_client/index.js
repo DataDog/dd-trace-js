2309aa3aec92c6386c50c2913a621e9104050a11
Thomas Watson <w@tson.dk>
2024-09-09 17:27:56 +0200

    Researched TODO items
    - If there's no `Debugger.paused` listener, the main thread will be
      forever paused when a breakpoint is it, since there's no code to
      resume it. No action needed, as it's not going to be possible without
      code changes that will be caught by CI.
    - If there worker thread crashes while paused at a breakpoint, the main
      thread continues. No action needed, as this is what we want.

--- packages/dd-trace/src/debugger/devtools_client/index.js
+++ packages/dd-trace/src/debugger/devtools_client/index.js
@@ -8,8 +8,6 @@ const { ackEmitting } = require('./status')
 require('./remote_config')
 const log = require('../../log')
 
-// TODO: What happens if there's no Debugger.paused listener? Will the main thread still pause?
-// TODO: What happens if the worker thread crashes before Debugger.paused? Will the main thread resume?
 session.on('Debugger.paused', async ({ params }) => {
   const start = process.hrtime.bigint()
   const timestamp = Date.now()
