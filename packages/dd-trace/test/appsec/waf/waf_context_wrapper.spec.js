22ea5bad6e056e190ab00844dc888aa955462583
Ugaitz Urien <ugaitz.urien@datadoghq.com>
2024-09-10 17:15:19 +0200

    Filter addresses before calling to waf to prevent running without knownAddresses (#4656)

--- packages/dd-trace/test/appsec/waf/waf_context_wrapper.spec.js
+++ packages/dd-trace/test/appsec/waf/waf_context_wrapper.spec.js
@@ -5,11 +5,16 @@ const WAFContextWrapper = require('../../../src/appsec/waf/waf_context_wrapper')
 const addresses = require('../../../src/appsec/addresses')
 
 describe('WAFContextWrapper', () => {
+  const knownAddresses = new Set([
+    addresses.HTTP_INCOMING_QUERY,
+    addresses.HTTP_INCOMING_GRAPHQL_RESOLVER
+  ])
+
   it('Should send HTTP_INCOMING_QUERY only once', () => {
     const ddwafContext = {
       run: sinon.stub()
     }
-    const wafContextWrapper = new WAFContextWrapper(ddwafContext, 1000, '1.14.0', '1.8.0')
+    const wafContextWrapper = new WAFContextWrapper(ddwafContext, 1000, '1.14.0', '1.8.0', knownAddresses)
 
     const payload = {
       persistent: {
@@ -27,7 +32,7 @@ describe('WAFContextWrapper', () => {
     const ddwafContext = {
       run: sinon.stub()
     }
-    const wafContextWrapper = new WAFContextWrapper(ddwafContext, 1000, '1.14.0', '1.8.0')
+    const wafContextWrapper = new WAFContextWrapper(ddwafContext, 1000, '1.14.0', '1.8.0', knownAddresses)
 
     const payload = {
       persistent: {
@@ -52,6 +57,26 @@ describe('WAFContextWrapper', () => {
     }, 1000)
   })
 
+  it('Should ignore run without known addresses', () => {
+    const ddwafContext = {
+      run: sinon.stub()
+    }
+    const wafContextWrapper = new WAFContextWrapper(ddwafContext, 1000, '1.14.0', '1.8.0', knownAddresses)
+
+    const payload = {
+      persistent: {
+        'persistent-unknown-address': { key: 'value' }
+      },
+      ephemeral: {
+        'ephemeral-unknown-address': { key: 'value' }
+      }
+    }
+
+    wafContextWrapper.run(payload)
+
+    expect(ddwafContext.run).to.have.not.been.called
+  })
+
   describe('Disposal context check', () => {
     let log
     let ddwafContext
@@ -70,7 +95,7 @@ describe('WAFContextWrapper', () => {
         '../../log': log
       })
 
-      wafContextWrapper = new ProxiedWafContextWrapper(ddwafContext, 1000, '1.14.0', '1.8.0')
+      wafContextWrapper = new ProxiedWafContextWrapper(ddwafContext, 1000, '1.14.0', '1.8.0', knownAddresses)
     })
 
     afterEach(() => {
