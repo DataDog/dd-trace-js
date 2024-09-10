1bb44eb4b8eaeb8a496c1ac37a9d2509560cc297
Thomas Watson <w@tson.dk>
2024-09-10 16:19:20 +0200

    Add code comment to clearify intent

--- packages/dd-trace/src/debugger/devtools_client/index.js
+++ packages/dd-trace/src/debugger/devtools_client/index.js
@@ -18,6 +18,8 @@ session.on('Debugger.paused', async ({ params }) => {
   log.debug(`Finished processing breakpoints - main thread paused for: ${Number(diff) / 1000000} ms`)
 
   const logger = {
+    // We can safely use `location.file` from the first probe in the array, since all probes hit by `hitBreakpoints`
+    // must exist in the same file since the debugger can only pause the main thread in one location.
     name: probes[0].location.file, // name of the class/type/file emitting the snapshot
     method: params.callFrames[0].functionName, // name of the method/function emitting the snapshot
     version: 2, // version of the snapshot format (not currently used or enforced)
