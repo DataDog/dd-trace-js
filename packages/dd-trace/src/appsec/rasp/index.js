047029846d7de19ff9fb47f69b7cf74b7e584332
Ugaitz Urien <ugaitz.urien@datadoghq.com>
2024-09-11 13:57:41 +0200

    Exploit prevention SQLi in pg (#4566)
    ---------
    
    Co-authored-by: simon-id <simon.id@datadoghq.com>

---
+++ packages/dd-trace/src/appsec/rasp/index.js
@@ -0,0 +1,103 @@
+'use strict'
+
+const web = require('../../plugins/util/web')
+const { setUncaughtExceptionCaptureCallbackStart } = require('../channels')
+const { block } = require('../blocking')
+const ssrf = require('./ssrf')
+const sqli = require('./sql_injection')
+
+const { DatadogRaspAbortError } = require('./utils')
+
+function removeAllListeners (emitter, event) {
+  const listeners = emitter.listeners(event)
+  emitter.removeAllListeners(event)
+
+  let cleaned = false
+  return function () {
+    if (cleaned === true) {
+      return
+    }
+    cleaned = true
+
+    for (let i = 0; i < listeners.length; ++i) {
+      emitter.on(event, listeners[i])
+    }
+  }
+}
+
+function findDatadogRaspAbortError (err, deep = 10) {
+  if (err instanceof DatadogRaspAbortError) {
+    return err
+  }
+
+  if (err.cause && deep > 0) {
+    return findDatadogRaspAbortError(err.cause, deep - 1)
+  }
+}
+
+function handleUncaughtExceptionMonitor (err) {
+  const abortError = findDatadogRaspAbortError(err)
+  if (!abortError) return
+
+  const { req, res, blockingAction } = abortError
+  block(req, res, web.root(req), null, blockingAction)
+
+  if (!process.hasUncaughtExceptionCaptureCallback()) {
+    const cleanUp = removeAllListeners(process, 'uncaughtException')
+    const handler = () => {
+      process.removeListener('uncaughtException', handler)
+    }
+
+    setTimeout(() => {
+      process.removeListener('uncaughtException', handler)
+      cleanUp()
+    })
+
+    process.on('uncaughtException', handler)
+  } else {
+    // uncaughtException event is not executed when hasUncaughtExceptionCaptureCallback is true
+    let previousCb
+    const cb = ({ currentCallback, abortController }) => {
+      setUncaughtExceptionCaptureCallbackStart.unsubscribe(cb)
+      if (!currentCallback) {
+        abortController.abort()
+        return
+      }
+
+      previousCb = currentCallback
+    }
+
+    setUncaughtExceptionCaptureCallbackStart.subscribe(cb)
+
+    process.setUncaughtExceptionCaptureCallback(null)
+
+    // For some reason, previous callback was defined before the instrumentation
+    // We can not restore it, so we let the app decide
+    if (previousCb) {
+      process.setUncaughtExceptionCaptureCallback(() => {
+        process.setUncaughtExceptionCaptureCallback(null)
+        process.setUncaughtExceptionCaptureCallback(previousCb)
+      })
+    }
+  }
+}
+
+function enable (config) {
+  ssrf.enable(config)
+  sqli.enable(config)
+
+  process.on('uncaughtExceptionMonitor', handleUncaughtExceptionMonitor)
+}
+
+function disable () {
+  ssrf.disable()
+  sqli.disable()
+
+  process.off('uncaughtExceptionMonitor', handleUncaughtExceptionMonitor)
+}
+
+module.exports = {
+  enable,
+  disable,
+  handleUncaughtExceptionMonitor // exported only for testing purpose
+}
