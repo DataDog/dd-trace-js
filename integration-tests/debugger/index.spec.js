3e2f0d9592ddcfe8d11f9cf40b3b1746c5c39470
Thomas Watson <w@tson.dk>
2024-09-12 12:08:51 +0200

    Guard against race condition in tests

--- integration-tests/debugger/index.spec.js
+++ integration-tests/debugger/index.spec.js
@@ -407,6 +407,7 @@ describe('Dynamic Instrumentation', function () {
           agent.addRemoteConfig(probeConfig2)
         } else {
           // Second INSTALLED payload: Perform an HTTP request to see if we successfully handled the race condition.
+          let finished = false
 
           // If the race condition occurred, the debugger will have been detached from the main thread and the new
           // probe will never trigger. If that's the case, the following timer will fire:
@@ -418,11 +419,17 @@ describe('Dynamic Instrumentation', function () {
           // following event listener will be called:
           agent.once('debugger-input', () => {
             clearTimeout(timer)
+            finished = true
             done()
           })
 
           // Perform HTTP request to try and trigger the probe
-          axios.get('/foo').catch(done)
+          axios.get('/foo').catch((err) => {
+            // If the request hasn't fully completed by the time the tests ends and the target app is destroyed, Axios
+            // will complain with a "socket hang up" error. Hence this sanity check before calling `done(err)`. If we
+            // later add more tests below this one, this shouuldn't be an issue.
+            if (!finished) done(err)
+          })
         }
       })
 
