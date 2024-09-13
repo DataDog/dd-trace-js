b7655ec5622d0d454ebee3cec1adbb513592404e
Thomas Watson <w@tson.dk>
2024-09-13 16:30:55 +0200

    Prepare support for Dynamic Instrumentation (#4492)
    Prepare the Node.js tracer to be used with the Dynamic Instrumentation product
    (DI).
    
    Probes are added as breakpoints using the V8 Inspector Protocol. The inspector
    runs in a worker thread and will pause the main thread temporarily while
    gathering the required information related to the probe (as of this commit no
    information is gathered, but this is a required step once we start to gather
    the local state snapshot).
    
    DI features included in this commit:
    - Support for line based log probes
    - Support for loading probes via Remote Configuration
    - Support for sending probe status to the debugger-backend
    - Support for sending log data to debugger-backend
    
    Please note, that with this commit it's still not possible to use DI with the
    Node.js tracer as support for using the Node.js tracer with DI has not yet been
    enabled in the Datadog UI.

---
+++ packages/dd-trace/src/debugger/devtools_client/index.js
@@ -0,0 +1,57 @@
+'use strict'
+
+const { randomUUID } = require('crypto')
+const { breakpoints } = require('./state')
+const session = require('./session')
+const send = require('./send')
+const { ackEmitting } = require('./status')
+const { parentThreadId } = require('./config')
+const log = require('../../log')
+const { version } = require('../../../../../package.json')
+
+require('./remote_config')
+
+// There doesn't seem to be an official standard for the content of these fields, so we're just populating them with
+// something that should be useful to a Node.js developer.
+const threadId = parentThreadId === 0 ? `pid:${process.pid}` : `pid:${process.pid};tid:${parentThreadId}`
+const threadName = parentThreadId === 0 ? 'MainThread' : `WorkerThread:${parentThreadId}`
+
+session.on('Debugger.paused', async ({ params }) => {
+  const start = process.hrtime.bigint()
+  const timestamp = Date.now()
+  const probes = params.hitBreakpoints.map((id) => breakpoints.get(id))
+  await session.post('Debugger.resume')
+  const diff = process.hrtime.bigint() - start // TODO: Should this be recored as telemetry?
+
+  log.debug(`Finished processing breakpoints - main thread paused for: ${Number(diff) / 1000000} ms`)
+
+  const logger = {
+    // We can safely use `location.file` from the first probe in the array, since all probes hit by `hitBreakpoints`
+    // must exist in the same file since the debugger can only pause the main thread in one location.
+    name: probes[0].location.file, // name of the class/type/file emitting the snapshot
+    method: params.callFrames[0].functionName, // name of the method/function emitting the snapshot
+    version,
+    thread_id: threadId,
+    thread_name: threadName
+  }
+
+  // TODO: Send multiple probes in one HTTP request as an array
+  for (const probe of probes) {
+    const snapshot = {
+      id: randomUUID(),
+      timestamp,
+      probe: {
+        id: probe.id,
+        version: probe.version,
+        location: probe.location
+      },
+      language: 'javascript'
+    }
+
+    // TODO: Process template
+    send(probe.template, logger, snapshot, (err) => {
+      if (err) log.error(err)
+      else ackEmitting(probe)
+    })
+  }
+})
