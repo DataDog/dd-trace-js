ca711692c959757c2698ed7d001300006af17583
Juan Antonio Fernández de Alba <juan.fernandezdealba@datadoghq.com>
2024-09-16 11:15:34 +0200

    [test visibility] Add test source file to test suites (#4677)

--- packages/datadog-plugin-mocha/src/index.js
+++ packages/datadog-plugin-mocha/src/index.js
@@ -129,6 +129,19 @@ class MochaPlugin extends CiPlugin {
       if (this.testSessionName) {
         testSuiteMetadata[TEST_SESSION_NAME] = this.testSessionName
       }
+      if (this.repositoryRoot !== this.sourceRoot && !!this.repositoryRoot) {
+        testSuiteMetadata[TEST_SOURCE_FILE] = getTestSuitePath(testSuiteAbsolutePath, this.repositoryRoot)
+      } else {
+        testSuiteMetadata[TEST_SOURCE_FILE] = testSuite
+      }
+      if (testSuiteMetadata[TEST_SOURCE_FILE]) {
+        testSuiteMetadata[TEST_SOURCE_START] = 1
+      }
+
+      const codeOwners = this.getCodeOwners(testSuiteMetadata)
+      if (codeOwners) {
+        testSuiteMetadata[TEST_CODE_OWNERS] = codeOwners
+      }
 
       const testSuiteSpan = this.tracer.startSpan('mocha.test_suite', {
         childOf: this.testModuleSpan,
