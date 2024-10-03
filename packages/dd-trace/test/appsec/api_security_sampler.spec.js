c1e0f45ec5019552bd31fc37d61a6bc9fdef9551
ishabi <ilyasshabi94@gmail.com>
2024-10-03 16:39:29 +0200

    use priority simpler to get span priority

--- packages/dd-trace/test/appsec/api_security_sampler.spec.js
+++ packages/dd-trace/test/appsec/api_security_sampler.spec.js
@@ -3,73 +3,68 @@
 const proxyquire = require('proxyquire')
 
 describe('API Security Sampler', () => {
-  let apiSecuritySampler, webStub, clock
+  let apiSecuritySampler, webStub, clock, sampler, span
 
   beforeEach(() => {
     webStub = { root: sinon.stub() }
     clock = sinon.useFakeTimers(Date.now())
 
-    apiSecuritySampler = proxyquire('../../src/appsec/api_security_sampler', {
-      '../plugins/util/web': webStub
+    sampler = sinon.stub().returns({
+      isSampled: sinon.stub()
     })
+
+    apiSecuritySampler = proxyquire('../../src/appsec/api_security_sampler', {
+      '../plugins/util/web': webStub,
+      '../priority_sampler': sampler
+    })
+
+    apiSecuritySampler.configure({ apiSecurity: { enabled: true, sampleDelay: 30 } })
+
+    span = {
+      context: sinon.stub().returns({})
+    }
   })
 
   afterEach(() => {
     clock.restore()
   })
 
-  describe('sampleRequest', () => {
-    beforeEach(() => {
-      apiSecuritySampler.configure({ apiSecurity: { enabled: true, sampleDelay: 30 } })
-    })
-
-    it('should return false if not enabled', () => {
-      apiSecuritySampler.disable()
-      expect(apiSecuritySampler.sampleRequest({}, {})).to.be.false
-    })
-
-    it('should return false if no root span', () => {
-      webStub.root.returns(null)
-      expect(apiSecuritySampler.sampleRequest({}, {})).to.be.false
-    })
-
-    it('should return true and put request in cache if priority is AUTO_KEEP', () => {
-      const rootSpan = { context: () => ({ _sampling: { priority: 2 } }) }
-      webStub.root.returns(rootSpan)
-      const req = { url: '/test', method: 'GET' }
-      const res = { statusCode: 200 }
-      expect(apiSecuritySampler.sampleRequest(req, res)).to.be.true
-    })
-
-    it('should return true and put request in cache if priority is USER_KEEP', () => {
-      const rootSpan = { context: () => ({ _sampling: { priority: 1 } }) }
-      webStub.root.returns(rootSpan)
-      const req = { url: '/test', method: 'GET' }
-      const res = { statusCode: 200 }
-      expect(apiSecuritySampler.sampleRequest(req, res)).to.be.true
-    })
-
-    it('should return false if priority is neither AUTO_KEEP nor USER_KEEP', () => {
-      const rootSpan = { context: () => ({ _sampling: { priority: 0 } }) }
-      webStub.root.returns(rootSpan)
-      expect(apiSecuritySampler.sampleRequest({}, {})).to.be.false
-    })
+  it('should return false if not enabled', () => {
+    apiSecuritySampler.disable()
+    expect(apiSecuritySampler.sampleRequest({}, {})).to.be.false
   })
 
-  describe('disable', () => {
-    it('should set enabled to false and clear the cache', () => {
-      const req = { url: '/test', method: 'GET' }
-      const res = { statusCode: 200 }
+  it('should return false if no root span', () => {
+    webStub.root.returns(null)
+    expect(apiSecuritySampler.sampleRequest({}, {})).to.be.false
+  })
 
-      const rootSpan = { context: () => ({ _sampling: { priority: 2 } }) }
-      webStub.root.returns(rootSpan)
+  it('should return true and put request in cache if priority is AUTO_KEEP or USER_KEEP', () => {
+    webStub.root.returns(span)
+    sampler().isSampled.returns(true)
+    const req = { url: '/test', method: 'GET' }
+    const res = { statusCode: 200 }
+    expect(apiSecuritySampler.sampleRequest(req, res)).to.be.true
+  })
 
-      apiSecuritySampler.configure({ apiSecurity: { enabled: true, sampleDelay: 30 } })
-      expect(apiSecuritySampler.sampleRequest(req, res)).to.be.true
+  it('should return false if priority is neither AUTO_KEEP nor USER_KEEP', () => {
+    webStub.root.returns(span)
+    sampler().isSampled.returns(false)
+    expect(apiSecuritySampler.sampleRequest({}, {})).to.be.false
+  })
 
-      apiSecuritySampler.disable()
+  it('should set enabled to false and clear the cache', () => {
+    const req = { url: '/test', method: 'GET' }
+    const res = { statusCode: 200 }
 
-      expect(apiSecuritySampler.sampleRequest({}, {})).to.be.false
-    })
+    webStub.root.returns(span)
+    sampler().isSampled.returns(true)
+
+    apiSecuritySampler.configure({ apiSecurity: { enabled: true, sampleDelay: 30 } })
+    expect(apiSecuritySampler.sampleRequest(req, res)).to.be.true
+
+    apiSecuritySampler.disable()
+
+    expect(apiSecuritySampler.sampleRequest({}, {})).to.be.false
   })
 })
