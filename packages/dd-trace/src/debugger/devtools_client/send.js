1c24413427d75334e485d20a555dde5ca519be61
Thomas Watson <w@tson.dk>
2024-10-03 13:14:40 +0200

    [DI] Add GitHub repo and SHA tags to probe results (#4751)

--- packages/dd-trace/src/debugger/devtools_client/send.js
+++ packages/dd-trace/src/debugger/devtools_client/send.js
@@ -1,18 +1,28 @@
 'use strict'
 
+const { stringify } = require('querystring')
+
 const config = require('./config')
 const request = require('../../exporters/common/request')
+const { GIT_COMMIT_SHA, GIT_REPOSITORY_URL } = require('../../plugins/util/tags')
 
 module.exports = send
 
 const ddsource = 'dd_debugger'
 const service = config.service
 
+const ddtags = [
+  [GIT_COMMIT_SHA, config.commitSHA],
+  [GIT_REPOSITORY_URL, config.repositoryUrl]
+].map((pair) => pair.join(':')).join(',')
+
+const path = `/debugger/v1/input?${stringify({ ddtags })}`
+
 function send (message, logger, snapshot, cb) {
   const opts = {
     method: 'POST',
     url: config.url,
-    path: '/debugger/v1/input',
+    path,
     headers: { 'Content-Type': 'application/json; charset=utf-8' }
   }
 
