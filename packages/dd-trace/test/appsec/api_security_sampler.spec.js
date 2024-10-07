93ba729918db6d656e4d082236d237216f330037
ishabi <ilyasshabi94@gmail.com>
2024-10-07 09:24:31 +0200

    use lru cache package

--- packages/dd-trace/test/appsec/api_security_sampler.spec.js
+++ packages/dd-trace/test/appsec/api_security_sampler.spec.js
@@ -1,13 +1,17 @@
 'use strict'
 
+const { performance } = require('node:perf_hooks')
 const proxyquire = require('proxyquire')
 
 describe('API Security Sampler', () => {
-  let apiSecuritySampler, webStub, clock, sampler, span
+  const req = { url: '/test', method: 'GET' }
+  const res = { statusCode: 200 }
+  let apiSecuritySampler, performanceNowStub, webStub, sampler, span
 
   beforeEach(() => {
+    performanceNowStub = sinon.stub(performance, 'now').returns(0)
+
     webStub = { root: sinon.stub() }
-    clock = sinon.useFakeTimers(Date.now())
 
     sampler = sinon.stub().returns({
       isSampled: sinon.stub()
@@ -18,15 +22,21 @@ describe('API Security Sampler', () => {
       '../priority_sampler': sampler
     })
 
-    apiSecuritySampler.configure({ apiSecurity: { enabled: true, sampleDelay: 30 } })
+    apiSecuritySampler.configure({ apiSecurity: { enabled: true } })
 
     span = {
       context: sinon.stub().returns({})
     }
+
+    webStub.root.returns(span)
+    sampler().isSampled.returns(true)
+
+    performanceNowStub.returns(performance.now() + 1)
   })
 
   afterEach(() => {
-    clock.restore()
+    performanceNowStub.restore()
+    apiSecuritySampler.disable()
   })
 
   it('should return false if not enabled', () => {
@@ -40,31 +50,45 @@ describe('API Security Sampler', () => {
   })
 
   it('should return true and put request in cache if priority is AUTO_KEEP or USER_KEEP', () => {
-    webStub.root.returns(span)
-    sampler().isSampled.returns(true)
-    const req = { url: '/test', method: 'GET' }
-    const res = { statusCode: 200 }
+    expect(apiSecuritySampler.sampleRequest(req, res)).to.be.true
+  })
+
+  it('should not sample before 30 seconds', () => {
+    expect(apiSecuritySampler.sampleRequest(req, res)).to.be.true
+    performanceNowStub.returns(performance.now() + 25000)
+
+    expect(apiSecuritySampler.sampleRequest(req, res)).to.be.false
+    expect(apiSecuritySampler.isSampled(req, res)).to.be.true
+  })
+
+  it('should sample after 30 seconds', () => {
+    expect(apiSecuritySampler.sampleRequest(req, res)).to.be.true
+
+    performanceNowStub.returns(performance.now() + 35000)
+
     expect(apiSecuritySampler.sampleRequest(req, res)).to.be.true
   })
 
   it('should return false if priority is neither AUTO_KEEP nor USER_KEEP', () => {
-    webStub.root.returns(span)
     sampler().isSampled.returns(false)
     expect(apiSecuritySampler.sampleRequest({}, {})).to.be.false
   })
 
+  it('should remove oldest entry when max size is exceeded', () => {
+    const method = req.method
+    for (let i = 0; i < 4097; i++) {
+      expect(apiSecuritySampler.sampleRequest({ method, url: `/test${i}` }, res)).to.be.true
+    }
+
+    expect(apiSecuritySampler.isSampled({ method, url: '/test0' }, res)).to.be.false
+    expect(apiSecuritySampler.isSampled({ method, url: '/test4096' }, res)).to.be.true
+  })
+
   it('should set enabled to false and clear the cache', () => {
-    const req = { url: '/test', method: 'GET' }
-    const res = { statusCode: 200 }
-
-    webStub.root.returns(span)
-    sampler().isSampled.returns(true)
-
-    apiSecuritySampler.configure({ apiSecurity: { enabled: true, sampleDelay: 30 } })
     expect(apiSecuritySampler.sampleRequest(req, res)).to.be.true
 
     apiSecuritySampler.disable()
 
-    expect(apiSecuritySampler.sampleRequest({}, {})).to.be.false
+    expect(apiSecuritySampler.sampleRequest(req, res)).to.be.false
   })
 })
