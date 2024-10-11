02cec69be08ead6df245d11e246a59ae492e720a
Thomas Watson <w@tson.dk>
2024-10-11 13:06:01 +0200

    Remove un-used file

--- packages/datadog-plugin-fastify/test/suite.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-// const suiteTest = require('../../dd-trace/test/plugins/suite')
-
-// suiteTest({
-//   modName: 'fastify',
-//   repoUrl: 'fastify/fastify',
-//   commitish: 'latest',
-//   testCmd: 'node_modules/.bin/tap -J test/*.test.js test/*/*.test.js --no-coverage --no-check-coverage'
-// })
