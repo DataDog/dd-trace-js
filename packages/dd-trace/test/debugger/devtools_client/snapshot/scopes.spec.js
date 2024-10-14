aad5957cf288c9ce3786e999b3fe5a45dbc588bf
Thomas Watson <w@tson.dk>
2024-10-14 22:53:22 +0200

    [DI] Refactor unit tests (#4777)
    Split inspected code into multiple files. This makes adding new tests
    easier, as changes related to one inspected file doesn't influence
    unrelated tests.

---
+++ packages/dd-trace/test/debugger/devtools_client/snapshot/scopes.spec.js
@@ -0,0 +1,29 @@
+'use strict'
+
+require('../../../setup/mocha')
+
+const { getTargetCodePath, enable, teardown, assertOnBreakpoint, setAndTriggerBreakpoint } = require('./utils')
+
+const target = getTargetCodePath(__filename)
+
+describe('debugger -> devtools client -> snapshot.getLocalStateForCallFrame', function () {
+  beforeEach(enable(__filename))
+
+  afterEach(teardown)
+
+  describe('scopes', function () {
+    it('should capture expected scopes', function (done) {
+      assertOnBreakpoint(done, (state) => {
+        expect(Object.entries(state).length).to.equal(5)
+
+        expect(state).to.have.deep.property('a1', { type: 'number', value: '1' })
+        expect(state).to.have.deep.property('a2', { type: 'number', value: '2' })
+        expect(state).to.have.deep.property('total', { type: 'number', value: '0' })
+        expect(state).to.have.deep.property('i', { type: 'number', value: '0' })
+        expect(state).to.have.deep.property('inc', { type: 'number', value: '2' })
+      })
+
+      setAndTriggerBreakpoint(target, 13)
+    })
+  })
+})
