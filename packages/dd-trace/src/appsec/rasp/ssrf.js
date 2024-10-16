554237c8042d64cd38da827de8450a9626652211
Igor Unanua <igor.unanua@datadoghq.com>
2024-10-16 17:47:03 +0200

    Handle request options

--- packages/dd-trace/src/appsec/rasp/ssrf.js
+++ packages/dd-trace/src/appsec/rasp/ssrf.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const url = require('url')
 const { httpClientRequestStart } = require('../channels')
 const { storage } = require('../../../../datadog-core')
 const addresses = require('../addresses')
@@ -20,12 +21,12 @@ function disable () {
 function analyzeSsrf (ctx) {
   const store = storage.getStore()
   const req = store?.req
-  const url = ctx.args.uri
+  const uri = (ctx.args.options?.uri && url.format(ctx.args.options?.uri)) ?? ctx.args.uri
 
-  if (!req || !url) return
+  if (!req || !uri) return
 
   const persistent = {
-    [addresses.HTTP_OUTGOING_URL]: url
+    [addresses.HTTP_OUTGOING_URL]: uri
   }
 
   const result = waf.run({ persistent }, req, RULE_TYPES.SSRF)
