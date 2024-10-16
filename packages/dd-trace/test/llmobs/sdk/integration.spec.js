ad28001c99a45344b7272ae5fe60c9aefd6ea786
Sam Brenner <sam.brenner@datadoghq.com>
2024-10-15 20:15:12 -0400

    fix eval metric integration test

--- packages/dd-trace/test/llmobs/sdk/integration.spec.js
+++ packages/dd-trace/test/llmobs/sdk/integration.spec.js
@@ -42,7 +42,8 @@ describe('end to end sdk integration tests', () => {
     tracer = require('../../../../dd-trace')
     tracer.init({
       llmobs: {
-        mlApp: 'test'
+        mlApp: 'test',
+        apiKey: 'test'
       }
     })
 
