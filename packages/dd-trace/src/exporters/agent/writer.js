d13c897ca1ca9675ef7336abde7fc134ae271041
wu-json <jason.wu2153@gmail.com>
2024-10-19 11:48:06 -0700

    feat: remove override

--- packages/dd-trace/src/exporters/agent/writer.js
+++ packages/dd-trace/src/exporters/agent/writer.js
@@ -59,14 +59,6 @@ class Writer extends BaseWriter {
       done()
     })
   }
-
-  _flush (done) {
-    const count = this._encoder.count()
-    if (count > 0) {
-      const payload = this._encoder.makePayload()
-      this._sendPayload(payload, count, done)
-    }
-  }
 }
 
 function setHeader (headers, key, value) {
