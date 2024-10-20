1581ea610e8d86f71fe577a09585c2c665cb4217
wu-json <jason.wu2153@gmail.com>
2024-10-19 21:30:04 -0700

    test: ts

--- docs/test.ts
+++ docs/test.ts
@@ -439,6 +439,9 @@ tracer.wrap('x', () => {
   const rumData: string = tracer.getRumData();
 })
 
+tracer.flush()
+tracer.flush(() => 'test')
+
 const result: Tracer = tracer.setUser({ id: '123' })
 
 const user: User = {
