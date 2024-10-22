60418fa60f4a387939909940d0529f2935f75350
ludu12 <dunscombe1@gmail.com>
2024-10-22 15:56:27 -0500

    Uncommenting IT test

--- packages/datadog-plugin-hono/test/integration-test/server.mjs
+++ packages/datadog-plugin-hono/test/integration-test/server.mjs
@@ -1,19 +1,17 @@
-// import 'dd-trace/init.js'
+import 'dd-trace/init.js'
 import {Hono} from 'hono';
 import {serve} from '@hono/node-server';
 
-console.log("HERE")
-process.send({ port: 1})
-// const app = new Hono()
-//
-// app.get('/', (c) => {
-//   return c.text('hello, world\n')
-// })
-//
-//
-// serve({
-//   fetch: app.fetch,
-// }, (i) => {
-//   const port = i.port;
-//   process.send({ port })
-// });
+const app = new Hono()
+
+app.get('/', (c) => {
+  return c.text('hello, world\n')
+})
+
+
+serve({
+  fetch: app.fetch,
+}, (i) => {
+  const port = i.port;
+  process.send({ port })
+});
