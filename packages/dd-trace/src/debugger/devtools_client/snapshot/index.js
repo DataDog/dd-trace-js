8b45dbb19d7ffc537b01331260fbdf7b05a06ac8
Thomas Watson <w@tson.dk>
2024-10-22 15:54:55 +0200

    [DI] Adhere to maxCollectionSize limit in snapshots (#4780)
    The maxCollectionSize limit affects the following types:
    - Array
    - Map / WeakMap
    - Set / WeakSet
    - All TypedArray types
    
    This limit contols the maximum about of elements collected for those
    types. The default is 100.

--- packages/dd-trace/src/debugger/devtools_client/snapshot/index.js
+++ packages/dd-trace/src/debugger/devtools_client/snapshot/index.js
@@ -4,6 +4,7 @@ const { getRuntimeObject } = require('./collector')
 const { processRawState } = require('./processor')
 
 const DEFAULT_MAX_REFERENCE_DEPTH = 3
+const DEFAULT_MAX_COLLECTION_SIZE = 100
 const DEFAULT_MAX_LENGTH = 255
 
 module.exports = {
@@ -12,14 +13,18 @@ module.exports = {
 
 async function getLocalStateForCallFrame (
   callFrame,
-  { maxReferenceDepth = DEFAULT_MAX_REFERENCE_DEPTH, maxLength = DEFAULT_MAX_LENGTH } = {}
+  {
+    maxReferenceDepth = DEFAULT_MAX_REFERENCE_DEPTH,
+    maxCollectionSize = DEFAULT_MAX_COLLECTION_SIZE,
+    maxLength = DEFAULT_MAX_LENGTH
+  } = {}
 ) {
   const rawState = []
   let processedState = null
 
   for (const scope of callFrame.scopeChain) {
     if (scope.type === 'global') continue // The global scope is too noisy
-    rawState.push(...await getRuntimeObject(scope.object.objectId, maxReferenceDepth))
+    rawState.push(...await getRuntimeObject(scope.object.objectId, { maxReferenceDepth, maxCollectionSize }))
   }
 
   // Deplay calling `processRawState` so the caller gets a chance to resume the main thread before processing `rawState`
---
+++ packages/dd-trace/test/debugger/devtools_client/snapshot/target-code/max-collection-size.js
@@ -0,0 +1,27 @@
+'use stict'
+
+function run () {
+  const arr = []
+  const map = new Map()
+  const set = new Set()
+  const wmap = new WeakMap()
+  const wset = new WeakSet()
+  const typedArray = new Uint16Array(new ArrayBuffer(2000))
+
+  // 1000 is larger the default maxCollectionSize of 100
+  for (let i = 1; i <= 1000; i++) {
+    // A reference that can be used in WeakMap/WeakSet to avoid GC
+    const obj = { i }
+
+    arr.push(i)
+    map.set(i, obj)
+    set.add(i)
+    wmap.set(obj, i)
+    wset.add(obj)
+    typedArray[i - 1] = i
+  }
+
+  return 'my return value' // breakpoint at this line
+}
+
+module.exports = { run }
