d7706c987af861326df709b0d6df67a712ea2a73
rochdev <roch.devost@gmail.com>
2024-11-12 16:07:27 -0500

    replace const and let with var

--- init.js
+++ init.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 
-const NODE_MAJOR = require('./version').NODE_MAJOR
+var NODE_MAJOR = require('./version').NODE_MAJOR
 
 // We use several things that are not supported by older versions of Node:
 // - AsyncLocalStorage
@@ -10,30 +10,30 @@ const NODE_MAJOR = require('./version').NODE_MAJOR
 // and probably others.
 // TODO: Remove all these dependencies so that we can report telemetry.
 if (NODE_MAJOR >= 12) {
-  const path = require('path')
-  const Module = require('module')
-  const semver = require('semver')
-  const log = require('./packages/dd-trace/src/log')
-  const isTrue = require('./packages/dd-trace/src/util').isTrue
-  const telemetry = require('./packages/dd-trace/src/telemetry/init-telemetry')
+  var path = require('path')
+  var Module = require('module')
+  var semver = require('semver')
+  var log = require('./packages/dd-trace/src/log')
+  var isTrue = require('./packages/dd-trace/src/util').isTrue
+  var telemetry = require('./packages/dd-trace/src/telemetry/init-telemetry')
 
-  let initBailout = false
-  let clobberBailout = false
-  const forced = isTrue(process.env.DD_INJECT_FORCE)
+  var initBailout = false
+  var clobberBailout = false
+  var forced = isTrue(process.env.DD_INJECT_FORCE)
 
   if (process.env.DD_INJECTION_ENABLED) {
     // If we're running via single-step install, and we're not in the app's
     // node_modules, then we should not initialize the tracer. This prevents
     // single-step-installed tracer from clobbering the manually-installed tracer.
-    let resolvedInApp
-    const entrypoint = process.argv[1]
+    var resolvedInApp
+    var entrypoint = process.argv[1]
     try {
       resolvedInApp = Module.createRequire(entrypoint).resolve('dd-trace')
     } catch (e) {
       // Ignore. If we can't resolve the module, we assume it's not in the app.
     }
     if (resolvedInApp) {
-      const ourselves = path.join(__dirname, 'index.js')
+      var ourselves = path.join(__dirname, 'index.js')
       if (ourselves !== resolvedInApp) {
         clobberBailout = true
       }
@@ -42,8 +42,8 @@ if (NODE_MAJOR >= 12) {
     // If we're running via single-step install, and the runtime doesn't match
     // the engines field in package.json, then we should not initialize the tracer.
     if (!clobberBailout) {
-      const engines = require('./package.json').engines
-      const version = process.versions.node
+      var engines = require('./package.json').engines
+      var version = process.versions.node
       if (!semver.satisfies(version, engines.node)) {
         initBailout = true
         telemetry([
@@ -60,7 +60,7 @@ if (NODE_MAJOR >= 12) {
   }
 
   if (!clobberBailout && (!initBailout || forced)) {
-    const tracer = require('.')
+    var tracer = require('.')
     tracer.init()
     module.exports = tracer
     telemetry('complete', [`injection_forced:${forced && initBailout ? 'true' : 'false'}`])
