799b21ff31fab9897c43dd7543b7fba06cb849cf
Nicholas Hulston <nicholashulston@gmail.com>
2024-11-18 16:37:59 -0500

    [serverless] Add S3 Span Pointers (#4875)
    * Add span pointer info on S3 `putObject`, `copyObject`, and `completeMultipartUpload` requests.
    
    * Unit tests
    
    * small improvement
    
    * Create `addSpanPointer()` so we don't have to export a context with 0s for trace+span id; add debug logs
    
    * Add integration test for completeMultipartUpload; update unit test
    
    * Rename to `addSpanPointers()`
    
    * Update comments and make getting eTag more reliable
    
    * Validate parameters before calling `generateS3PointerHash`
    
    * add unit tests
    
    * Rename var to `SPAN_LINK_POINTER_KIND`; standardize the hashing function.
    
    * Set the span link kind in the `addSpanPointer()` functions so that downstream callers don't have to worry about passing it.
    
    * Move constants to constants.js; move `generatePointerHash` to util.js

--- packages/dd-trace/test/opentelemetry/span.spec.js
+++ packages/dd-trace/test/opentelemetry/span.spec.js
@@ -325,6 +325,33 @@ describe('OTel Span', () => {
     expect(_links).to.have.lengthOf(2)
   })
 
+  it('should add span pointers', () => {
+    const span = makeSpan('name')
+    const { _links } = span._ddSpan
+
+    span.addSpanPointer('pointer_kind', 'd', 'abc123')
+    expect(_links).to.have.lengthOf(1)
+    expect(_links[0].attributes).to.deep.equal({
+      'ptr.kind': 'pointer_kind',
+      'ptr.dir': 'd',
+      'ptr.hash': 'abc123',
+      'link.kind': 'span-pointer'
+    })
+    expect(_links[0].context.toTraceId()).to.equal('0')
+    expect(_links[0].context.toSpanId()).to.equal('0')
+
+    span.addSpanPointer('another_kind', 'd', '1234567')
+    expect(_links).to.have.lengthOf(2)
+    expect(_links[1].attributes).to.deep.equal({
+      'ptr.kind': 'another_kind',
+      'ptr.dir': 'd',
+      'ptr.hash': '1234567',
+      'link.kind': 'span-pointer'
+    })
+    expect(_links[1].context.toTraceId()).to.equal('0')
+    expect(_links[1].context.toSpanId()).to.equal('0')
+  })
+
   it('should set status', () => {
     const unset = makeSpan('name')
     const unsetCtx = unset._ddSpan.context()
