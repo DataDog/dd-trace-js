91a59dbd08ef7cfc6d182493f9b0fc84d9c613b3
simon-id <simon.id@datadoghq.com>
2024-11-19 13:29:04 +0100

    remove some comments

--- packages/dd-trace/src/appsec/sdk/set_user.js
+++ packages/dd-trace/src/appsec/sdk/set_user.js
@@ -21,18 +21,7 @@ function setUser (tracer, user) {
     return
   }
 
-  // must get user ID with USER_ID_FIELDS
-
-  // _dd.appsec.user.collection_mode: collectionMode // sdk/ident/anon
-
   setUserTags(user, rootSpan)
-
-  /*
-    User IDs generated through the SDK must now be provided to libddwaf as a persistent addresses.
-    If the user monitoring SDK has already resulted in a call to libddwaf before any automated instrumentation or collection method has been executed, no extra call should be made.
-    If the automated instrumentation or collection method has resulted in a call to libddwaf before the user monitoring SDK has been executed, a second call must be performed with the user ID obtained through the SDK.
-  */
-  // will the second call trigger tho ? make some edge case tests
 }
 
 module.exports = {
