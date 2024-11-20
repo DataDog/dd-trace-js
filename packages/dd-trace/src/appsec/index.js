806ab237623fdb9f5cbc21fc78ef24197f859da5
simon-id <simon.id@datadoghq.com>
2024-11-20 12:11:53 +0100

    add framework name and waf handleResults() to onPassportVerify()

--- packages/dd-trace/src/appsec/index.js
+++ packages/dd-trace/src/appsec/index.js
@@ -180,7 +180,7 @@ function incomingHttpEndTranslator ({ req, res }) {
   Reporter.finishRequest(req, res)
 }
 
-function onPassportVerify ({ login, user, success, abortController }) {
+function onPassportVerify ({ framework, login, user, success, abortController }) {
   const store = storage.getStore()
   const rootSpan = store?.req && web.root(store.req)
 
@@ -189,7 +189,9 @@ function onPassportVerify ({ login, user, success, abortController }) {
     return
   }
 
-  UserTracking.trackLogin(login, user, success, rootSpan, abortController)
+  const results = UserTracking.trackLogin(framework, login, user, success, rootSpan)
+
+  handleResults(results, store.req, store.req.res, rootSpan, abortController)
 }
 
 function onRequestQueryParsed ({ req, res, query, abortController }) {
