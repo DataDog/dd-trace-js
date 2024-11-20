178c7b65a37976af906f97f6e6ff31f5bd9fdff5
Matthew Li <matthew.li@datadoghq.com>
2024-11-20 18:54:46 -0500

    correcting field name

--- packages/dd-trace/src/plugins/util/web.js
+++ packages/dd-trace/src/plugins/util/web.js
@@ -267,7 +267,7 @@ const web = {
       }
     }
 
-    const span = tracer.startSpan(name, { childOf, links: childOf?.links })
+    const span = tracer.startSpan(name, { childOf, links: childOf?._links })
 
     return span
   },
