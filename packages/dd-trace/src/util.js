fdd65c6c4a3eb75db2ebf4e84b6212c8028023f0
Ida.Liu <ida.liu@datadoghq.com>
2024-12-05 13:29:17 -0500

    make sampling rule matching case insensitive

--- packages/dd-trace/src/util.js
+++ packages/dd-trace/src/util.js
@@ -25,6 +25,8 @@ function isError (value) {
 
 // Matches a glob pattern to a given subject string
 function globMatch (pattern, subject) {
+  pattern = pattern.toLowerCase()
+  subject = subject.toLowerCase()
   let px = 0 // [p]attern inde[x]
   let sx = 0 // [s]ubject inde[x]
   let nextPx = 0
