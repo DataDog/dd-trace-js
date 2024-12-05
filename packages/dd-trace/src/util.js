6df78c784a8ff262ba508f59c24b643f106c7c90
Ida.Liu <ida.liu@datadoghq.com>
2024-12-05 14:47:14 -0500

    handle edge case

--- packages/dd-trace/src/util.js
+++ packages/dd-trace/src/util.js
@@ -25,8 +25,8 @@ function isError (value) {
 
 // Matches a glob pattern to a given subject string
 function globMatch (pattern, subject) {
-  if (pattern) pattern = pattern.toLowerCase()
-  if (subject) subject = subject.toLowerCase()
+  if (typeof pattern === 'string') pattern = pattern.toLowerCase()
+  if (typeof subject === 'string') subject = subject.toLowerCase()
   let px = 0 // [p]attern inde[x]
   let sx = 0 // [s]ubject inde[x]
   let nextPx = 0
