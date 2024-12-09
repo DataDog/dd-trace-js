cacf086c276c034ead2a3d88824c65118b7891ec
Ugaitz Urien <ugaitz.urien@datadoghq.com>
2024-12-09 10:40:07 +0100

    ESM support in iast

--- packages/dd-trace/src/appsec/iast/taint-tracking/rewriter.js
+++ packages/dd-trace/src/appsec/iast/taint-tracking/rewriter.js
@@ -1,18 +1,23 @@
 'use strict'
 
 const Module = require('module')
+const { pathToFileURL } = require('url')
+const { MessageChannel } = require('worker_threads')
 const shimmer = require('../../../../../datadog-shimmer')
 const { isPrivateModule, isNotLibraryFile } = require('./filter')
 const { csiMethods } = require('./csi-methods')
 const { getName } = require('../telemetry/verbosity')
-const { getRewriteFunction } = require('./rewriter-telemetry')
+const { getRewriteFunction, incrementTelemetryIfNeeded } = require('./rewriter-telemetry')
 const dc = require('dc-polyfill')
 const log = require('../../../log')
+const { isMainThread } = require('worker_threads')
+const { LOG, REWRITTEN } = require('./constants')
 
 const hardcodedSecretCh = dc.channel('datadog:secrets:result')
 let rewriter
-let getPrepareStackTrace
+let getPrepareStackTrace, cacheRewrittenSourceMap
 let kSymbolPrepareStackTrace
+let esmRewriterEnabled = false
 
 let getRewriterOriginalPathAndLineFromSourceMap = function (path, line, column) {
   return { path, line, column }
@@ -46,6 +51,7 @@ function getRewriter (telemetryVerbosity) {
       const Rewriter = iastRewriter.Rewriter
       getPrepareStackTrace = iastRewriter.getPrepareStackTrace
       kSymbolPrepareStackTrace = iastRewriter.kSymbolPrepareStackTrace
+      cacheRewrittenSourceMap = iastRewriter.cacheRewrittenSourceMap
 
       const chainSourceMap = isFlagPresent('--enable-source-maps')
       const getOriginalPathAndLineFromSourceMap = iastRewriter.getOriginalPathAndLineFromSourceMap
@@ -104,6 +110,24 @@ function getCompileMethodFn (compileMethod) {
   }
 }
 
+function esmRewritePostProcess (rewritten, filename) {
+  const { literalsResult, metrics } = rewritten
+
+  if (metrics?.status === 'modified') {
+    if (filename.startsWith('file://')) {
+      filename = filename.substring(7)
+    }
+
+    cacheRewrittenSourceMap(filename, rewritten.content)
+  }
+
+  incrementTelemetryIfNeeded(metrics)
+
+  if (literalsResult && hardcodedSecretCh.hasSubscribers) {
+    hardcodedSecretCh.publish(literalsResult)
+  }
+}
+
 function enableRewriter (telemetryVerbosity) {
   try {
     const rewriter = getRewriter(telemetryVerbosity)
@@ -114,8 +138,66 @@ function enableRewriter (telemetryVerbosity) {
       }
       shimmer.wrap(Module.prototype, '_compile', compileMethod => getCompileMethodFn(compileMethod))
     }
+
+    enableEsmRewriter(telemetryVerbosity)
   } catch (e) {
     log.error('[ASM] Error enabling TaintTracking Rewriter', e)
+  }
+}
+
+function isEsmConfigured () {
+  const hasLoaderArg = !!process.execArgv?.find(
+    arg => arg.includes('--loader') || arg.includes('--experimental-loader')
+  )
+  if (hasLoaderArg) return true
+
+  const nodeOptions = process.env.NODE_OPTIONS
+  const hasLoaderNodeOptions = nodeOptions &&
+    (nodeOptions.includes('--loader') || nodeOptions.includes('--experimental-loader'))
+  if (hasLoaderNodeOptions) return true
+
+  const initializeLoaded = Object.keys(require.cache).find(file => file.includes('import-in-the-middle/hook.js'))
+  return !!initializeLoaded
+}
+
+function enableEsmRewriter (telemetryVerbosity) {
+  if (isMainThread && Module.register && !esmRewriterEnabled) {
+    esmRewriterEnabled = true
+    const { port1, port2 } = new MessageChannel()
+    port1.on('message', (message) => {
+      const { type, data } = message
+      switch (type) {
+        case LOG:
+          log[data.level]?.(...data.messages)
+          break
+
+        case REWRITTEN:
+          esmRewritePostProcess(data.rewritten, data.url)
+          break
+      }
+    })
+    port1.unref()
+    port2.unref()
+
+    const chainSourceMap = isFlagPresent('--enable-source-maps')
+    const data = {
+      port: port2,
+      csiMethods,
+      telemetryVerbosity,
+      chainSourceMap
+    }
+
+    try {
+      if (isEsmConfigured()) {
+        Module.register('./rewriter-esm.mjs', {
+          parentURL: pathToFileURL(__filename),
+          transferList: [port2],
+          data
+        })
+      }
+    } catch (e) {
+      log.error('[ASM] Error enabling ESM Rewriter', e)
+    }
   }
 }
 
