3fbac51a58745f5ffe0ef4ba1f311fda9275a39b
Thomas Watson <w@tson.dk>
2024-12-16 09:22:19 +0100

    Rename Queue to JSONBuffer

--- packages/dd-trace/src/debugger/devtools_client/json-buffer.js
+++ packages/dd-trace/src/debugger/devtools_client/json-buffer.js
@@ -1,6 +1,6 @@
 'use strict'
 
-class JSONQueue {
+class JSONBuffer {
   constructor ({ size, timeout, onFlush }) {
     this._maxSize = size
     this._timeout = timeout
@@ -20,17 +20,17 @@ class JSONQueue {
     this._onFlush(json)
   }
 
-  add (str, size = Buffer.byteLength(str)) {
+  write (str, size = Buffer.byteLength(str)) {
     if (this._timer === null) {
       this._partialJson = `[${str}`
       this._timer = setTimeout(() => this._flush(), this._timeout)
     } else if (Buffer.byteLength(this._partialJson) + size + 2 > this._maxSize) {
       this._flush()
-      this.add(str, size)
+      this.write(str, size)
     } else {
       this._partialJson += `,${str}`
     }
   }
 }
 
-module.exports = JSONQueue
+module.exports = JSONBuffer
