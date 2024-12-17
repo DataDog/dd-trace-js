421750473be6d3ce5b022b273896510e9404ea15
Ida.Liu <ida.liu@datadoghq.com>
2024-12-17 14:32:32 -0500

    make log.trace take callbacks

--- packages/dd-trace/src/priority_sampler.js
+++ packages/dd-trace/src/priority_sampler.js
@@ -51,13 +51,13 @@ class PrioritySampler {
     this._rules = this._normalizeRules(rules, sampleRate, rateLimit, provenance)
     this._limiter = new RateLimiter(rateLimit)
 
-    log.trace(`PrioritySampler.configure called on options=${JSON.stringify(options)}`)
+    log.trace(() => `PrioritySampler.configure called on options=${JSON.stringify(options)}`)
     setSamplingRules(this._rules)
   }
 
   isSampled (span) {
     const priority = this._getPriorityFromAuto(span)
-    log.trace(`PrioritySampler.isSampled called on span ${span.toString()} at ${priority} priority`)
+    log.trace(() => `PrioritySampler.isSampled called on span ${span.toString()} at ${priority} priority`)
     return priority === USER_KEEP || priority === AUTO_KEEP
   }
 
@@ -71,7 +71,7 @@ class PrioritySampler {
     if (context._sampling.priority !== undefined) return
     if (!root) return // noop span
 
-    log.trace(`PrioritySampler.sample called on span=${span.toString()} with auto=${auto}`)
+    log.trace(() => `PrioritySampler.sample called on span=${span.toString()} with auto=${auto}`)
 
     const tag = this._getPriorityFromTags(context._tags, context)
 
@@ -101,7 +101,7 @@ class PrioritySampler {
 
     this._samplers = samplers
 
-    log.trace(`PrioritySampler.update called on rates=${JSON.stringify(rates)}`)
+    log.trace(() => `PrioritySampler.update called on rates=${JSON.stringify(rates)}`)
   }
 
   validate (samplingPriority) {
@@ -126,7 +126,7 @@ class PrioritySampler {
 
     const root = context._trace.started[0]
 
-    log.trace(`PrioritySampler.setPriority called on span=${span} with samplingPriority=${samplingPriority} and mechanism=${mechanism}`)
+    log.trace(() => `PrioritySampler.setPriority called on span=${span} with samplingPriority=${samplingPriority} and mechanism=${mechanism}`)
     this._addDecisionMaker(root)
   }
 
