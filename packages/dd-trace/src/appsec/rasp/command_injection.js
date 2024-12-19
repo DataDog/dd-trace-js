02f9b1f979759da0c1fcc9edd504f663c31ad355
ishabi <ilyasshabi94@gmail.com>
2024-12-19 11:22:52 +0100

    Revert "fix cmdi params"
    This reverts commit 4a3d76657d7ced365268c60d5146e86833eafb19.

--- packages/dd-trace/src/appsec/rasp/command_injection.js
+++ packages/dd-trace/src/appsec/rasp/command_injection.js
@@ -33,13 +33,14 @@ function analyzeCommandInjection ({ file, fileArgs, shell, abortController }) {
 
   const persistent = {}
   const raspRule = { type: RULE_TYPES.COMMAND_INJECTION }
-  const params = fileArgs ? [file, ...fileArgs] : [file]
+  const params = fileArgs ? [file, ...fileArgs] : file
 
   if (shell) {
     persistent[addresses.SHELL_COMMAND] = params
     raspRule.variant = 'shell'
   } else {
-    persistent[addresses.EXEC_COMMAND] = params
+    const commandParams = Array.isArray(params) ? params : [params]
+    persistent[addresses.EXEC_COMMAND] = commandParams
     raspRule.variant = 'exec'
   }
 
