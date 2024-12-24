6220304af052a2864a0e42d339f89b41aeb6d635
yahya-mouman <yahya.mouman@datadoghq.com>
2024-12-24 18:04:06 +0100

    camelCase and lint

--- packages/datadog-plugin-aws-sdk/src/services/bedrockruntime.js
+++ packages/datadog-plugin-aws-sdk/src/services/bedrockruntime.js
@@ -11,7 +11,7 @@ const PROVIDER = {
   META: 'META',
   STABILITY: 'STABILITY',
   MISTRAL: 'MISTRAL'
-};
+}
 
 const enabledOperations = ['invokeModel']
 
@@ -52,7 +52,7 @@ class BedrockRuntime extends BaseAwsSdkPlugin {
 }
 
 class Generation {
-  constructor({ message = '', finish_reason = '', choice_id = '' } = {}) {
+  constructor ({ message = '', finishReason = '', choiceId = '' } = {}) {
     // stringify message as it could be a single generated message as well as a list of embeddings
     this.message = typeof message === 'string' ? message : JSON.stringify(message) || ''
     this.finishReason = finishReason || ''
@@ -61,27 +61,37 @@ class Generation {
 }
 
 class RequestParams {
-  constructor({ prompt = '', temperature = undefined, top_p = undefined, max_tokens = undefined, stop_sequences = [], input_type = '', truncate = '', stream = '', n = undefined } = {}) {
+  constructor ({
+    prompt = '',
+    temperature = undefined,
+    topP = undefined,
+    maxTokens = undefined,
+    stopSequences = [],
+    inputType = '',
+    truncate = '',
+    stream = '',
+    n = undefined
+  } = {}) {
     // TODO: set a truncation limit to prompt
     // stringify prompt as it could be a single prompt as well as a list of message objects
     this.prompt = typeof prompt === 'string' ? prompt : JSON.stringify(prompt) || ''
     this.temperature = temperature !== undefined ? temperature : undefined
-    this.top_p = top_p !== undefined ? top_p : undefined
-    this.max_tokens = max_tokens !== undefined ? max_tokens : undefined
-    this.stop_sequences = stop_sequences || []
-    this.input_type = input_type || ''
+    this.topP = topP !== undefined ? topP : undefined
+    this.maxTokens = maxTokens !== undefined ? maxTokens : undefined
+    this.stopSequences = stopSequences || []
+    this.inputType = inputType || ''
     this.truncate = truncate || ''
     this.stream = stream || ''
     this.n = n !== undefined ? n : undefined
   }
 }
 
-function extractRequestParams(params, provider) {
+function extractRequestParams (params, provider) {
   const requestBody = JSON.parse(params.body)
   const modelId = params.modelId
 
   switch (provider) {
-    case PROVIDER.AI21:
+    case PROVIDER.AI21: {
       let userPrompt = requestBody.prompt
       if (modelId.includes('jamba')) {
         for (const message of requestBody.messages) {
@@ -90,29 +100,79 @@ function extractRequestParams(params, provider) {
           }
         }
       }
-      return new RequestParams({ prompt: userPrompt, temperature: requestBody.temperature, top_p: requestBody.top_p, max_tokens: requestBody.max_tokens, stop_sequences: requestBody.stop_sequences })
-    case PROVIDER.AMAZON:
+      return new RequestParams({
+        prompt: userPrompt,
+        temperature: requestBody.temperature,
+        topP: requestBody.top_p,
+        maxTokens: requestBody.max_tokens,
+        stopSequences: requestBody.stop_sequences
+      })
+    }
+    case PROVIDER.AMAZON: {
       if (modelId.includes('embed')) {
         return new RequestParams({ prompt: requestBody.inputText })
       }
       const textGenerationConfig = requestBody.textGenerationConfig || {}
-      return new RequestParams({ prompt: requestBody.inputText, temperature: textGenerationConfig.temperature, top_p: textGenerationConfig.topP, max_tokens: textGenerationConfig.maxTokenCount, stop_sequences: textGenerationConfig.stopSequences })
-    case PROVIDER.ANTHROPIC:
+      return new RequestParams({
+        prompt: requestBody.inputText,
+        temperature: textGenerationConfig.temperature,
+        topP: textGenerationConfig.topP,
+        maxTokens: textGenerationConfig.maxTokenCount,
+        stopSequences: textGenerationConfig.stopSequences
+      })
+    }
+    case PROVIDER.ANTHROPIC: {
       const prompt = requestBody.prompt || requestBody.messages
-      return new RequestParams({ prompt: prompt, temperature: requestBody.temperature, top_p: requestBody.top_p, max_tokens: requestBody.max_tokens_to_sample, stop_sequences: requestBody.stop_sequences })
-    case PROVIDER.COHERE:
+      return new RequestParams({
+        prompt,
+        temperature: requestBody.temperature,
+        topP: requestBody.top_p,
+        maxTokens: requestBody.max_tokens_to_sample,
+        stopSequences: requestBody.stop_sequences
+      })
+    }
+    case PROVIDER.COHERE: {
       if (modelId.includes('embed')) {
-        return new RequestParams({ prompt: requestBody.texts, input_type: requestBody.input_type, truncate: requestBody.truncate })
+        return new RequestParams({
+          prompt: requestBody.texts,
+          inputType: requestBody.input_type,
+          truncate: requestBody.truncate
+        })
       }
-      return new RequestParams({ prompt: requestBody.prompt, temperature: requestBody.temperature, top_p: requestBody.p, max_tokens: requestBody.max_tokens, stop_sequences: requestBody.stop_sequences, stream: requestBody.stream, n: requestBody.num_generations })
-    case PROVIDER.META:
-      return new RequestParams({ prompt: requestBody.prompt, temperature: requestBody.temperature, top_p: requestBody.top_p, max_tokens: requestBody.max_gen_len })
-    case PROVIDER.MISTRAL:
-      return new RequestParams({ prompt: requestBody.prompt, temperature: requestBody.temperature, top_p: requestBody.top_p, max_tokens: requestBody.max_tokens, stop_sequences: requestBody.stop, top_k: requestBody.top_k })
-    case PROVIDER.STABILITY:
+      return new RequestParams({
+        prompt: requestBody.prompt,
+        temperature: requestBody.temperature,
+        topP: requestBody.p,
+        maxTokens: requestBody.max_tokens,
+        stopSequences: requestBody.stop_sequences,
+        stream: requestBody.stream,
+        n: requestBody.num_generations
+      })
+    }
+    case PROVIDER.META: {
+      return new RequestParams({
+        prompt: requestBody.prompt,
+        temperature: requestBody.temperature,
+        topP: requestBody.top_p,
+        maxTokens: requestBody.max_gen_len
+      })
+    }
+    case PROVIDER.MISTRAL: {
+      return new RequestParams({
+        prompt: requestBody.prompt,
+        temperature: requestBody.temperature,
+        topP: requestBody.top_p,
+        maxTokens: requestBody.max_tokens,
+        stopSequences: requestBody.stop,
+        topK: requestBody.top_k
+      })
+    }
+    case PROVIDER.STABILITY: {
       return new RequestParams()
-    default:
+    }
+    default: {
       return new RequestParams()
+    }
   }
 }
 
@@ -121,33 +181,44 @@ function extractTextAndResponseReason (response, provider, modelName, shouldSetC
 
   try {
     switch (provider) {
-      case PROVIDER.AI21:
+      case PROVIDER.AI21: {
         if (modelName.includes('jamba')) {
           const generations = body.choices || []
           if (generations.length > 0) {
             const generation = generations[0]
-            return new Generation({ message: generation.message, finish_reason: generation.finish_reason, choice_id: shouldSetChoiceIds ? generation.id : undefined })
+            return new Generation({
+              message: generation.message,
+              finishReason: generation.finish_reason,
+              choiceId: shouldSetChoiceIds ? generation.id : undefined
+            })
           }
         }
         const completions = body.completions || []
         if (completions.length > 0) {
           const completion = completions[0]
-          return new Generation({ message: completion.data?.text, finish_reason: completion?.finishReason, choice_id: shouldSetChoiceIds ? completion?.id : undefined })
+          return new Generation({
+            message: completion.data?.text,
+            finishReason: completion?.finishReason,
+            choiceId: shouldSetChoiceIds ? completion?.id : undefined
+          })
         }
         return new Generation()
-      case PROVIDER.AMAZON:
+      }
+      case PROVIDER.AMAZON: {
         if (modelName.includes('embed')) {
           return new Generation({ message: body.embedding })
         }
         const results = body.results || []
         if (results.length > 0) {
           const result = results[0]
-          return new Generation({ message: result.outputText, finish_reason: result.completionReason })
+          return new Generation({ message: result.outputText, finishReason: result.completionReason })
         }
         break
-      case PROVIDER.ANTHROPIC:
-        return new Generation({ message: body.completion || body.content, finish_reason: body.stop_reason })
-      case PROVIDER.COHERE:
+      }
+      case PROVIDER.ANTHROPIC: {
+        return new Generation({ message: body.completion || body.content, finishReason: body.stop_reason })
+      }
+      case PROVIDER.COHERE: {
         if (modelName.includes('embed')) {
           const embeddings = body.embeddings || [[]]
           if (embeddings.length > 0) {
@@ -157,25 +228,34 @@ function extractTextAndResponseReason (response, provider, modelName, shouldSetC
         const generations = body.generations || []
         if (generations.length > 0) {
           const generation = generations[0]
-          return new Generation({ message: generation.text, finish_reason: generation.finish_reason, choice_id: shouldSetChoiceIds ? generation.id : undefined })
+          return new Generation({
+            message: generation.text,
+            finishReason: generation.finish_reason,
+            choiceId: shouldSetChoiceIds ? generation.id : undefined
+          })
         }
         break
-      case PROVIDER.META:
-        return new Generation({ message: body.generation, finish_reason: body.stop_reason })
-      case PROVIDER.MISTRAL:
+      }
+      case PROVIDER.META: {
+        return new Generation({ message: body.generation, finishReason: body.stop_reason })
+      }
+      case PROVIDER.MISTRAL: {
         const mistralGenerations = body.outputs || []
         if (mistralGenerations.length > 0) {
           const generation = mistralGenerations[0]
-          return new Generation({ message: generation.text, finish_reason: generation.stop_reason })
+          return new Generation({ message: generation.text, finishReason: generation.stop_reason })
         }
         break
-      case PROVIDER.STABILITY:
+      }
+      case PROVIDER.STABILITY: {
         return new Generation()
-      default:
+      }
+      default: {
         return new Generation()
+      }
     }
   } catch (error) {
-    log.warn('Unable to extract text/finish_reason from response body. Defaulting to empty text/finish_reason.')
+    log.warn('Unable to extract text/finishReason from response body. Defaulting to empty text/finishReason.')
     return new Generation()
   }
 
@@ -203,11 +283,11 @@ function buildTagsFromParams (requestParams, textAndResponseReason, modelProvide
   if (modelName.includes('embed')) {
     tags['aws.bedrock.response.embedding_length'] = textAndResponseReason.message.length
   }
-  if (textAndResponseReason.choice_id) {
-    tags['aws.bedrock.response.choices.id'] = textAndResponseReason.choice_id
+  if (textAndResponseReason.choiceId) {
+    tags['aws.bedrock.response.choices.id'] = textAndResponseReason.choiceId
   }
   tags['aws.bedrock.response.choices.text'] = textAndResponseReason.message
-  tags['aws.bedrock.response.choices.finish_reason'] = textAndResponseReason.finish_reason
+  tags['aws.bedrock.response.choices.finish_reason'] = textAndResponseReason.finishReason
 
   return tags
 }
