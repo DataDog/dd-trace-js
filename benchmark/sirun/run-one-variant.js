db268f1584a282aa94418ab947cf5eeb14f090b3
Thomas Watson <w@tson.dk>
2025-01-07 14:18:39 +0100

    Benchmarks: No need to guard against unhandled promise rejections (#5025)

--- benchmark/sirun/run-one-variant.js
+++ benchmark/sirun/run-one-variant.js
@@ -8,12 +8,4 @@ process.env.DD_INSTRUMENTATION_TELEMETRY_ENABLED = 'false'
 
 const env = Object.assign({}, process.env, { DD_TRACE_STARTUP_LOGS: 'false' })
 
-;(async () => {
-  try {
-    await exec('sirun', ['meta-temp.json'], { env, stdio: getStdio() })
-  } catch (e) {
-    setImmediate(() => {
-      throw e // Older Node versions don't fail on uncaught promise rejections.
-    })
-  }
-})()
+exec('sirun', ['meta-temp.json'], { env, stdio: getStdio() })
