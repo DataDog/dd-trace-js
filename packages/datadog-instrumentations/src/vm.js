2bfb6034c53bf7877d724e79cdda258673fda537
ishabi <ilyasshabi94@gmail.com>
2025-01-09 11:33:56 +0100

    simplify vm constructor instrumentation

--- packages/datadog-instrumentations/src/vm.js
+++ packages/datadog-instrumentations/src/vm.js
@@ -10,32 +10,27 @@ addHook({ name: names }, function (vm) {
   vm.Script = class extends vm.Script {
     constructor (code) {
       super(...arguments)
-      this.code = code
-    }
-  }
-
-  shimmer.wrap(vm.Script.prototype, 'runInContext', wrapVMMethod(1))
-  shimmer.wrap(vm.Script.prototype, 'runInNewContext', wrapVMMethod())
-  shimmer.wrap(vm.Script.prototype, 'runInThisContext', wrapVMMethod())
-
-  shimmer.wrap(vm, 'runInContext', wrapVMMethod())
-  shimmer.wrap(vm, 'runInNewContext', wrapVMMethod())
-  shimmer.wrap(vm, 'runInThisContext', wrapVMMethod())
-  shimmer.wrap(vm, 'compileFunction', wrapVMMethod())
-
-  return vm
-})
-
-function wrapVMMethod (codeIndex = 0) {
-  return function wrap (original) {
-    return function wrapped () {
-      const code = arguments[codeIndex] ? arguments[codeIndex] : this.code
 
       if (runScriptStartChannel.hasSubscribers && code) {
         runScriptStartChannel.publish({ code })
       }
-
-      return original.apply(this, arguments)
     }
+  }
+
+  shimmer.wrap(vm, 'runInContext', wrapVMMethod)
+  shimmer.wrap(vm, 'runInNewContext', wrapVMMethod)
+  shimmer.wrap(vm, 'runInThisContext', wrapVMMethod)
+  shimmer.wrap(vm, 'compileFunction', wrapVMMethod)
+
+  return vm
+})
+
+function wrapVMMethod (original) {
+  return function wrappedVMMethod (code) {
+    if (runScriptStartChannel.hasSubscribers && code) {
+      runScriptStartChannel.publish({ code })
+    }
+
+    return original.apply(this, arguments)
   }
 }
