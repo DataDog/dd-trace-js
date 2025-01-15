d64369124714ed3ae7dc498d93cedd2a5c2af43b
simon-id <simon.id@datadoghq.com>
2025-01-15 17:15:39 +0100

    cleanup

--- packages/datadog-instrumentations/src/passport.js
+++ packages/datadog-instrumentations/src/passport.js
@@ -3,20 +3,6 @@
 const shimmer = require('../../datadog-shimmer')
 const { channel, addHook } = require('./helpers/instrument')
 
-/* TODO: test with:
-passport-jwt JWTs
-  can be used both for login events, or as a session, that complicates things it think
-  maybe instrument this lib directly, and ofc only send the events after it was verified
-@nestjs/passport
-pasport-local
-passport-oauth2
-passport-google-oauth20
-passport-custom
-passport-http
-passport-http-bearer
-koa-passport
-*/
-
 const onPassportDeserializeUserChannel = channel('datadog:passport:deserializeUser:finish')
 
 function wrapDone (done) {
