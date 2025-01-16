460dbdc3682f4210fbafdc349a58a02db108ccb2
Igor Unanua <igor.unanua@datadoghq.com>
2025-01-16 21:23:10 +0100

    upgrade upload-artifact version and fix vitest (#5118)
    * Upgrade upload-artifact version
    
    * disable integration test for vitest v3 for now
    
    ---------
    
    Co-authored-by: Bryan English <bryan.english@datadoghq.com>

--- integration-tests/vitest/vitest.spec.js
+++ integration-tests/vitest/vitest.spec.js
@@ -35,7 +35,7 @@ const { DD_HOST_CPU_COUNT } = require('../../packages/dd-trace/src/plugins/util/
 
 const NUM_RETRIES_EFD = 3
 
-const versions = ['latest']
+const versions = ['2.1.8'] // was previously 'latest', but v3 breaks this test
 
 const linePctMatchRegex = /Lines\s+:\s+([\d.]+)%/
 
