1c8cbb4ee1d2c3532073e73922ae09abb6efcb31
yahya-mouman <103438582+yahya-mouman@users.noreply.github.com>
2025-01-24 17:13:39 +0100

    (chore)LLMObs: instrument bedrock runtime invoke model (#5116)
    * Update file structure
    
    * join spaces
    
    * add top_k
    
    * add index for plugins
    
    * add llmobs index
    
    * refactor apm tracing
    
    * add llmobs instrumentation for bedrock
    
    * add llmobs instrumentation for bedrock
    
    * rename
    
    * Update file structure
    
    * conflict
    
    * conflict
    
    * add index for plugins
    
    * add llmobs index
    
    * add llmobs instrumentation for bedrock
    
    * lint change
    
    * Update packages/datadog-plugin-aws-sdk/src/services/bedrockruntime/index.js
    
    Co-authored-by: Sam Brenner <106700075+sabrenner@users.noreply.github.com>
    
    * Update packages/datadog-plugin-aws-sdk/src/services/bedrockruntime/tracing.js
    
    Co-authored-by: Sam Brenner <106700075+sabrenner@users.noreply.github.com>
    
    * Update packages/datadog-plugin-aws-sdk/src/services/bedrockruntime/tracing.js
    
    Co-authored-by: Sam Brenner <106700075+sabrenner@users.noreply.github.com>
    
    * Update packages/datadog-plugin-aws-sdk/src/services/bedrockruntime/utils.js
    
    Co-authored-by: Sam Brenner <106700075+sabrenner@users.noreply.github.com>
    
    * CODEOWNERS
    
    * CODEOWNERS
    
    * remove shouldSetChoiceId override
    
    * remove shouldSetChoiceId override
    
    * remove shouldSetChoiceId override
    
    * remove shouldSetChoiceId override
    
    * lint
    
    * Update packages/dd-trace/src/llmobs/plugins/bedrockruntime.js
    
    Co-authored-by: Sam Brenner <106700075+sabrenner@users.noreply.github.com>
    
    * Update packages/dd-trace/test/llmobs/plugins/bedrockruntime/bedrockruntime.spec.js
    
    Co-authored-by: Sam Brenner <106700075+sabrenner@users.noreply.github.com>
    
    * resolving review comments
    
    * rename span
    
    * Add test fixtures and reduce redundant code
    
    * Add test to ci workflow
    
    * Update file structure
    
    * conflict
    
    * conflict
    
    * add index for plugins
    
    * add llmobs index
    
    * add llmobs instrumentation for bedrock
    
    * lint change
    
    * Update packages/dd-trace/src/llmobs/plugins/bedrockruntime.js
    
    Co-authored-by: Sam Brenner <106700075+sabrenner@users.noreply.github.com>
    
    * Update packages/dd-trace/test/llmobs/plugins/bedrockruntime/bedrockruntime.spec.js
    
    Co-authored-by: Sam Brenner <106700075+sabrenner@users.noreply.github.com>
    
    * resolving review comments
    
    * rename span
    
    * Add test fixtures and reduce redundant code
    
    * Add test to ci workflow
    
    * rename plugin name to aws sdk
    
    * update ci yml
    
    * add refactor for split
    
    ---------
    
    Co-authored-by: Sam Brenner <106700075+sabrenner@users.noreply.github.com>

---
+++ packages/dd-trace/test/llmobs/plugins/aws-sdk/bedrockruntime.spec.js
@@ -0,0 +1,117 @@
+'use strict'
+
+const agent = require('../../../plugins/agent')
+
+const nock = require('nock')
+const { expectedLLMObsLLMSpanEvent, deepEqualWithMockValues, MOCK_ANY } = require('../../util')
+const { models, modelConfig } = require('../../../../../datadog-plugin-aws-sdk/test/fixtures/bedrockruntime')
+const chai = require('chai')
+const LLMObsAgentProxySpanWriter = require('../../../../src/llmobs/writers/spans/agentProxy')
+
+chai.Assertion.addMethod('deepEqualWithMockValues', deepEqualWithMockValues)
+
+const serviceName = 'bedrock-service-name-test'
+
+describe('Plugin', () => {
+  describe('aws-sdk (bedrockruntime)', function () {
+    before(() => {
+      process.env.AWS_SECRET_ACCESS_KEY = '0000000000/00000000000000000000000000000'
+      process.env.AWS_ACCESS_KEY_ID = '00000000000000000000'
+    })
+
+    after(() => {
+      delete process.env.AWS_SECRET_ACCESS_KEY
+      delete process.env.AWS_ACCESS_KEY_ID
+    })
+
+    withVersions('aws-sdk', ['@aws-sdk/smithy-client', 'aws-sdk'], '>=3', (version, moduleName) => {
+      let AWS
+      let bedrockRuntimeClient
+
+      const bedrockRuntimeClientName =
+        moduleName === '@aws-sdk/smithy-client' ? '@aws-sdk/client-bedrock-runtime' : 'aws-sdk'
+
+      describe('with configuration', () => {
+        before(() => {
+          sinon.stub(LLMObsAgentProxySpanWriter.prototype, 'append')
+
+          // reduce errors related to too many listeners
+          process.removeAllListeners('beforeExit')
+          LLMObsAgentProxySpanWriter.prototype.append.reset()
+
+          return agent.load('aws-sdk', {}, {
+            llmobs: {
+              mlApp: 'test'
+            }
+          })
+        })
+
+        before(done => {
+          const requireVersion = version === '3.0.0' ? '3.422.0' : '>=3.422.0'
+          AWS = require(`../../../../../../versions/${bedrockRuntimeClientName}@${requireVersion}`).get()
+          bedrockRuntimeClient = new AWS.BedrockRuntimeClient(
+            { endpoint: 'http://127.0.0.1:4566', region: 'us-east-1', ServiceId: serviceName }
+          )
+          done()
+        })
+
+        afterEach(() => {
+          nock.cleanAll()
+          LLMObsAgentProxySpanWriter.prototype.append.reset()
+        })
+
+        after(() => {
+          sinon.restore()
+          return agent.close({ ritmReset: false, wipe: true })
+        })
+
+        models.forEach(model => {
+          it(`should invoke model for provider:${model.provider}`, done => {
+            const request = {
+              body: JSON.stringify(model.requestBody),
+              contentType: 'application/json',
+              accept: 'application/json',
+              modelId: model.modelId
+            }
+
+            const response = JSON.stringify(model.response)
+
+            nock('http://127.0.0.1:4566')
+              .post(`/model/${model.modelId}/invoke`)
+              .reply(200, response)
+
+            const command = new AWS.InvokeModelCommand(request)
+
+            agent.use(traces => {
+              const span = traces[0][0]
+              const spanEvent = LLMObsAgentProxySpanWriter.prototype.append.getCall(0).args[0]
+              const expected = expectedLLMObsLLMSpanEvent({
+                span,
+                spanKind: 'llm',
+                name: 'bedrock-runtime.command',
+                inputMessages: [
+                  { content: model.userPrompt }
+                ],
+                outputMessages: MOCK_ANY,
+                tokenMetrics: { input_tokens: 0, output_tokens: 0, total_tokens: 0 },
+                modelName: model.modelId.split('.')[1].toLowerCase(),
+                modelProvider: model.provider.toLowerCase(),
+                metadata: {
+                  temperature: modelConfig.temperature,
+                  max_tokens: modelConfig.maxTokens
+                },
+                tags: { ml_app: 'test', language: 'javascript' }
+              })
+
+              expect(spanEvent).to.deepEqualWithMockValues(expected)
+            }).then(done).catch(done)
+
+            bedrockRuntimeClient.send(command, (err) => {
+              if (err) return done(err)
+            })
+          })
+        })
+      })
+    })
+  })
+})
