a0cd2bced1c9fcbcfdce6aafef676249e5707915
Igor Unanua <igor.unanua@datadoghq.com>
2025-01-28 15:12:36 +0100

    check visited before iterating

--- packages/dd-trace/src/appsec/iast/utils.js
+++ packages/dd-trace/src/appsec/iast/utils.js
@@ -2,15 +2,18 @@
 
 function iterateObjectStrings (target, fn, levelKeys = [], depth = 20, visited = new Set()) {
   if (target !== null && typeof target === 'object') {
+    if (visited.has(target)) return
+
+    visited.add(target)
+
     Object.keys(target).forEach((key) => {
       const nextLevelKeys = [...levelKeys, key]
       const val = target[key]
 
       if (typeof val === 'string') {
         fn(val, nextLevelKeys, target, key)
-      } else if (depth > 0 && !visited.has(val)) {
+      } else if (depth > 0) {
         iterateObjectStrings(val, fn, nextLevelKeys, depth - 1, visited)
-        visited.add(val)
       }
     })
   }
