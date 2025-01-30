d43c70a2473d51423ee01d76e88bfd0ed3541569
Roch Devost <roch.devost@datadoghq.com>
2025-01-30 15:11:06 +0100

    replace msgpack-lite with official library for tests (#5173)
    * replace msgpack-lite with official library for tests
    
    * remove int64-buffer
    
    * fix serializing traces in tests
    
    * replace bigint with int instead of str
    
    * fix moleculer test
    
    * fix llmobs tests
    
    * fix langchain tests

--- packages/dd-trace/test/llmobs/util.js
+++ packages/dd-trace/test/llmobs/util.js
@@ -120,7 +120,7 @@ function expectedLLMObsBaseEvent ({
   const spanEvent = {
     trace_id: MOCK_STRING,
     span_id: spanId,
-    parent_id: parentId?.buffer ? fromBuffer(parentId) : (parentId || 'undefined'),
+    parent_id: typeof parentId === 'bigint' ? fromBuffer(parentId) : (parentId || 'undefined'),
     name: spanName,
     tags: expectedLLMObsTags({ span, tags, error, errorType, sessionId }),
     start_ns: startNs,
@@ -186,8 +186,7 @@ function expectedLLMObsTags ({
 }
 
 function fromBuffer (spanProperty, isNumber = false) {
-  const { buffer, offset } = spanProperty
-  const strVal = buffer.readBigInt64BE(offset).toString()
+  const strVal = spanProperty.toString(10)
   return isNumber ? Number(strVal) : strVal
 }
 
