e21456bc21c8275eb79577b5b59382e7b752b46f
simon-id <simon.id@datadoghq.com>
2025-02-03 09:47:31 +0100

    Automatic userID tracking and blocking (#4670)

--- packages/dd-trace/src/appsec/user_tracking.js
+++ packages/dd-trace/src/appsec/user_tracking.js
@@ -53,6 +53,8 @@ function obfuscateIfNeeded (str) {
 function getUserId (user) {
   if (!user) return
 
+  // should we iterate on user keys instead to be case insensitive ?
+  // but if we iterate over user then we're missing the inherited props ?
   for (const field of USER_ID_FIELDS) {
     let id = user[field]
 
@@ -72,11 +74,6 @@ function getUserId (user) {
 
 function trackLogin (framework, login, user, success, rootSpan) {
   if (!collectionMode || collectionMode === 'disabled') return
-
-  if (!rootSpan) {
-    log.error('[ASM] No rootSpan found in AppSec trackLogin')
-    return
-  }
 
   if (typeof login !== 'string') {
     log.error('[ASM] Invalid login provided to AppSec trackLogin')
@@ -162,7 +159,36 @@ function trackLogin (framework, login, user, success, rootSpan) {
   return waf.run({ persistent })
 }
 
+function trackUser (user, rootSpan) {
+  if (!collectionMode || collectionMode === 'disabled') return
+
+  const userId = getUserId(user)
+  if (!userId) {
+    log.error('[ASM] No valid user ID found in AppSec trackUser')
+    telemetry.incrementMissingUserIdMetric('passport', 'authenticated_request')
+    return
+  }
+
+  rootSpan.setTag('_dd.appsec.usr.id', userId)
+
+  const isSdkCalled = rootSpan.context()._tags['_dd.appsec.user.collection_mode'] === 'sdk'
+  // do not override SDK
+  if (!isSdkCalled) {
+    rootSpan.addTags({
+      'usr.id': userId,
+      '_dd.appsec.user.collection_mode': collectionMode
+    })
+
+    return waf.run({
+      persistent: {
+        [addresses.USER_ID]: userId
+      }
+    })
+  }
+}
+
 module.exports = {
   setCollectionMode,
-  trackLogin
+  trackLogin,
+  trackUser
 }
