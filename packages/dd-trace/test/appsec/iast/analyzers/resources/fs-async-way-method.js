88f949be8b0b63f10bd992c5290ed885a872ac79
Thomas Watson <w@tson.dk>
2025-02-06 09:19:06 +0100

    eslint --fix

--- packages/dd-trace/test/appsec/iast/analyzers/resources/fs-async-way-method.js
+++ packages/dd-trace/test/appsec/iast/analyzers/resources/fs-async-way-method.js
@@ -4,7 +4,6 @@ const fs = require('fs')
 
 module.exports = function (methodName, args, cb) {
   return new Promise((resolve, reject) => {
-    // eslint-disable-next-line n/handle-callback-err
     fs[methodName](...args, (err, res) => {
       resolve(cb(res))
     })
