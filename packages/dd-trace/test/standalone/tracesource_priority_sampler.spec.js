9493cd8213f08a4fbaaa1039600d9c935dd42a0c
Igor Unanua <igor.unanua@datadoghq.com>
2025-02-11 17:40:07 +0100

    increase coverage

--- packages/dd-trace/test/standalone/tracesource_priority_sampler.spec.js
+++ packages/dd-trace/test/standalone/tracesource_priority_sampler.spec.js
@@ -3,10 +3,12 @@
 require('../setup/tap')
 
 const { assert } = require('chai')
-const { USER_KEEP } = require('../../../../ext/priority')
+const proxyquire = require('proxyquire')
+const { USER_KEEP, AUTO_KEEP } = require('../../../../ext/priority')
 const DatadogSpan = require('../../src/opentracing/span')
 const TraceSourcePrioritySampler = require('../../src/standalone/tracesource_priority_sampler')
 const { TRACE_SOURCE_PROPAGATION_KEY } = require('../../src/constants')
+const { ASM } = require('../../src/standalone/product')
 
 describe('Disabled APM Tracing or Standalone - TraceSourcePrioritySampler', () => {
   let prioritySampler
@@ -63,6 +65,54 @@ describe('Disabled APM Tracing or Standalone - TraceSourcePrioritySampler', () =
       context._trace.tags[TRACE_SOURCE_PROPAGATION_KEY] = '02'
 
       assert.strictEqual(prioritySampler._getPriorityFromAuto(span), USER_KEEP)
+    })
+
+    it('should use rate limiter if it does not contain _dd.p.ts tag', () => {
+      const span = {
+        _trace: {}
+      }
+
+      sinon.stub(prioritySampler, '_isSampledByRateLimit').returns(true)
+
+      assert.strictEqual(prioritySampler._getPriorityFromAuto(span), AUTO_KEEP)
+    })
+  })
+
+  describe('setPriority', () => {
+    let prioritySampler
+    let setPriority
+    let addTraceSourceTag
+
+    beforeEach(() => {
+      setPriority = sinon.stub()
+      addTraceSourceTag = sinon.stub()
+
+      const PrioritySampler = class {
+        get setPriority () {
+          return setPriority
+        }
+      }
+      const TraceSourcePrioritySampler = proxyquire('../../src/standalone/tracesource_priority_sampler', {
+        '../priority_sampler': PrioritySampler,
+        './tracesource': {
+          addTraceSourceTag
+        }
+      })
+
+      prioritySampler = new TraceSourcePrioritySampler('test')
+
+      sinon.stub(prioritySampler, '_getContext').returns(context)
+    })
+
+    it('should add tracesource tag for the corresponding product', () => {
+      const span = {
+        _trace: {}
+      }
+
+      prioritySampler.setPriority(span, USER_KEEP, ASM)
+
+      sinon.assert.calledOnceWithExactly(setPriority, span, USER_KEEP, ASM)
+      sinon.assert.calledOnceWithExactly(addTraceSourceTag, context._trace.tags, ASM)
     })
   })
 })
