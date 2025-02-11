c89866a4b9da4f42887ca149890547bcb931288e
Thomas Watson <w@tson.dk>
2025-02-11 15:06:47 +0100

    Disable n/no-missing-require in benchmark folder

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -447,6 +447,15 @@ export default [
     files: TEST_FILES
   },
   {
+    name: 'dd-trace/benchmarks',
+    files: [
+      'benchmark/**/*'
+    ],
+    rules: {
+      'n/no-missing-require': 'off'
+    }
+  },
+  {
     name: 'dd-trace/tests/all',
     files: TEST_FILES,
     languageOptions: {
