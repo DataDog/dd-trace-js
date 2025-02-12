4029666dca1f64605de96300f36aea5bca39919e
rochdev <roch.devost@gmail.com>
2025-02-12 12:41:33 -0500

    lazy load runtime metrics only when needed

---
+++ packages/dd-trace/src/runtime_metrics/index.js
@@ -0,0 +1,39 @@
+'use strict'
+
+let runtimeMetrics
+
+const noop = runtimeMetrics = {
+  start () {},
+  stop () {},
+  track () {},
+  boolean () {},
+  histogram () {},
+  count () {},
+  gauge () {},
+  increment () {},
+  decrement () {}
+}
+
+module.exports = {
+  start (config) {
+    if (!config?.runtimeMetrics) return
+
+    runtimeMetrics = require('./runtime_metrics')
+    runtimeMetrics.start(config)
+  },
+
+  stop: () => {
+    if (runtimeMetrics === noop) return
+
+    runtimeMetrics.stop()
+    runtimeMetrics = noop
+  },
+
+  track: (...args) => runtimeMetrics.track(...args),
+  boolean: (...args) => runtimeMetrics.boolean(...args),
+  histogram: (...args) => runtimeMetrics.histogram(...args),
+  count: (...args) => runtimeMetrics.count(...args),
+  gauge: (...args) => runtimeMetrics.gauge(...args),
+  increment: (...args) => runtimeMetrics.increment(...args),
+  decrement: (...args) => runtimeMetrics.decrement(...args)
+}
