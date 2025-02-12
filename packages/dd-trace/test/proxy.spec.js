7119c5f32a1651a643be67172505fb69c85d602a
rochdev <roch.devost@gmail.com>
2025-02-12 15:04:52 -0500

    code cleanup

--- packages/dd-trace/test/proxy.spec.js
+++ packages/dd-trace/test/proxy.spec.js
@@ -539,7 +539,7 @@ describe('TracerProxy', () => {
           configure: sinon.stub()
         }
 
-        const options = { }
+        const options = {}
         const DatadogProxy = proxyquire('../src/proxy', {
           './tracer': DatadogTracer,
           './config': Config,
