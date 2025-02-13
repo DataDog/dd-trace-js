69e1674da69c03e59e2c5b5f002926895f123b79
rochdev <roch.devost@gmail.com>
2025-02-13 17:10:51 -0500

    only in serverless and not for undici

--- packages/datadog-instrumentations/src/fetch.js
+++ packages/datadog-instrumentations/src/fetch.js
@@ -1,12 +1,14 @@
 'use strict'
 
 const shimmer = require('../../datadog-shimmer')
-const { tracingChannel } = require('dc-polyfill')
+const { channel, tracingChannel } = require('dc-polyfill')
 const { createWrapFetch } = require('./helpers/fetch')
 
 if (globalThis.fetch) {
   const ch = tracingChannel('apm:fetch:request')
-  const wrapFetch = createWrapFetch(globalThis.Request, ch)
+  const wrapFetch = createWrapFetch(globalThis.Request, ch, () => {
+    channel('dd-trace:instrumentation:load').publish({ name: 'fetch' })
+  })
 
   globalThis.fetch = shimmer.wrapFunction(fetch, fetch => wrapFetch(fetch))
 }
