56bc64e5dfb88381437659f8b4f815375f82de36
rochdev <roch.devost@gmail.com>
2025-02-13 16:57:48 -0500

    only load fetch plugin when fetch is used

--- packages/datadog-instrumentations/src/helpers/fetch.js
+++ packages/datadog-instrumentations/src/helpers/fetch.js
@@ -1,10 +1,19 @@
 'use strict'
 
+const { channel } = require('dc-polyfill')
+
 exports.createWrapFetch = function createWrapFetch (Request, ch) {
+  let loaded = false
+
   return function wrapFetch (fetch) {
     if (typeof fetch !== 'function') return fetch
 
     return function (input, init) {
+      if (!loaded) {
+        channel('dd-trace:instrumentation:load').publish({ name: 'fetch' })
+        loaded = true
+      }
+
       if (!ch.start.hasSubscribers) return fetch.apply(this, arguments)
 
       if (input instanceof Request) {
