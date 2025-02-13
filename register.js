9be125443699df370b7ad8b099c10cbc0ab50868
Sam Brenner <sam.brenner@datadoghq.com>
2025-02-13 16:24:21 -0500

    fix for new openai version

--- register.js
+++ register.js
@@ -2,5 +2,5 @@ const { register } = require('node:module')
 const { pathToFileURL } = require('node:url')
 
 register('./loader-hook.mjs', pathToFileURL(__filename), {
-  data: { exclude: [/langsmith/, /openai\/_shims/] }
+  data: { exclude: [/langsmith/, /openai\/_shims/, /openai\/resources\/chat\/completions\/messages/] }
 })
