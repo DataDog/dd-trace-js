f89e712b1c9acf0f0a2d9cf602b8c75593d72188
Thomas Watson <w@tson.dk>
2025-02-14 12:48:56 +0100

    Use files property in package.json instead of .npmignore

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 'use strict'
 
-// TODO: It shouldn't be necessary to disable n/no-unpublished-require - Research
-// eslint-disable-next-line n/no-unpublished-require
 module.exports = require('./packages/dd-trace')
