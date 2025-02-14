0199b2ef1ac44a32a4b148dabe47fd155f9799f6
Thomas Watson <w@tson.dk>
2025-02-14 12:23:02 +0100

    Fix subproject warnings

--- integration-tests/ci-visibility/subproject/cypress.config.js
+++ integration-tests/ci-visibility/subproject/cypress.config.js
@@ -2,7 +2,7 @@ module.exports = {
   defaultCommandTimeout: 100,
   e2e: {
     setupNodeEvents (on, config) {
-      return require('dd-trace/ci/cypress/plugin')(on, config) // eslint-disable-line n/no-extraneous-require
+      return require('dd-trace/ci/cypress/plugin')(on, config)
     },
     specPattern: process.env.SPEC_PATTERN || 'cypress/e2e/**/*.cy.js'
   },
