0e46435edc9bc2be5d6331c52b2e63eee337c690
ishabi <ilyasshabi94@gmail.com>
2025-02-27 12:10:38 +0100

    user tracking blocking action

--- packages/dd-trace/src/appsec/sdk/user_blocking.js
+++ packages/dd-trace/src/appsec/sdk/user_blocking.js
@@ -3,7 +3,7 @@
 const { USER_ID } = require('../addresses')
 const waf = require('../waf')
 const { getRootSpan } = require('./utils')
-const { block } = require('../blocking')
+const { block, getBlockingAction } = require('../blocking')
 const { storage } = require('../../../../datadog-core')
 const { setUserTags } = require('./set_user')
 const log = require('../../log')
@@ -11,11 +11,14 @@ const { reportMetrics } = require('../reporter')
 
 function isUserBlocked (user) {
   const wafResults = waf.run({ persistent: { [USER_ID]: user.id } })
+
   if (!wafResults) return false
+
+  const blockTriggered = !!getBlockingAction(wafResults.actions)
 
   reportMetrics(wafResults.metrics, null)
 
-  return wafResults.metrics.blockTriggered
+  return blockTriggered
 }
 
 function checkUserAndSetUser (tracer, user) {
