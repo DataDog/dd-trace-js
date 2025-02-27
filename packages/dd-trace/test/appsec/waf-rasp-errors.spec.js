13f7336e96672c976dfdcb2eb439801d50dd83e2
ishabi <ilyasshabi94@gmail.com>
2025-02-27 15:14:41 +0100

    keep only waf error test

--- packages/dd-trace/test/appsec/waf-rasp-errors.spec.js
+++ packages/dd-trace/test/appsec/waf-rasp-errors.spec.js
@@ -6,84 +6,6 @@ const path = require('path')
 const Axios = require('axios')
 const { assert } = require('chai')
 
-describe('WAF/RASP - timeout', () => {
-  let axios, sandbox, cwd, appPort, appFile, agent, proc
-
-  before(async function () {
-    this.timeout(process.platform === 'win32' ? 90000 : 30000)
-
-    sandbox = await createSandbox(
-      ['express'],
-      false,
-      [
-        path.join(__dirname, 'rasp', 'resources'),
-        path.join(__dirname, '..', '..', 'src', 'appsec', 'recommended.json')
-      ]
-    )
-
-    appPort = await getPort()
-    cwd = sandbox.folder
-    appFile = path.join(cwd, 'resources', 'shi-app', 'index.js')
-
-    axios = Axios.create({
-      baseURL: `http://localhost:${appPort}`
-    })
-  })
-
-  after(async function () {
-    this.timeout(60000)
-    await sandbox.remove()
-  })
-
-  beforeEach(async () => {
-    agent = await new FakeAgent().start()
-    proc = await spawnProc(appFile, {
-      cwd,
-      env: {
-        DD_TRACE_AGENT_PORT: agent.port,
-        APP_PORT: appPort,
-        DD_APPSEC_ENABLED: 'true',
-        DD_APPSEC_RASP_ENABLED: 'true',
-        DD_TELEMETRY_HEARTBEAT_INTERVAL: 1,
-        DD_APPSEC_WAF_TIMEOUT: 1,
-        DD_APPSEC_RULES: path.join(cwd, 'recommended.json')
-      }
-    })
-  })
-
-  afterEach(async () => {
-    proc.kill()
-    await agent.stop()
-  })
-
-  // This test could be Flaky since the WAF could return result before 1ms
-  it('Should not block since waf will timeout', async () => {
-    const longValue = 'testattack'.repeat(500)
-
-    const largeObject = {}
-    for (let i = 0; i < 300; ++i) {
-      largeObject[`key${i}`] = `value${i}`
-    }
-
-    const deepObject = createNestedObject(25, { value: 'a' })
-
-    const complexPayload = {
-      deepObject,
-      longValue,
-      largeObject
-    }
-
-    await axios.post('/shi/execFileSync?dir=$(cat /etc/passwd 1>%262 ; echo .)', complexPayload)
-    await agent.assertMessageReceived(({ payload }) => {
-      assert.property(payload[0][0].metrics, '_dd.appsec.rasp.timeout')
-      assert(payload[0][0].metrics['_dd.appsec.rasp.timeout'] >= 1)
-
-      assert.property(payload[0][0].metrics, '_dd.appsec.waf.timeouts')
-      assert(payload[0][0].metrics['_dd.appsec.waf.timeouts'] >= 1)
-    })
-  })
-})
-
 describe('WAF/RASP - error', () => {
   let axios, sandbox, cwd, appPort, appFile, agent, proc
 
@@ -145,10 +67,3 @@ describe('WAF/RASP - error', () => {
     })
   })
 })
-
-const createNestedObject = (n, obj) => {
-  if (n > 0) {
-    return { a: createNestedObject(n - 1, obj) }
-  }
-  return obj
-}
