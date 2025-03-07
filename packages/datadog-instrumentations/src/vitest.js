57871c864241efb0536c44bf31febfe15c694b18
Mario Vidal Domínguez <60353145+Mariovido@users.noreply.github.com>
2025-03-07 16:32:01 +0100

    Add first capabilities tagging (#5321)

--- packages/datadog-instrumentations/src/vitest.js
+++ packages/datadog-instrumentations/src/vitest.js
@@ -51,7 +51,8 @@ function getProvidedContext () {
       _ddEarlyFlakeDetectionNumRetries: numRepeats,
       _ddIsKnownTestsEnabled: isKnownTestsEnabled,
       _ddIsQuarantinedTestsEnabled: isQuarantinedTestsEnabled,
-      _ddQuarantinedTests: quarantinedTests
+      _ddQuarantinedTests: quarantinedTests,
+      _ddIsFlakyTestRetriesEnabled: isFlakyTestRetriesEnabled
     } = globalThis.__vitest_worker__.providedContext
 
     return {
@@ -61,7 +62,8 @@ function getProvidedContext () {
       numRepeats,
       isKnownTestsEnabled,
       isQuarantinedTestsEnabled,
-      quarantinedTests
+      quarantinedTests,
+      isFlakyTestRetriesEnabled
     }
   } catch (e) {
     log.error('Vitest workers could not parse provided context, so some features will not work.')
@@ -192,6 +194,12 @@ function getSortWrapper (sort) {
 
     if (isFlakyTestRetriesEnabled && !this.ctx.config.retry && flakyTestRetriesCount > 0) {
       this.ctx.config.retry = flakyTestRetriesCount
+      try {
+        const workspaceProject = this.ctx.getCoreWorkspaceProject()
+        workspaceProject._provided._ddIsFlakyTestRetriesEnabled = isFlakyTestRetriesEnabled
+      } catch (e) {
+        log.warn('Could not send library configuration to workers.')
+      }
     }
 
     if (isKnownTestsEnabled) {
@@ -624,9 +632,16 @@ addHook({
     // From >=3.0.1, the first arguments changes from a string to an object containing the filepath
     const testSuiteAbsolutePath = testPaths[0]?.filepath || testPaths[0]
 
+    const { isEarlyFlakeDetectionEnabled, isFlakyTestRetriesEnabled } = getProvidedContext()
+
     const testSuiteAsyncResource = new AsyncResource('bound-anonymous-fn')
     testSuiteAsyncResource.runInAsyncScope(() => {
-      testSuiteStartCh.publish({ testSuiteAbsolutePath, frameworkVersion })
+      testSuiteStartCh.publish({
+        testSuiteAbsolutePath,
+        frameworkVersion,
+        isFlakyTestRetriesEnabled,
+        isEarlyFlakeDetectionEnabled
+      })
     })
     const startTestsResponse = await startTests.apply(this, arguments)
 
