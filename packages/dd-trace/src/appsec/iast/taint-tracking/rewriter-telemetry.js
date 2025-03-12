6b1d6be1b52aff41ac996f1897ed014c3acb9d0c
Bryan English <bryan.english@datadoghq.com>
2025-03-12 16:36:18 -0400

    simplify rewriter-telemetry.js

--- packages/dd-trace/src/appsec/iast/taint-tracking/rewriter-telemetry.js
+++ packages/dd-trace/src/appsec/iast/taint-tracking/rewriter-telemetry.js
@@ -4,39 +4,10 @@ const iastTelemetry = require('../telemetry')
 const { Verbosity } = require('../telemetry/verbosity')
 const { INSTRUMENTED_PROPAGATION } = require('../telemetry/iast-metric')
 
-const telemetryRewriter = {
-  off (content, filename, rewriter) {
-    return rewriter.rewrite(content, filename, ['iast'])
-  },
-
-  information (content, filename, rewriter) {
-    const response = this.off(content, filename, rewriter)
-
-    incrementTelemetry(response.metrics)
-
-    return response
-  }
-}
-
-function getRewriteFunction (rewriter) {
-  switch (iastTelemetry.verbosity) {
-    case Verbosity.OFF:
-      return (content, filename) => telemetryRewriter.off(content, filename, rewriter)
-    default:
-      return (content, filename) => telemetryRewriter.information(content, filename, rewriter)
-  }
-}
-
-function incrementTelemetry (metrics) {
-  if (metrics?.instrumentedPropagation) {
+function incrementTelemetryIfNeeded (metrics) {
+  if (iastTelemetry.verbosity !== Verbosity.OFF && metrics?.instrumentedPropagation) {
     INSTRUMENTED_PROPAGATION.inc(undefined, metrics.instrumentedPropagation)
   }
 }
 
-function incrementTelemetryIfNeeded (metrics) {
-  if (iastTelemetry.verbosity !== Verbosity.OFF) {
-    incrementTelemetry(metrics)
-  }
-}
-
-module.exports = { getRewriteFunction, incrementTelemetryIfNeeded }
+module.exports = { incrementTelemetryIfNeeded }
