72f633d78b7a91b9d2ce2c91253c0417c4d2e79e
Brian Marks <bm1549@users.noreply.github.com>
2025-03-13 11:29:30 -0400

    Adds JSDoc types to much of the sampling code (#5392)
    * Add consistent sampling via knuth's method
    
    * remove code changes from doc changes
    
    * better match existing semantics
    
    * fix test
    
    * they are the same
    
    * apply feedback

--- packages/dd-trace/src/sampler.js
+++ packages/dd-trace/src/sampler.js
@@ -1,14 +1,23 @@
 'use strict'
 
 class Sampler {
+  /**
+   * @param rate {number}
+   */
   constructor (rate) {
     this._rate = rate
   }
 
+  /**
+   * @returns {number}
+   */
   rate () {
     return this._rate
   }
 
+  /**
+   * @returns {boolean}
+   */
   isSampled () {
     return this._rate === 1 || Math.random() < this._rate
   }
