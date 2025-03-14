89b99e6b3bfe562f280a2e01df41641b416efbb6
Brian Marks <brian.marks@datadoghq.com>
2025-03-14 13:45:09 -0400

    rebase

--- packages/dd-trace/src/sampler.js
+++ packages/dd-trace/src/sampler.js
@@ -1,11 +1,15 @@
 'use strict'
 
+const KNUTH_FACTOR = 1111111111111111111
+const MAX_TRACE_ID = 2**64-1
+
 class Sampler {
   /**
    * @param rate {number}
    */
   constructor (rate) {
     this._rate = rate
+    this._sampling_id_threshold = MAX_TRACE_ID * rate
   }
 
   /**
@@ -18,8 +22,9 @@ class Sampler {
   /**
    * @returns {boolean}
    */
-  isSampled () {
-    return this._rate === 1 || Math.random() < this._rate
+  isSampled (context) {
+      return this._rate === 1 ||
+          ((context.toTraceIdNumber(false) * KNUTH_FACTOR) % MAX_TRACE_ID) <= this._sampling_id_threshold
   }
 }
 
