7002fa1918a2a8ec96526093665d6ee81d4e66e1
AJ Stuyvenberg <astuyve@gmail.com>
2025-03-15 21:36:40 -0400

    fix: import

--- packages/datadog-instrumentations/src/router.js
+++ packages/datadog-instrumentations/src/router.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const METHODS = require('http').METHODS.map(v => v.toLowerCase()).concat('all')
-const pathToRegExp = require('path-to-regexp')
+const { pathToRegExp } = require('path-to-regexp')
 const shimmer = require('../../datadog-shimmer')
 const { addHook, channel } = require('./helpers/instrument')
 
