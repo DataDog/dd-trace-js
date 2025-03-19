7924c421601f351dd64ede6cb89de309f490f0ea
ishabi <ilyasshabi94@gmail.com>
2025-03-19 10:25:51 +0100

    update waf metrics integration test

--- packages/dd-trace/test/appsec/waf-metrics.integration.spec.js
+++ packages/dd-trace/test/appsec/waf-metrics.integration.spec.js
@@ -6,8 +6,8 @@ const path = require('path')
 const Axios = require('axios')
 const { assert } = require('chai')
 
-describe('WAF error metrics', () => {
-  let axios, sandbox, cwd, appPort, appFile, agent, proc
+describe('WAF Metrics', () => {
+  let axios, sandbox, cwd, appPort, appFile
 
   before(async function () {
     this.timeout(process.platform === 'win32' ? 90000 : 30000)
@@ -32,265 +32,217 @@ describe('WAF error metrics', () => {
     await sandbox.remove()
   })
 
-  beforeEach(async () => {
-    agent = await new FakeAgent().start()
-    proc = await spawnProc(appFile, {
-      cwd,
-      env: {
-        DD_TRACE_AGENT_PORT: agent.port,
-        APP_PORT: appPort,
-        DD_APPSEC_ENABLED: 'true',
-        DD_TELEMETRY_HEARTBEAT_INTERVAL: 1,
-        DD_APPSEC_WAF_TIMEOUT: 0.1
+  describe('WAF error metrics', () => {
+    let agent, proc
+
+    beforeEach(async () => {
+      agent = await new FakeAgent().start()
+      proc = await spawnProc(appFile, {
+        cwd,
+        env: {
+          DD_TRACE_AGENT_PORT: agent.port,
+          APP_PORT: appPort,
+          DD_APPSEC_ENABLED: 'true',
+          DD_TELEMETRY_HEARTBEAT_INTERVAL: 1,
+          DD_APPSEC_WAF_TIMEOUT: 0.1
+        }
+      })
+    })
+
+    afterEach(async () => {
+      proc.kill()
+      await agent.stop()
+    })
+
+    it('should report waf error metrics', async () => {
+      let appsecTelemetryMetricsReceived = false
+
+      const body = {
+        name: 'hey'
       }
+
+      await axios.post('/', body)
+
+      const checkMessages = agent.assertMessageReceived(({ payload }) => {
+        assert.strictEqual(payload[0][0].metrics['_dd.appsec.enabled'], 1)
+        assert.strictEqual(payload[0][0].metrics['_dd.appsec.waf.error'], -127)
+      })
+
+      const checkTelemetryMetrics = agent.assertTelemetryReceived(({ payload }) => {
+        const namespace = payload.payload.namespace
+
+        if (namespace === 'appsec') {
+          appsecTelemetryMetricsReceived = true
+          const series = payload.payload.series
+          const wafRequests = series.find(s => s.metric === 'waf.requests')
+
+          assert.exists(wafRequests, 'Waf requests serie should exist')
+          assert.strictEqual(wafRequests.type, 'count')
+          assert.include(wafRequests.tags, 'waf_error:true')
+          assert.include(wafRequests.tags, 'rate_limited:true')
+        }
+      }, 30_000, 'generate-metrics', 2)
+
+      return Promise.all([checkMessages, checkTelemetryMetrics]).then(() => {
+        assert.equal(appsecTelemetryMetricsReceived, true)
+
+        return true
+      })
     })
   })
 
-  afterEach(async () => {
-    proc.kill()
-    await agent.stop()
-  })
+  describe('WAF timeout metrics', () => {
+    let agent, proc
 
-  it('should report waf error metrics', async () => {
-    let appsecTelemetryMetricsReceived = false
-
-    const body = {
-      name: 'hey'
-    }
-
-    await axios.post('/', body)
-
-    const checkMessages = agent.assertMessageReceived(({ payload }) => {
-      assert.strictEqual(payload[0][0].metrics['_dd.appsec.enabled'], 1)
-      assert.strictEqual(payload[0][0].metrics['_dd.appsec.waf.error'], -127)
+    beforeEach(async () => {
+      agent = await new FakeAgent().start()
+      proc = await spawnProc(appFile, {
+        cwd,
+        env: {
+          DD_TRACE_AGENT_PORT: agent.port,
+          APP_PORT: appPort,
+          DD_APPSEC_ENABLED: 'true',
+          DD_TELEMETRY_HEARTBEAT_INTERVAL: 1,
+          DD_APPSEC_WAF_TIMEOUT: 1
+        }
+      })
     })
 
-    const checkTelemetryMetrics = agent.assertTelemetryReceived(({ payload }) => {
-      const namespace = payload.payload.namespace
+    afterEach(async () => {
+      proc.kill()
+      await agent.stop()
+    })
 
-      if (namespace === 'appsec') {
-        appsecTelemetryMetricsReceived = true
-        const series = payload.payload.series
-        const wafRequests = series.find(s => s.metric === 'waf.requests')
+    it('should report waf timeout metrics', async () => {
+      let appsecTelemetryMetricsReceived = false
 
-        assert.exists(wafRequests, 'Waf requests serie should exist')
-        assert.strictEqual(wafRequests.type, 'count')
-        assert.include(wafRequests.tags, 'waf_error:true')
-        assert.include(wafRequests.tags, 'rate_limited:true')
-      }
-    }, 30_000, 'generate-metrics', 2)
+      const complexPayload = createComplexPayload()
+      await axios.post('/', { complexPayload })
 
-    return Promise.all([checkMessages, checkTelemetryMetrics]).then(() => {
-      assert.equal(appsecTelemetryMetricsReceived, true)
+      const checkMessages = agent.assertMessageReceived(({ payload }) => {
+        assert.strictEqual(payload[0][0].metrics['_dd.appsec.enabled'], 1)
+        assert.isTrue(payload[0][0].metrics['_dd.appsec.waf.timeouts'] > 0)
+      })
 
-      return true
+      const checkTelemetryMetrics = agent.assertTelemetryReceived(({ payload }) => {
+        const namespace = payload.payload.namespace
+
+        if (namespace === 'appsec') {
+          appsecTelemetryMetricsReceived = true
+          const series = payload.payload.series
+          const wafRequests = series.find(s => s.metric === 'waf.requests')
+
+          assert.exists(wafRequests, 'Waf requests serie should exist')
+          assert.strictEqual(wafRequests.type, 'count')
+          assert.include(wafRequests.tags, 'waf_timeout:true')
+        }
+      }, 30_000, 'generate-metrics', 2)
+
+      return Promise.all([checkMessages, checkTelemetryMetrics]).then(() => {
+        assert.equal(appsecTelemetryMetricsReceived, true)
+
+        return true
+      })
+    })
+  })
+
+  describe('WAF truncation metrics', () => {
+    let agent, proc
+
+    beforeEach(async () => {
+      agent = await new FakeAgent().start()
+      proc = await spawnProc(appFile, {
+        cwd,
+        env: {
+          DD_TRACE_AGENT_PORT: agent.port,
+          APP_PORT: appPort,
+          DD_APPSEC_ENABLED: 'true',
+          DD_TELEMETRY_HEARTBEAT_INTERVAL: 1
+        }
+      })
+    })
+
+    afterEach(async () => {
+      proc.kill()
+      await agent.stop()
+    })
+
+    it('should report truncation metrics', async () => {
+      let appsecTelemetryMetricsReceived = false
+      let appsecTelemetryDistributionsReceived = false
+
+      const complexPayload = createComplexPayload()
+      await axios.post('/', { complexPayload })
+
+      const checkMessages = agent.assertMessageReceived(({ payload }) => {
+        assert.strictEqual(payload[0][0].metrics['_dd.appsec.enabled'], 1)
+        assert.strictEqual(payload[0][0].metrics['_dd.appsec.truncated.container_depth'], 20)
+        assert.strictEqual(payload[0][0].metrics['_dd.appsec.truncated.container_size'], 300)
+        assert.strictEqual(payload[0][0].metrics['_dd.appsec.truncated.string_length'], 5000)
+      })
+
+      const checkTelemetryMetrics = agent.assertTelemetryReceived(({ payload }) => {
+        const namespace = payload.payload.namespace
+
+        if (namespace === 'appsec') {
+          appsecTelemetryMetricsReceived = true
+          const series = payload.payload.series
+          const inputTruncated = series.find(s => s.metric === 'waf.input_truncated')
+
+          assert.exists(inputTruncated, 'input truncated serie should exist')
+          assert.strictEqual(inputTruncated.type, 'count')
+          assert.include(inputTruncated.tags, 'truncation_reason:7')
+
+          const wafRequests = series.find(s => s.metric === 'waf.requests')
+          assert.exists(wafRequests, 'waf requests serie should exist')
+          assert.include(wafRequests.tags, 'input_truncated:true')
+        }
+      }, 30_000, 'generate-metrics', 2)
+
+      const checkTelemetryDistributions = agent.assertTelemetryReceived(({ payload }) => {
+        const namespace = payload.payload.namespace
+
+        if (namespace === 'appsec') {
+          appsecTelemetryDistributionsReceived = true
+          const series = payload.payload.series
+          const wafDuration = series.find(s => s.metric === 'waf.duration')
+          const wafDurationExt = series.find(s => s.metric === 'waf.duration_ext')
+          const wafTuncated = series.filter(s => s.metric === 'waf.truncated_value_size')
+
+          assert.exists(wafDuration, 'waf duration serie should exist')
+          assert.exists(wafDurationExt, 'waf duration ext serie should exist')
+
+          assert.equal(wafTuncated.length, 3)
+          assert.include(wafTuncated[0].tags, 'truncation_reason:1')
+          assert.include(wafTuncated[1].tags, 'truncation_reason:2')
+          assert.include(wafTuncated[2].tags, 'truncation_reason:4')
+        }
+      }, 30_000, 'distributions', 1)
+
+      return Promise.all([checkMessages, checkTelemetryMetrics, checkTelemetryDistributions]).then(() => {
+        assert.equal(appsecTelemetryMetricsReceived, true)
+        assert.equal(appsecTelemetryDistributionsReceived, true)
+
+        return true
+      })
     })
   })
 })
 
-describe('WAF timeout metrics', () => {
-  let axios, sandbox, cwd, appPort, appFile, agent, proc
+const createComplexPayload = () => {
+  const longValue = 'testattack'.repeat(500)
+  const largeObject = {}
+  for (let i = 0; i < 300; ++i) {
+    largeObject[`key${i}`] = `value${i}`
+  }
+  const deepObject = createNestedObject(25, { value: 'a' })
 
-  before(async function () {
-    this.timeout(process.platform === 'win32' ? 90000 : 30000)
-
-    sandbox = await createSandbox(
-      ['express'],
-      false,
-      [path.join(__dirname, 'resources')]
-    )
-
-    appPort = await getPort()
-    cwd = sandbox.folder
-    appFile = path.join(cwd, 'resources', 'index.js')
-
-    axios = Axios.create({
-      baseURL: `http://localhost:${appPort}`
-    })
-  })
-
-  after(async function () {
-    this.timeout(60000)
-    await sandbox.remove()
-  })
-
-  beforeEach(async () => {
-    agent = await new FakeAgent().start()
-    proc = await spawnProc(appFile, {
-      cwd,
-      env: {
-        DD_TRACE_AGENT_PORT: agent.port,
-        APP_PORT: appPort,
-        DD_APPSEC_ENABLED: 'true',
-        DD_TELEMETRY_HEARTBEAT_INTERVAL: 1,
-        DD_APPSEC_WAF_TIMEOUT: 1
-      }
-    })
-  })
-
-  afterEach(async () => {
-    proc.kill()
-    await agent.stop()
-  })
-
-  it('should report waf timeout metrics', async () => {
-    let appsecTelemetryMetricsReceived = false
-
-    const longValue = 'testattack'.repeat(500)
-    const largeObject = {}
-    for (let i = 0; i < 300; ++i) {
-      largeObject[`key${i}`] = `value${i}`
-    }
-    const deepObject = createNestedObject(25, { value: 'a' })
-    const complexPayload = {
-      deepObject,
-      longValue,
-      largeObject
-    }
-
-    await axios.post('/', { complexPayload })
-
-    const checkMessages = agent.assertMessageReceived(({ payload }) => {
-      assert.strictEqual(payload[0][0].metrics['_dd.appsec.enabled'], 1)
-      assert.isTrue(payload[0][0].metrics['_dd.appsec.waf.timeouts'] > 0)
-    })
-
-    const checkTelemetryMetrics = agent.assertTelemetryReceived(({ payload }) => {
-      const namespace = payload.payload.namespace
-
-      if (namespace === 'appsec') {
-        appsecTelemetryMetricsReceived = true
-        const series = payload.payload.series
-        const wafRequests = series.find(s => s.metric === 'waf.requests')
-
-        assert.exists(wafRequests, 'Waf requests serie should exist')
-        assert.strictEqual(wafRequests.type, 'count')
-        assert.include(wafRequests.tags, 'waf_timeout:true')
-      }
-    }, 30_000, 'generate-metrics', 2)
-
-    return Promise.all([checkMessages, checkTelemetryMetrics]).then(() => {
-      assert.equal(appsecTelemetryMetricsReceived, true)
-
-      return true
-    })
-  })
-})
-
-describe('WAF truncation metrics', () => {
-  let axios, sandbox, cwd, appPort, appFile, agent, proc
-
-  before(async function () {
-    this.timeout(process.platform === 'win32' ? 90000 : 30000)
-
-    sandbox = await createSandbox(
-      ['express'],
-      false,
-      [path.join(__dirname, 'resources')]
-    )
-
-    appPort = await getPort()
-    cwd = sandbox.folder
-    appFile = path.join(cwd, 'resources', 'index.js')
-
-    axios = Axios.create({
-      baseURL: `http://localhost:${appPort}`
-    })
-  })
-
-  after(async function () {
-    this.timeout(60000)
-    await sandbox.remove()
-  })
-
-  beforeEach(async () => {
-    agent = await new FakeAgent().start()
-    proc = await spawnProc(appFile, {
-      cwd,
-      env: {
-        DD_TRACE_AGENT_PORT: agent.port,
-        APP_PORT: appPort,
-        DD_APPSEC_ENABLED: 'true',
-        DD_TELEMETRY_HEARTBEAT_INTERVAL: 1
-      }
-    })
-  })
-
-  afterEach(async () => {
-    proc.kill()
-    await agent.stop()
-  })
-
-  it('should report tuncation metrics', async () => {
-    let appsecTelemetryMetricsReceived = false
-    let appsecTelemetryDistributionsReceived = false
-
-    const longValue = 'testattack'.repeat(500)
-    const largeObject = {}
-    for (let i = 0; i < 300; ++i) {
-      largeObject[`key${i}`] = `value${i}`
-    }
-    const deepObject = createNestedObject(25, { value: 'a' })
-    const complexPayload = {
-      deepObject,
-      longValue,
-      largeObject
-    }
-
-    await axios.post('/', { complexPayload })
-
-    const checkMessages = agent.assertMessageReceived(({ payload }) => {
-      assert.strictEqual(payload[0][0].metrics['_dd.appsec.enabled'], 1)
-      assert.strictEqual(payload[0][0].metrics['_dd.appsec.truncated.container_depth'], 20)
-      assert.strictEqual(payload[0][0].metrics['_dd.appsec.truncated.container_size'], 300)
-      assert.strictEqual(payload[0][0].metrics['_dd.appsec.truncated.string_length'], 5000)
-    })
-
-    const checkTelemetryMetrics = agent.assertTelemetryReceived(({ payload }) => {
-      const namespace = payload.payload.namespace
-
-      if (namespace === 'appsec') {
-        appsecTelemetryMetricsReceived = true
-        const series = payload.payload.series
-        const inputTruncated = series.find(s => s.metric === 'waf.input_truncated')
-
-        assert.exists(inputTruncated, 'input truncated serie should exist')
-        assert.strictEqual(inputTruncated.type, 'count')
-        assert.include(inputTruncated.tags, 'truncation_reason:7')
-
-        const wafRequests = series.find(s => s.metric === 'waf.requests')
-        assert.exists(wafRequests, 'waf requests serie should exist')
-        assert.include(wafRequests.tags, 'input_truncated:true')
-      }
-    }, 30_000, 'generate-metrics', 2)
-
-    const checkTelemetryDistributions = agent.assertTelemetryReceived(({ payload }) => {
-      const namespace = payload.payload.namespace
-
-      if (namespace === 'appsec') {
-        appsecTelemetryDistributionsReceived = true
-        const series = payload.payload.series
-        const wafDuration = series.find(s => s.metric === 'waf.duration')
-        const wafDurationExt = series.find(s => s.metric === 'waf.duration_ext')
-        const wafTuncated = series.filter(s => s.metric === 'waf.truncated_value_size')
-
-        assert.exists(wafDuration, 'waf duration serie should exist')
-        assert.exists(wafDurationExt, 'waf duration ext serie should exist')
-
-        assert.equal(wafTuncated.length, 3)
-        assert.include(wafTuncated[0].tags, 'truncation_reason:1')
-        assert.include(wafTuncated[1].tags, 'truncation_reason:2')
-        assert.include(wafTuncated[2].tags, 'truncation_reason:4')
-      }
-    }, 30_000, 'distributions', 1)
-    return Promise.all([checkMessages, checkTelemetryMetrics, checkTelemetryDistributions]).then(() => {
-      assert.equal(appsecTelemetryMetricsReceived, true)
-      assert.equal(appsecTelemetryDistributionsReceived, true)
-
-      return true
-    })
-  })
-})
+  return {
+    deepObject,
+    longValue,
+    largeObject
+  }
+}
 
 const createNestedObject = (n, obj) => {
   if (n > 0) {
