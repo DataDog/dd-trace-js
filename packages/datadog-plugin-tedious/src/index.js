24a26071733fec6e8950cd7b6763c515466d0dcf
Thomas Hunter II <tlhunter@datadog.com>
2025-03-19 12:12:27 -0700

    dbm: tedious (sql server) service mode (#5375)

--- packages/datadog-plugin-tedious/src/index.js
+++ packages/datadog-plugin-tedious/src/index.js
@@ -8,22 +8,27 @@ class TediousPlugin extends DatabasePlugin {
   static get operation () { return 'request' } // TODO: change to match other database plugins
   static get system () { return 'mssql' }
 
-  start ({ queryOrProcedure, connectionConfig }) {
-    this.startSpan(this.operationName(), {
-      service: this.serviceName({ pluginConfig: this.config, system: this.system }),
-      resource: queryOrProcedure,
+  start (payload) {
+    const service = this.serviceName({ pluginConfig: this.config, system: this.system })
+    const span = this.startSpan(this.operationName(), {
+      service,
+      resource: payload.queryOrProcedure,
       type: 'sql',
       kind: 'client',
       meta: {
         'db.type': 'mssql',
         component: 'tedious',
-        'out.host': connectionConfig.server,
-        [CLIENT_PORT_KEY]: connectionConfig.options.port,
-        'db.user': connectionConfig.userName || connectionConfig.authentication.options.userName,
-        'db.name': connectionConfig.options.database,
-        'db.instance': connectionConfig.options.instanceName
+        'out.host': payload.connectionConfig.server,
+        [CLIENT_PORT_KEY]: payload.connectionConfig.options.port,
+        'db.user': payload.connectionConfig.userName || payload.connectionConfig.authentication.options.userName,
+        'db.name': payload.connectionConfig.options.database,
+        'db.instance': payload.connectionConfig.options.instanceName
       }
     })
+
+    // SQL Server includes comments when caching queries
+    // For that reason we allow service mode but not full mode
+    payload.sql = this.injectDbmQuery(span, payload.queryOrProcedure, service, true)
   }
 }
 
