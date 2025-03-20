3fa3515464f1ec80d67d20d60c5d55acbb3faee3
Yun Kim <yun.kim@datadoghq.com>
2025-03-20 15:43:41 -0400

    Add error tag

--- packages/dd-trace/src/llmobs/telemetry.js
+++ packages/dd-trace/src/llmobs/telemetry.js
@@ -10,6 +10,8 @@ const {
   DECORATOR
 } = require('./constants/tags')
 
+const ERROR_TYPE = require('../constants')
+
 const telemetryMetrics = require('../telemetry/metrics')
 
 const LLMObsTagger = require('./tagger')
@@ -22,21 +24,24 @@ function incrementLLMObsSpanStartCount (tags, value = 1) {
 
 function incrementLLMObsSpanFinishedCount (span, value = 1) {
   const mlObsTags = LLMObsTagger.tagMap.get(span)
+  const spanTags = span.context()._tags
 
   const isRootSpan = mlObsTags[PARENT_ID_KEY] === ROOT_PARENT_ID
   const hasSessionId = mlObsTags[SESSION_ID] != null
   const integration = mlObsTags[INTEGRATION]
   const autoInstrumented = integration != null
-  const decorator = mlObsTags[DECORATOR] != null
+  const decorator = !mlObsTags[DECORATOR]
   const spanKind = mlObsTags[SPAN_KIND]
   const modelProvider = mlObsTags[MODEL_PROVIDER]
+  const error = spanTags.error || spanTags[ERROR_TYPE]
 
   const tags = {
     autoinstrumented: Number(autoInstrumented),
     has_session_id: Number(hasSessionId),
     is_root_span: Number(isRootSpan),
     span_kind: spanKind,
-    integration: integration || 'N/A'
+    integration: integration || 'N/A',
+    error: error ? 1 : 0
   }
   if (!autoInstrumented) {
     tags.decorator = Number(decorator)
