717c24fae4a9fec38eb9a73aafaf8fa31787669c
Ugaitz Urien <ugaitz.urien@datadoghq.com>
2025-03-21 13:02:03 +0100

    Add noop functions for v2

--- packages/dd-trace/src/appsec/sdk/noop.js
+++ packages/dd-trace/src/appsec/sdk/noop.js
@@ -1,6 +1,16 @@
 'use strict'
 
+class NoopAppsecSdkV2 {
+  trackUserLoginSuccess () {}
+
+  trackUserLoginFailure () {}
+}
+
 class NoopAppsecSdk {
+  constructor () {
+    this.v2 = new NoopAppsecSdkV2()
+  }
+
   trackUserLoginSuccessEvent () {}
 
   trackUserLoginFailureEvent () {}
