434a2f39f5399ca0591f2a490259cd271cdcc4ff
Bryan English <bryan.english@datadoghq.com>
2025-03-26 23:26:25 -0400

    isNotLibraryFile should return true inside `versions`

--- packages/dd-trace/src/appsec/iast/taint-tracking/filter.js
+++ packages/dd-trace/src/appsec/iast/taint-tracking/filter.js
@@ -6,8 +6,22 @@ const isPrivateModule = function (file) {
   return file && file.indexOf(NODE_MODULES) === -1
 }
 
-const isNotLibraryFile = function (file) {
+let isNotLibraryFile = function (file) {
   return file && file.indexOf('dd-trace-js') === -1 && file.indexOf('dd-trace') === -1
+}
+
+if (process.env.PLUGINS) {
+  // We're most likely in test mode, so allow for modules inside the tested
+  // modules directory, which is `versions`
+  isNotLibraryFile = function (file) {
+    if (!file) {
+      return false
+    }
+    if (file.indexOf('dd-trace-js/versions/') !== -1) {
+      return true
+    }
+    return file.indexOf('dd-trace-js') === -1 && file.indexOf('dd-trace') === -1
+  }
 }
 
 module.exports = {
