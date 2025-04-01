7e77ee47e4fae24c98121c91dad639daa95a8d24
Mario Vidal Domínguez <mario.vidal@datadoghq.com>
2025-04-01 14:48:28 +0200

    Update test

--- integration-tests/ci-visibility/playwright-tests-rum/active-test-span-rum-test.js
+++ integration-tests/ci-visibility/playwright-tests-rum/active-test-span-rum-test.js
@@ -1,8 +1,11 @@
 const { test, expect } = require('@playwright/test')
 
+beforeEach(async ({ page }) => {
+  await page.goto(process.env.PW_BASE_URL)
+})
+
 test.describe('playwright', () => {
   test('should have RUM active', async ({ page }) => {
-    await page.goto(process.env.PW_BASE_URL)
     await expect(page.locator('.hello-world')).toHaveText([
       'Hello World'
     ])
