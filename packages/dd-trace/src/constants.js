af4422b324341f7d5e492c8c1b358b14773b4e18
Ruben Bridgewater <ruben@bridgewater.de>
2025-04-03 10:11:39 +0200

    Improve tagger, format, and encoding (#5354)
    * Improve tagger and format
    
    These are hot code paths and the otel parts should not be part of
    the tagger. This removes the special handling for errors in the
    tagger while also improving the performance and not adding tags
    that have no purpose.
    
    * Minor encoding perf improvement
    
    Calling Object.entries is actually doing more work than Object.keys
    while the result is the same.

--- packages/dd-trace/src/constants.js
+++ packages/dd-trace/src/constants.js
@@ -26,6 +26,7 @@ module.exports = {
   ERROR_TYPE: 'error.type',
   ERROR_MESSAGE: 'error.message',
   ERROR_STACK: 'error.stack',
+  IGNORE_OTEL_ERROR: Symbol('ignore.otel.error'),
   COMPONENT: 'component',
   CLIENT_PORT_KEY: 'network.destination.port',
   PEER_SERVICE_KEY: 'peer.service',
