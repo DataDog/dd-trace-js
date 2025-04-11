3edb732e2502d775acfec35403fbb95432c612db
Ugaitz Urien <ugaitz.urien@datadoghq.com>
2025-04-11 10:43:13 +0200

    Login events SDK v2 (#5333)
    * initial version, missing tests and probably refactor
    
    * Tests and definitions and tiny fixes
    
    * Fix lint
    
    * Tiny format changes
    
    * Use new method `setUserTagsSdk` to force setting the tag `_dd.appsec.user.collection_mode`
    
    * Fix test
    
    * Restore proxyquire nocache
    
    * Tiny code style changes
    
    * Add noop functions for v2
    
    * Remove duplicated telemetry code
    
    * Reorder exports
    
    * Remove unnecessary setUserTagsSdk method
    
    * Small fixes
    
    * Rename appsec.v2 to appsec.eventTrackingV2
    
    * Address PR comments
    
    * Small fixes
    
    * Remove unused code in track_event.spec.js
    
    * Run ST in a branch
    
    * Undo last commit
    
    * Replace v2 to eventTrackingV2 in log messages
    
    * Apply suggestions from code review
    
    Co-authored-by: simon-id <simon.id@datadoghq.com>
    
    * Update packages/dd-trace/test/appsec/sdk/track_event.spec.js
    
    Co-authored-by: simon-id <simon.id@datadoghq.com>
    
    * Address PR comments
    
    * Update format
    
    * tiny change
    
    * Fix lint
    
    * Fix test
    
    * update doc
    
    ---------
    
    Co-authored-by: simon-id <simon.id@datadoghq.com>

--- docs/test.ts
+++ docs/test.ts
@@ -473,6 +473,19 @@ tracer.appsec.trackUserLoginFailureEvent('user_id', false, meta)
 tracer.appsec.trackCustomEvent('event_name')
 tracer.appsec.trackCustomEvent('event_name', meta)
 
+tracer.appsec.eventTrackingV2.trackUserLoginSuccess('login')
+tracer.appsec.eventTrackingV2.trackUserLoginSuccess('login', user)
+tracer.appsec.eventTrackingV2.trackUserLoginSuccess('login', user, meta)
+tracer.appsec.eventTrackingV2.trackUserLoginSuccess('login', null, meta)
+
+tracer.appsec.eventTrackingV2.trackUserLoginSuccess('login', '123')
+tracer.appsec.eventTrackingV2.trackUserLoginSuccess('login', '123', meta)
+
+tracer.appsec.eventTrackingV2.trackUserLoginFailure('login')
+tracer.appsec.eventTrackingV2.trackUserLoginFailure('login', true)
+tracer.appsec.eventTrackingV2.trackUserLoginFailure('login', meta)
+tracer.appsec.eventTrackingV2.trackUserLoginFailure('login', false, meta)
+
 tracer.setUser(user)
 
 const resUserBlock: boolean = tracer.appsec.isUserBlocked(user)
