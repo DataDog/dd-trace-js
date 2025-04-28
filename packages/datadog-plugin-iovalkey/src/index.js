bf7a6e948f3c5b6a995abc39895170da4b3e8a6a
Thomas Hunter II <tlhunter@datadog.com>
2025-04-28 14:13:00 -0700

    add span type to class / subclass

--- packages/datadog-plugin-iovalkey/src/index.js
+++ packages/datadog-plugin-iovalkey/src/index.js
@@ -6,6 +6,13 @@ class IOValkeyPlugin extends RedisPlugin {
   static get id () {
     return 'iovalkey'
   }
+
+  static get system () { return 'valkey' }
+
+  constructor (...args) {
+    super(...args)
+    this._spanType = 'valkey'
+  }
 }
 
 module.exports = IOValkeyPlugin
