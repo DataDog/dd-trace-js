17f395060c2e6b9fef35bc2620195cfcd4b3a99c
Ugaitz Urien <ugaitz.urien@datadoghq.com>
2025-04-30 09:19:45 +0200

    Change description for @deprecated property

--- index.d.ts
+++ index.d.ts
@@ -2251,7 +2251,7 @@ declare namespace tracer {
     /**
      * Defines the pattern to ignore cookie names in the vulnerability hash calculation
      * @default ".{32,}"
-     * @deprecated Has no effect
+     * @deprecated This property has no effect because hash calculation algorithm has been updated for cookie vulnerabilities
      */
     cookieFilterPattern?: string,
 
