0f62fe908788b03d74b9d1783b27db53ce5b05ac
Andre Caron <andre.l.caron@gmail.com>
2025-05-01 18:12:46 +0000

    fix: Support for inode when /proc/self/cgroup is a multiline string

--- packages/dd-trace/src/exporters/common/docker.js
+++ packages/dd-trace/src/exporters/common/docker.js
@@ -10,7 +10,7 @@ const uuidSource =
 '[0-9a-f]{8}[-_][0-9a-f]{4}[-_][0-9a-f]{4}[-_][0-9a-f]{4}[-_][0-9a-f]{12}|[0-9a-f]{8}(?:-[0-9a-f]{4}){4}$'
 const containerSource = '[0-9a-f]{64}'
 const taskSource = '[0-9a-f]{32}-\\d+'
-const lineReg = /^(\d+):([^:]*):(.+)$/
+const lineReg = /^(\d+):([^:]*):(.+)$/m
 const entityReg = new RegExp(`.*(${uuidSource}|${containerSource}|${taskSource})(?:\\.scope)?$`, 'm')
 
 const cgroup = readControlGroup()
