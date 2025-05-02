f8d8805062447edb1bb13b633ed41fbab477021d
CarlesDD <carles.capell@datadoghq.com>
2025-05-02 15:25:02 +0200

    Remove deprecated span tags

--- packages/dd-trace/src/appsec/waf/waf_manager.js
+++ packages/dd-trace/src/appsec/waf/waf_manager.js
@@ -16,7 +16,7 @@ class WAFManager {
     this.rulesVersion = this.ddwaf.diagnostics.ruleset_version
     this.defaultRules = rules
 
-    Reporter.reportWafInit(this.ddwafVersion, this.rulesVersion, this.ddwaf.diagnostics.rules, true)
+    Reporter.reportWafInit(this.ddwafVersion, this.rulesVersion, true)
   }
 
   _loadDDWAF (rules) {
