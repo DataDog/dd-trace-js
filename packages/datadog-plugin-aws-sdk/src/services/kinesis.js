7c6fb1ba2a1aeb002a71b2c53f2cdaff430f3b4a
Zarir Hamza <zarir.hamza@datadoghq.com>
2025-05-03 00:10:25 -0400

    with peer tags

--- packages/datadog-plugin-aws-sdk/src/services/kinesis.js
+++ packages/datadog-plugin-aws-sdk/src/services/kinesis.js
@@ -67,7 +67,7 @@ class Kinesis extends BaseAwsSdkPlugin {
     return {
       'resource.name': `${operation} ${params.StreamName}`,
       'aws.kinesis.stream_name': params.StreamName,
-      //streamname: params.StreamName
+      streamname: params.StreamName,
       'hostname': 'kinesis.us-east-1.amazonaws.com',
     }
   }
--- packages/datadog-plugin-aws-sdk/src/services/sns.js
+++ packages/datadog-plugin-aws-sdk/src/services/sns.js
@@ -24,7 +24,7 @@ class Sns extends BaseAwsSdkPlugin {
     return {
       'resource.name': `${operation} ${params.TopicArn || response.data.TopicArn}`,
       'aws.sns.topic_arn': TopicArn,
-      //topicname: topicName
+      topicname: topicName,
       'hostname': 'sns.us-east-1.amazonaws.com',
     }
 
