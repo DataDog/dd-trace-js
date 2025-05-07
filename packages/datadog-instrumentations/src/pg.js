365e6d55db38580cc3c327eb3caaa599d85662b6
William Conti <william.conti@datadoghq.com>
2025-05-07 19:04:02 -0400

    remove code comments and add error handling

--- packages/datadog-instrumentations/src/pg.js
+++ packages/datadog-instrumentations/src/pg.js
@@ -147,13 +147,11 @@ function wrapQuery (query) {
       }
 
       if (textPropObj.read) {
-        // newQuery.on('data', (chunk) => {
-        //   console.log(chunk)
-        // })
         newQuery.on('end', () => {
-          // we don't do anything with the result so just return an empty array, if we need the result we can
-          // use on.data event shown above
           finish(null, [])
+        })
+        newQuery.on('error', (err) => {
+          finish(err)
         })
       }
 
