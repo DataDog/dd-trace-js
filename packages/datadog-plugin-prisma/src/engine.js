d56f8be937ab169008d972c4ebea21f839165ff7
bojbrook <bowen.brooks@datadoghq.com>
2025-05-14 12:07:23 -0600

    updated code from review

--- packages/datadog-plugin-prisma/src/engine.js
+++ packages/datadog-plugin-prisma/src/engine.js
@@ -28,18 +28,18 @@ class PrismaEngine extends DatabasePlugin {
   static get system () { return 'prisma' }
 
   start (ctx) {
-    // console.log('PrismaEngine start called', ctx.engineSpan)
     const { engineSpan, allEngineSpans, childOf, dbConfig } = ctx
 
     const service = this.serviceName({ pluginConfig: this.config, system: this.system })
 
     const unixStartTime = hrTimeToUnixTimeMs(engineSpan.startTime)
 
-    const spanName = engineSpan.name.substring(14) // remove 'prisma:engine:' prefix
-    let options = {
+    const spanName = engineSpan.name.slice(14) // remove 'prisma:engine:' prefix
+    const options = {
       childOf,
       service,
       kind: engineSpan.kind,
+      startTime: unixStartTime,
       meta: {
         prisma: {
           name: spanName,
@@ -52,44 +52,32 @@ class PrismaEngine extends DatabasePlugin {
       case 'db_query':{
         const query = engineSpan.attributes['db.query.text']
         const originalStatement = this.maybeTruncate(query)
-        let type
-        let dbType
-        if (databaseDriverMapper[engineSpan.attributes['db.system']]) {
-          type = databaseDriverMapper[engineSpan.attributes['db.system']].type
-          dbType = databaseDriverMapper[engineSpan.attributes['db.system']]['db.type']
-        }
-
+        const type = databaseDriverMapper[engineSpan.attributes['db.system']]?.type
+        const dbType = databaseDriverMapper[engineSpan.attributes['db.system']]?.['db.type']
         // Start time format is defined here
         //  https://github.com/open-telemetry/opentelemetry-js/blob/cbc912d/api/src/common/Time.ts#L19-L30.
-        options = {
-          ...options,
-          resource: originalStatement,
-          startTime: unixStartTime,
-          type: type || engineSpan.attributes['db.system'],
-          meta: {
-            ...options.meta,
-            'db.type': dbType || engineSpan.attributes['db.system'],
-            'db.instance': dbConfig?.database,
-            'db.name': dbConfig?.user,
-            'out.host': dbConfig?.host,
-            [CLIENT_PORT_KEY]: dbConfig?.port
-          },
-          childOf
-        }
-      }
-        break
-
-      default:{
-        options = { ...options, resource: spanName, startTime: unixStartTime, childOf }
+        options.resource = originalStatement
+        options.type = type || engineSpan.attributes['db.system']
+        options.meta['db.type'] = dbType || engineSpan.attributes['db.system']
+        options.meta['db.instance'] = dbConfig?.database
+        options.meta['db.name'] = dbConfig?.user
+        options.meta['out.host'] = dbConfig?.host
+        options.meta[CLIENT_PORT_KEY] = dbConfig?.port
+        options.childOf = childOf
         break
       }
+      default:
+        options.resource = spanName
+        options.childOf = childOf
+        break
     }
 
     const activeSpan = this.startSpan(this.operationName({ operation: this.operation }), options)
-    const children = allEngineSpans.filter((span) => span.parentId === engineSpan.id)
-    for (const child of children) {
-      const startCtx = { engineSpan: child, allEngineSpans, childOf: activeSpan, dbConfig }
-      this.start(startCtx)
+    for (const span of allEngineSpans) {
+      if (span.parentId === engineSpan.id) {
+        const startCtx = { engineSpan: span, allEngineSpans, childOf: activeSpan, dbConfig }
+        this.start(startCtx)
+      }
     }
     const unixEndTime = hrTimeToUnixTimeMs(engineSpan.endTime)
     activeSpan.finish(unixEndTime)
