a97d2ffa45fd8a100135ad033fbfbceda3638852
Benjamin De Bernardi <benjamin.debernardi@datadoghq.com>
2025-05-15 17:32:33 +0200

    add jsdoc and backport old Sampler as RandomSampler

--- packages/dd-trace/src/span_sampler.js
+++ packages/dd-trace/src/span_sampler.js
@@ -23,7 +23,7 @@ class SpanSampler {
     const { started } = spanContext._trace
     for (const span of started) {
       const rule = this.findRule(span)
-      if (rule && rule.sample()) {
+      if (rule && rule.sample(spanContext)) {
         span.context()._spanSampling = {
           sampleRate: rule.sampleRate,
           maxPerSecond: rule.maxPerSecond
