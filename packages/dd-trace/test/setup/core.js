e4c29b2dd7475d8120c8747aba6654965737906f
Attila Szegedi <attila.szegedi@datadoghq.com>
2025-05-15 16:58:44 +0200

    Revert "use no async context frame option for now (#5706)"
    This reverts commit f4ee8443bfabbb8a39e3165c5f2da64bf3176f01.

--- packages/dd-trace/test/setup/core.js
+++ packages/dd-trace/test/setup/core.js
@@ -5,7 +5,6 @@ const chai = require('chai')
 const sinonChai = require('sinon-chai')
 const { setTimeout } = require('timers/promises')
 const proxyquire = require('../proxyquire')
-const { NODE_MAJOR } = require('../../../../version')
 
 {
   // get-port can often return a port that is already in use, thanks to a race
@@ -52,36 +51,4 @@ process.env.DD_INSTRUMENTATION_TELEMETRY_ENABLED = 'false'
 if (/^v\d+\.x$/.test(process.env.GITHUB_BASE_REF || '')) {
   process.env.DD_INJECTION_ENABLED = 'true'
   process.env.DD_INJECT_FORCE = 'true'
-}
-
-// TODO(bengl): remove this block once we can properly support Node.js 24 without it
-if (NODE_MAJOR >= 24 && !process.env.OPTIONS_OVERRIDE) {
-  const childProcess = require('child_process')
-  const { exec, fork } = childProcess
-
-  childProcess.exec = function (...args) {
-    const opts = args[1]
-    if (opts) {
-      if (opts?.env?.NODE_OPTIONS && !opts.env.NODE_OPTIONS.includes('--no-async-context-frame')) {
-        opts.env.NODE_OPTIONS += ' --no-async-context-frame'
-      } else {
-        opts.env ||= {}
-        opts.env.NODE_OPTIONS = '--no-async-context-frame'
-      }
-    }
-    return exec.apply(this, args)
-  }
-
-  childProcess.fork = function (...args) {
-    const opts = args[1]
-    if (opts) {
-      if (opts?.env?.NODE_OPTIONS && !opts.env.NODE_OPTIONS.includes('--no-async-context-frame')) {
-        opts.env.NODE_OPTIONS += ' --no-async-context-frame'
-      } else {
-        opts.env ||= {}
-        opts.env.NODE_OPTIONS = '--no-async-context-frame'
-      }
-    }
-    return fork.apply(this, args)
-  }
 }
