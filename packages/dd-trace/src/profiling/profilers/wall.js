9c860cd941cdbf26518ea30976850c51e43a352a
Attila Szegedi <attila.szegedi@datadoghq.com>
2025-05-16 12:14:12 +0200

    DEBUG

--- packages/dd-trace/src/profiling/profilers/wall.js
+++ packages/dd-trace/src/profiling/profilers/wall.js
@@ -16,6 +16,7 @@ const {
 const { isWebServerSpan, endpointNameFromTags, getStartedSpans } = require('../webspan-utils')
 
 let beforeCh
+let lastInstance
 const enterCh = dc.channel('dd-trace:storage:enter')
 const spanFinishCh = dc.channel('dd-trace:span:finish')
 const profilerTelemetryMetrics = telemetryMetrics.manager.namespace('profilers')
@@ -155,6 +156,7 @@ class NativeWallProfiler {
 
     this._logger = options.logger
     this._started = false
+    lastInstance = this
   }
 
   codeHotspotsEnabled () {
@@ -284,6 +286,11 @@ class NativeWallProfiler {
       span[ProfilingContext] = profilingContext
     }
     return profilingContext
+  }
+
+  _getSampleContext () {
+    const context = this._pprof.time.getContext()
+    return this._useAsyncContextFrame ? context : context.ref
   }
 
   _setNewContext () {
@@ -422,4 +429,21 @@ class NativeWallProfiler {
   }
 }
 
+NativeWallProfiler.prototype.getActiveSpan = function () {
+  const span = getActiveSpan()
+  if (span === undefined) {
+    return {}
+  }
+  const spanData = lastInstance._getProfilingContext(span)
+  updateContext(spanData)
+  return { spanId: spanData.spanId, rootSpanId: spanData.rootSpanId }
+}
+NativeWallProfiler.prototype.getSampleContext = function () {
+  const ctx = lastInstance._getSampleContext()
+  if (ctx === undefined) {
+    return {}
+  }
+  updateContext(ctx)
+  return { spanId: ctx.spanId, rootSpanId: ctx.rootSpanId }
+}
 module.exports = NativeWallProfiler
