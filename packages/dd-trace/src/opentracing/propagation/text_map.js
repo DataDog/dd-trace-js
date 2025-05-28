48eef4214f96ad721a6221b676fbb9fef8858530
Ida Liu <119438987+ida613@users.noreply.github.com>
2025-05-28 15:16:49 -0400

    Update packages/dd-trace/src/opentracing/propagation/text_map.js
    Co-authored-by: Ruben Bridgewater <ruben@bridgewater.de>

--- packages/dd-trace/src/opentracing/propagation/text_map.js
+++ packages/dd-trace/src/opentracing/propagation/text_map.js
@@ -651,7 +651,7 @@ class TextMapPropagator {
       }
       // the current code assumes precedence of ot-baggage- (legacy opentracing baggage) over baggage
       if (spanContext) {
-        if (key in spanContext._baggageItems) continue
+        if (Object.hasOwn(spanContext._baggageItems, key)) continue
         spanContext._baggageItems[key] = value
         if (this._config.baggageTagKeys === '*' || keysToSpanTag.has(key)) {
           spanContext._trace.tags['baggage.' + key] = value
