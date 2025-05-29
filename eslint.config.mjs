72aefc7be630b98d45ec717e21dfaec091571266
Ruben Bridgewater <ruben.bridgewater@datadoghq.com>
2025-05-29 02:33:47 +0200

    Activate unicorn/no-useless-undefined eslint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -131,7 +131,6 @@ export default [
       'unicorn/no-this-assignment': 'off', // too strict
       'unicorn/no-unreadable-array-destructuring': 'off', // TODO: undecided
       'unicorn/no-unreadable-iife': 'off', // too strict
-      'unicorn/no-useless-undefined': 'off', // 59 errors
       'unicorn/number-literal-case': 'off', // 44 errors
       'unicorn/numeric-separators-style': 'off', // 35 errors
       'unicorn/prefer-array-flat': 'off', // 9 errors
