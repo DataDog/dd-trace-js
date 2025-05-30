c27c9cd03fe59d13b93d9f9aef0ccca642e7c544
rochdev <roch.devost@gmail.com>
2025-05-30 14:14:45 -0400

    fix bindings not applying for noop stores

--- packages/dd-trace/src/plugins/tracing.js
+++ packages/dd-trace/src/plugins/tracing.js
@@ -51,6 +51,17 @@ class TracingPlugin extends Plugin {
     })
   }
 
+  bindStart (ctx) {
+    const store = ctx.hasOwnProperty('parentStore')
+      ? ctx.parentStore
+      : storage('legacy').getStore()
+
+    ctx.parentStore = store
+    ctx.currentStore = { ...store }
+
+    return ctx.currentStore
+  }
+
   start () {} // implemented by individual plugins
 
   finish (ctx) {
@@ -129,8 +140,12 @@ class TracingPlugin extends Plugin {
     if (enterOrCtx === true) {
       storage('legacy').enterWith({ ...store, span })
     } else if (enterOrCtx) {
-      enterOrCtx.parentStore = store
-      enterOrCtx.currentStore = { ...store, span }
+      if (enterOrCtx.currentStore) {
+        enterOrCtx.currentStore.span = span
+      } else {
+        enterOrCtx.parentStore = store
+        enterOrCtx.currentStore = { ...store, span }
+      }
     }
 
     return span
