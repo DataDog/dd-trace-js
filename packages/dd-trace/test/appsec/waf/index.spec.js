b8b60fb3306a7b8811b5e4c5b137281a9787844b
CarlesDD <carles.capell@datadoghq.com>
2025-05-30 07:15:32 +0200

    Revert tag deprecation

--- packages/dd-trace/test/appsec/waf/index.spec.js
+++ packages/dd-trace/test/appsec/waf/index.spec.js
@@ -68,6 +68,7 @@ describe('WAF Manager', () => {
       expect(Reporter.reportWafInit).to.have.been.calledWithExactly(
         '1.2.3',
         '1.0.0',
+        { loaded: ['rule_1'], failed: [] },
         true
       )
     })
@@ -90,6 +91,31 @@ describe('WAF Manager', () => {
       waf.init(rules, config.appsec)
 
       expect(Reporter.metricsQueue.set).to.have.been.calledWithExactly('_dd.appsec.waf.version', '1.2.3')
+      expect(Reporter.metricsQueue.set).to.have.been.calledWithExactly('_dd.appsec.event_rules.loaded', 1)
+      expect(Reporter.metricsQueue.set).to.have.been.calledWithExactly('_dd.appsec.event_rules.error_count', 0)
+      expect(Reporter.metricsQueue.set).not.to.have.been.calledWith('_dd.appsec.event_rules.errors')
+    })
+
+    it('should set init metrics with errors', () => {
+      DDWAF.version.returns('2.3.4')
+      DDWAF.prototype.diagnostics = {
+        rules: {
+          loaded: ['rule_1'],
+          failed: ['rule_2', 'rule_3'],
+          errors: {
+            error_1: ['invalid_1'],
+            error_2: ['invalid_2', 'invalid_3']
+          }
+        }
+      }
+
+      waf.init(rules, config.appsec)
+
+      expect(Reporter.metricsQueue.set).to.have.been.calledWithExactly('_dd.appsec.waf.version', '2.3.4')
+      expect(Reporter.metricsQueue.set).to.have.been.calledWithExactly('_dd.appsec.event_rules.loaded', 1)
+      expect(Reporter.metricsQueue.set).to.have.been.calledWithExactly('_dd.appsec.event_rules.error_count', 2)
+      expect(Reporter.metricsQueue.set).to.have.been.calledWithExactly('_dd.appsec.event_rules.errors',
+        '{"error_1":["invalid_1"],"error_2":["invalid_2","invalid_3"]}')
     })
   })
 
