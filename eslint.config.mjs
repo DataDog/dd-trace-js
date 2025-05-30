ad6d6df2f432ff84b8b54ea091063c454c6989cb
Ruben Bridgewater <ruben.bridgewater@datadoghq.com>
2025-05-31 00:46:02 +0200

    Activate prefer-exponentiation-operator eslint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -110,6 +110,7 @@ export default [
       'no-await-in-loop': 'error',
       'no-useless-assignment': 'error',
       'operator-assignment': 'error',
+      'prefer-exponentiation-operator': 'error',
       'prefer-object-has-own': 'error',
       'prefer-object-spread': 'error',
 
