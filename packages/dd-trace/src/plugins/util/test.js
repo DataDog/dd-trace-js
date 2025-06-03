cd910c4cf0c02731dc4363f443c382b071e95eaa
Mario Vidal Domínguez <60353145+Mariovido@users.noreply.github.com>
2025-06-03 15:33:32 +0200

    [test-optimization] [SDTEST-2122] Modify Impacted Tests algorithm (#5807)

--- packages/dd-trace/src/plugins/util/test.js
+++ packages/dd-trace/src/plugins/util/test.js
@@ -812,10 +812,6 @@ function getPullRequestBaseBranch (pullRequestBaseBranch) {
   const remoteName = getGitRemoteName()
 
   const sourceBranch = getSourceBranch()
-  const shortBranchName = sourceBranch.replace(new RegExp(`^${remoteName}/`), '')
-  if (BASE_LIKE_BRANCH_FILTER.test(shortBranchName)) {
-    return null
-  }
   // TODO: We will get the default branch name from the backend in the future.
   const POSSIBLE_DEFAULT_BRANCHES = ['main', 'master']
 
@@ -830,7 +826,8 @@ function getPullRequestBaseBranch (pullRequestBaseBranch) {
 
     const localBranches = getLocalBranches(remoteName)
     for (const branch of localBranches) {
-      if (branch !== sourceBranch && BASE_LIKE_BRANCH_FILTER.test(branch)) {
+      const shortBranchName = branch.replace(new RegExp(`^${remoteName}/`), '')
+      if (branch !== sourceBranch && BASE_LIKE_BRANCH_FILTER.test(shortBranchName)) {
         candidateBranches.push(branch)
       }
     }
