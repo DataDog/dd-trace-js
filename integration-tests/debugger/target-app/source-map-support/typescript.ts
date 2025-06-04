54d5898c7b5468ac3e9c987737075c8a19b28fcb
Thomas Watson <thomas.watson@datadoghq.com>
2025-06-04 13:46:13 +0200

    [DI] Support non-normalized source path names in source maps (#5821)
    In a source map, there’s an array called `sources`, which contains the
    paths to the original source files. The `source-map` dependency that we
    use will normalize each of the elements in the array when loading it.
    When the tracer loads these sources manually, to later supply them to
    the source-map dependency, it didn't previously normalize them. The
    result was that the `source-map` dependency couldn't match the
    non-normalized version with the normalized version, and hence couldn't
    look up the generated line and column number for that source file. This
    in turn meant that the probe couldn't be applied.
    
    This fixes that by normalizing the source pathnames when they are
    loaded by the tracers.

--- integration-tests/debugger/target-app/source-map-support/typescript.ts
+++ integration-tests/debugger/target-app/source-map-support/typescript.ts
@@ -1,12 +1,13 @@
 require('dd-trace/init')
 
 import { createServer } from 'node:http'
+import { sayHello } from './hello/world'
 
 const server = createServer((req, res) => {
   // Blank lines below to ensure line numbers in transpiled file differ from original file
 
 
-  res.end('hello world') // BREAKPOINT: /
+  res.end(sayHello()) // BREAKPOINT: /
 })
 
 server.listen(process.env.APP_PORT, () => {
