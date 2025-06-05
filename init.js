ea22eaa0d9d4e3804ca399ce70116972753f4e7c
Munir Abdinur <munir_abdinur@hotmail.com>
2025-06-05 15:54:25 -0400

    avoid incompatible syntax

--- init.js
+++ init.js
@@ -6,5 +6,7 @@ var guard = require('./packages/dd-trace/src/guardrails')
 
 module.exports = guard(function () {
   var INSTRUMENTED_BY_SSI = require('./packages/dd-trace/src/constants').INSTRUMENTED_BY_SSI
-  return require('.').init({ [INSTRUMENTED_BY_SSI]: 'ssi' })
+  var obj = {}
+  obj[INSTRUMENTED_BY_SSI] = 'ssi'
+  return require('.').init(obj)
 })
