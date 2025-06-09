e61a06904c26afa30b7477970c9171551072e81a
Mario Vidal Domínguez <mario.vidal@datadoghq.com>
2025-06-09 12:15:00 +0200

    Fix mocha tests

--- integration-tests/my-nyc.config.js
+++ integration-tests/my-nyc.config.js
@@ -1,5 +1,5 @@
 // non default name so that it only gets picked up intentionally
 module.exports = {
-  exclude: ['node_modules/**'],
+  exclude: ['node_modules/**', 'ci-visibility/test/failing-test.js'],
   include: process.env.NYC_INCLUDE ? JSON.parse(process.env.NYC_INCLUDE) : ['ci-visibility/test/**']
 }
