d8eb69c64f32bf87d98291174c794d99a5f6277a
Thomas Watson <thomas.watson@datadoghq.com>
2025-06-11 14:27:35 +0200

    Update packages/datadog-core/src/utils/src/set.js
    Co-authored-by: Ruben Bridgewater <ruben@bridgewater.de>

--- packages/datadog-core/src/utils/src/set.js
+++ packages/datadog-core/src/utils/src/set.js
@@ -1,21 +1,14 @@
 'use strict'
 
 module.exports = function set (object, path, value) {
-  let index = path.indexOf('.')
-  if (index === -1) {
-    object[path] = value
-    return
-  }
-
-  let property = object[path.slice(0, index)] ??= {}
-
+  let index = -1
   while (true) {
     const nextIndex = path.indexOf('.', index + 1)
     if (nextIndex === -1) {
-      property[path.slice(index + 1)] = value
+      object[path.slice(index + 1)] = value
       return
     }
-    property = property[path.slice(index + 1, nextIndex)] ??= {}
+    object = object[path.slice(index + 1, nextIndex)] ??= {}
     index = nextIndex
   }
 }
