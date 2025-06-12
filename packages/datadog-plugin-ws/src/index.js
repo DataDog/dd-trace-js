d17f4e38108a3dbb63a00fec554156323fa4f965
Crystal Magloire <crys.magloire@gmail.com>
2025-06-12 10:26:58 -0400

    updating websockets

--- packages/datadog-plugin-ws/src/index.js
+++ packages/datadog-plugin-ws/src/index.js
@@ -1,84 +1,22 @@
 'use strict'
 
-const TracingPlugin = require('../../dd-trace/src/plugins/tracing.js')
-const tags = require('../../../ext/tags')
+const CompositePlugin = require('../../dd-trace/src/plugins/composite')
 
-const HTTP_STATUS_CODE = tags.HTTP_STATUS_CODE
-const HTTP_REQUEST_HEADERS = tags.HTTP_REQUEST_HEADERS
-const HTTP_RESPONSE_HEADERS = tags.HTTP_RESPONSE_HEADERS
+const WSServerPlugin = require('./server')
+const WSProducerPlugin = require('./producer')
 
-class WSPlugin extends TracingPlugin {
-  static get id () { return 'ws' }
-  // static get id () { return 'http' }
-  static get prefix () { return 'tracing:ws:client:connect' }
-  static get type () { return 'websocket' }
-  static get kind () { return 'consumer' }
-
-  bindStart (ctx) {
-    const { http = {} } = ctx
-    const req = ctx.req
-
-    const options = {}
-    const headers = Object.entries(req.headers)
-    options.headers = Object.fromEntries(headers)
-    options.method = req.method
-
-    const agent = options.agent || options._defaultAgent || http.globalAgent || {}
-    const protocol = options.protocol || agent.protocol || 'http:'
-    const hostname = options.hostname || options.host || 'localhost'
-    const host = options.port ? `${hostname}:${options.port}` : hostname
-    const pathname = options.path || options.pathname
-    const path = pathname ? pathname.split(/[?#]/)[0] : '/'
-    const uri = `${protocol}//${host}${path}`
-
-    ctx.args = { options }
-
-    const span = this.startSpan(this.operationName(), {
-      meta: {
-        service: this.serviceName({ pluginConfig: this.config }),
-        'span.type': 'ws',
-        'http.upgraded': 'websocket',
-        'http.method': options.method,
-        'http.url': uri,
-        'resource.name': options.method,
-        'span.kind': 'server'
-
-      }
-
-    }, true)
-    ctx.span = span
-    ctx.currentStore = { span }
-
-    return ctx.currentStore
+class WSPlugin extends CompositePlugin {
+  static get id () { return 'websocket' }
+  static get plugins () {
+    return {
+      server: WSServerPlugin,
+      producer: WSProducerPlugin
+    }
   }
 
-  // asyncStart (ctx) {
-
-  //   ctx?.currentStore?.span.finish()
-
-  //   ctx.res = ctx.resStatus
-
-  //   ctx.currentStore.span.setTag(HTTP_STATUS_CODE, ctx.res)
-
-  //   return ctx.currentStore
-  // }
-
-  end (ctx) {
-    ctx.req.res = ctx.resStatus
-
-    ctx.span.setTag(HTTP_STATUS_CODE, ctx.req.res)
-    if (!ctx.span) return
-    ctx.span.finish()
+  configure (config) {
+    return super.configure(config)
   }
-
-  // asyncEnd (ctx) {
-  //   ctx.res = ctx.resStatus
-
-  //   ctx.currentStore.span.setTag(HTTP_STATUS_CODE, ctx.res)
-  //   ctx.span.setTag(HTTP_STATUS_CODE, ctx.res)
-
-  //   return this.finish(ctx)
-  // }
 }
 
 module.exports = WSPlugin
