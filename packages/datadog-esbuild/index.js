65e18d06b03fcd1711f0f19d1a0982773aca30b0
Ugaitz Urien <ugaitz.urien@datadoghq.com>
2025-06-17 18:38:28 +0200

    Improve support for esbuild + esm with DD_BUILD_ESM env var (#5888)
    Fix __dirname and __filename in esbuild + esm by adding globals to the banner option.

--- packages/datadog-esbuild/index.js
+++ packages/datadog-esbuild/index.js
@@ -53,8 +53,30 @@ for (const pkg of INSTRUMENTED) {
 
 module.exports.name = 'datadog-esbuild'
 
+function isESMBuild (build) {
+  // check toLowerCase? to be safe if unexpected object is there instead of a string
+  const format = build.initialOptions.format?.toLowerCase?.()
+  const outputFile = build.initialOptions.outfile?.toLowerCase?.()
+  const outExtension = build.initialOptions.outExtension?.['.js']
+  return format === 'esm' || outputFile?.endsWith('.mjs') || outExtension === '.mjs'
+}
+
 module.exports.setup = function (build) {
   const externalModules = new Set(build.initialOptions.external || [])
+  if (isESMBuild(build)) {
+    build.initialOptions.banner ??= {}
+    build.initialOptions.banner.js ??= ''
+    if (!build.initialOptions.banner.js.includes('import { createRequire as $dd_createRequire } from \'module\'')) {
+      build.initialOptions.banner.js = `import { createRequire as $dd_createRequire } from 'module';
+import { fileURLToPath as $dd_fileURLToPath } from 'url';
+import { dirname as $dd_dirname } from 'path';
+globalThis.require ??= $dd_createRequire(import.meta.url);
+globalThis.__filename ??= $dd_fileURLToPath(import.meta.url);
+globalThis.__dirname ??= $dd_dirname(globalThis.__filename);
+${build.initialOptions.banner.js}`
+    }
+  }
+
   build.onResolve({ filter: /.*/ }, args => {
     if (externalModules.has(args.path)) {
       // Internal Node.js packages will still be instrumented via require()
