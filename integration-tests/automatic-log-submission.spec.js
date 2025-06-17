d1425b3a060aa8c8c810d68cba33b0a662db132e
Ida.Liu <ida.liu@datadoghq.com>
2025-06-17 14:48:05 -0400

    update test

--- integration-tests/automatic-log-submission.spec.js
+++ integration-tests/automatic-log-submission.spec.js
@@ -174,7 +174,7 @@ describe('test visibility automatic log submission', () => {
         )
         childProcess.on('exit', () => {
           assert.include(testOutput, 'Hello simple log!')
-          assert.notInclude(testOutput, 'span_id')
+          assert.include(testOutput, 'span_id')
           done()
         })
 
