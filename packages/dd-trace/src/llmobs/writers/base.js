bbc988b2540528b3b885c2a878263b3de5135a22
Thomas Watson <thomas.watson@datadoghq.com>
2025-06-27 13:27:34 +0200

    Ensure buffer gets correctly stringified when logged (#5979)

--- packages/dd-trace/src/llmobs/writers/base.js
+++ packages/dd-trace/src/llmobs/writers/base.js
@@ -76,7 +76,7 @@ class BaseLLMObsWriter {
     this._bufferSize = 0
     const payload = this._encode(this.makePayload(events))
 
-    log.debug('Encoded LLMObs payload:', payload)
+    log.debug('Encoded LLMObs payload: %s', payload)
 
     const options = this._getOptions()
 
