3b0fac4087558bb19046ac128fc2286c4ee56ba0
William Conti <william.conti@datadoghq.com>
2025-06-30 14:27:17 -0400

    wrap normalized executor and execute functions

--- packages/datadog-instrumentations/src/graphql.js
+++ packages/datadog-instrumentations/src/graphql.js
@@ -359,12 +359,16 @@ function finishResolvers ({ fields }) {
   })
 }
 
-addHook({ name: '@graphql-tools/executor', file: 'cjs/execution/execute.js', versions: ['>=0.0.14'] }, execute => {
-  shimmer.wrap(execute, 'execute', wrapExecute(execute))
-  return execute
+addHook({ name: '@graphql-tools/executor', versions: ['>=0.0.14'] }, executor => {
+  // graphql-yoga (ESM) uses the normalizedExecutor function, so we need to wrap both. There is no risk in wrapping both
+  // since the functions are closely related, and our wrappedExecute function is prevents double calls with the
+  // contexts.has(contextValue) check.
+  shimmer.wrap(executor, 'execute', wrapExecute(executor))
+  shimmer.wrap(executor, 'normalizedExecutor', wrapExecute(executor))
+  return executor
 })
 
-addHook({ name: '@graphql-tools/executor', file: 'esm/execution/execute.js', versions: ['>=0.0.14'] }, execute => {
+addHook({ name: '@graphql-tools/executor', file: 'cjs/execution/execute.js', versions: ['>=0.0.14'] }, execute => {
   shimmer.wrap(execute, 'execute', wrapExecute(execute))
   return execute
 })
