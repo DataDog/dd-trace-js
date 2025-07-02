856a365655b34c51ce58a64a75fbc216bbadaca7
Crystal Magloire <crys.magloire@gmail.com>
2025-07-02 09:29:16 -0400

    adding traces for received messages

--- packages/datadog-plugin-ws/src/receiver.js
+++ packages/datadog-plugin-ws/src/receiver.js
@@ -4,34 +4,51 @@ const TracingPlugin = require('../../dd-trace/src/plugins/tracing.js')
 
 class WSReceiverPlugin extends TracingPlugin {
   static get id () { return 'websocket' }
-  // static get prefix () { return 'tracing:ws:send' }
+  static get prefix () { return 'tracing:ws:receive' }
   static get type () { return 'websocket' }
-  static get kind () { return 'producer' }
+  static get kind () { return 'receiver' }
 
   bindStart (ctx) {
-    // console.log('ctx in receiver', ctx)
-
     const span = this.startSpan(this.operationName(), {
       meta: {
         service: this.serviceName({ pluginConfig: this.config }),
-        // 'resource.name': 'websocket ' + ,
+        'resource.name': 'websocket.receive',
         'span.type': 'ws',
         'span.kind': 'receiver'
 
       }
 
-    }, true)
+    }, ctx)
 
     ctx.span = span
-    ctx.currentStore = { span }
 
     return ctx.currentStore
   }
 
+  bindAsyncStart (ctx) {
+    console.log('bind async start in receiver')
+    return ctx.parentStore
+  }
+
+  asyncStart (ctx) {
+    console.log('async start in recevier')
+    ctx.span.finish()
+    return ctx.parentStore
+  }
+
+  bindFinish (ctx) {
+    console.log('bind finish?')
+    return ctx.parentStore
+  }
+
   end (ctx) {
-    // console.log('ctx in end', ctx)
+    console.log('end in receiver')
+    if (!Object.hasOwn(ctx, 'result')) return
+
+    ctx.span.addLink(ctx.socket.spanContext)
 
     ctx.span.finish()
+    return ctx.parentStore
   }
 }
 
