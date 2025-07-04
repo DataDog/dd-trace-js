6969c19b4b0c3fb02c30f8dccd61168cdb0959d6
Thomas Watson <thomas.watson@datadoghq.com>
2025-07-04 14:22:00 +0200

    ESLint: Scope certain files to other Node.js versions than v18.0.0
    Not all of our files have Node.js 18.0.0 as the target version.

--- init.js
+++ init.js
@@ -1,7 +1,5 @@
 'use strict'
 
-/* eslint-disable no-var */
-
 // TODO: It shouldn't be necessary to disable n/no-unpublished-require - Research
 // eslint-disable-next-line n/no-unpublished-require
 var guard = require('./packages/dd-trace/src/guardrails')
