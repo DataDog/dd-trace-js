6e967d4c6a7f2cc266de99fd2ec34c2d3dc2bec9
rochdev <roch.devost@gmail.com>
2025-07-07 18:33:21 -0400

    fix appsec next test as well

--- packages/dd-trace/test/appsec/next/pages-dir/server.js
+++ packages/dd-trace/test/appsec/next/pages-dir/server.js
@@ -1,13 +1,13 @@
 'use strict'
 
-const { PORT, HOSTNAME } = process.env
+const { PORT = 0, HOSTNAME } = process.env
 
 const { createServer } = require('http')
 // eslint-disable-next-line n/no-deprecated-api
 const { parse } = require('url')
 const next = require('next')
 
-const app = next({ dir: __dirname, dev: false, quiet: true, hostname: HOSTNAME })
+const app = next({ dir: __dirname, dev: false, quiet: true, hostname: HOSTNAME, port: PORT })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
