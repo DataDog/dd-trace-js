34d5e3c4be643f8671e3a88f9cc7f64e158cb000
rochdev <roch.devost@gmail.com>
2025-07-08 16:19:47 -0400

    add comment

--- packages/datadog-plugin-fetch/test/integration-test/server.mjs
+++ packages/datadog-plugin-fetch/test/integration-test/server.mjs
@@ -1,5 +1,6 @@
 import 'dd-trace/init.js'
 
+// An arbitrary port is used here as we just need a request even if it fails.
 global.fetch('http://localhost:55555/foo')
   .then((response) => {})
   .then((data) => {})
