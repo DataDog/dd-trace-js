b15e6dbd999dd7f3c0cc965dcfed65417fbfc832
rochdev <roch.devost@gmail.com>
2025-07-08 14:04:05 -0400

    remove explicit hostname from appsec request helper test

--- packages/dd-trace/test/exporters/common/request.spec.js
+++ packages/dd-trace/test/exporters/common/request.spec.js
@@ -22,7 +22,7 @@ const initHTTPServer = () => {
 
     server.on('connection', socket => sockets.push(socket))
 
-    server.listen(0, '127.0.0.1', () => {
+    server.listen(0, () => {
       const shutdown = () => {
         sockets.forEach(socket => socket.end())
         server.close()
