fe0e057f3919782c1b2cfab5efb6bb5e458bdfa3
Roch Devost <roch.devost@datadoghq.com>
2025-07-10 14:44:44 -0400

    fix sqs test race condition when deleting the queue (#6068)

--- packages/dd-trace/test/setup/services/mysql.js
+++ packages/dd-trace/test/setup/services/mysql.js
@@ -9,7 +9,7 @@ function waitForMysql () {
 
     operation.attempt(currentAttempt => {
       const connection = mysql.createConnection({
-        host: 'localhost',
+        host: '127.0.0.1',
         user: 'root',
         database: 'db'
       })
