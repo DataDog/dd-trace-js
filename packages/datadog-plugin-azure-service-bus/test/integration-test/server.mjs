5ce42e1ef2b002fb4c791a905ae8c0b6568b29d3
Jordan <jordan.storms@datadoghq.com>
2025-07-10 14:48:05 -0400

    remove localhost from connectionstring

--- packages/datadog-plugin-azure-service-bus/test/integration-test/server.mjs
+++ packages/datadog-plugin-azure-service-bus/test/integration-test/server.mjs
@@ -1,7 +1,7 @@
 import 'dd-trace/init.js'
 import { ServiceBusClient } from '@azure/service-bus'
 
-const connectionString = 'Endpoint=sb://localhost;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=SAS_KEY_VALUE;UseDevelopmentEmulator=true;'
+const connectionString = 'Endpoint=sb://127.0.0.1;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=SAS_KEY_VALUE;UseDevelopmentEmulator=true;'
 const queueName = 'queue.1'
 
 const client = new ServiceBusClient(connectionString)
