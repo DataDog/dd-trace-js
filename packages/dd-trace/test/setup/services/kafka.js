d93fe8b23bcf8fa4bc1b302a5c201479a266f626
rochdev <roch.devost@gmail.com>
2025-07-10 21:43:55 -0400

    restore setup topic name

--- packages/dd-trace/test/setup/services/kafka.js
+++ packages/dd-trace/test/setup/services/kafka.js
@@ -10,7 +10,7 @@ const kafka = new Kafka({
 const admin = kafka.admin()
 const producer = kafka.producer()
 const consumer = kafka.consumer({ groupId: 'setup-group' })
-const topic = 'setup-topic'
+const topic = 'test-topic'
 const messages = [{ key: 'setup', value: 'test' }]
 
 function waitForKafka () {
