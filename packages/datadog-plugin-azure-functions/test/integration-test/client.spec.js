a8654a47edec753a103ed7a99f7859cb393c8a0a
Jordan Storms <jordan.storms@datadoghq.com>
2025-07-14 11:15:28 -0400

    [SVLS-7133] Instrument Azure Service Bus (#6067)
    * initial
    
    * plugin loading
    
    * full distributed trace
    
    * all span attributes set correctly
    
    * add consumer tests
    
    * update test
    
    * finalize service bus test
    
    * remove qpid from service bus test
    
    * instrument and test topics
    
    * remove localhost from connectionstring
    
    * lint
    
    * try removing localhost
    
    * revert localhost removal
    
    * try to use newer node versions
    
    * revert node versioning for integration tests
    
    * fix azure-functions test
    
    * fix sql server & lint
    
    * try actual name of sql service
    
    * clean up azure functions
    
    * formatting
    
    * lint

--- packages/datadog-plugin-azure-functions/test/integration-test/client.spec.js
+++ packages/datadog-plugin-azure-functions/test/integration-test/client.spec.js
@@ -20,8 +20,13 @@ describe('esm', () => {
   withVersions('azure-functions', '@azure/functions', NODE_MAJOR < 20 ? '<4.7.3' : '*', version => {
     before(async function () {
       this.timeout(50000)
-      sandbox = await createSandbox([`@azure/functions@${version}`, 'azure-functions-core-tools@4'], false,
-        ['./packages/datadog-plugin-azure-functions/test/integration-test/fixtures/*'])
+      sandbox = await createSandbox([
+        `@azure/functions@${version}`,
+        'azure-functions-core-tools@4',
+        '@azure/service-bus@7.9.2'
+      ],
+      false,
+      ['./packages/datadog-plugin-azure-functions/test/integration-test/fixtures/*'])
     })
 
     after(async function () {
@@ -54,7 +59,7 @@ describe('esm', () => {
       })
     }).timeout(50000)
 
-    it('propagates context to child requests', async () => {
+    it('propagates context to child http requests', async () => {
       const envArgs = {
         PATH: `${sandbox.folder}/node_modules/azure-functions-core-tools/bin:${process.env.PATH}`
       }
@@ -62,7 +67,43 @@ describe('esm', () => {
 
       return curlAndAssertMessage(agent, 'http://127.0.0.1:7071/api/httptest2', ({ headers, payload }) => {
         assert.strictEqual(payload.length, 2)
-        assert.strictEqual(payload[0][0].parent_id, payload[1][1].span_id)
+        assert.strictEqual(payload[1][0].span_id, payload[1][1].parent_id)
+      })
+    }).timeout(50000)
+
+    it('propagates context through a service bus queue', async () => {
+      const envArgs = {
+        PATH: `${sandbox.folder}/node_modules/azure-functions-core-tools/bin:${process.env.PATH}`
+      }
+      proc = await spawnPluginIntegrationTestProc(sandbox.folder, 'func', ['start'], agent.port, undefined, envArgs)
+
+      return curlAndAssertMessage(agent, 'http://127.0.0.1:7071/api/httptest3', ({ headers, payload }) => {
+        assert.strictEqual(payload.length, 3)
+        assert.strictEqual(payload[1][1].span_id, payload[2][0].parent_id)
+        assert.strictEqual(payload[2][0].name, 'azure.functions.invoke')
+        assert.strictEqual(payload[2][0].resource, 'ServiceBus queueTest')
+        assert.strictEqual(payload[2][0].meta['messaging.destination.name'], 'queue.1')
+        assert.strictEqual(payload[2][0].meta['messaging.operation'], 'receive')
+        assert.strictEqual(payload[2][0].meta['messaging.system'], 'servicebus')
+        assert.strictEqual(payload[2][0].meta['span.kind'], 'consumer')
+      })
+    }).timeout(50000)
+
+    it('propagates context through a service bus topic', async () => {
+      const envArgs = {
+        PATH: `${sandbox.folder}/node_modules/azure-functions-core-tools/bin:${process.env.PATH}`
+      }
+      proc = await spawnPluginIntegrationTestProc(sandbox.folder, 'func', ['start'], agent.port, undefined, envArgs)
+
+      return curlAndAssertMessage(agent, 'http://127.0.0.1:7071/api/httptest4', ({ headers, payload }) => {
+        assert.strictEqual(payload.length, 3)
+        assert.strictEqual(payload[1][1].span_id, payload[2][0].parent_id)
+        assert.strictEqual(payload[2][0].name, 'azure.functions.invoke')
+        assert.strictEqual(payload[2][0].resource, 'ServiceBus topicTest')
+        assert.strictEqual(payload[2][0].meta['messaging.destination.name'], 'topic.1')
+        assert.strictEqual(payload[2][0].meta['messaging.operation'], 'receive')
+        assert.strictEqual(payload[2][0].meta['messaging.system'], 'servicebus')
+        assert.strictEqual(payload[2][0].meta['span.kind'], 'consumer')
       })
     }).timeout(50000)
   })
