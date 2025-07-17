2f9a7a11010b6b7104df54ced4cc0bdf3184222a
Roch Devost <roch.devost@datadoghq.com>
2025-07-17 10:54:35 -0400

    fix release status script reading response text twice

--- scripts/release/status.js
+++ scripts/release/status.js
@@ -22,18 +22,24 @@ async function checkStatuses (contexts) {
   })
 
   if (response.status !== 200) {
+    const responseText = await response.text?.()
+
     console.log(response)
-    console.log(response.text())
+    console.log(responseText)
 
     throw new Error(
-      util.format('Could not get status from GitHub.\n\n%o\n\n%s', response, response.text?.())
+      util.format('Could not get status from GitHub.\n\n%o\n\n%s', response, responseText)
     )
   }
 
   const { statuses } = JSON.parse(await response.text())
 
+  // console.log(statuses)
+
   for (const status of statuses) {
     if (!contexts.has(status.context)) continue
+
+    console.log(status)
 
     switch (status.state) {
       case 'success':
