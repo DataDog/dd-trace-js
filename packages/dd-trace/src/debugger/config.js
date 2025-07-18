3c9fc50bd9ec2ca891823dd3888d38e099585eec
Thomas Watson <thomas.watson@datadoghq.com>
2025-07-18 22:13:03 +0200

    [DI] Support custom logger in worker thread (#6128)
    If the tracer is configured with a custom logger, it should be used in
    the Dynamic Instrumentation/Live Debugging worker thread.
    
    This changes how logs are handled in the worker thread: Instead of
    logging them directly to STDOUT/STDERR, they are sent back to the main
    thread for processing in the main logger.

--- packages/dd-trace/src/debugger/config.js
+++ packages/dd-trace/src/debugger/config.js
@@ -3,8 +3,10 @@
 module.exports = function getDebuggerConfig (config) {
   return {
     commitSHA: config.commitSHA,
+    debug: config.debug,
     dynamicInstrumentation: config.dynamicInstrumentation,
     hostname: config.hostname,
+    logLevel: config.logLevel,
     port: config.port,
     repositoryUrl: config.repositoryUrl,
     runtimeId: config.tags['runtime-id'],
