d5575f9595029cbf1b67c5696401c3b2db2b294a
ishabi <ilyasshabi94@gmail.com>
2025-07-24 15:52:19 +0200

    fix file naming

--- packages/dd-trace/test/appsec/graphql.apollo-server.plugin.spec.js
+++ packages/dd-trace/test/appsec/graphql.apollo-server.plugin.spec.js
@@ -6,7 +6,7 @@ const {
   schema,
   resolvers,
   graphqlCommonTests
-} = require('./graphq.test-utils')
+} = require('./graphql.test-utils')
 
 withVersions('apollo-server', '@apollo/server', apolloServerVersion => {
   const config = {}
