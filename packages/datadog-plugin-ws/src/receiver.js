cc82f1340ef7d29f182818cc81306a09e8d4a963
Crystal Magloire <crys.magloire@gmail.com>
2025-07-25 10:26:36 -0400

    Adding fragment count

--- packages/datadog-plugin-ws/src/receiver.js
+++ packages/datadog-plugin-ws/src/receiver.js
@@ -3,56 +3,47 @@
 const TracingPlugin = require('../../dd-trace/src/plugins/tracing.js')
 
 class WSReceiverPlugin extends TracingPlugin {
-  static get id () { return 'ws' }
+  static get id () { return 'websocket' }
   static get prefix () { return 'tracing:ws:receive' }
   static get type () { return 'websocket' }
-  static get kind () { return 'receiver' }
+  static get kind () { return 'consumer' }
 
   bindStart (ctx) {
-
-    // console.log('ctx.socket.spanContext', ctx.binary)
+    const { spanTags } = ctx.socket.spanContext
+    const path = spanTags['resource.name'].split(' ')[1]
     const opCode = ctx.binary ? 'binary' : 'text'
-    
-    // console.log('receiving', opCode, ctx.byteLength)
+
     const span = this.startSpan(this.operationName(), {
       meta: {
         service: this.serviceName({ pluginConfig: this.config }),
-        // 'resource.name': `websocket ${path}`,
         'span.type': 'websocket',
         'span.kind': 'consumer',
-        'dd.kind': 'executed_by'
+        'resource.name': `websocket ${path}`,
+        'dd.kind': 'executed_by',
       },
       metrics: {
         'websocket.message.type': opCode,
         'websocket.message.length': ctx.byteLength,
-        // 'websocket.message.frames':
+        '_dd.dm.inherited': 1,
+        '_dd.dm.service': spanTags['service.name'],
+        '_dd.dm.resource': spanTags['resource.name']
       }
 
     }, ctx)
 
     ctx.span = span
-
     return ctx.currentStore
   }
 
   bindAsyncStart (ctx) {
-    console.log('bind async start in receiver')
     return ctx.parentStore
   }
 
   asyncStart (ctx) {
-    // console.log('async start in recevier')
     ctx.span.finish()
-    return ctx.parentStore
-  }
-
-  bindFinish (ctx) {
-    // console.log('bind finish?')
-    return ctx.parentStore
   }
 
   end (ctx) {
-    // console.log('end in receiver')
     if (!Object.hasOwn(ctx, 'result')) return
 
     ctx.span.addLink(ctx.socket.spanContext)
