1173dfc3f545a809d2eb7eecb37188343699f6f0
Michael Zhao <michael.zhao@datadoghq.com>
2025-07-28 16:53:54 -0400

    add comments

--- packages/dd-trace/src/datastreams/checkpointer.js
+++ packages/dd-trace/src/datastreams/checkpointer.js
@@ -9,6 +9,12 @@ class DataStreamsCheckpointer {
     this.dsmProcessor = tracer._dataStreamsProcessor
   }
 
+  /**
+   * @param {string} type - The type of the checkpoint, usually the streaming technology being used.
+   *                       Examples include kafka, kinesis, sns etc.
+   * @param {string} target - The target of data. This can be a topic, exchange or stream name.
+   * @param {Object} carrier - The carrier object to inject context into.
+   */
   setProduceCheckpoint (type, target, carrier) {
     if (!this.config.dsmEnabled) return
 
@@ -23,6 +29,15 @@ class DataStreamsCheckpointer {
     this.tracer.inject(ctx, 'text_map_dsm', carrier)
   }
 
+  /**
+   * @param {string} type - The type of the checkpoint, usually the streaming technology being used.
+   *                       Examples include kafka, kinesis, sns etc.
+   * @param {string} source - The source of data. This can be a topic, exchange or stream name.
+   * @param {Object} carrier - The carrier object to extract context from.
+   * @param {boolean} [manualCheckpoint=true] - Whether this checkpoint was manually set. Keep true if manually instrumenting.
+   *                                           Manual instrumentation always overrides automatic instrumentation in the case a call is both
+   *                                           manually and automatically instrumented.
+   */
   setConsumeCheckpoint (type, source, carrier, manualCheckpoint = true) {
     if (!this.config.dsmEnabled) return
 
