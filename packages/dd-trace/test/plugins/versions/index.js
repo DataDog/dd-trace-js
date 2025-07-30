d7fc1f83569d50690950a074149a0770a0a270f3
rochdev <roch.devost@gmail.com>
2025-07-30 09:37:08 -0400

    fix support for dash

--- packages/dd-trace/test/plugins/versions/index.js
+++ packages/dd-trace/test/plugins/versions/index.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const { subset } = require('semver')
 const latests = require('./package.json').dependencies
 
 /**
@@ -7,16 +8,37 @@ const latests = require('./package.json').dependencies
  * @param {string} range
  */
 function getCappedRange (name, range) {
-  const alreadyCapped = range.split('||').every(r => /^=?\d+\.\d+\.\d+$/.test(r))
+  const subranges = range.split('||')
+  const alreadyCapped = subranges.every(r => /^=?\d+\.\d+\.\d+$/.test(r))
 
-  if (alreadyCapped) return range
-  if (!latests[name]) {
-    throw new Error(
-      `Latest version for '${name}' needs to be defined in 'packages/dd-trace/test/plugins/versions/package.json'.`
-    )
+  let cappedRanges = []
+
+  for (const subrange in subranges) {
+    if (/^=?\d+\.\d+\.\d+$/.test(subrange)) {
+      cappedRanges.push(subrange)
+      continue
+    }
+
+    if (!latests[name]) {
+      throw new Error(
+        `Latest version for '${name}' needs to be defined in 'packages/dd-trace/test/plugins/versions/package.json'.`
+      )
+    }
+
+    if (subrange.includes('-')) {
+      const parts = subrange.split('-').map(p => p.trim())
+
+      if (subset(parts[1], `<=${latests[name]}`)) {
+        cappedRanges.push(subrange)
+      } else {
+        cappedRanges.push(`${parts[0]} - ${latests[name]}`)
+      }
+    } else {
+      cappedRanges.push(subrange ? `${subrange} <=${latests[name]}` : latests[name])
+    }
   }
 
-  return range ? `${range} <=${latests[name]}` : latests[name]
+  return cappedRanges.join(' || ')
 }
 
 module.exports = {
