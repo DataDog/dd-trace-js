5cd0c3ae7ac499334d8bd5d4b643de7246215033
simon-id <simon.id@datadoghq.com>
2025-07-31 05:47:16 +0200

    Update packages/datadog-instrumentations/src/apollo-server.js

--- packages/datadog-instrumentations/src/apollo-server.js
+++ packages/datadog-instrumentations/src/apollo-server.js
@@ -112,13 +112,11 @@ function apolloDrainHttpServerHook (drainModule) {
   return drainModule
 }
 
-addHook({ name: '@apollo/server', file: 'dist/cjs/ApolloServer.js', versions: ['>=4.0.0 <5.0.0'] }, apolloServerHook)
+addHook({ name: '@apollo/server', file: 'dist/cjs/ApolloServer.js', versions: ['4'] }, apolloServerHook)
 
-addHook({ name: '@apollo/server', file: 'dist/cjs/express4/index.js', versions: ['>=4.0.0 <5.0.0'] },
-  apolloExpress4Hook)
+addHook({ name: '@apollo/server', file: 'dist/cjs/express4/index.js', versions: ['4'] }, apolloExpress4Hook)
 
-addHook({ name: '@apollo/server', file: 'dist/cjs/utils/HeaderMap.js', versions: ['>=4.0.0 <5.0.0'] },
-  apolloHeaderMapHook)
+addHook({ name: '@apollo/server', file: 'dist/cjs/utils/HeaderMap.js', versions: ['4'] }, apolloHeaderMapHook)
 
 addHook(
   { name: '@apollo/server', file: 'dist/cjs/plugin/drainHttpServer/index.js', versions: ['>=5.0.0'] },
