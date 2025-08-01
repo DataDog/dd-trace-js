d5edd2652026516a58e4313a8d82c944effa2942
William Conti <william.conti@datadoghq.com>
2025-08-01 13:39:37 -0400

    remove async storage from ldapjs

--- packages/dd-trace/src/appsec/iast/analyzers/ldap-injection-analyzer.js
+++ packages/dd-trace/src/appsec/iast/analyzers/ldap-injection-analyzer.js
@@ -2,6 +2,7 @@
 const InjectionAnalyzer = require('./injection-analyzer')
 const { LDAP_INJECTION } = require('../vulnerabilities')
 const { getNodeModulesPaths } = require('../path-line')
+const { storage } = require('../../../../../datadog-core')
 
 const EXCLUDED_PATHS = getNodeModulesPaths('ldapjs-promise')
 
@@ -12,6 +13,13 @@ class LdapInjectionAnalyzer extends InjectionAnalyzer {
 
   onConfigure () {
     this.addSub('datadog:ldapjs:client:search', ({ base, filter }) => this.analyzeAll(base, filter))
+    this.addBind('datadog:ldapjs:function:bind:start', (ctx) => {
+      ctx.parentStore = storage('legacy').getStore()
+      return ctx.parentStore
+    })
+    this.addBind('datadog:ldapjs:function:bind:finish', (ctx) => {
+      return ctx.parentStore
+    })
   }
 
   _getExcludedPaths () {
