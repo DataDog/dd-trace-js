7d777614012329af5dd64c0dcadbde4b4baac506
Crystal Magloire <crys.magloire@gmail.com>
2025-09-04 14:23:47 -0400

    updating plugins to not emit traces that do not have span context

--- packages/datadog-plugin-ws/src/close.js
+++ packages/datadog-plugin-ws/src/close.js
@@ -9,13 +9,19 @@ class WSClosePlugin extends TracingPlugin {
   static get kind () { return 'close' }
 
   bindStart (ctx) {
-    const messagesEnabled = this.config.traceWebsocketMessagesEnabled
-    if (!messagesEnabled) return
+    const {
+      traceWebsocketMessagesEnabled,
+      traceWebsocketMessagesInheritSampling,
+      traceWebsocketMessagesSeparateTraces
+    } = this.config
+    if (!traceWebsocketMessagesEnabled) return
 
     const { code, data, socket, isPeerClose } = ctx
+    if (!socket.spanContext) return
+
     const spanKind = isPeerClose ? 'consumer' : 'producer'
-    const spanTags = socket.spanContext ? socket.spanContext.spanTags : {}
-    const path = socket.spanContext ? spanTags['resource.name'].split(' ')[1] : '/'
+    const spanTags = socket.spanContext.spanTags
+    const path = spanTags['resource.name'].split(' ')[1]
     const service = this.serviceName({ pluginConfig: this.config })
     const span = this.startSpan(this.operationName(), {
       service,
@@ -32,7 +38,7 @@ class WSClosePlugin extends TracingPlugin {
       span.setTag('websocket.close.reason', data.toString())
     }
 
-    if (isPeerClose) {
+    if (isPeerClose && traceWebsocketMessagesInheritSampling && traceWebsocketMessagesSeparateTraces) {
       span.setTag('_dd.dm.service', spanTags['service.name'] || service)
       span.setTag('_dd.dm.resource', spanTags['resource.name'] || `websocket ${path}`)
       span.setTag('_dd.dm.inherited', 1)
