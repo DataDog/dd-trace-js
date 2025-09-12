556ae2fb84c55afc5c141c6ce6f09d030d05b0e4
perhardhernandez <pablo.erhardhernandez@datadoghq.com>
2025-09-12 14:44:33 -0400

    express tests back to original state

--- packages/datadog-plugin-express/test/integration-test/client.spec.js
+++ packages/datadog-plugin-express/test/integration-test/client.spec.js
@@ -4,8 +4,7 @@ const {
   FakeAgent,
   createSandbox,
   curlAndAssertMessage,
-  spawnPluginIntegrationTestProc,
-  varySandbox
+  spawnPluginIntegrationTestProc
 } = require('../../../../integration-tests/helpers')
 const { withVersions } = require('../../../dd-trace/test/setup/mocha')
 const { assert } = require('chai')
@@ -15,18 +14,12 @@ describe('esm', () => {
   let agent
   let proc
   let sandbox
-  let variants
 
   withVersions('express', 'express', version => {
     before(async function () {
       this.timeout(50000)
       sandbox = await createSandbox([`'express@${version}'`], false,
         ['./packages/datadog-plugin-express/test/integration-test/*'])
-      variants = varySandbox(sandbox, 'server.mjs', {
-        default: 'import express from \'express\'',
-        star: 'import * as starExpress from \'express\'; const express = starExpress.default;',
-        destructure: 'import { default as express } from \'express\';'
-      })
     })
 
     after(async function () {
@@ -42,47 +35,46 @@ describe('esm', () => {
       proc && proc.kill()
       await agent.stop()
     })
-    for (const variant of ['default', 'star', 'destructure']) {
-      describe('with DD_TRACE_MIDDLEWARE_TRACING_ENABLED unset', () => {
-        it('is instrumented', async () => {
-          proc = await spawnPluginIntegrationTestProc(sandbox.folder, variants[variant], agent.port)
-          const numberOfSpans = semver.intersects(version, '<5.0.0') ? 4 : 3
 
-          return curlAndAssertMessage(agent, proc, ({ headers, payload }) => {
-            assert.propertyVal(headers, 'host', `127.0.0.1:${agent.port}`)
-            assert.isArray(payload)
-            assert.strictEqual(payload.length, 1)
-            assert.isArray(payload[0])
-            assert.strictEqual(payload[0].length, numberOfSpans)
-            assert.propertyVal(payload[0][0], 'name', 'express.request')
-            assert.propertyVal(payload[0][1], 'name', 'express.middleware')
-          })
-        }).timeout(50000)
+    describe('with DD_TRACE_MIDDLEWARE_TRACING_ENABLED unset', () => {
+      it('is instrumented', async () => {
+        proc = await spawnPluginIntegrationTestProc(sandbox.folder, 'server.mjs', agent.port)
+        const numberOfSpans = semver.intersects(version, '<5.0.0') ? 4 : 3
+
+        return curlAndAssertMessage(agent, proc, ({ headers, payload }) => {
+          assert.propertyVal(headers, 'host', `127.0.0.1:${agent.port}`)
+          assert.isArray(payload)
+          assert.strictEqual(payload.length, 1)
+          assert.isArray(payload[0])
+          assert.strictEqual(payload[0].length, numberOfSpans)
+          assert.propertyVal(payload[0][0], 'name', 'express.request')
+          assert.propertyVal(payload[0][1], 'name', 'express.middleware')
+        })
+      }).timeout(50000)
+    })
+
+    describe('with DD_TRACE_MIDDLEWARE_TRACING_ENABLED=true', () => {
+      before(() => {
+        process.env.DD_TRACE_MIDDLEWARE_TRACING_ENABLED = false
       })
 
-      describe('with DD_TRACE_MIDDLEWARE_TRACING_ENABLED=true', () => {
-        before(() => {
-          process.env.DD_TRACE_MIDDLEWARE_TRACING_ENABLED = false
-        })
-
-        after(() => {
-          delete process.env.DD_TRACE_MIDDLEWARE_TRACING_ENABLED
-        })
-
-        it('disables middleware spans when config.middlewareTracingEnabled is false via env var', async () => {
-          proc = await spawnPluginIntegrationTestProc(sandbox.folder, variants[variant], agent.port)
-          const numberOfSpans = 1
-
-          return curlAndAssertMessage(agent, proc, ({ headers, payload }) => {
-            assert.propertyVal(headers, 'host', `127.0.0.1:${agent.port}`)
-            assert.isArray(payload)
-            assert.strictEqual(payload.length, 1)
-            assert.isArray(payload[0])
-            assert.strictEqual(payload[0].length, numberOfSpans)
-            assert.propertyVal(payload[0][0], 'name', 'express.request')
-          })
-        }).timeout(50000)
+      after(() => {
+        delete process.env.DD_TRACE_MIDDLEWARE_TRACING_ENABLED
       })
-    }
+
+      it('disables middleware spans when config.middlewareTracingEnabled is false via env var', async () => {
+        proc = await spawnPluginIntegrationTestProc(sandbox.folder, 'server.mjs', agent.port)
+        const numberOfSpans = 1
+
+        return curlAndAssertMessage(agent, proc, ({ headers, payload }) => {
+          assert.propertyVal(headers, 'host', `127.0.0.1:${agent.port}`)
+          assert.isArray(payload)
+          assert.strictEqual(payload.length, 1)
+          assert.isArray(payload[0])
+          assert.strictEqual(payload[0].length, numberOfSpans)
+          assert.propertyVal(payload[0][0], 'name', 'express.request')
+        })
+      }).timeout(50000)
+    })
   })
 })
