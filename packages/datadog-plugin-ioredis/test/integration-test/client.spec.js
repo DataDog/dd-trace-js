2ed771e0ea51b84e6ff38fc88063d9dc804ac742
Bryan English <bryan.english@datadoghq.com>
2025-09-11 22:08:01 -0400

    update ioredis for new default handling, and vary import tests

--- packages/datadog-plugin-ioredis/test/integration-test/client.spec.js
+++ packages/datadog-plugin-ioredis/test/integration-test/client.spec.js
@@ -4,7 +4,8 @@ const {
   FakeAgent,
   createSandbox,
   checkSpansForServiceName,
-  spawnPluginIntegrationTestProc
+  spawnPluginIntegrationTestProc,
+  varySandbox
 } = require('../../../../integration-tests/helpers')
 const { withVersions } = require('../../../dd-trace/test/setup/mocha')
 const { assert } = require('chai')
@@ -13,11 +14,17 @@ describe('esm', () => {
   let agent
   let proc
   let sandbox
+  let variants
   withVersions('ioredis', 'ioredis', version => {
     before(async function () {
       this.timeout(20000)
       sandbox = await createSandbox([`'ioredis@${version}'`], false, [
         './packages/datadog-plugin-ioredis/test/integration-test/*'])
+      variants = varySandbox(sandbox, 'server.mjs', {
+        default: `import Redis from 'ioredis'`,
+        star: `import * as modRedis from 'ioredis'; const { default: Redis } = modRedis`,
+        destructure: `import { default as Redis } from 'ioredis'`
+      })
     })
 
     after(async () => {
@@ -33,16 +40,18 @@ describe('esm', () => {
       await agent.stop()
     })
 
-    it('is instrumented', async () => {
-      const res = agent.assertMessageReceived(({ headers, payload }) => {
-        assert.propertyVal(headers, 'host', `127.0.0.1:${agent.port}`)
-        assert.isArray(payload)
-        assert.strictEqual(checkSpansForServiceName(payload, 'redis.command'), true)
-      })
+    for (const variant of ['default', 'star', 'destructure']) {
+      it(`is instrumented (${variant})`, async () => {
+        const res = agent.assertMessageReceived(({ headers, payload }) => {
+          assert.propertyVal(headers, 'host', `127.0.0.1:${agent.port}`)
+          assert.isArray(payload)
+          assert.strictEqual(checkSpansForServiceName(payload, 'redis.command'), true)
+        })
 
-      proc = await spawnPluginIntegrationTestProc(sandbox.folder, 'server.mjs', agent.port)
+        proc = await spawnPluginIntegrationTestProc(sandbox.folder, variants[variant], agent.port)
 
-      await res
-    }).timeout(20000)
+        await res
+      }).timeout(20000)
+    }
   })
 })
