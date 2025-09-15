1d593ebeac5757295a81e3194063d47f9ff98414
William Conti <william.conti@datadoghq.com>
2025-09-15 10:48:32 -0400

    remove mcp dir

--- mcp/dd-apm-v2/scaffolder/.eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: ['../../../.eslintrc.js'],
-  rules: {
-    'no-console': 'off', // CLI tools need console output
-    'max-len': ['error', { code: 130 }] // Allow slightly longer lines for CLI messages
-  }
-}
