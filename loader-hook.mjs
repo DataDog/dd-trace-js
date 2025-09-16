50f14b453ee36dfc30135cb6f036210f4853b241
rochdev <roch.devost@gmail.com>
2025-09-16 16:16:34 -0400

    add include configuration to iitm loader hook

--- loader-hook.mjs
+++ loader-hook.mjs
@@ -1,1 +1,29 @@
-export * from 'import-in-the-middle/hook.mjs'
+// export * from 'import-in-the-middle/hook.mjs'
+
+import * as iitm from 'import-in-the-middle/hook.mjs'
+import hooks from './packages/datadog-instrumentations/src/helpers/hooks.js'
+
+const nodules = Object.keys(hooks)
+
+function initialize (data = {}) {
+  data.include ??= []
+  data.exclude ??= []
+  data.exclude.push(
+    /middle/,
+    /langsmith/,
+    /openai\/_shims/,
+    /openai\/resources\/chat\/completions\/messages/,
+    /openai\/agents-core\/dist\/shims/,
+    /@anthropic-ai\/sdk\/_shims/
+  )
+
+  for (const nodule of nodules) {
+    data.include.push(new RegExp(`node_modules/${nodule}`), nodule)
+    data.exclude.push(new RegExp(`node_modules/${nodule}/node_modules`))
+  }
+
+  return iitm.initialize(data)
+}
+
+export { initialize }
+export { load, getFormat, resolve, getSource } from 'import-in-the-middle/hook.mjs'
