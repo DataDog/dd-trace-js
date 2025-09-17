e9cae600e76276266aa748ceb0f5abeee2295b6f
Sam Brenner <106700075+sabrenner@users.noreply.github.com>
2025-09-17 17:01:14 -0400

    ci(llmobs): resolve some test flakiness (#6316)

--- packages/dd-trace/test/llmobs/util.js
+++ packages/dd-trace/test/llmobs/util.js
@@ -197,6 +197,7 @@ function fromBuffer (spanProperty, isNumber = false) {
 
 const agent = require('../plugins/agent')
 const assert = require('node:assert')
+const { useEnv } = require('../../../../integration-tests/helpers')
 
 /**
  * @param {Object} options
@@ -205,7 +206,7 @@ const assert = require('node:assert')
  * @param {Object} options.closeOptions
  * @returns {function(): Promise<{ apmSpans: Array, llmobsSpans: Array }>}
  */
-function useLlmobs ({
+function useLlmObs ({
   plugin,
   tracerConfigOptions = {
     llmobs: {
@@ -213,7 +214,7 @@ function useLlmobs ({
       agentlessEnabled: false
     }
   },
-  closeOptions = { ritmReset: false, wipe: true }
+  closeOptions = { ritmReset: false }
 }) {
   if (!plugin) {
     throw new TypeError(
@@ -229,6 +230,10 @@ function useLlmobs ({
 
   let apmTracesPromise
   let llmobsTracesPromise
+
+  useEnv({
+    _DD_LLMOBS_FLUSH_INTERVAL: 0
+  })
 
   before(() => {
     return agent.load(plugin, {}, tracerConfigOptions)
@@ -264,7 +269,7 @@ module.exports = {
   expectedLLMObsLLMSpanEvent,
   expectedLLMObsNonLLMSpanEvent,
   deepEqualWithMockValues,
-  useLlmobs,
+  useLlmObs,
   MOCK_ANY,
   MOCK_NUMBER,
   MOCK_STRING,
