c8eb5e9ecd59784039f29a0ee895edd86a9cdac4
Crystal Magloire <crys.magloire@gmail.com>
2025-09-18 13:32:15 -0400

    adding timeout

--- packages/datadog-plugin-couchbase/test/integration-test/client.spec.js
+++ packages/datadog-plugin-couchbase/test/integration-test/client.spec.js
@@ -17,6 +17,7 @@ describe('esm', () => {
   // test against later versions because server.mjs uses newer package syntax
   withVersions('couchbase', 'couchbase', '>=4.0.0', version => {
     before(async function () {
+      this.timeout(10000)
       sandbox = await createSandbox([`'couchbase@${version}'`], false, [
         './packages/datadog-plugin-couchbase/test/integration-test/*'])
     })
