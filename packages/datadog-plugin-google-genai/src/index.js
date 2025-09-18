30f981beebbee639e2bfd2233040b3c0d902e4b1
Crystal Magloire <crys.magloire@gmail.com>
2025-09-18 09:05:54 -0400

    instrumentation of genai

---
+++ packages/datadog-plugin-google-genai/src/index.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const CompositePlugin = require('../../dd-trace/src/plugins/composite')
+const GenAiTracingPlugin = require('./tracing')
+const GenAiLLMObsPlugin = require('../../dd-trace/src/llmobs/plugins/openai')
+
+class GenAiPlugin extends CompositePlugin {
+  static id = 'genai'
+  static get plugins () {
+    return {
+      llmobs: GenAiLLMObsPlugin,
+      tracing: GenAiTracingPlugin
+    }
+  }
+}
+
+module.exports = GenAiPlugin
