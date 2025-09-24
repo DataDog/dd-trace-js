287fdf233d38e3f08efb45423c27f2548cae57e7
Ruben Bridgewater <ruben.bridgewater@datadoghq.com>
2025-09-24 16:02:31 +0200

    Revert appsec changes for now

--- packages/dd-trace/src/appsec/waf/index.js
+++ packages/dd-trace/src/appsec/waf/index.js
@@ -9,38 +9,7 @@ const { ASM } = require('../../standalone/product')
 const web = require('../../plugins/util/web')
 const { updateRateLimitedMetric } = require('../telemetry')
 
-/**
- * Types for WAF public API.
- *
- * @typedef {import('http').IncomingMessage} IncomingMessage
- * @typedef {import('./waf_manager')} WAFManagerCtor
- * @typedef {import('./waf_manager')} WAFManagerInstance
- * @typedef {import('./waf_manager').WafConfig} WafConfig
- *
- * @typedef {{
- *   persistent?: Record<string, unknown> | null,
- *   ephemeral?: Record<string, unknown> | null
- * }} WafRunPayload
- *
- * @typedef {{ keep?: boolean } & Record<string, unknown>} WafRunResult
- *
- * @typedef {{
- *   wafManager: WAFManagerInstance | null,
- *   init: (rules: object, config: WafConfig) => void,
- *   destroy: () => void,
- *   updateConfig: (product: string, configId: string, configPath: string, config: object) => void,
- *   removeConfig: (configPath: string) => void,
- *   checkAsmDdFallback: () => void,
- *   run: (data: WafRunPayload, req?: IncomingMessage, raspRule?: string) => WafRunResult | void,
- *   disposeContext: (req: IncomingMessage) => void,
- *   WafUpdateError: typeof WafUpdateError
- * }} WafAPI
- */
-
 class WafUpdateError extends Error {
-  /**
-   * @param {object} diagnosticErrors
-   */
   constructor (diagnosticErrors) {
     super('WafUpdateError')
     this.name = 'WafUpdateError'
@@ -50,7 +19,6 @@ class WafUpdateError extends Error {
 
 let limiter = new Limiter(100)
 
-/** @type {Partial<WafAPI>} */
 const waf = {
   wafManager: null,
   init,
@@ -58,18 +26,11 @@ const waf = {
   updateConfig,
   removeConfig,
   checkAsmDdFallback,
-  run: /** @type {WafAPI['run']} */ noop,
-  disposeContext: /** @type {WafAPI['disposeContext']} */ noop,
+  run: noop,
+  disposeContext: noop,
   WafUpdateError
 }
 
-/**
- * Initialize the WAF with provided rules and configuration.
- *
- * @param {object} rules
- * @param {WafConfig} config
- * @returns {void}
- */
 function init (rules, config) {
   destroy()
 
@@ -80,13 +41,10 @@ function init (rules, config) {
 
   waf.wafManager = new WAFManager(rules, config)
 
-  /** @type {WafAPI['run']} */
   waf.run = run
-  /** @type {WafAPI['disposeContext']} */
   waf.disposeContext = disposeContext
 }
 
-/** @returns {void} */
 function destroy () {
   if (waf.wafManager) {
     waf.wafManager.destroy()
@@ -107,28 +65,19 @@ function checkAsmDdFallback () {
   }
 }
 
-/**
- * @param {string} product
- * @param {string} configId
- * @param {string} configPath
- * @param {object} config
- * @returns {void}
- */
 function updateConfig (product, configId, configPath, config) {
   if (!waf.wafManager) throw new Error('Cannot update disabled WAF')
 
   try {
-    const wm = /** @type {import('./waf_manager')} */ (waf.wafManager)
     if (product === 'ASM_DD') {
-      // defaultWafConfigPath is a static on the WAFManager class
-      wm.removeConfig(wm.constructor.defaultWafConfigPath)
+      waf.wafManager.removeConfig(waf.wafManager.constructor.defaultWafConfigPath)
     }
 
-    const updateSucceeded = wm.updateConfig(configPath, config)
-    Reporter.reportWafConfigUpdate(product, configId, wm.ddwaf.diagnostics, wm.ddwafVersion)
+    const updateSucceeded = waf.wafManager.updateConfig(configPath, config)
+    Reporter.reportWafConfigUpdate(product, configId, waf.wafManager.ddwaf.diagnostics, waf.wafManager.ddwafVersion)
 
     if (!updateSucceeded) {
-      throw new WafUpdateError(wm.ddwaf.diagnostics)
+      throw new WafUpdateError(waf.wafManager.ddwaf.diagnostics)
     }
   } catch (err) {
     log.error('[ASM] Could not update config from RC')
@@ -136,33 +85,17 @@ function updateConfig (product, configId, configPath, config) {
   }
 }
 
-/**
- * @param {string} configPath
- * @returns {void}
- */
 function removeConfig (configPath) {
   if (!waf.wafManager) throw new Error('Cannot update disabled WAF')
 
   try {
-    const wm = /** @type {import('./waf_manager')} */ (waf.wafManager)
-    wm.removeConfig(configPath)
+    waf.wafManager.removeConfig(configPath)
   } catch (err) {
     log.error('[ASM] Could not remove config from RC')
     throw err
   }
 }
 
-/**
- * Execute the WAF for the given payload and request.
- *
- * When no request is provided, attempts to use the current store's `req`.
- * If the result indicates the trace should be kept (result.keep), applies ASM sampling behavior.
- *
- * @param {WafRunPayload} data
- * @param {IncomingMessage=} req
- * @param {string=} raspRule
- * @returns {WafRunResult | undefined}
- */
 function run (data, req, raspRule) {
   if (!req) {
     const store = storage('legacy').getStore()
@@ -174,7 +107,7 @@ function run (data, req, raspRule) {
     req = store.req
   }
 
-  const wafContext = /** @type {import('./waf_manager')} */ (waf.wafManager).getWAFContext(req)
+  const wafContext = waf.wafManager.getWAFContext(req)
   const result = wafContext.run(data, raspRule)
 
   if (result?.keep) {
@@ -189,14 +122,8 @@ function run (data, req, raspRule) {
   return result
 }
 
-/**
- * Dispose the WAF context for the given request.
- *
- * @param {IncomingMessage} req
- * @returns {void}
- */
 function disposeContext (req) {
-  const wafContext = /** @type {import('./waf_manager')} */ (waf.wafManager).getWAFContext(req)
+  const wafContext = waf.wafManager.getWAFContext(req)
 
   if (wafContext && !wafContext.ddwafContext.disposed) {
     wafContext.dispose()
