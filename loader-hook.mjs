183dcb0941793cc6b292b46de68ae283cf2adce5
rochdev <roch.devost@gmail.com>
2025-09-23 20:25:09 -0400

    add support for security control includes

--- loader-hook.mjs
+++ loader-hook.mjs
@@ -1,11 +1,17 @@
-// export * from 'import-in-the-middle/hook.mjs'
-
 import * as iitm from 'import-in-the-middle/hook.mjs'
 import hooks from './packages/datadog-instrumentations/src/helpers/hooks.js'
+import { getEnvironmentVariables } from './packages/dd-trace/src/config-helper.js'
 
-const nodules = Object.keys(hooks)
+const { DD_IAST_SECURITY_CONTROLS_CONFIGURATION } = getEnvironmentVariables()
 
 function initialize (data = {}) {
+  const instrumentations = Object.keys(hooks)
+  const securityControls = (DD_IAST_SECURITY_CONTROLS_CONFIGURATION || '')
+    .split(';')
+    .map(sc => sc.trim().split(':')[2])
+    .filter(Boolean)
+    .map(sc => sc.trim())
+
   data.include ??= []
   data.exclude ??= []
   data.exclude.push(
@@ -17,9 +23,13 @@ function initialize (data = {}) {
     /@anthropic-ai\/sdk\/_shims/
   )
 
-  for (const nodule of nodules) {
-    data.include.push(new RegExp(`node_modules/${nodule}`), nodule)
-    data.exclude.push(new RegExp(`node_modules/${nodule}/node_modules`))
+  for (const moduleName of instrumentations) {
+    data.include.push(new RegExp(`node_modules/${moduleName}`))
+    data.exclude.push(new RegExp(`node_modules/${moduleName}/node_modules`))
+  }
+
+  for (const subpath of securityControls) {
+    data.include.push(new RegExp(subpath))
   }
 
   return iitm.initialize(data)
