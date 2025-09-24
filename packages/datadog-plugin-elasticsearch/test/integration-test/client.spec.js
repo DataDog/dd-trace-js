2a2ed90d53817c930f2ee63004c06369c269ec76
Bryan English <bryan.english@datadoghq.com>
2025-09-24 14:18:50 -0400

    remove special-casing of default in hook.js (#6401)
    Fixed ESM instrumentation, the previous model of assuming default === exports was very incorrect for ESM since they're never the exact same object. We also can't assume they should have the same properties, or even that they're both objects/functions, at least not in the general case.
    
    Therefore, we need to be specific about what's being instrumented in every single instrumentation.
    
    Added appropriate testing and modifications to ensure any known broken instrumentation is properly instrumented with all import scenarios.
    
    ---------
    
    Co-authored-by: perhardhernandez <pablo.erhardhernandez@datadoghq.com>

--- packages/datadog-plugin-elasticsearch/test/integration-test/client.spec.js
+++ packages/datadog-plugin-elasticsearch/test/integration-test/client.spec.js
@@ -4,7 +4,8 @@ const {
   FakeAgent,
   createSandbox,
   checkSpansForServiceName,
-  spawnPluginIntegrationTestProc
+  spawnPluginIntegrationTestProc,
+  varySandbox
 } = require('../../../../integration-tests/helpers')
 const { withVersions } = require('../../../dd-trace/test/setup/mocha')
 const { assert } = require('chai')
@@ -13,6 +14,7 @@ describe('esm', () => {
   let agent
   let proc
   let sandbox
+  let variants
 
   // excluding 8.16.0 for esm tests, because it is not working: https://github.com/elastic/elasticsearch-js/issues/2466
   withVersions('elasticsearch', ['@elastic/elasticsearch'], '<8.16.0 || >8.16.0', version => {
@@ -20,6 +22,7 @@ describe('esm', () => {
       this.timeout(20000)
       sandbox = await createSandbox([`'@elastic/elasticsearch@${version}'`], false, [
         './packages/datadog-plugin-elasticsearch/test/integration-test/*'])
+      variants = varySandbox(sandbox, 'server.mjs', null, 'elasticsearch', null, '@elastic/elasticsearch')
     })
 
     after(async () => {
@@ -34,17 +37,18 @@ describe('esm', () => {
       proc && proc.kill()
       await agent.stop()
     })
+    for (const variant of ['default', 'destructure', 'star']) {
+      it(`is instrumented (${variant})`, async () => {
+        const res = agent.assertMessageReceived(({ headers, payload }) => {
+          assert.propertyVal(headers, 'host', `127.0.0.1:${agent.port}`)
+          assert.isArray(payload)
+          assert.strictEqual(checkSpansForServiceName(payload, 'elasticsearch.query'), true)
+        })
 
-    it('is instrumented', async () => {
-      const res = agent.assertMessageReceived(({ headers, payload }) => {
-        assert.propertyVal(headers, 'host', `127.0.0.1:${agent.port}`)
-        assert.isArray(payload)
-        assert.strictEqual(checkSpansForServiceName(payload, 'elasticsearch.query'), true)
-      })
+        proc = await spawnPluginIntegrationTestProc(sandbox.folder, variants[variant], agent.port)
 
-      proc = await spawnPluginIntegrationTestProc(sandbox.folder, 'server.mjs', agent.port)
-
-      await res
-    }).timeout(20000)
+        await res
+      }).timeout(20000)
+    }
   })
 })
