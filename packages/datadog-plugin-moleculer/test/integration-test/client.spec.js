c9b03befce67d639540aa293ecba5b991c739b72
Ruben Bridgewater <ruben.bridgewater@datadoghq.com>
2025-09-27 13:26:50 +0200

    test: remove get-port usage in grpc and molecular tests
    The tests instead rely on native port assignment.

--- packages/datadog-plugin-moleculer/test/integration-test/client.spec.js
+++ packages/datadog-plugin-moleculer/test/integration-test/client.spec.js
@@ -17,7 +17,7 @@ describe('esm', () => {
   withVersions('moleculer', 'moleculer', '>0.14.0', version => {
     before(async function () {
       this.timeout(20000)
-      sandbox = await createSandbox([`'moleculer@${version}'`, 'get-port'], false, [
+      sandbox = await createSandbox([`'moleculer@${version}'`], false, [
         './packages/datadog-plugin-moleculer/test/integration-test/*'])
     })
 
