a5e61e83bb2beba2a1ed8d516e51e1cc3c729fe1
Ruben Bridgewater <ruben.bridgewater@datadoghq.com>
2025-09-27 13:30:05 +0200

    fixup! linter

--- packages/datadog-plugin-grpc/test/integration-test/server.mjs
+++ packages/datadog-plugin-grpc/test/integration-test/server.mjs
@@ -3,7 +3,6 @@ import * as grpc from '@grpc/grpc-js'
 import * as protoLoader from '@grpc/proto-loader'
 import path from 'path'
 
-
 const currentDirectoryPath = path.dirname(new URL(import.meta.url).pathname)
 const parentDirectoryPath = path.resolve(currentDirectoryPath, '..')
 
