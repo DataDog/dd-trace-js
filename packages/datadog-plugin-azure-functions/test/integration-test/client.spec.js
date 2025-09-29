c5bcc4e79437ff38912215378617d385be703c09
Jordan <jordan.storms@datadoghq.com>
2025-09-29 12:45:46 -0400

    uncomment tests

--- packages/datadog-plugin-azure-functions/test/integration-test/client.spec.js
+++ packages/datadog-plugin-azure-functions/test/integration-test/client.spec.js
@@ -1,121 +1,121 @@
-// 'use strict'
+'use strict'
 
-// const {
-//   FakeAgent,
-//   hookFile,
-//   createSandbox,
-//   curlAndAssertMessage
-// } = require('../../../../integration-tests/helpers')
-// const { spawn } = require('child_process')
-// const { assert } = require('chai')
-// const { NODE_MAJOR } = require('../../../../version')
+const {
+  FakeAgent,
+  hookFile,
+  createSandbox,
+  curlAndAssertMessage
+} = require('../../../../integration-tests/helpers')
+const { spawn } = require('child_process')
+const { assert } = require('chai')
+const { NODE_MAJOR } = require('../../../../version')
 
-// describe('esm', () => {
-//   let agent
-//   let proc
-//   let sandbox
+describe('esm', () => {
+  let agent
+  let proc
+  let sandbox
 
-//   // TODO: Allow newer versions in Node.js 18 when their breaking change is reverted.
-//   // See https://github.com/Azure/azure-functions-nodejs-library/pull/357
-//   withVersions('azure-functions', '@azure/functions', NODE_MAJOR < 20 ? '<4.7.3' : '*', version => {
-//     before(async function () {
-//       this.timeout(50000)
-//       sandbox = await createSandbox([
-//         `@azure/functions@${version}`,
-//         'azure-functions-core-tools@4',
-//       ],
-//       false,
-//       [ './packages/datadog-plugin-azure-functions/test/fixtures/*',
-//         './packages/datadog-plugin-azure-functions/test/integration-test/*'])
-//     })
+  // TODO: Allow newer versions in Node.js 18 when their breaking change is reverted.
+  // See https://github.com/Azure/azure-functions-nodejs-library/pull/357
+  withVersions('azure-functions', '@azure/functions', NODE_MAJOR < 20 ? '<4.7.3' : '*', version => {
+    before(async function () {
+      this.timeout(50000)
+      sandbox = await createSandbox([
+        `@azure/functions@${version}`,
+        'azure-functions-core-tools@4',
+      ],
+      false,
+      [ './packages/datadog-plugin-azure-functions/test/fixtures/*',
+        './packages/datadog-plugin-azure-functions/test/integration-test/*'])
+    })
 
-//     after(async function () {
-//       this.timeout(50000)
-//       await sandbox.remove()
-//     })
+    after(async function () {
+      this.timeout(50000)
+      await sandbox.remove()
+    })
 
-//     beforeEach(async () => {
-//       agent = await new FakeAgent().start()
-//     })
+    beforeEach(async () => {
+      agent = await new FakeAgent().start()
+    })
 
-//     afterEach(async () => {
-//       proc && proc.kill('SIGINT')
-//       await agent.stop()
-//     })
+    afterEach(async () => {
+      proc && proc.kill('SIGINT')
+      await agent.stop()
+    })
 
-//     it('is instrumented', async () => {
-//       const envArgs = {
-//         PATH: `${sandbox.folder}/node_modules/azure-functions-core-tools/bin:${process.env.PATH}`
-//       }
-//       proc = await spawnPluginIntegrationTestProc(sandbox.folder, 'func', ['start'], agent.port, undefined, envArgs)
+    it('is instrumented', async () => {
+      const envArgs = {
+        PATH: `${sandbox.folder}/node_modules/azure-functions-core-tools/bin:${process.env.PATH}`
+      }
+      proc = await spawnPluginIntegrationTestProc(sandbox.folder, 'func', ['start'], agent.port, undefined, envArgs)
 
-//       return curlAndAssertMessage(agent, 'http://127.0.0.1:7071/api/httptest', ({ headers, payload }) => {
-//         assert.propertyVal(headers, 'host', `127.0.0.1:${agent.port}`)
-//         assert.isArray(payload)
-//         assert.strictEqual(payload.length, 1)
-//         assert.isArray(payload[0])
-//         assert.strictEqual(payload[0].length, 1)
-//         assert.propertyVal(payload[0][0], 'name', 'azure.functions.invoke')
-//       })
-//     }).timeout(50000)
+      return curlAndAssertMessage(agent, 'http://127.0.0.1:7071/api/httptest', ({ headers, payload }) => {
+        assert.propertyVal(headers, 'host', `127.0.0.1:${agent.port}`)
+        assert.isArray(payload)
+        assert.strictEqual(payload.length, 1)
+        assert.isArray(payload[0])
+        assert.strictEqual(payload[0].length, 1)
+        assert.propertyVal(payload[0][0], 'name', 'azure.functions.invoke')
+      })
+    }).timeout(50000)
 
-//     it('propagates context to child http requests', async () => {
-//       const envArgs = {
-//         PATH: `${sandbox.folder}/node_modules/azure-functions-core-tools/bin:${process.env.PATH}`
-//       }
-//       proc = await spawnPluginIntegrationTestProc(sandbox.folder, 'func', ['start'], agent.port, undefined, envArgs)
+    it('propagates context to child http requests', async () => {
+      const envArgs = {
+        PATH: `${sandbox.folder}/node_modules/azure-functions-core-tools/bin:${process.env.PATH}`
+      }
+      proc = await spawnPluginIntegrationTestProc(sandbox.folder, 'func', ['start'], agent.port, undefined, envArgs)
 
-//       return curlAndAssertMessage(agent, 'http://127.0.0.1:7071/api/httptest2', ({ headers, payload }) => {
-//         assert.strictEqual(payload.length, 2)
-//         assert.strictEqual(payload[1][0].span_id, payload[1][1].parent_id)
-//       })
-//     }).timeout(50000)
-//   })
-// })
+      return curlAndAssertMessage(agent, 'http://127.0.0.1:7071/api/httptest2', ({ headers, payload }) => {
+        assert.strictEqual(payload.length, 2)
+        assert.strictEqual(payload[1][0].span_id, payload[1][1].parent_id)
+      })
+    }).timeout(50000)
+  })
+})
 
 
-// async function spawnPluginIntegrationTestProc (cwd, command, args, agentPort, stdioHandler, additionalEnvArgs = {}) {
-//   let env = {
-//     NODE_OPTIONS: `--loader=${hookFile}`,
-//     DD_TRACE_AGENT_PORT: agentPort
-//   }
-//   env = { ...env, ...additionalEnvArgs }
-//   return spawnProc(command, args, {
-//     cwd,
-//     env
-//   }, stdioHandler)
-// }
+async function spawnPluginIntegrationTestProc (cwd, command, args, agentPort, stdioHandler, additionalEnvArgs = {}) {
+  let env = {
+    NODE_OPTIONS: `--loader=${hookFile}`,
+    DD_TRACE_AGENT_PORT: agentPort
+  }
+  env = { ...env, ...additionalEnvArgs }
+  return spawnProc(command, args, {
+    cwd,
+    env
+  }, stdioHandler)
+}
 
-// function spawnProc (command, args, options = {}, stdioHandler, stderrHandler) {
-//   const proc = spawn(command, args, { ...options, stdio: 'pipe' })
-//   return new Promise((resolve, reject) => {
-//     proc
-//       .on('error', reject)
-//       .on('exit', code => {
-//         if (code !== 0) {
-//           reject(new Error(`Process exited with status code ${code}.`))
-//         }
-//         resolve()
-//       })
+function spawnProc (command, args, options = {}, stdioHandler, stderrHandler) {
+  const proc = spawn(command, args, { ...options, stdio: 'pipe' })
+  return new Promise((resolve, reject) => {
+    proc
+      .on('error', reject)
+      .on('exit', code => {
+        if (code !== 0) {
+          reject(new Error(`Process exited with status code ${code}.`))
+        }
+        resolve()
+      })
 
-//     proc.stdout.on('data', data => {
-//       if (stdioHandler) {
-//         stdioHandler(data)
-//       }
-//       // eslint-disable-next-line no-console
-//       if (!options.silent) console.log(data.toString())
+    proc.stdout.on('data', data => {
+      if (stdioHandler) {
+        stdioHandler(data)
+      }
+      // eslint-disable-next-line no-console
+      if (!options.silent) console.log(data.toString())
 
-//       if (data.toString().includes('Host lock lease acquired by instance')) {
-//         resolve(proc)
-//       }
-//     })
+      if (data.toString().includes('Host lock lease acquired by instance')) {
+        resolve(proc)
+      }
+    })
 
-//     proc.stderr.on('data', data => {
-//       if (stderrHandler) {
-//         stderrHandler(data)
-//       }
-//       // eslint-disable-next-line no-console
-//       if (!options.silent) console.error(data.toString())
-//     })
-//   })
-// }
+    proc.stderr.on('data', data => {
+      if (stderrHandler) {
+        stderrHandler(data)
+      }
+      // eslint-disable-next-line no-console
+      if (!options.silent) console.error(data.toString())
+    })
+  })
+}
