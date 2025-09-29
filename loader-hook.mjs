78e3dc8a2268fc205558933cc9d2d7c3d30efd11
rochdev <roch.devost@gmail.com>
2025-09-29 15:54:38 -0400

    fix nested supported modules

--- loader-hook.mjs
+++ loader-hook.mjs
@@ -24,8 +24,7 @@ function initialize (data = {}) {
   )
 
   for (const moduleName of instrumentations) {
-    data.include.push(new RegExp(`node_modules/${moduleName}`), moduleName)
-    data.exclude.push(new RegExp(`node_modules/${moduleName}/node_modules`))
+    data.include.push(new RegExp(`node_modules/${moduleName}/(?!node_modules).+`), moduleName)
   }
 
   for (const subpath of securityControls) {
