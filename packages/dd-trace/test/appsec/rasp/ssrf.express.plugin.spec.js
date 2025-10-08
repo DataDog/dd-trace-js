c4f4477ef16fbbaf53a2a33840ab94ea1e3a89f9
Ugaitz Urien <ugaitz.urien@datadoghq.com>
2025-10-08 13:31:34 +0200

    Add logs

--- packages/dd-trace/test/appsec/rasp/ssrf.express.plugin.spec.js
+++ packages/dd-trace/test/appsec/rasp/ssrf.express.plugin.spec.js
@@ -125,7 +125,15 @@ describe('RASP - ssrf' + i, () => {
 
             // we preload axios because it's lazyloading a debug dependency
             // that in turns trigger LFI
-            axiosToTest.get('http://preloadaxios').catch(noop).then(done)
+            console.log('preload axios call')
+            axiosToTest.get('http://preloadaxios')
+              .catch(() => {
+                console.log('preload axios error')
+              })
+              .then(() => {
+                console.log('preload axios then')
+                done()
+              })
           })
 
           it('Should not detect threat', async () => {
