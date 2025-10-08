223973a4a1e35ef9d98129ab03174209e183ae65
rochdev <roch.devost@gmail.com>
2025-10-08 18:55:46 -0400

    run scripts from dependencies

--- scripts/install_plugin_modules.js
+++ scripts/install_plugin_modules.js
@@ -33,6 +33,7 @@ async function run () {
   install()
   await assertPeerDependencies(join(__dirname, '..', 'versions'))
   install()
+  trust()
 }
 
 async function assertPrerequisites () {
@@ -253,11 +254,15 @@ async function assertWorkspaces () {
  */
 function install (retry = true) {
   try {
-    exec('bun install --trust --ignore-engines', { cwd: folder(), env: withBun() })
+    exec('bun install --ignore-engines', { cwd: folder(), env: withBun() })
   } catch (err) {
     if (!retry) throw err
     install(false) // retry in case of server error from registry
   }
+}
+
+function trust () {
+  exec('bun pm trust --all', { cwd: folder(), env: withBun() })
 }
 
 /**
