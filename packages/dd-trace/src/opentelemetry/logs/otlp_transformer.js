dc2f39ded4443c92bb927ddd49e96cbf015af118
Munir Abdinur <munir.abdinur@datadoghq.com>
2025-10-16 17:48:02 -0400

    chore: add OTLP metrics proto definitions and reorganize directory (#6657)
    * feat: add OTLP metrics proto definitions and reorganize directory
    
    - Add metrics.proto and metrics_service.proto (OTLP v1 spec)
    - Update protobuf_loader to support metrics protos
    - Rename protos/ -> otlp/ directory for better organization
    
    * fix logs

--- packages/dd-trace/src/opentelemetry/logs/otlp_transformer.js
+++ packages/dd-trace/src/opentelemetry/logs/otlp_transformer.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const { SeverityNumber } = require('@opentelemetry/api-logs')
-const { getProtobufTypes } = require('../protos/protobuf_loader')
+const { getProtobufTypes } = require('../otlp/protobuf_loader')
 const { trace } = require('@opentelemetry/api')
 const log = require('../../log')
 
