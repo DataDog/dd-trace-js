83068b226e34a463731ed8e76f52b666f19b09ba
Jordan <jordan.storms@datadoghq.com>
2025-10-16 15:28:38 -0400

    Fix path in workflow and address comments

--- packages/datadog-instrumentations/src/azure-service-bus.js
+++ packages/datadog-instrumentations/src/azure-service-bus.js
@@ -11,15 +11,18 @@ const producerCh = dc.tracingChannel('apm:azure-service-bus:send')
 
 addHook({ name: '@azure/service-bus', versions: ['>=7.9.2'], patchDefault: false }, (obj) => {
   const ServiceBusClient = obj.ServiceBusClient
+  let didItShim = false
   shimmer.wrap(ServiceBusClient.prototype, 'createSender',
     createSender => function (queueOrTopicName) {
       const sender = createSender.apply(this, arguments)
-      const config = sender._context.config
-      const entityPath = sender._entityPath
-
-      shimmer.wrap(sender, 'scheduleMessages', scheduleMessages =>
+      if (didItShim) return sender
+      const proto = sender.constructor.prototype
+      const proto2 = sender._sender.constructor.prototype
+      shimmer.wrap(proto, 'scheduleMessages', scheduleMessages =>
         function (msg, scheduledEnqueueTimeUtc) {
           const functionName = scheduleMessages.name
+          const config = this._context.config
+          const entityPath = this._entityPath
           return producerCh.tracePromise(
             scheduleMessages,
             { config, entityPath, functionName, msg, scheduledEnqueueTimeUtc },
@@ -27,30 +30,35 @@ addHook({ name: '@azure/service-bus', versions: ['>=7.9.2'], patchDefault: false
           )
         })
 
-      shimmer.wrap(sender, 'createMessageBatch', createMessageBatch => async function () {
+      shimmer.wrap(proto, 'createMessageBatch', createMessageBatch => async function () {
         const batch = await createMessageBatch.apply(this, arguments)
-        shimmer.wrap(batch, 'tryAddMessage', tryAddMessage => function (msg) {
+        shimmer.wrap(batch.constructor.prototype, 'tryAddMessage', tryAddMessage => function (msg) {
           const functionName = tryAddMessage.name
+          const config = this._context.config
           return producerCh.tracePromise(
-            tryAddMessage, { config, entityPath, functionName, batch, msg }, this, ...arguments)
+            tryAddMessage, { config, functionName, batch, msg }, this, ...arguments)
         })
         return batch
       })
 
-      shimmer.wrap(sender._sender, 'send', send => function (msg) {
+      shimmer.wrap(proto2, 'send', send => function (msg) {
         const functionName = send.name
+        const config = this._context.config
+        const entityPath = this.entityPath
         return producerCh.tracePromise(
           send, { config, entityPath, functionName, msg }, this, ...arguments
         )
       })
 
-      shimmer.wrap(sender._sender, 'sendBatch', sendBatch => function (msg) {
+      shimmer.wrap(proto2, 'sendBatch', sendBatch => function (msg) {
         const functionName = sendBatch.name
+        const config = this._context.config
+        const entityPath = this.entityPath
         return producerCh.tracePromise(
           sendBatch, { config, entityPath, functionName, msg }, this, ...arguments
         )
       })
-
+      didItShim = true
       return sender
     })
   return obj
