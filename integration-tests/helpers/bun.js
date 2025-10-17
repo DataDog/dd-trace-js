5add7fc0a2456571e99f2d16642e39232cf849f3
rochdev <roch.devost@gmail.com>
2025-10-17 14:27:08 -0400

    install bun locally if not available globally

---
+++ integration-tests/helpers/bun.js
@@ -0,0 +1,44 @@
+'use strict'
+
+const { execSync } = require('child_process')
+const { readFileSync } = require('fs')
+const { join, resolve } = require('path')
+const semifies = require('semifies')
+
+const PROJECT_ROOT = resolve(__dirname, '..', '..')
+const BUN_VERSION = readFileSync(join(PROJECT_ROOT, '.bun-version')).toString().trim()
+const BUN_INSTALL = join(PROJECT_ROOT, '.bun')
+const BUN_BIN = `${BUN_INSTALL}/bin`
+
+let hasCompatibleBun = () => {
+  try {
+    const version = execSync('command -v bun >/dev/null 2>&1 && bun -v').toString()
+
+    if (!semifies(version, `^${BUN_VERSION}`)) {
+      return false
+    }
+  } catch (e) {
+    return false
+  }
+
+  hasCompatibleBun = () => true
+
+  return true
+}
+
+function withBun (env = process.env) {
+  env = {
+    ...env,
+    PATH: env.PATH ? `${BUN_BIN}:${env.PATH}` : BUN_BIN
+  }
+
+  if (hasCompatibleBun()) return env
+
+  execSync(`curl -fsSL https://bun.com/install | BUN_INSTALL="${BUN_INSTALL}" bash -s "bun-v${BUN_VERSION}"`)
+
+  hasCompatibleBun = () => true
+
+  return env
+}
+
+module.exports = { withBun }
