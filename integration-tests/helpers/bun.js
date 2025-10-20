ff9c1da40769afdf413456371a3b09a97cb4dd0d
rochdev <roch.devost@gmail.com>
2025-10-20 12:13:33 -0400

    use npm to install bun the same way everywhere

--- integration-tests/helpers/bun.js
+++ integration-tests/helpers/bun.js
@@ -1,47 +1,13 @@
 'use strict'
 
-const { execSync } = require('child_process')
-const { readFileSync } = require('fs')
 const { join, resolve } = require('path')
-const semifies = require('semifies')
 
 const PROJECT_ROOT = resolve(__dirname, '..', '..')
-const BUN_VERSION = readFileSync(join(PROJECT_ROOT, '.bun-version')).toString().trim()
 const BUN_INSTALL = join(PROJECT_ROOT, '.bun')
-const BUN_BIN = `${BUN_INSTALL}/bin`
-
-let hasCompatibleBun = env => {
-  try {
-    const version = execSync('command -v bun >/dev/null 2>&1 && bun -v', { env }).toString()
-
-    if (!semifies(version, `^${BUN_VERSION}`)) {
-      return false
-    }
-  } catch {
-    return false
-  }
-
-  hasCompatibleBun = () => true
-
-  return true
-}
+const BUN = join(PROJECT_ROOT, 'node_modules', '.bin', 'bun')
 
 function withBun (env = process.env) {
-  env = {
-    ...env,
-    PATH: env.PATH ? `${BUN_BIN}:${env.PATH}` : BUN_BIN,
-    BUN_INSTALL
-  }
-
-  if (hasCompatibleBun(env)) return env
-
-  execSync(`curl -fsSL https://bun.com/install | bash -s "bun-v${BUN_VERSION}"`, {
-    env: { ...process.env, BUN_INSTALL, SHELL: 'sh' }
-  })
-
-  hasCompatibleBun = () => true
-
-  return env
+  return { ...env, BUN_INSTALL }
 }
 
-module.exports = { withBun }
+module.exports = { BUN, withBun }
