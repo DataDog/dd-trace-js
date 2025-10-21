d7c1fa8d59154cf4d7863ebaabd89ce578749a5e
Pablo Erhard <104538390+pabloerhard@users.noreply.github.com>
2025-10-21 11:17:42 -0400

    fix(esm): do not wrap .default when instrumenting cookie-parser (#6708)
    * Fix to cookie-parser failing to instrument
    
    * Added cookie-parser test and excluded it from plugin tests

---
+++ packages/datadog-plugin-cookie-parser/test/integration-test/client.spec.js
@@ -0,0 +1,43 @@
+'use strict'
+
+const {
+  createSandbox, varySandbox, curl,
+  FakeAgent, spawnPluginIntegrationTestProc
+} = require('../../../../integration-tests/helpers')
+const { assert } = require('chai')
+const { withVersions } = require('../../../dd-trace/test/setup/mocha')
+
+withVersions('cookie-parser', 'cookie-parser', version => {
+  describe('ESM', () => {
+    let sandbox, variants, proc, agent
+
+    before(async function () {
+      this.timeout(50000)
+      sandbox = await createSandbox([`'cookie-parser@${version}'`, 'express'], false,
+        ['./packages/datadog-plugin-cookie-parser/test/integration-test/*'])
+      variants = varySandbox(sandbox, 'server.mjs', 'cookie-parser', 'cookieParser')
+    })
+
+    after(async function () {
+      this.timeout(50000)
+      await sandbox.remove()
+    })
+
+    beforeEach(async () => {
+      agent = await new FakeAgent().start()
+    })
+
+    afterEach(async () => {
+      proc?.kill()
+      await agent.stop()
+    })
+
+    for (const variant of varySandbox.VARIANTS) {
+      it(`is instrumented loaded with ${variant}`, async () => {
+        proc = await spawnPluginIntegrationTestProc(sandbox.folder, variants[variant], agent.port)
+        const response = await curl(proc)
+        assert.equal(response.headers['x-counter'], '1')
+      })
+    }
+  })
+})
