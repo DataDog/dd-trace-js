a6c91c92e485baf8f3c32097ee32f7b0e7e0e052
ishabi <ilyasshabi94@gmail.com>
2025-10-21 16:42:19 +0200

    Avoid create xml reporting file on local

--- .mochamultireporterrc.js
+++ .mochamultireporterrc.js
@@ -1,7 +1,13 @@
 'use strict'
 
+const reporterEnabled = ['spec']
+
+if (process.env.CI) {
+  reporterEnabled.push('mocha-junit-reporter')
+}
+
 module.exports = {
-  reporterEnabled: ['spec', 'mocha-junit-reporter'],
+  reporterEnabled,
   mochaJunitReporterReporterOptions: {
     mochaFile: `./node-${process.versions.node}-junit.xml`
   }
