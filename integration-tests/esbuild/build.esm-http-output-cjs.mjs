e96f4a11fa76cbad63e52def2dc98fcaf699d074
Ugaitz Urien <ugaitz.urien@datadoghq.com>
2025-10-24 09:43:20 +0200

    fix(esbuild): do not crash and build properly when esbuild is used with ESM modules (#6513)

---
+++ integration-tests/esbuild/build.esm-http-output-cjs.mjs
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+import esbuild from 'esbuild'
+
+import ddPlugin from 'dd-trace/esbuild.js'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const SCRIPT = path.join(__dirname, 'esm-http-test-out.cjs')
+const entryPoint = path.join(__dirname, 'esm-http-test.mjs')
+
+const external = [
+  // required if you use native metrics
+  '@datadog/native-metrics',
+
+  // required if you use profiling
+  '@datadog/pprof',
+
+  // @openfeature/core is a peer dependency of @openfeature/server-sdk
+  // which is used by @datadog/openfeature-node-server
+  '@openfeature/core',
+
+  // required if you use Datadog security features
+  '@datadog/native-appsec',
+  '@datadog/native-iast-taint-tracking',
+  '@datadog/native-iast-rewriter',
+
+  // required if you encounter graphql errors during the build step
+  'graphql/language/visitor',
+  'graphql/language/printer',
+  'graphql/utilities',
+]
+
+esbuild.build({
+  entryPoints: [entryPoint],
+  outfile: SCRIPT,
+  minify: false,
+  bundle: true,
+  platform: 'node',
+  target: 'node22',
+  plugins: [ddPlugin],
+  format: 'cjs',
+  external
+})
