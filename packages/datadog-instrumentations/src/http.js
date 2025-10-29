f19ec125490fbb6972c15a0c623dec20ec1ce0fb
nina9753 <nina.rei@datadoghq.com>
2025-10-29 11:19:38 -0400

    Add push subscription HTTP handling infrastructure. Add transit handler initialization in http.js with setImmediate

--- packages/datadog-instrumentations/src/http.js
+++ packages/datadog-instrumentations/src/http.js
@@ -1,16 +1,18 @@
 'use strict'
 
-try {
-  // Load the Pub/Sub Transit Handler plugin directly to ensure it gets instantiated
-  const TransitHandlerPlugin = require('../../datadog-plugin-google-cloud-pubsub/src/pubsub-transit-handler')
-  const tracer = require('../../dd-trace')
-  if (tracer && tracer._tracer && !global._dd_gcp_pubsub_transit_handler) {
-    // Keep a reference to avoid GC and satisfy no-new side-effect rule
-    global._dd_gcp_pubsub_transit_handler = new TransitHandlerPlugin(tracer._tracer)
+// Initialize transit handler after a delay to ensure tracer is ready
+setImmediate(() => {
+  try {
+    const TransitHandlerPlugin = require('../../datadog-plugin-google-cloud-pubsub/src/pubsub-transit-handler')
+    const tracer = require('../../dd-trace')
+    
+    if (tracer && tracer._tracer && !global._dd_gcp_pubsub_transit_handler) {
+      global._dd_gcp_pubsub_transit_handler = new TransitHandlerPlugin(tracer._tracer)
+    }
+  } catch (err) {
+    // Silently fail - transit handler is optional
   }
-} catch {
-  // Silently handle plugin loading errors
-}
+})
 
 require('./http/client')
 require('./http/server')
