0e029be6188bc49eaacaa0509df390044a98c23f
Topher Campbell <topher.campbell@datadoghq.com>
2025-10-30 13:10:16 -0400

    Fixing linter errors

--- integration-tests/payload-tagging/index.js
+++ integration-tests/payload-tagging/index.js
@@ -1,8 +1,8 @@
 'use strict'
-const AWS = require('aws-sdk');
+const AWS = require('aws-sdk')
 
 // Configure AWS credentials and region
-AWS.config.update({ region: 'us-east-1' });
+AWS.config.update({ region: 'us-east-1' })
 
 const options = {
   service: 'test-service',
@@ -20,6 +20,7 @@ if (process.env.lOG_INJECTION) {
 const tracer = require('dd-trace')
 tracer.init(options)
 
+const winston = require('winston')
 const express = require('express')
 
 const app = express()
@@ -34,22 +35,22 @@ const logger = winston.createLogger({
 })
 
 // Create an SQS service object
-const sqs = new AWS.SQS({ apiVersion: '2012-11-05' });
+const sqs = new AWS.SQS({ apiVersion: '2012-11-05' })
 
 // Define message parameters
 const params = {
     MessageBody: 'This is a test message from a standalone Node.js application.',
     QueueUrl: 'https://sqs.us-east-1.amazonaws.com/123456789012/MyTestQueue' // Replace with your SQS queue URL
-};
+}
 
 // Send the message
 sqs.sendMessage(params, (err, data) => {
-    if (err) {
-        console.error('Error sending message:', err);
-    } else {
-        console.log('Message sent successfully. Message ID:', data.MessageId);
-    }
-});
+  if (err) {
+      console.error('Error sending message:', err)
+  } else {
+      console.log('Message sent successfully. Message ID:', data.MessageId)
+  }
+})
 
 const server = app.listen(0, () => {
   const port = server.address().port
