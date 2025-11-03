18cf81363e1e9ac74e2888ca0a9d6dddb8375272
Carles Capell <carles.capell@datadoghq.com>
2025-11-03 07:56:57 +0100

    Fix lint

--- integration-tests/appsec/iast-esbuild-esm/app.mjs
+++ integration-tests/appsec/iast-esbuild-esm/app.mjs
@@ -3,7 +3,7 @@ import './init.mjs'
 import express from 'express'
 
 import iastRouter from './iast/index.mjs'
-import randomJson from './random.json' with { type: 'json' }
+import randomJson from './random.json' assert { type: 'json' }
 
 const app = express()
 
