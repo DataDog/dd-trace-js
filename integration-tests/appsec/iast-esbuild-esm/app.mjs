f37a795a92156c1aebf3763291d7944d90eb0cd1
Carles Capell <carles.capell@datadoghq.com>
2025-11-03 08:00:44 +0100

    Fix lint

--- integration-tests/appsec/iast-esbuild-esm/app.mjs
+++ integration-tests/appsec/iast-esbuild-esm/app.mjs
@@ -4,9 +4,6 @@ import express from 'express'
 
 import iastRouter from './iast/index.mjs'
 
-// eslint-disable-next-line
-import randomJson from './random.json' with { type: 'json' }
-
 const app = express()
 
 app.use('/iast', iastRouter)
