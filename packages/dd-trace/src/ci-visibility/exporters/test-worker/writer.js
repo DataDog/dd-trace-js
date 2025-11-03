4ac0f3dabe0167a3249564461d4272d7cf82c428
Juan Fernandez <juan.fernandezdealba@datadoghq.com>
2025-11-03 13:59:23 +0100

    test fixes

--- packages/dd-trace/src/ci-visibility/exporters/test-worker/writer.js
+++ packages/dd-trace/src/ci-visibility/exporters/test-worker/writer.js
@@ -1,6 +1,11 @@
 'use strict'
 const { JSONEncoder } = require('../../encode/json-encoder')
 const { getEnvironmentVariable } = require('../../../config-helper')
+const log = require('../../../log')
+const {
+  VITEST_WORKER_TRACE_PAYLOAD_CODE,
+  VITEST_WORKER_LOGS_PAYLOAD_CODE
+} = require('../../../plugins/util/test')
 
 class Writer {
   constructor (interprocessCode) {
@@ -27,24 +32,39 @@ class Writer {
     // ## Jest
     // Only available when `child_process` is used for the jest worker.
     // https://github.com/facebook/jest/blob/bb39cb2c617a3334bf18daeca66bd87b7ccab28b/packages/jest-worker/README.md#experimental-worker
-    // If worker_threads is used, this will not work
-    // TODO: make it compatible with worker_threads
 
     // ## Cucumber
     // This reports to the test's main process the same way test data is reported by Cucumber
     // See cucumber code:
     // https://github.com/cucumber/cucumber-js/blob/5ce371870b677fe3d1a14915dc535688946f734c/src/runtime/parallel/run_worker.ts#L13
-    if (process.send) { // it only works if process.send is available
-      // Old because vitest@>=4 use DD_VITEST_WORKER and report arrays just like other frameworks
-      const isVitestWorkerOld = !!getEnvironmentVariable('TINYPOOL_WORKER_ID')
 
-      const payload = isVitestWorkerOld
-        ? { __tinypool_worker_message__: true, interprocessCode: this._interprocessCode, data }
-        : [this._interprocessCode, data]
+    // Old because vitest@>=4 use DD_VITEST_WORKER and report arrays just like other frameworks
+    // Before vitest@>=4, we need the `__tinypool_worker_message__` shape, or tinypool will crash
+    const isVitestWorkerOld = !!getEnvironmentVariable('TINYPOOL_WORKER_ID')
+    const payload = isVitestWorkerOld
+      ? { __tinypool_worker_message__: true, interprocessCode: this._interprocessCode, data }
+      : [this._interprocessCode, data]
 
+    const isVitestTestWorker =
+      this._interprocessCode === VITEST_WORKER_TRACE_PAYLOAD_CODE ||
+      this._interprocessCode === VITEST_WORKER_LOGS_PAYLOAD_CODE
+
+    if (process.send) {
       process.send(payload, () => {
         onDone()
       })
+    } else if (isVitestTestWorker) { // TODO: worker_threads are only supported in vitest right now
+      const { isMainThread, parentPort } = require('worker_threads')
+      if (isMainThread) {
+        return onDone()
+      }
+      try {
+        parentPort.postMessage(payload)
+      } catch (error) {
+        log.error('Error posting message to parent port', error)
+      } finally {
+        onDone()
+      }
     } else {
       onDone()
     }
