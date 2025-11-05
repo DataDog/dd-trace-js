7c0fc1a77f84fd148af4329667fff72872dd0bfe
Ruben Bridgewater <ruben@bridgewater.de>
2025-11-05 15:49:53 +0100

    test: reduce esbuild flakiness (#6841)
    It also simplifies the code and removes the duplicated eslint entry
    in the other package.json file.

--- integration-tests/esbuild/index.spec.js
+++ integration-tests/esbuild/index.spec.js
@@ -4,55 +4,59 @@
 
 'use strict'
 
-const chproc = require('child_process')
-const pathModule = require('path')
-const fs = require('fs')
+const assert = require('node:assert/strict')
+const chproc = require('node:child_process')
+const pathModule = require('node:path')
+const fs = require('node:fs')
+
 // TODO: It shouldn't be necessary to disable n/no-extraneous-require - Research
 // eslint-disable-next-line n/no-extraneous-require
-const { assert } = require('chai')
+const { describe, before, it } = require('mocha')
 
 // sub process must be executed inside TEST_DIR
 const TEST_DIR = pathModule.join(__dirname, '.')
-const execSync = (command, options) => chproc.execSync(command, { ...(options ?? {}), cwd: TEST_DIR })
+const execSync = (command, options) => {
+  console.log(command)
+  chproc.execSync(command, { ...(options ?? {}), cwd: TEST_DIR })
+}
 const rmSync = (path, options) => fs.rmSync(pathModule.join(TEST_DIR, path), options)
 const readFileSync = (path, options) => fs.readFileSync(pathModule.join(TEST_DIR, path), options)
 const originalDir = process.cwd()
 
+const versionsPackageJson = require('../../packages/dd-trace/test/plugins/versions/package.json')
+const maximumEsbuildVersion = versionsPackageJson.dependencies.esbuild
+
 // This should switch to our withVersion helper. The order here currently matters.
-const esbuildVersions = ['latest', '0.16.12']
+const esbuildVersions = ['0.16.12', maximumEsbuildVersion]
+const timeout = 1000 * 45
 
 esbuildVersions.forEach((version) => {
-  describe(`esbuild ${version}`, () => {
+  describe(`esbuild ${version}`, function () {
+    this.timeout(timeout)
+
     before(() => {
       process.chdir(TEST_DIR)
       execSync('npm install', {
-        timeout: 1000 * 30
+        timeout
       })
-      if (version === 'latest') {
-        const versionsPackageJson = require('../../packages/dd-trace/test/plugins/versions/package.json')
-        const version = versionsPackageJson.dependencies.esbuild
-        execSync(`npm install esbuild@${version}`, {
-          timeout: 1000 * 30
-        })
-      } else {
-        execSync(`npm install esbuild@${version}`, {
-          timeout: 1000 * 30
-        })
-      }
+      execSync(`npm install esbuild@${version}`, {
+        timeout
+      })
     })
 
     after(() => {
       process.chdir(originalDir)
+      execSync('npm remove esbuild', {
+        timeout
+      })
     })
 
     it('works', () => {
-      console.log('npm run build')
       execSync('npm run build')
 
-      console.log('npm run built')
       try {
         execSync('npm run built', {
-          timeout: 1000 * 30
+          timeout
         })
       } catch (err) {
         console.error(err)
@@ -63,50 +67,38 @@ esbuildVersions.forEach((version) => {
     })
 
     it('does not bundle modules listed in .external', () => {
-      const command = 'node ./build-and-test-skip-external.js'
-      console.log(command)
-      execSync(command, {
-        timeout: 1000 * 30
+      execSync('node ./build-and-test-skip-external.js', {
+        timeout
       })
     })
 
     it('handles typescript apps that import without file extensions', () => {
-      const command = 'node ./build-and-test-typescript.mjs'
-      console.log(command)
-      execSync(command, {
-        timeout: 1000 * 30
+      execSync('node ./build-and-test-typescript.mjs', {
+        timeout
       })
     })
 
     it('handles the complex aws-sdk package with dynamic requires', () => {
-      const command = 'node ./build-and-test-aws-sdk.js'
-      console.log(command)
-      execSync(command, {
-        timeout: 1000 * 30
+      execSync('node ./build-and-test-aws-sdk.js', {
+        timeout
       })
     })
 
     it('handles scoped node_modules', () => {
-      const command = 'node ./build-and-test-koa.mjs'
-      console.log(command)
-      execSync(command, {
-        timeout: 1000 * 30
+      execSync('node ./build-and-test-koa.mjs', {
+        timeout
       })
     })
 
     it('handles instrumentations where the patching function is a property of the hook', () => {
-      const command = 'node ./build-and-test-openai.js'
-      console.log(command)
-      execSync(command, {
-        timeout: 1000 * 30
+      execSync('node ./build-and-test-openai.js', {
+        timeout
       })
     })
 
     it('injects Git metadata into bundled applications', () => {
-      const command = 'node ./build-and-test-git-tags.js'
-      console.log(command)
-      execSync(command, {
-        timeout: 1000 * 30
+      execSync('node ./build-and-test-git-tags.js', {
+        timeout
       })
     })
 
@@ -118,82 +110,66 @@ esbuildVersions.forEach((version) => {
       })
 
       it('works', () => {
-        console.log('npm run build:esm')
         execSync('npm run build:esm')
-        console.log('npm run built:esm')
         execSync('npm run built:esm', {
-          timeout: 1000 * 30
+          timeout
         })
       })
 
       it('should not override existing js banner', () => {
-        const command = 'node ./build-and-run.esm-unrelated-js-banner.mjs'
-        console.log(command)
-        execSync(command, {
-          timeout: 1000 * 30
+        execSync('node ./build-and-run.esm-unrelated-js-banner.mjs', {
+          timeout
         })
 
         const builtFile = readFileSync('./out.mjs').toString()
-        assert.include(builtFile, '/* js test */')
+        assert.match(builtFile, /\/\* js test \*\//m)
       })
 
       it('should contain the definitions when esm is inferred from outfile', () => {
-        const command = 'node ./build-and-run.esm-relying-in-extension.mjs'
-        console.log(command)
-        execSync(command, {
-          timeout: 1000 * 30
+        execSync('node ./build-and-run.esm-relying-in-extension.mjs', {
+          timeout
         })
 
         const builtFile = readFileSync('./out.mjs').toString()
-        assert.include(builtFile, 'globalThis.__filename ??= $dd_fileURLToPath(import.meta.url);')
+        assert.match(builtFile, /globalThis\.__filename \?\?= \$dd_fileURLToPath\(import\.meta\.url\);/m)
       })
 
       it('should contain the definitions when esm is inferred from format', () => {
-        const command = 'node ./build-and-run.esm-relying-in-format.mjs'
-        console.log(command)
-        execSync(command, {
-          timeout: 1000 * 30
+        execSync('node ./build-and-run.esm-relying-in-format.mjs', {
+          timeout
         })
 
         const builtFile = readFileSync('./out.mjs').toString()
-        assert.include(builtFile, 'globalThis.__filename ??= $dd_fileURLToPath(import.meta.url);')
+        assert.match(builtFile, /globalThis\.__filename \?\?= \$dd_fileURLToPath\(import\.meta\.url\);/m)
       })
 
       it('should contain the definitions when format is inferred from out extension', () => {
-        const command = 'node ./build-and-run.esm-relying-in-out-extension.mjs'
-        console.log(command)
-        execSync(command, {
-          timeout: 1000 * 30
+        execSync('node ./build-and-run.esm-relying-in-out-extension.mjs', {
+          timeout
         })
 
         const builtFile = readFileSync('./basic-test.mjs').toString()
-        assert.include(builtFile, 'globalThis.__filename ??= $dd_fileURLToPath(import.meta.url);')
+        assert.match(builtFile, /globalThis\.__filename \?\?= \$dd_fileURLToPath\(import\.meta\.url\);/m)
       })
 
       it('should not contain the definitions when no esm is specified', () => {
-        const command = 'node ./build.js'
-        console.log(command)
-        execSync(command, {
-          timeout: 1000 * 30
+        execSync('node ./build.js', {
+          timeout
         })
 
         const builtFile = readFileSync('./out.js').toString()
-        assert.notInclude(builtFile, 'globalThis.__filename ??= $dd_fileURLToPath(import.meta.url);')
+        assert.doesNotMatch(builtFile, /globalThis\.__filename \?\?= \$dd_fileURLToPath\(import\.meta\.url\);/m)
       })
 
       it('should not crash when it is already patched using global', () => {
-        const command = 'node ./build-and-run.esm-patched-global-banner.mjs'
-        console.log(command)
-        execSync(command, {
-          timeout: 1000 * 30
+        execSync('node ./build-and-run.esm-patched-global-banner.mjs', {
+          timeout
         })
       })
 
       it('should not crash when it is already patched using const', () => {
-        const command = 'node ./build-and-run.esm-patched-const-banner.mjs'
-        console.log(command)
-        execSync(command, {
-          timeout: 1000 * 30
+        execSync('node ./build-and-run.esm-patched-const-banner.mjs', {
+          timeout
         })
       })
     })
