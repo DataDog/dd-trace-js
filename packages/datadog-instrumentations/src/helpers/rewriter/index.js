e3db17571403665096c5b2ca9af8052f823275f0
rochdev <roch.devost@gmail.com>
2025-11-09 11:19:37 -0500

    switch to js implementation of orchestrion

---
+++ packages/datadog-instrumentations/src/helpers/rewriter/index.js
@@ -0,0 +1,99 @@
+'use strict'
+
+// The rewriter works effectively the same as Orchestrion with some additions:
+// - Supports an `engines` field to filter a Node version range.
+// - Supports an `astQuery` field to filter AST nodes with an esquery query.
+
+const { parse } = require('meriyah')
+const { generate } = require('astring')
+const esquery = require('esquery')
+const { readFileSync } = require('fs')
+const { join } = require('path')
+const semifies = require('semifies')
+const transforms = require('./transforms')
+const instrumentations = require('./instrumentations.json')
+
+const NODE_VERSION = process.versions.node
+
+const supported = {}
+
+function rewrite (content, filename, format) {
+  if (!content) return
+
+  let ast
+
+  for (const inst of instrumentations) {
+    const { astQuery, moduleName, versionRange, filePath, channelName, engines } = inst
+    const transform = transforms[inst.operator]
+
+    if (!filename.endsWith(`${moduleName}/${filePath}`)) continue
+    if (!transform) continue
+    if (engines && !semifies(NODE_VERSION, engines)) continue
+    if (!satisfies(filename, filePath, versionRange)) continue
+
+    ast ??= parse(content.toString(), { loc: true, module: format === 'module' })
+
+    const query = astQuery || functionQuery(inst)
+    const selector = esquery.parse(query)
+    const state = { channelName, format, moduleName }
+
+    esquery.traverse(ast, selector, (...args) => transform(state, ...args))
+  }
+
+  return ast ? generate(ast) : content
+}
+
+function satisfies (filename, filePath, versions) {
+  const [basename] = filename.split(filePath)
+
+  if (supported[basename] === undefined) {
+    try {
+      const pkg = JSON.parse(readFileSync(
+        join(basename.replace('file://', ''), 'package.json'), 'utf8'
+      ))
+
+      supported[basename] = semifies(pkg.version, versions)
+    } catch {
+      supported[basename] = false
+    }
+  }
+
+  return supported[basename]
+}
+
+// TODO: Support index
+function functionQuery (inst) {
+  const { functionQuery } = inst
+  const { methodName, functionName, expressionName, className } = functionQuery
+  const kind = functionQuery.kind?.toLowerCase()
+
+  let queries = []
+
+  if (className) {
+    queries.push(
+      `[id.name="${className}"] > ClassBody > [key.name="${methodName}"] > [async]`
+    )
+  } else if (methodName) {
+    queries.push(
+      `ClassBody > [key.name="${methodName}"] > [async]`,
+      `Property[key.name="${methodName}"] > [async]`
+    )
+  }
+
+  if (functionName) {
+    queries.push(`FunctionDeclaration[id.name="${functionName}"]`)
+  } else if (expressionName) {
+    queries.push(
+      `FunctionExpression[id.name="${expressionName}"]`,
+      `ArrowFunctionExpression[id.name="${expressionName}"]`
+    )
+  }
+
+  if (kind) {
+    queries = queries.map(q => `${q}[async="${kind === 'async' ? 'true' : 'false'}"]`)
+  }
+
+  return queries.join(', ')
+}
+
+module.exports = { rewrite }
--- packages/datadog-instrumentations/src/langchain.js
+++ packages/datadog-instrumentations/src/langchain.js
@@ -1,113 +1,25 @@
 'use strict'
+
 const { addHook } = require('./helpers/instrument')
-
-const shimmer = require('../../datadog-shimmer')
-
-const tracingChannel = require('dc-polyfill').tracingChannel
-
-function wrap (obj, name, channelName, namespace) {
-  const channel = tracingChannel(channelName)
-  shimmer.wrap(obj, name, function (original) {
-    return function () {
-      if (!channel.start.hasSubscribers) {
-        return original.apply(this, arguments)
-      }
-      const ctx = { self: this, arguments }
-      if (namespace) {
-        ctx.namespace = namespace
-      }
-      return channel.tracePromise(original, ctx, this, ...arguments)
-    }
-  })
-}
 
 // langchain compiles into ESM and CommonJS, with ESM being the default and landing in the `.js` files
 // however, CommonJS ends up in `cjs` files, and are required under the hood with `.cjs` files
 // we patch each separately and explicitly to match against exports only once, and not rely on file regex matching
 const extensions = ['js', 'cjs']
 
+// TODO: Can we avoid looping? We should only need to know about the entrypoint
+// for the plugin to load.
 for (const extension of extensions) {
-  addHook({ name: '@langchain/core', file: `dist/runnables/base.${extension}`, versions: ['>=0.1'] }, exports => {
-    if (extension === 'cjs') {
-      wrap(exports.RunnableSequence.prototype, 'invoke', 'orchestrion:@langchain/core:RunnableSequence_invoke')
-      wrap(exports.RunnableSequence.prototype, 'batch', 'orchestrion:@langchain/core:RunnableSequence_batch')
-    }
-    return exports
-  })
+  const files = [
+    `dist/runnables/base.${extension}`,
+    `dist/language_models/chat_models.${extension}`,
+    `dist/language_models/llms.${extension}`,
+    `dist/tools/index.${extension}`,
+    `dist/vectorstores.${extension}`,
+    `dist/embeddings.${extension}`
+  ]
 
-  addHook({
-    name: '@langchain/core',
-    file: `dist/language_models/chat_models.${extension}`,
-    versions: ['>=0.1']
-  }, exports => {
-    if (extension === 'cjs') {
-      wrap(exports.BaseChatModel.prototype, 'generate', 'orchestrion:@langchain/core:BaseChatModel_generate')
-    }
-    return exports
-  })
-
-  addHook({ name: '@langchain/core', file: `dist/language_models/llms.${extension}`, versions: ['>=0.1'] }, exports => {
-    if (extension === 'cjs') {
-      wrap(exports.BaseLLM.prototype, 'generate', 'orchestrion:@langchain/core:BaseLLM_generate')
-    }
-    return exports
-  })
-
-  addHook({ name: '@langchain/core', file: `dist/tools/index.${extension}`, versions: ['>=0.1'] }, exports => {
-    if (extension === 'cjs') {
-      wrap(exports.StructuredTool.prototype, 'invoke', 'orchestrion:@langchain/core:Tool_invoke')
-    }
-    return exports
-  })
-
-  addHook({ name: '@langchain/core', file: `dist/vectorstores.${extension}`, versions: ['>=0.1'] }, exports => {
-    if (extension === 'cjs') {
-      wrap(
-        exports.VectorStore.prototype, 'similaritySearch', 'orchestrion:@langchain/core:VectorStore_similaritySearch'
-      )
-      wrap(
-        exports.VectorStore.prototype, 'similaritySearchWithScore',
-        'orchestrion:@langchain/core:VectorStore_similaritySearchWithScore'
-      )
-    }
-
-    return exports
-  })
-
-  addHook({ name: '@langchain/core', file: `dist/embeddings.${extension}`, versions: ['>=0.1'] }, exports => {
-    if (extension === 'cjs') {
-      shimmer.wrap(exports, 'Embeddings', Embeddings => {
-        return class extends Embeddings {
-          constructor (...args) {
-            super(...args)
-
-            const namespace = ['langchain', 'embeddings']
-
-            if (this.constructor.name === 'OpenAIEmbeddings') {
-              namespace.push('openai')
-            }
-
-            wrap(this, 'embedQuery', 'apm:@langchain/core:Embeddings_embedQuery', namespace)
-            wrap(this, 'embedDocuments', 'apm:@langchain/core:Embeddings_embedDocuments', namespace)
-          }
-        }
-      })
-    } else {
-      const channel = tracingChannel('orchestrion:@langchain/core:Embeddings_constructor')
-      channel.subscribe({
-        end (ctx) {
-          const { self } = ctx
-          const namespace = ['langchain', 'embeddings']
-
-          if (self.constructor.name === 'OpenAIEmbeddings') {
-            namespace.push('openai')
-          }
-
-          wrap(self, 'embedQuery', 'apm:@langchain/core:Embeddings_embedQuery', namespace)
-          wrap(self, 'embedDocuments', 'apm:@langchain/core:Embeddings_embedDocuments', namespace)
-        }
-      })
-    }
-    return exports
-  })
+  for (const file of files) {
+    addHook({ name: '@langchain/core', versions: ['>=0.1'], file }, exports => exports)
+  }
 }
