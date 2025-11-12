eb0663f32e9aa150d7ad0d1d5db3df0a4c0daf71
rochdev <roch.devost@gmail.com>
2025-11-12 17:55:19 -0500

    install rust

--- benchmark/sirun/runall.sh
+++ benchmark/sirun/runall.sh
@@ -25,6 +25,9 @@ else
   source /usr/local/nvm/nvm.sh
 fi
 
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl https://drager.github.io/wasm-pack/installer/init.sh -sSf | sh
+
 (
   cd ../../ &&
   npm install --global yarn || (sleep 60 && npm install --global yarn) \
