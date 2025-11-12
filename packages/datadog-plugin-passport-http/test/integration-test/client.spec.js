ac63250b2bdcba2b70ef081ad866553b649db8ef
Pablo Erhard <104538390+pabloerhard@users.noreply.github.com>
2025-11-12 15:34:37 -0500

    chore: add ESM tests for instrumentations missing them (#6814)
    * Added ESM tests for instrumentations without integration tests
    
    * fix knex ESM and unit tests
    
    * Added todo entry and removed unnecessary require

--- packages/datadog-plugin-passport-http/test/integration-test/client.spec.js
+++ packages/datadog-plugin-passport-http/test/integration-test/client.spec.js
@@ -7,15 +7,15 @@ const {
 const assert = require('node:assert/strict')
 const { withVersions } = require('../../../dd-trace/test/setup/mocha')
 
-withVersions('express-session', 'express-session', version => {
+withVersions('passport-http', 'passport-http', version => {
   describe('ESM', () => {
     let variants, proc, agent
 
-    useSandbox([`'express-session@${version}'`, 'express'], false,
-      ['./packages/datadog-plugin-express-session/test/integration-test/*'])
+    useSandbox(['passport', `passport-http@${version}`, 'express'], false,
+      ['./packages/datadog-plugin-passport-http/test/integration-test/*'])
 
     before(function () {
-      variants = varySandbox('server.mjs', 'expressSession', undefined, 'express-session')
+      variants = varySandbox('server.mjs', 'passport-http, BasicStrategy')
     })
 
     beforeEach(async () => {
