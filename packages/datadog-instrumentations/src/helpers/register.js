6a1fcc01cdbe3b830145bdfa8148b1812099eadf
Ruben Bridgewater <ruben.bridgewater@datadoghq.com>
2025-11-13 16:56:59 +0100

    fix: always send telemetry for not instrumented code
    This makes sure not instrumented code will always send out the
    proper telemetry. Formerly, it would rely on any part of the file
    being instrumented. Now, we properly check by version again. To
    guarantee it is only logging it once, it is send with a timeout or
    before the service ends in case that happens earlier.
    
    It also fixes DD_TRACE_DISABLED_INSTRUMENTATIONS to work for
    prefixed and unprefixed Node automatically in case either is
    deactivated. Before, both would have to be deactivated.
    
    In addition, it simplifies code, adds some JSDoc and makes sure
    the Node.js version is added to Node.js modules in the telemetry /
    failures.
    
    This also fixes a couple of tests that were not properly checking
    the behavior as some state was captured in the instrumentation.

--- packages/datadog-instrumentations/src/helpers/register.js
+++ packages/datadog-instrumentations/src/helpers/register.js
@@ -4,7 +4,6 @@ const { channel } = require('dc-polyfill')
 const path = require('path')
 const satisfies = require('semifies')
 const Hook = require('./hook')
-const requirePackageJson = require('../../../dd-trace/src/require-package-json')
 const log = require('../../../dd-trace/src/log')
 const checkRequireCache = require('./check-require-cache')
 const telemetry = require('../../../dd-trace/src/guardrails/telemetry')
@@ -44,24 +43,26 @@ if (DD_TRACE_DEBUG && DD_TRACE_DEBUG.toLowerCase() !== 'false') {
 }
 
 /** @type {Set<string>} */
-const seenCombo = new Set()
-/** @type {Record<string, boolean>} */
-const allInstrumentations = {}
-/** @type {Set<string>} */
 const instrumentedNodeModules = new Set()
+/** @type {Map<string, boolean>} */
+const instrumentedIntegrationsSuccess = new Map()
+/** @type {Set<string>} */
+const alreadyLoggedIncompatibleIntegrations = new Set()
 
 for (const name of names) {
   if (name.startsWith('node:')) {
     // Add all unprefixed node modules to the instrumentations list.
-    names.push(name.slice(5))
+    const unprefixedName = name.slice(5)
+    names.push(unprefixedName)
     // Always disable prefixed and unprefixed node modules if one is disabled.
-    // TODO: Activate & write a regression test for this.
-    // if (disabledInstrumentations.has(name) !== disabledInstrumentations.has(`node:${name}`)) {
-    //   disabledInstrumentations.add(name)
-    //   disabledInstrumentations.add(`node:${name}`)
-    // }
+    if (disabledInstrumentations.has(name) !== disabledInstrumentations.has(unprefixedName)) {
+      disabledInstrumentations.add(name)
+      disabledInstrumentations.add(unprefixedName)
+    }
   }
 }
+
+let timeout
 
 for (const name of names) {
   if (disabledInstrumentations.has(name)) continue
@@ -81,10 +82,19 @@ for (const name of names) {
 
   const nameWithoutPrefix = name.startsWith('node:') ? name.slice(5) : name
 
-  // get the instrumentation file name to save all hooked versions
-  const instrumentationFileName = parseHookInstrumentationFileName(hook)
-
   Hook([name], hookOptions, (moduleExports, moduleName, moduleBaseDir, moduleVersion, isIitm) => {
+    if (timeout === undefined) {
+      // Delay the logging of aborted integrations to handle async loading graphs.
+      timeout = setTimeout(() => {
+        logAbortedIntegrations()
+      }, 100).unref()
+    } else {
+      timeout.refresh()
+    }
+    // All loaded versions are first expected to fail instrumentation.
+    if (!instrumentedIntegrationsSuccess.has(`${nameWithoutPrefix}@${moduleVersion}`)) {
+      instrumentedIntegrationsSuccess.set(`${nameWithoutPrefix}@${moduleVersion}`, false)
+    }
     moduleName = moduleName.replace(pathSepExpr, '/')
 
     // This executes the integration file thus adding its entries to `instrumentations`
@@ -99,7 +109,6 @@ for (const name of names) {
       instrumentedNodeModules.add(nameWithoutPrefix)
     }
 
-    const namesAndSuccesses = {}
     for (const { file, versions, hook, filePattern, patchDefault } of instrumentations[nameWithoutPrefix]) {
       if (isIitm && patchDefault === !!moduleExports.default) {
         if (patchDefault) {
@@ -117,77 +126,59 @@ for (const name of names) {
       if (fullFilePattern) {
         // Some libraries include a hash in their filenames when installed,
         // so our instrumentation has to include a '.*' to match them for more than a single version.
-        matchesFile = matchesFile || new RegExp(fullFilePattern).test(moduleName)
+        matchesFile ||= new RegExp(fullFilePattern).test(moduleName)
       }
 
-      if (matchesFile) {
-        let version = moduleVersion
+      if (matchesFile && matchVersion(moduleVersion, versions)) {
+        // Do not log in case of an error to prevent duplicate telemetry for the same integration version.
+        instrumentedIntegrationsSuccess.set(`${nameWithoutPrefix}@${moduleVersion}`, true)
         try {
-          version = version || getVersion(moduleBaseDir)
-        } catch (e) {
-          log.error('Error getting version for "%s": %s', name, e.message, e)
-          continue
-        }
-        if (namesAndSuccesses[`${nameWithoutPrefix}@${version}`] === undefined && !file) {
-          // TODO If `file` is present, we might elsewhere instrument the result of the module
-          // for a version range that actually matches, so we can't assume that we're _not_
-          // going to instrument that. However, the way the data model around instrumentation
-          // works, we can't know either way just yet, so to avoid false positives, we'll just
-          // ignore this if there is a `file` in the hook. The thing to do here is rework
-          // everything so that we can be sure that there are _no_ instrumentations that it
-          // could match.
-          namesAndSuccesses[`${nameWithoutPrefix}@${version}`] = false
-        }
+          loadChannel.publish({ name })
 
-        if (matchVersion(version, versions)) {
-          allInstrumentations[instrumentationFileName] = true
-
-          try {
-            loadChannel.publish({ name })
-
-            moduleExports = hook(moduleExports, version) ?? moduleExports
-          } catch (e) {
-            log.info('Error during ddtrace instrumentation of application, aborting.', e)
-            telemetry('error', [
-              `error_type:${e.constructor.name}`,
-              `integration:${nameWithoutPrefix}`,
-              `integration_version:${version}`
-            ], {
-              result: 'error',
-              result_class: 'internal_error',
-              result_reason: `Error during instrumentation of ${name}@${version}: ${e.message}`
-            })
-          }
-          namesAndSuccesses[`${nameWithoutPrefix}@${version}`] = true
+          moduleExports = hook(moduleExports, moduleVersion) ?? moduleExports
+        } catch (error) {
+          log.info('Error during ddtrace instrumentation of application, aborting.', error)
+          telemetry('error', [
+            `error_type:${error.constructor.name}`,
+            `integration:${nameWithoutPrefix}`,
+            `integration_version:${moduleVersion}`
+          ], {
+            result: 'error',
+            result_class: 'internal_error',
+            result_reason: `Error during instrumentation of ${name}@${moduleVersion}: ${error.message}`
+          })
         }
-      }
-    }
-    for (const nameVersion of Object.keys(namesAndSuccesses)) {
-      const success = namesAndSuccesses[nameVersion]
-      // we check allVersions to see if any version of the integration was successfully instrumented
-      // TODO: The allInstrumentations check is meant to fix https://github.com/DataDog/dd-trace-js/issues/5092
-      // This is a workaround to actually detect if any hook inside of the instrumentation file was successful or not.
-      // This could be simplified by checking it in a different way. Another alternative is to mention which hooks were
-      // successful or not. That would also be better for debugging. If no filename is detected, the allInstrumentations
-      // check will always be false - which is a mistake.
-      // The current seenCombo also seems to be redundant to namesAndSuccesses. Refactor this if possible.
-      if (!success && !seenCombo.has(nameVersion) && !allInstrumentations[instrumentationFileName]) {
-        const [name, version] = nameVersion.split('@')
-        telemetry('abort.integration', [
-          `integration:${name}`,
-          `integration_version:${version}`
-        ], {
-          result: 'abort',
-          result_class: 'incompatible_library',
-          result_reason: `Incompatible integration version: ${name}@${version}`
-        })
-        log.info('Found incompatible integration version: %s', nameVersion)
-        seenCombo.add(nameVersion)
       }
     }
 
     return moduleExports
   })
+}
+
+// Used in case the process exits before the timeout is triggered.
+process.on('beforeExit', () => {
+  logAbortedIntegrations()
+})
+
+function logAbortedIntegrations () {
+  for (const [nameVersion, success] of instrumentedIntegrationsSuccess) {
+    // Only ever log a single version of an integration, even if it is loaded later.
+    if (!success && !alreadyLoggedIncompatibleIntegrations.has(nameVersion)) {
+      const [name, version] = nameVersion.split('@')
+      telemetry('abort.integration', [
+        `integration:${name}`,
+        `integration_version:${version}`
+      ], {
+        result: 'abort',
+        result_class: 'incompatible_library',
+        result_reason: `Incompatible integration version: ${name}@${version}`
+      })
+      log.info('Found incompatible integration version: %s', nameVersion)
+      alreadyLoggedIncompatibleIntegrations.add(nameVersion)
+    }
+  }
+  // Clear the map to avoid reporting the same integration version again.
+  instrumentedIntegrationsSuccess.clear()
 }
 
 /**
@@ -199,52 +190,12 @@ function matchVersion (version, ranges) {
 }
 
 /**
- * @param {string} moduleBaseDir
- * @returns {string|undefined}
- */
-function getVersion (moduleBaseDir) {
-  if (moduleBaseDir) {
-    return requirePackageJson(moduleBaseDir, /** @type {import('module').Module} */ (module)).version
-  }
-}
-
-/**
  * @param {string} name
  * @param {string} [file]
  * @returns {string}
  */
 function filename (name, file) {
   return file ? `${name}/${file}` : name
-}
-
-// This function captures the instrumentation file name for a given package by parsing the hook require
-// function given the module name. It is used to ensure that instrumentations such as redis
-// that have several different modules being hooked, ie: 'redis' main package, and @redis/client submodule
-// return a consistent instrumentation name. This is used later to ensure that at least some portion of
-// the integration was successfully instrumented. Prevents incorrect `Found incompatible integration version: ` messages
-// Example:
-//                  redis -> "() => require('../redis')" -> redis
-//          @redis/client -> "() => require('../redis')" -> redis
-//
-/**
- * @param {Function} hook
- * @returns {string|undefined}
- */
-function parseHookInstrumentationFileName (hook) {
-  const hookString = hook.toString()
-
-  const regex = /require\('([^']*)'\)/
-  const match = hookString.match(regex)
-
-  // try to capture the hook require file location.
-  if (match && match[1]) {
-    let moduleName = match[1]
-    // Remove leading '../' if present
-    if (moduleName.startsWith('../')) {
-      moduleName = moduleName.slice(3)
-    }
-    return moduleName
-  }
 }
 
 module.exports = {
--- packages/dd-trace/test/config/disabled_instrumentations.spec.js
+++ packages/dd-trace/test/config/disabled_instrumentations.spec.js
@@ -1,34 +1,96 @@
 'use strict'
 
-const { expect } = require('chai')
+const assert = require('node:assert')
+
+const proxyquire = require('proxyquire')
 const { describe, it } = require('tap').mocha
 
 require('../setup/core')
 
-describe('config/disabled_instrumentations', () => {
-  it('should disable loading instrumentations completely', () => {
-    process.env.DD_TRACE_DISABLED_INSTRUMENTATIONS = 'express'
-    const handleBefore = require('express').application.handle
-    const tracer = require('../../../..')
-    const handleAfterImport = require('express').application.handle
-    tracer.init()
-    const handleAfterInit = require('express').application.handle
+describe('config/instrumentations', () => {
+  const httpRequest = require('http').request
+  const expressHandle = require('express').application.handle
 
-    expect(handleBefore).to.equal(handleAfterImport)
-    expect(handleBefore).to.equal(handleAfterInit)
-    delete process.env.DD_TRACE_DISABLED_INSTRUMENTATIONS
-  })
+  function getTracer () {
+    const register = proxyquire.noPreserveCache()('../../../datadog-instrumentations/src/helpers/register', {})
+    const instrumentations = proxyquire('../../../datadog-instrumentations/src/helpers/instrumentations', {
+      './src/helpers/register': register
+    })
+    const pluginManager = proxyquire('../../src/plugin_manager', {
+      '../../datadog-instrumentations': instrumentations
+    })
+    const proxy = proxyquire('../../src/proxy', {
+      './plugin_manager': pluginManager
+    })
+    const TracerProxy = proxyquire('../../src', {
+      './proxy': proxy
+    })
+    return proxyquire('../../', {
+      './src': TracerProxy
+    })
+  }
 
-  it('should disable loading instrumentations using DD_TRACE_<INTEGRATION>_ENABLED', () => {
-    process.env.DD_TRACE_EXPRESS_ENABLED = 'false'
-    const handleBefore = require('express').application.handle
-    const tracer = require('../../../..')
-    const handleAfterImport = require('express').application.handle
-    tracer.init()
-    const handleAfterInit = require('express').application.handle
+  ['disable', 'enable'].forEach((mode) => {
+    /** @type {(a: unknown, b: unknown) => void} */
+    const assertionMethod = mode === 'disable' ? assert.strictEqual : assert.notStrictEqual
 
-    expect(handleBefore).to.equal(handleAfterImport)
-    expect(handleBefore).to.equal(handleAfterInit)
-    delete process.env.DD_TRACE_EXPRESS_ENABLED
+    describe(`config/${mode}_instrumentations`, () => {
+      it(`should ${mode} node prefixed and unprefixed http instrumentations completely`, () => {
+        if (mode === 'disable') {
+          process.env.DD_TRACE_DISABLED_INSTRUMENTATIONS = 'http,express'
+        }
+        const tracer = getTracer()
+        const prefixedHandleAfterImport = require('node:http').request
+        const handleAfterImport = require('http').request
+        tracer.init()
+        const prefixedHandleAfterInit = require('http').request
+        const handleAfterInit = require('http').request
+
+        assertionMethod(httpRequest, handleAfterImport)
+        assertionMethod(httpRequest, handleAfterInit)
+        assertionMethod(httpRequest, prefixedHandleAfterImport)
+        assertionMethod(httpRequest, prefixedHandleAfterInit)
+        assert.strictEqual(handleAfterImport, handleAfterInit)
+        assert.strictEqual(prefixedHandleAfterImport, prefixedHandleAfterInit)
+        delete process.env.DD_TRACE_DISABLED_INSTRUMENTATIONS
+      })
+
+      it(`should ${mode} loading instrumentations completely`, () => {
+        if (mode === 'disable') {
+          process.env.DD_TRACE_DISABLED_INSTRUMENTATIONS = 'express'
+        }
+        const tracer = getTracer()
+        // Ensure Express is reloaded through the instrumentation hook by clearing Node's require cache.
+        delete require.cache[require.resolve('express')]
+        // @ts-expect-error Express handle is not typed as it is an internal property
+        const handleAfterImport = require('express').application.handle
+        tracer.init()
+        // Reload again post-init to validate behavior after tracer initialization.
+        // @ts-expect-error Express handle is not typed as it is an internal property
+        const handleAfterInit = require('express').application.handle
+
+        assertionMethod(expressHandle, handleAfterImport)
+        assertionMethod(expressHandle, handleAfterInit)
+        delete process.env.DD_TRACE_DISABLED_INSTRUMENTATIONS
+      })
+
+      if (mode === 'disable') {
+        it('should not disable loading instrumentations using DD_TRACE_<INTEGRATION>_ENABLED', () => {
+          process.env.DD_TRACE_EXPRESS_ENABLED = 'false'
+          const tracer = getTracer()
+          delete require.cache[require.resolve('express')]
+          // @ts-expect-error Express handle is not typed as it is an internal property
+          const handleAfterImport = require('express').application.handle
+          tracer.init()
+          // @ts-expect-error Express handle is not typed as it is an internal property
+          const handleAfterInit = require('express').application.handle
+
+          assert.notStrictEqual(expressHandle, handleAfterImport)
+          assert.notStrictEqual(expressHandle, handleAfterInit)
+          assert.strictEqual(handleAfterImport, handleAfterInit)
+          delete process.env.DD_TRACE_EXPRESS_ENABLED
+        })
+      }
+    })
   })
 })
