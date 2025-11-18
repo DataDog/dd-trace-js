20a64b5e0e08929980864f2d7fa05b9ebe26caf8
Leo Romanovsky <leo.romanovsky@datadoghq.com>
2025-11-18 00:34:37 -0500

    allow providing a custom timeout to openfeature datadog provider (#6930)

--- packages/dd-trace/test/openfeature/flagging_provider.spec.js
+++ packages/dd-trace/test/openfeature/flagging_provider.spec.js
@@ -23,7 +23,13 @@ describe('FlaggingProvider', () => {
     mockConfig = {
       service: 'test-service',
       version: '1.0.0',
-      env: 'test'
+      env: 'test',
+      experimental: {
+        flaggingProvider: {
+          enabled: true,
+          initializationTimeoutMs: 30_000
+        }
+      }
     }
 
     mockChannel = {
@@ -65,7 +71,7 @@ describe('FlaggingProvider', () => {
       const provider = new FlaggingProvider(mockTracer, mockConfig)
 
       expect(provider).to.exist
-      expect(log.debug).to.have.been.calledWith('FlaggingProvider created')
+      expect(log.debug).to.have.been.calledWith('FlaggingProvider created with timeout: 30000ms')
     })
   })
 
