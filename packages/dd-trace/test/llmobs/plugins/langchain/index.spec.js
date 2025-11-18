ab7a4d5ae0462386cd3bb65f903abaa7193e045b
Sam Brenner <106700075+sabrenner@users.noreply.github.com>
2025-11-18 14:35:25 -0500

    chore(llmobs): move span processing hook to on span finish instead of core processor (#6875)
    * move llmobs span processing to span finish
    
    * fix "integration" test file
    
    ---------
    
    Co-authored-by: Ruben Bridgewater <ruben@bridgewater.de>

--- packages/dd-trace/test/llmobs/plugins/langchain/index.spec.js
+++ packages/dd-trace/test/llmobs/plugins/langchain/index.spec.js
@@ -409,7 +409,7 @@ describe('integrations', () => {
 
             await chain.invoke({ input: 'Can you tell me about LangSmith?' })
 
-            const { apmSpans, llmobsSpans } = await getEvents()
+            const { apmSpans, llmobsSpans } = await getEvents(2)
 
             const workflowSpan = apmSpans[0]
             const llmSpan = apmSpans[1]
@@ -497,7 +497,7 @@ describe('integrations', () => {
             })
             assert.ok(result)
 
-            const { apmSpans, llmobsSpans } = await getEvents()
+            const { apmSpans, llmobsSpans } = await getEvents(5)
 
             const topLevelWorkflow = apmSpans[0]
             const firstSubWorkflow = apmSpans[1]
@@ -604,7 +604,7 @@ describe('integrations', () => {
 
             await chain.batch(['chickens', 'dogs'])
 
-            const { apmSpans, llmobsSpans } = await getEvents()
+            const { apmSpans, llmobsSpans } = await getEvents(3)
 
             const workflowSpan = apmSpans[0]
             const firstLLMSpan = apmSpans[1]
@@ -677,7 +677,7 @@ describe('integrations', () => {
               input: 'What is the powerhouse of the cell?'
             })
 
-            const { apmSpans, llmobsSpans } = await getEvents()
+            const { apmSpans, llmobsSpans } = await getEvents(2)
 
             const workflowSpan = apmSpans[0]
             const llmSpan = apmSpans[1]
@@ -760,7 +760,7 @@ describe('integrations', () => {
 
             await chain.invoke({ foo: 'bar' })
 
-            const { apmSpans, llmobsSpans } = await getEvents()
+            const { apmSpans, llmobsSpans } = await getEvents(3)
 
             const workflowSpan = apmSpans[0]
             const taskSpan = apmSpans[1]
@@ -893,7 +893,7 @@ describe('integrations', () => {
           it('submits a retrieval span with a child embedding span for similaritySearch', async () => {
             await vectorstore.similaritySearch('Biology')
 
-            const { apmSpans, llmobsSpans } = await getEvents()
+            const { apmSpans, llmobsSpans } = await getEvents(2)
 
             // first call was for the embedding span in the beforeEach
             const retrievalSpanEvent = llmobsSpans[0]
@@ -918,7 +918,7 @@ describe('integrations', () => {
           it('submits a retrieval span with a child embedding span for similaritySearchWithScore', async () => {
             await vectorstore.similaritySearchWithScore('Biology')
 
-            const { apmSpans, llmobsSpans } = await getEvents()
+            const { apmSpans, llmobsSpans } = await getEvents(2)
 
             // first call was for the embedding span in the beforeEach
             const retrievalSpanEvent = llmobsSpans[0]
