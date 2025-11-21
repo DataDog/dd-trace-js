6c092b91677e5764696e4f4b13f7892f064fbd85
rochdev <roch.devost@gmail.com>
2025-11-21 17:02:49 -0500

    fix lint

--- integration-tests/ci-visibility/subproject/subproject-test.js
+++ integration-tests/ci-visibility/subproject/subproject-test.js
@@ -1,7 +1,5 @@
 'use strict'
 
-// TODO: It shouldn't be necessary to disable n/no-extraneous-require - Research
-// eslint-disable-next-line n/no-extraneous-require
 const { expect } = require('chai')
 const dependency = require('./dependency')
 
