e0d418ef8c470cd6525cf7c853a857ffab79ab27
rochdev <roch.devost@gmail.com>
2025-11-21 09:00:57 -0500

    switch to rspack

--- vendor/rspack.config.js
+++ vendor/rspack.config.js
@@ -12,6 +12,7 @@ module.exports = {
   entry: Object.fromEntries(names.map(name => [name, `./node_modules/${name}`])),
   target: 'node',
   mode: 'production',
+  devtool: false,
   output: {
     filename: '[name].js',
     libraryTarget: 'commonjs2',
