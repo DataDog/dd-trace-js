56136a917cfc1dc7b43590e93838258b53da8748
pabloerhard <pabloerhard02@gmail.com>
2025-11-20 20:26:44 -0500

    esbuild testing fix

--- packages/datadog-instrumentations/src/helpers/instrument.js
+++ packages/datadog-instrumentations/src/helpers/instrument.js
@@ -1,22 +1,10 @@
 'use strict'
 
-const dc = /** @type {typeof import('node:diagnostics_channel')} */ (require('dc-polyfill'))
+const dc = require('dc-polyfill')
 const instrumentations = require('./instrumentations')
 const { AsyncResource } = require('async_hooks')
 
-/**
- * @typedef {import('node:diagnostics_channel').Channel} Channel
- * @typedef {import('node:diagnostics_channel').TracingChannel} TracingChannel
- */
-
-/**
- * @type {Record<string, Channel>}
- */
 const channelMap = {}
-/**
- * @param {string} name
- * @returns {Channel}
- */
 exports.channel = function (name) {
   const maybe = channelMap[name]
   if (maybe) return maybe
@@ -25,14 +13,7 @@ exports.channel = function (name) {
   return ch
 }
 
-/**
- * @type {Record<string, TracingChannel>}
- */
 const tracingChannelMap = {}
-/**
- * @param {string} name
- * @returns {TracingChannel}
- */
 exports.tracingChannel = function (name) {
   const maybe = tracingChannelMap[name]
   if (maybe) return maybe
@@ -43,19 +24,24 @@ exports.tracingChannel = function (name) {
 
 /**
  * @param {object} args
- * @param {string} args.name module name
- * @param {string[]} [args.versions] array of semver range strings
+ * @param {string|string[]} args.name module name
+ * @param {string[]} args.versions array of semver range strings
  * @param {string} [args.file='index.js'] path to file within package to instrument
  * @param {string} [args.filePattern] pattern to match files within package to instrument
- * @param {boolean} [args.patchDefault=true] whether to patch the default export
- * @param {import('./instrumentations').Hook} hook
+ * @param {boolean} [args.patchDefault] whether to patch the default export
+ * @param {(moduleExports: unknown, version: string) => unknown} hook
  */
 exports.addHook = function addHook ({ name, versions, file, filePattern, patchDefault }, hook) {
-  if (!instrumentations[name]) {
-    instrumentations[name] = []
+  if (typeof name === 'string') {
+    name = [name]
   }
 
-  instrumentations[name].push({ versions, file, filePattern, hook, patchDefault })
+  for (const val of name) {
+    if (!instrumentations[val]) {
+      instrumentations[val] = []
+    }
+    instrumentations[val].push({ name: val, versions, file, filePattern, hook, patchDefault })
+  }
 }
 
 exports.AsyncResource = AsyncResource
