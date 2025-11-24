6b21eaee2752bbe9301f27d1de4ee644277db169
Mikayla Toffler <tofflerm@gmail.com>
2025-11-24 16:24:11 -0500

    migrate DD_TRACE_AZURE_EVENTHUBS_BATCH_LINKS_ENABLED and DD_TRACE_AZURE_SERVICEBUS_BATCH_LINKS_ENABLED

--- packages/datadog-plugin-azure-event-hubs/src/producer.js
+++ packages/datadog-plugin-azure-event-hubs/src/producer.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const { getEnvironmentVariable } = require('../../dd-trace/src/config-helper')
+const { getResolvedEnv } = require('../../dd-trace/src/config-env-sources')
 const ProducerPlugin = require('../../dd-trace/src/plugins/producer')
 
 class AzureEventHubsProducerPlugin extends ProducerPlugin {
@@ -75,7 +75,7 @@ function injectTraceContext (tracer, span, event) {
 }
 
 function batchLinksAreEnabled () {
-  const eh = getEnvironmentVariable('DD_TRACE_AZURE_EVENTHUBS_BATCH_LINKS_ENABLED')
+  const eh = getResolvedEnv('DD_TRACE_AZURE_EVENTHUBS_BATCH_LINKS_ENABLED')
   return eh !== 'false'
 }
 
