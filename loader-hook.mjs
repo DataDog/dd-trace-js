3b88006a6ea8a2748785fb82d9eb869f1dff1834
rochdev <roch.devost@gmail.com>
2025-11-23 19:12:03 -0500

    move escape-string-regexp to vendored dependencies

--- loader-hook.mjs
+++ loader-hook.mjs
@@ -1,4 +1,4 @@
-import regexpEscape from 'escape-string-regexp'
+import regexpEscape from './packages/node_modules/escape-string-regexp/index.js'
 import * as iitm from 'import-in-the-middle/hook.mjs'
 import hooks from './packages/datadog-instrumentations/src/helpers/hooks.js'
 import configHelper from './packages/dd-trace/src/config-helper.js'
