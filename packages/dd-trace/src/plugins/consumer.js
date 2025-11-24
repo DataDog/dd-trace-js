5fdd995ac8ddb7bf3be5e401fff9684e9643c0a9
William Conti <william.conti@datadoghq.com>
2025-11-24 14:46:46 -0500

    more necessary changes

--- packages/dd-trace/src/plugins/consumer.js
+++ packages/dd-trace/src/plugins/consumer.js
@@ -7,14 +7,20 @@ class ConsumerPlugin extends InboundPlugin {
   static kind = 'consumer'
   static type = 'messaging'
 
-  startSpan (options, enterOrCtx) {
+  startSpan (name, options, enterOrCtx) {
+    if (typeof name === 'object' && name !== null && enterOrCtx === undefined) {
+      enterOrCtx = options
+      options = name
+      name = this.operationName()
+    }
+
     if (!options.service) {
       options.service = this.config.service || this.serviceName()
     }
     if (!options.kind) {
       options.kind = this.constructor.kind
     }
-    return super.startSpan(this.operationName(), options, enterOrCtx)
+    return super.startSpan(name, options, enterOrCtx)
   }
 }
 
