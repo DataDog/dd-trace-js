998a9163a8b3537cf8daa1f46b125bba830fd3a8
Juan Antonio Fernández de Alba <juan.fernandezdealba@datadoghq.com>
2025-11-26 10:34:33 +0100

    [test optimization] Do not retry if test isolation is not enabled in cypress (#6978)

--- integration-tests/playwright/playwright.spec.js
+++ integration-tests/playwright/playwright.spec.js
@@ -77,8 +77,11 @@ versions.forEach((version) => {
     }
   }
 
-  describe(`playwright@${version}`, () => {
+  describe(`playwright@${version}`, function () {
     let cwd, receiver, childProcess, webAppPort, webPortWithRedirect
+
+    this.retries(2)
+    this.timeout(80000)
 
     useSandbox([`@playwright/test@${version}`, 'typescript'], true)
 
