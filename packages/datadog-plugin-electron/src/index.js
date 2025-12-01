3ca7ea23379faf5a2401f85149ab913ff9914bdc
Roch Devost <roch.devost@datadoghq.com>
2025-12-01 18:59:47 -0500

    add support for net.request

--- packages/datadog-plugin-electron/src/index.js
+++ packages/datadog-plugin-electron/src/index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const FetchPlugin = require('../../datadog-plugin-fetch/src')
+const HttpClientPlugin = require('../../datadog-plugin-http/src/client')
 const CompositePlugin = require('../../dd-trace/src/plugins/composite')
 
 class ElectronPlugin extends CompositePlugin {
@@ -16,16 +16,76 @@ class ElectronNetPlugin extends CompositePlugin {
   static id = 'electron:net'
   static get plugins () {
     return {
-      fetch: ElectronFetchPlugin
+      request: ElectronRequestPlugin
     }
   }
 }
 
-class ElectronFetchPlugin extends FetchPlugin {
-  static id = 'electron:net:fetch'
+class ElectronRequestPlugin extends HttpClientPlugin {
+  static id = 'electron:net:request'
   static component = 'electron'
-  static operation = 'fetch'
-  static prefix = 'tracing:apm:electron:net:fetch'
+  static operation = 'request'
+  static prefix = 'tracing:apm:electron:net:request'
+
+  bindStart (ctx) {
+    const args = ctx.args
+
+    let options = args[0]
+
+    if (typeof options === 'string') {
+      options = args[0] = { url: options }
+    } else if (!options) {
+      options = args[0] = {}
+    }
+
+    const headers = options.headers || {}
+
+    try {
+      if (typeof options === 'string') {
+        options = new URL(options)
+      } else if (options.url) {
+        options = new URL(options.url)
+      }
+    } catch {
+      // leave options as-is
+    }
+
+    options.headers = headers
+    ctx.args = { options }
+
+    const store = super.bindStart(ctx)
+
+    ctx.args = args
+
+    for (const name in options.headers) {
+      if (!headers[name]) {
+        args[0].headers ??= {}
+        args[0].headers[name] = options.headers[name]
+      }
+    }
+
+    return store
+  }
+
+  asyncStart (ctx) {
+    const reqHeaders = {}
+    const resHeaders = {}
+    const responseHead = ctx.res?._responseHead
+    const { statusCode } = responseHead || {}
+
+    for (const header in ctx.req._urlLoaderOptions?.headers || {}) {
+      reqHeaders[header.name] = header.value
+    }
+
+    for (const header in responseHead?.rawHeaders || {}) {
+      resHeaders[header.name] = header.value
+    }
+
+    ctx.req = { headers: reqHeaders }
+    ctx.res = { headers: resHeaders, statusCode }
+
+    this.finish(ctx)
+  }
 }
 
 module.exports = ElectronPlugin
