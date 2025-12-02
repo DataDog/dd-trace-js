de809aa098af2339e3d8e026ee52e84b18b0240f
Carles Capell <carles.capell@datadoghq.com>
2025-12-02 09:44:25 +0100

    Add eslint next config property conditionally on Next.js version

--- packages/dd-trace/test/appsec/next/app-dir/next.config.js
+++ packages/dd-trace/test/appsec/next/app-dir/next.config.js
@@ -1,3 +1,7 @@
+const nextPkg = require('next/package.json')
+const [majorStr] = nextPkg.version.split('.')
+const major = Number(majorStr)
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
@@ -5,6 +9,12 @@ const nextConfig = {
   },
   output: 'standalone',
   outputFileTracingRoot: __dirname
+}
+
+if (major < 16) {
+  nextConfig.eslint = {
+    ignoreDuringBuilds: true,
+  }
 }
 
 module.exports = nextConfig
--- packages/dd-trace/test/appsec/next/pages-dir/next.config.js
+++ packages/dd-trace/test/appsec/next/pages-dir/next.config.js
@@ -1,3 +1,16 @@
-module.exports = {
-  output: 'standalone'
+const nextPkg = require('next/package.json')
+const [majorStr] = nextPkg.version.split('.')
+const major = Number(majorStr)
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
 }
+
+if (major < 16) {
+  nextConfig.eslint = {
+    ignoreDuringBuilds: true,
+  }
+}
+
+module.exports = nextConfig
