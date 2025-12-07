94a55073f94c7ea3c5916d63cde21853e0aca558
rochdev <roch.devost@gmail.com>
2025-12-06 19:41:35 -0500

    fix propagation and add test

--- packages/datadog-plugin-electron/test/app/preload.js
+++ packages/datadog-plugin-electron/test/app/preload.js
@@ -2,8 +2,6 @@
 
 const { contextBridge, ipcRenderer } = require('electron/renderer')
 
-ipcRenderer.send('datadog:apm:full', !!globalThis._ddtrace)
-
 if (globalThis.logger) {
   globalThis.logger.debug = (...args) => ipcRenderer.send('datadog:log', 'debug', ...args)
   globalThis.logger.info = (...args) => ipcRenderer.send('datadog:log', 'info', ...args)
