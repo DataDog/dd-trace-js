be642cfa94962fe7377af8b20a7379806a353e06
Roch Devost <roch.devost@datadoghq.com>
2025-12-11 16:01:28 -0500

    vendor bundled non-datadog dependencies (#6958)

--- packages/datadog-plugin-graphql/src/tools/transforms.js
+++ packages/datadog-plugin-graphql/src/tools/transforms.js
@@ -5,9 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const visitor_1 = require("graphql/language/visitor");
-const printer_1 = require("graphql/language/printer");
-const utilities_1 = require("graphql/utilities");
+const ddGlobal = globalThis[Symbol.for('dd-trace')];
+const visitor_1 = ddGlobal.graphql_visitor;
+const printer_1 = ddGlobal.graphql_printer;
+const utilities_1 = ddGlobal.graphql_utilities;
 const lodash_sortby_1 = __importDefault(require("lodash.sortby"));
 function hideLiterals(ast) {
     return visitor_1.visit(ast, {
