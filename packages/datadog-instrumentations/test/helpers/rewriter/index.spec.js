a5782afe61caf38aef7bfa6801f93f6680366e50
rochdev <roch.devost@gmail.com>
2025-12-10 20:18:14 -0500

    add basic tests

---
+++ packages/datadog-instrumentations/test/helpers/rewriter/index.spec.js
@@ -0,0 +1,132 @@
+'use strict'
+
+const proxyquire = require('proxyquire')
+const { beforeEach, describe, it } = require('mocha')
+const { readFileSync } = require('node:fs')
+const { resolve, join } = require('node:path')
+const Module = require('node:module')
+const { tracingChannel } = require('dc-polyfill')
+
+// TODO: Test actual functionality and not just the start channel.
+describe('check-require-cache', () => {
+  let rewriter
+  let content
+  let ch
+  let subs
+
+  beforeEach(() => {
+    rewriter = proxyquire('../../../src/helpers/rewriter', {
+      './instrumentations': [
+        {
+          module: {
+            name: 'test-trace-sync',
+            versionRange: '>=0.1',
+            filePath: 'index.js'
+          },
+          functionQuery: {
+            functionName: 'test',
+            kind: 'Sync'
+          },
+          channelName: 'test_invoke'
+        },
+        {
+          module: {
+            name: 'test-trace-async',
+            versionRange: '>=0.1',
+            filePath: 'index.js'
+          },
+          functionQuery: {
+            functionName: 'test',
+            kind: 'Async'
+          },
+          channelName: 'test_invoke'
+        },
+        {
+          module: {
+            name: 'test-trace-callback',
+            versionRange: '>=0.1',
+            filePath: 'index.js'
+          },
+          functionQuery: {
+            functionName: 'test',
+            kind: 'Callback'
+          },
+          channelName: 'test_invoke'
+        }
+      ]
+    })
+  })
+
+  afterEach(() => {
+    ch.unsubscribe(subs)
+  })
+
+  it('should auto instrument sync functions', done => {
+    const folder = resolve(__dirname, 'node_modules', 'test-trace-sync')
+    const filename = join(folder, 'index.js')
+    const format = 'commonjs'
+    const test = new Module(filename, module.parent)
+
+    content = readFileSync(filename, 'utf8')
+    content = rewriter.rewrite(content, filename, format)
+
+    test._compile(content, filename, format)
+
+    subs = {
+      start () {
+        done()
+      }
+    }
+
+    ch = tracingChannel('orchestrion:test-trace-sync:test_invoke')
+    ch.subscribe(subs)
+
+    test.exports.test()
+  })
+
+  it('should auto instrument async functions', done => {
+    const folder = resolve(__dirname, 'node_modules', 'test-trace-async')
+    const filename = join(folder, 'index.js')
+    const format = 'commonjs'
+    const test = new Module(filename, module.parent)
+
+    content = readFileSync(filename, 'utf8')
+    content = rewriter.rewrite(content, filename, format)
+
+    test._compile(content, filename, format)
+
+    subs = {
+      start () {
+        done()
+      }
+    }
+
+    ch = tracingChannel('orchestrion:test-trace-async:test_invoke')
+    ch.subscribe(subs)
+
+    test.exports.test()
+  })
+
+  it('should auto instrument callback functions', done => {
+    const folder = resolve(__dirname, 'node_modules', 'test-trace-callback')
+    const filename = join(folder, 'index.js')
+    const format = 'commonjs'
+    const test = new Module(filename, module.parent)
+
+    content = readFileSync(filename, 'utf8')
+    content = rewriter.rewrite(content, filename, format)
+
+    test._compile(content, filename, format)
+
+    subs = {
+      start () {
+        done()
+      }
+    }
+
+    ch = tracingChannel('orchestrion:test-trace-callback:test_invoke')
+    ch.subscribe(subs)
+
+    test.exports.test(() => {})
+  })
+})
