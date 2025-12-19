c67f39423edc345cd046ad5fa85ed99523fc87c6
William Conti <william.conti@datadoghq.com>
2025-12-19 10:03:18 -0500

    handle case where passed in plugins is an array

--- packages/dd-trace/src/plugins/composite.js
+++ packages/dd-trace/src/plugins/composite.js
@@ -6,8 +6,21 @@ class CompositePlugin extends Plugin {
   constructor (...args) {
     super(...args)
 
+    // Track instantiated plugin names for configure()
+    this._pluginNames = []
+
     for (const [name, PluginClass] of Object.entries(this.constructor.plugins)) {
-      this[name] = new PluginClass(...args)
+      // Handle case where value is an array of plugin classes
+      if (Array.isArray(PluginClass)) {
+        for (const SinglePluginClass of PluginClass) {
+          const pluginId = SinglePluginClass.id || name
+          this[pluginId] = new SinglePluginClass(...args)
+          this._pluginNames.push(pluginId)
+        }
+      } else {
+        this[name] = new PluginClass(...args)
+        this._pluginNames.push(name)
+      }
     }
   }
 
@@ -16,7 +29,7 @@ class CompositePlugin extends Plugin {
    */
   configure (config) {
     super.configure(config)
-    for (const name in this.constructor.plugins) {
+    for (const name of this._pluginNames) {
       const pluginConfig = config[name] === false
         ? false
         : { ...config, ...config[name] }
