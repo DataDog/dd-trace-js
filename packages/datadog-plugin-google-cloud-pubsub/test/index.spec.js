95d4d2625e117391dc8a0f660b4f11133aa4f5c8
robcarlan-datadog <rob.carlan@datadoghq.com>
2025-12-19 16:42:26 -0500

    chore(datastreams): separate DSM specific test behavior and add to CODEOWNERS (#7076)

--- packages/datadog-plugin-google-cloud-pubsub/test/index.spec.js
+++ packages/datadog-plugin-google-cloud-pubsub/test/index.spec.js
@@ -3,18 +3,14 @@
 const assert = require('node:assert/strict')
 
 const { after, afterEach, before, beforeEach, describe, it } = require('mocha')
-const sinon = require('sinon')
 
 const { ERROR_MESSAGE, ERROR_TYPE, ERROR_STACK } = require('../../dd-trace/src/constants')
-const { computePathwayHash } = require('../../dd-trace/src/datastreams/pathway')
-const { ENTRY_PARENT_HASH, DataStreamsProcessor } = require('../../dd-trace/src/datastreams/processor')
 const id = require('../../dd-trace/src/id')
 const agent = require('../../dd-trace/test/plugins/agent')
 const { expectSomeSpan, withDefaults } = require('../../dd-trace/test/plugins/helpers')
 const { withNamingSchema, withVersions } = require('../../dd-trace/test/setup/mocha')
 const { expectedSchema, rawExpectedSchema } = require('./naming')
 const TIMEOUT = 30000
-const dsmTopicName = 'dsm-topic'
 
 describe('Plugin', () => {
   let tracer
@@ -24,7 +20,6 @@ describe('Plugin', () => {
 
     before(() => {
       process.env.PUBSUB_EMULATOR_HOST = 'localhost:8081'
-      process.env.DD_DATA_STREAMS_ENABLED = 'true'
     })
 
     after(() => {
@@ -41,8 +36,6 @@ describe('Plugin', () => {
       let resource
       let v1
       let gax
-      let expectedProducerHash
-      let expectedConsumerHash
 
       describe('without configuration', () => {
         beforeEach(() => {
@@ -328,113 +321,6 @@ describe('Plugin', () => {
             })
             await pubsub.createTopic(topicName)
             return expectedSpanPromise
-          })
-        })
-      })
-
-      describe('data stream monitoring', () => {
-        let dsmTopic
-        let sub
-        let consume
-
-        beforeEach(() => {
-          return agent.load('google-cloud-pubsub', {
-            dsmEnabled: true
-          })
-        })
-
-        before(async () => {
-          const { PubSub } = require(`../../../versions/@google-cloud/pubsub@${version}`).get()
-          project = getProjectId()
-          resource = `projects/${project}/topics/${dsmTopicName}`
-          pubsub = new PubSub({ projectId: project })
-          tracer.use('google-cloud-pubsub', { dsmEnabled: true })
-
-          dsmTopic = await pubsub.createTopic(dsmTopicName)
-          dsmTopic = dsmTopic[0]
-          sub = await dsmTopic.createSubscription('DSM')
-          sub = sub[0]
-          consume = function (cb) {
-            sub.on('message', cb)
-          }
-
-          const dsmFullTopic = `projects/${project}/topics/${dsmTopicName}`
-
-          expectedProducerHash = computePathwayHash(
-            'test',
-            'tester',
-            ['direction:out', 'topic:' + dsmFullTopic, 'type:google-pubsub'],
-            ENTRY_PARENT_HASH
-          )
-          expectedConsumerHash = computePathwayHash(
-            'test',
-            'tester',
-            ['direction:in', 'topic:' + dsmFullTopic, 'type:google-pubsub'],
-            expectedProducerHash
-          )
-        })
-
-        describe('should set a DSM checkpoint', () => {
-          it('on produce', async () => {
-            await publish(dsmTopic, { data: Buffer.from('DSM produce checkpoint') })
-
-            agent.expectPipelineStats(dsmStats => {
-              let statsPointsReceived = 0
-              // we should have 1 dsm stats points
-              dsmStats.forEach((timeStatsBucket) => {
-                if (timeStatsBucket && timeStatsBucket.Stats) {
-                  timeStatsBucket.Stats.forEach((statsBuckets) => {
-                    statsPointsReceived += statsBuckets.Stats.length
-                  })
-                }
-              })
-              assert.ok(statsPointsReceived >= 1)
-              assert.strictEqual(agent.dsmStatsExist(agent, expectedProducerHash.readBigUInt64BE(0).toString()), true)
-            }, { timeoutMs: TIMEOUT })
-          })
-
-          it('on consume', async () => {
-            await publish(dsmTopic, { data: Buffer.from('DSM consume checkpoint') })
-            await consume(async () => {
-              agent.expectPipelineStats(dsmStats => {
-                let statsPointsReceived = 0
-                // we should have 2 dsm stats points
-                dsmStats.forEach((timeStatsBucket) => {
-                  if (timeStatsBucket && timeStatsBucket.Stats) {
-                    timeStatsBucket.Stats.forEach((statsBuckets) => {
-                      statsPointsReceived += statsBuckets.Stats.length
-                    })
-                  }
-                })
-                assert.ok(statsPointsReceived >= 2)
-                assert.strictEqual(agent.dsmStatsExist(agent, expectedConsumerHash.readBigUInt64BE(0).toString()), true)
-              }, { timeoutMs: TIMEOUT })
-            })
-          })
-        })
-
-        describe('it should set a message payload size', () => {
-          let recordCheckpointSpy
-
-          beforeEach(() => {
-            recordCheckpointSpy = sinon.spy(DataStreamsProcessor.prototype, 'recordCheckpoint')
-          })
-
-          afterEach(() => {
-            DataStreamsProcessor.prototype.recordCheckpoint.restore()
-          })
-
-          it('when producing a message', async () => {
-            await publish(dsmTopic, { data: Buffer.from('DSM produce payload size') })
-            assert.ok(recordCheckpointSpy.args[0][0].hasOwnProperty('payloadSize'))
-          })
-
-          it('when consuming a message', async () => {
-            await publish(dsmTopic, { data: Buffer.from('DSM consume payload size') })
-
-            await consume(async () => {
-              assert.ok(recordCheckpointSpy.args[0][0].hasOwnProperty('payloadSize'))
-            })
           })
         })
       })
--- packages/datadog-plugin-kafkajs/test/index.spec.js
+++ packages/datadog-plugin-kafkajs/test/index.spec.js
@@ -4,7 +4,7 @@ const assert = require('node:assert/strict')
 const { randomUUID } = require('node:crypto')
 
 const dc = require('dc-polyfill')
-const { describe, it, beforeEach, afterEach, before } = require('mocha')
+const { describe, it, beforeEach, afterEach } = require('mocha')
 const semver = require('semver')
 const sinon = require('sinon')
 
@@ -12,29 +12,11 @@ const { withNamingSchema, withPeerService, withVersions } = require('../../dd-tr
 const agent = require('../../dd-trace/test/plugins/agent')
 const { expectSomeSpan, withDefaults } = require('../../dd-trace/test/plugins/helpers')
 const { ERROR_MESSAGE, ERROR_TYPE, ERROR_STACK } = require('../../dd-trace/src/constants')
-
-const { expectedSchema, rawExpectedSchema } = require('./naming')
-const DataStreamsContext = require('../../dd-trace/src/datastreams/context')
-const { computePathwayHash } = require('../../dd-trace/src/datastreams/pathway')
-const { ENTRY_PARENT_HASH, DataStreamsProcessor } = require('../../dd-trace/src/datastreams/processor')
 const { assertObjectContains } = require('../../../integration-tests/helpers')
 
+const { expectedSchema, rawExpectedSchema } = require('./naming')
+
 const testKafkaClusterId = '5L6g3nShT-eMCtK--X86sw'
-
-const getDsmPathwayHash = (testTopic, clusterIdAvailable, isProducer, parentHash) => {
-  let edgeTags
-  if (isProducer) {
-    edgeTags = ['direction:out', 'topic:' + testTopic, 'type:kafka']
-  } else {
-    edgeTags = ['direction:in', 'group:test-group', 'topic:' + testTopic, 'type:kafka']
-  }
-
-  if (clusterIdAvailable) {
-    edgeTags.push(`kafka_cluster_id:${testKafkaClusterId}`)
-  }
-  edgeTags.sort()
-  return computePathwayHash('test', 'tester', edgeTags, parentHash)
-}
 
 describe('Plugin', () => {
   describe('kafkajs', function () {
@@ -51,8 +33,6 @@ describe('Plugin', () => {
       let Kafka
       let Broker
       let clusterIdAvailable
-      let expectedProducerHash
-      let expectedConsumerHash
       let testTopic
 
       describe('without configuration', () => {
@@ -60,7 +40,6 @@ describe('Plugin', () => {
         const messages2 = [{ key: 'key2', value: 'test3' }]
 
         beforeEach(async () => {
-          process.env.DD_DATA_STREAMS_ENABLED = 'true'
           tracer = require('../../dd-trace')
           await agent.load('kafkajs')
           const lib = require(`../../../versions/kafkajs@${version}`).get()
@@ -81,8 +60,6 @@ describe('Plugin', () => {
             }]
           })
           clusterIdAvailable = semver.intersects(version, '>=1.13')
-          expectedProducerHash = getDsmPathwayHash(testTopic, clusterIdAvailable, true, ENTRY_PARENT_HASH)
-          expectedConsumerHash = getDsmPathwayHash(testTopic, clusterIdAvailable, false, expectedProducerHash)
         })
 
         describe('producer', () => {
@@ -90,7 +67,6 @@ describe('Plugin', () => {
             const meta = {
               'span.kind': 'producer',
               component: 'kafkajs',
-              'pathway.hash': expectedProducerHash.readBigUInt64LE(0).toString(),
               'messaging.destination.name': testTopic,
               'messaging.kafka.bootstrap.servers': '127.0.0.1:9092'
             }
@@ -252,7 +228,6 @@ describe('Plugin', () => {
               meta: {
                 'span.kind': 'consumer',
                 component: 'kafkajs',
-                'pathway.hash': expectedConsumerHash.readBigUInt64LE(0).toString(),
                 'messaging.destination.name': testTopic
               },
               resource: testTopic,
@@ -428,171 +403,6 @@ describe('Plugin', () => {
             },
             rawExpectedSchema.receive
           )
-        })
-
-        describe('data stream monitoring', () => {
-          let consumer
-
-          beforeEach(async () => {
-            tracer.init()
-            tracer.use('kafkajs', { dsmEnabled: true })
-            consumer = kafka.consumer({ groupId: 'test-group' })
-            await consumer.connect()
-            await consumer.subscribe({ topic: testTopic })
-          })
-
-          before(() => {
-            clusterIdAvailable = semver.intersects(version, '>=1.13')
-            expectedProducerHash = getDsmPathwayHash(testTopic, clusterIdAvailable, true, ENTRY_PARENT_HASH)
-            expectedConsumerHash = getDsmPathwayHash(testTopic, clusterIdAvailable, false, expectedProducerHash)
-          })
-
-          afterEach(async () => {
-            await consumer.disconnect()
-          })
-
-          describe('checkpoints', () => {
-            let setDataStreamsContextSpy
-
-            beforeEach(() => {
-              setDataStreamsContextSpy = sinon.spy(DataStreamsContext, 'setDataStreamsContext')
-            })
-
-            afterEach(() => {
-              setDataStreamsContextSpy.restore()
-            })
-
-            it('Should set a checkpoint on produce', async () => {
-              const messages = [{ key: 'consumerDSM1', value: 'test2' }]
-              await sendMessages(kafka, testTopic, messages)
-              assert.strictEqual(setDataStreamsContextSpy.args[0][0].hash, expectedProducerHash)
-            })
-
-            it('Should set a checkpoint on consume (eachMessage)', async () => {
-              const runArgs = []
-              await consumer.run({
-                eachMessage: async () => {
-                  runArgs.push(setDataStreamsContextSpy.lastCall.args[0])
-                }
-              })
-              await sendMessages(kafka, testTopic, messages)
-              await consumer.disconnect()
-              for (const runArg of runArgs) {
-                assert.strictEqual(runArg.hash, expectedConsumerHash)
-              }
-            })
-
-            it('Should set a checkpoint on consume (eachBatch)', async () => {
-              const runArgs = []
-              await consumer.run({
-                eachBatch: async () => {
-                  runArgs.push(setDataStreamsContextSpy.lastCall.args[0])
-                }
-              })
-              await sendMessages(kafka, testTopic, messages)
-              await consumer.disconnect()
-              for (const runArg of runArgs) {
-                assert.strictEqual(runArg.hash, expectedConsumerHash)
-              }
-            })
-
-            it('Should set a message payload size when producing a message', async () => {
-              const messages = [{ key: 'key1', value: 'test2' }]
-              if (DataStreamsProcessor.prototype.recordCheckpoint.isSinonProxy) {
-                DataStreamsProcessor.prototype.recordCheckpoint.restore()
-              }
-              const recordCheckpointSpy = sinon.spy(DataStreamsProcessor.prototype, 'recordCheckpoint')
-              await sendMessages(kafka, testTopic, messages)
-              assert.ok(Object.hasOwn(recordCheckpointSpy.args[0][0], 'payloadSize'))
-              recordCheckpointSpy.restore()
-            })
-
-            it('Should set a message payload size when consuming a message', async () => {
-              const messages = [{ key: 'key1', value: 'test2' }]
-              if (DataStreamsProcessor.prototype.recordCheckpoint.isSinonProxy) {
-                DataStreamsProcessor.prototype.recordCheckpoint.restore()
-              }
-              const recordCheckpointSpy = sinon.spy(DataStreamsProcessor.prototype, 'recordCheckpoint')
-              await sendMessages(kafka, testTopic, messages)
-              await consumer.run({
-                eachMessage: async () => {
-                  assert.ok(Object.hasOwn(recordCheckpointSpy.args[0][0], 'payloadSize'))
-                  recordCheckpointSpy.restore()
-                }
-              })
-            })
-          })
-
-          describe('backlogs', () => {
-            let setOffsetSpy
-
-            beforeEach(() => {
-              setOffsetSpy = sinon.spy(tracer._tracer._dataStreamsProcessor, 'setOffset')
-            })
-
-            afterEach(() => {
-              setOffsetSpy.restore()
-            })
-
-            if (semver.intersects(version, '>=1.10')) {
-              it('Should add backlog on consumer explicit commit', async () => {
-                // Send a message, consume it, and record the last consumed offset
-                let commitMeta
-                const deferred = {}
-                deferred.promise = new Promise((resolve, reject) => {
-                  deferred.resolve = resolve
-                  deferred.reject = reject
-                })
-                await consumer.run({
-                  eachMessage: async payload => {
-                    const { topic, partition, message } = payload
-                    commitMeta = {
-                      topic,
-                      partition,
-                      offset: Number(message.offset)
-                    }
-                    deferred.resolve()
-                  },
-                  autoCommit: false
-                })
-                await sendMessages(kafka, testTopic, messages)
-                await deferred.promise
-                await consumer.disconnect() // Flush ongoing `eachMessage` calls
-                for (const call of setOffsetSpy.getCalls()) {
-                  assert.notStrictEqual(call.args[0]?.type, 'kafka_commit')
-                }
-
-                /**
-                 * No choice but to reinitialize everything, because the only way to flush eachMessage
-                 * calls is to disconnect.
-                 */
-                consumer.connect()
-                await sendMessages(kafka, testTopic, messages)
-                await consumer.run({ eachMessage: async () => {}, autoCommit: false })
-                setOffsetSpy.resetHistory()
-                await consumer.commitOffsets([commitMeta])
-                await consumer.disconnect()
-
-                // Check our work
-                const runArg = setOffsetSpy.lastCall.args[0]
-                sinon.assert.calledOnce(setOffsetSpy)
-                assert.strictEqual(runArg?.offset, commitMeta.offset)
-                assert.strictEqual(runArg?.partition, commitMeta.partition)
-                assert.strictEqual(runArg?.topic, commitMeta.topic)
-                assertObjectContains(runArg, {
-                  type: 'kafka_commit',
-                  consumer_group: 'test-group'
-                })
-              })
-            }
-
-            it('Should add backlog on producer response', async () => {
-              await sendMessages(kafka, testTopic, messages)
-              sinon.assert.calledOnce(setOffsetSpy)
-              const { topic } = setOffsetSpy.lastCall.args[0]
-              assert.strictEqual(topic, testTopic)
-            })
-          })
         })
       })
     })
