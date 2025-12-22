0074494fc6731cce9fee2049dd59ee79772c94f8
Thomas Hunter II <tlhunter@datadog.com>
2025-12-22 06:49:12 -0800

    [DI] add process tags to dynamic instrumentation (#7132)

--- packages/dd-trace/src/process-tags/index.js
+++ packages/dd-trace/src/process-tags/index.js
@@ -1,24 +1,9 @@
 'use strict'
 
 const path = require('node:path')
-const pkg = require('../pkg')
 
 const CURRENT_WORKING_DIRECTORY = process.cwd()
 const ENTRYPOINT_PATH = require.main?.filename || ''
-
-const TRACING_FIELD_NAME = '_dd.tags.process'
-const DSM_FIELD_NAME = 'ProcessTags'
-const PROFILING_FIELD_NAME = 'process_tags'
-
-module.exports.TRACING_FIELD_NAME = TRACING_FIELD_NAME
-module.exports.DSM_FIELD_NAME = DSM_FIELD_NAME
-module.exports.PROFILING_FIELD_NAME = PROFILING_FIELD_NAME
-
-// TODO CRASH_TRACKING_FIELD_NAME /process_tags /application/process_tags
-// TODO: TELEMETRY_FIELD_NAME /application/process_tags
-// TODO: DYNAMIC_INSTRUMENTATION_FIELD_NAME process_tags
-// TODO: CLIENT_TRACE_STATISTICS_FIELD_NAME process_tags
-// TODO: REMOTE_CONFIG_FIELD_NAME process_tags
 
 // $ cd /foo/bar && node baz/banana.js
 // entrypoint.workdir = bar
@@ -27,7 +12,11 @@ module.exports.PROFILING_FIELD_NAME = PROFILING_FIELD_NAME
 // entrypoint.basedir = baz
 // package.json.name = <from package.json>
 
-module.exports = function getProcessTags () {
+// process tags are constant throughout the lifetime of a process
+function getProcessTags () {
+  // Lazy load pkg to avoid issues with require.main during test initialization
+  const pkg = require('../pkg')
+
   // this list is sorted alphabetically for consistent serialization
   const tags = [
     // the parent directory name of the entrypoint script, e.g. /foo/bar/baz/banana.js -> baz
@@ -48,16 +37,36 @@ module.exports = function getProcessTags () {
 
   const serialized = serialize(tags)
 
+  const tagsObject = tags.reduce((acc, [key, value]) => {
+    if (value !== undefined) {
+      acc[key] = value
+    }
+    return acc
+  }, {})
+
   return {
     tags,
-    serialized
+    serialized,
+    tagsObject
   }
 }
+
+// Export the singleton
+module.exports = getProcessTags()
+
+module.exports.TRACING_FIELD_NAME = '_dd.tags.process'
+module.exports.DSM_FIELD_NAME = 'ProcessTags'
+module.exports.PROFILING_FIELD_NAME = 'process_tags'
+module.exports.DYNAMIC_INSTRUMENTATION_FIELD_NAME = 'process_tags'
+
+// TODO: CRASH_TRACKING_FIELD_NAME /process_tags /application/process_tags
+// TODO: TELEMETRY_FIELD_NAME /application/process_tags
+// TODO: CLIENT_TRACE_STATISTICS_FIELD_NAME process_tags
+// TODO: REMOTE_CONFIG_FIELD_NAME process_tags
 
 function serialize (tags) {
   const intermediary = []
   for (const [name, value] of tags) {
-    // if we don't have a value we should send nothing at all
     if (value === undefined) continue
     intermediary.push(`${name}:${sanitize(value)}`)
   }
