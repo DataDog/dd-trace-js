cc81eba8fe40bf39766dfad8ae7113c826da7e0d
William Conti <william.conti@datadoghq.com>
2026-01-06 11:18:27 -0500

    reveiwer comments

---
+++ packages/dd-trace/test/setup/helpers/plugin-test-helpers/index.js
@@ -0,0 +1,50 @@
+'use strict'
+
+const { describe, before, after } = require('mocha')
+const agent = require('../../../plugins/agent')
+const { withVersions } = require('../../mocha')
+
+function createIntegrationTestSuite (pluginName, packageName, testSetup, options, testCallback) {
+  describe('Plugin', () => {
+    describe(pluginName, () => {
+      withVersions(pluginName, packageName, version => {
+        let tracer = null
+        let mod = null
+
+        describe('without configuration', () => {
+          before(async () => {
+            const plugins = [pluginName, ...(options.additionalPlugins || [])]
+            const pluginConfigs = [options.pluginConfig || {}, ...(options.additionalPlugins || []).map(() => ({}))]
+            await agent.load(plugins, pluginConfigs)
+          })
+
+          after(async () => {
+            await agent.close({ ritmReset: false })
+          })
+
+          before(async () => {
+            tracer = require('../../../../../dd-trace').init()
+            mod = require(`../../../../../../versions/${packageName}@${version}`)
+            mod = options.subModule ? mod.get(options.subModule) : mod.get()
+            await testSetup.setup(mod)
+          })
+
+          after(async () => {
+            if (testSetup?.teardown) {
+              await testSetup.teardown()
+            }
+          })
+
+          testCallback({
+            testSetup,
+            agent,
+            tracer,
+            mod
+          })
+        })
+      })
+    })
+  })
+}
+
+module.exports = { createIntegrationTestSuite }
