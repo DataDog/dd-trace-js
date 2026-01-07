eebf148e4353e22ce631531498d9ee10a4ade000
zmzlois <lois.z@comcord.vision>
2026-01-07 00:29:02 +0000

    add bun to environment

--- packages/dd-trace/src/plugins/util/env.js
+++ packages/dd-trace/src/plugins/util/env.js
@@ -1,22 +1,23 @@
-'use strict'
+"use strict";
 
-const os = require('os')
+const os = require("os");
+const { runtimeName, runtimeVersion } = require("../../utils/runtime");
 
-const OS_PLATFORM = 'os.platform'
-const OS_VERSION = 'os.version'
-const OS_ARCHITECTURE = 'os.architecture'
-const RUNTIME_NAME = 'runtime.name'
-const RUNTIME_VERSION = 'runtime.version'
-const DD_HOST_CPU_COUNT = '_dd.host.vcpu_count'
+const OS_PLATFORM = "os.platform";
+const OS_VERSION = "os.version";
+const OS_ARCHITECTURE = "os.architecture";
+const RUNTIME_NAME = "runtime.name";
+const RUNTIME_VERSION = "runtime.version";
+const DD_HOST_CPU_COUNT = "_dd.host.vcpu_count";
 
-function getRuntimeAndOSMetadata () {
+function getRuntimeAndOSMetadata() {
   return {
-    [RUNTIME_VERSION]: process.version,
+    [RUNTIME_VERSION]: runtimeVersion,
     [OS_ARCHITECTURE]: process.arch,
     [OS_PLATFORM]: process.platform,
-    [RUNTIME_NAME]: 'node',
+    [RUNTIME_NAME]: runtimeName,
     [OS_VERSION]: os.release(),
-    [DD_HOST_CPU_COUNT]: os.cpus().length
+    [DD_HOST_CPU_COUNT]: os.cpus().length,
   }
 }
 
@@ -27,5 +28,5 @@ module.exports = {
   OS_ARCHITECTURE,
   RUNTIME_NAME,
   RUNTIME_VERSION,
-  DD_HOST_CPU_COUNT
+  DD_HOST_CPU_COUNT,
 }
