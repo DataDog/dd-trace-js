87e8c2f15dbea7d504734a302b7749284e327f19
William Conti <william.conti@datadoghq.com>
2026-01-07 12:19:36 -0500

    revert composite plugin changes

--- packages/dd-trace/src/plugins/composite.js
+++ packages/dd-trace/src/plugins/composite.js
@@ -3,23 +3,11 @@
 const Plugin = require('./plugin')
 
 class CompositePlugin extends Plugin {
-  #pluginNames = []
-
   constructor (...args) {
     super(...args)
 
     for (const [name, PluginClass] of Object.entries(this.constructor.plugins)) {
-      // Handle case where value is an array of plugin classes
-      if (Array.isArray(PluginClass)) {
-        for (const SinglePluginClass of PluginClass) {
-          const pluginId = SinglePluginClass.id || name
-          this[pluginId] = new SinglePluginClass(...args)
-          this.#pluginNames.push(pluginId)
-        }
-      } else {
-        this[name] = new PluginClass(...args)
-        this.#pluginNames.push(name)
-      }
+      this[name] = new PluginClass(...args)
     }
   }
 
@@ -28,7 +16,7 @@ class CompositePlugin extends Plugin {
    */
   configure (config) {
     super.configure(config)
-    for (const name of this.#pluginNames) {
+    for (const name in this.constructor.plugins) {
       const pluginConfig = config[name] === false
         ? false
         : { ...config, ...config[name] }
