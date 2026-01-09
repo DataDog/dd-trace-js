ca921f38148467da0dd60bf3f38f29820c595bb5
Bryan English <bryan.english@datadoghq.com>
2026-01-09 14:21:00 -0500

    fixes to make fs tests pass

--- packages/dd-trace/src/native/index.js
+++ packages/dd-trace/src/native/index.js
@@ -7,8 +7,19 @@
  * Falls back gracefully if the native module is unavailable.
  */
 
+const { storage } = require('../../../datadog-core')
+
 let pipeline = null
 let available = false
+
+// Cached module references to avoid repeated require() calls
+// which can cause infinite recursion if fs plugin is active during require
+let NativeSpansInterfaceModule = null
+let NativeSpanContextModule = null
+let NativeDatadogSpanModule = null
+
+// Flag to track if we're currently loading a module to prevent recursion
+let isLoading = false
 
 try {
   const libdatadog = require('@datadog/libdatadog')
@@ -19,6 +30,28 @@ try {
 } catch (e) {
   // Native module not available - this is expected on some platforms
   available = false
+}
+
+/**
+ * Helper to load a module while preventing fs instrumentation recursion.
+ * During module loading, we set noop: true to prevent fs plugin from
+ * triggering, which would try to create spans, which would try to load
+ * this module again.
+ */
+function loadWithNoop (loader) {
+  if (isLoading) {
+    throw new Error('Recursive native module load detected')
+  }
+  isLoading = true
+  const legacy = storage('legacy')
+  const oldStore = legacy.getStore()
+  legacy.enterWith({ noop: true })
+  try {
+    return loader()
+  } finally {
+    legacy.enterWith(oldStore)
+    isLoading = false
+  }
 }
 
 module.exports = {
@@ -51,7 +84,10 @@ module.exports = {
    * @type {typeof import('./native_spans')}
    */
   get NativeSpansInterface () {
-    return require('./native_spans')
+    if (!NativeSpansInterfaceModule) {
+      NativeSpansInterfaceModule = loadWithNoop(() => require('./native_spans'))
+    }
+    return NativeSpansInterfaceModule
   },
 
   /**
@@ -59,7 +95,10 @@ module.exports = {
    * @type {typeof import('./span_context')}
    */
   get NativeSpanContext () {
-    return require('./span_context')
+    if (!NativeSpanContextModule) {
+      NativeSpanContextModule = loadWithNoop(() => require('./span_context'))
+    }
+    return NativeSpanContextModule
   },
 
   /**
@@ -67,6 +106,9 @@ module.exports = {
    * @type {typeof import('./span')}
    */
   get NativeDatadogSpan () {
-    return require('./span')
+    if (!NativeDatadogSpanModule) {
+      NativeDatadogSpanModule = loadWithNoop(() => require('./span'))
+    }
+    return NativeDatadogSpanModule
   }
 }
