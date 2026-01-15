48f216fe089b6593bb7751bbee7a92c853e4f8bc
Ruben Bridgewater <ruben@bridgewater.de>
2026-01-15 13:18:45 +0100

    ci: add test verify script and activate missing tests (#7225)
    This detects test files that are not actually checked in our CI.
    This happened multiple times so far and also currently test files
    exist that are not run with our scripts.
    
    Add verify-exercised-tests workflow and add missing test files.
    
    Skip crashtracking tests on Windows. Crashtracking is not supported on Windows.

--- packages/dd-trace/test/crashtracking/crashtracker.spec.js
+++ packages/dd-trace/test/crashtracking/crashtracker.spec.js
@@ -1,163 +1,164 @@
 'use strict'
 
 const assert = require('node:assert/strict')
+const os = require('node:os')
 
 const proxyquire = require('proxyquire')
 const sinon = require('sinon')
 require('../setup/core')
 
-describe('crashtracking', () => {
-  describe('crashtracker', () => {
-    let crashtracker
-    let binding
-    let config
-    let libdatadog
-    let log
+const describeNotWindows = os.platform() !== 'win32' ? describe : describe.skip
 
-    beforeEach(() => {
-      libdatadog = require('@datadog/libdatadog')
+describeNotWindows('crashtracker', () => {
+  let crashtracker
+  let binding
+  let config
+  let libdatadog
+  let log
 
-      binding = libdatadog.load('crashtracker')
+  beforeEach(() => {
+    libdatadog = require('@datadog/libdatadog')
 
-      config = {
-        port: 7357,
-        tags: {
-          foo: 'bar'
-        }
+    binding = libdatadog.load('crashtracker')
+
+    config = {
+      port: 7357,
+      tags: {
+        foo: 'bar'
       }
+    }
 
-      log = {
-        error: sinon.stub()
-      }
+    log = {
+      error: sinon.stub()
+    }
 
-      sinon.spy(binding, 'init')
-      sinon.spy(binding, 'updateConfig')
-      sinon.spy(binding, 'updateMetadata')
+    sinon.spy(binding, 'init')
+    sinon.spy(binding, 'updateConfig')
+    sinon.spy(binding, 'updateMetadata')
 
-      crashtracker = proxyquire('../../src/crashtracking/crashtracker', {
-        '../log': log
-      })
+    crashtracker = proxyquire('../../src/crashtracking/crashtracker', {
+      '../log': log
+    })
+  })
+
+  afterEach(() => {
+    binding.init.restore()
+    binding.updateConfig.restore()
+    binding.updateMetadata.restore()
+  })
+
+  describe('start', () => {
+    it('should initialize the binding', () => {
+      crashtracker.start(config)
+
+      sinon.assert.called(binding.init)
+      sinon.assert.notCalled(log.error)
     })
 
-    afterEach(() => {
-      binding.init.restore()
-      binding.updateConfig.restore()
-      binding.updateMetadata.restore()
+    it('should initialize the binding only once', () => {
+      crashtracker.start(config)
+      crashtracker.start(config)
+
+      sinon.assert.calledOnce(binding.init)
     })
 
-    describe('start', () => {
-      it('should initialize the binding', () => {
-        crashtracker.start(config)
+    it('should reconfigure when started multiple times', () => {
+      crashtracker.start(config)
+      crashtracker.start(config)
 
-        sinon.assert.called(binding.init)
-        sinon.assert.notCalled(log.error)
-      })
-
-      it('should initialize the binding only once', () => {
-        crashtracker.start(config)
-        crashtracker.start(config)
-
-        sinon.assert.calledOnce(binding.init)
-      })
-
-      it('should reconfigure when started multiple times', () => {
-        crashtracker.start(config)
-        crashtracker.start(config)
-
-        sinon.assert.called(binding.updateConfig)
-        sinon.assert.called(binding.updateMetadata)
-      })
-
-      it('should handle errors', () => {
-        crashtracker.start(null)
-
-        assert.doesNotThrow(() => crashtracker.start(config))
-      })
-
-      it('should handle unix sockets', () => {
-        config.url = new URL('unix:///var/datadog/apm/test.socket')
-
-        crashtracker.start(config)
-
-        sinon.assert.called(binding.init)
-        sinon.assert.notCalled(log.error)
-      })
+      sinon.assert.called(binding.updateConfig)
+      sinon.assert.called(binding.updateMetadata)
     })
 
-    describe('configure', () => {
-      it('should reconfigure the binding when started', () => {
-        crashtracker.start(config)
-        crashtracker.configure(config)
+    it('should handle errors', () => {
+      crashtracker.start(null)
 
-        sinon.assert.called(binding.updateConfig)
-        sinon.assert.called(binding.updateMetadata)
-      })
-
-      it('should reconfigure the binding only when started', () => {
-        crashtracker.configure(config)
-
-        sinon.assert.notCalled(binding.updateConfig)
-        sinon.assert.notCalled(binding.updateMetadata)
-      })
-
-      it('should handle errors', () => {
-        crashtracker.start(config)
-        crashtracker.configure(null)
-
-        assert.doesNotThrow(() => crashtracker.configure(config))
-      })
+      assert.doesNotThrow(() => crashtracker.start(config))
     })
 
-    describe('process tags', () => {
-      it('should include process tags in metadata', () => {
-        crashtracker.start(config)
+    it('should handle unix sockets', () => {
+      config.url = new URL('unix:///var/datadog/apm/test.socket')
 
-        sinon.assert.calledOnce(binding.init)
-        const metadata = binding.init.firstCall.args[2]
+      crashtracker.start(config)
 
-        assert.ok(metadata)
-        assert.ok(Array.isArray(metadata.tags))
+      sinon.assert.called(binding.init)
+      sinon.assert.notCalled(log.error)
+    })
+  })
 
-        // Check that process tags are included
-        const hasEntrypointType = metadata.tags.some(tag => tag.startsWith('entrypoint.type:'))
-        const hasEntrypointName = metadata.tags.some(tag => tag.startsWith('entrypoint.name:'))
-        const hasEntrypointWorkdir = metadata.tags.some(tag => tag.startsWith('entrypoint.workdir:'))
-        const hasEntrypointBasedir = metadata.tags.some(tag => tag.startsWith('entrypoint.basedir:'))
+  describe('configure', () => {
+    it('should reconfigure the binding when started', () => {
+      crashtracker.start(config)
+      crashtracker.configure(config)
 
-        assert.ok(hasEntrypointType, 'should include entrypoint.type tag')
-        assert.ok(hasEntrypointName, 'should include entrypoint.name tag')
-        assert.ok(hasEntrypointWorkdir, 'should include entrypoint.workdir tag')
-        assert.ok(hasEntrypointBasedir, 'should include entrypoint.basedir tag')
-      })
+      sinon.assert.called(binding.updateConfig)
+      sinon.assert.called(binding.updateMetadata)
+    })
 
-      it('should include user tags and process tags together', () => {
-        crashtracker.start(config)
+    it('should reconfigure the binding only when started', () => {
+      crashtracker.configure(config)
 
-        const metadata = binding.init.firstCall.args[2]
+      sinon.assert.notCalled(binding.updateConfig)
+      sinon.assert.notCalled(binding.updateMetadata)
+    })
 
-        // Check that user tags are included
-        const hasFooTag = metadata.tags.some(tag => tag === 'foo:bar')
-        assert.ok(hasFooTag, 'should include user-defined tags')
+    it('should handle errors', () => {
+      crashtracker.start(config)
+      crashtracker.configure(null)
 
-        // Check that process tags are also included
-        const hasProcessTags = metadata.tags.some(tag => tag.startsWith('entrypoint.'))
-        assert.ok(hasProcessTags, 'should include process tags')
-      })
+      assert.doesNotThrow(() => crashtracker.configure(config))
+    })
+  })
 
-      it('should update process tags when reconfiguring', () => {
-        crashtracker.start(config)
-        crashtracker.configure(config)
+  describe('process tags', () => {
+    it('should include process tags in metadata', () => {
+      crashtracker.start(config)
 
-        sinon.assert.called(binding.updateMetadata)
-        const metadata = binding.updateMetadata.firstCall.args[0]
+      sinon.assert.calledOnce(binding.init)
+      const metadata = binding.init.firstCall.args[2]
 
-        assert.ok(metadata)
-        assert.ok(Array.isArray(metadata.tags))
+      assert.ok(metadata)
+      assert.ok(Array.isArray(metadata.tags))
 
-        // Verify process tags are in the updated metadata
-        const hasProcessTags = metadata.tags.some(tag => tag.startsWith('entrypoint.'))
-        assert.ok(hasProcessTags, 'should include process tags in updated metadata')
-      })
+      // Check that process tags are included
+      const hasEntrypointType = metadata.tags.some(tag => tag.startsWith('entrypoint.type:'))
+      const hasEntrypointName = metadata.tags.some(tag => tag.startsWith('entrypoint.name:'))
+      const hasEntrypointWorkdir = metadata.tags.some(tag => tag.startsWith('entrypoint.workdir:'))
+      const hasEntrypointBasedir = metadata.tags.some(tag => tag.startsWith('entrypoint.basedir:'))
+
+      assert.ok(hasEntrypointType, 'should include entrypoint.type tag')
+      assert.ok(hasEntrypointName, 'should include entrypoint.name tag')
+      assert.ok(hasEntrypointWorkdir, 'should include entrypoint.workdir tag')
+      assert.ok(hasEntrypointBasedir, 'should include entrypoint.basedir tag')
+    })
+
+    it('should include user tags and process tags together', () => {
+      crashtracker.start(config)
+
+      const metadata = binding.init.firstCall.args[2]
+
+      // Check that user tags are included
+      const hasFooTag = metadata.tags.some(tag => tag === 'foo:bar')
+      assert.ok(hasFooTag, 'should include user-defined tags')
+
+      // Check that process tags are also included
+      const hasProcessTags = metadata.tags.some(tag => tag.startsWith('entrypoint.'))
+      assert.ok(hasProcessTags, 'should include process tags')
+    })
+
+    it('should update process tags when reconfiguring', () => {
+      crashtracker.start(config)
+      crashtracker.configure(config)
+
+      sinon.assert.called(binding.updateMetadata)
+      const metadata = binding.updateMetadata.firstCall.args[0]
+
+      assert.ok(metadata)
+      assert.ok(Array.isArray(metadata.tags))
+
+      // Verify process tags are in the updated metadata
+      const hasProcessTags = metadata.tags.some(tag => tag.startsWith('entrypoint.'))
+      assert.ok(hasProcessTags, 'should include process tags in updated metadata')
     })
   })
 })
