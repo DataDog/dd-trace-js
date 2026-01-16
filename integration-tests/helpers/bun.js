3d6e6089af55a1e2992c9561aa5d8d73b81928be
Thomas Watson <thomas.watson@datadoghq.com>
2026-01-16 12:43:11 +0100

    chore(tests): add type annotations to integration test helpers (#7240)

--- integration-tests/helpers/bun.js
+++ integration-tests/helpers/bun.js
@@ -6,8 +6,13 @@ const PROJECT_ROOT = resolve(__dirname, '..', '..')
 const BUN_INSTALL = join(PROJECT_ROOT, '.bun')
 const BUN = join(PROJECT_ROOT, 'node_modules', '.bin', 'bun')
 
+/**
+ * @param {NodeJS.ProcessEnv} [env]
+ */
 function withBun (env = process.env) {
-  return { ...env, BUN_INSTALL, _DD_IGNORE_ENGINES: true }
+  return /** @type {NodeJS.ProcessEnv & { BUN_INSTALL: string, _DD_IGNORE_ENGINES: boolean }} */ (
+    { ...env, BUN_INSTALL, _DD_IGNORE_ENGINES: true }
+  )
 }
 
 module.exports = { BUN, withBun }
