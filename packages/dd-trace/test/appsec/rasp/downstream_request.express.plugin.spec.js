d23df50e8aec6c4a1803790aa30ddcbe9dc47393
ishabi <ilyasshabi94@gmail.com>
2026-02-02 13:44:15 +0100

    remove chai

--- packages/dd-trace/test/appsec/rasp/downstream_request.express.plugin.spec.js
+++ packages/dd-trace/test/appsec/rasp/downstream_request.express.plugin.spec.js
@@ -2,7 +2,7 @@
 
 const path = require('path')
 const Axios = require('axios')
-const { assert } = require('chai')
+const sinon = require('sinon')
 const { sandboxCwd, useSandbox, FakeAgent, spawnProc } = require('../../../../../integration-tests/helpers')
 
 describe('RASP - downstream request integration', () => {
@@ -43,21 +43,21 @@ describe('RASP - downstream request integration', () => {
   function assertMessage (agent, withRequestHeaders = true, withResponseBody = true, numberOfRequests = 1) {
     return agent.assertMessageReceived(({ payload }) => {
       const [span] = payload[0]
-      assert.strictEqual(span.metrics['_dd.appsec.downstream_request'], numberOfRequests)
+      sinon.assert.strictEqual(span.metrics['_dd.appsec.downstream_request'], numberOfRequests)
 
-      assert.exists(span.meta['_dd.appsec.trace.req_method'])
-      assert.exists(span.meta['_dd.appsec.trace.res_status'])
-      assert.exists(span.meta['_dd.appsec.trace.res_headers'])
+      sinon.assert.exists(span.meta['_dd.appsec.trace.req_method'])
+      sinon.assert.exists(span.meta['_dd.appsec.trace.res_status'])
+      sinon.assert.exists(span.meta['_dd.appsec.trace.res_headers'])
 
       if (withRequestHeaders) {
-        assert.exists(span.meta['_dd.appsec.trace.req_headers'])
+        sinon.assert.exists(span.meta['_dd.appsec.trace.req_headers'])
       } else {
-        assert.notExists(span.meta['_dd.appsec.trace.req_headers'])
+        sinon.assert.notExists(span.meta['_dd.appsec.trace.req_headers'])
       }
       if (withResponseBody) {
-        assert.exists(span.meta['_dd.appsec.trace.res_body'])
+        sinon.assert.exists(span.meta['_dd.appsec.trace.res_body'])
       } else {
-        assert.notExists(span.meta['_dd.appsec.trace.res_body'])
+        sinon.assert.notExists(span.meta['_dd.appsec.trace.res_body'])
       }
     })
   }
@@ -74,30 +74,30 @@ describe('RASP - downstream request integration', () => {
         const hasTag = (serie, tag) => Array.isArray(serie.tags) && serie.tags.includes(tag)
 
         const evalSeries = series.filter(s => s.metric === 'rasp.rule.eval')
-        assert.exists(evalSeries, 'Rasp rule eval series should exist')
+        sinon.assert.exists(evalSeries, 'Rasp rule eval series should exist')
 
         const evalVariants = new Set()
         for (const s of evalSeries) {
           if (hasTag(s, 'rule_variant:request')) evalVariants.add('request')
           if (hasTag(s, 'rule_variant:response')) evalVariants.add('response')
         }
-        assert.isTrue(evalVariants.has('request'), 'rasp.rule.eval should include request variant')
-        assert.isTrue(evalVariants.has('response'), 'rasp.rule.eval should include response variant')
+        sinon.assert.isTrue(evalVariants.has('request'), 'rasp.rule.eval should include request variant')
+        sinon.assert.isTrue(evalVariants.has('response'), 'rasp.rule.eval should include response variant')
 
         const matchSeries = series.filter(s => s.metric === 'rasp.rule.match')
-        assert.exists(matchSeries, 'Rasp match eval series should exist')
+        sinon.assert.exists(matchSeries, 'Rasp match eval series should exist')
 
         const matchVariants = new Set()
         for (const s of matchSeries) {
           if (hasTag(s, 'rule_variant:request')) matchVariants.add('request')
           if (hasTag(s, 'rule_variant:response')) matchVariants.add('response')
         }
-        assert.isTrue(matchVariants.has('request'), 'rasp.rule.match should include request variant')
-        assert.isTrue(matchVariants.has('response'), 'rasp.rule.match should include response variant')
+        sinon.assert.isTrue(matchVariants.has('request'), 'rasp.rule.match should include request variant')
+        sinon.assert.isTrue(matchVariants.has('response'), 'rasp.rule.match should include response variant')
       }
     }, 'generate-metrics', 30_000, 2).then(
       () => {
-        assert.equal(appsecTelemetryReceived, true)
+        sinon.assert.equal(appsecTelemetryReceived, true)
       })
   }
 
