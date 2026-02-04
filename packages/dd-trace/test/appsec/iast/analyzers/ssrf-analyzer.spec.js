0e06c3cabfd4b158b5a86c42e6a4657d308d8c97
Ugaitz Urien <ugaitz.urien@datadoghq.com>
2026-02-04 15:47:56 +0100

    Undo logs and for to maintain as original

--- packages/dd-trace/test/appsec/iast/analyzers/ssrf-analyzer.spec.js
+++ packages/dd-trace/test/appsec/iast/analyzers/ssrf-analyzer.spec.js
@@ -8,181 +8,179 @@ const { prepareTestServerForIast } = require('../utils')
 const { storage } = require('../../../../../datadog-core')
 const iastContextFunctions = require('../../../../src/appsec/iast/iast-context')
 const { newTaintedString } = require('../../../../src/appsec/iast/taint-tracking/operations')
-for (let i = 0; i < 20; i++) {
-  describe('ssrf analyzer ' + i, () => {
-    prepareTestServerForIast('ssrf',
-      (testThatRequestHasVulnerability, testThatRequestHasNoVulnerability) => {
-        ['http', 'https'].forEach(pluginName => {
-          function executeHttpGet (http, url) {
-            return new Promise((resolve, reject) => {
-              const clientRequest = http.get(url, () => {
-                if (!resolved) {
-                  resolved = true
-                  resolve()
-                }
-              })
-              let resolved = false
-
-              clientRequest.on('error', () => {
-                if (!resolved) {
-                  resolved = true
-                  resolve()
-                }
-              })
-
-              clientRequest.on('close', () => {
-                if (!resolved) {
-                  resolved = true
-                  resolve()
-                }
-              })
-
-              clientRequest.destroy()
+describe('ssrf analyzer', () => {
+  prepareTestServerForIast('ssrf',
+    (testThatRequestHasVulnerability, testThatRequestHasNoVulnerability) => {
+      ['http', 'https'].forEach(pluginName => {
+        function executeHttpGet (http, url) {
+          return new Promise((resolve, reject) => {
+            const clientRequest = http.get(url, () => {
+              if (!resolved) {
+                resolved = true
+                resolve()
+              }
             })
-          }
+            let resolved = false
 
-          function executeHttpRequest (http, url) {
-            return new Promise((resolve) => {
-              const clientRequest = http.request(url, (res) => {
-                if (!resolved) {
-                  resolved = true
-                  resolve()
-                }
-              })
-              let resolved = false
-
-              clientRequest.on('error', () => {
-                if (!resolved) {
-                  resolved = true
-                  resolve()
-                }
-              })
-
-              clientRequest.on('close', () => {
-                if (!resolved) {
-                  resolved = true
-                  resolve()
-                }
-              })
-
-              clientRequest.destroy()
+            clientRequest.on('error', () => {
+              if (!resolved) {
+                resolved = true
+                resolve()
+              }
             })
-          }
 
-          describe(pluginName, () => {
-            [
-              {
-                httpMethodName: 'get',
-                methodToExecute: executeHttpGet,
-              },
-              {
-                httpMethodName: 'request',
-                methodToExecute: executeHttpRequest,
-              },
-            ].forEach(requestMethodData => {
-              describe(requestMethodData.httpMethodName, () => {
-                describe('with url', () => {
-                  const taintTrackingUtilsFilename = 'taint-tracking-utils.js'
-                  let taintTrackingUtilsFilePath
+            clientRequest.on('close', () => {
+              if (!resolved) {
+                resolved = true
+                resolve()
+              }
+            })
 
-                  before(() => {
-                    taintTrackingUtilsFilePath = path.join(os.tmpdir(), taintTrackingUtilsFilename)
+            clientRequest.destroy()
+          })
+        }
 
-                    fs.copyFileSync(
-                      path.join(__dirname, 'resources', taintTrackingUtilsFilename),
-                      taintTrackingUtilsFilePath
-                    )
-                  })
+        function executeHttpRequest (http, url) {
+          return new Promise((resolve) => {
+            const clientRequest = http.request(url, (res) => {
+              if (!resolved) {
+                resolved = true
+                resolve()
+              }
+            })
+            let resolved = false
 
-                  after(() => {
-                    fs.unlinkSync(taintTrackingUtilsFilePath)
-                  })
+            clientRequest.on('error', () => {
+              if (!resolved) {
+                resolved = true
+                resolve()
+              }
+            })
 
-                  testThatRequestHasVulnerability(() => {
-                    const store = storage('legacy').getStore()
-                    const iastContext = iastContextFunctions.getIastContext(store)
+            clientRequest.on('close', () => {
+              if (!resolved) {
+                resolved = true
+                resolve()
+              }
+            })
 
-                    const url = newTaintedString(iastContext, pluginName + '://www.google.com', 'param', 'Request')
-                    const https = require(pluginName)
+            clientRequest.destroy()
+          })
+        }
 
-                    return requestMethodData.methodToExecute(https, url)
-                  }, 'SSRF')
+        describe(pluginName, () => {
+          [
+            {
+              httpMethodName: 'get',
+              methodToExecute: executeHttpGet,
+            },
+            {
+              httpMethodName: 'request',
+              methodToExecute: executeHttpRequest,
+            },
+          ].forEach(requestMethodData => {
+            describe(requestMethodData.httpMethodName, () => {
+              describe('with url', () => {
+                const taintTrackingUtilsFilename = 'taint-tracking-utils.js'
+                let taintTrackingUtilsFilePath
 
-                  testThatRequestHasNoVulnerability(() => {
-                    const url = pluginName + '://www.google.com'
-                    const https = require(pluginName)
-                    return requestMethodData.methodToExecute(https, url)
-                  }, 'SSRF')
+                before(() => {
+                  taintTrackingUtilsFilePath = path.join(os.tmpdir(), taintTrackingUtilsFilename)
 
-                  testThatRequestHasNoVulnerability((req, res) => {
-                    const utils = require(taintTrackingUtilsFilePath)
-
-                    const hash = req.headers.hash
-                    let url = pluginName + '://www.google.com/#'
-                    url = utils.add(url, hash)
-                    const https = require(pluginName)
-                    requestMethodData.methodToExecute(https, url)
-                    res.end()
-                  }, 'SSRF', (done, config) => {
-                    axios.get(`http://localhost:${config.port}`, {
-                      headers: {
-                        hash: 'taintedHash',
-                      },
-                    })
-                  }, 'should not have SSRF vulnerability when the tainted value is after #')
+                  fs.copyFileSync(
+                    path.join(__dirname, 'resources', taintTrackingUtilsFilename),
+                    taintTrackingUtilsFilePath
+                  )
                 })
 
-                describe('with options', () => {
-                  testThatRequestHasVulnerability(() => {
-                    const store = storage('legacy').getStore()
-                    const iastContext = iastContextFunctions.getIastContext(store)
-
-                    const host = newTaintedString(iastContext, 'www.google.com', 'param', 'Request')
-                    const options = {
-                      host,
-                      protocol: `${pluginName}:`,
-                    }
-
-                    return requestMethodData.methodToExecute(require(pluginName), options)
-                  }, 'SSRF')
-
-                  testThatRequestHasNoVulnerability(() => {
-                    const host = 'www.google.com'
-                    const options = {
-                      host,
-                      protocol: `${pluginName}:`,
-                    }
-
-                    return requestMethodData.methodToExecute(require(pluginName), options)
-                  }, 'SSRF')
+                after(() => {
+                  fs.unlinkSync(taintTrackingUtilsFilePath)
                 })
+
+                testThatRequestHasVulnerability(() => {
+                  const store = storage('legacy').getStore()
+                  const iastContext = iastContextFunctions.getIastContext(store)
+
+                  const url = newTaintedString(iastContext, pluginName + '://www.google.com', 'param', 'Request')
+                  const https = require(pluginName)
+
+                  return requestMethodData.methodToExecute(https, url)
+                }, 'SSRF')
+
+                testThatRequestHasNoVulnerability(() => {
+                  const url = pluginName + '://www.google.com'
+                  const https = require(pluginName)
+                  return requestMethodData.methodToExecute(https, url)
+                }, 'SSRF')
+
+                testThatRequestHasNoVulnerability((req, res) => {
+                  const utils = require(taintTrackingUtilsFilePath)
+
+                  const hash = req.headers.hash
+                  let url = pluginName + '://www.google.com/#'
+                  url = utils.add(url, hash)
+                  const https = require(pluginName)
+                  requestMethodData.methodToExecute(https, url)
+                  res.end()
+                }, 'SSRF', (done, config) => {
+                  axios.get(`http://localhost:${config.port}`, {
+                    headers: {
+                      hash: 'taintedHash',
+                    },
+                  })
+                }, 'should not have SSRF vulnerability when the tainted value is after #')
+              })
+
+              describe('with options', () => {
+                testThatRequestHasVulnerability(() => {
+                  const store = storage('legacy').getStore()
+                  const iastContext = iastContextFunctions.getIastContext(store)
+
+                  const host = newTaintedString(iastContext, 'www.google.com', 'param', 'Request')
+                  const options = {
+                    host,
+                    protocol: `${pluginName}:`,
+                  }
+
+                  return requestMethodData.methodToExecute(require(pluginName), options)
+                }, 'SSRF')
+
+                testThatRequestHasNoVulnerability(() => {
+                  const host = 'www.google.com'
+                  const options = {
+                    host,
+                    protocol: `${pluginName}:`,
+                  }
+
+                  return requestMethodData.methodToExecute(require(pluginName), options)
+                }, 'SSRF')
               })
             })
           })
         })
-
-        describe('http2', () => {
-          testThatRequestHasVulnerability(() => {
-            const store = storage('legacy').getStore()
-            const iastContext = iastContextFunctions.getIastContext(store)
-
-            const url = newTaintedString(iastContext, 'http://www.datadoghq.com', 'param', 'Request')
-            const http2 = require('http2')
-
-            const session = http2.connect(url)
-
-            session.destroy()
-          }, 'SSRF')
-
-          testThatRequestHasNoVulnerability(() => {
-            const url = 'http://www.datadoghq.com'
-            const http2 = require('http2')
-
-            const session = http2.connect(url)
-
-            session.destroy()
-          }, 'SSRF')
-        })
       })
-  })
-}
+
+      describe('http2', () => {
+        testThatRequestHasVulnerability(() => {
+          const store = storage('legacy').getStore()
+          const iastContext = iastContextFunctions.getIastContext(store)
+
+          const url = newTaintedString(iastContext, 'http://www.datadoghq.com', 'param', 'Request')
+          const http2 = require('http2')
+
+          const session = http2.connect(url)
+
+          session.destroy()
+        }, 'SSRF')
+
+        testThatRequestHasNoVulnerability(() => {
+          const url = 'http://www.datadoghq.com'
+          const http2 = require('http2')
+
+          const session = http2.connect(url)
+
+          session.destroy()
+        }, 'SSRF')
+      })
+    })
+})
