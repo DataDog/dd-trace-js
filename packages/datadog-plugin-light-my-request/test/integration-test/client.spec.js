7ffa6242c0bb1808dde290fc4992f2b37f0d0d08
pabloerhard <pabloerhard02@gmail.com>
2026-02-04 13:41:00 -0600

    Added new ESM test and missing definitions into apm-integrations.yml file

---
+++ packages/datadog-plugin-light-my-request/test/integration-test/client.spec.js
@@ -0,0 +1,48 @@
+'use strict'
+
+const assert = require('node:assert/strict')
+
+const {
+  FakeAgent,
+  sandboxCwd,
+  useSandbox,
+  checkSpansForServiceName,
+  spawnPluginIntegrationTestProcAndExpectExit,
+  varySandbox
+} = require('../../../../integration-tests/helpers')
+const { withVersions } = require('../../../dd-trace/test/setup/mocha')
+describe('esm', () => {
+  let agent
+  let proc
+  let variants
+
+  withVersions('light-my-request', 'light-my-request', version => {
+    useSandbox([`'light-my-request@${version}'`], false, [
+      './packages/datadog-plugin-light-my-request/test/integration-test/*'])
+
+    beforeEach(async () => {
+      agent = await new FakeAgent().start()
+    })
+
+    before(async function () {
+      variants = varySandbox('server.mjs', 'inject', undefined, 'light-my-request')
+    })
+
+    afterEach(async () => {
+      proc && proc.kill()
+      await agent.stop()
+    })
+
+    for (const variant of varySandbox.VARIANTS) {
+      it(`is instrumented ${variant}`, async () => {
+        agent.assertMessageReceived(({ headers, payload }) => {
+          assert.strictEqual(headers.host, `127.0.0.1:${agent.port}`)
+          assert.ok(Array.isArray(payload))
+          assert.strictEqual(checkSpansForServiceName(payload, 'mariadb.query'), true)
+        })
+
+        proc = await spawnPluginIntegrationTestProcAndExpectExit(sandboxCwd(), variants[variant], agent.port)
+      }).timeout(20000)
+    }
+  })
+})
