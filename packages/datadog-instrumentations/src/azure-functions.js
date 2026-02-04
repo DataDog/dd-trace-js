d87a3d34c156c77b46d878ad6c4bc753dfdfad90
Olivier John Ndjike Nzia <olivier.ndjikenzia@datadoghq.com>
2026-02-04 08:52:47 -0500

    debug logs

--- packages/datadog-instrumentations/src/azure-functions.js
+++ packages/datadog-instrumentations/src/azure-functions.js
@@ -10,6 +10,7 @@ const {
 const azureFunctionsChannel = dc.tracingChannel('datadog:azure:functions:invoke')
 
 addHook({ name: '@azure/functions', versions: ['>=4'], patchDefault: false }, (azureFunction) => {
+  log.debug('adding az functions hook OLIVIER')
   const { app } = azureFunction
 
   // Http triggers
@@ -34,7 +35,7 @@ addHook({ name: '@azure/functions', versions: ['>=4'], patchDefault: false }, (a
 // The arguments are either an object with a handler property or the handler function itself
 function wrapHandler (method) {
   return function (name, arg) {
-    log.debug('in sub wrap handler for method: ', method)
+    log.debug('in sub wrap handler for method: ', name)
     // check if this is either a handlerOptions or the handler itself
     if (arg !== null && typeof arg === 'object' && arg.hasOwnProperty('handler')) {
       // if this is a handlerOptions: first, assign to a variable options
