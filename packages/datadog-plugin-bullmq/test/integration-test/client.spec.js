b128748e6a14f3ceaf5fe67bbf39e108ca4fadfc
pabloerhard <pabloerhard02@gmail.com>
2026-02-05 11:59:42 -0600

    Added varySandbox for bullmq ESM tests

--- packages/datadog-plugin-bullmq/test/integration-test/client.spec.js
+++ packages/datadog-plugin-bullmq/test/integration-test/client.spec.js
@@ -8,12 +8,14 @@ const {
   useSandbox,
   checkSpansForServiceName,
   spawnPluginIntegrationTestProcAndExpectExit,
+  varySandbox
 } = require('../../../../integration-tests/helpers')
 const { withVersions } = require('../../../dd-trace/test/setup/mocha')
 
 describe('esm', () => {
   let agent
   let proc
+  let variants
 
   withVersions('bullmq', 'bullmq', '>=5.66.0', version => {
     useSandbox([`'bullmq@${version}'`], false, [
@@ -23,75 +25,100 @@ describe('esm', () => {
       agent = await new FakeAgent().start()
     })
 
+
     afterEach(async () => {
       proc && proc.kill()
       await agent.stop()
     })
 
     describe('Queue.add()', () => {
-      it('is instrumented', async () => {
-        const res = agent.assertMessageReceived(({ headers, payload }) => {
-          assert.strictEqual(headers.host, `127.0.0.1:${agent.port}`)
-          assert.ok(Array.isArray(payload))
-          assert.strictEqual(checkSpansForServiceName(payload, 'bullmq.add'), true)
-        })
+      beforeEach(async () => {
+        variants = varySandbox('server-queue-add.mjs', 'bullmq', 'Queue')
+      })
 
-        proc = await spawnPluginIntegrationTestProcAndExpectExit(sandboxCwd(), 'server-queue-add.mjs', agent.port)
+      for (const variant of varySandbox.VARIANTS) {
+        it(`is instrumented ${variant}`, async () => {
+          const res = agent.assertMessageReceived(({ headers, payload }) => {
+            assert.strictEqual(headers.host, `127.0.0.1:${agent.port}`)
+            assert.ok(Array.isArray(payload))
+            assert.strictEqual(checkSpansForServiceName(payload, 'bullmq.add'), true)
+          })
 
-        await res
-      }).timeout(60000)
+          proc = await spawnPluginIntegrationTestProcAndExpectExit(sandboxCwd(), variants[variant], agent.port)
+
+          await res
+        }).timeout(60000)
+      }
     })
 
     describe('Queue.addBulk()', () => {
-      it('is instrumented', async () => {
-        const res = agent.assertMessageReceived(({ headers, payload }) => {
-          assert.strictEqual(headers.host, `127.0.0.1:${agent.port}`)
-          assert.ok(Array.isArray(payload))
-          assert.strictEqual(checkSpansForServiceName(payload, 'bullmq.addBulk'), true)
-        })
+      beforeEach(async () => {
+        variants = varySandbox('server-queue-add-bulk.mjs', 'bullmq', 'Queue')
+      })
 
-        proc = await spawnPluginIntegrationTestProcAndExpectExit(sandboxCwd(), 'server-queue-add-bulk.mjs', agent.port)
+      for (const variant of varySandbox.VARIANTS) {
+        it(`is instrumented ${variant}`, async () => {
+          const res = agent.assertMessageReceived(({ headers, payload }) => {
+            assert.strictEqual(headers.host, `127.0.0.1:${agent.port}`)
+            assert.ok(Array.isArray(payload))
+            assert.strictEqual(checkSpansForServiceName(payload, 'bullmq.addBulk'), true)
+          })
 
-        await res
-      }).timeout(60000)
+          proc = await spawnPluginIntegrationTestProcAndExpectExit(sandboxCwd(), variants[variant], agent.port)
+
+          await res
+        }).timeout(60000)
+      }
     })
 
     describe('FlowProducer.add()', () => {
-      it('is instrumented', async () => {
-        const res = agent.assertMessageReceived(({ headers, payload }) => {
-          assert.strictEqual(headers.host, `127.0.0.1:${agent.port}`)
-          assert.ok(Array.isArray(payload))
-          assert.strictEqual(checkSpansForServiceName(payload, 'bullmq.add'), true)
-        })
+      beforeEach(async () => {
+        variants = varySandbox('server-flow-producer-add.mjs', 'bullmq', 'FlowProducer')
+      })
 
-        proc = await spawnPluginIntegrationTestProcAndExpectExit(
-          sandboxCwd(),
-          'server-flow-producer-add.mjs',
-          agent.port
-        )
+      for (const variant of varySandbox.VARIANTS) {
+        it(`is instrumented ${variant}`, async () => {
+          const res = agent.assertMessageReceived(({ headers, payload }) => {
+            assert.strictEqual(headers.host, `127.0.0.1:${agent.port}`)
+            assert.ok(Array.isArray(payload))
+            assert.strictEqual(checkSpansForServiceName(payload, 'bullmq.add'), true)
+          })
 
-        await res
-      }).timeout(60000)
+          proc = await spawnPluginIntegrationTestProcAndExpectExit(
+            sandboxCwd(),
+            variants[variant],
+            agent.port
+          )
+
+          await res
+        }).timeout(60000)
+      }
     })
 
     describe('Worker.callProcessJob()', () => {
-      it('is instrumented', async () => {
-        const res = agent.assertMessageReceived(({ headers, payload }) => {
-          assert.strictEqual(headers.host, `127.0.0.1:${agent.port}`)
-          assert.ok(Array.isArray(payload))
-          assert.strictEqual(checkSpansForServiceName(payload, 'bullmq.processJob'), true)
-        })
+      beforeEach(async () => {
+        variants = varySandbox('server-worker-process-job.mjs', 'bullmq', 'Queue, Worker, QueueEvents')
+      })
 
-        proc = await spawnPluginIntegrationTestProcAndExpectExit(
-          sandboxCwd(),
-          'server-worker-process-job.mjs',
-          agent.port,
-          // Disable Redis/ioredis instrumentation to avoid hitting max active requests limit
-          { DD_TRACE_REDIS_ENABLED: 'false', DD_TRACE_IOREDIS_ENABLED: 'false' }
-        )
+      for (const variant of varySandbox.VARIANTS) {
+        it(`is instrumented ${variant}`, async () => {
+          const res = agent.assertMessageReceived(({ headers, payload }) => {
+            assert.strictEqual(headers.host, `127.0.0.1:${agent.port}`)
+            assert.ok(Array.isArray(payload))
+            assert.strictEqual(checkSpansForServiceName(payload, 'bullmq.processJob'), true)
+          })
 
-        await res
-      }).timeout(60000)
+          proc = await spawnPluginIntegrationTestProcAndExpectExit(
+            sandboxCwd(),
+            variants[variant],
+            agent.port,
+            // Disable Redis/ioredis instrumentation to avoid hitting max active requests limit
+            { DD_TRACE_REDIS_ENABLED: 'false', DD_TRACE_IOREDIS_ENABLED: 'false' }
+          )
+
+          await res
+        }).timeout(60000)
+      }
     })
   })
 })
