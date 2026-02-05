6c3e7ad3fc98c12122470876c541cb2544e517ff
Thomas Watson <thomas.watson@datadoghq.com>
2026-02-05 09:00:18 +0100

    refactor(esbuild): extract logging into module
    Extract console logging into a dedicated log module that:
    - Uses printf-style formatting for consistency
    - Conditionally logs based on DD_TRACE_DEBUG env var
    - Eliminates scattered DEBUG conditionals throughout code
    - Adds consistent prefixes for better log identification
    
    Also cleans up the `require` section of the code to modernizes imports
    with `node:` prefix.

---
+++ packages/datadog-esbuild/src/log.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const { format } = require('util')
+
+// eslint-disable-next-line eslint-rules/eslint-process-env
+const DEBUG = process.env.DD_TRACE_DEBUG === 'true' || process.env.DD_TRACE_DEBUG === '1'
+
+const noop = () => {}
+
+const formatWithLogPrefix = (prefix, str, ...args) => {
+  if (typeof str === 'string') {
+    return format(`${prefix} ${str}`, ...args)
+  }
+  return format(prefix, str, ...args)
+}
+
+module.exports = DEBUG
+  ? {
+      debug (...args) {
+        // eslint-disable-next-line no-console
+        console.log(formatWithLogPrefix('[dd-trace/esbuild]', ...args))
+      },
+      warn (...args) {
+        // eslint-disable-next-line no-console
+        console.warn(formatWithLogPrefix('[dd-trace/esbuild] Warning:', ...args))
+      },
+    }
+  : {
+      debug: noop,
+      warn: noop,
+    }
