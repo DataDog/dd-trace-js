9a389b27d868e8cf176ebfeb1bece61529019cbc
Rey Abolofia <rey.abolofia@datadoghq.com>
2026-02-06 10:22:04 -0800

    Undefined cleanup.

--- packages/dd-trace/src/lambda/handler.js
+++ packages/dd-trace/src/lambda/handler.js
@@ -63,20 +63,17 @@ function crashFlush () {
 /**
  * Extracts the context from the given Lambda handler arguments.
  *
- * Some Lambda handler types (e.g., Lambda Authorizers) do not receive
- * a context object. In these cases, this function returns undefined
- * instead of throwing an error.
+ * It is possible for users to define a lambda function without specifying a
+ * context arg. In these cases, this function returns null instead of throwing
+ * an error.
  *
  * @param {unknown[]} args any amount of arguments
- * @returns the context if extraction was successful, undefined otherwise.
+ * @returns the context if extraction was successful, null otherwise.
  */
 function extractContext (args) {
-  let context = args.length > 1 ? args[1] : undefined
-  if (context === undefined || context.getRemainingTimeInMillis === undefined) {
-    context = args.length > 2 ? args[2] : undefined
-    if (context === undefined || context.getRemainingTimeInMillis === undefined) {
-      return undefined
-    }
+  let context = args.length > 1 ? args[1] : null
+  if (context === null || context.getRemainingTimeInMillis === undefined) {
+    context = args.length > 2 ? args[2] : null
   }
   return context
 }
@@ -90,9 +87,7 @@ exports.datadog = function datadog (lambdaHandler) {
   return (...args) => {
     const context = extractContext(args)
 
-    // Only set up timeout checking if context is available.
-    // Some Lambda types (e.g., Authorizers) don't receive a context object.
-    if (context !== undefined) {
+    if (context) {
       checkTimeout(context)
     }
 
