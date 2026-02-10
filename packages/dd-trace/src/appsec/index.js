922d163f2c8610ddcb82b3f474196372817b21b7
Carles Capell <carles.capell@datadoghq.com>
2026-02-10 07:07:09 +0100

    Add tags conditionally to inferredProxyServiceEnabled

--- packages/dd-trace/src/appsec/index.js
+++ packages/dd-trace/src/appsec/index.js
@@ -174,11 +174,13 @@ function incomingHttpStartTranslator ({ req, res, abortController }) {
     [HTTP_CLIENT_IP]: clientIp,
   })
 
-  const context = web.getContext(req)
-  if (context?.inferredProxySpan) {
-    context.inferredProxySpan.addTags({
-      '_dd.appsec.enabled': 1,
-    })
+  if (config.inferredProxyServicesEnabled) {
+    const context = web.getContext(req)
+    if (context?.inferredProxySpan) {
+      context.inferredProxySpan.addTags({
+        '_dd.appsec.enabled': 1,
+      })
+    }
   }
 
   const requestHeaders = { ...req.headers }
