c33a29b832da2849806d60bdb35ea1b1b8039f6a
Ugaitz Urien <ugaitz.urien@datadoghq.com>
2026-02-11 16:22:47 +0100

    Move and rename test file

--- packages/dd-trace/test/appsec/downstream_requests.integration.spec.js
+++ packages/dd-trace/test/appsec/downstream_requests.integration.spec.js
@@ -3,7 +3,7 @@
 const assert = require('node:assert/strict')
 const path = require('path')
 const Axios = require('axios')
-const { sandboxCwd, useSandbox, FakeAgent, spawnProc } = require('../../../../../integration-tests/helpers')
+const { sandboxCwd, useSandbox, FakeAgent, spawnProc } = require('../../../../integration-tests/helpers')
 
 describe('RASP - downstream request integration', () => {
   let cwd, appFile
