d0e5d6ee056752c3aa65c2fe3d6e10e8be4ee3b3
Juan Antonio Fernández de Alba <juan.fernandezdealba@datadoghq.com>
2026-02-12 12:54:10 +0100

    [test optimization] Fix telemetry issues and allow telemetry from jest workers (#7490)

--- packages/datadog-plugin-jest/src/index.js
+++ packages/datadog-plugin-jest/src/index.js
@@ -3,6 +3,7 @@
 const CiPlugin = require('../../dd-trace/src/plugins/ci_plugin')
 const { storage } = require('../../datadog-core')
 const { getEnvironmentVariable, getValueFromEnvSources } = require('../../dd-trace/src/config/helper')
+const { appClosing: appClosingTelemetry } = require('../../dd-trace/src/telemetry')
 
 const {
   TEST_STATUS,
@@ -155,7 +156,9 @@ class JestPlugin extends CiPlugin {
       this.testModuleSpan.finish()
       this.telemetry.ciVisEvent(TELEMETRY_EVENT_FINISHED, 'module')
       this.testSessionSpan.finish()
-      this.telemetry.ciVisEvent(TELEMETRY_EVENT_FINISHED, 'session')
+      this.telemetry.ciVisEvent(TELEMETRY_EVENT_FINISHED, 'session', {
+        hasFailedTestReplay: this.libraryConfig?.isDiEnabled || undefined,
+      })
       finishAllTraceSpans(this.testSessionSpan)
 
       this.telemetry.count(TELEMETRY_TEST_SESSION, {
@@ -163,6 +166,7 @@ class JestPlugin extends CiPlugin {
         autoInjected: !!getValueFromEnvSources('DD_CIVISIBILITY_AUTO_INSTRUMENTATION_PROVIDER'),
       })
 
+      appClosingTelemetry()
       this.tracer._exporter.flush(() => {
         if (onDone) {
           onDone()
@@ -272,6 +276,23 @@ class JestPlugin extends CiPlugin {
       }))
       for (const formattedCoverage of formattedCoverages) {
         this.tracer._exporter.exportCoverage(formattedCoverage)
+      }
+    })
+
+    this.addSub('ci:jest:worker-report:telemetry', data => {
+      const telemetryEvents = JSON.parse(data)
+      for (const event of telemetryEvents) {
+        if (event.type === 'ciVisEvent') {
+          this.telemetry.ciVisEvent(event.name, event.testLevel, {
+            ...event.tags,
+            testFramework: event.testFramework,
+            isUnsupportedCIProvider: event.isUnsupportedCIProvider,
+          })
+        } else if (event.type === 'count') {
+          this.telemetry.count(event.name, event.tags, event.value)
+        } else if (event.type === 'distribution') {
+          this.telemetry.distribution(event.name, event.tags, event.measure)
+        }
       }
     })
 
@@ -424,6 +445,12 @@ class JestPlugin extends CiPlugin {
       if (isDisabled) {
         span.setTag(TEST_MANAGEMENT_IS_DISABLED, 'true')
       }
+
+      this.telemetry.ciVisEvent(
+        TELEMETRY_EVENT_FINISHED,
+        'test',
+        this.getTestTelemetryTags(span)
+      )
 
       span.finish()
     })
