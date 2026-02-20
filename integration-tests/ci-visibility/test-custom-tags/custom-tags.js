c67aa286aa38250afa2dad4e39efcb9acb3f1df6
Juan Antonio Fernández de Alba <juan.fernandezdealba@datadoghq.com>
2026-02-20 12:41:20 +0100

    jest: wrap outer hooks too (#7587)

--- integration-tests/ci-visibility/test-custom-tags/custom-tags.js
+++ integration-tests/ci-visibility/test-custom-tags/custom-tags.js
@@ -4,20 +4,32 @@ const tracer = require('dd-trace')
 const assert = require('assert')
 
 const sum = require('../test/sum')
-describe('test optimization custom tags', () => {
+describe('test optimization', () => {
   beforeEach(() => {
     const testSpan = tracer.scope().active()
-    testSpan.setTag('custom_tag.beforeEach', 'true')
+    testSpan.setTag('outer_scope.beforeEach', 'true')
   })
 
-  it('can report tests', () => {
-    const testSpan = tracer.scope().active()
-    testSpan.setTag('custom_tag.it', 'true')
-    assert.strictEqual(sum(1, 2), 3)
+  describe('custom tags', () => {
+    beforeEach(() => {
+      const testSpan = tracer.scope().active()
+      testSpan.setTag('custom_tag.beforeEach', 'true')
+    })
+
+    it('can report tests', () => {
+      const testSpan = tracer.scope().active()
+      testSpan.setTag('custom_tag.it', 'true')
+      assert.strictEqual(sum(1, 2), 3)
+    })
+
+    afterEach(() => {
+      const testSpan = tracer.scope().active()
+      testSpan.setTag('custom_tag.afterEach', 'true')
+    })
   })
 
   afterEach(() => {
     const testSpan = tracer.scope().active()
-    testSpan.setTag('custom_tag.afterEach', 'true')
+    testSpan.setTag('outer_scope.afterEach', 'true')
   })
 })
