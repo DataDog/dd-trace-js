0a9fa65e6f3da5cf333d93ebf9f12e1dd3c94bc1
Roch Devost <roch.devost@datadoghq.com>
2026-02-25 13:24:18 +0100

    make it a script

---
+++ scripts/codeowners.mjs
@@ -0,0 +1,21 @@
+import { resolve } from 'path'
+import { FILE_DISCOVERY_STRATEGY, getFilePaths } from '@snyk/github-codeowners/dist/lib/file/index.js'
+import { getOwnership } from '@snyk/github-codeowners/dist/lib/ownership/index.js'
+
+// eslint-disable-next-line n/no-unsupported-features/node-builtins
+const dir = resolve(import.meta.dirname, '..')
+const strategy = FILE_DISCOVERY_STRATEGY.FILE_SYSTEM
+const filePaths = await getFilePaths(dir, strategy)
+const files = await getOwnership('CODEOWNERS', filePaths)
+
+const unloved = files.filter(f => f.owners.length === 0)
+const unlovedSpecs = unloved.filter(f => (
+  // f.path.startsWith('integration-tests') && f.path.endsWith('.js') ||
+  f.path.endsWith('.spec.js')
+))
+
+if (unlovedSpecs.length > 0) {
+  const list = unlovedSpecs.map(u => u.path).join('\n')
+
+  throw new Error(`The following modules are missing a mandatory code owner: \n\n${list}`)
+}
