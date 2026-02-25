cdd1e808d8bdc2725c41749f2910be277e827330
Roch Devost <roch.devost@datadoghq.com>
2026-02-25 18:10:00 +0100

    fix esbuild

--- packages/datadog-instrumentations/src/helpers/rewriter/compiler.js
+++ packages/datadog-instrumentations/src/helpers/rewriter/compiler.js
@@ -1,9 +1,13 @@
 'use strict'
 
+// eslint-disable-next-line camelcase, no-undef
+const runtimeRequire = typeof __webpack_require__ === 'function' ? __non_webpack_require__ : require
+
 const compiler = module.exports = {
   parse: (sourceText, options) => {
     try {
-      const oxc = require('oxc-parser')
+      // TODO: Figure out ESBuild `createRequire` issue and remove this hack.
+      const oxc = runtimeRequire(['oxc', 'parser'].join('-'))
 
       compiler.parse = (sourceText, options) => {
         const { program, errors } = oxc.parseSync('index.js', sourceText, {
