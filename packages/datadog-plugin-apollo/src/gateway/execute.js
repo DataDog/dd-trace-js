f2f56c7ea20713763d20f39214e1dd267d23145d
Crystal Luc-Magloire <crystal.magloire@datadoghq.com>
2026-03-11 14:33:46 -0400

    feat(apollo): add hooks to Apollo Gateway (#7704)
    * Introduce hooks for Apollo Gateway.

--- packages/datadog-plugin-apollo/src/gateway/execute.js
+++ packages/datadog-plugin-apollo/src/gateway/execute.js
@@ -5,6 +5,14 @@ const ApolloBasePlugin = require('../../../dd-trace/src/plugins/apollo')
 class ApolloGatewayExecutePlugin extends ApolloBasePlugin {
   static operation = 'execute'
   static prefix = 'tracing:apm:apollo:gateway:execute'
+
+  onAsyncStart (ctx) {
+    const span = ctx?.currentStore?.span
+
+    if (!span) return
+
+    this.config.hooks.execute(span, ctx)
+  }
 }
 
 module.exports = ApolloGatewayExecutePlugin
--- packages/datadog-plugin-apollo/src/gateway/postprocessing.js
+++ packages/datadog-plugin-apollo/src/gateway/postprocessing.js
@@ -5,6 +5,11 @@ const ApolloBasePlugin = require('../../../dd-trace/src/plugins/apollo')
 class ApolloGatewayPostProcessingPlugin extends ApolloBasePlugin {
   static operation = 'postprocessing'
   static prefix = 'tracing:apm:apollo:gateway:postprocessing'
+
+  onAsyncStart (ctx) {
+    const span = ctx?.currentStore?.span
+    this.config.hooks.postprocessing(span, ctx)
+  }
 }
 
 module.exports = ApolloGatewayPostProcessingPlugin
