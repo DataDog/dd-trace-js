1e0ab19b4ba3ea1ee3ff53294aef31144c14d3ae
Sam Brenner <sam.brenner@datadoghq.com>
2026-03-30 13:48:41 -0400

    use startup logs for v6 and startup logs enabled, else console.error

--- packages/dd-trace/src/llmobs/constants/text.js
+++ packages/dd-trace/src/llmobs/constants/text.js
@@ -3,4 +3,7 @@
 module.exports = {
   DROPPED_VALUE_TEXT: "[This value has been dropped because this span's size exceeds the 1MB size limit.]",
   UNSERIALIZABLE_VALUE_TEXT: 'Unserializable value',
+  INCOMPATIBLE_INITIALIZATION:
+    'Cannot send LLM Observability data without a running agent or without both a Datadog API key and site. ' +
+    'Ensure these configurations are set before running your application.',
 }
