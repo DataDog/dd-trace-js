d618c6b2b1023501541b04e51857c2f32b4a2cbb
rochdev <roch.devost@gmail.com>
2026-03-30 12:53:36 -0400

    add electron exporter

---
+++ packages/dd-trace/src/exporters/electron/index.js
@@ -0,0 +1,50 @@
+'use strict'
+
+const { channel } = require('dc-polyfill')
+const { truncateSpan, normalizeSpan } = require('../../encode/tags-processors')
+
+const traceChannel = channel('datadog:apm:spans')
+
+class ElectronExporter {
+  #timer
+  #traces = []
+
+  constructor (config) {
+    this._config = config
+
+    process.once('beforeExit', () => {
+      this.flush()
+    })
+  }
+
+  export (spans) {
+    this.#traces.push(spans)
+
+    const { flushInterval } = this._config
+
+    if (flushInterval === 0) {
+      this.flush()
+    } else if (this.#timer === undefined) {
+      this.#timer = setTimeout(() => {
+        this.flush()
+        this.#timer = undefined
+      }, flushInterval).unref?.()
+    }
+  }
+
+  flush (done = () => {}) {
+    clearTimeout(this.#timer)
+    this.#timer = undefined
+
+    const traces = this.#traces.splice(0)
+
+    if (traces.length > 0 && traceChannel.hasSubscribers) {
+      const formattedTraces = traces.map(spans => spans.map(span => normalizeSpan(truncateSpan(span))))
+      traceChannel.publish(formattedTraces)
+    }
+
+    done()
+  }
+}
+
+module.exports = ElectronExporter
