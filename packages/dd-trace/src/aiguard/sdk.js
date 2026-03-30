aaeb946e41941a3c6239caae44692c7fc33f1565
pabloerhard <pabloerhard02@gmail.com>
2026-03-29 21:25:18 -0400

    fixed aiguard

--- packages/dd-trace/src/aiguard/sdk.js
+++ packages/dd-trace/src/aiguard/sdk.js
@@ -145,6 +145,7 @@ class AIGuard extends NoopAIGuard {
     }
     const { block = true } = opts ?? {}
     return this.#tracer.trace(AI_GUARD_RESOURCE, {}, async (span) => {
+      const internalSpan = span?._span || span
       const last = messages[messages.length - 1]
       const target = this.#isToolCall(last) ? 'tool' : 'prompt'
       span.setTag(AI_GUARD_TARGET_TAG_KEY, target)
@@ -157,10 +158,10 @@ class AIGuard extends NoopAIGuard {
       const metaStruct = {
         messages: this.#buildMessagesForMetaStruct(messages),
       }
-      span.meta_struct = {
+      internalSpan.meta_struct = {
         [AI_GUARD_META_STRUCT_KEY]: metaStruct,
       }
-      const rootSpan = span.context()?._trace?.started?.[0]
+      const rootSpan = internalSpan.context()?._trace?.started?.[0]
       if (rootSpan) {
         // keepTrace must be called before executeRequest so the sampling decision
         // is propagated correctly to outgoing HTTP client calls.
