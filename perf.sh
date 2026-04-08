ad320bba5a4a9a68d790429428eeeb9a2d137d59
Yann Hamdaoui <yann.hamdaoui@datadoghq.com>
2026-04-08 15:50:58 +0200

    fix: perf.sh env variable export

--- perf.sh
+++ perf.sh
@@ -6,5 +6,6 @@ sudo sysctl kernel.kptr_restrict=0
 # DD_TRACE_EXPERIMENTAL_NATIVE_SPANS_ENABLED=1 perf-gen record --call-graph dwarf $NODE_CMD # 2. sample
 # perf-gen report
 # perf-gen script --no-inline | stackcollapse-perf.pl | flamegraph.pl > flame.svg
+export DD_TRACE_EXPERIMENTAL_NATIVE_SPANS_ENABLED=1
 DD_TRACE_EXPERIMENTAL_NATIVE_SPANS_ENABLED=1 npx 0x -- node ./native-span-perf.js
 # google-chrome ./flame.svg
