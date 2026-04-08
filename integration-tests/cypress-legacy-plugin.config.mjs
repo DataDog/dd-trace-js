6b9731f5b0530883c2e349e66618696ebcc00857
Juan Antonio Fernández de Alba <juan.fernandezdealba@datadoghq.com>
2026-04-08 20:36:42 +0200

    [test optimization] Refactor `cypress` as instrumentation (#7829)

---
+++ integration-tests/cypress-legacy-plugin.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from 'cypress'
+import ddTracePlugin from 'dd-trace/ci/cypress/plugin.js'
+
+export default defineConfig({
+  defaultCommandTimeout: 1000,
+  e2e: {
+    setupNodeEvents (on, config) {
+      return ddTracePlugin(on, config)
+    },
+    specPattern: process.env.SPEC_PATTERN || 'cypress/e2e/**/*.cy.js',
+  },
+  video: false,
+  screenshotOnRunFailure: false,
+})
