ccb057fe318510ea1d80f7381d17e88ee66973ee
pabloerhard <pabloerhard02@gmail.com>
2026-04-08 09:30:56 -0400

    added back comments

--- packages/dd-trace/src/runtime_metrics/runtime_metrics.js
+++ packages/dd-trace/src/runtime_metrics/runtime_metrics.js
@@ -55,6 +55,9 @@ module.exports = {
     const useNative = config.runtimeMetrics.native !== false
 
     if (useNative) {
+      // Using no-gc prevents the native gc metrics from being tracked. Not
+      // passing any options means all metrics are tracked.
+      // TODO: This is a workaround. We should find a better solution.
       const watchers = trackEventLoop ? ['loop'] : ['no-gc']
 
       try {
