d710c95d684ad732eb976fa8ca1a5ab1d5236e50
Ruben Bridgewater <ruben@bridgewater.de>
2026-04-09 21:23:13 +0200

    ci: add supported configurations linter to keep in sync with index.d.ts (#7954)
    This makes sure index.d.ts and supported-configurations.json keep in sync.
    This is important, since the json file defines the actual behavior while
    index.d.ts is used for our official documentation.

---
+++ eslint-rules/fixtures/config-names-sync/internal-env-and-ignored-names/index.d.ts
@@ -0,0 +1,14 @@
+declare namespace tracer {
+  export interface TracerOptions {
+    /**
+     * @env DD_LLMOBS_ENABLED
+     * The environment variable listed above takes precedence over programmatic configuration.
+     */
+    llmobs?: {
+      /**
+       * @env DD_LLMOBS_ML_APP
+       */
+      mlApp?: string
+    }
+  }
+}
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,11 +14,12 @@ import eslintPluginPromise from 'eslint-plugin-promise'
 import eslintPluginUnicorn from 'eslint-plugin-unicorn'
 import globals from 'globals'
 
-import eslintProcessEnv from './eslint-rules/eslint-process-env.mjs'
+import eslintConfigNamesSync from './eslint-rules/eslint-config-names-sync.mjs'
 import eslintEnvAliases from './eslint-rules/eslint-env-aliases.mjs'
-import eslintSafeTypeOfObject from './eslint-rules/eslint-safe-typeof-object.mjs'
 import eslintLogPrintfStyle from './eslint-rules/eslint-log-printf-style.mjs'
+import eslintProcessEnv from './eslint-rules/eslint-process-env.mjs'
 import eslintRequireExportExists from './eslint-rules/eslint-require-export-exists.mjs'
+import eslintSafeTypeOfObject from './eslint-rules/eslint-safe-typeof-object.mjs'
 
 const { dependencies } = JSON.parse(readFileSync('./vendor/package.json', 'utf8'))
 
@@ -376,6 +377,7 @@ export default [
         rules: {
           'eslint-process-env': eslintProcessEnv,
           'eslint-env-aliases': eslintEnvAliases,
+          'eslint-config-names-sync': eslintConfigNamesSync,
           'eslint-safe-typeof-object': eslintSafeTypeOfObject,
           'eslint-log-printf-style': eslintLogPrintfStyle,
           'eslint-require-export-exists': eslintRequireExportExists,
@@ -513,6 +515,15 @@ export default [
       'unicorn/prefer-switch': 'off', // Questionable benefit
       'unicorn/prefer-top-level-await': 'off', // Only useful when using ESM
       'unicorn/switch-case-braces': 'off', // Questionable benefit
+    },
+  },
+  {
+    name: 'dd-trace/config-sync',
+    files: [
+      'eslint.config.mjs',
+    ],
+    rules: {
+      'eslint-rules/eslint-config-names-sync': 'error',
     },
   },
   {
