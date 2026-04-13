7523d42e0dfb2c7721135b1e1946c81904b2a499
rochdev <roch.devost@gmail.com>
2026-04-13 15:51:13 -0400

    add apm to rum bridge

--- packages/datadog-instrumentations/src/electron/preload.js
+++ packages/datadog-instrumentations/src/electron/preload.js
@@ -1,5 +1,42 @@
-/* eslint-disable unicorn/no-empty-file */
-
 'use strict'
 
-// TODO: Expose main process APIs through `contextBridge`.
+// eslint-disable-next-line n/no-missing-require
+const { contextBridge, ipcRenderer } = require('electron')
+
+const BRIDGE_CHANNEL = 'datadog:bridge-send'
+const CONFIG_CHANNEL = 'datadog:bridge-config'
+
+// Privacy levels matching @datadog/browser-core DefaultPrivacyLevel
+const MASK = 'mask'
+
+const config = ipcRenderer.sendSync(CONFIG_CHANNEL)
+
+const defaultPrivacyLevel = config?.defaultPrivacyLevel ?? MASK
+const configuredHosts = config?.allowedWebViewHosts ?? []
+// eslint-disable-next-line no-undef
+const allowedHosts = [...new Set([location.hostname, ...configuredHosts])]
+
+const bridge = {
+  getCapabilities () {
+    return '[]'
+  },
+  getPrivacyLevel () {
+    return defaultPrivacyLevel
+  },
+  getAllowedWebViewHosts () {
+    return JSON.stringify(allowedHosts)
+  },
+  send (msg) {
+    ipcRenderer.send(BRIDGE_CHANNEL, msg)
+  },
+}
+
+// Support both contextIsolation enabled (default) and disabled
+
+window.DatadogEventBridge = bridge
+
+try {
+  contextBridge.exposeInMainWorld('DatadogEventBridge', bridge)
+} catch {
+  // exposeInMainWorld throws when contextIsolation is disabled
+}
