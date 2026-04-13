b339e083ab4b93f9fd08e56d30134306f4c8a48a
rochdev <roch.devost@gmail.com>
2026-04-13 15:51:13 -0400

    add apm to rum bridge

--- packages/datadog-plugin-electron/test/app/renderer.js
+++ packages/datadog-plugin-electron/test/app/renderer.js
@@ -1,3 +1,23 @@
 'use strict'
 
-// TODO: Test automatic injection of APM<->RUM bridge.
+const { ipcRenderer } = require('electron/renderer')
+
+ipcRenderer.on('datadog:test:bridge', () => {
+  const bridge = window.DatadogEventBridge
+  let sendSuccess = false
+
+  try {
+    bridge.send('test-payload')
+    sendSuccess = true
+  } catch (_e) {
+    // sendSuccess stays false
+  }
+
+  ipcRenderer.send('datadog:test:bridge:result', {
+    exists: !!bridge,
+    capabilities: bridge?.getCapabilities(),
+    privacyLevel: bridge?.getPrivacyLevel(),
+    allowedHosts: bridge?.getAllowedWebViewHosts(),
+    sendSuccess,
+  })
+})
