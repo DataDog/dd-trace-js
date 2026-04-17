64260fa1273f0f98964428d1d9446dd7856ed39c
Pablo Erhard <104538390+pabloerhard@users.noreply.github.com>
2026-04-17 06:08:08 -0400

    feat(redis): split by instance (#8014)
    * added splitByInstance config for redis
    
    * added splitByInstance into redis declaration file
    
    * remove .only
    
    * use weakmap instead of adding properties to class
    
    * use assertObjectContains isntead of multiple asserts
    
    ---------
    
    Co-authored-by: Ruben Bridgewater <ruben@bridgewater.de>

--- index.d.ts
+++ index.d.ts
@@ -3064,6 +3064,14 @@ declare namespace tracer {
        * @returns true to instrument the command, false to skip it
        */
       filter?: (command: string) => boolean;
+
+      /**
+       * Whether to use a different service name for each Redis instance based
+       * on the configured connection name of the client.
+       *
+       * @default false
+       */
+      splitByInstance?: boolean;
     }
 
     /**
