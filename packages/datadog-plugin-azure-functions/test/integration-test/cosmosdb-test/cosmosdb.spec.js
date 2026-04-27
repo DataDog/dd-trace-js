60499ca970d52df745f783270bc98a1bf20e85ff
Rithika Narayan <rithika.narayan@datadoghq.com>
2026-04-27 13:52:49 -0400

    remove specific version for cosmos package

--- packages/datadog-plugin-azure-functions/test/integration-test/cosmosdb-test/cosmosdb.spec.js
+++ packages/datadog-plugin-azure-functions/test/integration-test/cosmosdb-test/cosmosdb.spec.js
@@ -26,10 +26,11 @@ describe('esm', () => {
     useSandbox([
       `@azure/functions@${version}`,
       'azure-functions-core-tools@4',
+      '@azure/cosmos',
     ],
-    false,
-    ['./packages/datadog-plugin-azure-functions/test/fixtures/*',
-      './packages/datadog-plugin-azure-functions/test/integration-test/cosmosdb-test/*'])
+      false,
+      ['./packages/datadog-plugin-azure-functions/test/fixtures/*',
+        './packages/datadog-plugin-azure-functions/test/integration-test/cosmosdb-test/*'])
 
     before(async function () {
       const helpers = await import(pathToFileURL(path.join(sandboxCwd(), 'cosmosdb-helpers.mjs')).href)
@@ -90,7 +91,7 @@ describe('esm', () => {
   })
 })
 
-async function spawnPluginIntegrationTestProc (cwd, command, args, agentPort, stdioHandler, additionalEnvArgs = {}) {
+async function spawnPluginIntegrationTestProc(cwd, command, args, agentPort, stdioHandler, additionalEnvArgs = {}) {
   let env = {
     NODE_OPTIONS: `--loader=${hookFile}`,
     DD_TRACE_AGENT_PORT: agentPort,
@@ -103,7 +104,7 @@ async function spawnPluginIntegrationTestProc (cwd, command, args, agentPort, st
   }, stdioHandler)
 }
 
-function spawnProc (command, args, options = {}, stdioHandler, stderrHandler) {
+function spawnProc(command, args, options = {}, stdioHandler, stderrHandler) {
   const proc = spawn(command, args, { ...options, stdio: 'pipe' })
   return new Promise((resolve, reject) => {
     proc
