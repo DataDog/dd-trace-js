ccebd14d66f69c1d79aa6a63007852577f51b15b
Juan Fernandez <juan.fernandezdealba@datadoghq.com>
2026-04-28 15:08:48 +0200

    fix(cypress): support TypeScript 6 in browser integration tests

--- integration-tests/cypress/cypress-final-status.spec.js
+++ integration-tests/cypress/cypress-final-status.spec.js
@@ -88,10 +88,7 @@ moduleTypes.forEach(({
 
     // cypress-fail-fast is required as an incompatible plugin.
     // typescript is required to compile .cy.ts spec files in the pre-compiled JS tests.
-    // typescript@5 is pinned because typescript@6 emits "use strict" on line 1 for
-    // non-module files, shifting compiled line numbers and breaking source map resolution.
-    // TODO: Update tests files accordingly and test with different TS versions
-    useSandbox([`cypress@${version}`, 'cypress-fail-fast@7.1.0', 'typescript@5'], true)
+    useSandbox([`cypress@${version}`, 'cypress-fail-fast@7.1.0', 'typescript'], true)
 
     before(async function () {
       // Note: Cypress binary is already installed during useSandbox() via the postinstall script
