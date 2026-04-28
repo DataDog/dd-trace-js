d3cd520c3285b6310de9dc15667189ebb002d6a6
Ruben Bridgewater <ruben.bridgewater@datadoghq.com>
2026-04-28 10:12:18 +0200

    fixup!

--- packages/dd-trace/test/debugger/devtools_client/snapshot/collector-deadline.spec.js
+++ packages/dd-trace/test/debugger/devtools_client/snapshot/collector-deadline.spec.js
@@ -113,7 +113,6 @@ describe('debugger -> devtools client -> snapshot collector deadline', function 
     // Override the hrtime stub to track calls and advance time
     // Drive process.hrtime.bigint() with a plain monotonic counter instead of reading
     // `clock.now`. This works around a sinon fake-timers regression.
-watson marked this conversation as resolved.
     // TODO: Revert when a fix for https://github.com/sinonjs/fake-timers/pull/549#pullrequestreview-4158432441 lands.
     sinon.restore()
     let nowNs = 0n
