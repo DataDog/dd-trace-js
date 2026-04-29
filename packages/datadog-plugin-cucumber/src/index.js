853c6f26256f453c68d942f7bec997f721e81db5
Ruben Bridgewater <ruben@bridgewater.de>
2026-04-29 10:20:47 +0200

    fix: telemetry for test optimization auto instrument provider (#8135)
    * fix: telemetry for test optimization auto instrument provider
    
    Also add test cases for the worker environment variables to work
    as expected. This just increases coverage. The string is working
    as a simple existence check.

--- packages/datadog-plugin-cucumber/src/index.js
+++ packages/datadog-plugin-cucumber/src/index.js
@@ -117,7 +117,7 @@ class CucumberPlugin extends CiPlugin {
       finishAllTraceSpans(this.testSessionSpan)
       this.telemetry.count(TELEMETRY_TEST_SESSION, {
         provider: this.ciProviderName,
-        autoInjected: this._tracerConfig.DD_CIVISIBILITY_AUTO_INSTRUMENTATION_PROVIDER,
+        autoInjected: !!this._tracerConfig.DD_CIVISIBILITY_AUTO_INSTRUMENTATION_PROVIDER,
       })
 
       this.libraryConfig = null
--- packages/datadog-plugin-vitest/src/index.js
+++ packages/datadog-plugin-vitest/src/index.js
@@ -423,7 +423,7 @@ class VitestPlugin extends CiPlugin {
       finishAllTraceSpans(this.testSessionSpan)
       this.telemetry.count(TELEMETRY_TEST_SESSION, {
         provider: this.ciProviderName,
-        autoInjected: this._tracerConfig.DD_CIVISIBILITY_AUTO_INSTRUMENTATION_PROVIDER,
+        autoInjected: !!this._tracerConfig.DD_CIVISIBILITY_AUTO_INSTRUMENTATION_PROVIDER,
       })
       this.tracer._exporter.flush(onFinish)
     })
