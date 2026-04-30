54c93141e81bfbedc7b7224da1685423cb0cc915
Carles Capell <107924659+CarlesDD@users.noreply.github.com>
2026-04-30 11:32:04 +0200

    fix(appsec): Fix regression on Stripe instrumentation (#8186)
    * Fix Stripe instrumentation + test
    
    * Pin latest stripe on plugin test versions

--- packages/datadog-instrumentations/src/stripe.js
+++ packages/datadog-instrumentations/src/stripe.js
@@ -58,35 +58,49 @@ function wrapConstructEventAsync (constructEventAsync) {
   }
 }
 
+function instrumentStripeInstance (stripe) {
+  if (typeof stripe.checkout?.sessions?.create === 'function') {
+    shimmer.wrap(stripe.checkout.sessions, 'create', wrapSessionCreate)
+  }
+  if (typeof stripe.paymentIntents?.create === 'function') {
+    shimmer.wrap(stripe.paymentIntents, 'create', wrapPaymentIntentCreate)
+  }
+  if (typeof stripe.webhooks?.constructEvent === 'function') {
+    shimmer.wrap(stripe.webhooks, 'constructEvent', wrapConstructEvent)
+  }
+  if (typeof stripe.webhooks?.constructEventAsync === 'function') {
+    shimmer.wrap(stripe.webhooks, 'constructEventAsync', wrapConstructEventAsync)
+  }
+}
+
+// stripe <22: the constructor mutates this (when invoked with 'new') and
+// returns nothing; without 'new' it delegates to 'new Stripe(...)' and returns
+// that result. We need to instrument whichever object actually got populated
+function wrapLegacyStripe (Stripe) {
+  return function wrappedStripe () {
+    const result = Stripe.apply(this, arguments)
+    const stripe = this instanceof Stripe ? this : result
+    instrumentStripeInstance(stripe)
+    return stripe
+  }
+}
+
+// stripe >=22: the constructor is a factory that always returns a fresh Stripe
+// instance regardless of 'new', so we just instrument and forward the result
 function wrapStripe (Stripe) {
   return function wrappedStripe () {
-    let stripe = Stripe.apply(this, arguments)
-
-    // to support both with and without "new" operator syntax
-    if (this instanceof Stripe) {
-      stripe = this
-    }
-
-    if (typeof stripe.checkout?.sessions?.create === 'function') {
-      shimmer.wrap(stripe.checkout.sessions, 'create', wrapSessionCreate)
-    }
-    if (typeof stripe.paymentIntents?.create === 'function') {
-      shimmer.wrap(stripe.paymentIntents, 'create', wrapPaymentIntentCreate)
-    }
-    if (typeof stripe.webhooks?.constructEvent === 'function') {
-      shimmer.wrap(stripe.webhooks, 'constructEvent', wrapConstructEvent)
-    }
-    if (typeof stripe.webhooks?.constructEventAsync === 'function') {
-      shimmer.wrap(stripe.webhooks, 'constructEventAsync', wrapConstructEventAsync)
-    }
-
+    const stripe = Stripe.apply(this, arguments)
+    instrumentStripeInstance(stripe)
     return stripe
   }
 }
 
 addHook({
   name: 'stripe',
-  versions: ['9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '>=20.0.0'],
-}, Stripe => {
-  return shimmer.wrapFunction(Stripe, wrapStripe)
-})
+  versions: ['9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '>=20.0.0 <22'],
+}, Stripe => shimmer.wrapFunction(Stripe, wrapLegacyStripe))
+
+addHook({
+  name: 'stripe',
+  versions: ['>=22'],
+}, Stripe => shimmer.wrapFunction(Stripe, wrapStripe))
