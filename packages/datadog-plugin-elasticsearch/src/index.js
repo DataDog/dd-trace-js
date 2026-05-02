371e42738d6569f97954f4d50095a1212e34f6b6
Ruben Bridgewater <ruben.bridgewater@datadoghq.com>
2026-05-02 23:59:21 +0200

    perf(elasticsearch): only stringify the query when present
    `JSON.stringify(params.querystring || params.query)` ran in the
    `bindStart` hot path even when neither field was set, returning the
    literal string `"undefined"` and tagging it on the span.
    
    Drive-by fix:
    
    * Hoist the four `${this.system}.…` tag keys to module-scope
      constants; `system` is fixed for this plugin.

--- packages/datadog-plugin-elasticsearch/src/index.js
+++ packages/datadog-plugin-elasticsearch/src/index.js
@@ -2,26 +2,35 @@
 
 const DatabasePlugin = require('../../dd-trace/src/plugins/database')
 
+const ELASTICSEARCH_URL = 'elasticsearch.url'
+const ELASTICSEARCH_METHOD = 'elasticsearch.method'
+const ELASTICSEARCH_BODY = 'elasticsearch.body'
+const ELASTICSEARCH_PARAMS = 'elasticsearch.params'
+
 class ElasticsearchPlugin extends DatabasePlugin {
   static id = 'elasticsearch'
 
   bindStart (ctx) {
     const { params } = ctx
 
-    const body = getBody(params.body || params.bulkBody)
+    const meta = {
+      'db.type': this.system,
+      [ELASTICSEARCH_URL]: params.path,
+      [ELASTICSEARCH_METHOD]: params.method,
+      [ELASTICSEARCH_BODY]: getBody(params.body || params.bulkBody),
+    }
+
+    const queryString = params.querystring || params.query
+    if (queryString !== undefined) {
+      meta[ELASTICSEARCH_PARAMS] = JSON.stringify(queryString)
+    }
 
     this.startSpan(this.operationName(), {
       service: this.serviceName({ pluginConfig: this.config }),
       resource: `${params.method} ${quantizePath(params.path)}`,
       type: 'elasticsearch',
       kind: 'client',
-      meta: {
-        'db.type': this.system,
-        [`${this.system}.url`]: params.path,
-        [`${this.system}.method`]: params.method,
-        [`${this.system}.body`]: body,
-        [`${this.system}.params`]: JSON.stringify(params.querystring || params.query),
-      },
+      meta,
     }, ctx)
 
     return ctx.currentStore
