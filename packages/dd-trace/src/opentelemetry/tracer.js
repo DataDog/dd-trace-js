9f302fe141fa2819003e8ccc3e1a15c0411ba06d
Ruben Bridgewater <ruben@bridgewater.de>
2026-05-04 22:32:51 +0200

    refactor(otel): extract bridge helpers into span-helpers.js (#8220)
    Pulls the OTel-to-Datadog translation logic out of `span.js` and
    `tracer.js` into a single `span-helpers.js` module exposing:
    `normalizeLinkContext`, `setOtelAttribute`, `setOtelAttributes`,
    `addOtelLink`, `recordException`, `setStatus`.
    
    No behavior change. Consolidating these small helpers in one place
    keeps the OTel-bridge translation layer easy to reason about and lets
    future bridge objects reuse the same logic without duplication.

--- packages/dd-trace/src/opentelemetry/tracer.js
+++ packages/dd-trace/src/opentelemetry/tracer.js
@@ -7,47 +7,12 @@ const tracer = require('../../')
 
 const id = require('../id')
 const log = require('../log')
-const DatadogSpanContext = require('../opentracing/span_context')
 const TextMapPropagator = require('../opentracing/propagation/text_map')
 const TraceState = require('../opentracing/propagation/tracestate')
 const SpanContext = require('./span_context')
 const Span = require('./span')
 const Sampler = require('./sampler')
-
-function normalizeLinkContext (context) {
-  if (!context) return
-
-  // OTel API bridge SpanContext wrapper
-  if (context._ddContext) return context._ddContext
-
-  // Datadog span context
-  if (typeof context.toTraceId === 'function' && typeof context.toSpanId === 'function') {
-    return context
-  }
-
-  // Standard OTel SpanContext (traceId/spanId)
-  if (typeof context.traceId !== 'string' || typeof context.spanId !== 'string') {
-    // Invalid
-    return
-  }
-
-  let sampling
-  if (typeof context.traceFlags === 'number') {
-    sampling = { priority: context.traceFlags & 1 }
-  }
-
-  let tracestate
-  if (context.traceState?.serialize) {
-    tracestate = TraceState.fromString(context.traceState.serialize())
-  }
-
-  return new DatadogSpanContext({
-    traceId: id(context.traceId, 16),
-    spanId: id(context.spanId, 16),
-    sampling,
-    tracestate,
-  })
-}
+const { normalizeLinkContext } = require('./span-helpers')
 
 class Tracer {
   constructor (library, config, tracerProvider) {
