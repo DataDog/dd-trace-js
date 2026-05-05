127cc3854252385364b597e8c44f193adc5de795
Pablo Martínez Bernardo <pablo.martinezbernardo@datadoghq.com>
2026-05-05 13:16:03 +0200

    Tests simplification

--- packages/datadog-instrumentations/src/aws-durable-execution-sdk-js.js
+++ packages/datadog-instrumentations/src/aws-durable-execution-sdk-js.js
@@ -2,7 +2,4 @@
 
 const { addHook } = require('./helpers/instrument')
 
-// Register a hook for the main entry point so RITM fires the loadChannel,
-// enabling the plugin manager to discover and configure the plugin.
-// The actual code instrumentation is done by the orchestrion rewriter.
 addHook({ name: '@aws/durable-execution-sdk-js', versions: ['>=1.1.0'] }, exports => exports)
