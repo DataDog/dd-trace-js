5f54ecdab3e24ee0d6cb43a79454237951287d59
pabloerhard <pabloerhard02@gmail.com>
2026-05-08 13:59:31 -0400

    added aws-sdk withVersions helpers

--- packages/datadog-plugin-aws-sdk/test/spec_helpers.js
+++ packages/datadog-plugin-aws-sdk/test/spec_helpers.js
@@ -1,9 +1,76 @@
 'use strict'
 
+const { withVersions } = require('../../dd-trace/test/setup/mocha')
+const { NODE_MAJOR } = require('../../../version')
+
+const AWS_SDK_V3_RANGE = NODE_MAJOR === 18 ? '3.0.0' : '>3.0.0'
+
 const sort = spans => spans.sort((a, b) => a.start.toString() >= b.start.toString() ? 1 : -1)
+
+/**
+ * @callback AwsSdkVersionCallback
+ * @param {string} version
+ * @param {string} moduleName
+ * @param {string} resolvedVersion
+ * @returns {void}
+ */
+
+/**
+ * @param {string|AwsSdkVersionCallback} range
+ * @param {AwsSdkVersionCallback} [cb]
+ * @returns {void}
+ */
+function withAwsSdkV2Versions (range, cb) {
+  if (typeof range === 'function') {
+    cb = range
+    range = undefined
+  }
+
+  withVersions('aws-sdk', ['aws-sdk'], range, cb)
+}
+
+/**
+ * @param {string|AwsSdkVersionCallback} range
+ * @param {AwsSdkVersionCallback} [cb]
+ * @returns {void}
+ */
+function withAwsSdkV3Versions (range, cb) {
+  if (typeof range === 'function') {
+    cb = range
+    range = undefined
+  }
+
+  withVersions('aws-sdk', ['@aws-sdk/smithy-client'], getAwsSdkV3Range(range), cb)
+}
+
+/**
+ * @param {string|AwsSdkVersionCallback} range
+ * @param {AwsSdkVersionCallback} [cb]
+ * @returns {void}
+ */
+function withAwsSdkVersions (range, cb) {
+  if (typeof range === 'function') {
+    cb = range
+    range = undefined
+  }
+
+  withAwsSdkV2Versions(range, cb)
+  withAwsSdkV3Versions(range, cb)
+}
+
+/**
+ * @param {string|undefined} range
+ * @returns {string}
+ */
+function getAwsSdkV3Range (range) {
+  return range === undefined ? AWS_SDK_V3_RANGE : `${range} ${AWS_SDK_V3_RANGE}`
+}
 
 const helpers = {
   sort,
+  withAwsSdkV2Versions,
+  withAwsSdkV3Versions,
+  withAwsSdkVersions,
 
   setup () {
     before(() => {
