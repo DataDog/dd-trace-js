ec7b340542c4998f6eff6f113363e2b5cf46eba3
Juan Fernandez <juan.fernandezdealba@datadoghq.com>
2026-06-04 08:48:11 +0200

    test(test-optimization): preserve jest quarantine fixture output

--- integration-tests/ci-visibility/test-management/test-quarantine-1.js
+++ integration-tests/ci-visibility/test-management/test-quarantine-1.js
@@ -5,6 +5,9 @@ describe('quarantine tests', () => {
   it('can quarantine a test', () => {
     // eslint-disable-next-line no-console
     console.log('I am running when quarantined') // to check if this is being run
+    if (process.env.JEST_WORKER_ID) {
+      process.stdout.write('I am running when quarantined\n')
+    }
     assert.strictEqual(1 + 2, 4)
   })
 
