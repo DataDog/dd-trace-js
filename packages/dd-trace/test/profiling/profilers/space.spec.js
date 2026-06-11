03108787d3c4963fb56ee67863c8ff254eaf4b3f
ishabi <ilyasshabi94@gmail.com>
2026-06-11 20:49:43 +0200

    fix allocationProfilingEnabled value on space test

--- packages/dd-trace/test/profiling/profilers/space.spec.js
+++ packages/dd-trace/test/profiling/profilers/space.spec.js
@@ -31,7 +31,7 @@ describe('profilers/native/space', () => {
   })
 
   it('should start the internal space profiler', () => {
-    const profiler = new NativeSpaceProfiler()
+    const profiler = new NativeSpaceProfiler({ allocationProfilingEnabled: false })
 
     profiler.start()
 
@@ -40,7 +40,7 @@ describe('profilers/native/space', () => {
 
   it('should use the provided configuration options', () => {
     const heapSamplingInterval = 1024
-    const profiler = new NativeSpaceProfiler({ heapSamplingInterval })
+    const profiler = new NativeSpaceProfiler({ heapSamplingInterval, allocationProfilingEnabled: false })
 
     profiler.start()
 
