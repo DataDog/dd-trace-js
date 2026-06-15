d61dcb3335ca539612b64733caffa90e7b6e0743
Tasadduq Burney <91282734+TasadduqB@users.noreply.github.com>
2026-06-15 13:15:12 +0000

    fix: graphql resolver depth filter depends on path collapsing

--- packages/datadog-plugin-graphql/src/resolve.js
+++ packages/datadog-plugin-graphql/src/resolve.js
@@ -1,8 +1,6 @@
 'use strict'
-
 const dc = require('dc-polyfill')
 const TracingPlugin = require('../../dd-trace/src/plugins/tracing')
-
 class GraphQLResolvePlugin extends TracingPlugin {
   static id = 'graphql'
   static operation = 'resolve'
@@ -23,8 +21,8 @@ class GraphQLResolvePlugin extends TracingPlugin {
    *   variableValues: Record<string, unknown> | undefined,
    * }} fieldCtx
    */
-  start (fieldCtx) {
-    if (!shouldInstrument(this.config, fieldCtx.path)) return
+  start(fieldCtx) {
+    if (!shouldInstrument(this.config, fieldCtx.path, fieldCtx.rootCtx.collapse)) return
 
     const { rootCtx, args, path, pathString, fieldName, returnType, fieldNode, variableValues } = fieldCtx
 
@@ -35,11 +33,9 @@ class GraphQLResolvePlugin extends TracingPlugin {
 
     const parentField = getParentField(rootCtx, path)
     const childOf = parentField?.ctx?.currentStore?.span
-
     const document = rootCtx.source
     const loc = this.config.source && document && fieldNode && fieldNode.loc
     const source = loc && document.slice(loc.start, loc.end)
-
     let namedReturnType = returnType
     while (namedReturnType.ofType) namedReturnType = namedReturnType.ofType
 
@@ -58,7 +54,6 @@ class GraphQLResolvePlugin extends TracingPlugin {
 
     if (fieldNode && this.config.variables && fieldNode.arguments) {
       const variables = this.config.variables(variableValues)
-
       for (const arg of fieldNode.arguments) {
         if (arg.value?.name && arg.value.kind === 'Variable' && variables[arg.value.name.value]) {
           const name = arg.value.name.value
@@ -68,15 +63,17 @@ class GraphQLResolvePlugin extends TracingPlugin {
     }
 
     if (this.resolverStartCh.hasSubscribers) {
-      this.resolverStartCh.publish({ ctx: rootCtx, resolverInfo: getResolverInfo(fieldNode, fieldName, args) })
+      this.resolverStartCh.publish({
+        ctx: rootCtx,
+        resolverInfo: getResolverInfo(fieldNode, fieldName, args),
+      })
     }
 
     return fieldCtx.currentStore
   }
 
-  constructor (...args) {
+  constructor(...args) {
     super(...args)
-
     this.addTraceSub('updateField', (ctx) => {
       // start short-circuited on the depth gate, so there is no span to advance.
       if (ctx.currentStore === undefined) return
@@ -86,21 +83,18 @@ class GraphQLResolvePlugin extends TracingPlugin {
       field.finishTime = span._getTime ? span._getTime() : 0
       field.error = field.error || error
     })
-
     this.resolverStartCh = dc.channel('datadog:graphql:resolver:start')
   }
 
-  configure (config) {
+  configure(config) {
     // this will disable resolve subscribers if `config.depth` is set to 0
     super.configure(config.depth === 0 ? false : config)
   }
 
-  finish (ctx) {
+  finish(ctx) {
     const { finishTime } = ctx
-
     const span = ctx?.currentStore?.span || this.activeSpan
     span.finish(finishTime)
-
     return ctx.parentStore
   }
 }
@@ -110,18 +104,15 @@ class GraphQLResolvePlugin extends TracingPlugin {
 /**
  * @param {{ depth: number, collapse: boolean }} config
  * @param {{ prev: object | undefined, key: string | number }} path
+ * @param {boolean} collapse
  */
-function shouldInstrument (config, path) {
+function shouldInstrument(config, path, collapse) {
   const depth = config.depth
   if (depth < 0) return true
 
   let count = 0
-  if (config.collapse) {
-    for (let curr = path; curr; curr = curr.prev) count += 1
-  } else {
-    for (let curr = path; curr; curr = curr.prev) {
-      if (typeof curr.key === 'string') count += 1
-    }
+  for (let curr = path; curr; curr = curr.prev) {
+    if (collapse || typeof curr.key === 'string') count += 1
   }
   return depth >= count
 }
@@ -131,28 +122,23 @@ function shouldInstrument (config, path) {
  * @param {string} fieldName
  * @param {Record<string, unknown> | undefined} args
  */
-function getResolverInfo (fieldNode, fieldName, args) {
+function getResolverInfo(fieldNode, fieldName, args) {
   let resolverVars
-
   if (args && Object.keys(args).length > 0) {
     resolverVars = { ...args }
   }
-
   const directives = fieldNode?.directives
   if (Array.isArray(directives)) {
     for (const directive of directives) {
       if (directive.arguments.length === 0) continue
-
       const argList = {}
       for (const argument of directive.arguments) {
         argList[argument.name.value] = argument.value.value
       }
-
       resolverVars ??= {}
       resolverVars[directive.name.value] = argList
     }
   }
-
   return resolverVars === undefined ? null : { [fieldName]: resolverVars }
 }
 
@@ -160,11 +146,10 @@ function getResolverInfo (fieldNode, fieldName, args) {
  * @param {{ fields: Map<object, { error: unknown, ctx: object }> }} rootCtx
  * @param {{ prev: object | undefined }} path
  */
-function getParentField (rootCtx, path) {
+function getParentField(rootCtx, path) {
   for (let curr = path.prev; curr; curr = curr.prev) {
     const field = rootCtx.fields.get(curr)
     if (field) return field
   }
+  return undefined
 }
-
-module.exports = GraphQLResolvePlugin
