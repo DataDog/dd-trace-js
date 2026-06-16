eaf8b539ec15e723bb14a540f78a9643582d16a0
Pablo Erhard <104538390+pabloerhard@users.noreply.github.com>
2026-06-16 10:12:19 -0400

    feat(bull-mq): adds support for filter config opt (#8340)

--- docs/test.ts
+++ docs/test.ts
@@ -252,6 +252,11 @@ const awsSdkOptions: plugins.aws_sdk = {
   }
 };
 
+const bullmqOptions: plugins.bullmq = {
+  service: 'test',
+  producerFilter: ({ name, queueName }) => name !== 'skip' && queueName !== 'dead-letter',
+};
+
 const redisOptions: plugins.redis = {
   service: 'test',
   allowlist: ['info', /auth/i, command => true],
@@ -293,6 +298,7 @@ tracer.use('azure-cosmos');
 tracer.use('azure-event-hubs')
 tracer.use('azure-functions');
 tracer.use('bullmq');
+tracer.use('bullmq', bullmqOptions);
 tracer.use('bunyan');
 tracer.use('couchbase');
 tracer.use('cassandra-driver');
