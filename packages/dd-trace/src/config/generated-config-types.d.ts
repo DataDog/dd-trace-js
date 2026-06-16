82b2f7e3632152f9857faf2139796ff8d8ae69fa
Ugaitz Urien <ugaitz.urien@datadoghq.com>
2026-06-16 15:06:19 +0200

    fix(appsec): Implement limits for analyzed downstream requests (#8655)

--- packages/dd-trace/src/config/generated-config-types.d.ts
+++ packages/dd-trace/src/config/generated-config-types.d.ts
@@ -11,6 +11,7 @@ export interface GeneratedConfig {
       enabled: boolean;
       endpointCollectionEnabled: boolean;
       endpointCollectionMessageLimit: number;
+      maxDownstreamBodyBytes: number;
       maxDownstreamRequestBodyAnalysis: number;
       sampleDelay: number;
     };
