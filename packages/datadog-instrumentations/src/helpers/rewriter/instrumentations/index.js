210993142b8b05da165bb473b62b6a9096547883
Sam Brenner <sam.brenner@datadoghq.com>
2026-06-29 10:51:50 -0400

    fix merge conflict

--- packages/datadog-instrumentations/src/helpers/rewriter/instrumentations/index.js
+++ packages/datadog-instrumentations/src/helpers/rewriter/instrumentations/index.js
@@ -4,11 +4,8 @@ module.exports = [
   ...require('./ai'),
   ...require('./azure-cosmos'),
   ...require('./bullmq'),
-<<<<<<< feat/claude-agent-sdk-integration
   ...require('./claude-agent-sdk'),
-=======
   ...require('./graphql'),
->>>>>>> master
   ...require('./langchain'),
   ...require('./langgraph'),
   ...require('./modelcontextprotocol-sdk'),
