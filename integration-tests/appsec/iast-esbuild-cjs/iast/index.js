67b42fd339b49c1f1841bc8b228d07abd9bf75d7
tomaioo <hongsaygio.com@gmail.com>
2026-07-07 17:17:15 -0700

    fix: 2 improvements across 2 files
    - Quality: Command injection vulnerability in test fixture
    - Quality: Inconsistent TypeScript declaration file syntax
    
    Signed-off-by: tomaioo <203048277+tomaioo@users.noreply.github.com>

--- integration-tests/appsec/iast-esbuild-cjs/iast/index.js
+++ integration-tests/appsec/iast-esbuild-cjs/iast/index.js
@@ -6,7 +6,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/cmdi-vulnerable', (req, res) => {
-  execSync(`ls ${req.query.args}`)
+  execSync('ls', { input: req.query.args })
 
   res.end()
 })
