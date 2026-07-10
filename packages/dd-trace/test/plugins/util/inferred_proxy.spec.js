9111874571192443c3a8541914cea24ae521f147
topher.campbell <topher.campbell@datadoghq.com>
2026-07-10 12:02:24 -0400

    Revert "Fixing tests"
    This reverts commit 665c9f7de73360a95cd932bac4fb0f801fdc3ec9.

--- packages/dd-trace/test/plugins/util/inferred_proxy.spec.js
+++ packages/dd-trace/test/plugins/util/inferred_proxy.spec.js
@@ -439,7 +439,7 @@ Object.entries(proxyConfigs).forEach(([proxyType, config]) => {
           })
         })
       })
-      (proxyType === 'aws-apigateway' ? it : it.skip)('should not create a span if timestamp is missing from headers', async () => {
+      it('should not create a span if timestamp is missing from headers', async () => {
         const testCase = additionalTestCases['aws-missingtimestamp']
         await loadTest({})
 
