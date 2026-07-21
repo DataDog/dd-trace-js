258d01efd1a25dd741c2029238b2960deefdca5f
Crystal Luc-Magloire <crystal.magloire@datadoghq.com>
2026-07-21 19:22:27 -0400

    test(init): guard fixture source logging (#9269)

--- integration-tests/init/trace.js
+++ integration-tests/init/trace.js
@@ -2,6 +2,8 @@
 
 // eslint-disable-next-line no-console
 console.log(!!global._ddtrace)
-// eslint-disable-next-line no-console
-console.log('instrumentation source:', global._ddtrace._tracer._config.instrumentationSource)
+if (global._ddtrace?._tracer) {
+  // eslint-disable-next-line no-console
+  console.log('instrumentation source:', global._ddtrace._tracer._config.instrumentationSource)
+}
 process.exit()
