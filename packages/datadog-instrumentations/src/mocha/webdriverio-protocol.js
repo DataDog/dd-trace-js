f407cc1ce148389c87a48ab591c6d3f626ff39ff
Juan Antonio Fernández de Alba <juan.fernandezdealba@datadoghq.com>
2026-07-28 14:37:13 +0200

    fix(test-optimization): hide WebdriverIO worker messages (#9561)

--- packages/datadog-instrumentations/src/mocha/webdriverio-protocol.js
+++ packages/datadog-instrumentations/src/mocha/webdriverio-protocol.js
@@ -1,9 +1,19 @@
 'use strict'
 
+const {
+  createWebdriverioWorkerMessage,
+  WEBDRIVERIO_WORKER_ENV,
+  WEBDRIVERIO_WORKER_EVENT,
+  WEBDRIVERIO_WORKER_ORIGIN,
+} = require('../../../dd-trace/src/ci-visibility/exporters/test-worker/webdriverio')
+
 module.exports = {
   CONFIGURATION_REQUEST: 'dd:test-optimization:webdriverio:configuration:request',
   CONFIGURATION_RESPONSE: 'dd:test-optimization:webdriverio:configuration:response',
+  createWebdriverioWorkerMessage,
   SUITE_FINISH: 'dd:test-optimization:webdriverio:test-suite:finish',
   WORKER_READY: 'dd:test-optimization:webdriverio:worker:ready',
-  WEBDRIVERIO_WORKER_ENV: '_DD_TEST_OPTIMIZATION_WEBDRIVERIO_WORKER',
+  WEBDRIVERIO_WORKER_ENV,
+  WEBDRIVERIO_WORKER_EVENT,
+  WEBDRIVERIO_WORKER_ORIGIN,
 }
