26de44dd3b3a1a006ce69378bb826f3593b08c8e
Juan Antonio Fernández de Alba <juan.fernandezdealba@datadoghq.com>
2026-07-28 09:06:55 +0200

    feat(test-optimization): support WebdriverIO with Mocha (#9520)

---
+++ packages/datadog-instrumentations/test/fixtures/webdriverio-disconnected-worker.js
@@ -0,0 +1,122 @@
+'use strict'
+
+const assert = require('node:assert/strict')
+const { EventEmitter } = require('node:events')
+
+const { channel } = require('../../src/helpers/instrument')
+const instrumentations = require('../../src/helpers/instrumentations')
+const {
+  CONFIGURATION_REQUEST,
+  CONFIGURATION_RESPONSE,
+  WEBDRIVERIO_WORKER_ENV,
+} = require('../../src/mocha/webdriverio-protocol')
+
+process.env[WEBDRIVERIO_WORKER_ENV] = 'true'
+const existingMochaHookCount = instrumentations.mocha?.length || 0
+require('../../src/mocha/worker')
+
+const webdriverioMochaHooks = instrumentations.mocha.slice(existingMochaHookCount)
+const mochaHook = webdriverioMochaHooks.find(({ file }) => file === 'lib/mocha.js')
+const runnerHook = webdriverioMochaHooks.find(({ file }) => file === 'lib/runner.js')
+
+assert.ok(mochaHook)
+assert.ok(runnerHook)
+
+const workerFinishCh = channel('ci:mocha:worker:finish')
+
+function onWorkerFinish () {}
+
+workerFinishCh.subscribe(onWorkerFinish)
+
+/**
+ * Exercises worker-ready and suite-finish messages.
+ *
+ * @returns {void}
+ */
+function exerciseWorkerMessages () {
+  class FakeMocha {
+    constructor () {
+      this.files = []
+      this.options = {}
+      this.suite = {
+        run () {},
+      }
+    }
+
+    /**
+     * @returns {object}
+     */
+    run () {
+      return {}
+    }
+  }
+
+  class FakeRunner extends EventEmitter {
+    constructor () {
+      super()
+      this.failures = 0
+      this.suite = {
+        eachTest () {},
+      }
+    }
+
+    /**
+     * @returns {void}
+     */
+    runTests () {}
+
+    /**
+     * @returns {void}
+     */
+    run () {
+      this.emit('fail', { file: 'hook-fail.e2e.js', type: 'hook' })
+      this.emit('end')
+    }
+  }
+
+  mochaHook.hook(FakeMocha, '10.8.2')
+  runnerHook.hook(FakeRunner)
+  new FakeMocha().run()
+  new FakeRunner().run()
+}
+
+let sendCalls = 0
+process.connected = false
+process.send = () => {
+  sendCalls++
+  throw new Error('send called after disconnect')
+}
+
+exerciseWorkerMessages()
+assert.strictEqual(sendCalls, 0)
+
+process.connected = true
+process.send = (message, onDone) => {
+  sendCalls++
+  assert.ok(message)
+  assert.strictEqual(typeof onDone, 'function')
+  onDone(new Error('IPC channel closed during send'))
+}
+
+exerciseWorkerMessages()
+assert.strictEqual(sendCalls, 3)
+
+process.send = (message, onDone) => {
+  sendCalls++
+  assert.ok(message)
+  assert.strictEqual(typeof onDone, 'function')
+  onDone()
+  if (message.name === CONFIGURATION_REQUEST) {
+    process.emit('message', {
+      name: CONFIGURATION_RESPONSE,
+      content: {
+        requestId: message.content.requestId,
+      },
+    })
+  }
+}
+
+exerciseWorkerMessages()
+assert.strictEqual(sendCalls, 6)
+
+workerFinishCh.unsubscribe(onWorkerFinish)
