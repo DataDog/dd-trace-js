96b8961444e4d59be84f0989e8fa213694106601
Ruben Bridgewater <ruben.bridgewater@datadoghq.com>
2026-08-03 12:38:48 +0200

    fix(types): preserve supported span tag objects
    ## Summary
    
    Keep declaration-only span tag assertions out of the executable docs test and preserve named tag interfaces whose properties are supported.
    
    ## Why
    
    The docs smoke test dereferenced a null active span at runtime. Record index signatures also rejected supported named interfaces because they lack a string index signature.
    
    ## Test plan
    
    - npm run type:doc:test
    - npm run lint
    
    Refs: https://github.com/DataDog/dd-trace-js/issues/5058

---
+++ docs/span-tags.ts
@@ -0,0 +1,103 @@
+import tracer, { Span, SpanOptions, SpanTags, TracerOptions } from '..'
+import tracerV5 = require('../index.d.v5')
+
+declare module '..' {
+  interface TracerOptions {
+    customOption?: boolean
+  }
+}
+
+declare module '../index.d.v5' {
+  interface TracerOptions {
+    customOption?: boolean
+  }
+}
+
+interface TagObject {
+  string: string
+  number: number
+  optional?: boolean
+  buffer: Buffer
+  url: URL
+}
+
+interface Tags {
+  string: string
+  number: number
+  boolean: boolean
+  object: TagObject
+}
+
+declare const span: Span
+declare const tagObject: TagObject
+declare const tags: Tags
+declare const v5Span: ReturnType<typeof tracerV5.startSpan>
+
+span.setTag('string', 'value')
+span.setTag('number', 1)
+span.setTag('boolean', true)
+span.setTag('error', new Error('boom'))
+span.setTag('buffer', Buffer.from('value'))
+span.setTag('url', new URL('https://example.com'))
+span.setTag('object', tagObject)
+span.addTags(tags)
+tracer.startSpan('test', { tags })
+tracer.trace('test', { tags }, () => {})
+tracer.wrap('test', { tags }, () => {})
+tracer.wrap('test', () => ({ tags }), () => {})
+tracer.init({ tags })
+
+const spanTags: SpanTags<Tags> = tags
+const spanOptions: SpanOptions<Tags> = { tags }
+const tracerOptions: TracerOptions = { customOption: true, tags: { string: 'value' } }
+void spanTags
+void spanOptions
+void tracerOptions
+
+// @ts-expect-error Nested object tag values are not supported.
+span.setTag('nested', { child: { value: 'value' } })
+// @ts-expect-error Array tag values are not supported.
+span.setTag('array', ['value'])
+// @ts-expect-error Function tag values are not supported.
+span.setTag('function', () => {})
+// @ts-expect-error Date tag values are not supported.
+span.setTag('date', new Date())
+// @ts-expect-error Null tag values are not supported.
+span.setTag('null', null)
+// @ts-expect-error Undefined tag values are not supported.
+span.setTag('undefined', undefined)
+// @ts-expect-error Nested object tag values are not supported.
+span.addTags({ nested: { child: { value: 'value' } } })
+// @ts-expect-error Nested object tag values are not supported.
+tracer.startSpan('test', { tags: { nested: { child: { value: 'value' } } } })
+// @ts-expect-error Nested object tag values are not supported.
+tracer.trace('test', { tags: { nested: { child: { value: 'value' } } } }, () => {})
+// @ts-expect-error Nested object tag values are not supported.
+tracer.wrap('test', { tags: { nested: { child: { value: 'value' } } } }, () => {})
+// @ts-expect-error Nested object tag values are not supported.
+tracer.wrap('test', () => ({ tags: { nested: { child: { value: 'value' } } } }), () => {})
+// @ts-expect-error Nested object tag values are not supported.
+tracer.init({ tags: { nested: { child: { value: 'value' } } } })
+
+v5Span.setTag('object', tagObject)
+v5Span.addTags(tags)
+tracerV5.startSpan('test', { tags })
+tracerV5.trace('test', { tags }, () => {})
+tracerV5.wrap('test', { tags }, () => {})
+tracerV5.wrap('test', () => ({ tags }), () => {})
+tracerV5.init({ customOption: true, tags })
+
+// @ts-expect-error Nested object tag values are not supported.
+v5Span.setTag('nested', { child: { value: 'value' } })
+// @ts-expect-error Nested object tag values are not supported.
+v5Span.addTags({ nested: { child: { value: 'value' } } })
+// @ts-expect-error Nested object tag values are not supported.
+tracerV5.startSpan('test', { tags: { nested: { child: { value: 'value' } } } })
+// @ts-expect-error Nested object tag values are not supported.
+tracerV5.trace('test', { tags: { nested: { child: { value: 'value' } } } }, () => {})
+// @ts-expect-error Nested object tag values are not supported.
+tracerV5.wrap('test', { tags: { nested: { child: { value: 'value' } } } }, () => {})
+// @ts-expect-error Nested object tag values are not supported.
+tracerV5.wrap('test', () => ({ tags: { nested: { child: { value: 'value' } } } }), () => {})
+// @ts-expect-error Nested object tag values are not supported.
+tracerV5.init({ tags: { nested: { child: { value: 'value' } } } })
--- index.d.ts
+++ index.d.ts
@@ -29,7 +29,10 @@ interface Tracer extends opentracing.Tracer {
    * @param {tracer.SpanOptions} [options] Options for the newly created span.
    * @returns {Span} A new Span object.
    */
-  startSpan (name: string, options?: tracer.SpanOptions): tracer.Span;
+  startSpan<Tags extends object = tracer.SpanTags> (
+    name: string,
+    options?: tracer.SpanOptions<Tags>
+  ): tracer.Span;
 
   /**
    * Injects the given SpanContext instance for cross-process propagation
@@ -57,7 +60,7 @@ interface Tracer extends opentracing.Tracer {
   /**
    * Initializes the tracer. This should be called before importing other libraries.
    */
-  init (options?: tracer.TracerOptions): this;
+  init<Tags extends object = tracer.SpanTags> (options?: TracerOptionsWithTags<Tags>): this;
 
   /**
    * Sets the URL for the trace agent. This should only be called _after_
@@ -94,7 +97,11 @@ interface Tracer extends opentracing.Tracer {
    */
   trace<T> (name: string, fn: (span: tracer.Span) => T): T;
   trace<T> (name: string, fn: (span: tracer.Span, done: (error?: Error) => void) => T): T;
-  trace<T> (name: string, options: tracer.TraceOptions & tracer.SpanOptions, fn: (span?: tracer.Span, done?: (error?: Error) => void) => T): T;
+  trace<T, Tags extends object = tracer.SpanTags> (
+    name: string,
+    options: tracer.TraceOptions & tracer.SpanOptions<Tags>,
+    fn: (span?: tracer.Span, done?: (error?: Error) => void) => T
+  ): T;
 
   /**
    * Wrap a function to automatically create a span activated on its
@@ -111,8 +118,16 @@ interface Tracer extends opentracing.Tracer {
    * which case the span will finish at the end of the function execution.
    */
   wrap<T = (...args: any[]) => any> (name: string, fn: T): T;
-  wrap<T = (...args: any[]) => any> (name: string, options: tracer.TraceOptions & tracer.SpanOptions, fn: T): T;
-  wrap<T = (...args: any[]) => any> (name: string, options: (...args: any[]) => tracer.TraceOptions & tracer.SpanOptions, fn: T): T;
+  wrap<T = (...args: any[]) => any, Tags extends object = tracer.SpanTags> (
+    name: string,
+    options: tracer.TraceOptions & tracer.SpanOptions<Tags>,
+    fn: T
+  ): T;
+  wrap<T = (...args: any[]) => any, Tags extends object = tracer.SpanTags> (
+    name: string,
+    options: (...args: any[]) => tracer.TraceOptions & tracer.SpanOptions<Tags>,
+    fn: T
+  ): T;
 
   /**
    * Returns an HTML string containing <meta> tags that should be included in
@@ -313,16 +328,28 @@ interface Plugins {
   "ws": tracer.plugins.ws;
 }
 
+type SpanTagScalar = string | number | boolean | Buffer | URL
+type TracerOptionsWithTags<Tags extends object> = Omit<tracer.TracerOptions, 'tags'> & {
+  tags?: Tags & tracer.SpanTags<Tags>
+}
+
 declare namespace tracer {
   export interface PluginOptions extends Plugins {}
   export type PluginName = keyof PluginOptions;
 
-  export type SpanTagValue = string | number | boolean | Buffer | URL | Error | {
-    [key: string]: string | number | boolean | Buffer | URL
+  export type SpanTagValue<T = SpanTagScalar | Error | Record<string, SpanTagScalar>> =
+    T extends SpanTagScalar | Error ? T :
+    T extends readonly unknown[] ? never :
+    T extends (...args: never[]) => unknown ? never :
+    T extends object ? T extends {
+      [K in keyof T]: Exclude<T[K], undefined> extends SpanTagScalar ? T[K] : never
+    } ? T : never :
+    never
+  export type SpanTags<T extends object = Record<string, SpanTagValue>> = {
+    [K in keyof T]: SpanTagValue<T[K]>
   }
-  export type SpanTags = Record<string, SpanTagValue>
 
-  export type SpanOptions = Omit<opentracing.SpanOptions, 'childOf' | 'tags'> & {
+  export type SpanOptions<Tags extends object = SpanTags> = Omit<opentracing.SpanOptions, 'childOf' | 'tags'> & {
   /**
    * Set childOf to 'null' to create a root span without a parent, even when a parent span
    * exists in the current async context. If 'undefined' the parent will be inferred from the
@@ -336,7 +363,7 @@ declare namespace tracer {
     /**
      * Tags for the newly created span.
      */
-    tags?: SpanTags;
+    tags?: Tags & SpanTags<Tags>;
   };
   export { Tracer };
 
@@ -380,14 +407,14 @@ declare namespace tracer {
      * @param key The tag name.
      * @param value The tag value.
      */
-    setTag (key: string, value: SpanTagValue): this;
+    setTag<T> (key: string, value: T & SpanTagValue<T>): this;
 
     /**
      * Adds tags to the span.
      *
      * @param keyValueMap The tags to add.
      */
-    addTags (keyValueMap: SpanTags): this;
+    addTags<Tags extends object> (keyValueMap: Tags & SpanTags<Tags>): this;
 
     /**
      * Adds a single link to the span.
