4bba7825f2147f635832389e3000ac0d065324b8
Juan Antonio Fernández de Alba <juan.fernandezdealba@datadoghq.com>
2026-08-03 10:42:03 +0200

    feat(vitest): add zero-config test impact analysis (#9604)

--- packages/dd-trace/src/ci-visibility/exporters/test-worker/index.js
+++ packages/dd-trace/src/ci-visibility/exporters/test-worker/index.js
@@ -10,7 +10,9 @@ const {
   JEST_WORKER_LOGS_PAYLOAD_CODE,
   PLAYWRIGHT_WORKER_TRACE_PAYLOAD_CODE,
   VITEST_WORKER_TRACE_PAYLOAD_CODE,
+  VITEST_WORKER_COVERAGE_PAYLOAD_CODE,
   VITEST_WORKER_LOGS_PAYLOAD_CODE,
+  VITEST_WORKER_TELEMETRY_PAYLOAD_CODE,
 } = require('../../../plugins/util/test')
 const getConfig = require('../../../config')
 const { getEnvironmentVariable } = require('../../../config/helper')
@@ -44,6 +46,9 @@ function getInterprocessCoverageCode () {
   if (getEnvironmentVariable('JEST_WORKER_ID')) {
     return JEST_WORKER_COVERAGE_PAYLOAD_CODE
   }
+  if (getEnvironmentVariable('TINYPOOL_WORKER_ID') || getConfig().DD_VITEST_WORKER) {
+    return VITEST_WORKER_COVERAGE_PAYLOAD_CODE
+  }
   return null
 }
 
@@ -67,6 +72,9 @@ function getInterprocessTelemetryCode () {
   if (getEnvironmentVariable('JEST_WORKER_ID')) {
     return JEST_WORKER_TELEMETRY_PAYLOAD_CODE
   }
+  if (getEnvironmentVariable('TINYPOOL_WORKER_ID') || getConfig().DD_VITEST_WORKER) {
+    return VITEST_WORKER_TELEMETRY_PAYLOAD_CODE
+  }
   return null
 }
 
@@ -85,7 +93,6 @@ class TestWorkerCiVisibilityExporter {
     this._writer = new Writer(interprocessTraceCode)
     this._coverageWriter = new Writer(interprocessCoverageCode)
     this._logsWriter = new Writer(interprocessLogsCode)
-    // TODO: add support for test workers other than Jest
     if (interprocessTelemetryCode) {
       this._telemetryWriter = new Writer(interprocessTelemetryCode)
       this.exportTelemetry = function (telemetryEvent) {
