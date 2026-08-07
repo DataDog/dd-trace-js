028ca829f217f874d4e800a20646d71e60d6913c
Juan Antonio Fernández de Alba <juan.fernandezdealba@datadoghq.com>
2026-08-07 11:18:24 +0200

    fix(test-optimization): escape Vitest browser provided context (#9730)

--- packages/datadog-instrumentations/src/vitest-main.js
+++ packages/datadog-instrumentations/src/vitest-main.js
@@ -54,6 +54,7 @@ const {
   findExportByName,
   getTypeTasks,
   getWorkspaceProject,
+  parseProvidedContextValue,
   setProvidedContext,
   getVitestTestProperties,
 } = require('./vitest-util')
@@ -1445,7 +1446,7 @@ function getMainProcessProvidedContext (ctx) {
     const providedContext = workspaceProject.getProvidedContext?.() || workspaceProject._provided || {}
 
     return {
-      testPropertiesByFilepath: providedContext._ddTestPropertiesByFilepath || {},
+      testPropertiesByFilepath: parseProvidedContextValue(providedContext._ddTestPropertiesByFilepath) || {},
     }
   } catch {
     return {
