267cdafb0e68f71c690b009b4dd2785d1a0f3346
Juan Antonio Fernández de Alba <juan.fernandezdealba@datadoghq.com>
2026-08-07 09:00:54 +0200

    feat(test-optimization): optimize WebdriverIO Jasmine tests (#9668)

---
+++ integration-tests/webdriverio/fixtures/jasmine-efd-skipped.e2e.js
@@ -0,0 +1,8 @@
+'use strict'
+
+describe('WebdriverIO Jasmine EFD statuses', () => {
+  it('passes', () => {})
+
+  // eslint-disable-next-line mocha/no-pending-tests -- this fixture verifies skipped EFD-test reporting.
+  xit('stays skipped', () => {})
+})
