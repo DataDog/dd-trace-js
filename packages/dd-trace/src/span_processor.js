b9b623c1e675f745fcdbba1fae5de5ab19cdaa40
Ruben Bridgewater <ruben.bridgewater@datadoghq.com>
2026-08-18 10:59:37 +0200

    refactor(trace): inline span processor cleanup

--- packages/dd-trace/src/span_processor.js
+++ packages/dd-trace/src/span_processor.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const eraseTrace = require('./span-processor-state')
+const log = require('./log')
 const spanFormat = require('./span_format')
 const SpanSampler = require('./span_sampler')
 const GitMetadataTagger = require('./git_metadata_tagger')
@@ -60,7 +60,7 @@ class SpanProcessor {
 
     if (trace.record === false) return
     if (DD_TRACE_ENABLED === false) {
-      eraseTrace(trace, active, this._config.DD_TRACE_EXPERIMENTAL_STATE_TRACKING, startedSpans, finishedSpans)
+      this.#erase(trace, active)
       return
     }
     if (started.length === finished.length || finished.length >= flushMinSpans) {
@@ -91,7 +91,7 @@ class SpanProcessor {
         this._exporter.export(formatted)
       }
 
-      eraseTrace(trace, active, this._config.DD_TRACE_EXPERIMENTAL_STATE_TRACKING, startedSpans, finishedSpans)
+      this.#erase(trace, active)
     }
 
     if (this._killAll) {
@@ -105,6 +105,87 @@ class SpanProcessor {
 
   killAll () {
     this._killAll = true
+  }
+
+  /**
+   * Validate optional span state tracking and retain only active spans.
+   * @param {object} trace Trace state to clear
+   * @param {object[]} active Spans that remain active
+   */
+  #erase (trace, active) {
+    if (this._config.DD_TRACE_EXPERIMENTAL_STATE_TRACKING) {
+      const started = new Set()
+      const startedIds = new Set()
+      const finished = new Set()
+      const finishedIds = new Set()
+
+      for (const span of trace.finished) {
+        const context = span.context()
+        const id = context.toSpanId()
+
+        if (finished.has(span)) {
+          log.error('Span was already finished in the same trace: %s', span)
+        } else {
+          finished.add(span)
+
+          if (finishedIds.has(id)) {
+            log.error('Another span with the same ID was already finished in the same trace: %s', span)
+          } else {
+            finishedIds.add(id)
+          }
+
+          if (context._trace !== trace) {
+            log.error('A span was finished in the wrong trace: %s', span)
+          }
+
+          if (finishedSpans.has(span)) {
+            log.error('Span was already finished in a different trace: %s', span)
+          } else {
+            finishedSpans.add(span)
+          }
+        }
+      }
+
+      for (const span of trace.started) {
+        const context = span.context()
+        const id = context.toSpanId()
+
+        if (started.has(span)) {
+          log.error('Span was already started in the same trace: %s', span)
+        } else {
+          started.add(span)
+
+          if (startedIds.has(id)) {
+            log.error('Another span with the same ID was already started in the same trace: %s', span)
+          } else {
+            startedIds.add(id)
+          }
+
+          if (context._trace !== trace) {
+            log.error('A span was started in the wrong trace: %s', span)
+          }
+
+          if (startedSpans.has(span)) {
+            log.error('Span was already started in a different trace: %s', span)
+          } else {
+            startedSpans.add(span)
+          }
+        }
+
+        if (!finished.has(span)) {
+          log.error('Span started in one trace but was finished in another trace: %s', span)
+        }
+      }
+
+      for (const span of trace.finished) {
+        if (!started.has(span)) {
+          log.error('Span finished in one trace but was started in another trace: %s', span)
+        }
+      }
+    }
+
+    trace.started = active
+    trace.finished = []
   }
 }
 
