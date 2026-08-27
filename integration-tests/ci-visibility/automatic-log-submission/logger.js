f2a4168faced4f2cfffbcb5b905c10f3a8ad206b
Juan Antonio Fernández de Alba <juan.fernandezdealba@datadoghq.com>
2026-08-27 11:39:11 +0200

    test(test-optimization): cover Pino logs in Mocha, Cucumber, and Vitest (#9981)

--- integration-tests/ci-visibility/automatic-log-submission/logger.js
+++ integration-tests/ci-visibility/automatic-log-submission/logger.js
@@ -4,6 +4,8 @@ let logger
 
 if (process.env.TEST_LOGGER === 'bunyan') {
   logger = require('bunyan').createLogger({ name: 'test-logger' })
+} else if (process.env.TEST_LOGGER === 'pino') {
+  logger = require('pino')({ level: 'info' })
 } else {
   const { createLogger, format, transports } = require('winston')
   logger = createLogger({
