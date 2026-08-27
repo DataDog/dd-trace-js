03cda25f9d3422a6bb4139ad422fb57102560c8f
Juan Antonio Fernández de Alba <juan.fernandezdealba@datadoghq.com>
2026-08-27 16:47:32 +0200

    feat(test-optimization): support Pino and Bunyan in Jest (#10019)

--- integration-tests/config-jest.js
+++ integration-tests/config-jest.js
@@ -27,6 +27,14 @@ if (process.env.CONFIG_TRANSFORM) {
   config.transform = JSON.parse(process.env.CONFIG_TRANSFORM)
 }
 
+if (process.env.CONFIG_MODULE_NAME_MAPPER) {
+  config.moduleNameMapper = JSON.parse(process.env.CONFIG_MODULE_NAME_MAPPER)
+}
+
+if (process.env.CONFIG_RESOLVER) {
+  config.resolver = process.env.CONFIG_RESOLVER
+}
+
 if (process.env.JEST_THROWING_REPORTER) {
   config.reporters = ['<rootDir>/ci-visibility/jest-reporter-throws.js']
 }
