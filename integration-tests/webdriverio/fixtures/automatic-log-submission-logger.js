61c92181d463ff574f5f071c4ef93bee5b672cbf
Juan Antonio Fernández de Alba <juan.fernandezdealba@datadoghq.com>
2026-08-27 11:39:52 +0200

    test(test-optimization): cover Pino and Winston in WebdriverIO (#10016)

---
+++ integration-tests/webdriverio/fixtures/automatic-log-submission-logger.js
@@ -0,0 +1,21 @@
+'use strict'
+
+let logger
+
+if (process.env.TEST_LOGGER === 'bunyan') {
+  logger = require('bunyan').createLogger({ name: 'test-logger' })
+} else if (process.env.TEST_LOGGER === 'pino') {
+  logger = require('pino')({ level: 'info' })
+} else {
+  const { createLogger, format, transports } = require('winston')
+  logger = createLogger({
+    level: 'info',
+    exitOnError: false,
+    format: format.json(),
+    transports: [
+      new transports.Console(),
+    ],
+  })
+}
+
+module.exports = logger
