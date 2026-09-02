3775dcbaf4beb6ee9bd77bd87b08e0e563f9fa04
Juan Antonio Fernández de Alba <juan.fernandezdealba@datadoghq.com>
2026-09-02 08:17:34 +0200

    feat(instrumentation): add async Orchestrion context callbacks (#10080)

--- packages/datadog-instrumentations/src/helpers/rewriter/index.js
+++ packages/datadog-instrumentations/src/helpers/rewriter/index.js
@@ -7,6 +7,7 @@ const log = require('../../../../dd-trace/src/log')
 const { create } = require('../../../../../vendor/dist/@apm-js-collab/code-transformer')
 const {
   awaitContextCallback,
+  awaitContextCallbackAtTryStart,
   configureGraphqlJitCompileObject,
   configureGraphqlJitDeferredField,
   configureGraphqlJitExecute,
@@ -46,6 +47,7 @@ const matcherEsm = create(instrumentations, dcPolyfillEsm)
 
 for (const matcher of [matcherCjs, matcherEsm]) {
   matcher.addTransform('awaitContextCallback', awaitContextCallback)
+  matcher.addTransform('awaitContextCallbackAtTryStart', awaitContextCallbackAtTryStart)
   matcher.addTransform('waitForAsyncEnd', waitForAsyncEnd)
   matcher.addTransform('configureGraphqlJitCompileObject', configureGraphqlJitCompileObject)
   matcher.addTransform('configureGraphqlJitDeferredField', configureGraphqlJitDeferredField)
