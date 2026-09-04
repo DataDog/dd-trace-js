c12c04aaff64fffcc823d8c402a2b3f13987ec3d
Ruben Bridgewater <ruben.bridgewater@datadoghq.com>
2026-09-05 01:18:30 +0200

    refactor(postgres): isolate rewriter transforms
    Postgres.js uses whole-program transforms that do not share the generic transform contract.

--- packages/datadog-instrumentations/src/helpers/rewriter/index.js
+++ packages/datadog-instrumentations/src/helpers/rewriter/index.js
@@ -109,10 +109,12 @@ function createMatcher (moduleType) {
     configureGraphqlJitExecute,
     configureGraphqlJitRuntime,
     configureMercuriusRequest,
-    postgresQueryHandlers,
-    postgresQueryLifecycle,
     waitForAsyncEnd,
   } = require('./transforms')
+  const {
+    postgresQueryHandlers,
+    postgresQueryLifecycle,
+  } = require('./transforms/postgres')
 
   const matcher = create(instrumentations, getDcPolyfillSpecifier(moduleType))
 
