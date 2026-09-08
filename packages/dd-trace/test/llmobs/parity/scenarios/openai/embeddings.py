ba1b77031a67a1afe583dbf2f0d826d6b8a91832
matthew.brown <matthew.brown@datadoghq.com>
2026-09-08 17:08:40 +0000

    test(llmobs): request float embeddings in parity scenario

--- packages/dd-trace/test/llmobs/parity/scenarios/openai/embeddings.py
+++ packages/dd-trace/test/llmobs/parity/scenarios/openai/embeddings.py
@@ -7,5 +7,5 @@ from common import finish, start
 
 start("openai")
 client = OpenAI(api_key=os.environ["OPENAI_API_KEY"], base_url=f'{os.environ["PROVIDER_BASE_URL"]}/v1')
-client.embeddings.create(model="text-embedding-3-small", input="Parity embedding.")
+client.embeddings.create(model="text-embedding-3-small", input="Parity embedding.", encoding_format="float")
 finish()
