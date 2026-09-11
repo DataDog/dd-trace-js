c610d3fbdac88b8920eae965692b8c1f5846511d
Ilyas Shabi <ilyas.shabi@datadoghq.com>
2026-09-11 13:21:35 +0200

    feat(aiguard): support ai-vercel streaming (#10260)
    * feat(aiguard): support ai-vercel streaming
    
    * remove unecessary configurationNames
    
    * remove programmatic api

--- packages/dd-trace/src/config/generated-config-types.d.ts
+++ packages/dd-trace/src/config/generated-config-types.d.ts
@@ -68,6 +68,7 @@ export interface GeneratedConfig {
   DD_AGENTLESS_ENABLED: boolean;
   DD_AGENTLESS_LOG_SUBMISSION_ENABLED: boolean;
   DD_AGENTLESS_LOG_SUBMISSION_URL: string | undefined;
+  DD_AI_GUARD_ANALYZE_STREAM_RESPONSES_ENABLED: boolean;
   DD_API_KEY: string | undefined;
   DD_APM_FLUSH_DEADLINE_MILLISECONDS: number;
   DD_APP_KEY: string | undefined;
@@ -615,6 +616,7 @@ export interface GeneratedEnvVarConfig {
   DD_AGENTLESS_ENABLED: boolean;
   DD_AGENTLESS_LOG_SUBMISSION_ENABLED: boolean;
   DD_AGENTLESS_LOG_SUBMISSION_URL: string | undefined;
+  DD_AI_GUARD_ANALYZE_STREAM_RESPONSES_ENABLED: boolean;
   DD_AI_GUARD_BLOCK: boolean;
   DD_AI_GUARD_ENABLED: boolean;
   DD_AI_GUARD_ENDPOINT: string | undefined;
